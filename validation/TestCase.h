dc9029a856dc8455f38a6837762d757809e2fed2
Chris Berger <cberger8@yahoo.com>
2014-02-22 03:31:51 -0500

    Updated TestCase.h by removing class Check and condensing its information into class TestCase.

--- validation/TestCase.h
+++ validation/TestCase.h
@@ -17,7 +17,7 @@ enum p_check { NEVER = 0, WARNING_OR_FAILURE = 1, ALWAYS = 2 };
 /*Check is used to define an analysis of a specific output.
 It is given a grading module and expected output of a test
 and some parameters for displaying output from the results.*/
-class Check{
+/*class Check{
 public:
 	//Accessors
 	std::string filename() const { return _filename; }
@@ -25,7 +25,7 @@ public:
 	std::string expected() const { return _expected; }
 	
 	/*Calls the function designated by the function pointer and returns the
-	result if successful or -1 otherwise. The arguments of the compare*/
+	result if successful or -1 otherwise. The arguments of the compare
 	int compare(std::string &student_out, std::string &expected_out){
 		if(cmp_output != 0) return cmp_output(student_out, expected_out);
 		else return -1;
@@ -52,21 +52,30 @@ private:
 	int (*cmp_output)(std::string, std::string);
 	bool side_by_side;
 	p_check print;
-};
+};*/
+
 /*TestCase is used to define individual test cases for homeworks. These are to
 be populated using the Mutators prior to use and will be executed by the 
 validator and graded by the grader.*/
-class TestCase{
+class TestCase {
+
 public:
 	//Accessors
 	std::string title() const { return _title; }
 	std::string details() const { return _details; }
 	std::string command() const { return _command; }
+	std::string filename() const { return _filename; }
+	std::string description() const { return _description; }
+	std::string expected() const { return _expected; }
 	int points() const { return _points; }
 	bool hidden() const { return _hidden; }
 	
-	//TODO: Is this neccessary? Is this expensive?
-	std::vector<Check> checks() const { return _checks; }
+	/*Calls the function designated by the function pointer and returns the
+	result if successful or -1 otherwise. The arguments of the compare*/
+	int compare(std::string &student_out, std::string &expected_out){
+		if(cmp_output != 0) return cmp_output(student_out, expected_out);
+		else return -1;
+	}
 	
 	//Mutators for configuring the test case
 	TestCase* const setTitle(const std::string &new_title) 
@@ -77,15 +86,25 @@ public:
 		{ _command = new_command; return this; }
 	TestCase* const setPoints(int new_points)
 		{ _points = new_points; return this; }
-	TestCase* const addCheck(const Check &new_check)
-		{ _checks.push_back(new_check); return this; }
 	TestCase* const setHidden(const bool new_hidden)
 		{ _hidden = new_hidden; }
+	TestCase* const setFilename(const std::string &new_filename) 
+		{ _filename = new_filename; return this; }
+	TestCase* const setDescription(const std::string &new_desc) 
+		{ _description = new_desc; return this; }
+	TestCase* const setExpected(const std::string &new_exp) 
+		{ _expected = new_exp; return this; }
+	TestCase* const setCompare(int (*cmp)(std::string, std::string)) 
+		{ cmp_output = cmp; return this; }
+		
 private:
 	std::string _title;
 	std::string _details;
 	std::string _command;
+	std::string _filename;
+	std::string _description;
+	std::string _expected;
 	int _points;
 	bool _hidden;
-	std::vector<Check> _checks;
+	int (*cmp_output)(std::string, std::string);
 };
