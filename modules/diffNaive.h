0b263a1d60fc334da8f1e995148fe770237b8611
Jesse Freitas <freitas.jesse@gmail.com>
2014-02-23 16:44:40 -0500

    added diffNoWhiteSpace() function body.

--- modules/diffNaive.h
+++ modules/diffNaive.h
@@ -5,9 +5,21 @@ All rights reserved.
 This code is licensed using the BSD "3-Clause" license. Please refer to
 "LICENSE.md" for the full license.
 */
+
+#ifndef __DIFF__
+#define __DIFF__
+
 #include <stdlib.h>
 #include <string>
+#include <algorithm>
+#include "STRUtil.h"
 
+//TODO: Change the return type for these functions.
+
+/*diffNaive does a per character comparison including white space and new lines.
+It returns a number between 0 and 100 (inclusive) indicating the number of
+characters the student string was off by. The strings are not changed in this
+comparison.*/
 int diffNaive(const std::string& _student, const std::string& _instructor){
 	std::string student = string_trim_right(_student);
 	std::string instructor = string_trim_right(_instructor);
@@ -22,6 +34,38 @@ int diffNaive(const std::string& _student, const std::string& _instructor){
 	diff += extra;
 	return double(instructor.length() - diff) / instructor.length() * 100.0;
 }
-int diffNoWhiteSpace(std::string student, std::string instuctor){
-	return 0;
+/*diffNoWhiteSpace does a per character comparison not including white space but
+including new lines. It returns a number between 0 and 100 (inclusive) 
+indicating the number of characters the student string was off by. The strings 
+are not changed in this comparison.*/
+int diffNoWhiteSpace(const std::string& _student, const std::string& _instuctor){
+	std::string student = string_trim_right(_student);
+	std::string instructor = string_trim_right(_instructor);
+	int diff = 0;
+	int i = 0;
+	int j = 0;
+	while( i != student.size() && j != instructor.size() ){
+		if(student[i] == ' '){
+			i++; continue;
+		}
+		if(instructor[j] == ' '){
+			j++; continue;
+		}
+		if(student[i] != instructor[j])
+			diff++;
+		i++; j++;
+	} 
+	while( i != student.size() ){
+		if(student[i] != ' ')
+			diff++;
+		i++;
+	}
+	while( j != instructor.size() ){
+		if(instructor[j] != ' ')
+			diff++;
+		j++;
+	}
+	return double(instructor.length() - diff) / instructor.length() * 100.0;
 }
+
+#endif //__DIFF__
