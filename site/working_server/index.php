363feea34df6a852cc39ba8f100bb10baf8ae041
0xWinstonSmith <git@winston-smith.org>
2014-03-24 21:10:48 -0400

    Add uploading files
    Just adding files to start working on uploading (mostly for Research
    Symposium Demo).  No guarantee everything works properly yet.

---
+++ site/working_server/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>So You Want To Submit Some CS Homework?</h1>
+
+<form action="uploader.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
