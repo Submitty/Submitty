4cdaec679291c9796ad8dcccf4d0e1f0400516fb
Jesse Freitas <freitas.jesse@gmail.com>
2014-03-27 19:23:48 -0400

    fixed diffLine and diffNoSpace to use the Difference Class

--- modules/modules.h
+++ modules/modules.h
@@ -1,7 +1,6 @@
-#include <../modules/diffNaive.h>
-#include <../modules/partialDiff.h>
-#include <../modules/STRutil.h>
-#include <../modules/TextDiff.h>
-#include <../modules/tokenSearch.h>
-#include <../modules/myersDiff.h>
+#include "../modules/diffNaive.h"
+#include "../modules/partialDiff.h"
+#include "../modules/STRutil.h"
+#include "../modules/tokenSearch.h"
+#include "../modules/myersDiff.h"
 
