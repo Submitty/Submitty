136e2fa75255513feb10f785f23db2b7236ce965
Samuel Seng <djsamseng@gmail.com>
2014-04-29 22:01:52 -0400

    Completed summary section of homework page

--- site/private/controller/homework.php
+++ site/private/controller/homework.php
@@ -3,8 +3,11 @@ require_once("private/controller/helper.php");
 
 //Make model function calls for homework here
 
-$points_received = 30;
-$points_possible = 30;
+$points_received = 11;
+$points_possible = 15;
 
-render("homework", array("homework_number"=>$homework_number, "last_homework"=>$last_homework, "points_possible"=>$points_possible, "points_received"=>$points_received));
+$homework_summary = array("Points for README.txt"=>"3/3", "Points for compilation"=>"4/4", "Test 1"=>"4/4", "Test 2"=>"0/4", "Automatic extra credit(w/o hidden)"=>"+0 points", "Automatic grading total"=>"11/15");
+
+
+render("homework", array("homework_number"=>$homework_number, "last_homework"=>$last_homework, "points_possible"=>$points_possible, "points_received"=>$points_received, "homework_summary"=>$homework_summary));
 ?>
--- site/private/view/homework.php
+++ site/private/view/homework.php
@@ -4,6 +4,7 @@ if ($points_possible == 0) {
 } else {
     $percent = (int)($points_received * 100 / $points_possible);
 }
+
 ?>
 
 <div class="col-md-10 col-sm-9 blog-main">
@@ -12,17 +13,9 @@ if ($points_possible == 0) {
     </div>
     <div class="panel panel-default">
         <div class="panel-body">
-            
-            <div class="progress">
-              <div style="position: absolute; width: 100%; text-align:center;">
-                <span id="centered-progress" style="margin-left: auto; text-align: center;"></span>
-              </div>
-              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percent;?>%;">
-                <span id="bar-progress"></span>
-              </div>
-            </div>
-            <div class="pull-right">
-                <div class="btn-group">
+            <div class="panel-body" style="text-align: right">
+            <span style="margin-right: 50px;" >Select Homework</span>
+                <div class="btn-group" style="text-align: left">
                   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"> Homework <?php echo $homework_number;?> <span class="caret"></span>
                   </button>
                   <ul class="dropdown-menu" role="menu">
@@ -33,9 +26,42 @@ if ($points_possible == 0) {
                     <li><a href="?page=homework&number=<?php echo $last_homework;?>">Current Homework</a></li>
                   </ul>
                 </div>
-                <span class="group-btn">
-                    <button class="btn btn-primary" type="button">Go!</button>
-                </span>
+            </div>
+            <div class="progress">
+              <div style="position: absolute; width: 100%; text-align:center;">
+                <span id="centered-progress" style="margin-left: auto; text-align: center;"></span>
+              </div>
+              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percent;?>%;">
+                <span id="bar-progress"></span>
+              </div>
+            </div>
+            <div class="panel-body">
+                            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    Summary:
+                </div>
+                <div class="col-sm-8">
+                    <ul class="list-group">
+                        <?php foreach($homework_summary as $key=>$value) {?> 
+                          <li class="list-group-item">
+                              <span class="badge"><?php echo $value;?></span>
+                                  <?php echo $key;?>
+                          </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel-body" style="text-align: right">
+                <form action="#" method="post" enctype="multipart/form-data">
+                    <label for="file" style="margin-right: 5%;">Filename:</label>
+                    <input type="file" name="file" id="file" style="display: inline" />
+                    <span class="group-btn">
+                        <input type="submit" name="submit" value="Submit" class="btn btn-primary" style="margin-top: 10px">
+                    </span>
+                </form>
+            </div>
+            </div> 
             </div>
         </div>
     </div>
