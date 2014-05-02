7e995cdfb65957d736706a4e6c20c24f17ed6498
Samuel Seng <djsamseng@gmail.com>
2014-05-01 21:25:28 -0400

    Made the homework page look a lot better. Added colors for varying degrees of success on diff comparison.  Added containers for diff comparison data to be filled in.  Updated controller to use arrays of dictionaries for cleaner acceess for filling view with data

--- site/private/controller/homework.php
+++ site/private/controller/homework.php
@@ -6,8 +6,20 @@ require_once("private/controller/helper.php");
 $points_received = 11;
 $points_possible = 15;
 
-$homework_summary = array("Points for README.txt"=>"3/3", "Points for compilation"=>"4/4", "Test 1"=>"4/4", "Test 2"=>"0/4", "Automatic extra credit(w/o hidden)"=>"+0 points", "Automatic grading total"=>"11/15");
+$homework_summary = array(
+    array("title"=>"Points for README.txt", "score"=>3, "points_possible"=>"3"),
+    array("title"=>"Points for compilation", "score"=>4, "points_possible"=>4),
+    array("title"=>"Test 1", "score"=>4, "points_possible"=>4),
+    array("title"=>"Test 2", "score"=>0, "points_possible"=>4),
+    array("title"=>"Automatic extra credit(w/o hidden)", "value"=>"+0 points"),
+    array("title"=>"Automatic grading total", "score"=>11, "points_possible"=>15)
+);
+
+$homework_tests = array(
+    array("title"=>"Test 1", "score"=>4, "points_possible"=>4),
+    array("title"=>"Test 2", "score"=>0, "points_possible"=>4)
+);
 
 
-render("homework", array("homework_number"=>$homework_number, "last_homework"=>$last_homework, "points_possible"=>$points_possible, "points_received"=>$points_received, "homework_summary"=>$homework_summary));
+render("homework", array("homework_number"=>$homework_number, "last_homework"=>$last_homework, "points_possible"=>$points_possible, "points_received"=>$points_received, "homework_summary"=>$homework_summary, "homework_tests"=>$homework_tests));
 ?>
