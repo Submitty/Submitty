666bf0a926bdefc9f4a334ef2869be7ac2dd50cb
Barb Cutler <cutler@cs.rpi.edu>
2014-08-17 23:07:08 -0400

    started to work on rlimit

--- grading/runner.cpp
+++ grading/runner.cpp
@@ -14,6 +14,11 @@
  *
 */
 
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include <stdlib.h>
 #include <string>
 #include <iostream>
@@ -23,102 +28,156 @@
 #include "modules/modules.h"
 #include "grading/TestCase.h"
 
+
 #include "grading/TestCase.cpp"   /* should not #include a .cpp file */
 
-const std::string compile_command = "g++ -g *.cpp";  /* relocated (temporarily?) from  config file */
 
-//#include "../sampleConfig/HW1/config.h"
-
-using std::ifstream;
-using std::ofstream;
-using std::string;
-using std::cout;
-using std::endl;
-using std::cerr;
-
-int execute(const string &cmd);
-string to_string(int i);
+int execute(const std::string &cmd, int seconds_to_run=5);
+std::string to_string(int i);
 
 int main(int argc, char *argv[]) {
-  cout << "Running User Code..." << endl;
+  std::cout << "Running User Code..." << std::endl;
 
   // Make sure arguments are entered correctly
   if (argc != 1) {
     // Pass in the current working directory to run the programs
-    cout << "Incorrect # of arguments:" << argc << endl;
-    cout << "Usage : " << endl << "     ./runner" << endl;
+    std::cout << "Incorrect # of arguments:" << argc << std::endl;
+    std::cout << "Usage : " << std::endl << "     ./runner" << std::endl;
     return 2;
   }
+
+
+  /*
+    // FIXME: commented out for now.   currently getting compiled in the grade_students script.
+    //  (where should it really happen??)
   if (compile_command != "") {
     int compile = execute(compile_command + " 2>.compile_out.txt");
     if (compile) {
-      cerr << "COMPILATION FAILED" << endl;
+      cerr << "COMPILATION FAILED" << std::endl;
       exit(1);
     }
   }
-  int n = 0;
-  //if (readmeTestCase != NULL)
-  //  n++;
-  //if (compileTestCase != NULL)
-  //  n++;
+  */
 
   // Run each test case and create output files
-  for (unsigned int i = 0 + n; i < num_testcases; i++) {
-    cout << testcases[i].command() << std::endl;
+  for (unsigned int i = 0; i < num_testcases; i++) {
+    std::cout << "========================================================" << std::endl;
+    std::cout << "TEST " << i+1 << " " << testcases[i].command() << std::endl;
 
-    /*
-      // MULTIPLE COMPILATION IS DONE ELSEHWERE
-
-    if (testcases[i].recompile()) {
-      int recomp = execute(testcases[i].compile_cmd() + " 2>.compile_out_" +
-                           to_string(i) + ".txt");
-      if (recomp) {
-        cerr << "Recompilation in test case " << testcases[i].title()
-             << " failed; No points will be awarded." << std::endl;
-        continue;
-      }
-    }
-    */
-
-    string cmd = testcases[i].command();
-    if (cmd != "" &&
-	cmd != "FILE_EXISTS") {
-      int exit_no =
-          execute(cmd + " 1>test" + to_string(i + 1 - n) + "_cout.txt 2>test" +
-                  to_string(i + 1 - n) + "_cerr.txt");
-
-
-
-
-
-      cerr << "WANT TO: mv "+testcases[i].raw_filename()+" "+testcases[i].filename() << std::endl;
-      
-      // append the test case # to the front of the output file
+    std::string cmd = testcases[i].command();
+    if (cmd != "" && cmd != "FILE_EXISTS") {
+      // run the command, capturing STDOUT & STDERR
+      int exit_no = execute(cmd + 
+			    " 1>test" + to_string(i + 1) + "_cout.txt" +
+			    " 2>test" + to_string(i + 1) + "_cerr.txt",
+			    testcases[i].seconds_to_run());
+      // append the test case # to the front of the output file (if it exists)
       if (testcases[i].raw_filename() != "" &&
-	  access( testcases[i].raw_filename().c_str(), F_OK|R_OK|W_OK ) != -1 /* file exists */
-	  ) {
-	assert (access( testcases[i].filename().c_str(), F_OK|R_OK|W_OK ) == -1);
+	  access( testcases[i].raw_filename().c_str(), F_OK|R_OK|W_OK ) != -1) { /* file exists */
 	execute ("mv "+testcases[i].raw_filename()+" "+testcases[i].filename());
       }
-
     }
-    
-    /*   SHOULD NOT BE RECOMPILING INSIDE OF RUNNER
-    // Recompile the original in case the executable was overwritten
-    if (testcases[i].recompile() && compile_command != "") {
-      execute(compile_command);
-    }
-    */
-
   }
+
+  std::cout << "========================================================" << std::endl;
+  std::cout << "FINISHED ALL TESTS" << std::endl;
+  // allow hwcron read access so the files can be copied back
+  execute ("find . -user untrusted -exec chmod o+r {} \\;");
 
   return 0;
 }
 
-// Executes command (from shell) and returns error code (0 = success)
-int execute(const string &cmd) { return system(cmd.c_str()); }
+// ----------------------------------------------------------------
 
-string to_string(int i) {
+// Executes command (from shell) and returns error code (0 = success)
+int execute(const std::string &cmd, int seconds_to_run) { 
+  std::cout << "IN runner.cpp, going to execute '" << cmd << "'" << std::endl;
+
+  // Forking to allow the setting of limits of RLIMITS on the command
+  int result = -1;
+  pid_t childPID = fork();
+  // ensure fork was successful
+  assert (childPID >= 0);
+
+  if (childPID == 0) {
+    // CHILD PROCESS
+
+    // limit CPU time (unfortunately this is *not* wall clock time)
+    rlimit time_limit;
+    time_limit.rlim_cur = time_limit.rlim_max = seconds_to_run*2;
+    int set_success = setrlimit(RLIMIT_CPU, &time_limit);
+    assert (set_success == 0);
+    
+
+    // want to put all child processes in the same process group so we
+    // can kill them later
+
+    // FIXME: unfortunately, if the student code also does a setpgid
+    // this could lead to runaway grandchildren processes
+    int pgrp = setpgid(getpid(), 0);
+    assert(pgrp == 0);
+
+    // FIXME: should replace this system with an exec, but dealing
+    // with stdout/stderr command line redirection will need to be
+    // handled separately.  If replaced with an exec, we can know the
+    // actual child pid and RLIMIT creation of additional processes,
+    // so then the setpgid problem noted above is eliminated.
+    int child_result = system(cmd.c_str()); 
+
+    // send the system status code back to the parent process
+    std::cout << "    child_result = " << child_result << std::endl;
+    exit(child_result);
+
+  } else {
+    // PARENT PROCESS
+    std::cout << "childPID = " << childPID << std::endl;
+    std::cout << "PARENT PROCESS START: ";
+    fflush(stdout);
+    int parent_result = system("date");
+    assert (parent_result == 0);
+    //std::cout << "  parent_result = " << parent_result << std::endl;
+
+    float elapsed = 0;
+    int status;
+    pid_t wpid = 0;
+    do {
+      wpid = waitpid(childPID, &status, WNOHANG);
+      if (wpid == 0) {
+	if (elapsed < seconds_to_run) {
+	  // sleep 1/10 of a second
+	  usleep(100000); 
+	  elapsed+= 0.1;
+	}
+	else {
+	  std::cout << "Killing child process" << childPID << " after " << elapsed << " seconds elapsed." << std::endl;
+	  // the '-' here means to kill the group
+	  kill(-childPID, SIGKILL); 
+	  usleep(1000); /* wait 1/1000th of a second for the process to die */
+	}
+      }
+    } while (wpid == 0 && elapsed <= seconds_to_run);
+    
+    if (WIFEXITED(status)) {
+      printf("Child exited, status=%d\n", WEXITSTATUS(status));
+      result = WEXITSTATUS(status);
+    }
+    else if (WIFSIGNALED(status)) {
+      printf("Child %d was terminated with a status of: %d \n", childPID, WTERMSIG(status));
+      result = WTERMSIG(status);
+    }
+
+    std::cout << "PARENT PROCESS COMPLETE: ";
+    fflush(stdout);
+    parent_result = system("date");
+    assert (parent_result == 0);
+  }
+
+  return result;
+}
+
+// ----------------------------------------------------------------
+
+std::string to_string(int i) {
   std::ostringstream tmp;
   tmp << std::setfill('0') << std::setw(2) << i;
   return tmp.str();
