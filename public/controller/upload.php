3d8610e93e938c3af84fbe7ab667871f88ebd855
Samuel Seng <djsamseng@gmail.com>
2014-09-07 12:19:46 -0400

    Updated site structure.  Removed unnecessary files

---
+++ public/controller/upload.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once("controller/helper_functions.php");
+
+
+//Upload the stuff
+
+$course = "UPLOAD_NONE_A";
+
+   if (isset($_GET["course"])) {
+   $tmp = htmlspecialchars($_GET["course"]);
+   if (!is_valid_course($tmp)) {
+       $course = "UPLOAD_NONE_B".$tmp;
+   } else {
+   $course = $tmp;
+   }
+   }
+
+if (isset($_FILES["file"])) {
+
+    if (!isset($_GET["course"])) {
+        echo "No course id";
+        exit();
+    }
+    $course = htmlspecialchars($_GET["course"]);
+    if ($course != "csci1100" &&
+        $course != "csci1200" &&
+        $course != "csci1200test" &&
+        $course != "csci1100test") {
+        echo "BAD COURSE '".$course."'";
+        exit();
+    }
+
+    if (!isset($_GET["assignment_id"])) {
+        echo "No assignment id";
+        exit();
+    }
+    $assignment_id = htmlspecialchars($_GET["assignment_id"]);
+    if (strpos($assignment_id," ")) {
+        //Go to error page?
+        echo "Invalid assignment id";
+        exit();
+    }
+    $uploaded_file = $_FILES["file"];//THIS NEEDS TO BE MADE HACKER PROOF
+    $result = upload_homework($_SESSION["id"], $course, $assignment_id,$uploaded_file);
+    if (isset($result["error"])) {
+        //Go to error page?
+        if ($result["error"] == "assignment_closed") {
+            $_SESSION["status"] = "assignment_closed";
+            header("Location: index.php?page=displaymessage&course=".$course."&assignment_id=".$assignment_id);
+            exit();
+        }
+        $_SESSION["status"] = isset($result["message"]) ? $result["message"] : "upload_failed";
+        header("Location: index.php?page=displaymessage&course=".$course."&assignment_id=".$assignment_id);
+        exit();
+    }
+}
+//Go back to homework page
+$_SESSION["status"] = "uploaded_no_error";
+header("Location: index.php?page=displaymessage&course=".$course."&assignment_id=".$assignment_id);
+
+?>
---
+++ public/view/nav_container.php
@@ -0,0 +1,86 @@
+<?php $link_absolute = "https://www.cs.rpi.edu/academics/courses/spring14/ds/";?>
+<title>CSCI 1200 Data Structures - Spring 2014</title>
+
+<body height=100% leftmargin=0 rightmargin=0 topmargin=0 bottommargin=0 bgcolor=228833>
+
+<table cellpadding=10 border=0 cellspacing=0 width=100% height=100% background="resources/background_image.jpg"> 
+
+<tr>
+<td class=upper_left_corner align=center>
+<a href="http://www.cs.rpi.edu">
+<img border = 0 src="resources/rpi_cs_transparent_black.png" width=200 height=40>
+</a>
+</td>
+<td class=top_and_left_bars width=100% align=right>
+<b>CSCI 1200 Data Structures<br>Spring 2014</b>
+</td>
+</tr>
+
+<tr valign=top><td class=top_and_left_bars align=top height=100%>
+
+<p>
+<a href="<?php echo $link_absolute;?>index.php">Home</a><br> 
+&nbsp;&nbsp;Contact Information<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>announcements.php">Announcements</a><br>
+&nbsp;&nbsp;<a href="http://lms.rpi.edu/">Discussion Forum (LMS)</a>
+</p>
+
+<p>
+<a href="<?php echo $link_absolute;?>syllabus.php">Syllabus</a><br>
+&nbsp;&nbsp;Learning Outcomes<br>
+&nbsp;&nbsp;Prerequistites<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>grading.php">Course Grades</a>
+</p>
+
+<p>
+<a href="<?php echo $link_absolute;?>calendar.php">Calendar</a><br>
+&nbsp;&nbsp;Lecture notes<br>
+&nbsp;&nbsp;Lab materials<br>
+&nbsp;&nbsp;Homework<br>
+&nbsp;&nbsp;Test reviews 
+</p>
+
+<p>
+<a href="<?php echo $link_absolute;?>schedule.php">Weekly Schedule</a><br>
+&nbsp;&nbsp;Office Hours</a><br>
+&nbsp;&nbsp;Lab Times
+</p>
+
+<p>
+<a href="<?php echo $link_absolute;?>getting_help.php">Getting Help</a><br>
+&nbsp;&nbsp;Tutoring<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>advice.php">Advice from TAs</a><br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>advice_from_2011_students.php">Advice from Students</a>
+</p>
+
+<p>
+<a href="<?php echo $link_absolute;?>homework.php">Homework</a><br>
+&nbsp;&nbsp;Due Date and Time<br>
+&nbsp;&nbsp;Late Day Policy<br>
+&nbsp;&nbsp;Compilers<br>
+&nbsp;&nbsp;Electronic Submission<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>hw_grading_criteria.php">HW Grading Criteria</a>
+</p>
+
+<a href="<?php echo $link_absolute;?>academic_integrity.php">Collaboration Policy & <br> 
+Academic Integrity</a><br>
+
+<p>
+<a href="<?php echo $link_absolute;?>references.php">References</a><br>
+&nbsp;&nbsp;Optional Textbooks<br>
+&nbsp;&nbsp;Web Resources<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>development_environment.php">C++ Development</a><br>
+&nbsp;&nbsp;&nbsp;&nbsp;Compilers<br>
+&nbsp;&nbsp;&nbsp;&nbsp;Code Editors & IDEs<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>other_information.php">Misc. C++ Programming</a><br>
+&nbsp;&nbsp;&nbsp;&nbsp;Command Line Args<br>
+&nbsp;&nbsp;&nbsp;&nbsp;File I/O<br>
+&nbsp;&nbsp;&nbsp;&nbsp;string &rarr; int/float<br>
+&nbsp;&nbsp;<a href="<?php echo $link_absolute;?>memory_debugging.php">Memory Debugging</a><br>
+&nbsp;&nbsp;&nbsp;&nbsp;Valgrind<br>
+&nbsp;&nbsp;&nbsp;&nbsp;Dr. Memory
+</p>
+</td>
+
+
+<td class=main_panel valign=top height=100%>
