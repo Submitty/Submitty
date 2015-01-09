5b536e81d42bcb449f7166d1f4e253177b9137f1
Barb Cutler <cutler@cs.rpi.edu>
2015-01-09 15:28:29 -0500

    cleaning up uncommited files

---
+++ Sample_Files/sample_assignments/hw1/config.h
@@ -0,0 +1,170 @@
+/* Copyright (c) 2014, Chris Berger, Jesse Freitas, Severin Ibarluzea,
+Kiana McNellis, Kienan Knight-Boehm, Sam Seng
+
+All rights reserved.
+This code is licensed using the BSD "3-Clause" license. Please refer to
+"LICENSE.md" for the full license.
+*/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "grading/TestCase.h"
+
+const std::string assignment_message = "";
+
+// Submission parameters
+const int max_submissions = 20;
+const int submission_penalty = 5;
+
+// Compile-time parameters
+const int max_clocktime = 2;		// in seconds
+const int max_cputime = 2;			// in seconds
+<<<<<<< HEAD:Sample_Files/sampleConfig/csci1200_hw1_text_justification/config.h
+const int max_submission_size = 1000;	// in KB
+const int max_output_size = 100000;	// in KB
+	// OTHERS?
+=======
+const int max_submission_size = 100000;	// in KB
+const int max_output_size = 100;	// in KB
+>>>>>>> 050c612bbd2a4f5588b0157dba112d2cbed939de:Sample_Files/sample_assignments/hw1/config.h
+
+// Grading parameters
+const int total_pts = 50;
+const int auto_pts = 25;
+const int ta_pts = 25;
+const int extra_credit_pts = 5;
+
+// Test cases
+const int num_testcases = 11;
+
+TestCase testcases[num_testcases] = {
+
+/************* README AND COMPILATION *****************/
+
+  TestCase::MakeFileExists(
+	"README",
+	"README.txt",
+	TestCasePoints(2)
+  ),
+
+  TestCase::MakeCompilation(
+			 "Compilation",
+			 "/usr/bin/g++ -Wall *.cpp -o a.out",
+			 "a.out",		// name of .exe created by student
+			 TestCasePoints(3)
+),
+
+/******************** TEST CASES **********************/
+TestCase::MakeTestCase(
+	"left justify example",							// title
+	"./justify.exe example.txt output.txt 16 flush_left",						// details
+	"./a.out example.txt output.txt 16 flush_left",	// command
+	TestCasePoints(3),
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "example_16_flush_left.txt"),			// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"right justify example",							// title
+	"./justify.exe example.txt output.txt 16 flush_right",						// details
+	"./a.out example.txt output.txt 16 flush_right",	// command
+	TestCasePoints(3),
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "example_16_flush_right.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"full justify example",							// title
+	"./justify.exe example.txt output.txt 16 full_justify",						// details
+	"./a.out example.txt output.txt 16 full_justify",	// command
+	TestCasePoints(4),
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "example_16_full_justify.txt"),			// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"left justify gettysburg address",							// title
+	"./justify.exe gettysburg_address.txt output.txt 70 flush_left",						// details
+	"./a.out gettysburg_address.txt output.txt 70 flush_left",	// command
+	TestCasePoints(2,true,false),     // hidden, not extra credit
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "gettysburg_address_70_flush_left.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"right justify gettysburg address",							// title
+	"./justify.exe gettysburg_address.txt output.txt 70 flush_right",						// details
+	"./a.out gettysburg_address.txt output.txt 70 flush_right",	// command
+	TestCasePoints(2,true,false),     // hidden, not extra credit
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "gettysburg_address_70_flush_right.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"full justify gettysburg address",							// title
+	"./justify.exe gettysburg_address.txt output.txt 70 full_justify",	// details
+	"./a.out gettysburg_address.txt output.txt 70 full_justify",	// command
+	TestCasePoints(3),
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "gettysburg_address_70_full_justify.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"full justify long word",							// title
+	"./justify.exe long_word.txt output.txt 15 full_justify",	// details
+	"./a.out long_word.txt output.txt 15 full_justify",	// command
+	TestCasePoints(3),
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "long_word_15_full_justify.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	"lengthier document",							// title
+	"./justify.exe hidden_input.txt output.txt 100 full_justify",	// details
+	"./a.out hansel_and_gretel.txt output.txt 100 full_justify",	// command
+	TestCasePoints(3,true,true),   // hidden, extra credit!
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "hansel_and_gretel_100_full_justify.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       ),
+TestCase::MakeTestCase(
+	" lots of long words",							// title
+	"./justify.exe hidden_input.txt output.txt 20 flush_right",	// details
+	"./a.out longestwords.txt output.txt 20 flush_right",	// command
+	TestCasePoints(2,true,true),   // hidden,extra credit
+	new TestCaseComparison(&myersDiffbyLinebyChar,				// compare function [V]			   ),
+	                   "output.txt",					// output file name [V]
+			   "output.txt",					// output file description
+			   "longestwords_20_flush_right.txt"),				// expected output file [V]
+	new TestCaseComparison(&warnIfNotEmpty,"cout.txt","STDOUT"),
+	new TestCaseComparison(&warnIfNotEmpty,"cerr.txt","STDERR")
+		       )
+};
+
+
+#endif
