21efebb9a61ebe753d42ee7a4d839b673f8cffeb
Barb Cutler <cutler@cs.rpi.edu>
2015-02-01 16:12:21 -0500

    course/semester checking

--- public/controller/defaults.php
+++ public/controller/defaults.php
@@ -2,7 +2,7 @@
     $default_semester="s15";
     $default_course="default_class";
     $avalible_courses=[
-        "s15" => ["csci1200","csci1100", "csci4960","csci4530"],
+        "s15" => ["csci1200","csci1100", "csci4962","csci4530"],
         "f14" => ["csci1200", "csci4960","csci4530"]
         /*,"default" => [default]*/
     ];
