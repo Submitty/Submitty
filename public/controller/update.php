e22e8462019e39e0fc5ced2f653372b739bbf2e4
kmcnellis <kiki15@me.com>
2015-02-10 12:51:52 -0500

    Fix dev team not being defined bug that caused changing active assignment bug

--- public/controller/update.php
+++ public/controller/update.php
@@ -5,7 +5,7 @@ require_once("controller/controller_functions.php");
 $course=check_course();
 $semester=check_semester();
 $class_config=check_class_config($semester,$course);
-
+$dev_team=$class_config["dev_team"];
 $assignment_id=check_assignment_id($class_config);
 $assignment_version=check_assignment_version($semester, $course, $assignment_id);
 $assignment_config = get_assignment_config($semester, $course, $assignment_id);
