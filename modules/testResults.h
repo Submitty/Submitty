50502a2bbeed9cdf3b7fd275a59ea23877d3b0f7
sihsob <bcsihsobhon@gmail.com>
2015-04-12 20:06:37 -0400

    Added header descriptions and method descriptions for documentation

--- modules/testResults.h
+++ modules/testResults.h
@@ -3,7 +3,9 @@
  * AUTHORS: Please refer to 'AUTHORS.md' for a list of contributors
  * LICENSE: Please refer to 'LICENSE.md' for the conditions of using this code
  *
- * RELEVANT DOCUMENTATION:
+ * RELEVANT DOCUMENTATION: 
+ * The testResults.h acts as a super class for several other classes such as
+ * difference.h and tokens.h.
  */
 
 #ifndef __differences__testResults__
@@ -23,13 +25,39 @@ public:
   int distance;
 
   virtual void printJSON(std::ostream & file_out); // =0;
+
   //  virtual float grade() { return my_grade; } //=0;
+
+  /* GRADE METHODS */
+  /* METHOD: getGrade
+   * ARGS: none
+   * RETURN: float
+   * PURPOSE: Returns a floating point number representing grade
+   */
   float getGrade() { assert (my_grade >= 0); return my_grade; } //=0;
 
+  /* METHOD: setGrade
+   * ARGS: g - new grade to be set
+   * RETURN: void
+   * PURPOSE: Sets the current grade to a new grade passed in
+   */
   void setGrade(float g) { assert (g >= 0); my_grade = g; }
 
+  /* MESSAGE METHODS */
+  /* METHOD: get_message
+   * ARGS: none
+   * RETURN: string
+   * PURPOSE: Returns a string containing the message for the test
+   */
   std::string get_message() { return message; }
+
+  /* METHOD: setMessage
+   * ARGS: m - new message to be set
+   * RETURN: void
+   * PURPOSE: Sets the current message to the new message passed in
+   */
   void setMessage(const std::string &m) { message=m; }
+
 protected:
   std::string message;
   float my_grade;
@@ -37,6 +65,12 @@ protected:
 
 //TestResults::TestResults():distance(0){}
 
+/* METHOD: printJSON
+ * ARGS: ostream
+ * RETURN: void
+ * PURPOSE: Prints to a file on the server with the data of the results
+ * Extended in difference.cpp
+ */
 inline void TestResults::printJSON(std::ostream & file_out) {
 
   file_out << "{" << std::endl;
