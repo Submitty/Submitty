62fdd8326d6b0771f49c1ebc4fea0b4cf5953f55
SLiNv <zzy809@126.com>
2015-04-30 19:13:15 -0400

    + Pseudo Logout
    + Pseudo Logout proposed by @MasterOdin
    + Logout button && right justified.

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ permissions aren't lost for newly created files & directories.*/
 // GET THE USERNAME OF THE AUTHENTICATED USER
 // ============================================
 
-
+/* This is the old simple HTTP basic auth which doesn't support logout*/
+/*
 if (isset($_SERVER['PHP_AUTH_USER'])) {
     $user = $_SERVER['PHP_AUTH_USER'];
 } else if (isset($_SERVER['REMOTE_USER'])) {
@@ -16,15 +17,54 @@ if (isset($_SERVER['PHP_AUTH_USER'])) {
 } else {
     // if not already authenticated do it
     //
-    echo 'Internal Error - Not Authenticated'; exit();//here
-    //
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+	header('WWW-Authenticate: Basic realm=HWServer'); 
+	header('HTTP/1.0 401 Unauthorized'); 
+	exit;
+    } else { 
+	$user = $_SERVER['PHP_AUTH_USER'];}
 }
+*/
+
+// ==============================================
+// HTTP BASIC AUTHENTICATION WITH "PSEUDO" LOGOUT
+// ==============================================
+session_start();
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    echo "Logged out";
+    exit();
+}
+if (!isset($_SESSION['logged_in'])) {
+    $_SESSION['logged_in'] = false;
+}
+else {
+    if (isset($_SERVER['PHP_AUTH_USER'])) {
+        $user = $_SERVER['PHP_AUTH_USER'];
+        $_SESSION['logged_in'] = true;
+    } 
+    else if (isset($_SERVER['REMOTE_USER'])) {
+        $user = $_SERVER['PHP_AUTH_USER'];
+        $_SESSION['logged_in'] = true;
+    }
+}
+
+if (!$_SESSION['logged_in']) {
+    header('WWW-Authenticate: Basic realm=HWServer'); 
+    header('HTTP/1.0 401 Unauthorized'); 
+    exit;
+}
+
 
 //Remove error reporting and ini set for production code
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
+/*session starts here if using the old HTTP Auth without 
+                                        "pseudo" logout*/
+//session_start();
+
 $_SESSION["id"] = $user;
 
 if (isset($_GET["page"])) {
@@ -32,6 +72,12 @@ if (isset($_GET["page"])) {
 } else {
     $page = "homework";
 }
+
+/*logout button for pseudo logout, comment out this block 
+                              if old HTTP auth is in use*/
+echo "<div align='right'>";
+echo "<a class='btn btn-primary' href='?logout'>logout</a>";
+echo "</div>";
 
 //This needs to be wrapped around session Ids and logins
 if ($page == "upload") {
