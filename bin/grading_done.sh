1099b3c7adf61ba18015bc3135a0cf718f476642
Barb Cutler <bmcutler@gmail.com>
2015-08-22 19:55:24 -0400

    improvements to grading_done.sh script

--- bin/grading_done.sh
+++ bin/grading_done.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script is used to monitor the contents of the two grading
+# queues (interactive & batch).
+
+# USAGE:
+# ./grading_done.sh  
+#     [or]
+# ./grading_done.sh continuous
+
 # ======================================================================
 # these variables will be replaced by INSTALL.sh
 
@@ -7,39 +15,88 @@ HSS_INSTALL_DIR=__INSTALL__FILLIN__HSS_INSTALL_DIR__
 HSS_DATA_DIR=__INSTALL__FILLIN__HSS_DATA_DIR__
 
 # ======================================================================
+# some error checking on the queues (& permissions of this user)
+
+INTERACTIVE_QUEUE=$HSS_DATA_DIR/to_be_graded_interactive
+BATCH_QUEUE=$HSS_DATA_DIR/to_be_graded_batch
+
+if [ ! -d "$INTERACTIVE_QUEUE" ]; then
+    echo "ERROR: interactive queue $INTERACTIVE_QUEUE does not exist"
+    exit
+fi
+
+if [ ! -d "$BATCH_QUEUE" ]; then 
+    echo "ERROR: batch queue $BATCH_QUEUE does not exist"
+    exit
+fi
+
+if [ ! -r "$INTERACTIVE_QUEUE" ]; then
+    # most instructors do not have read access to the interactive queue
+    echo "WARNING: interactive queue $INTERACTIVE_QUEUE is not readable"
+fi
+
+if [ ! -r "$BATCH_QUEUE" ]; then 
+    echo "ERROR: batch queue $BATCH_QUEUE is not readable"
+    exit
+fi
+
+# ======================================================================
+# by default, this script quits when the queues are both empty
+# optional argument will allow this to run continuously
+
+continuous=false
+if [ "$#" -eq 1 ] && [ "$1" = "continuous" ]; then 
+    continuous=true
+fi
+
+
+# ======================================================================
+
 
 while true; do
-    interactive_count=`ls -1 2>/dev/null $HSS_DATA_DIR/to_be_graded_interactive/* | wc -l`
-    interactive_grading_count=`ls -1 2>/dev/null $HSS_DATA_DIR/to_be_graded_interactive/GRADING* | wc -l`
 
-    batch_count=`ls -1 2>/dev/null $HSS_DATA_DIR/to_be_graded_batch/* | wc -l`
-    batch_grading_count=`ls -1 2>/dev/null $HSS_DATA_DIR/to_be_graded_batch/GRADING* | wc -l`
 
-    printf 'INTERACTIVE todo:%3d '    $[$interactive_count-$interactive_grading_count] 
-    if [ $interactive_grading_count -eq "0" ]; then
-	printf '                 '
-    else
-	printf '(grading:%3d)    '    $interactive_grading_count 
+    pgrep_results=$(pgrep grade_students)
+    pgrep_results=( $pgrep_results ) # recast as array
+    numparallel=${#pgrep_results[@]} # count elements in array
+
+
+    # collect the counts from the two queues
+    interactive_count=`ls -1 2>/dev/null $INTERACTIVE_QUEUE/* | wc -l`
+    interactive_grading_count=`ls -1 2>/dev/null $INTERACTIVE_QUEUE/GRADING* | wc -l`
+    batch_count=`ls -1 2>/dev/null $BATCH_QUEUE/* | wc -l`
+    batch_grading_count=`ls -1 2>/dev/null $BATCH_QUEUE/GRADING* | wc -l`
+
+
+    # print out the counts
+
+    printf 'GRADING PROCESSES:%3d       ' $numparallel  
+
+    # most instructors do not have read access to the interactive queue
+    if [ -r "$INTERACTIVE_QUEUE" ]; then
+	printf 'INTERACTIVE todo:%3d '    $[$interactive_count-$interactive_grading_count] 
+	if [ $interactive_grading_count -eq "0" ]; then
+	    printf '                 '
+	else
+	    printf '(grading:%3d)    '    $interactive_grading_count 
+	fi
     fi
     printf 'BATCH todo:%3d '          $[$batch_count-$batch_grading_count] 
-
-
     if [ ! $batch_grading_count -eq "0" ]; then
 	printf '(grading:%3d)'        $batch_grading_count 
     fi
     printf '\n'
 
 
+    # quit when the queues are empty
     total_count=$[$interactive_count+$batch_count]
-
-    if [ $total_count -eq "0" ]; then
+    if [ $total_count -eq "0" ] && ! $continuous ; then
 	exit
     fi    
-    if (( "$total_count" < "10" )); then
-	sleep 1
-    else
-	sleep 1
-    fi    
+
+
+    # pause before checking again
+    sleep 1
 
 done
 
