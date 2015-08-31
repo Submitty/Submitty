ef7c38d77e580fa61b1308471f9aeedb85294d25
Matthew Peveler <matt.peveler@gmail.com>
2015-08-31 12:02:49 -0400

    Don't display exceptions for invalid users

--- TAGradingServer/toolbox/functions.php
+++ TAGradingServer/toolbox/functions.php
@@ -71,11 +71,13 @@ else {
     $suggested_username = $_SERVER["PHP_AUTH_USER"];
 }
 $params = array($suggested_username);
-User::loadUser($suggested_username);
-$user_info = User::$user_details;
-if (!isset($user_info['user_id'])) {
+try {
+    User::loadUser($suggested_username);
+}
+catch (InvalidArgumentException $e) {
     die("Unrecognized user: {$suggested_username}. Please contact an administrator to get an account.");
 }
+$user_info = User::$user_details;
 $user_logged_in = isset($user_info['user_id']);
 $user_is_administrator = User::$is_administrator;
 $user_id = $user_info['user_id'];
