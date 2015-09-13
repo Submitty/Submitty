146ac8fcc69109f25c31763c2f4a31c261d72121
Barb Cutler <bmcutler@gmail.com>
2015-09-13 09:54:58 -0400

    add focs hw

---
+++ sample_files/sample_assignment_config/csci2200_any_homework/config.h
@@ -0,0 +1,16 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// ===================================================================================
+
+#define ASSIGNMENT_MESSAGE "No automatic grading"
+
+// Test cases
+std::vector<TestCase> testcases
+{
+
+/************* NO TEST CASES *****************/
+
+};
+
+#endif
