3382372bd3787c5196bff210bd5234c315b2d240
Matthew Peveler <matt.peveler@gmail.com>
2015-09-13 22:51:29 -0400

    revert change on master.php config

--- TAGradingServer/toolbox/configs/master.php
+++ TAGradingServer/toolbox/configs/master.php
@@ -1,15 +1,15 @@
 <?php
 // General Information
-define("__BASE_URL__", "http://localhost:8888/hwgrading");
+define("__BASE_URL__", "__INSTALL__FILLIN__TAGRADING_URL__");
 
-define("__DATABASE_HOST__", "localhost");
-define("__DATABASE_USER__", "postgres");
-define("__DATABASE_PASSWORD__", "postgres");
+define("__DATABASE_HOST__", "__INSTALL__FILLIN__DATABASE_HOST__");
+define("__DATABASE_USER__", "__INSTALL__FILLIN__DATABASE_USER__");
+define("__DATABASE_PASSWORD__", "__INSTALL__FILLIN__DATABASE_PASSWORD__");
 
 define("__SUBMISSION_GRACE_PERIOD_SECONDS__", 30 * 60);
 define("__OUTPUT_MAX_LENGTH__", 100000);
 
-define("__DEBUG__", true);
+define("__DEBUG__", false);
 
 define("__LOG_EXCEPTIONS__", true);
-define("__LOG_PATH__", __DIR__."/../../../logs");
+define("__LOG_PATH__", "__INSTALL__FILLIN__TAGRADING_LOG_PATH__");
