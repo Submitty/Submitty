680601562da230ece6cd093a418f0becc5038cf4
Matthew Peveler <matt.peveler@gmail.com>
2015-10-15 12:46:30 -0400

    remove master.php config file and replace with template

--- CONFIGURE.sh
+++ CONFIGURE.sh
@@ -2,12 +2,35 @@
 
 ########################################################################################################################
 ########################################################################################################################
-# this script must be run by root or sudo 
+# this script must be run by root or sudo
 if [[ "$UID" -ne "0" ]] ; then
     echo "ERROR: This script must be run by root or sudo"
     exit
 fi
 
+if [[ "$#" -eq 0 ]] ; then
+    echo -e "\n\nWelcome to the Homework Submission Server (HSS) Configuration\n\n"
+    # defaults above are fine
+else
+	echo -e "\n\nERROR: This script should be run with zero arguments\n\n"
+	exit
+fi
+
+echo "What is the database host?"
+read DATABASE_HOST
+
+echo "What is the database user?"
+read DATABASE_USER
+
+echo "What is the database password for the database user $DATABASE_USER?"
+read DATABASE_PASSWORD
+
+echo "What is the url for the Grading Server?"
+read TAGRADING_URL
+
+echo "What is the SVN path to be used?"
+# SVN_PATH=svn+ssh://csci2600svn.cs.rpi.edu/var/lib/svn/csci2600
+read SVN_PATH
 
 ########################################################################################################################
 ########################################################################################################################
@@ -26,57 +49,22 @@ TAGRADING_REPOSITORY=$HSS_REPOSITORY/TAGradingServer
 HSS_INSTALL_DIR=/usr/local/hss
 HSS_DATA_DIR=/var/local/hss
 
-#FIXME: When multiple courses use SVN, this will need to be updated...
-SVN_PATH=svn+ssh://csci2600svn.cs.rpi.edu/var/lib/svn/csci2600
+# Log locations
+TAGRADING_LOG_PATH=$HSS_DATA_DIR/tagrading_logs/
+AUTOGRADING_LOG_PATH=$HSS_DATA_DIR/autograding_logs/
 
 # recommended names for special users & groups related to the HSS system
 HWPHP_USER=hwphp
 HWCRON_USER=hwcron
 HWCRONPHP_GROUP=hwcronphp
 COURSE_BUILDERS_GROUP=course_builders
-DATABASE_USER=hsdbu
 
-UNTRUSTED_UID=`id -u untrusted` # untrusted's user id 
+UNTRUSTED_UID=`id -u untrusted` # untrusted's user id
 UNTRUSTED_GID=`id -g untrusted` # untrusted's group id
-HWCRON_UID=`id -u hwcron`       # hwcron's user id 
+HWCRON_UID=`id -u hwcron`       # hwcron's user id
 HWCRON_GID=`id -g hwcron`       # hwcron's group id
-HWPHP_UID=`id -u hwphp`         # hwphp's user id 
+HWPHP_UID=`id -u hwphp`         # hwphp's user id
 HWPHP_GID=`id -g hwphp`         # hwphp's group id
-
-########################################################################################################################
-########################################################################################################################
-
-if [[ "$#" -eq 0 ]] ; then
-    echo -e "\n\nWelcome to the Homework Submission Server (HSS) Default Configuration"
-    echo -e "(rerun this script with a single argument "custom" to customize the installation\n"
-    # defaults above are fine
-else
-    if [[ "$#" -ne 1 || $1 != "custom" ]] ; then
-	echo -e "\n\nERROR: This script should be run with zero arguments or a single argument "custom"\n\n"
-	exit
-    fi
-    echo -e "\n\nWelcome to the Homework Submission Server (HSS) Interactive Custom Configuration"
-    # FIXME: query user to ask if they would like to change the defaults above
-    echo -e "Sorry, the interactive script is not written yet....  you are stuck with the defaults.\n"
-fi
-
-
-########################################################################################################################
-########################################################################################################################
-
-
-echo "What is the database password for the database user $DATABASE_USER?"
-read DATABASE_PASSWORD
-
-
-# assumptions
-
-DATABASE_HOST=csdb3
-TAGRADING_URL=https://hwgrading.cs.rpi.edu/
-TAGRADING_LOG_PATH=$HSS_DATA_DIR/tagrading_logs/
-
-AUTOGRADING_LOG_PATH=$HSS_DATA_DIR/autograding_logs/
-
 
 ########################################################################################################################
 ########################################################################################################################
@@ -84,17 +72,15 @@ AUTOGRADING_LOG_PATH=$HSS_DATA_DIR/autograding_logs/
 # FIXME: DO SOME ERROR CHECKING ON THE VARIABLE SETTINGS
 #        (variables are different from each other, directories valid/exist/writeable, etc)
 
-
 # copy the installation script
+rm $HSS_REPOSITORY/INSTALL.sh > /dev/null 2>&1
 cp $HSS_REPOSITORY/bin/INSTALL_template.sh $HSS_REPOSITORY/INSTALL.sh
 
-
-# set the permissions of this file 
+# set the permissions of this file
 chown root:root $HSS_REPOSITORY/INSTALL.sh
 chmod 500 $HSS_REPOSITORY/INSTALL.sh
 
-
-# fillin the necessary variables 
+# fillin the necessary variables
 sed -i -e "s|__CONFIGURE__FILLIN__HSS_REPOSITORY__|$HSS_REPOSITORY|g" $HSS_REPOSITORY/INSTALL.sh
 sed -i -e "s|__CONFIGURE__FILLIN__TAGRADING_REPOSITORY__|$TAGRADING_REPOSITORY|g" $HSS_REPOSITORY/INSTALL.sh
 sed -i -e "s|__CONFIGURE__FILLIN__HSS_INSTALL_DIR__|$HSS_INSTALL_DIR|g" $HSS_REPOSITORY/INSTALL.sh
