1801b966370cb64b2250c361513564b365d654ce
sihsob <bcsihsobhon@gmail.com>
2016-03-10 14:07:22 -0500

    Test files for various error messages

---
+++ sample_files/test_errors/infiniteloop_with_output.cpp
@@ -0,0 +1,19 @@
+/* OUTPUT: ERROR: FILE SIZE LIMIT EXCEEDED
+ *         Program Terminated
+ *         (RED) ERROR! Student file too large for grader
+ * Child exited with signal 25 or 31
+ * Note: Termined beause it hit the max file size set by instructor
+ * Message located in execute.cpp
+ */
+
+#include <iostream>
+
+int main()
+{
+    while(true)
+    {
+        std::cout << "Hello" << std::endl;
+    }
+
+    return 0;
+}
