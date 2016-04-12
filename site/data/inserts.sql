921affd44e7ab7b096f3a58807f46fb88851cfe6
Matthew Peveler <matt.peveler@gmail.com>
2016-04-12 17:02:36 -0400

    add group inserts

--- site/data/inserts.sql
+++ site/data/inserts.sql
@@ -6,15 +6,25 @@
 --
 -- Config Inserts
 --
-INSERT INTO config (config_name, config_type, config_value) VALUES ('course_name',4,'My Course Name');
-INSERT INTO config (config_name, config_type, config_value) VALUES ('default_hw_late_days',1,'2');
-INSERT INTO config (config_name, config_type, config_value) VALUES ('default_student_late_days',1,'3');
-INSERT INTO config (config_name, config_type, config_value) VALUES ('use_autograder',3,'true');
-INSERT INTO config (config_name, config_type, config_value) VALUES ('generate_diff',3,'true');
-INSERT INTO config (config_name, config_type, config_value) VALUES ('zero_rubric_grades',3,'false');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('course_name', 4, 'My Course Name');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('default_hw_late_days', 1, '2');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('default_student_late_days', 1, '3');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('use_autograder', 3, 'true');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('generate_diff', 3, 'true');
+INSERT INTO config (config_name, config_type, config_value) VALUES ('zero_rubric_grades', 3, 'false');
 
 
 --
 -- Section Inserts
 --
 INSERT INTO sections (section_number, section_title, section_is_enabled) VALUES (-1, 'Disabled Section', 0);
+
+
+--
+-- Group Inserts
+--
+INSERT INTO groups (group_number, group_name) VALUES (1, 'Student');
+INSERT INTO groups (group_number, group_name) VALUES (2, 'Grader');
+INSERT INTO groups (group_number, group_name) VALUES (3, 'TA');
+INSERT INTO groups (group_number, group_name) VALUES (4, 'Instructor');
+INSERT INTO groups (group_number, group_name) VALUES (5, 'Developer');
