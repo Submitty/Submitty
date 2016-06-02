5ef108a4f7cb5858a5ecf6bad77d59126bc0e0bd
Samuel Breese <samuel@chame.co>
2016-06-02 14:44:10 -0400

    Modify INSTALL_template

--- tests/integrationTests/scripts/run.py
+++ tests/integrationTests/scripts/run.py
@@ -7,23 +7,6 @@ import subprocess
 
 import lib
 
-# Ensure the correct paths for loading modules are recognized by
-# Python. We require both the test packages themselves (which are
-# are stored outside of the repository), as well as the "lib"
-# module residing within the repository in the scripts directory.
-
-tests_dir = "__INSTALL__FILLIN__HSS_REPOSITORY__/tests/integrationTests/tests"
-grading_source_dir = "__INSTALL__FILLIN__HSS_INSTALL_DIR__/src/grading"
-autograde_install_dir = "__INSTALL__FILLIN__HSS_DATA_DIR__/autograde_tests/"
-
-sys.path.insert(0, autograde_install_dir)
-
-# The directory containing lib.py should be the same as the one
-# that contains this file.
-sys.path.insert(0, "__INSTALL__FILLIN__HSS_INSTALL_DIR__/bin")
-
-# Load all test packages, which will populate the dictionary in
-# the "lib" module.
 import tests
 
 # If we were given an argument, run only the test with that name.
