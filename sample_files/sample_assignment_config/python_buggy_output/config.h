8a21ce5c791bc930b26677f0cb39a6ebe23a83d9
Barb Cutler <bmcutler@gmail.com>
2016-06-04 23:31:44 -0400

    added a new code file with reordered lines to the python_buggy_output sample that shows off diffLineSwapOk grader

--- sample_files/sample_assignment_config/python_buggy_output/config.h
+++ sample_files/sample_assignment_config/python_buggy_output/config.h
@@ -1,10 +1,10 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define ASSIGNMENT_MESSAGE "Test of LineHighlight"
+#define ASSIGNMENT_MESSAGE "Test of diffLineSwapOk compared to myersDiffbyLinebyChar"
 
-#define TOTAL_POINTS 40
-#define AUTO_POINTS 40
+#define TOTAL_POINTS 90
+#define AUTO_POINTS 90
 #define TA_POINTS 0
 #define EXTRA_CREDIT_POINTS 0
 
@@ -24,37 +24,88 @@ std::vector<TestCase> testcases
      }
     ),
    
+
   TestCase::MakeTestCase
-    ("DUPLICATE LINES",
+    ("DUPLICATE LINES - Required Order",
      "gettysburg_address.txt",
      "/usr/bin/python code_duplicate_lines.py gettysburg_address.txt output_duplicates.txt",
      TestCasePoints(10),
-     { new TestCaseComparison(&diffLineSwapOk,        "output_duplicates.txt","","output_instructor.txt",0.5),
-       new TestCaseComparison(&myersDiffbyLinebyChar, "output_duplicates.txt","","output_instructor.txt",0.5),
+     { new TestCaseComparison(&myersDiffbyLinebyChar, "output_duplicates.txt","","output_instructor.txt",1.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
+     }
+    ),
+  TestCase::MakeTestCase
+    ("DUPLICATE LINES - Re-Ordering OK",
+     "gettysburg_address.txt",
+     "/usr/bin/python code_duplicate_lines.py gettysburg_address.txt output_duplicates.txt",
+     TestCasePoints(10),
+     { new TestCaseComparison(&diffLineSwapOk,        "output_duplicates.txt","","output_instructor.txt",1.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
      }
     ),
 
+
   TestCase::MakeTestCase
-    ("EXTRA LINES",
+    ("EXTRA LINES - Required Order",
      "gettysburg_address.txt",
      "/usr/bin/python code_extra_lines_no_duplicates.py gettysburg_address.txt output_extra.txt",
      TestCasePoints(10),
-     { new TestCaseComparison(&diffLineSwapOk,        "output_extra.txt","","output_instructor.txt",0.5),
-       new TestCaseComparison(&myersDiffbyLinebyChar, "output_extra.txt","","output_instructor.txt",0.5),
+     { new TestCaseComparison(&myersDiffbyLinebyChar, "output_extra.txt","","output_instructor.txt",1.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
+     }
+    ),
+  TestCase::MakeTestCase
+    ("EXTRA LINES - Re-Ordering OK",
+     "gettysburg_address.txt",
+     "/usr/bin/python code_extra_lines_no_duplicates.py gettysburg_address.txt output_extra.txt",
+     TestCasePoints(10),
+     { new TestCaseComparison(&diffLineSwapOk,        "output_extra.txt","","output_instructor.txt",1.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
      }
     ),
 
+
   TestCase::MakeTestCase
-    ("MISSING LINES",
+    ("MISSING LINES - Required Order",
      "gettysburg_address.txt",
      "/usr/bin/python code_missing_lines.py gettysburg_address.txt output_missing.txt",
      TestCasePoints(10),
-     { new TestCaseComparison(&diffLineSwapOk,        "output_missing.txt","","output_instructor.txt",0.5),
-       new TestCaseComparison(&myersDiffbyLinebyChar, "output_missing.txt","","output_instructor.txt",0.5),
+     { new TestCaseComparison(&myersDiffbyLinebyChar, "output_missing.txt","","output_instructor.txt",1.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
+         }
+     ),  TestCase::MakeTestCase
+    ("MISSING LINES - Re-Ordering OK",
+     "gettysburg_address.txt",
+     "/usr/bin/python code_missing_lines.py gettysburg_address.txt output_missing.txt",
+     TestCasePoints(10),
+     { new TestCaseComparison(&diffLineSwapOk,        "output_missing.txt","","output_instructor.txt",1.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
+         }
+     ),
+
+
+  TestCase::MakeTestCase
+    ("OUT OF ORDER - Required Order",
+     "gettysburg_address.txt",
+     "/usr/bin/python code_out_of_order.py gettysburg_address.txt output_missing.txt",
+     TestCasePoints(10),
+     { new TestCaseComparison(&myersDiffbyLinebyChar, "output_missing.txt","","output_instructor.txt",1.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
+       new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
+         }
+     ),
+  TestCase::MakeTestCase
+    ("OUT OF ORDER Re-Ordering OK",
+     "gettysburg_address.txt",
+     "/usr/bin/python code_out_of_order.py gettysburg_address.txt output_missing.txt",
+     TestCasePoints(10),
+     { new TestCaseComparison(&diffLineSwapOk,        "output_missing.txt","","output_instructor.txt",1.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDOUT.txt","Standard OUTPUT (STDOUT)","",0.0),
        new TestCaseComparison(&warnIfNotEmpty,        "STDERR.txt","Standard ERROR (STDERR)","",0.0)
          }
