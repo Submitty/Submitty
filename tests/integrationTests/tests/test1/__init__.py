a172230b4b164acb92b13bd3c1531c7844d4d5a0
Samuel Breese <samuel@chame.co>
2016-06-06 12:13:52 -0400

    Fixes for #372, general code cleanup in lib.py

--- tests/integrationTests/tests/test1/__init__.py
+++
@@ -1,9 +0,0 @@
-from lib import testcase
-
-@testcase
-def correct_json_output(test):
-    test.build()
-    test.run_validator()
-    test.diff("test03_0_diff.json")
-    test.diff("test03_1_diff.json")
-    test.diff("submission.json")
