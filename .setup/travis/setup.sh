634318a35e9da8f3efcb045b2bf04361c1ea3869
Matthew Peveler <matt.peveler@gmail.com>
2016-06-20 02:04:23 -0400

    valgrind and dr memory

--- .setup/travis/setup.sh
+++ .setup/travis/setup.sh
@@ -28,7 +28,7 @@ cp "$TRAVIS_BUILD_DIR/tests/test.php" "$TRAVIS_BUILD_DIR/TAGradingServer/toolbox
 touch "$TRAVIS_BUILD_DIR/TAGradingServer/toolbox/configs/test_course.php"
 
 echo "Setting up auto-grader test suite"
-apt-get install -yqq --force-yes python automake cmake make clang gcc g++ g++-multilib libseccomp2 seccomp libseccomp-dev
+apt-get install -yqq --force-yes python automake cmake make clang gcc g++ g++-multilib libseccomp2 seccomp libseccomp-dev valgrind
 mkdir -p /usr/local/hss
 mkdir -p /var/local/hss
 mkdir -p /usr/local/hss/src
@@ -58,3 +58,14 @@ make
 popd
 
 chmod -R 777 /usr/local/hss
+
+echo "Getting DrMemory..."
+mkdir -p /usr/local/hss/DrMemory
+pushd /tmp
+DRMEM_TAG=release_1.10.1
+DRMEM_VER=1.10.1-3
+wget https://github.com/DynamoRIO/drmemory/releases/download/${DRMEM_TAG}/DrMemory-Linux-${DRMEM_VER}.tar.gz -o /dev/null > /dev/null 2>&1
+tar -xpzf DrMemory-Linux-${DRMEM_VER}.tar.gz -C /usr/local/hss/DrMemory
+ln -s /usr/local/hss/DrMemory/DrMemory-Linux-${DRMEM_VER} /usr/local/hss/drmemory
+rm DrMemory-Linux-${DRMEM_VER}.tar.gz
+popd
