9a74d972a87a7d88f30e23d1e2cfb08261aec3b2
Matthew Peveler <matt.peveler@gmail.com>
2016-06-20 14:08:58 -0400

    try and fix broken E2E tests

--- .setup/travis/setup.sh
+++ .setup/travis/setup.sh
@@ -1,65 +1,29 @@
 #!/bin/bash
 
 if [[ "$RUN_E2E" = "true" ]]; then
-    apt-get update > /dev/null
-    apt-get install -yqq --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-pgsql
+    sudo apt-get update > /dev/null
+    sudo apt-get install -yqq --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-pgsql
 
-
-    sed -i -e "s,/var/www,$(pwd),g" /etc/apache2/sites-available/default
+    sudo sed -i -e "s,/var/www,$(pwd),g" /etc/apache2/sites-available/default
     # cat /etc/apache2/sites-available/default
-    /etc/init.d/apache2 restart
+    sudo /etc/init.d/apache2 restart
 
     sh -e /etc/init.d/xvfb start
     export DISPLAY=:99.0
 
     if [ ! -f "$SELENIUM_JAR" ]; then
         echo "Downloading Selenium"
-        mkdir -p $(dirname "$SELENIUM_JAR")
-        wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
+        sudo mkdir -p $(dirname "$SELENIUM_JAR")
+        sudo wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
         echo "Downloaded Selenium"
     fi
 
     echo "Installing Firefox"
-    apt-get install firefox -yqq --no-install-recommends
+    sudo apt-get install firefox -yqq --no-install-recommends
 fi
 
 echo "Setting up config files"
 cp ${TRAVIS_BUILD_DIR}/tests/test.php ${TRAVIS_BUILD_DIR}/TAGradingServer/toolbox/configs/master.php
 touch ${TRAVIS_BUILD_DIR}/TAGradingServer/toolbox/configs/test_course.php
 
-echo "Setting up auto-grader test suite"
-apt-get install -yqq --force-yes python automake cmake make clang gcc g++ g++-multilib libseccomp2 seccomp libseccomp-dev valgrind
-mkdir -p /usr/local/hss
-mkdir -p /var/local/hss
-mkdir -p /usr/local/hss/src
-cp -r tests /usr/local/hss/test_suite
-cp -r sample_files /usr/local/hss/sample_files
-cp -r grading/ /usr/local/hss/src/
-
-sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/integrationTests/scripts/lib.py
-sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/integrationTests/scripts/run.py
-sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/system_call_check.cpp
-sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.txt
-sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.txt
-sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/CMakeLists.txt
-sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/CMakeLists.txt
-
-# building the autograding library
-mkdir -p /usr/local/hss/src/grading/lib
-pushd /usr/local/hss/src/grading/lib
-cmake ..
-make
-popd
-
-chmod -R 777 /usr/local/hss
-
-echo "Getting DrMemory..."
-mkdir -p /usr/local/hss/DrMemory
-pushd /tmp
-DRMEM_TAG=release_1.10.1
-DRMEM_VER=1.10.1-3
-wget https://github.com/DynamoRIO/drmemory/releases/download/${DRMEM_TAG}/DrMemory-Linux-${DRMEM_VER}.tar.gz -o /dev/null > /dev/null 2>&1
-tar -xpzf DrMemory-Linux-${DRMEM_VER}.tar.gz -C /usr/local/hss/DrMemory
-ln -s /usr/local/hss/DrMemory/DrMemory-Linux-${DRMEM_VER} /usr/local/hss/drmemory
-rm DrMemory-Linux-${DRMEM_VER}.tar.gz
-popd
+sudo source ${BEFORE_SCRIPT_DIR}/travis/autograder.sh
