684934939996a1bf9ef277b16d4759ffcb3afad0
Matthew Peveler <matt.peveler@gmail.com>
2016-06-20 00:22:38 -0400

    fix some parts of the travis script

--- .setup/travis/setup.sh
+++ .setup/travis/setup.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
 if [[ "$RUN_E2E" = "true" ]]; then
-    sudo apt-get update > /dev/null
-    sudo apt-get install -yqq --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-pgsql python
+    apt-get update > /dev/null
+    apt-get install -yqq --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-pgsql
 
-    sudo sed -i -e "s,/var/www,$(pwd),g" /etc/apache2/sites-available/default
+
+    sed -i -e "s,/var/www,$(pwd),g" /etc/apache2/sites-available/default
     # cat /etc/apache2/sites-available/default
-    sudo /etc/init.d/apache2 restart
+    /etc/init.d/apache2 restart
 
     sh -e /etc/init.d/xvfb start
     export DISPLAY=:99.0
 
     if [ ! -f "$SELENIUM_JAR" ]; then
         echo "Downloading Selenium"
-        sudo mkdir -p $(dirname "$SELENIUM_JAR")
-        sudo wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
+        mkdir -p $(dirname "$SELENIUM_JAR")
+        wget -nv -O "$SELENIUM_JAR" "$SELENIUM_DOWNLOAD_URL"
         echo "Downloaded Selenium"
     fi
 
     echo "Installing Firefox"
-    sudo apt-get install firefox -yqq --no-install-recommends
+    apt-get install firefox -yqq --no-install-recommends
 fi
 
 echo "Setting up config files"
 cp "$TRAVIS_BUILD_DIR/tests/test.php" "$TRAVIS_BUILD_DIR/TAGradingServer/toolbox/configs/master.php"
 touch "$TRAVIS_BUILD_DIR/TAGradingServer/toolbox/configs/test_course.php"
 
+echo "Setting up auto-grader test suite"
+apt-get install -yqq --force-yes python clang cmake make
 mkdir -p /usr/local/hss
-cp tests /usr/local/hss/test_suite
-cp sample_files /usr/local/hss/sample_files
+mkdir -p /var/local/hss
+cp -r tests /usr/local/hss/test_suite
+cp -r sample_files /usr/local/hss/sample_files
+
+
+# copy the files from the repo
+cp -r grading /usr/local/hss/src
+
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/scripts/lib.py
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/scripts/run.py
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/system_call_check.cpp
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.cpp
+sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.cpp
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/CMakeLists.cpp
+sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/CMakeLists.cpp
+
+# building the autograding library
+mkdir -p /usr/local/hss/src/grading/lib
+pushd /usr/local/hss/src/grading/lib
+cmake ..
+make
+popd
+
 chmod -R 777 /usr/local/hss
-sed -i -e "s|__INSTALL__FILLIN__HSS_REPOSITORY__|/usr/local/hss|g" /usr/local/hss/test_suite/scripts/lib.py
-sed -i -e "s|__INSTALL__FILLIN__HSS_REPOSITORY__|/usr/local/hss|g" /usr/local/hss/test_suite/scripts/run.py
