e1d919c65e9c8b29d3d43a1832777118659489bc
Matthew Peveler <matt.peveler@gmail.com>
2016-06-20 21:39:01 -0400

    Enable autograder testing within Travis (#396)
    * add code to run integration tests on travis
    
    * add sudo to the script
    
    * fix some parts of the travis script
    
    * fix some path stuff
    
    * another path fix
    
    * once more on the path
    
    * debug path stuff
    
    * fix file extension
    
    * add additional dependencies
    
    * add compiler debug
    
    * enable trusty
    
    * more dependencies
    
    * sec comp
    
    * valgrind and dr memory
    
    * remove debug lines
    
    * Updated composer script
    
    * try and fix some broken path
    
    * reset back phpunit-selenium version
    
    * try and fix broken E2E tests
    
    * try a new path
    
    * no message
    
    * try again
    
    * fix sed command
    
    * try again with updating the sed
    
    * stupid sed
    
    * add debug info
    
    * try again with the path
    
    * try and fix permissions error
    
    * try to figure out why getting 403 forbidden
    
    * find where apache is logging files
    
    * try to get the log files again
    
    * more debug
    
    * no message
    
    * no message
    
    * try again with permissions
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * fix sed path?
    
    * different delimiters
    
    * no message
    
    * no message
    
    * test all of travis build
    
    * re-enable python test suite

---
+++ .setup/travis/autograder.sh
@@ -0,0 +1,36 @@
+echo "Setting up auto-grader test suite"
+apt-get install -yqq --force-yes python automake cmake make clang gcc g++ g++-multilib libseccomp2 seccomp libseccomp-dev valgrind
+mkdir -p /usr/local/hss
+mkdir -p /var/local/hss
+mkdir -p /usr/local/hss/src
+cp -r tests /usr/local/hss/test_suite
+cp -r sample_files /usr/local/hss/sample_files
+cp -r grading/ /usr/local/hss/src/
+
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/integrationTests/scripts/lib.py
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/test_suite/integrationTests/scripts/run.py
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/system_call_check.cpp
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.txt
+sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/Sample_CMakeLists.txt
+sed -i -e "s|__INSTALL__FILLIN__HSS_INSTALL_DIR__|/usr/local/hss|g" /usr/local/hss/src/grading/CMakeLists.txt
+sed -i -e "s|__INSTALL__FILLIN__HSS_DATA_DIR__|/var/local/hss|g" /usr/local/hss/src/grading/CMakeLists.txt
+
+# building the autograding library
+mkdir -p /usr/local/hss/src/grading/lib
+pushd /usr/local/hss/src/grading/lib
+cmake ..
+make
+popd
+
+chmod -R 777 /usr/local/hss
+
+echo "Getting DrMemory..."
+mkdir -p /usr/local/hss/DrMemory
+pushd /tmp
+DRMEM_TAG=release_1.10.1
+DRMEM_VER=1.10.1-3
+wget https://github.com/DynamoRIO/drmemory/releases/download/${DRMEM_TAG}/DrMemory-Linux-${DRMEM_VER}.tar.gz -o /dev/null > /dev/null 2>&1
+tar -xpzf DrMemory-Linux-${DRMEM_VER}.tar.gz -C /usr/local/hss/DrMemory
+ln -s /usr/local/hss/DrMemory/DrMemory-Linux-${DRMEM_VER} /usr/local/hss/drmemory
+rm DrMemory-Linux-${DRMEM_VER}.tar.gz
+popd
