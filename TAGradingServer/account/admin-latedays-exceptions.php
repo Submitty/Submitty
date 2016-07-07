fadf2935606c38bb5ac16e0a573746d320fbf00a
pbailie <bailip2@rpi.edu>
2016-07-07 19:46:35 -0400

    Changes to be committed: 	modified:   TAGradingServer/account/admin-latedays-exceptions.php
    NEED TO DO:
    * Process input and upsert to DB
    * Error message view for improper CSV upload
    
    WILL NEED FIXING LATER:
    * Need scrollbar when student table view is clipped.

--- TAGradingServer/account/admin-latedays-exceptions.php
+++ TAGradingServer/account/admin-latedays-exceptions.php
@@ -6,34 +6,43 @@
 
 include "../header.php";
 
-//Retrieve view data
-$view = get_views();
-
-echo $view['head'];
-
 check_administrator();
 
 if($user_is_administrator) {
 //User is administrator -- proceed with process.
 
+	$view = new local_view();
+
 	/* Process -------------------------------------------------------------- */
 	//Check POST
+	if (isset($_POST['selected_gradeable']))
+		$g_id = substr($_POST['selected_gradeable'], 2);
+	else
+		$g_id = -1;
+		
+	//TO DO: More POST checking (to do upsert)
 
-	//print form
-	echo $view['form'];
+	//configure form
+	$gradeables_db_data = retrieve_gradeables_from_db();
+	$view->configure_form($g_id, $gradeables_db_data);
 	
-	//print student table
-	echo $view['student_review_table'];
+	//configure student table
+	$student_table_db_data = retrieve_students_from_db($g_id);
+	$view->configure_table($student_table_db_data);
+	
+	//display
+	$view->display(/*$state*/);  //$state derived from post or db data
 	
 	/* END Process ---------------------------------------------------------- */
 
 } else {
 //User is NOT administrator -- operation not permitted.  Display error.
 
-    echo $view['unauthorized'];
+    $view = new local_view();
+    $view->display('unauthorized');
+    unset($view);
 }
 
-echo $view['tail'];
 include "../footer.php";
 exit;
 
@@ -46,7 +55,8 @@ function retrieve_gradeables_from_db() {
 SELECT
 	rubric_id,
 	rubric_name
-FROM rubrics;
+FROM rubrics
+ORDER BY rubric_id DESC;
 SQL;
 
 	\lib\Database::query($sql);
@@ -60,7 +70,8 @@ SELECT
 	g_title
 FROM gradeables
 WHERE
-	gradeable_type=0";
+	gradeable_type=0"
+ORDER BY gradeable_id DESC;
 SQL;
 
 	\lib\Database::query($sql);
@@ -69,7 +80,7 @@ SQL;
 	return \lib\Database::rows();
 }
 
-/* ========================================================================== */
+/* END FUNCTION retrieve_gradeables_from_db() =============================== */
 
 function retrieve_students_from_db($gradeable_id = 0) {
 
@@ -88,7 +99,8 @@ WHERE
 AND
 	late_day_exceptions.ex_late_days IS NOT NULL
 AND
-	late_day_exceptions.ex_late_days>0;
+	late_day_exceptions.ex_late_days>0
+ORDER BY students.student_rcs ASC;
 SQL;
 
 	\lib\Database::query($sql);
@@ -112,7 +124,8 @@ AND
 AND
 	late_day_exceptions.ex_late_days IS NOT NULL
 AND
-	late_day_exceptions.ex_late_days>0;
+	late_day_exceptions.ex_late_days>0
+ORDER BY users.user_email ASC;
 SQL;
 
 	\lib\Database::query($sql);
@@ -121,18 +134,26 @@ SQL;
 	return \lib\Database::rows();
 }
 
-/* ========================================================================== */
+/* END FUNCTION retrieve_students_from_db() ================================= */
 
 function upsert(array $data) {
 
 }
 
-function get_views() {
-	$utf8_styled_x  = "&#x2718";
-	$utf8_checkmark = "&#x2714";
+/* END FUNCTION upsert() ==================================================== */
 
-	$view = array();
-	$view['head'] = <<<HTML
+class local_view {
+
+	static private $utf8_styled;
+	static private $utf8_checkmark;
+	static private $view;
+	
+	public function __construct() {
+		$this->utf8_styled_x  = "&#x2718";
+		$this->utf8_checkmark = "&#x2714";
+		$this->view = array();
+		
+		$this->view['head'] = <<<HTML
 <div id="container" style="width:100%; margin-top:40px;">
 <div class="modal hide fade in" style="display:block; margin-top:5%; z-index:100;">
 <div class="modal-header">
@@ -140,34 +161,84 @@ function get_views() {
 </div>
 HTML;
 
-	$view['tail'] = <<<HTML
+		$this->view['tail'] = <<<HTML
 </div>
 </div>	
 HTML;
 
-	//BUILD input form
-	//Retrieve rubrics/gradeables DB data
-	$rubrics = retrieve_gradeables_from_db();
-	
-	$view['form'] = <<<HTML
+		$this->view['student_not_found'] = <<<HTML
+<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
+<p><em style="color:red; font-weight:bold; font-style:normal;">
+{$this->utf8_styled_x} Student not found.</em>
+</div>
 HTML;
-         
-	//BUILD student review table of existing late day exceptions
-	$late_day_records = retrieve_students_from_db(/*$g_id*/ 3); //TODO: $g_id retrieved from POST
-	
-	if (!is_array($late_day_records) || count($late_day_records) < 1) {
-	//No late days in DB -- indicate as much.
 
-		$view['student_review_table'] = <<<HTML
+		$this->view['confirmed'] = <<<HTML
+<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
+<p><em style="color:green; font-weight:bold; font-style:normal;">
+{$this->utf8_checkmark} Late days are updated.</em>
+</div>
+HTML;
+
+		$this->view['unauthorized'] = <<<HTML
+<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
+<p><em style="color:red; font-weight:bold; font-style:normal;">
+{$this->utf8_styled_x} You are not permitted to add late day exceptions.</em>
+</div>
+HTML;
+	}
+	
+	public function configure_form($g_id, $db_data) {
+		$this->view['form'] = <<<HTML
+<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
+<form action="admin-latedays-exceptions.php" method="POST">
+<p>Select Rubric:
+<select name="selected_gradeable" onchange="this.form.submit()">
+HTML;
+
+		foreach($db_data as $index => $gradeable) {
+		
+			if ($g_id == $gradeable[0]) {
+				$this->view['form'] .= <<<HTML
+<option value="g_{$gradeable[0]}" selected="selected">{$gradeable[1]}</option>
+HTML;
+			} else {
+				$this->view['form'] .= <<<HTML
+<option value="g_{$gradeable[0]}">{$gradeable[1]}</option>
+HTML;
+			}
+		}
+
+		$this->view['form'] .= <<<HTML
+</select>
+<h4>Single Student Entry</h4>
+<table style="border:5px solid white;"><tr>
+<td style="border:5px solid white;">RCS ID:<br><input type="text" name="rcs_id"></td>
+<td style="border:5px solid white;">Late Days:<br><input type="text" name="late_days"></td>
+<td style="border:5px solid white;"><input type="submit" value="Submit"></td>
+</tr></table>
+<h4>Multiple Student Entry Via CSV Upload</h4>
+<input type="file" name="csv" onchange="this.form.submit()">
+</form>
+</div>
+HTML;
+
+	}
+	
+	public function configure_table($db_data) {
+		if (!is_array($db_data) || count($db_data) < 1) {
+		//No late days in DB -- indicate as much.
+
+			$this->view['student_review_table'] = <<<HTML
 <div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
 <p style="font-weight:bold; font-size:1.2em;">No late day exceptions are currently entered for this assignment.
 </div>
 HTML;
-	} else {
-	//Late days found in DB -- build table to display
+		} else {
+		//Late days found in DB -- build table to display
 
-		//Table HEAD
-		$view['student_review_table'] = <<<HTML
+			//Table HEAD
+			$this->view['student_review_table'] = <<<HTML
 <div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
 <table style="border:5px solid white; border-collapse:collapse; margin: 0 auto; text-align:center;">
 <caption style="caption-side:top; font-weight:bold; font-size:1.2em;">
@@ -179,10 +250,10 @@ Current Late Day Exceptions
 <th style="background:lavender; width:25%;">Late Day Exceptions</th>
 HTML;
 	
-		//Table BODY
-		$cell_color = array('white', 'aliceblue');
-		foreach ($late_day_records as $index => $record) {
-			$view['student_review_table'] .= <<<HTML
+			//Table BODY
+			$cell_color = array('white', 'aliceblue');
+			foreach ($db_data as $index => $record) {
+				$this->view['student_review_table'] .= <<<HTML
 <tr>
 <td style="background:{$cell_color[$index%2]};">{$record[0]}</td>
 <td style="background:{$cell_color[$index%2]};">{$record[1]}</td>
@@ -190,39 +261,48 @@ HTML;
 <td style="background:{$cell_color[$index%2]};">{$record[3]}</td>
 </tr>
 HTML;
-	}
+			}
 
-		//Table TAIL
-		$view['student_review_table'] .= <<<HTML
+			//Table TAIL
+			$this->view['student_review_table'] .= <<<HTML
 </table>
 </div>
 HTML;
+		}
 	}
+
+	public function display($state) {
 	
-	//%s = student_id.  Output with printf().
-	$view['student_not_found'] = <<<HTML
-<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
-<p><em style="color:red; font-weight:bold; font-style:normal;">{$utf8_styled_x} Student %s not found.</em>
-</div>
-HTML;
+		switch($state) {
+		case 'unauthorized':
+			echo $this->view['head']         .
+			     $this->view['unauthorized'] .
+			     $this->view['tail'];
+			break;
+		case 'student_not_found':
+			echo $this->view['head']                 .
+			     $this->view['student_not_found']    .
+   				 $this->view['form']                 . 
+			     $this->view['student_review_table'] .
+			     $this->view['tail'];
+			break;
+		case 'confirmed':
+			echo $this->view['head']                 .
+				 $this->view['confirmed']            . 
+				 $this->view['form']                 . 
+			     $this->view['student_review_table'] .
+			     $this->view['tail'];
+			break;
+		default:
+			echo $this->view['head']                 .
+				 $this->view['form']                 . 
+			     $this->view['student_review_table'] .
+			     $this->view['tail'];
+			break;
+		}
+	}
+}	
 
-	//%s = student_id, %d = late days, %s = gradeable.  Output with printf().
-	$view['confirmed'] = <<<HTML
-<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
-<p><em style="color:green; font-weight:bold; font-style:normal;">
-{$utf8_checkmark} Student %s now has %d late days for %s.
-</em>
-</div>
-HTML;
-
-	$view['unauthorized'] = <<<HTML
-<div class="modal-body" style="padding-top:20px; padding-bottom:20px;">
-<p><em style="color:red; font-weight:bold; font-style:normal;">{$utf8_styled_x} You are not permitted to add late day exceptions.</em>
-</div>
-HTML;
-
-	return $view;
-}
-
+/* END CLASS local_view ===================================================== */
 /* EOF ====================================================================== */
 ?>
