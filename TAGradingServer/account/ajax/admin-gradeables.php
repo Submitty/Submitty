eaf9325598fd877de250905862e8d40d208ec077
bdrumheller <drumhb@rpi.edu>
2016-07-15 14:52:30 +0000

    delete json config while when gradeable is delted

--- TAGradingServer/account/ajax/admin-gradeables.php
+++ TAGradingServer/account/ajax/admin-gradeables.php
@@ -25,6 +25,10 @@ switch($action) {
     case 'delete':
         $id = $_GET['id'];
         $db->query("DELETE FROM gradeable WHERE g_id=?",array($id));
+        $json_config = __SUBMISSION_SERVER__ . '/config/form/form_'.$id.'.json';
+        if (is_file($json_config)){
+            unlink($json_config);
+        }
         print "success|".$id;
         break;
     default:
