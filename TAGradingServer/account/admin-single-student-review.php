803274bb41fbe3c750c5b68a5f9f795c211fae4d
pbailie <bailip2@rpi.edu>
2016-08-09 19:36:54 -0400

    Changes to be committed: 	modified:   TAGradingServer/account/admin-single-student-review.php

--- TAGradingServer/account/admin-single-student-review.php
+++ TAGradingServer/account/admin-single-student-review.php
@@ -16,6 +16,36 @@ $view = new local_view();
 //Default state.
 $state = "";
 
+//Validate submission
+//Is Student ID submitted (required!)
+if (isset($_POST['student_id']) && $_POST['student_id'] !== "") {
+
+	//Is this a lookup or upsert?  (determined in *'d fields are filled or not)
+	if ((isset($_POST['first_name']) && $_POST['first_name'] === "") &&
+	    (isset($_POST['last_name'])  && $_POST['last_name']  === "") &&
+	    (isset($_POST['email'])      && $_POST['email']      === "")) {
+
+		//Do DB lookup for student by Student ID
+	} else {
+		//Validate all fields
+		//No validation on student id as pattern varies among Universities
+		if (preg_match("~^[a-zA-Z.'`\- ]+$~",                               $_POST['first_name']) &&
+		    preg_match("~^[a-zA-Z.'`\- ]+$~",                               $_POST['last_name'] ) &&
+		    preg_match("~^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+.[a-zA-Z]{2,}$~", $_POST['email']     ) &&
+		    preg_match("~^[0-9]+$~",                                        $_POST['r_section'] )) {
+		    $is_validated = true;
+		} else {
+			$is_validated = false;
+		}
+
+		if ($is_validated) {		
+			//Do upsert
+		} else {
+			$state = 'invalid_student_info';
+		}
+	}
+}
+
 //display
 $view->display($state);
 
@@ -66,6 +96,16 @@ HTML;
 {$utf8_styled_x} Student not found.</em>
 HTML;
 
+		self::$view['missing_student_id'] = <<<HTML
+<p style="margin:0; padding-bottom:20px;"><em style="color:red; font-weight:bold; font-style:normal;">
+{$utf8_styled_x} Student ID field required.</em>
+HTML;
+
+		self::$view['invalid_student_info'] = <<<HTML
+<p style="margin:0; padding-bottom:20px;"><em style="color:red; font-weight:bold; font-style:normal;">
+{$utf8_styled_x} Invalid student information.</em>
+HTML;
+
 		self::$view['upsert_done'] = <<<HTML
 <p style="margin:0; padding-bottom:20px;"><em style="color:green; font-weight:bold; font-style:normal;">
 {$utf8_checkmark} Late days are updated.</em>
@@ -87,6 +127,7 @@ HTML;
 		//validate data parameters and/or set defaults.
 		if (!is_array($db_data)) {
 			$db_data = array(
+				'student_id' => "",
 				'first_name' => "",
 				'last_name'  => "",
 				'email'      => "",
@@ -96,6 +137,10 @@ HTML;
 		} else {
 			//Some fields may be OK, and others may need to be set to default.
 			//Check each individually to be thorough.
+			if (!isset($db_data['student_id'])) {
+				$db_data['student_id'] = "";
+			}
+
 			if (!isset($db_data['first_name'])) {
 				$db_data['first_name'] = "";
 			}
@@ -123,16 +168,16 @@ HTML;
 		
 		//Construct rest of form.  Note string expansions in HTML: {}
 		self::$view['form'] = <<<HTML
-<form action="admin-latedays.php" method="POST" enctype="multipart/form-data">
-<div style="width:30%; display:block;">Student ID:<br><input type="text" name="student_id" style="width:95%;"></div>
+<form action="admin-single-student-review.php" method="POST" enctype="multipart/form-data">
+<div style="width:30%; display:block;">Student ID:<br><input type="text" name="student_id" value="{$db_data['student_id']}" style="width:95%;"></div>
 <p>To only lookup a student's enrollment, leave blank all fields marked <span style="color:red;">*</span>.
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">First Name: <span style="color:red;">*</span><br><input type="text" name="first_name" value="{$db_data['first_name']}" style="width:95%;"></div>
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Last Name: <span style="color:red;">*</span><br><input type="text" name="last_name" value="{$db_data['last_name']}" style="width:95%;"></div>
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Email: <span style="color:red;">*</span><br><input type="text" name="email" value="{$db_data['email']}" style="width:95%;"></div>
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Registered Section:<br><input type="text" name="r_section" value="{$db_data['r_section']}" style="width:95%;"></div>
 <div style="width:50%; display:inline-block; vertical-align:top; padding-right:5px;"><p><input type="checkbox" name="manual" style="position:inherit; text-align:center; padding-top:1em;" {$is_checked}> Manually Registered Student<br>
-	<p><span style="color:red;">*</span> Required <span style="font-style:italic;">only</span> when upserting student.</div>
-<div style="display:inline-block; vertical-align:top; padding-top:1.5em;"><input type="submit" value="Submit"></div>
+	<p><span style="color:red;">*</span> Required <span style="font-style:italic;">only</span> for add/updating student.</div>
+<div style="display:inline-block; vertical-align:top; padding-top:1.5em;"><input type="submit" name="submit" value="Submit"></div>
 </form>
 HTML;
 
@@ -152,10 +197,22 @@ HTML;
 			     self::$view['student_not_found'] .
 			     self::$view['tail'];
 			break;
+		case 'missing_student_id':
+			echo self::$view['head']                .
+				 self::$view['form']                . 
+				 self::$view['missing_student_id']  . 
+			     self::$view['tail'];
+			break;
+		case 'missing_required_fields':
+			echo self::$view['head']                  .
+				 self::$view['form']                  . 
+				 self::$view['invalid_student_info']  . 
+			     self::$view['tail'];
+			break;
 		case 'upsert_done':
-			echo self::$view['head']         .
-				 self::$view['form']         . 
-				 self::$view['upsert_done']  . 
+			echo self::$view['head']        .
+				 self::$view['form']        . 
+				 self::$view['upsert_done'] . 
 			     self::$view['tail'];
 			break;
 		default:
