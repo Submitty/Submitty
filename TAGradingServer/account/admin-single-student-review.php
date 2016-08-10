fe8266c4a98d9c7dc576f8d447083d7020a93469
pbailie <bailip2@rpi.edu>
2016-08-10 18:39:11 -0400

    Changes to be committed: 	modified:   TAGradingServer/account/admin-single-student-review.php

--- TAGradingServer/account/admin-single-student-review.php
+++ TAGradingServer/account/admin-single-student-review.php
@@ -1,6 +1,6 @@
 <?php
 
-//Author: Peter Bailie, Systems Programmer, RPI Computer Science, July 2016
+//Author: Peter Bailie, Systems Programmer, RPI Computer Science, August 2016
 
 /* MAIN ===================================================================== */
 
@@ -16,6 +16,8 @@ $view = new local_view();
 //Default state.
 $state = "";
 
+echo nl2br("\n\n\n\n\n" . var_export($_POST, true));
+
 //Validate submission
 //Is Student ID submitted (required!)
 if (isset($_POST['student_id']) && $_POST['student_id'] !== "") {
@@ -27,19 +29,28 @@ if (isset($_POST['student_id']) && $_POST['student_id'] !== "") {
 
 		//Do DB lookup for student by Student ID
 	} else {
-		//Validate all fields
-		//No validation on student id as pattern varies among Universities
+		//Do form data validation.
+		//No validation on student id as pattern varies among Universities.
+		//No validation on checkbox as only possible values are true or false.
+		//Email regex should match most cases (including unusual TLDs).
+		//Email regex will NOT match domains dictated by IP address.
 		if (preg_match("~^[a-zA-Z.'`\- ]+$~",                               $_POST['first_name']) &&
 		    preg_match("~^[a-zA-Z.'`\- ]+$~",                               $_POST['last_name'] ) &&
 		    preg_match("~^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+.[a-zA-Z]{2,}$~", $_POST['email']     ) &&
-		    preg_match("~^[0-9]+$~",                                        $_POST['r_section'] )) {
+		    preg_match("~^$|^[0-9]+$~",                                     $_POST['r_section'] )) {
 		    $is_validated = true;
 		} else {
 			$is_validated = false;
 		}
 
-		if ($is_validated) {		
-			//Do upsert
+		if ($is_validated) {
+			//upsert's argument expects 2D array
+			upsert(array( array( $_POST['student_id'],
+			                     $_POST['first_name'],
+			                     $_POST['last_name'],
+			                     $_POST['email'],
+								 ($_POST['r_section') === "") ? "NULL" : $_POST['r_section'],
+			                     $_POST['is_manual'] )));
 		} else {
 			$state = 'invalid_student_info';
 		}
@@ -55,6 +66,116 @@ include "../footer.php";
 exit;
 
 /* END MAIN ================================================================= */
+
+function lookup_student_in_db($student_id) {
+
+}
+
+function upsert(array $data) {
+//IN:  Data to be "upserted" as 2D array.
+//OUT: No return.  This is assumed to work.  (Server should throw an exception
+//     if this process fails)
+//PURPOSE:  "Update/Insert" data into the database.  Code capable of "batch"
+//          upserts.
+
+/* -----------------------------------------------------------------------------
+ * This SQL code was adapted from upsert discussion on Stack Overflow and is
+ * meant to be compatible with PostgreSQL prior to v9.5.
+ *
+ * 	q.v. http://stackoverflow.com/questions/17267417/how-to-upsert-merge-insert-on-duplicate-update-in-postgresql
+ * -------------------------------------------------------------------------- */
+ 
+/* -----------------------------------------------------------------------------
+ * SUGGESTION: For future-facing maintenance, maintain this code for batch 
+ *             transactions even though this page currently transacts only 
+ *             single rows of information.
+ * -------------------------------------------------------------------------- */
+
+	$student_group = "4";
+	$sql = array();
+	
+	//TEMPORARY table to hold all new values that will be "upserted"
+	/* *** NOTE: is_manual property is not yet active until DB is updated *** */
+	$sql['temp_table'] = <<<SQL
+CREATE TEMPORARY TABLE temp
+	(student_id VARCHAR(255),
+	 first_name VARCHAR(255),
+	 last_name  VARCHAR(255),
+	 email      VARCHAR(255),
+	 group      INTEGER,
+	 r_section  INTEGER,
+	 is_manual  BOOLEAN)	
+ON COMMIT DROP;
+SQL;
+
+	//INSERT new data into temporary table -- prepares all data to be upserted
+	//in a single DB transaction.
+	//NOTE: hard coded value 4 is used in temp.group and user.usergroup to
+	//      identify students.
+	for ($i=0; $i<count($data); $i++) {
+		$sql["data_{$i}"] = <<<SQL
+INSERT INTO temp VALUES (?,?,?,?,{$student_group},?,?);
+SQL;
+	}
+
+	//LOCK will prevent sharing collisions while upsert is in process.
+	$sql['lock'] = <<<SQL
+LOCK TABLE users IN EXCLUSIVE MODE;
+SQL;
+
+	//This portion ensures that UPDATE will only occur when a record already exists.
+	//TO-DO: "SET users.user_is_manual=temp.is_manual" once DB is updated.
+	$sql['update'] = <<<SQL
+UPDATE users
+SET
+	users.user_firstname=temp.first_name,
+	users.user_lastname=temp.last_name,
+	users.useremail=temp.email,
+	users.registration_section=temp.r_section,
+FROM temp
+WHERE users.user_id=temp.student_id
+SQL;
+
+	//This portion ensures that INSERT will only occur when data record is new.
+	//TO-DO: "INSERT users.user_is_manual SELECT temp.is_manual" once DB is updated.
+	$sql['insert'] = <<<SQL
+INSERT INTO users
+	(user_id,
+	 user_firstname,
+	 user_lastname,
+	 user_email,
+	 user_group,
+	 registration_section)
+SELECT
+	temp.student_id,
+	temp.first_name,
+	temp.last_name,
+	temp.email,
+	temp.group,
+	temp.r_section
+FROM temp 
+LEFT OUTER JOIN users
+	ON users.user_id=temp.student_id
+WHERE users.user_id is NULL
+SQL;
+
+	//Begin DB transaction
+	\lib\Database::beginTransaction();
+	\lib\Database::query($sql['temp_table']);
+	
+	foreach ($data as $index => $record) {
+		\lib\Database::query($sql["data_{$index}"], $record);
+	}
+	
+	\lib\Database::query($sql['lock']);
+	\lib\Database::query($sql['update']);
+	\lib\Database::query($sql['insert']);
+	\lib\Database::commit();
+	//All Done!
+	//Server will throw exception if there is a problem with DB access.
+}
+
+/* END FUNCTION upsert() ==================================================== */
 
 class local_view {
 //View class for admin-latedays.php
@@ -170,7 +291,7 @@ HTML;
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Last Name: <span style="color:red;">*</span><br><input type="text" name="last_name" value="{$db_data['last_name']}" style="width:95%;"></div>
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Email: <span style="color:red;">*</span><br><input type="text" name="email" value="{$db_data['email']}" style="width:95%;"></div>
 <div style="width:30%; display:inline-block; vertical-align:top; padding-right:10px;">Registered Section:<br><input type="text" name="r_section" value="{$db_data['r_section']}" style="width:95%;"></div>
-<div style="width:50%; display:inline-block; vertical-align:top; padding-right:5px;"><p><input type="checkbox" name="manual" style="position:inherit; text-align:center; padding-top:1em;" {$is_checked}> Manually Registered Student<br>
+<div style="width:50%; display:inline-block; vertical-align:top; padding-right:5px;"><p><input type="checkbox" name="is_manual" style="position:inherit; text-align:center; padding-top:1em;" {$is_checked}> Manually Registered Student<br>
 	<p><span style="color:red;">*</span> Required <span style="font-style:italic;">only</span> for add/updating student.</div>
 <div style="display:inline-block; vertical-align:top; padding-top:1.5em;"><input type="submit" name="submit" value="Submit"></div>
 </form>
@@ -193,9 +314,9 @@ HTML;
 			     self::$view['tail'];
 			break;
 		case 'invalid_student_info':
-			echo self::$view['head']                  .
-				 self::$view['form']                  . 
-				 self::$view['invalid_student_info']  . 
+			echo self::$view['head']                 .
+				 self::$view['form']                 . 
+				 self::$view['invalid_student_info'] . 
 			     self::$view['tail'];
 			break;
 		case 'upsert_done':
