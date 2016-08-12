5bb9592f7904728def8fd9337f06bd92bd37763f
Peter Bailie <bailip2@rpi.edu>
2016-08-12 19:53:03 -0400

    Remove commented code from TAGradingServer/index.php

--- TAGradingServer/index.php
+++ TAGradingServer/index.php
@@ -2,24 +2,3 @@
 require_once("toolbox/functions.php");
 
 header('Location: '.__BASE_URL__.'/account/index.php?course='.$_GET['course'].'&semester='.$_GET['semester']);
-
-/*
-include "header.php"; ?>
-	
-	<div id="home-container" style="margin: 0px auto; width:227px;">
-		<div class="row-fluid">	
-			<div class="span12" style="margin: 76px 0px; margin-left:12px;">
-				<a href="<?php echo $BASE_URL; ?>"><img src="toolbox/include/images/logo-wide.jpg" width="200px" height="150px"/></a>
-			</div>
-		</div>
-		
-		<div class="row-fluid">
-			<div class="span12">
-				<a href="<?php echo $BASE_URL; ?>/login.php" class="btn btn-default btn-wide" style="margin-bottom:20px; width:200px;">Login</a>
-			</div>
-		</div>
-	</div>
-	
-<?php include "footer.php"; ?> 
-*/
-
