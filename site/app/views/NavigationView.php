0b06c50a985d60e36945247fb75e8dcd09860823
Matthew Peveler <matt.peveler@gmail.com>
2016-08-14 23:45:51 -0400

    Use external link icon to link to instructions url

--- site/app/views/NavigationView.php
+++ site/app/views/NavigationView.php
@@ -15,7 +15,7 @@ class NavigationView {
     public function __construct(Core $core) {
         $this->core = $core;
     }
-
+    
     public function showGradeables($sections_to_list) {
         $return = '<div class="content"><table class="gradeable_list" style="width:100%;">';
         $ta_base_url = $this->core->getConfig()->getTABaseUrl();
@@ -24,32 +24,33 @@ class NavigationView {
         $site_url = $this->core->getConfig()->getSiteUrl();
         $return.= <<<HTML
 HTML;
-        $title_to_button_type_submission = array("FUTURE" => "btn-default", "OPEN" => "btn-primary" , "CLOSED" => "btn-danger", 
+        $title_to_button_type_submission = array("FUTURE" => "btn-default", "OPEN" => "btn-primary" , "CLOSED" => "btn-danger",
                                                  "ITEMS BEING GRADED" => "btn-default", "GRADED" => 'btn-success');
-        $title_to_button_type_grading = array("FUTURE" => "btn-default", "OPEN" => "btn-default" , "CLOSED" => "btn-default", 
+        $title_to_button_type_grading = array("FUTURE" => "btn-default", "OPEN" => "btn-default" , "CLOSED" => "btn-default",
                                                  "ITEMS BEING GRADED" => "btn-primary", "GRADED" => 'btn-danger');
         $title_to_prefix = array("FUTURE" => "OPEN DATE", "OPEN" => "SUBMIT", "CLOSED" => "CLOSED", "ITEMS BEING GRADED" => "GRADING", "GRADED" => "GRADED");
         foreach($sections_to_list as $title => $gradeable_list){
             if(count($gradeable_list)==0){
-                continue;  
-            } 
+                continue;
+            }
             $return .= <<<HTML
                                     <tr class="bar"><td colspan="4"></td></tr>
                                     <tr class="colspan"><td colspan="4">{$title}</td></tr>
 HTML;
             foreach($gradeable_list as $gradeable => $g_data){
+                /** @var Gradeable $g_data */
                 $time = ($title=="GRADED") ? "": " @ H:i";
-                $gradeable_grade_range = ($title=='GRADED' || $title=='ITEMS BEING GRADED') ? 
+                $gradeable_grade_range = ($title=='GRADED' || $title=='ITEMS BEING GRADED') ?
                                          'GRADING (due '.$g_data->getGradeStartDate()->format("m/d/y{$time}").')' : 'GRADING (open '.$g_data->getGradeReleasedDate()->format("m/d/y{$time}").")";
                 
                 if ($g_data->getType() == GradeableType::ELECTRONIC_FILE){
                     if(trim($g_data->getInstructionsURL())!=''){
-                        $gradeable_title = '<a href="'.$g_data->getInstructionsURL().'" target="_blank"><label class="has-url">'.$g_data->getName().'</label></a>';
+                        $gradeable_title = '<label class="has-url">'.$g_data->getName().'</label><a href="'.$g_data->getInstructionsURL().'" target="_blank"><i style="margin-left: 10px;" class="fa fa-external-link"></i></a>';
                     }
                     else{
                         $gradeable_title = $g_data->getName();
                     }
-                    $display_date = ($title=="FUTURE") ? $g_data->getOpenDate()->format("m/d/y{$time}") : "(due ".$g_data->getDueDate()->format("m/d/y{$time}").")";  
+                    $display_date = ($title=="FUTURE") ? $g_data->getOpenDate()->format("m/d/y{$time}") : "(due ".$g_data->getDueDate()->format("m/d/y{$time}").")";
                     $gradeable_open_range = <<<HTML
                                          <button class="btn {$title_to_button_type_submission[$title]}" style="width:100%;" onclick="location.href='{$site_url}&component=student&gradeable_id={$gradeable}';">
                                              {$title_to_prefix[$title]} {$display_date}
@@ -98,5 +99,5 @@ HTML;
                         </div>
 HTML;
         return $return;
-    }   
+    }
 }
