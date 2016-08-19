fcc8c60f7c479f24513e4ba61ef6bb13151b8f98
Matthew Peveler <matt.peveler@gmail.com>
2016-08-19 16:21:34 -0400

    add sudo to everything

--- .setup/travis/setup.sh
+++ .setup/travis/setup.sh
@@ -26,56 +26,56 @@ fi
 sudo mkdir -p ${SUBMITTY_INSTALL_DIR}
 sudo mkdir -p ${SUBMITTY_DATA_DIR}
 
-python ${DIR}/../create_untrusted_users.py
+sudo python ${DIR}/../create_untrusted_users.py
 
-adduser ta --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "ta:ta" | sudo chpasswd
-adduser instructor --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "instructor:instructor" | sudo chpasswd
-adduser instructor sudo
+sudo adduser ta --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo echo "ta:ta" | sudo chpasswd
+sudo adduser instructor --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo echo "instructor:instructor" | sudo chpasswd
+sudo adduser instructor sudo
 
-addgroup hwcronphp
-addgroup course_builders
-adduser hwphp --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-adduser hwcgi --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-adduser hwcgi hwphp
-adduser hwcgi shadow
-adduser hwcron --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-adduser hwphp hwcronphp
-adduser hwcron hwcronphp
+sudo addgroup hwcronphp
+sudo addgroup course_builders
+sudo adduser hwphp --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo adduser hwcgi --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo adduser hwcgi hwphp
+sudo adduser hwcgi shadow
+sudo adduser hwcron --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo adduser hwphp hwcronphp
+sudo adduser hwcron hwcronphp
 
-addgroup csci1000
-addgroup csci1000_tas_www
-adduser hwphp csci1000_tas_www
-adduser hwcron csci1000_tas_www
+sudo addgroup csci1000
+sudo addgroup csci1000_tas_www
+sudo adduser hwphp csci1000_tas_www
+sudo adduser hwcron csci1000_tas_www
 
-adduser instructor course_builders
-adduser ta csci1000
-adduser ta csci1000_tas_www
-adduser instructor csci1000
-adduser	instructor csci1000_tas_www
+sudo adduser instructor course_builders
+sudo adduser ta csci1000
+sudo adduser ta csci1000_tas_www
+sudo adduser instructor csci1000
+sudo adduser	instructor csci1000_tas_www
 
 sudo chown hwphp:hwphp ${SUBMITTY_INSTALL_DIR}
 sudo chown hwphp:hwphp ${SUBMITTY_DATA_DIR}
 sudo chmod 777         ${SUBMITTY_INSTALL_DIR}
 sudo chmod 777         ${SUBMITTY_DATA_DIR}
 
-echo -e "localhost
+sudo echo -e "localhost
 hsdbu
 hsdbu
 http://192.168.56.101
 http://192.168.56.101/TAGrading
 http://192.168.56.101/cgi-bin
-svn+ssh:192.168.56.103" | source ${SUBMITTY_REPOSITORY}/.setup/CONFIGURE_SUBMITTY.sh
+svn+ssh:192.168.56.103" | sudo bash ${SUBMITTY_REPOSITORY}/.setup/CONFIGURE_SUBMITTY.sh
 
-source ${SUBMITTY_INSTALL_DIR}/.setup/INSTALL_SUBMITTY.sh clean
+sudo bash ${SUBMITTY_INSTALL_DIR}/.setup/INSTALL_SUBMITTY.sh clean
 
-source ${SUBMITTY_REPOSITORY}/Docs/sample_bin/admin_scripts_setup
-echo "student" >> ${SUBMITTY_DATA_DIR}/instructors/authlist
-echo "student" >> ${SUBMITTY_DATA_DIR}/instructors/valid
-echo "smithj" >> ${SUBMITTY_DATA_DIR}/instructors/authlist
-echo "smithj" >> ${SUBMITTY_DATA_DIR}/instructors/valid
-${SUBMITTY_DATA_DIR}/bin/authonly.pl
-echo "student:student" | sudo chpasswd
-echo "smithj:smithj" | sudo chpasswd
-python ${SUBMITTY_REPOSITORY}/.setup/add_sample_courses.py csci1100
+sudo bash ${SUBMITTY_REPOSITORY}/Docs/sample_bin/admin_scripts_setup
+sudo echo "student" >> ${SUBMITTY_DATA_DIR}/instructors/authlist
+sudo echo "student" >> ${SUBMITTY_DATA_DIR}/instructors/valid
+sudo echo "smithj" >> ${SUBMITTY_DATA_DIR}/instructors/authlist
+sudo echo "smithj" >> ${SUBMITTY_DATA_DIR}/instructors/valid
+sudo ${SUBMITTY_DATA_DIR}/bin/authonly.pl
+sudo echo "student:student" | sudo chpasswd
+sudo echo "smithj:smithj" | sudo chpasswd
+sudo python ${SUBMITTY_REPOSITORY}/.setup/add_sample_courses.py csci1100
