4245a6bf88de41881892bf300784e66e1cdccdc6
Matthew Peveler <matt.peveler@gmail.com>
2016-08-30 18:46:05 -0400

    [ci skip]

--- .setup/travis/vagrant.sh
+++ .setup/travis/vagrant.sh
@@ -19,13 +19,16 @@ chmod +rx /etc/motd
 
 travis_password=`perl -e 'printf("%s\n", crypt("travis", "password"))'`
 useradd -m -p travis_password -s /bin/bash travis
-pushd /home/travis
+
 
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev \
 sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev rbenv
 
 # We use rbenv to install Ruby for the travis user to be a newer version than that comes with Ubuntu 14.04
-su travis -c "rbenv install -v 2.3.1"
-su travis -c "rbenv global 2.3.1"
+sudo -u travis bash << EOF
+pushd /home/travis
+rbenv install -v 2.3.1
+rbenv global 2.3.1
+EOF
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/travis/.bash_profile
 source /home/travis/.bash_profile
