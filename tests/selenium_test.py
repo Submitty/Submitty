3b077bc2b41596009d4e2ee3d8e7a693290f936e
Matthew Peveler <matt.peveler@gmail.com>
2016-09-09 15:23:36 -0400

    no message

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.get("http://localhost/index.php?semester=f16&course=csci1000")
+print(driver.current_url)
 print(driver.page_source)
 #assert "CSCI1000" in driver.title
 elem = driver.find_element_by_name('user_id')
