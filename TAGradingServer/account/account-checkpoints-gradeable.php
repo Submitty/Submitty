75f3aa4bff2614822ce3f28f6ce3d3b7ef566b44
Matthew Peveler <matt.peveler@gmail.com>
2016-09-10 14:37:15 -0400

    remove fixme comments

--- TAGradingServer/account/account-checkpoints-gradeable.php
+++ TAGradingServer/account/account-checkpoints-gradeable.php
@@ -16,10 +16,6 @@ if (!User::$is_administrator
         $button = "<a class='btn' href='{$BASE_URL}/account/account-checkpoints-gradeable.php?course={$_GET['course']}&semester={$_GET['semester']}&g_id={$_GET['g_id']}'>View Your Sections</a>";
     }
     else {
-// not sure why this isn't working.  it's ending up with 2 copies of the course & semester variables and giving errors.
-//        $button = "<a class='btn' href='{$BASE_URL}/account/account-checkpoints-gradeable.php?course={$_GET['course']}&semester={$_GET['semester']}&g_id={$_GET['g_id']}&all=true'>View ALL Sections</a>";
-
-// this does work.  course & semester variables are somehow added to the end.
         $button = "<a class='btn' href='{$BASE_URL}/account/account-checkpoints-gradeable.php?course={$_GET['course']}&semester={$_GET['semester']}&g_id={$_GET['g_id']}&all=true'>View ALL Sections</a>";
     }
 }
