d54b5131fd93ec5b7f658d2b5b66d27a9af47fd1
Barb Cutler <bmcutler@gmail.com>
2016-09-14 20:45:42 -0400

    remove release date check from grade summaries

--- RainbowGrades/main.cpp
+++ RainbowGrades/main.cpp
@@ -258,31 +258,27 @@ void gradeable_helper(std::ifstream& istr, GRADEABLE_ENUM g) {
 
 
 bool string_to_gradeable_enum(const std::string &s, GRADEABLE_ENUM &return_value) {
-  if (s == "hw" || s == "homework") { return_value = GRADEABLE_ENUM::HOMEWORK;          return true;  }
-  if (s == "assignment")            { return_value = GRADEABLE_ENUM::ASSIGNMENT;        return true;  }
-  if (s == "problem_set")           { return_value = GRADEABLE_ENUM::PROBLEM_SET;       return true;  }
-
-  if (s == "quiz" || s == "quizze") { return_value = GRADEABLE_ENUM::QUIZ;              return true;  }
-  if (s == "test")                  { return_value = GRADEABLE_ENUM::TEST;              return true;  }
-  if (s == "exam")                  { return_value = GRADEABLE_ENUM::EXAM;              return true;  }
-
-  if (s == "exercise")              { return_value = GRADEABLE_ENUM::EXERCISE;          return true;  }
-  if (s == "Lecture-exercise" ||
-      s == "lecture_exercise" ||
-      s == "lec_ex")      { return_value = GRADEABLE_ENUM::LECTURE_EXERCISE;  return true;  }
-  if (s == "reading")               { return_value = GRADEABLE_ENUM::READING;           return true;  }
-  if (s == "lab" || s == "Lab")     { return_value = GRADEABLE_ENUM::LAB;               return true;  }
-
-  if (s == "recitation")            { return_value = GRADEABLE_ENUM::RECITATION;        return true;  }
-  if (s == "project")               { return_value = GRADEABLE_ENUM::PROJECT;           return true;  }
-  if (s == "participation")         { return_value = GRADEABLE_ENUM::PARTICIPATION;     return true;  }
-
-
-  if (s == "instructor_note")       { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
-  if (s == "note")                  { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
-
-  if (s == "None" || s == "none" || 
-      s == "None (for Practice Only)")  { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
+  std::string s2;
+  for (unsigned int i = 0; i < s.size(); ++i) {
+    s2.push_back(std::tolower(s[i]));
+  }
+  std::replace( s2.begin(), s2.end(), '-', '_');
+  if (s2 == "hw" || s2 == "homework")          { return_value = GRADEABLE_ENUM::HOMEWORK;          return true;  }
+  if (s2 == "assignment")                      { return_value = GRADEABLE_ENUM::ASSIGNMENT;        return true;  }
+  if (s2 == "problem_set")                     { return_value = GRADEABLE_ENUM::PROBLEM_SET;       return true;  }
+  if (s2 == "quiz" || s2 == "quizze")          { return_value = GRADEABLE_ENUM::QUIZ;              return true;  }
+  if (s2 == "test")                            { return_value = GRADEABLE_ENUM::TEST;              return true;  }
+  if (s2 == "exam")                            { return_value = GRADEABLE_ENUM::EXAM;              return true;  }
+  if (s2 == "exercise")                        { return_value = GRADEABLE_ENUM::EXERCISE;          return true;  }
+  if (s2 == "lecture_exercise")                { return_value = GRADEABLE_ENUM::LECTURE_EXERCISE;  return true;  }
+  if (s2 == "reading")                         { return_value = GRADEABLE_ENUM::READING;           return true;  }
+  if (s2 == "lab")                             { return_value = GRADEABLE_ENUM::LAB;               return true;  }
+  if (s2 == "recitation")                      { return_value = GRADEABLE_ENUM::RECITATION;        return true;  }
+  if (s2 == "project")                         { return_value = GRADEABLE_ENUM::PROJECT;           return true;  }
+  if (s2 == "participation")                   { return_value = GRADEABLE_ENUM::PARTICIPATION;     return true;  }
+  if (s2 == "instructor_note" || s2 == "note") { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
+  if (s2 == "note")                            { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
+  if (s2.substr(0,4) == "none")                { return_value = GRADEABLE_ENUM::NOTE;              return true;  }
   return false;
 }
 
@@ -928,7 +924,7 @@ void load_student_grades(std::vector<Student*> &students) {
                   std::string gradeable_name = (*itr2).value("name",gradeable_id);
 		  float score = (*itr2).value("score",0.0);
 		  
-                  std::string other_note = (*itr2).value("text","");
+                  std::string other_note =  ""; //(*itr2).value("text","");
 
 		  // Search through the gradeable categories as needed to find where this item belongs
 		  // (e.g. project may be prefixed by "hw", or exam may be prefixed by "test")
