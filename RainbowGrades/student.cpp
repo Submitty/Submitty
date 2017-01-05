5b01280492562593b2f9310b6080454937d7415a
Barb Cutler <bmcutler@gmail.com>
2017-01-05 17:18:01 -0500

    recommendations and other edits

--- RainbowGrades/student.cpp
+++ RainbowGrades/student.cpp
@@ -94,12 +94,12 @@ const ItemGrade& Student::getGradeableItemGrade(GRADEABLE_ENUM g, int i) const {
 
 
 void Student::setGradeableItemGrade(GRADEABLE_ENUM g, int i, float value, 
-                                    int late_days_used, const std::string &note) {
+                                    int late_days_used, const std::string &note, const std::string &status) {
   assert (i >= 0 && i < GRADEABLES[g].getCount());
   std::map<GRADEABLE_ENUM,std::vector<ItemGrade> >::iterator itr = all_item_grades.find(g);
   assert (itr != all_item_grades.end());
   assert (int(itr->second.size()) > i);
-  itr->second[i] = ItemGrade(value,late_days_used,note);
+  itr->second[i] = ItemGrade(value,late_days_used,note,status);
 }
 
 
