3828de3a826d04f48a06c510726e166d0cd066bc
Barb Cutler <bmcutler@users.noreply.github.com>
2017-01-09 10:59:05 -0500

    site/data/tables.sql (#822)

--- site/data/tables.sql
+++ site/data/tables.sql
@@ -154,7 +154,8 @@ CREATE TABLE electronic_gradeable_data (
 CREATE TABLE electronic_gradeable_version (
     g_id character varying(255) NOT NULL,
     user_id character varying(255) NOT NULL,
-    active_version integer
+    active_version integer,
+    user_viewed_date timestamp(6) without time zone DEFAULT NULL
 );
 
 
