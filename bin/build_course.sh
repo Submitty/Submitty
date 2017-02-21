4b58944559ca186fdd1897a358314df6341605ea
Barb Cutler <bmcutler@gmail.com>
2017-02-20 21:56:34 -0500

    display url after BUILD_course.sh script

--- bin/build_course.sh
+++ bin/build_course.sh
@@ -5,6 +5,7 @@
 # VARIABLES CONFIGURED BY INSTALL.sh
 SUBMITTY_INSTALL_DIR=__CREATE_COURSE__FILLIN__SUBMITTY_INSTALL_DIR__
 SUBMITTY_DATA_DIR=__CREATE_COURSE__FILLIN__SUBMITTY_DATA_DIR__
+SUBMISSION_URL=__CREATE_COURSE__FILLIN__SUBMISSION_URL__
 
 # VARIABLES CONFIGURED BY create_course.sh
 semester=__CREATE_COURSE__FILLIN__SEMESTER__
@@ -30,5 +31,7 @@ $SUBMITTY_INSTALL_DIR/bin/make_assignments_txt_file.py $MY_COURSE_DIR/config/for
 . $MY_COURSE_DIR/ASSIGNMENTS.txt
 
 echo "done building course=$course semester=$semester"
+
+echo -e "course page url: ${SUBMISSION_URL}/index.php?semester=${semester}&course=${course}"
 
 ##########################################################################
