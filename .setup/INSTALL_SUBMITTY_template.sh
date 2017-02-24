3a0180b787e4aeaf5de9b7b82fd177188282deef
Barb Cutler <bmcutler@users.noreply.github.com>
2017-02-23 23:21:15 -0500

    Debug char changes (#909)
    * print statements
    
    * improve character change count
    
    * remove output
    
    * test case for better diff highlighting
    
    * fix test suite
    
    * fix up analysis tools installation
    
    * fixing test cases
    
    * add new assignment config to vagrant course

--- .setup/INSTALL_SUBMITTY_template.sh
+++ .setup/INSTALL_SUBMITTY_template.sh
@@ -496,12 +496,12 @@ make
 mkdir -p ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools
 mkdir -p ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/bin
 mkdir -p ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/lang
-mkdir -p ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/config
+#mkdir -p ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/config
 #rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/bin/count_node      ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/bin
 #rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/bin/count_token     ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/bin
 #rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/bin/count_function  ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/bin
 rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/lang/*              ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/lang
-rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/config/*            ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/config
+#rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/config/*            ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/config
 rsync -rtz ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_AnalysisTools/bin/*               ${SUBMITTY_INSTALL_DIR}/SubmittyAnalysisTools/bin
 
 # change permissions
