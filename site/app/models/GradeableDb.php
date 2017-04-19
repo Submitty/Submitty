a4148df197495923c474d14204aec281f414bccc
Barb Cutler <bmcutler@gmail.com>
2017-04-19 15:27:51 -0400

    fix penalty (negative) totaling on the index page

--- site/app/models/GradeableDb.php
+++ site/app/models/GradeableDb.php
@@ -96,7 +96,7 @@ class GradeableDb extends Gradeable{
                     $this->graded_tagrading += $this->components[$component_details['gc_order']]->getScore();
                 }
             }
-            $this->graded_tagrading = max(0, $this->graded_tagrading);
+            // NOTE: the TA grading total may be negative!
         }
         
         $this->grade_by_registration = $details['g_grade_by_registration'] === true;
