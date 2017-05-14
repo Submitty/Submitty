98ff4fa16e488aae67f424581f4e02a8a4c3762b
Matthew Peveler <matt.peveler@gmail.com>
2017-05-14 18:53:54 -0400

    improve code style

--- bin/untrusted_canary.py
+++ bin/untrusted_canary.py
@@ -15,30 +15,35 @@ Any user found will be printed on sys.stderr.
 import sys
 import psutil
 
-pid_list = psutil.pids()
-pid_list.reverse()
 
-untrusted_users = []
-commands = []
+def main():
+    """main function"""
+    pid_list = psutil.pids()
+    pid_list.reverse()
 
-for pid in pid_list:
-    try:
-        proc = psutil.Process(pid)
-        if 'untrusted' in proc.username() and proc.username() not in untrusted_users:
-            untrusted_users.append(proc.username())
-        elif proc.username() == 'hwcron':
-            for user in untrusted_users:
+    untrusted_users = []
+    commands = []
+
+    for pid in pid_list:
+        try:
+            proc = psutil.Process(pid)
+            if 'untrusted' in proc.username() and proc.username() not in untrusted_users:
+                untrusted_users.append(proc.username())
+            elif proc.username() == 'hwcron':
                 if '/usr/local/submitty/bin/grade_students.sh' in proc.cmdline():
                     commands.append(proc.cmdline())
-    except psutil.NoSuchProcess:
-        pass
+        except psutil.NoSuchProcess:
+            pass
 
-for untrusted in untrusted_users:
-    found = False
-    for command in commands:
-        if untrusted in command:
-            found = True
-            break
+    for untrusted in untrusted_users:
+        found = False
+        for command in commands:
+            if untrusted in command:
+                found = True
+                break
 
-    if found is False:
-        print('Undead processes belonging to {}.'.format(untrusted), file=sys.stderr)
+        if found is False:
+            print('Undead processes belonging to {}.'.format(untrusted), file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
