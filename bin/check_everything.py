fec54e6eebb8c96e69aeadef070b35b03b6edac4
Barb Cutler <bmcutler@gmail.com>
2017-05-29 17:09:30 -0400

    a bit more

--- bin/check_everything.py
+++ bin/check_everything.py
@@ -7,6 +7,7 @@ from os import stat
 from pwd import getpwuid
 from grp import getgrgid
 from stat import *
+from subprocess import call
 
 SUBMITTY_INSTALL_DIR="__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__"
 SUBMITTY_DATA_DIR="__INSTALL__FILLIN__SUBMITTY_DATA_DIR__"
@@ -18,42 +19,53 @@ print ("check_everything.py start")
 ###########################################################################
 def helper (my_path, my_owner, my_group, my_bits) : 
     print ("check "+my_path)
-    #if not os.path.isdir(my_path):
-    #    raise SystemExit("ERROR!")
     if not os.path.exists(my_path):
         raise SystemExit("ERROR! "+my_path+" does not exist")
     if getpwuid(stat(my_path).st_uid).pw_name != my_owner:
         raise SystemExit("ERROR! "+my_path+" should be owned by "+my_owner)
     if getgrgid(stat(my_path).st_gid).gr_name != my_group:
         raise SystemExit("ERROR! "+my_path+" should be group "+my_group)
-    bits = stat("/usr/local/submitty/bin/untrusted_execute")[ST_MODE]&0o777
+    bits = stat(my_path)[ST_MODE]
+    bits &= 0o777
     if bits != my_bits:
         raise SystemExit("ERROR! "+my_path+" permission is "+oct(bits)+" should be "+oct(my_bits))
 
 
-helper(SUBMITTY_INSTALL_DIR,"root","course_builders",0o550)
+###########################################################################
+
+
+# CHECK THE INSTALLATION DIRECTORY
+helper(SUBMITTY_INSTALL_DIR,"root","course_builders",0o751)
 helper(SUBMITTY_INSTALL_DIR+"/bin/untrusted_execute","root","hwcron",0o550)
 
-helper(SUBMITTY_DATA_DIR,"root","course_builders",0o550)
 
-helper(SUBMITTY_DATA_DIR+"/courses","root","course_builders",0o550)
+# CHECK THE DATA DIRECTORY
+helper(SUBMITTY_DATA_DIR,"root","course_builders",0o751)
+helper(SUBMITTY_DATA_DIR+"/courses","root","course_builders",0o751)
 
+
+# CHECK EACH COURSE
 for semester in os.listdir(SUBMITTY_DATA_DIR+"/courses"):
-    if not os.path.isdir(SUBMITTY_DATA_DIR+"/courses/"+semester):
+    semester_path=SUBMITTY_DATA_DIR+"/courses/"+semester
+    if not os.path.isdir(semester_path):
         continue
-    for course in os.listdir(SUBMITTY_DATA_DIR+"/courses/"+semester):
-        course_path=SUBMITTY_DATA_DIR+"/courses/"+semester+"/"+course
+    for course in os.listdir(semester_path):
+        course_path=semester_path+"/"+course
         if not os.path.isdir(course_path):
             continue
         c_instructor=getpwuid(stat(course_path).st_uid).pw_name
         c_group=getgrgid(stat(course_path).st_gid).gr_name 
-        print ("a course ", course, " inst=", c_instructor, " gr=",c_group)
-
-
-        helper(course_path,c_instructor,c_group,0o550)
+        print ("check course=", course, " inst=", c_instructor, " group=",c_group)
+        helper(course_path,c_instructor,c_group,0o770)
+        helper(course_path+"/ASSIGNMENTS.txt",c_instructor,c_group,0o660)
+        helper(course_path+"/test_input",c_instructor,c_group,0o770)
+        helper(course_path+"/test_output",c_instructor,c_group,0o770)
+        helper(course_path+"/test_code",c_instructor,c_group,0o770)
         helper(course_path+"/submissions","hwphp",c_group,0o750)
+        helper(course_path+"/results","hwcron",c_group,0o750)
 
 
+###########################################################################
 
 print ("check_everything.py finish")
 
