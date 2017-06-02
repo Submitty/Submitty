d00216e8761d6008e5cdd6a4b2f8230504a2265d
Barb Cutler <bmcutler@gmail.com>
2017-06-02 17:21:37 -0400

    fix grading_done.py (comments, a bunch of bugs)

--- bin/grading_done.py
+++ bin/grading_done.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 
+# ======================================================================
+# This script is used to monitor the contents of the two grading
+# queues (interactive & batch).
+
+# USAGE:
+# ./grading_done.py
+#     [or]
+# ./grading_done.sh --continuous
+
+# ======================================================================
+
 import argparse
 import os
 import subprocess
 import time
 
+# these variables will be replaced by INSTALL.sh
 SUBMITTY_INSTALL_DIR = "__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__"
 SUBMITTY_DATA_DIR = "__INSTALL__FILLIN__SUBMITTY_DATA_DIR__"
+
 INTERACTIVE_QUEUE = os.path.join(SUBMITTY_DATA_DIR, "to_be_graded_interactive")
 BATCH_QUEUE = os.path.join(SUBMITTY_DATA_DIR, "to_be_graded_batch")
+
+# ======================================================================
+# some error checking on the queues (& permissions of this user)
 
 if not os.path.isdir(INTERACTIVE_QUEUE):
     raise SystemError("ERROR: interactive queue {} does not exist".format(INTERACTIVE_QUEUE))
 if not os.path.isdir(BATCH_QUEUE):
     raise SystemError("ERROR: batch queue {} does not exist".format(BATCH_QUEUE))
 if not os.access(INTERACTIVE_QUEUE, os.R_OK):
+    # most instructors do not have read access to the interactive queue
     print("WARNING: interactive queue {} is not readable".format(INTERACTIVE_QUEUE))
 if not os.access(BATCH_QUEUE, os.R_OK):
     raise SystemError("ERROR: batch queue {} is not readeable".format(BATCH_QUEUE))
 
+# ======================================================================
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -29,32 +47,67 @@ def parse_args():
 def main():
     args = parse_args()
     while True:
+
+        #
+        # FIXME: This command is buggy, sometimes overcounting grading processes
+        #
+        # we are attempting to count instances of
+        #    /usr/local/submitty/bin/grade_students.sh
+        # but it may also be matching:
+        #    /usr/local/submitty/bin/grade_students__results_history.py
+        #
+        # and it perhaps also overcounts submissions that use fork (??)
+        #
+
         proc = subprocess.Popen(["pgrep", "grade_students"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, _ = proc.communicate()
-        grading_processes = len(str(out).strip().split("\n"))
 
-        interactive_queue = os.listdir(INTERACTIVE_QUEUE)
-        grading_interactive_queue = list(filter(lambda x: x.startswith("GRADING"), interactive_queue))
+        if len(str(out)) == 3:
+            # special case, no matching processes
+            print ("WARNING: No matching grade_students.sh processes!")
+            grading_processes = 0
+        else:
+            # convert to string
+            out = str(out)
+            # chop of first 2 and last 1 character
+            out = out[2:len(out)-3]
+            # split on escaped newline characters
+            after = out.split('\\n')
+            #print ("after: ",after)
+            grading_processes = len(after)
+
+        done = True
+
         batch_queue = os.listdir(BATCH_QUEUE)
         grading_batch_queue = list(filter(lambda x: x.startswith("GRADING"), batch_queue))
+        if len(batch_queue) != 0:
+            done = False
 
         print("GRADING PROCESSES:{:3d}       ".format(grading_processes), end="")
 
-        if not os.access(INTERACTIVE_QUEUE, os.R_OK):
+        if os.access(INTERACTIVE_QUEUE, os.R_OK):
+            # most instructors do not have read access to the interactive queue
+            interactive_queue = os.listdir(INTERACTIVE_QUEUE)
+            grading_interactive_queue = list(filter(lambda x: x.startswith("GRADING"), interactive_queue))
             print("INTERACTIVE todo:{:3d} ".format(len(interactive_queue)-len(grading_interactive_queue)), end="")
             if len(grading_interactive_queue) == 0:
                 print("                 ", end="")
             else:
                 print("(grading:{:3d})    ".format(len(grading_interactive_queue)), end="")
 
-        print("BATCH todo:%3d ".format(len(batch_queue) - len(grading_batch_queue)))
+            if len(interactive_queue) != 0:
+                done = False
+
+        print("BATCH todo:{:3d} ".format(len(batch_queue) - len(grading_batch_queue)), end="")
         if len(grading_batch_queue) != 0:
             print("(grading:{:3d})".format(len(grading_batch_queue)), end="")
         print()
 
-        if len(interactive_queue) + len(batch_queue) == 0 and not args.continuous:
+        # quit when the queues are empty
+        if done and not args.continuous:
             raise SystemExit()
 
+        # pause before checking again
         time.sleep(5)
 
 
