6adc7f4bf88fb6630f5740e64a8e40a6faa49dd1
Barb Cutler <bmcutler@gmail.com>
2017-06-02 11:03:03 -0400

    tweak

--- .setup/CONFIGURE_SUBMITTY.sh
+++ .setup/CONFIGURE_SUBMITTY.sh
@@ -22,9 +22,7 @@ read DATABASE_PASSWORD
 echo "What is the url for submission? (ex: http://localhost or https://submitty.cs.rpi.edu/)"
 read SUBMISSION_URL
 
-#echo "What is the url for the Grading Server? (ex: http://localhost/hwgrading or https://submitty.cs.rpi.edu/hwgrading/)"
-#read TAGRADING_URL
-TAGRADING_URL=SUBMISSION_URL/hwgrading
+TAGRADING_URL=${SUBMISSION_URL}/hwgrading
 
 echo "What is the url for the CGI scripts (cgi-bin)? (ex: http://localhost/cgi-bin or https://submitty-cgi.cs.rpi.edu/)"
 read CGI_URL
