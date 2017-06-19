d36e2f46647cc165ab0cfd34b01378ffa42b718f
wangj35 <wangj35@rpi.edu>
2017-06-19 16:15:07 -0400

    Removed some code. Need to back up and figure out how the gradeable is obtained. Currently, it goes by the submissions that the instructor has for the gradeable, which is not correct.

--- site/app/views/NavigationView.php
+++ site/app/views/NavigationView.php
@@ -239,6 +239,7 @@ HTML;
                     $gradeable_title = '<label>'.$g_data->getName().'</label>';
                 }
 
+                $grader_upload = "";
                 if ($g_data->getType() == GradeableType::ELECTRONIC_FILE){
 
                     $display_date = ($title == "FUTURE" || $title == "BETA") ? "<span style=\"font-size:smaller;\">(opens ".$g_data->getOpenDate()->format("m/d/y{$time}")."</span>)" : "<span style=\"font-size:smaller;\">(due ".$g_data->getDueDate()->format("m/d/y{$time}")."</span>)";
@@ -273,6 +274,21 @@ HTML;
                     Need to run BUILD_{$this->core->getConfig()->getCourse()}.sh
                 </button>
 HTML;
+                    }
+
+                    // Upload button, only visible to those with grading access
+                    if ($this->core->getUser()->accessFullGrading()) {
+                        $display_date = ($title == "FUTURE" || $title == "BETA") ? "<span style=\"font-size:smaller;\">(opens ".$g_data->getGradeStartDate()->format("m/d/y{$time}")."</span>)" : "<span style=\"font-size:smaller;\">(due ".$g_data->getGradeReleasedDate()->format("m/d/y{$time}")."</span>)";
+                        if ($title=="GRADED" || $title=="ITEMS BEING GRADED") { $display_date = ""; }
+                        $button_text = "UPLOAD </br> {$display_date}";
+                        $grader_upload = <<<HTML
+                 <button class="btn btn-default" style="width:100%;" onclick="location.href='{$this->core->buildUrl(array('component' => 'grading', 'page' => 'upload', 'gradeable_id' => $gradeable))}'">
+                     {$button_text}
+                 </button>
+HTML;
+                    }
+                    else {
+                        $grader_upload = "";
                     }
                 }
                 else{
@@ -310,21 +326,6 @@ HTML;
                 <button class="btn btn-default" style="width:100%;" onclick="location.href='{$this->core->buildUrl(array('component' => 'grading', 'page' => 'team_list', 'gradeable_id' => $gradeable))}'"> View Teams
                 </button>
 HTML;
-                }
-
-                // Upload button, only visible to those with grading access
-                if ($this->core->getUser()->accessGrading()) {
-                    $display_date = ($title == "FUTURE" || $title == "BETA") ? "<span style=\"font-size:smaller;\">(opens ".$g_data->getGradeStartDate()->format("m/d/y{$time}")."</span>)" : "<span style=\"font-size:smaller;\">(due ".$g_data->getGradeReleasedDate()->format("m/d/y{$time}")."</span>)";
-                    if ($title=="GRADED" || $title=="ITEMS BEING GRADED") { $display_date = ""; }
-                    $button_text = "UPLOAD </br> {$display_date}";
-                    $grader_upload = <<<HTML
-                 <button class="btn btn-default" style="width:100%;" onclick="location.href='{$this->core->buildUrl(array('component' => 'grading', 'page' => 'upload', 'gradeable_id' => $gradeable))}'">
-                     {$button_text}
-                 </button>
-HTML;
-                }
-                else {
-                    $grader_upload = "";
                 }
 
                 if ($this->core->getUser()->accessAdmin()) {
--- site/app/views/grading/UploadView.php
+++ site/app/views/grading/UploadView.php
@@ -32,34 +32,20 @@ HTML;
      * @return string
      */
     public function showUpload($gradeable) {
-    	$return = <<<HTML
+        $upload_message = $this->core->getConfig()->getUploadMessage();
+        $current_version = $gradeable->getCurrentVersion();
+        $current_version_number = $gradeable->getCurrentVersionNumber();
+        $return = <<<HTML
 <script type="text/javascript" src="{$this->core->getConfig()->getBaseUrl()}js/drag-and-drop.js"></script>
 <div class="content">
     <h2>New upload for: {$gradeable->getName()}</h2>
-    <div class="upload">
-HTML;
-        $rcs_id = "";
-        $return .= <<<HTML
-        <div>Student RCS ID: <span><input type="text" name="rcs_id" value="{$rcs_id}" /></span></div>
-        <div id="upload-boxes" style="display:table; border-spacing: 5px; width:100%">
-HTML;
-            $label = "Drag your files() here or click to open file browser";
-            $return .= <<<HTML
-            <div id="upload0" style="cursor: pointer; text-align: center; border: dashed 2px lightgrey; display:table-cell; height: 150px;">
-                <h3 class="label" id="label0">{$label}</h3>
-                <input type="file" name="files" id="input_file0" style="display: none" onchange="addFilesFromInput(0)" multiple />
-            </div>
-HTML;
-
-        $return .= <<<HTML
-        </div>
-    </div>
 </div>
+HTML;
+        $return .= <<<HTML
 <div class="content">
     <span style="font-style: italic">Students for this assignment blahbalba.</span>
 </div>
 HTML;
         return $return;
     }
-
 }
