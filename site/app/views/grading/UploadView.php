6103c4b6bcf99b916aae8f7b2197782effb07f5e
wangj35 <wangj35@rpi.edu>
2017-06-19 13:29:13 -0400

    New div for student information

--- site/app/views/grading/UploadView.php
+++ site/app/views/grading/UploadView.php
@@ -41,7 +41,22 @@ HTML;
         $rcs_id = "";
         $return .= <<<HTML
         <div>Student RCS ID: <span><input type="text" name="rcs_id" value="{$rcs_id}" /></span></div>
+        <div id="upload-boxes" style="display:table; border-spacing: 5px; width:100%">
+HTML;
+            $label = "Drag your files() here or click to open file browser";
+            $return .= <<<HTML
+            <div id="upload0" style="cursor: pointer; text-align: center; border: dashed 2px lightgrey; display:table-cell; height: 150px;">
+                <h3 class="label" id="label0">{$label}</h3>
+                <input type="file" name="files" id="input_file0" style="display: none" onchange="addFilesFromInput(0)" multiple />
+            </div>
+HTML;
+
+        $return .= <<<HTML
+        </div>
     </div>
+</div>
+<div class="content">
+    <span style="font-style: italic">Students for this assignment blahbalba.</span>
 </div>
 HTML;
         return $return;
