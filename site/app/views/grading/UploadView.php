a71085ca4de7232ad327a1c2c2b809b3d8f8d0e6
wangj35 <wangj35@rpi.edu>
2017-06-19 12:00:34 -0400

    UploadView now has a text box for RCS.

--- site/app/views/grading/UploadView.php
+++ site/app/views/grading/UploadView.php
@@ -6,6 +6,25 @@ use app\views\AbstractView;
 
 class UploadView extends AbstractView {
 
+    public function noGradeable($gradeable_id) {
+        if ($gradeable_id === null) {
+            return <<<HTML
+<div class="content">
+    No gradeable id specified. Contact your instructor if you think this is an error.
+</div>
+HTML;
+        }
+        else {
+            $gradeable = htmlentities($gradeable_id, ENT_QUOTES);
+            return <<<HTML
+<div class="content">
+    {$gradeable} is not a valid electronic submission gradeable. Contact your instructor if you think this
+    is an error.
+</div>
+HTML;
+        }
+    }
+
 	/**
      *
      * @param Gradeable $gradeable
@@ -14,9 +33,15 @@ class UploadView extends AbstractView {
      */
     public function showUpload($gradeable) {
     	$return = <<<HTML
+<script type="text/javascript" src="{$this->core->getConfig()->getBaseUrl()}js/drag-and-drop.js"></script>
 <div class="content">
-    <h2>New submission for: {$gradeable->getName()}</h2>
-    <div class="sub">
+    <h2>New upload for: {$gradeable->getName()}</h2>
+    <div class="upload">
+HTML;
+        $rcs_id = "";
+        $return .= <<<HTML
+        <div>Student RCS ID: <span><input type="text" name="rcs_id" value="{$rcs_id}" /></span></div>
+    </div>
 </div>
 HTML;
         return $return;
