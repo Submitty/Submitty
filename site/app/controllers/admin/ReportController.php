1596380d3fafea4ac998fb1c2d6d0bea508d6b87
Josh Barthelmess <joshbarthelmess3@gmail.com>
2017-06-26 13:53:56 -0400

    Very close to being done, fixing some last bugs

--- site/app/controllers/admin/ReportController.php
+++ site/app/controllers/admin/ReportController.php
@@ -19,7 +19,7 @@ class ReportController extends AbstractController {
                 $this->showReportPage();
                 break;
             case 'csv':
-                $this->showCSVReport();
+                $this->generateCSVReport();
                 break;
             case 'summary':
                 $this->generateGradeSummaries();
@@ -37,8 +37,68 @@ class ReportController extends AbstractController {
         $this->core->getOutput()->renderOutput(array('admin', 'Report'), 'showReportUpdates');
     }
     
-    public function showCSVReport() {
+    public function generateCSVReport() {
+        $students = $this->core->getQueries()->getAllUsers();
+        $student_ids = array_map(function($stu) {return $stu->getId();}, $students);
+        $gradeables = $this->core->getQueries()->getGradeables(null, $student_ids);
+        $results = array();
+        $results['header_model'] = array('First' => 'First Name', 'Last'=> 'Last Name', 'reg_section' => 'Registration Section');
+        foreach($gradeables as $gradeable) {
+            $stu_id = $gradeable->getUser()->getId();
+            if(!isset($results[$stu_id])) {
+                $results[$stu_id] = array('First' => $gradeable->getUser()->getFirstName(), 'Last' => $gradeable->getUser()->getLastName(), 'reg_section' => $gradeable->getUser()->getRegistrationSection());
+                foreach($results['header_model'] as $s_buckets => $assignments) {
+                    $results[$stu_id][$s_buckets] = array();
+                }
+            }
+            $g_id = $gradeable->getId();
+            $syllabus_bucket = $gradeable->getSyllabusBucket();
+            if(!isset($results['header_model'][$syllabus_bucket])) {
+                $results['header_model'][$syllabus_bucket] = array($g_id => $g_id);
+            }
+            if(!isset($results[$stu_id][$syllabus_bucket])) {
+                $results[$stu_id][$syllabus_bucket] = array($g_id => $gradeable->getGradedTAPoints());
+            }
+            else {
+                $results[$stu_id][$syllabus_bucket][$g_id] = $gradeable->getGradedTAPoints();
+            }
+            if(!isset($results['header_model'][$syllabus_bucket][$g_id])) {
+                $results['header_model'][$syllabus_bucket][$g_id] = $g_id;
+            }
+        }
         
+        $csv_output = "";
+        
+        foreach($results as $id => $user_grades) {
+            $row = array();
+            if($id === 'header_model') {
+                $row[] = 'UserId';
+            }
+            else {
+                $row[] = $id;
+            }
+            
+            $row[] = $results[$id]['First'];
+            $row[] = $results[$id]['Last'];
+            $row[] = $results[$id]['reg_section'];
+            foreach($results[$id] as $s_bucket => $gradeables) {
+                if($s_bucket == 'First' || $s_bucket == 'Last' || $s_bucket == 'reg_section') {
+                    continue;
+                }
+                $row[] = $s_bucket;
+                foreach($results[$id][$s_bucket] as $score) {
+                    $row[] = $score;
+                }
+            }
+            $student_row = implode(",", $row);
+            $csv_output .= $student_row."\n";
+        }
+        header("Content-type: text/plain");
+        header("Contenet-Disposition: attachment; filename=hwserver-report.csv");
+        header("Content-Length: ".strlen($csv_output));
+        
+        echo $csv_output;
+        $this->core->getOutput()->renderOutput(array('admin', 'Report'), 'showReportUpdates');
     }
     
     public function generateGradeSummaries() {
@@ -55,4 +115,4 @@ class ReportController extends AbstractController {
         $this->core->getOutput()->renderOutput(array('admin', 'Report'), 'showReportUpdates');
     }
 }
-?>
+
