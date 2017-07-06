2ac2e702081e94c6e727196ddc1fd2c22407bcdf
Matthew Peveler <matt.peveler@gmail.com>
2017-07-06 12:37:53 -0400

    Implement master submitty DB and use it for single sign-on across courses (#1138)
    * add second db connection
    
    * code changes
    
    * fix the database creation step to spin up both DBs now.
    
    * update table sql
    
    * Insert Trigger
    
    Sync user data with course DB specifically on INSERT of a new record.
    
    * Insert Trigger
    
    Tweaks
    
    * Insert Trigger
    
    Setup UPDATE branch
    
    * Trigger
    
    Tweaks.
    
    * Triggers
    
    UPDATE on courses_users
    
    To Do: UPDATE on users
    
    * Triggers
    
    UPDATE trigger implemented.
    
    * Finish implementation for creating courses and logging in with two DBs
    
    * Change from two column tab to two spaces for indentation
    
    * update triggers to also update user_group and manual_registration across the DBs
    
    * modify code to allow for creating/editing a user from the course site.
    
    * Improve SQL for handling someone being in multiple courses
    
    * add new DB schema
    
    * missing hypen
    
    * fix DB logging in
    
    * Show an error page for the course if user tries to access course he is not a part of
    
    * rewrite the PamAuthentication method to be more inline with the DBAuthentication
    
    * use one cookie for all courses so that it's actually single signon
    
    * fix bug with trying to create a user
    
    * update some messed up DB functions
    
    * another missing abstract db function
    
    * fix bad reference
    
    * forgot insert

--- tests/unitTests/BaseUnitTest.php
+++ tests/unitTests/BaseUnitTest.php
@@ -3,7 +3,7 @@
 namespace tests\unitTests;
 
 use app\libraries\Core;
-use app\libraries\database\IDatabaseQueries;
+use app\libraries\database\AbstractDatabaseQueries;
 use app\libraries\GradeableType;
 use app\libraries\Output;
 use app\libraries\Utils;
@@ -57,7 +57,7 @@ class BaseUnitTest extends \PHPUnit_Framework_TestCase {
             $core->method('isTesting')->willReturn(true);
         }
 
-        $queries = $this->createMock(IDatabaseQueries::class);
+        $queries = $this->createMock(AbstractDatabaseQueries::class);
         $core->method('getQueries')->willReturn($queries);
 
         $user = $this->createMockModel(User::class);
