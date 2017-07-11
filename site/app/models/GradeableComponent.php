d571a96a408c4754822890621c79b4973ed0a6e3
Marisa Lee <lee.p.marisa@gmail.com>
2017-07-11 09:58:10 -0400

    some work on models

--- site/app/models/GradeableComponent.php
+++ site/app/models/GradeableComponent.php
@@ -60,6 +60,9 @@ class GradeableComponent extends AbstractModel {
     /** @property @var User */
     protected $grader = null;
 
+    /** @property @var int $graded_version  The version graded for the component */
+    protected $graded_version = -1;
+
     /** @property @var \DateTime */
     protected $grade_time = null;
 
@@ -81,7 +84,10 @@ class GradeableComponent extends AbstractModel {
         $this->order = $details['gc_order'];
         if (isset($details['gcd_score']) && $details['gcd_score'] !== null) {
             $this->has_grade = true;
-            $this->grader = isset($details['gcd_grader']) ? $details['gcd_grader'] : null;
+            // $this->grader = isset($details['gcd_grader']) ? $details['gcd_grader'] : null;
+            $this->grader = $this->core->getQueries()->getUserById($details['gcd_grader_id']);
+            // $this->graded_version = isset($details['gcd_graded_version']) ? $details['gcd_graded_version'] : -1;
+            $this->graded_version = $details['gcd_graded_version'];
             if (isset($details['gcd_grade_time'])) {
                 $this->grade_time = new \DateTime($details['gcd_grade_time'], $this->core->getConfig()->getTimezone());
             }
