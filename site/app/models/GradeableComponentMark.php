b608fbc14afe6691f940d6b80d23aae127fb4d62
Josh Barthelmess <joshbarthelmess3@gmail.com>
2017-07-25 12:20:24 -0400

    [WIP] Peer grading allocating graders (#1223)
    * wip
    
    * started adding view for rubric redesign in adminGradeableView
    
    * database tables added
    
    * the view for the create gradeable page is done
    
    * wip
    
    * wip
    
    * Create Edit gradeable done
    
    * draft of get gradeables with marks
    
    * wip
    
    * Reworking gradeable page to use models almost done; memory error exists
    
    * Finished reworking create newgradeable page; might be small bugs around though
    
    * accidental overwrite
    
    * fixed one of the small bugs
    
    * initial work
    
    * moved peer grading in database to electronic gradeables
    
    * added people amount and check for component validity
    
    * additional peer stuff
    
    * adding to peer_assign table works
    
    * adding things to peer_assign works correctly with check for getting stuck in algorithm
    
    * closed security hole for pull request
    
    * stupid syntax mistake
    
    * fixed loading from a template, and editing
    
    * fixed saving peer_grade_set in database
    
    * changes that I missed

---
+++ site/app/models/GradeableComponentMark.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+use app\libraries\Core;
+
+/**
+ * Class GradeableComponentMark
+ *
+ */
+
+class GradeableComponentMark extends AbstractModel {
+	/** @property @var int Unique identifier for the mark */
+    protected $id = null;
+    /** @property @var int Unique identifier for the component associated with this mark */
+    protected $gc_id = null;
+    /** @property @var float Maximum value that the component can have */
+    protected $max_value = 0;
+    /** @property @var int Order for marks to be shown in */
+    protected $order = 1;
+    /** @property @var float Points for this mark */
+    protected $points = 0;
+    /** @property @var string Comment for this mark */
+    protected $note = "";
+    /** @property @var bool This person earned this mark*/
+    protected $has_mark = false;
+
+    public function __construct(Core $core, $details=array()) {
+        parent::__construct($core);
+        if (!isset($details['gcm_id'])) {
+            return;
+        }
+        $this->id = $details['gcm_id'];
+        $this->gc_id = $details['gc_id'];
+        $this->points = $details['gcm_points'];
+        $this->order = $details['gcm_order'];
+        $this->note = $details['gcm_note'];
+        if (isset($details['gcm_has_mark']) && $details['gcm_has_mark'] !== null) {
+            $has_mark = true;
+        }
+    }
+
+    public function saveData($gd_id, $gc_id) {
+        if ($this->modified) {
+        	if ($this->has_mark) {
+                $this->core->getQueries()->insertGradeableCompnentMarkData($gd_id, $gc_id, $this);
+        	} else {
+        		$this->core->getQueries()->deleteGradeableComponentMarkData($gd_id, $gc_id, $this);
+        	}
+        }
+    }
+}
