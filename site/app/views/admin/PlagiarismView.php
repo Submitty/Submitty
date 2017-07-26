80e6b98d9c9afcd3e77398357350de8645f14fed
Samuel Breese <samuel@chame.co>
2017-07-26 11:56:49 -0400

    Use file_get_contents

--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -9,7 +9,7 @@ class PlagiarismView extends AbstractView {
         $return .= <<<HTML
 <div class="content">
 HTML;
-    readFile("/var/local/submitty/courses/f17/development/plagiarism/report/var/local/submitty/courses/f17/development/submissions/cpp_cats/index.html");
+        $return .= file_get_contents("/var/local/submitty/courses/f17/development/plagiarism/report/var/local/submitty/courses/f17/development/submissions/cpp_cats/index.html");
         $return .= <<<HTML
 </div>
 HTML;
