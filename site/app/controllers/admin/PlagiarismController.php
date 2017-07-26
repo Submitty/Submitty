15df8c37572243bbf2cec7df1bf8de8374684b87
Samuel Breese <samuel@chame.co>
2017-07-26 12:59:59 -0400

    Add compare

--- site/app/controllers/admin/PlagiarismController.php
+++ site/app/controllers/admin/PlagiarismController.php
@@ -7,11 +7,24 @@ use app\libraries\Output;
 
 class PlagiarismController extends AbstractController {
     public function run() {
-        $this->core->getOutput()->addBreadcrumb("Plagiarism");
-        $this->plagiarismIndex();
+        switch ($_REQUEST['action']) {
+            case 'compare':
+                $this->plagiarismCompare();
+                break;
+            default:
+                $this->core->getOutput()->addBreadcrumb("Plagiarism");
+                $this->plagiarismIndex();
+                break;
+        }
     }
 
     public function plagiarismIndex() {
         $this->core->getOutput()->renderOutput(array('admin', 'Plagiarism'), 'plagiarismIndex');
+    }
+
+    public function plagiarismCompare() {
+        $studenta = $_REQUEST['studenta'];
+        $studentb = $_REQUEST['studentb'];
+        $this->core->getOutput()->renderOutput(array('admin', 'Plagiarism'), 'plagiarismCompare', $studenta, $studentb);
     }
 }
--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -15,4 +15,16 @@ HTML;
 HTML;
         return $return;
     }
+
+    public function plagiarismCompare($studenta, $studentb) {
+        $return = "";
+        $return .= <<<HTML
+<div class="content">
+HTML;
+        $return .= file_get_contents("/var/local/submitty/courses/f17/development/plagiarism/report/var/local/submitty/courses/f17/development/submissions/cpp_cats/compare/" . $studenta . "_" . $studentb . ".html");
+        $return .= <<<HTML
+</div>
+HTML;
+        return $return;
+    }
 }
