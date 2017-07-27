8f5936b147f659e0d73e950e478f6f70a4972f76
Samuel Breese <samuel@chame.co>
2017-07-27 13:09:57 -0400

    Semester and assignment for compare page

--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -4,12 +4,12 @@ namespace app\views\admin;
 use app\views\AbstractView;
 
 class PlagiarismView extends AbstractView {
-    public function plagiarismCompare($studenta, $studentb) {
+    public function plagiarismCompare($semester, $course, $assignment, $studenta, $studentb) {
         $return = "";
         $return .= <<<HTML
 <div class="content">
 HTML;
-        $return .= file_get_contents("/var/local/submitty/courses/f17/development/plagiarism/report/var/local/submitty/courses/f17/development/submissions/cpp_cats/compare/" . $studenta . "_" . $studentb . ".html");
+        $return .= file_get_contents("/var/local/submitty/courses/$semester/$course/plagiarism/report/var/local/submitty/courses/$semester/$course/submissions/$assignment/compare/" . $studenta . "_" . $studentb . ".html");
         $return .= <<<HTML
 </div>
 HTML;
