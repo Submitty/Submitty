3174b1dc553d65e166757bef89c506251dc7ec54
Evan Maicus <emaicus@outlook.com>
2017-07-27 15:13:34 -0400

    Made suggested changes.

--- .setup/bin/generate_csv.py
+++ .setup/bin/generate_csv.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import csv
+import argparse
 
 # Given a directory mirroring a submission directory (see arguments), this script will generate 
 # a csv userlist for upload to submitty.
@@ -12,37 +13,39 @@ import csv
 # OUTPUT:
 # 1) A csv of the form: username, LastName, FirstName, email, RegistrationSection
 def main():
-  if len(sys.argv) < 2:
-    print("ERROR: Please enter the directory that you wish to traverse for student names.")
-    sys.exit(1)
-  user_dir = sys.argv[1]
-  if not os.path.isdir(user_dir):
-    print("The provided directory (" + user_dir + ") does not exist.")
-    sys.exit(1)  
-  course_name = sys.argv[1].split('/')[-2]
-  csv_name = course_name + ".csv"
+    parser = argparse.ArgumentParser(description='Given a directory mirroring a submission directory (see arguments),\
+     this script will generate a csv userlist for upload to submitty.')
+    parser.add_argument('archived_directory', help='The path to the top level of the old assignment\'s directory tree.\
+     This folder\'s subdirectories should mirror a /var/local/submitty/courses/<semester>/<course>/submissions/<assignment_name>\
+     folder. ')
+    args = parser.parse_args()
 
-  #make a list of students
-  student_list = list()
-  for student_name in os.listdir(user_dir):
-    student_dir = os.path.join(user_dir, student_name)
-    if not os.path.isdir(student_dir):
-      continue
-    else:
-      print("Added " + student_name)
-      student_list.append(student_name)
+    if not os.path.isdir(args.archived_directory):
+        raise SystemExit("The provided directory (" + args.archived_directory + ") does not exist.") 
+    course_name = sys.argv[1].split('/')[-2]
+    csv_name = course_name + ".csv"
 
-  f = open(csv_name, 'wt')
-  try:
-    with open(os.path.join(os.getcwd(), csv_name), "w") as out_file:
-      writer = csv.writer(out_file)
-      for student in student_list:
-        #TODO: Do something clever with the name/last name/email/section.
-        writer.writerow((student, "a", "b", "c@email.com", str(1)))
-  finally:
-    f.close()
+    #make a list of students
+    student_list = list()
+    for student_name in os.listdir(args.archived_directory):
+        student_dir = os.path.join(args.archived_directory, student_name)
+        if not os.path.isdir(student_dir):
+            continue
+        else:
+            print("Added " + student_name)
+            student_list.append(student_name)
+
+    f = open(csv_name, 'wt')
+    try:
+        with open(os.path.join(os.getcwd(), csv_name), "w") as out_file:
+            writer = csv.writer(out_file)
+            for student in student_list:
+                #TODO: Do something clever with the name/last name/email/section.
+                writer.writerow((student, "a", "b", "c@email.com", str(1)))
+    finally:
+        f.close()
 
   
 
 if __name__ == '__main__':
-  main()
+    main()
--- .setup/bin/reupload_old_assignments.py
+++ .setup/bin/reupload_old_assignments.py
@@ -2,8 +2,9 @@
 import sys
 import os
 import json
-from sqlalchemy import create_engine, Table, MetaData, bindparam, and_
 import shutil
+import argparse
+from sqlalchemy import create_engine, Table, MetaData, bindparam, and_
 
 SUBMITTY_DATA_DIR = "/var/local/submitty"
 
@@ -12,7 +13,7 @@ import submitty_utils
 
 DB_HOST = "localhost"
 DB_USER = "hsdbu"
-DB_PASS = "hsdbu"
+DB_PASS = "Graphics"
 
 
 
@@ -30,43 +31,38 @@ DB_PASS = "hsdbu"
 #NOTE: variables beginning with ARCHIVED_ are populated using the homework assignment being copied.
 #Varialbes beginning with CURRENT_ are populated using the destination/current semester's directories.
 def main():
-    grade = False
     #In order to successfully re-upload the assignment, we need the directory of the assignment to be inflated,
     #and the semester, course name, and assignment name that it should be uploaded to.
-    semester = course_name = assignment_name = ARCHIVED_directory =""
-    if len(sys.argv) < 5:
-        print("ERROR: Please provide:\n\t1)Directory with your files\n\t2)semester \n\t3)course code \n\t4)assignment name.\n\t5)--grade (optional)")
-        sys.exit(1)
-    else:
-        ARCHIVED_directory = sys.argv[1]
-        if not os.path.isdir(ARCHIVED_directory):
-            print("The provided directory (" + ARCHIVED_directory + + ") does not exist.")
-            sys.exit(1)
-        semester = sys.argv[2]
-        course_name = sys.argv[3]
-        assignment_name = sys.argv[4]
-        #The optional argument --grade causes the uploaded assignment to be added to the grading queue.
-        if len(sys.argv) == 6:
-            if sys.argv[5] == "--grade":
-                grade = True
-    print("You are about to attempt to copy\n\tSOURCE: " + ARCHIVED_directory + "\n\tDESTINATION: " + SUBMITTY_DATA_DIR + "/" + semester+"/courses/"+course_name+"/submissions/"+assignment_name)
+    parser = argparse.ArgumentParser(description='This script was created to help professors re-upload an old semester\'s\
+        assignments for autograding and reevaluation. To run it, please create a new assignment via the submitty interface to\
+        which you wish to upload a past assignment\'s submissions. Then, run this script.')
+    parser.add_argument('-g', '--grade', action='store_true', help='adds assignments to the grading queue.')
+    parser.add_argument('ARCHIVED_directory', help='The path to the top level of the old assignment\'s directory tree. This folder\'s\
+        subdirectories should mirror a /var/local/submitty/courses/<semester>/<course>/submissions/<assignment_name> folder.')
+    parser.add_argument('semester', help='The semester of the course you wish to upload to.')
+    parser.add_argument('course_name', help='The name of the course you wish to upload to.')
+    parser.add_argument('assignment_name', help='The assignment name you wish to upload to.')
+    args = parser.parse_args()
+
+    print("You are about to attempt to copy\n\tSOURCE: " + args.ARCHIVED_directory + "\n\tDESTINATION: "
+     + SUBMITTY_DATA_DIR + "/" + args.semester+"/courses/"+args.course_name+"/submissions/"+args.assignment_name)
 
     #Check the existence of the submission path (the user should have already created the assignment for us to populate)
-    CURRENT_course_path = os.path.join(SUBMITTY_DATA_DIR, "courses", semester, course_name)
+    CURRENT_course_path = os.path.join(SUBMITTY_DATA_DIR, "courses", args.semester, args.course_name)
     print("Current course path:" + CURRENT_course_path)
-    CURRENT_assignment_path = os.path.join(CURRENT_course_path, "submissions", assignment_name)
+    CURRENT_assignment_path = os.path.join(CURRENT_course_path, "submissions", args.assignment_name)
     print("Current assignment path" + CURRENT_assignment_path)
     #we check the bin directory to see if the assignment has ever been built. (Not a guarantee, but high probability of success)
-    CURRENT_bin_directory = os.path.join(CURRENT_course_path, "bin", assignment_name)
+    CURRENT_bin_directory = os.path.join(CURRENT_course_path, "bin", args.assignment_name)
     if not os.path.isdir(CURRENT_bin_directory):
-        print("ERROR: The directory " + CURRENT_bin_directory + " does not exist. Please make sure that you\n\t1) Configured the assignment on the course website\n\t2) Did not mistype the program arguments.")
-        sys.exit(1)
+        raise SystemExit("ERROR: The directory " + CURRENT_bin_directory + " does not exist. Please make sure that you\n\t\
+            1) Configured the assignment on the course website\n\t2) Did not mistype the program arguments.") 
+
     else:
         print("SUCCESS: The directory " + CURRENT_assignment_path + " does exist.")    
 
     #Make a connection to the database and grab the necessary tables.
-    os.environ['PGPASSWORD'] = DB_PASS
-    database = "submitty_" + semester + "_" + course_name
+    database = "submitty_" + args.semester + "_" + args.course_name
     print("Connecting to database: ", end="")
     engine = create_engine("postgresql://{}:{}@{}/{}".format(DB_USER, DB_PASS, DB_HOST,
                                                            database))
@@ -78,9 +74,9 @@ def main():
 
     #For every user folder in our directory, for every submission folder inside of it, copy over that user/submission, add it to 
     #the database, and create a queue file. 
-    for user_folder in os.listdir(ARCHIVED_directory):
+    for user_folder in os.listdir(args.ARCHIVED_directory):
         #If the item we are currently looking at is a directory, we will assume it is a student submission directory.
-        ARCHIVED_user_dir = os.path.join(ARCHIVED_directory, user_folder)
+        ARCHIVED_user_dir = os.path.join(args.ARCHIVED_directory, user_folder)
         print("Starting work on archived dir: " + ARCHIVED_user_dir)
         if not os.path.isdir(ARCHIVED_user_dir):
             print("Skipping the following as it is not a directory: " + ARCHIVED_user_dir)
@@ -109,29 +105,29 @@ def main():
                 continue
             #This permission also sets the underlying submission paths recursively.g
             print("Set permissions on the submission")
-            os.system("chown -R hwphp:{}_tas_www {}".format(course_name, CURRENT_user_path))
+            os.system("chown -R hwphp:{}_tas_www {}".format(args.course_name, CURRENT_user_path))
 
             #TODO: Sort the submissions so that they are guaranteed to be given in chronological (1,2,3,etc) order.
             #copy in the submission directory.
             print("Copied from\n\tSOURCE: " + ARCHIVED_submission_path +"\n\t" + "DESTINATION: " + CURRENT_submission_path)
             shutil.copytree(ARCHIVED_submission_path, CURRENT_submission_path)
             #give the appropriate permissions
-            os.system("chown -R hwphp:{}_tas_www {}".format(course_name, CURRENT_submission_path))
+            os.system("chown -R hwphp:{}_tas_www {}".format(args.course_name, CURRENT_submission_path))
             #add each submission to the database.
             current_time_string = submitty_utils.write_submitty_date()
 
-            conn.execute(electronic_gradeable_data.insert(), g_id=assignment_name, user_id=user_name,
+            conn.execute(electronic_gradeable_data.insert(), g_id=args.assignment_name, user_id=user_name,
                          g_version=submission, submission_time=current_time_string)
             #If this is the first submission, create a new entry in the table, otherwise, update.
             #TODO use a more reliable method of determining if this is the first submission.
             if int(submission) == 1:
                 print("Entered new user " + user_name + " because submission was " + submission)
-                conn.execute(electronic_gradeable_version.insert(), g_id=assignment_name, user_id=user_name,
+                conn.execute(electronic_gradeable_version.insert(), g_id=args.assignment_name, user_id=user_name,
                          active_version=submission)
             else:
-                print("UPDATED: where g_id is " + assignment_name + " and user id is " + user_name + " to value " + str(submission))
+                print("UPDATED: where g_id is " + args.assignment_name + " and user id is " + user_name + " to value " + str(submission))
                 stmt = electronic_gradeable_version.update().\
-                        where(and_(electronic_gradeable_version.c.g_id==assignment_name, electronic_gradeable_version.c.user_id==user_name)).\
+                        where(and_(electronic_gradeable_version.c.g_id==args.assignment_name, electronic_gradeable_version.c.user_id==user_name)).\
                         values(active_version=submission)
                 conn.execute(stmt)
             with open(os.path.join(CURRENT_user_path, "user_assignment_settings.json"), "w") as open_file:
@@ -140,24 +136,22 @@ def main():
             with open(os.path.join(CURRENT_submission_path, ".submit.timestamp"), "w") as open_file:
                 open_file.write(current_time_string + "\n")
 
-                if grade:
+                if args.grade:
                     #Create a queue file for each submission            
-                    queue_file = "__".join([semester, course_name, assignment_name, user_name, submission])
+                    queue_file = "__".join([args.semester, args.course_name, args.assignment_name, user_name, submission])
                     print("Creating queue file:", queue_file)
                     queue_file = os.path.join(SUBMITTY_DATA_DIR, "to_be_graded_batch", queue_file)
                     with open(queue_file, "w") as open_file:
                         # FIXME: This will need to be adjusted for team assignments!
-                        json.dump({"semester": semester,
-                                   "course": course_name,
-                                   "gradeable": assignment_name,
+                        json.dump({"semester": args.semester,
+                                   "course": args.course_name,
+                                   "gradeable": args.assignment_name,
                                    "user": user_name,
                                    "version": submission,
                                    "who": user_name,
                                    "is_team": False,
                                    "team": ""}, open_file)
     conn.close()
-    os.environ['PGPASSWORD'] = ""
-
 
 if __name__ == "__main__":
     main()
