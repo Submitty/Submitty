000dde2aaff1fb321709a362dcfe94f482b1b977
Samuel Breese <samuel@chame.co>
2017-07-28 13:06:35 -0400

    Add explanatory text

--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -42,13 +42,20 @@ HTML;
         $return .= <<<HTML
 <div class="content">
 <h1 style="text-align: center">Plagiarism Detection</h1>
-<ul>
 HTML;
-        foreach ($assignments as $assignment) {
-            $return .= "<li><a href=\"{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'index', 'assignment' => $assignment))}\">$assignment</a></li>";
+        if ($assignments) {
+            $return .= '<ul>'
+                foreach ($assignments as $assignment) {
+                    $return .= "<li><a href=\"{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'index', 'assignment' => $assignment))}\">$assignment</a></li>";
+                }
+            $return .= '</ul>'
+        } else {
+            $return .= <<<HTML
+<p>It looks like you have yet to run plagiarism detection on any assignment. See <a href="http://submitty.org/instructor/static_analysis">here</a> for details.</p>
+HTML;
         }
         $return .= <<<HTML
-</ul></div>
+</div>
 HTML;
         return $return;
     }
