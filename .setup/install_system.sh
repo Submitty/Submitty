eadcaf988c49423ee9c629634d36b740823b9330
Barb Cutler <bmcutler@gmail.com>
2017-08-02 15:18:32 -0400

    specify version in autograder.sh too

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -309,6 +309,7 @@ if [ -d ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_Tutorial ]; then
 else
     git clone 'https://github.com/Submitty/Tutorial' ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_Tutorial
     pushd ${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT_Tutorial
+    # remember to change this version in .setup/travis/autograder.sh too
     git checkout v0.9
     popd
 fi
