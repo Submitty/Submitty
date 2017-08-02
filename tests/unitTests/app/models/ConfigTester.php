dd5824151f274270ea3fa55905590a60d9ccebe8
AndrewMRPI <maa5@rpi.edu>
2017-08-02 09:29:14 -0400

    Hopefully fixes Travis

--- tests/unitTests/app/models/ConfigTester.php
+++ tests/unitTests/app/models/ConfigTester.php
@@ -84,7 +84,8 @@ class ConfigTester extends \PHPUnit_Framework_TestCase {
                 'upload_message' => "",
                 'keep_previous_files' => false,
                 'display_iris_grades_summary' => false,
-                'display_custom_message' => false
+                'display_custom_message' => false,
+                'course_email' => 'Please contact your TA or instructor for a regrade request.'
             )
         );
 
@@ -256,7 +257,7 @@ class ConfigTester extends \PHPUnit_Framework_TestCase {
             'course_details' => array(
                 'course_name', 'course_home_url', 'default_hw_late_days', 'default_student_late_days',
                 'zero_rubric_grades', 'upload_message', 'keep_previous_files', 'display_iris_grades_summary',
-                'display_custom_message'
+                'display_custom_message', 'course_email'
             )
         );
         $return = array();
