83779335b074fa497fa01de6bc0f4b09be13471d
James <jamesmanderson319@gmail.com>
2017-08-03 15:28:15 -0400

    travis fix

--- tests/e2e/test_navigation_page_non_student.py
+++ tests/e2e/test_navigation_page_non_student.py
@@ -19,7 +19,7 @@ class TestNavigationPageNonStudent(BaseTestCase):
                                  .find_element_by_id('beta_tbody')
                                  .find_elements_by_class_name('gradeable_row')))
         self.assertEqual("open", elements[2].get_attribute('id'))
-        self.assertEqual(1, len(self.driver
+        self.assertEqual(2, len(self.driver
                                 .find_element_by_id('open_tbody')
                                 .find_elements_by_class_name("gradeable_row")))
         self.assertEqual("closed", elements[3].get_attribute('id'))
@@ -46,7 +46,7 @@ class TestNavigationPageNonStudent(BaseTestCase):
                                 .find_element_by_id('beta_tbody')
                                 .find_elements_by_class_name("gradeable_row")))
         self.assertEqual("open", elements[1].get_attribute('id'))
-        self.assertEqual(1, len(self.driver
+        self.assertEqual(2, len(self.driver
                                 .find_element_by_id('open_tbody')
                                 .find_elements_by_class_name("gradeable_row")))
         self.assertEqual("closed", elements[2].get_attribute('id'))
--- tests/e2e/test_navigation_page_student.py
+++ tests/e2e/test_navigation_page_student.py
@@ -7,7 +7,7 @@ class TestNavigationPageStudent(BaseTestCase):
         elements = self.driver.find_elements_by_class_name('nav-title-row')
         self.assertEqual(4, len(elements))
         self.assertEqual("open", elements[0].get_attribute('id'))
-        self.assertEqual(1, len(self.driver
+        self.assertEqual(2, len(self.driver
                                 .find_element_by_id('open_tbody')
                                 .find_elements_by_class_name("gradeable_row")))
         self.assertEqual("closed", elements[1].get_attribute('id'))
