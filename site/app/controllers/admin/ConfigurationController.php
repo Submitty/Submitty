6c8347852c66d3b81758cc60ee0f4be69d9849b1
Jessica Wang <15.jmwang@gmail.com>
2017-08-07 15:33:54 -0400

    Redesign VCS UI (#1299)
    * wip to adding fields for vcs in admin config and creat/edit gradeable
    
    * fields added to and saved for course settings. wip for create edit gradeable
    
    * finished changes for create edit gradeable
    
    * replaced mentions of 'svn' with 'vcs', added vcs base url to the create edit gradeable page, made bulk upload not visible for vcs, added input of repo id on submission page (though it is not yet used in the submission controller)
    
    * small fixes
    
    * travis fix
    
    * Fixed formatting of variable names in create edit gradeable page. Added parsing of the vcs path
    
    * user_id and repo_id can't be used at the same time, repo_id input only shows up if it is in the vcs path
    
    * remove error message that was used for testing
    
    * travis fix
    
    * made vcs path input optional, other minor fixes
    
    * update to master ElectronicGraderView
    
    * one line fix
    
    * travis fix

--- site/app/controllers/admin/ConfigurationController.php
+++ site/app/controllers/admin/ConfigurationController.php
@@ -33,7 +33,9 @@ class ConfigurationController extends AbstractController {
             'keep_previous_files'       => $this->core->getConfig()->keepPreviousFiles(),
             'display_iris_grades_summary' => $this->core->getConfig()->displayIrisGradesSummary(),
             'display_custom_message'      => $this->core->getConfig()->displayCustomMessage(),
-            'course_email'              => $this->core->getConfig()->getCourseEmail()
+            'course_email'              => $this->core->getConfig()->getCourseEmail(),
+            'vcs_base_url'              => $this->core->getConfig()->getVcsBaseUrl(),
+            'vcs_type'                  => $this->core->getConfig()->getVcsType()
         );
 
         foreach (array('course_name', 'upload_message', 'course_email') as $key) {
@@ -98,7 +100,9 @@ class ConfigurationController extends AbstractController {
                 'keep_previous_files'       => $_POST['keep_previous_files'],
                 'display_iris_grades_summary' => $_POST['display_iris_grades_summary'],
                 'display_custom_message'      => $_POST['display_custom_message'],
-                'course_email'                => $_POST['course_email']
+                'course_email'                => $_POST['course_email'],
+                'vcs_base_url'              => $_POST['vcs_base_url'],
+                'vcs_type'                  => $_POST['vcs_type']
             )
         );
         
