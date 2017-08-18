15b5e35a6174681df1eaff9038179064c69c4afc
Matthew Peveler <matt.peveler@gmail.com>
2017-08-18 11:11:07 -0400

    add support for EXTRA env var when creating vagrant to use optional install scripts (#1347)
    * add support for EXTRA env var when creating vagrant
    
    * remove unneeded run tag
    
    * move some more stuff to rpi
    
    * update

--- .setup/vagrant/setup_vagrant.sh
+++ .setup/vagrant/setup_vagrant.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# this script must be run by root or sudo
+if [[ "$UID" -ne "0" ]] ; then
+    echo "ERROR: This script must be run by root or sudo"
+    exit
+fi
+
+DISTRO=$(lsb_release -i | sed -e "s/Distributor\ ID\:\t//g" | tr '[:upper:]' '[:lower:]')
 SUBMITTY_INSTALL_DIR=/usr/local/submitty
 SUBMITTY_REPOSITORY=/usr/local/submitty/GIT_CHECKOUT_Submitty
 
@@ -24,5 +31,6 @@ fi
 pip3 install -U PyYAML
 
 python3 ${SUBMITTY_REPOSITORY}/.setup/bin/reset_system.py
-sudo ${SUBMITTY_REPOSITORY}/.setup/install_system.sh vagrant
+
+sudo ${SUBMITTY_REPOSITORY}/.setup/install_system.sh --vagrant ${@}
 
