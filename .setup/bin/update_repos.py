59fb8aa7b10c2b3eb0f8de4cbff27871988ff7b4
Matthew Peveler <matt.peveler@gmail.com>
2017-08-23 13:28:47 -0400

    aaa

--- .setup/bin/update_repos.py
+++ .setup/bin/update_repos.py
@@ -4,13 +4,12 @@
 Pull new code from the 2 auxiliary repos.
 """
 
-
 import os
 
-if os.path.isdir("/usr/local/submitty/GIT_CHECKOUT_AnalysisTools"):
-    os.chdir("/usr/local/submitty/GIT_CHECKOUT_Tutorial/")
+if os.path.isdir("/usr/local/submitty/GIT_CHECKOUT_Tutorial"):
+    os.chdir("/usr/local/submitty/GIT_CHECKOUT_Tutorial")
     os.system("git pull origin master")
 
 if os.path.isdir("/usr/local/submitty/GIT_CHECKOUT_AnalysisTools"):
-    os.chdir("/usr/local/submitty/GIT_CHECKOUT_AnalysisTools/")
+    os.chdir("/usr/local/submitty/GIT_CHECKOUT_AnalysisTools")
     os.system("git pull origin master")
