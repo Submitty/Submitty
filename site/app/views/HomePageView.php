729707250cbe9a2cd2e7f03990aa01cda1742763
andrewaikens87 <andrewaikens87@gmail.com>
2017-10-04 00:24:48 -0400

    Create ranking labels for homepage view

--- site/app/views/HomePageView.php
+++ site/app/views/HomePageView.php
@@ -99,48 +99,76 @@ HTML;
 HTML;
 
                     $userPermission = $this->core->getQueries()->getGroupForUserInClass($user->getId());
-                    $group_name = "";
-
+                    $userPermissionLength = count($userPermission);
+                    $header = "";
                     $pos = 0;
+                    $rankWithCourse = array();
+
+                    for($i = 0; $i < 4; $i++){
+                        $rankWithCourse[$i] = array();
+                    }
+
+                    
                     foreach($courses as $course){
-                        $display_text = $course->getSemester() . " " . $course->getTitle();
-                        if($course->getDisplayName() !== "")
-                        {
-                            $display_text .= " " . $course->getDisplayName();
-                        }
 
-                        switch($userPermission[$pos]){
-                              case 0:
-                                $group_name = " [ Developer ]";
-                                break;
-                              case 1:
-                                $group_name = " [ Instructor ]";
-                                break;
-                              case 2:
-                                $group_name = " [ TA ]";
-                                break;
-                              case 3:
-                                $group_name = " [ Grader ]";
-                                break;
-                              case 4:
-                                $group_name = " [ Student ]";
-                                break;
-                              default:
-                                $group_name = " [ ]";
-                                break;
-                          }
+                        array_push($rankWithCourse[$userPermission[$pos]-1], $course);
 
-                        $display_text .= $group_name;
                         $pos++;
 
-                        $return .= <<<HTML
+                    }
+
+                    $pos = 0;
+
+                    for($i = 0; $i < 4; $i++){
+                        if(count($rankWithCourse[$i]) == 0){
+                            continue;
+                        }
+
+                        switch ($i+1) {
+                                case 1:
+                                   
+                                        $header = "<h3>Instructor:</h3>";
+
+                                    break;
+                                case 2:
+                                        $header = "<h3>TA:</h3>";
+                                break;
+                                case 3:
+                                        $header = "<h3>Grader:</h3>";
+                                break;
+                                default:
+                                        $header = "<h3>Student:</h3>";
+                                    break;
+                        }
+                            
+                            $return .= <<<HTML
                         <tr>
-                            <td colspan="8">
-                                <a class="btn btn-primary btn-block" style="width:95%;white-space: normal;" href="{$this->core->buildUrl(array('component' => 'navigation', 'course' => $course->getTitle(), 'semester' => $course->getSemester()))}"> {$display_text}{$user->accessAdmin()}</a>
+                            <td colspan="8">     
+                                {$header}
                             </td>
                         </tr>
 HTML;
+
+                        for($q = 0; $q < count($rankWithCourse[$i]); $q++){
+                            $display_text = $rankWithCourse[$i][$q]->getSemester() . " " . $rankWithCourse[$i][$q]->getTitle();
+                            if($course->getDisplayName() !== "") {
+                                $display_text .= " " . $course->getDisplayName();
+                            }
+                        
+                        $return .= <<<HTML
+                        
+                        <tr>
+                            <td colspan="8">
+                                <a class="btn btn-primary btn-block" style="width:95%;white-space: normal;" href="{$this->core->buildUrl(array('component' => 'navigation', 'course' => $rankWithCourse[$i][$q]->getTitle(), 'semester' => $rankWithCourse[$i][$q]->getSemester()))}"> {$display_text}{$user->accessAdmin()}</a>
+                            </td>
+                        </tr>
+HTML;
+                        }
+
+                        $pos++;
                     }
+
+
                     $return .= <<<HTML
                     </tbody>
                 </table>
