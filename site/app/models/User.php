ef021f766d7f3d4a0db26520e5de501f95b59333
Peter Bailie <pbailie@users.noreply.github.com>
2017-10-05 20:16:22 -0400

    Changes to be committed: modified:   site/app/controllers/HomePageController.php modified:   site/app/controllers/admin/UsersController.php modified:   site/app/models/User.php
    Work in progess #1562
    
    * Consolidate user data validation into User.php model class.
    * admin/UsersController now uses validation in User.php model.
    * HomePageController now uses preferred_firstname regex validation in User.php model.

--- site/app/models/User.php
+++ site/app/models/User.php
@@ -233,4 +233,36 @@ class User extends AbstractModel {
         }
         return $this->anon_id;
     }
+
+    /**
+     * Checks $data to make sure it is acceptable for $field.
+     *
+     * @param string $field
+     * @param string $data
+     * @return bool
+     */
+    static public function validateUserData($field, $data) {
+
+    	switch($field) {
+		case 'user_id':
+			//Username / useer_id must contain only lowercase alpha, numbers, underscores, hyphens
+			return preg_match("~^[a-z0-9_\-]+$~", $data) === 1;
+		case 'user_firstname':
+		case 'user_lastname':
+		case 'user_preferred_firstname':
+			//First, Last, Preferred name must be alpha characters, white-space, or certain punctuation.
+        	return preg_match("~^[a-zA-Z'`\-\. ]+$~", $data) === 1;
+		case 'user_email':
+			//Check email address for appropriate format. e.g. "user@university.edu", "user@cs.university.edu", etc.
+			return preg_match("~^[^(),:;<>@\\\"\[\]]+@(?!\-)[a-zA-Z0-9\-]+(?<!\-)(\.[a-zA-Z0-9]+)+$~", $data) === 1;
+		case 'user_group':
+            //user_group check is a digit between 1 - 4.
+			return preg_match("~^[1-4]{1}$~", $data) === 1;
+		case 'user_password':
+	        //Database password cannot be blank, no check on format
+			return $data !== "";
+		default:
+			trigger_error("validateUserData() called with unknown field: " . var_export(htmlentities($field), true) . "; data: " . var_export(htmlentities($data), true), E_USER_ERROR);
+    	}
+    }
 }
