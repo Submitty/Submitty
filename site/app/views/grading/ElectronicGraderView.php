3ace0b7eea5f6b633b687b54afeddd0bc3d2f5a9
Evan Maicus <emaicus@outlook.com>
2017-10-20 16:52:23 -0400

    Fixed the issue.

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -584,10 +584,15 @@ HTML;
                     $btn_class = "btn-default";
                     if($row->validateVersions()) {
                         $contents = "{$row->getGradedTAPoints()}&nbsp;/&nbsp;{$row->getTotalTANonExtraCreditPoints()}";
-			$graded += $row->getGradedTAPoints();
+			            $graded += $row->getGradedTAPoints();
                     }
                     else{
-                        $contents = "Version Conflict";
+                        if(!$row->isFullyGraded()){
+                            $contents = "Not Fully Graded";
+                        }
+                        else{
+                            $contents = "Version Conflict";
+                        }
                     }
                 }
                 else {
