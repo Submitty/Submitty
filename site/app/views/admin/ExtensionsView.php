50ad7846f0b162a27075ce5c9561cfdd3b833f04
andrewaikens87 <andrewaikens87@gmail.com>
2017-11-05 13:47:05 -0500

    Change late days to extensions

--- site/app/views/admin/ExtensionsView.php
+++ site/app/views/admin/ExtensionsView.php
@@ -31,10 +31,10 @@ HTML;
     <input type="hidden" name="csrf_token" value="{$this->core->getCsrfToken()}" />
         <div class="option-title">Single Student Entry<br></div>
         <div class="option" style="width:25%; display:inline-block;">Student ID:<br><input class="option-input" type="text" name="user_id" id="user_id" style="float:left"></div>
-        <div class="option" style="width:25%; display:inline-block;">Late Days:<br><input class="option-input" type="text" name="late_days" id="late_days" style="float:left"></div>
+        <div class="option" style="width:25%; display:inline-block;">Extension Days:<br><input class="option-input" type="text" name="late_days" id="late_days" style="float:left"></div>
         <div class="option" style="width:10%; display:inline-block; vertical-align: bottom;"><input class="btn btn-primary" type="submit" style="float:left"></div>
         <div class="option-title">Multiple Student Entry Via CSV Upload</div>
-        <div>Do not use column headers. CSV must be of the following form: student_id, gradeable_id, late_days</div>
+        <div>Do not use column headers. CSV must be of the following form: student_id, gradeable_id, extension_days</div>
         <div style="padding-bottom:20px;"><input type="file" name="csv_upload" id="csv_upload" onchange="return updateHomeworkExtensions($(this));"></div>
     </form>
     </div>
@@ -46,7 +46,7 @@ HTML;
             <td>Student ID</td>
             <td>First Name</td>
             <td>Last Name</td>
-            <td>Late Day Exceptions</td>
+            <td>Extension Days</td>
         </thead>
         </div>
     </table>
