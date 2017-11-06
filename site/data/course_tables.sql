54b2959e24ae5b4a8417ba602fe55ddcb4e05ef8
Barb Cutler <bmcutler@gmail.com>
2017-11-05 23:50:55 -0500

    add allow_late_submission boolean

--- site/data/course_tables.sql
+++ site/data/course_tables.sql
@@ -133,6 +133,7 @@ CREATE TABLE electronic_gradeable (
     eg_submission_open_date timestamp(6) with time zone NOT NULL,
     eg_submission_due_date timestamp(6) with time zone NOT NULL,
     eg_late_days integer DEFAULT (-1) NOT NULL,
+    eg_allow_late_submission boolean DEFAULT true,
     eg_peer_grade_set integer DEFAULT (0) NOT NULL,
     eg_precision numeric NOT NULL,
     CONSTRAINT eg_submission_date CHECK ((eg_submission_open_date <= eg_submission_due_date))
