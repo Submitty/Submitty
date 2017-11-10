9813d46a04af7ad44be6cdf2c83e21167c7ec7f9
Andrew Aikens <andrewaikens87@gmail.com>
2017-11-09 20:17:57 -0500

    Fix error message on Excused Absence Page (#1602)
    * Add message for invalid user
    
    * Fix validateHomework function
    
    * Change late days to extensions
    
    * Revert "Fix validateHomework function"
    
    This reverts commit 67c86678e62feac80454dad0e62c3a597cfad21e.
    
    * Revert "Change late days to extensions"
    
    This reverts commit 50ad7846f0b162a27075ce5c9561cfdd3b833f04.
    
    * Remove unnecessary function I added
    
    * Change exception handling to be generalized
    
    * Make invalid user be caught in error check
    
    * Change is_null to empty function

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -825,7 +825,13 @@ function updateHomeworkExtensions(data) {
         cache: false,
         contentType: false,
         success: function(data) {
-            var json = JSON.parse(data);
+            try { 
+                var json = JSON.parse(data);
+            } catch(err){
+                var message ='<div class="inner-message alert alert-error" style="position: fixed;top: 40px;left: 50%;width: 40%;margin-left: -20%;" id="theid"><a class="fa fa-times message-close" onClick="removeMessagePopup(\'theid\');"></a><i class="fa fa-times-circle"></i>Error parsing data. Please try again.</div>';
+                $('#messages').append(message);
+                return;
+            }
             if(json['error']){
                 var message ='<div class="inner-message alert alert-error" style="position: fixed;top: 40px;left: 50%;width: 40%;margin-left: -20%;" id="theid"><a class="fa fa-times message-close" onClick="removeMessagePopup(\'theid\');"></a><i class="fa fa-times-circle"></i>' + json['error'] + '</div>';
                 $('#messages').append(message);
@@ -878,7 +884,6 @@ function loadHomeworkExtensions(g_id) {
         }
     });
 }
-
 
 function updateLateDays(data) {
     var fd = new FormData($('#lateDayForm').get(0));
