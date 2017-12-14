fdfbbd4be56ccdb125d5c6505ad201d7395e44bc
Barb Cutler <bmcutler@users.noreply.github.com>
2017-12-14 17:52:48 -0500

    Clicker tweak2 (#1642)
    * revise 3 page notes instructions
    
    * revise 2 page notes instructions
    
    * replacement iclicker
    
    * edits

--- grading/execute.cpp
+++ grading/execute.cpp
@@ -869,14 +869,12 @@ int exec_this_command(const std::string &cmd, std::ofstream &logfile, const nloh
   }
 
 
-
   // SECCOMP: install the filter (system calls restrictions)
   if (install_syscall_filter(prog_is_32bit, my_program,logfile, whole_config)) {
     std::cout << "seccomp filter install failed" << std::endl;
     return 1;
   }
   // END SECCOMP
-
 
   
   int child_result =  execv ( my_program.c_str(), my_char_args );
