3e54a5220d2e5bbbbbe9dc91f09d0f177642cb74
andrewaikens87 <andrewaikens87@gmail.com>
2017-12-30 18:15:56 -0500

    Add create thread and posts

--- site/app/controllers/forum/ForumController.php
+++ site/app/controllers/forum/ForumController.php
@@ -29,27 +29,61 @@ class ForumController extends AbstractController {
             case 'create_thread':
                 $this->showCreateThread();
                 break;
+            case 'publish_thread':
+                $this->publishThread();
+                break;
+            case 'publish_post':
+                $this->publishPost();
+                break;
             case 'view_thread':
-            case 'home_page':
             default:
                 $this->showThreads();
                 break;
         }
     }
 
+    public function publishThread(){
+        $title = htmlentities($_POST["title"], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $thread_content = htmlentities($_POST["thread_content"], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        if(empty($title) || empty($thread_content)){
+            $this->core->addErrorMessage("One of the fields was empty. Please re-submit your thread.");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'forum', 'page' => 'create_thread')));
+        } else {
+            $id = $this->core->getQueries()->createThread($this->core->getUser()->getId(), $title, $thread_content, 0);
+            $this->core->redirect($this->core->buildUrl(array('component' => 'forum', 'page' => 'view_thread', 'thread_id' => $id)));
+        }
+    }
+
+    private function publishPost(){
+        $post_content = htmlentities($_POST["post_content"], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        $thread_id = htmlentities($_POST["thread_id"], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+
+        if(empty($post_content) || empty($thread_id)){
+            $this->core->addErrorMessage("There was an error submitting your post. Please re-submit your post.");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'forum', 'page' => 'view_thread')));
+        } else {
+            $this->core->getQueries()->createPost($this->core->getUser()->getId(), $post_content, $thread_id, 0, 0);
+            $this->core->redirect($this->core->buildUrl(array('component' => 'forum', 'page' => 'view_thread', 'thread_id' => $thread_id)));
+        }
+    }
+
     public function showThreads(){
-        $user = $this->core->getUser();
+        $user = $this->core->getUser()->getId();
         $threads = $this->core->getQueries()->loadThreads();
         $posts = null;
-        if(isset($_POST["thread_id"])){
-            $posts = $this->core->getQueries()->getPostsForThread($threads);
+        if(isset($_REQUEST["thread_id"])){
+            $posts = $this->core->getQueries()->getPostsForThread($_REQUEST["thread_id"]);
         } else {
             //We are at the "Home page"
             //Show the first post
-            $posts = $this->core->getQueries()->getPostsForThread($threads);
+            $posts = $this->core->getQueries()->getPostsForThread(-1);
             
         }
         $this->core->getOutput()->renderOutput('forum\ForumThread', 'showForumThreads', $user, $posts, $threads);
+    }
+
+    public function showCreateThread(){
+         $this->core->getOutput()->renderOutput('forum\ForumThread', 'createThread');
     }
 
 }
