18811c723092d27b0c9b27215c0f8d0af0819f22
Barb Cutler <bmcutler@users.noreply.github.com>
2018-01-08 16:44:04 -0500

    Make database timestamp types consistent (#1653)

--- .setup/update_database.py
+++ .setup/update_database.py
@@ -22,6 +22,15 @@ usr_path = "/usr/local/submitty"
 settings = json.load(open(os.path.join(usr_path, ".setup", "submitty_conf.json")))
 
 
+# ==============================
+# edits to the master database
+
+# edits to make timestamps consistent
+os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE sessions ALTER COLUMN session_expires SET DATA TYPE timestamp(6) with time zone'".format(settings['database_password'], settings['database_host'], settings['database_user'], 'submitty'))
+os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE users ALTER COLUMN last_updated SET DATA TYPE timestamp(6) with time zone'".format(settings['database_password'], settings['database_host'], settings['database_user'], 'submitty'))
+
+# ==============================
+# edits to each course database
 for term in os.scandir(os.path.join(settings['submitty_data_dir'],"courses")):
     for course in os.scandir(os.path.join(settings['submitty_data_dir'], "courses", term.name)):
         name = course.name
@@ -40,3 +49,8 @@ for term in os.scandir(os.path.join(settings['submitty_data_dir'],"courses")):
         os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE gradeable_component ALTER COLUMN gc_lower_clamp SET DATA TYPE numeric'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
         os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE gradeable_component ALTER COLUMN gc_default SET DATA TYPE numeric'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
         os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE gradeable_component ALTER COLUMN gc_upper_clamp SET DATA TYPE numeric'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
+
+        # edits to make timestamps consistent
+        os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE late_days ALTER COLUMN since_timestamp SET DATA TYPE timestamp(6) with time zone'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
+        os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE sessions ALTER COLUMN session_expires SET DATA TYPE timestamp(6) with time zone'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
+        os.system("PGPASSWORD='{}' psql --host={} --username={} --dbname={} -c 'ALTER TABLE users ALTER COLUMN last_updated SET DATA TYPE timestamp(6) with time zone'".format(settings['database_password'], settings['database_host'], settings['database_user'], db))
