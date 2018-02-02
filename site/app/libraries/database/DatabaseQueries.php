e9dede26793f7a7153bf6053fa5fa2912e3f50e6
Andrew Aikens <andrewaikens87@gmail.com>
2018-02-02 01:29:34 -0500

    Forum Enhancements (#1690)
    * Implementents enable announcement, multiple attachments, and db transaction block
    
    * client-side error check
    
    * server side error check for upload number
    
    * Multiple attachment tweaks
    
    * Resolve merge conflicts
    
    * Resolves conlflicts

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -118,10 +118,17 @@ class DatabaseQueries {
             $this->course_db->query("SELECT id FROM posts where timestamp = (SELECT MAX(timestamp) from posts where thread_id = ? and deleted=false)", array($thread_id));
             $parent_post = $this->course_db->rows()[0]["id"];
         }
-        $this->course_db->query("INSERT INTO posts (thread_id, parent_id, author_user_id, content, timestamp, anonymous, deleted, endorsed_by, resolved, type, has_attachment) VALUES (?, ?, ?, ?, current_timestamp, ?, ?, ?, ?, ?, ?)", array($thread_id, $parent_post, $user, $content, $anonymous, 0, NULL, 0, $type, $hasAttachment));
 
+        try {
+        $this->course_db->query("INSERT INTO posts (thread_id, parent_id, author_user_id, content, timestamp, anonymous, deleted, endorsed_by, resolved, type, has_attachment) VALUES (?, ?, ?, ?, current_timestamp, ?, ?, ?, ?, ?, ?)", array($thread_id, $parent_post, $user, $content, $anonymous, 0, NULL, 0, $type, $hasAttachment));
+        $this->course_db->query("DELETE FROM viewed_responses WHERE thread_id = ?", array($thread_id));
         //retrieve generated thread_id
         $this->course_db->query("SELECT MAX(id) as max_id from posts where thread_id=? and author_user_id=?", array($thread_id, $user));
+        } catch (DatabaseException $dbException){
+            if($this->course_db->inTransaction()){
+                $this->course_db->rollback();
+            }
+        } 
 
         return $this->course_db->rows()[0]["max_id"];
     }
@@ -133,16 +140,24 @@ class DatabaseQueries {
 
     public function createThread($user, $title, $content, $anon, $prof_pinned, $hasAttachment){
 
+        $this->course_db->beginTransaction();
+
+        try {
         //insert data
         $this->course_db->query("INSERT INTO threads (title, created_by, pinned, deleted, merged_id, is_visible) VALUES (?, ?, ?, ?, ?, ?)", array($title, $user, $prof_pinned, 0, -1, true));
 
         //retrieve generated thread_id
         $this->course_db->query("SELECT MAX(id) as max_id from threads where title=? and created_by=?", array($title, $user));
+        } catch(DatabaseException $dbException) {
+            $this->course_db->rollback();
+        } 
 
         //Max id will be the most recent post
         $id = $this->course_db->rows()[0]["max_id"];
 
         $post_id = $this->createPost($user, $content, $id, $anon, 0, true, $hasAttachment);
+
+        $this->course_db->commit();
 
         return array("thread_id" => $id, "post_id" => $post_id);
     }
