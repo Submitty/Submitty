5efa604857176dad37c5cb6a22d8392a56026599
Shail Patel <shailpatel67@gmail.com>
2018-02-15 19:51:33 -0500

    Users can now add '+' and other weird charectors to file names
    File names are now URI encoded when uploading files and decoded properlly when viewing them. This handles '+' and any other weird charectors '(', '%', '&', etc

--- site/app/controllers/MiscController.php
+++ site/app/controllers/MiscController.php
@@ -179,20 +179,21 @@ class MiscController extends AbstractController {
             return false;
         }
 
-        $mime_type = FileUtils::getMimeType($_REQUEST['path']);
+        $corrected_name = pathinfo($_REQUEST['path'], PATHINFO_DIRNAME) . "/" . rawurlencode( basename($_REQUEST['path']));
+        $mime_type = FileUtils::getMimeType($corrected_name);
         $this->core->getOutput()->useHeader(false);
         $this->core->getOutput()->useFooter(false);
         if ($mime_type === "application/pdf" || Utils::startsWith($mime_type, "image/")) {
             header("Content-type: ".$mime_type);
-            header('Content-Disposition: inline; filename="' .  basename($_REQUEST['path']) . '"');
-            readfile($_REQUEST['path']);
+            header('Content-Disposition: inline; filename="' . basename($_REQUEST['path']) . '"');
+            readfile($corrected_name);
             $this->core->getOutput()->renderString($_REQUEST['path']);
         }
         else {
-            $contents = htmlentities(file_get_contents($_REQUEST['path']), ENT_SUBSTITUTE);
+            $contents = htmlentities(file_get_contents($corrected_name), ENT_SUBSTITUTE);
             if ($_REQUEST['ta_grading'] === "true") {
-                $filename = htmlentities($_REQUEST['file'], ENT_SUBSTITUTE);
-                $this->core->getOutput()->renderOutput('Misc', 'displayCode', $mime_type, $filename, $contents);
+                $filename = htmlentities($corrected_name, ENT_SUBSTITUTE);
+                $this->core->getOutput()->renderOutput('Misc', 'displayCode', $mime_type, $corrected_name, $contents);
             }
             else {
                 $this->core->getOutput()->renderOutput('Misc', 'displayFile', $contents);
@@ -215,7 +216,7 @@ class MiscController extends AbstractController {
         header('Content-Type: application/octet-stream');
         header("Content-Transfer-Encoding: Binary"); 
         header("Content-disposition: attachment; filename=\"{$_REQUEST['file']}\"");
-        readfile($_REQUEST['path']);
+        readfile(pathinfo($_REQUEST['path'], PATHINFO_DIRNAME) . "/" . rawurlencode( basename($_REQUEST['path'])));
     }
 
     private function downloadZip() {
--- site/app/views/submission/HomeworkView.php
+++ site/app/views/submission/HomeworkView.php
@@ -573,10 +573,12 @@ HTML;
                         // get the full filename for PDF popout
                         // add "timestamp / full filename" to count_array so that path to each filename is to the full PDF, not the cover
                         $url = $this->core->getConfig()->getSiteUrl()."&component=misc&page=display_file&dir=uploads&file=".$filename."&path=".$path."&ta_grading=false";
-                        $filename_full = str_replace("_cover.pdf", ".pdf", $filename);
+                        $filename_full = str_replace("_cover.pdf", ".pdf", rawurldecode( $filename) );
                         $path_full = str_replace("_cover.pdf", ".pdf", $path);
                         $url_full = $this->core->getConfig()->getSiteUrl()."&component=misc&page=display_file&dir=uploads&file=".$filename_full."&path=".$path_full."&ta_grading=false";
-                        $count_array[$count] = FileUtils::joinPaths($timestamp, $filename_full);
+                        $count_array[$count] = FileUtils::joinPaths($timestamp, rawurlencode( $filename_full) );
+                        //decode the filename after to display correctly for users
+                        $filename_full = rawurldecode($filename_full);
                         $return .= <<<HTML
             <tr class="tr tr-vertically-centered">
                 <td>{$count}</td>
