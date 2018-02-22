3c95f285917246e8a3296bf9b82f1170be415b2b
Matthew Peveler <matt.peveler@gmail.com>
2018-02-21 23:40:12 -0500

    Write out submitty details to conf folder

--- .setup/CONFIGURE_SUBMITTY.py
+++ .setup/CONFIGURE_SUBMITTY.py
@@ -7,7 +7,7 @@ import json
 import os
 import pwd
 import shutil
-import stat
+
 
 def get_uid(user):
     return pwd.getpwnam(user).pw_uid
@@ -37,8 +37,12 @@ def get_input(question, default=""):
 if os.getuid() != 0:
     raise SystemExit('ERROR: This script must be run by root or sudo')
 
-parser = argparse.ArgumentParser(description='Submitty configuration script')
-parser.add_argument('--debug', action='store_true', default=False, help='Configure Submitty in debug mode')
+parser = argparse.ArgumentParser(description='Submitty configuration script',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--debug', action='store_true', default=False, help='Configure Submitty to be in debug mode. '
+                                                                        'This should not be used in production!')
+parser.add_argument('--install-dir', default='/usr/local/submitty', help='Set the install directory for Submitty')
+parser.add_argument('--data-dir', default='/var/local/submitty', help='Set the data directory for Submitty')
 args = parser.parse_args()
 
 # determine location of SUBMITTY GIT repository
@@ -49,8 +53,14 @@ SUBMITTY_REPOSITORY = os.path.dirname(SETUP_SCRIPT_DIRECTORY)
 
 # recommended (default) directory locations
 # FIXME: Check that directories exist and are readable/writeable?
-SUBMITTY_INSTALL_DIR = '/usr/local/submitty'
-SUBMITTY_DATA_DIR = '/var/local/submitty'
+SUBMITTY_INSTALL_DIR = args.install_dir
+if not os.path.isdir(SUBMITTY_INSTALL_DIR) or not os.access(SUBMITTY_INSTALL_DIR, os.R_OK | os.W_OK):
+    raise SystemExit('Install directory {} does not exist or is not accessible'.format(SUBMITTY_INSTALL_DIR))
+
+SUBMITTY_DATA_DIR = args.data_dir
+if not os.path.isdir(SUBMITTY_DATA_DIR) or not os.access(SUBMITTY_DATA_DIR, os.R_OK | os.W_OK):
+    raise SystemExit('Data directory {} does not exist or is not accessible'.format(SUBMITTY_DATA_DIR))
+
 SUBMITTY_TUTORIAL_DIR = os.path.join(SUBMITTY_INSTALL_DIR, 'GIT_CHECKOUT_Tutorial')
 
 TAGRADING_LOG_PATH = os.path.join(SUBMITTY_DATA_DIR, 'logs')
@@ -62,6 +72,7 @@ AUTOGRADING_LOG_PATH = os.path.join(SUBMITTY_DATA_DIR, 'logs', 'autograding')
 HWPHP_USER = 'hwphp'
 HWCGI_USER = 'hwcgi'
 HWCRON_USER = 'hwcron'
+HWCRON_GROUP = 'hwcron'
 HWPHP_UID, HWPHP_GID = get_ids(HWPHP_USER)
 HWCGI_UID, HWCGI_GID = get_ids(HWCGI_USER)
 HWCRON_UID, HWCRON_GID = get_ids(HWCRON_USER)
@@ -108,8 +119,13 @@ SETUP_REPOSITORY_DIR = os.path.join(SUBMITTY_REPOSITORY, '.setup')
 
 CONFIGURATION_FILE = os.path.join(SETUP_INSTALL_DIR, 'INSTALL_SUBMITTY.sh')
 CONFIGURATION_JSON = os.path.join(SETUP_INSTALL_DIR, 'submitty_conf.json')
-SITE_CONFIG_DIR  = os.path.join(SUBMITTY_INSTALL_DIR, "site", "config")
-WORKERS_JSON = os.path.join(SETUP_INSTALL_DIR, "autograding_workers.json")
+SITE_CONFIG_DIR = os.path.join(SUBMITTY_INSTALL_DIR, "site", "config")
+
+CONF_INSTALL_DIR = os.path.join(SUBMITTY_INSTALL_DIR, 'conf')
+
+DATABASE_JSON = os.path.join(CONF_INSTALL_DIR, 'database_conf.json')
+SUBMITTY_JSON = os.path.join(CONF_INSTALL_DIR, 'submitty_conf.json')
+WORKERS_JSON = os.path.join(CONF_INSTALL_DIR, 'autograding_workers.json')
 
 ##############################################################################
 
@@ -130,7 +146,7 @@ if os.path.isfile(CONFIGURATION_JSON):
 # grab anything not loaded in (useful for backwards compatibility if a new default is added that 
 # is not in an existing config file.)
 for key in defaults.keys():
-    if not key in loaded_defaults:
+    if key not in loaded_defaults:
         loaded_defaults[key] = defaults[key]
 defaults = loaded_defaults
 
@@ -158,10 +174,12 @@ if DATABASE_PASS == '' and DATABASE_USER == defaults['database_user'] and 'datab
     DATABASE_PASS = defaults['database_password']
 print()
 
-SUBMISSION_URL = get_input('What is the url for submission? (ex: http://192.168.56.101 or https://submitty.cs.rpi.edu)', defaults['submission_url']).rstrip('/')
+SUBMISSION_URL = get_input('What is the url for submission? (ex: http://192.168.56.101 or '
+                           'https://submitty.cs.rpi.edu)', defaults['submission_url']).rstrip('/')
 print()
 
-INSTITUTION_NAME = get_input('What is the name of your institution? (Leave blank/type "none" if not desired)', defaults['institution_name'])
+INSTITUTION_NAME = get_input('What is the name of your institution? (Leave blank/type "none" if not desired)',
+                             defaults['institution_name'])
 if INSTITUTION_NAME.lower() == "none":
     INSTITUTION_NAME = ''
 print()
@@ -169,7 +187,8 @@ print()
 if INSTITUTION_NAME == '' or INSTITUTION_NAME.isspace():
     INSTITUTION_HOMEPAGE = ''
 else:
-    INSTITUTION_HOMEPAGE = get_input('What is the url of your institution\'s homepage? (Leave blank/type "none" if not desired)', defaults['institution_homepage'])
+    INSTITUTION_HOMEPAGE = get_input("What is the url of your institution\'s homepage? "
+                                     '(Leave blank/type "none" if not desired)', defaults['institution_homepage'])
     if INSTITUTION_HOMEPAGE.lower() == "none":
         INSTITUTION_HOMEPAGE = ''
     print()
@@ -198,14 +217,6 @@ CGI_URL = SUBMISSION_URL + '/cgi-bin'
 
 ##############################################################################
 # make the installation setup directory
-worker_dict = {}
-
-if os.path.isfile(WORKERS_JSON):
-    with open(WORKERS_JSON, 'r') as f:
-        worker_dict = json.load(f)
-else:
-    worker_dict["primary"] = {"capabilities" : ["default"], "address" : "", "username" : "",
-        "num_autograding_workers" : NUM_GRADING_SCHEDULER_WORKERS}
 
 if os.path.isdir(SETUP_INSTALL_DIR):
     shutil.rmtree(SETUP_INSTALL_DIR)
@@ -213,54 +224,55 @@ os.makedirs(SETUP_INSTALL_DIR, exist_ok=True)
 shutil.chown(SETUP_INSTALL_DIR, 'root', COURSE_BUILDERS_GROUP)
 os.chmod(SETUP_INSTALL_DIR, 0o751)
 
-with open(WORKERS_JSON, 'w') as workers_file:
-    json.dump(worker_dict, workers_file, indent=4)
-
+if os.path.isdir(CONF_INSTALL_DIR):
+    shutil.rmtree(CONF_INSTALL_DIR)
+os.makedirs(CONF_INSTALL_DIR, exist_ok=True)
+shutil.chown(CONF_INSTALL_DIR, 'root', COURSE_BUILDERS_GROUP)
+os.chmod(CONF_INSTALL_DIR, 0o751)
 
 ##############################################################################
-# WRITE THE VARIABLES TO A FILE
+# WRITE CONFIG FILES IN ${SUBMITTY_INSTALL_DIR}/.setup
 
-obj = OrderedDict()
-obj['submitty_install_dir'] = SUBMITTY_INSTALL_DIR
-obj['submitty_repository'] = SUBMITTY_REPOSITORY
-obj['submitty_tutorial_dir'] = SUBMITTY_TUTORIAL_DIR
-obj['submitty_data_dir'] = SUBMITTY_DATA_DIR
-obj['hwphp_user'] = HWPHP_USER
-obj['hwcgi_user'] = HWCGI_USER
-obj['hwcron_user'] = HWCRON_USER
-obj['hwcronphp_group'] = HWCRONPHP_GROUP
-obj['course_builders_group'] = COURSE_BUILDERS_GROUP
+config = OrderedDict()
+config['submitty_install_dir'] = SUBMITTY_INSTALL_DIR
+config['submitty_repository'] = SUBMITTY_REPOSITORY
+config['submitty_tutorial_dir'] = SUBMITTY_TUTORIAL_DIR
+config['submitty_data_dir'] = SUBMITTY_DATA_DIR
+config['hwphp_user'] = HWPHP_USER
+config['hwcgi_user'] = HWCGI_USER
+config['hwcron_user'] = HWCRON_USER
+config['hwcronphp_group'] = HWCRONPHP_GROUP
+config['course_builders_group'] = COURSE_BUILDERS_GROUP
 
-obj['num_untrusted'] = NUM_UNTRUSTED
-obj['first_untrusted_uid'] = FIRST_UNTRUSTED_UID
-obj['first_untrusted_gid'] = FIRST_UNTRUSTED_UID
+config['num_untrusted'] = NUM_UNTRUSTED
+config['first_untrusted_uid'] = FIRST_UNTRUSTED_UID
+config['first_untrusted_gid'] = FIRST_UNTRUSTED_UID
 
-obj['hwcron_uid'] = HWCRON_UID
-obj['hwcron_gid'] = HWCRON_GID
-obj['hwphp_uid'] = HWPHP_UID
-obj['hwphp_gid'] = HWPHP_GID
+config['hwcron_uid'] = HWCRON_UID
+config['hwcron_gid'] = HWCRON_GID
+config['hwphp_uid'] = HWPHP_UID
+config['hwphp_gid'] = HWPHP_GID
 
-obj['database_host'] = DATABASE_HOST
-obj['database_user'] = DATABASE_USER
-obj['database_password'] = DATABASE_PASS
+config['database_host'] = DATABASE_HOST
+config['database_user'] = DATABASE_USER
+config['database_password'] = DATABASE_PASS
 
-obj['authentication_method'] = AUTHENTICATION_METHOD
+config['authentication_method'] = AUTHENTICATION_METHOD
 
-obj['submission_url'] = SUBMISSION_URL
-obj['tagrading_url'] = TAGRADING_URL
-obj['cgi_url'] = CGI_URL
+config['submission_url'] = SUBMISSION_URL
+config['tagrading_url'] = TAGRADING_URL
+config['cgi_url'] = CGI_URL
 
-obj['autograding_log_path'] = AUTOGRADING_LOG_PATH
-obj['site_log_path'] = TAGRADING_LOG_PATH
+config['autograding_log_path'] = AUTOGRADING_LOG_PATH
+config['site_log_path'] = TAGRADING_LOG_PATH
 
-obj['num_grading_scheduler_workers'] = NUM_GRADING_SCHEDULER_WORKERS
+config['num_grading_scheduler_workers'] = NUM_GRADING_SCHEDULER_WORKERS
 
-obj['debugging_enabled'] = DEBUGGING_ENABLED
+config['debugging_enabled'] = DEBUGGING_ENABLED
 
-obj['institution_name'] = INSTITUTION_NAME
-obj['username_change_text'] = USERNAME_TEXT
-obj['institution_homepage'] = INSTITUTION_HOMEPAGE
-
+config['institution_name'] = INSTITUTION_NAME
+config['username_change_text'] = USERNAME_TEXT
+config['institution_homepage'] = INSTITUTION_HOMEPAGE
 
 with open(CONFIGURATION_FILE, 'w') as open_file:
     def write(x=''):
@@ -272,7 +284,7 @@ with open(CONFIGURATION_FILE, 'w') as open_file:
     write('# Manual editing is allowed (but will be clobbered if CONFIGURE_SUBMITTY.py is re-run)')
     write()
 
-    for key, value in obj.items():
+    for key, value in config.items():
         key = str(key).upper()
         if isinstance(value, str):
             # To escape a single quote in bash, use '\'' because bash is awful
@@ -288,10 +300,83 @@ with open(CONFIGURATION_FILE, 'w') as open_file:
 os.chmod(CONFIGURATION_FILE, 0o700)
 
 with open(CONFIGURATION_JSON, 'w') as json_file:
-    json.dump(obj, json_file, indent=2)
-    json_file.write('\n')
+    json.dump(config, json_file, indent=2)
 
 os.chmod(CONFIGURATION_JSON, 0o500)
+
+##############################################################################
+# WRITE CONFIG FILES IN ${SUBMITTY_INSTALL_DIR}/conf
+
+# Write protected details. This should be readable only by root + hwcron
+config = OrderedDict()
+config['database_host'] = DATABASE_HOST
+config['database_user'] = DATABASE_USER
+config['database_password'] = DATABASE_PASS
+config['debugging_enabled'] = DEBUGGING_ENABLED
+with open(DATABASE_JSON, 'w') as json_file:
+    json.dump(config, json_file, indent=2)
+
+shutil.chown(DATABASE_JSON, 'root', HWCRON_GROUP)
+os.chmod(DATABASE_JSON, 0o440)
+
+config = OrderedDict()
+config['submitty_install_dir'] = SUBMITTY_INSTALL_DIR
+config['submitty_repository'] = SUBMITTY_REPOSITORY
+config['submitty_tutorial_dir'] = SUBMITTY_TUTORIAL_DIR
+config['submitty_data_dir'] = SUBMITTY_DATA_DIR
+config['hwphp_user'] = HWPHP_USER
+config['hwcgi_user'] = HWCGI_USER
+config['hwcron_user'] = HWCRON_USER
+config['hwcronphp_group'] = HWCRONPHP_GROUP
+config['course_builders_group'] = COURSE_BUILDERS_GROUP
+
+config['num_untrusted'] = NUM_UNTRUSTED
+config['first_untrusted_uid'] = FIRST_UNTRUSTED_UID
+config['first_untrusted_gid'] = FIRST_UNTRUSTED_UID
+
+config['hwcron_uid'] = HWCRON_UID
+config['hwcron_gid'] = HWCRON_GID
+config['hwphp_uid'] = HWPHP_UID
+config['hwphp_gid'] = HWPHP_GID
+
+config['authentication_method'] = AUTHENTICATION_METHOD
+
+config['submission_url'] = SUBMISSION_URL
+config['tagrading_url'] = TAGRADING_URL
+config['cgi_url'] = CGI_URL
+
+config['autograding_log_path'] = AUTOGRADING_LOG_PATH
+config['site_log_path'] = TAGRADING_LOG_PATH
+
+config['num_grading_scheduler_workers'] = NUM_GRADING_SCHEDULER_WORKERS
+
+config['institution_name'] = INSTITUTION_NAME
+config['username_change_text'] = USERNAME_TEXT
+config['institution_homepage'] = INSTITUTION_HOMEPAGE
+
+with open(SUBMITTY_JSON, 'w') as json_file:
+    json.dump(config, json_file, indent=2)
+shutil.chown(SUBMITTY_JSON, 'root', COURSE_BUILDERS_GROUP)
+os.chmod(SUBMITTY_JSON, 0o440)
+
+##############################################################################
+# WRITE THE WORKER CONF TO FILE
+
+worker_dict = {}
+
+if os.path.isfile(WORKERS_JSON):
+    with open(WORKERS_JSON, 'r') as f:
+        worker_dict = json.load(f)
+else:
+    worker_dict["primary"] = {
+        "capabilities": ["default"],
+        "address": "",
+        "username": "",
+        "num_autograding_workers": NUM_GRADING_SCHEDULER_WORKERS
+    }
+
+with open(WORKERS_JSON, 'w') as workers_file:
+    json.dump(worker_dict, workers_file, indent=4)
 
 ##############################################################################
 
