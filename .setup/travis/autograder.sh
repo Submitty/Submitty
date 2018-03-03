17fb8488ebbc9d51233d9b14bb525b6c786f5440
Barb Cutler <bmcutler@gmail.com>
2018-03-02 23:14:43 -0500

    add jacoco to travis installation

--- .setup/travis/autograder.sh
+++ .setup/travis/autograder.sh
@@ -38,8 +38,20 @@ mv emma-2.0.5312/lib/emma.jar emma.jar
 rm -rf emma-2.0.5312
 rm emma-2.0.5312.zip
 rm index.html* > /dev/null 2>&1
-
 chmod o+r . *.jar
+
+# JaCoCo is a potential replacement for EMMA
+echo "Getting JaCoCo..."
+JACOCO_VER=0.8.0
+wget https://github.com/jacoco/jacoco/releases/download/v${JACOCO_VER}/jacoco-${JACOCO_VER}.zip -o /dev/null > /dev/null 2>&1
+mkdir jacoco-${JACOCO_VER}
+unzip jacoco-${JACOCO_VER}.zip -d jacoco-${JACOCO_VER} > /dev/null
+mv jacoco-${JACOCO_VER}/lib/jacococli.jar jacococli.jar
+mv jacoco-${JACOCO_VER}/lib/jacocoagent.jar jacocoagent.jar
+rm -rf jacoco-${JACOCO_VER}
+rm jacoco-${JACOCO_VER}.zip
+chmod o+r . *.jar
+
 popd
 
 # --------------------------------------
