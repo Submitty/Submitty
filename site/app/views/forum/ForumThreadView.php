0606141f931807be1afe4528fa9987755f900e37
andrewaikens87 <andrewaikens87@gmail.com>
2018-03-06 21:58:25 -0500

    Fix multiple links in a post

--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -294,18 +294,17 @@ HTML;
                         $pos = 0;
                         if(count($result) > 0) {
                         	foreach($result[1] as $url){
-                        		$decoded_url = trim(strip_tags(html_entity_decode($url, ENT_QUOTES | ENT_HTML5, 'UTF-8')));
+                        		$decoded_url = filter_var(trim(strip_tags(html_entity_decode($url, ENT_QUOTES | ENT_HTML5, 'UTF-8'))), FILTER_SANITIZE_URL);
                         		$parsed_url = parse_url($decoded_url, PHP_URL_SCHEME);
                         		if(filter_var($decoded_url, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED | FILTER_FLAG_HOST_REQUIRED) !== false && in_array($parsed_url, $accepted_schemes, true)){
-                        			$pre_post = preg_replace('#\&lbrack;url&equals;(.*?)&rsqb;(.*?)(&lbrack;&sol;url&rsqb;)#', '<a href="' . $decoded_url . '">'. htmlentities(htmlspecialchars(strip_tags($result[2][$pos])), ENT_QUOTES | ENT_HTML5, 'UTF-8') .'</a>', $post_content);
+                        			$pre_post = preg_replace('#\&lbrack;url&equals;(.*?)&rsqb;(.*?)(&lbrack;&sol;url&rsqb;)#', '<a href="' . $decoded_url . '">'. $result[2][$pos] .'</a>', $post_content, 1);
 
                         		} else {
-                        			$pre_post = preg_replace('#\&lbrack;url&equals;(.*?)&rsqb;(.*?)(&lbrack;&sol;url&rsqb;)#', htmlentities(htmlspecialchars($decoded_url), ENT_QUOTES | ENT_HTML5, 'UTF-8'), $post_content);
+                        			$pre_post = preg_replace('#\&lbrack;url&equals;(.*?)&rsqb;(.*?)(&lbrack;&sol;url&rsqb;)#', htmlentities(htmlspecialchars($decoded_url), ENT_QUOTES | ENT_HTML5, 'UTF-8'), $post_content, 1);
                         		}
                         		if(!empty($pre_post)){
                         			$post_content = $pre_post;
-                        		
-                        		}
+                        		} $pre_post = "";
                         		 $pos++;
                         	}
                         }
