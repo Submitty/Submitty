35ca1fecefae76a2db259920de07ae57448a243a
Barb Cutler <bmcutler@users.noreply.github.com>
2018-03-28 00:49:25 -0400

    attempt to fix travis?

--- tests/unitTests/app/libraries/ExceptionHandlerTester.php
+++ tests/unitTests/app/libraries/ExceptionHandlerTester.php
@@ -40,7 +40,7 @@ class ExceptionHandlerTester extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(FileUtils::createDir(FileUtils::joinPaths($tmp_dir, 'site_errors')));
         Logger::setLogPath($tmp_dir);
         $default_zone = "America/New_York";
-        $timezone = file_get_contents("/etc/timezone");
+        $timezone = trim(file_get_contents("/etc/timezone"));
         if (!strlen($timezone)) {
             $timezone = $default_zone;
         }
