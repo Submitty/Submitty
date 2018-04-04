2998b2781fd9c6a05772751a6fd44b7988bd68dd
Victor Nazzaro <victor741naruto@gmail.com>
2018-04-04 09:56:35 -0400

    Makes code cleaner

--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -377,11 +377,7 @@ HTML;
 		if($this->core->getQueries()->isStaffPost($post["author_user_id"])){
 			$classes .= " important";
 		}
-		if($reply_level>=7){
-			$offset = 180; //To make sure that replies doesn't get too messy
-		} else {
-			$offset = ($reply_level-1)*30;
-		}
+		$offset = min(($reply_level - 1) * 30, 180);
 		
 							$return = <<<HTML
 								<div class="$classes" id="$post_id" style="margin-left:{$offset}px;" reply-level="$reply_level">
