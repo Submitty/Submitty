a8d3c3afb6dfdd104f2abda1d70c014de7acf8d2
Matthew Peveler <matt.peveler@gmail.com>
2018-04-04 22:12:20 -0400

    Improve site_watcher script

--- .setup/bin/site_watcher.py
+++ .setup/bin/site_watcher.py
@@ -15,6 +15,8 @@ INSTALL_PATH = os.path.realpath(os.path.join(CURRENT_PATH, '..', '..', '..', 'si
 
 HWPHP_UID = pwd.getpwnam('hwphp').pw_uid
 HWPHP_GID = grp.getgrnam('hwphp').gr_gid
+HWCGI_UID = pwd.getpwnam('hwcgi').pw_uid
+HWCGI_GID = grp.getgrnam('hwcgi').gr_gid
 
 
 def set_permissions(path):
@@ -22,7 +24,16 @@ def set_permissions(path):
     if os.path.isdir(path):
         os.chmod(path, 0o551)
     else:
-        os.chmod(path, 0o440)
+        if path.endswith('.php'):
+            os.chmod(path, 0o440)
+        elif path.endswith('.js'):
+            os.chmod(path, 0o445)
+        elif path.endswith('.cgi'):
+            os.chown(path, HWCGI_UID, HWCGI_GID)
+            os.chmod(path, 0o540)
+        else:
+            os.chmod(path, 0o444)
+
 
 
 class FileHandler(FileSystemEventHandler):
@@ -32,17 +43,36 @@ class FileHandler(FileSystemEventHandler):
         self.site_path = site_path
 
     def on_created(self, event):
-        if event.src_path.endswith('.swp'):
+        if event.src_path.endswith('.swp') or event.src_path == '.DS_Store':
             return
         print(event)
+        install_path = event.src_path.replace(self.site_path, self.install_path)
+        install_dir = install_path if event.is_directory else os.path.dirname(install_path)
+        folders = []
+        full_path = '/'
+        for path in install_dir.split('/'):
+            os.path.join(full_path, path)
+            if not os.path.isdir(full_path):
+                folders.append(full_path)
+        os.makedirs(install_dir, exist_ok=True)
+        if not event.is_directory:
+            shutil.copy2(event.src_path, install_path)
+        for folder in folders:
+            set_permissions(folder)
+        set_permissions(install_path)
 
     def on_deleted(self, event):
-        if event.src_path.endswith('.swp'):
+        if event.src_path.endswith('.swp') or event.src_path == '.DS_Store':
             return
         print(event)
+        install_path = event.src_path.replace(self.site_path, self.install_path)
+        if event.is_directory:
+            shutil.rmtree(install_path, ignore_errors=True)
+        else:
+            os.unlink(install_path)
 
     def on_modified(self, event):
-        if event.src_path.endswith('.swp') or event.is_directory:
+        if event.src_path.endswith('.swp') or event.is_directory or event.src_path == '.DS_Store':
             return
 
         # Only do stuff for files that aren't the tmp ones created by filesystem
@@ -52,7 +82,7 @@ class FileHandler(FileSystemEventHandler):
         set_permissions(dst)
 
     def on_moved(self, event):
-        if event.src_path.endswith('.swp'):
+        if event.src_path.endswith('.swp') or event.src_path == '.DS_Store':
             return
         print(event)
 
