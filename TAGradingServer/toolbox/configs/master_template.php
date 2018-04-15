46ef0c1621c1016a2f979af2c94500386647d5e3
Rylan <rylanoc@gmail.com>
2018-04-15 13:03:38 -0400

    Remove unnecessary references to grace period

--- TAGradingServer/toolbox/configs/master_template.php
+++ TAGradingServer/toolbox/configs/master_template.php
@@ -5,7 +5,6 @@ define("__BASE_URL__", "__INSTALL__FILLIN__TAGRADING_URL__");
 define("__DATABASE_HOST__", "__INSTALL__FILLIN__DATABASE_HOST__");
 define("__DATABASE_USER__", "__INSTALL__FILLIN__DATABASE_USER__");
 define("__DATABASE_PASSWORD__", "__INSTALL__FILLIN__DATABASE_PASSWORD__");
-define("__SUBMISSION_GRACE_PERIOD_SECONDS__", 5 * 60);
 define("__OUTPUT_MAX_LENGTH__", 100000);
 define("__DEBUG__", false);
 define("__LOG_EXCEPTIONS__", true);
