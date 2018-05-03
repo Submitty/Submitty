7c0ff9fb07c06ceba24600e58691f1f9a83a03a1
Matthew Peveler <matt.peveler@gmail.com>
2018-05-03 01:55:12 -0400

    play around more with build stage aliases

--- .setup/travis/before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# this
-BUILD_STAGE=$(echo ${TRAVIS_BUILD_STAGE_NAME} | tr '[:upper:]' '[:lower:]')
-echo "before install"
--- .setup/travis/script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo ${TRAVIS_PHP_VERSION}
-echo ${TRAVIS_BUILD_STAGE_NAME}
-echo "script"
