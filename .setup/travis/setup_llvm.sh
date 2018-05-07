c5db588c9609c0fc2a3de9e8654f676ce11f4bcd
Matthew Peveler <matt.peveler@gmail.com>
2018-05-07 19:51:53 -0400

    try, try again

--- .setup/travis/setup_llvm.sh
+++ .setup/travis/setup_llvm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -sSL "http://apt.llvm.org/llvm-snapshot.gpg.key" | sudo -E apt-key add -
-echo "deb http://apt.llvm.org/precise/ llvm-toolchain-precise-${CLANG_VER} main" | sudo tee -a /etc/apt/sources.list > /dev/null
+echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-${CLANG_VER} main" | sudo tee -a /etc/apt/sources.list > /dev/null
 sudo -E apt-get -yq update &>> ~/apt-get-update.log
