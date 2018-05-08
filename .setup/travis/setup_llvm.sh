0ebf0100b0d1aa822bd702c55bd47dfc87211dbd
Matthew Peveler <matt.peveler@gmail.com>
2018-05-07 22:53:14 -0400

    what is going on?

--- .setup/travis/setup_llvm.sh
+++ .setup/travis/setup_llvm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -sSL "http://apt.llvm.org/llvm-snapshot.gpg.key" | sudo -E apt-key add -
-echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-${CLANG_VER} main" | sudo tee -a /etc/apt/sources.list > /dev/null
-sudo -E apt-get -yq update &>> ~/apt-get-update.log
+if [ ${CLANG_VER:0:2} != "6.0" ]; then
+    curl -sSL "http://apt.llvm.org/llvm-snapshot.gpg.key" | sudo -E apt-key add -
+    echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-${CLANG_VER} main" | sudo tee -a /etc/apt/sources.list > /dev/null
+    sudo -E apt-get -yq update &>> ~/apt-get-update.log
+fi
