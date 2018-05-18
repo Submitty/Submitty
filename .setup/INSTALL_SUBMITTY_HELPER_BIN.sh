cceebc89a4bfec5c8852e75b9bac690a7ed711c0
Matthew Peveler <matt.peveler@gmail.com>
2018-05-18 16:08:25 -0400

    Move some scripts from bin to sbin

--- .setup/INSTALL_SUBMITTY_HELPER_BIN.sh
+++ .setup/INSTALL_SUBMITTY_HELPER_BIN.sh
@@ -4,7 +4,7 @@
 ########################################################################################################################
 # COPY VARIOUS SCRIPTS USED BY INSTRUCTORS AND SYS ADMINS FOR COURSE ADMINISTRATION
 
-echo -e "Copy the non-root scripts"
+echo -e "Copy the user scripts"
 
 if [ -z ${SUBMITTY_INSTALL_DIR+x} ]; then
     # constants are not initialized,
@@ -20,16 +20,8 @@ mkdir -p ${SUBMITTY_INSTALL_DIR}/bin
 chown root:${COURSE_BUILDERS_GROUP} ${SUBMITTY_INSTALL_DIR}/bin
 chmod 755 ${SUBMITTY_INSTALL_DIR}/bin
 
-mkdir -p ${SUBMITTY_INSTALL_DIR}/bin/autograder
-chown root:${HWCRON_USER} ${SUBMITTY_INSTALL_DIR}/bin/autograder
-chmod 755 ${SUBMITTY_INSTALL_DIR}/bin/autograder
-
 # copy all of the files
 rsync -rtz  ${SUBMITTY_REPOSITORY}/bin/*   ${SUBMITTY_INSTALL_DIR}/bin/
-
-# www-data needs to have access to this so that it can authenticate for git
-chown root:www-data ${SUBMITTY_INSTALL_DIR}/bin/authentication.py
-chmod 550 ${SUBMITTY_INSTALL_DIR}/bin/authentication.py
 
 # all course builders (instructors & head TAs) need read/execute access to these scripts
 array=( grading_done.py left_right_parse.py read_iclicker_ids.py regrade.py )
@@ -38,15 +30,39 @@ for i in "${array[@]}"; do
     chmod 550 ${SUBMITTY_INSTALL_DIR}/bin/${i}
 done
 
-# everyone needs to run this script
-chmod 555 ${SUBMITTY_INSTALL_DIR}/bin/killall.py
-
 # course builders & hwcron need access to these scripts
 array=( build_homework_function.sh make_assignments_txt_file.py )
 for i in "${array[@]}"; do
     chown ${HWCRON_USER}:${COURSE_BUILDERS_GROUP} ${SUBMITTY_INSTALL_DIR}/bin/${i}
     chmod 550 ${SUBMITTY_INSTALL_DIR}/bin/${i}
 done
+
+#####################################
+
+echo -e "Copy the non-user scripts"
+
+# make the directory (has a different name)
+mkdir -p ${SUBMITTY_INSTALL_DIR}/sbin
+chown root:root ${SUBMITTY_INSTALL_DIR}/sbin
+
+mkdir -p ${SUBMITTY_INSTALL_DIR}/bin/autograder
+chown root:${HWCRON_USER} ${SUBMITTY_INSTALL_DIR}/bin/autograder
+
+chmod -R 755 ${SUBMITTY_INSTALL_DIR}/sbin
+
+# copy all of the files
+rsync -rtz  ${SUBMITTY_REPOSITORY}/sbin/*   ${SUBMITTY_INSTALL_DIR}/sbin/
+
+# most of the scripts should be root only
+find ${SUBMITTY_INSTALL_DIR}/sbin -type f -exec chown root:root {} \;
+find ${SUBMITTY_INSTALL_DIR}/sbin -type f -exec chmod 500 {} \;
+
+# www-data needs to have access to this so that it can authenticate for git
+chown root:www-data ${SUBMITTY_INSTALL_DIR}/bin/authentication.py
+chmod 550 ${SUBMITTY_INSTALL_DIR}/bin/authentication.py
+
+# everyone needs to be able to run this script
+chmod 555 ${SUBMITTY_INSTALL_DIR}/sbin/killall.py
 
 # hwcron only things
 array=( build_config_upload.py submitty_autograding_shipper.py submitty_autograding_worker.py )
@@ -57,19 +73,3 @@ done
 
 chown root:${HWCRON_USER} ${SUBMITTY_INSTALL_DIR}/bin/autograder/*
 chmod 550 ${SUBMITTY_INSTALL_DIR}/bin/autograder/*
-
-#####################################
-
-echo -e "Copy the root scripts"
-
-# make the directory (has a different name)
-mkdir -p ${SUBMITTY_INSTALL_DIR}/sbin
-chown root:root ${SUBMITTY_INSTALL_DIR}/sbin
-chmod 755 ${SUBMITTY_INSTALL_DIR}/sbin
-
-# copy all of the files
-rsync -rtz  ${SUBMITTY_REPOSITORY}/sbin/*   ${SUBMITTY_INSTALL_DIR}/sbin/
-
-# the scripts should be root only
-find ${SUBMITTY_INSTALL_DIR}/sbin -type f -exec chown root:root {} \;
-find ${SUBMITTY_INSTALL_DIR}/sbin -type f -exec chmod 500 {} \;
