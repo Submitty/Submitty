5c2dc629e402481dc1905dd46f5746d397babfca
Glenn Smith <couleeapps@gmail.com>
2018-05-23 15:55:46 -0400

    Student list twig

--- site/app/views/admin/UsersView.php
+++ site/app/views/admin/UsersView.php
@@ -11,92 +11,16 @@ class UsersView extends AbstractView {
      * @return string
      */
     public function listStudents($students) {
-        $section = -1;
-        $return = <<<HTML
-<div class="content">
-    <div style="float: right; margin-bottom: 20px;">
-        <a onclick="newDownloadForm()" class="btn btn-primary">Download Users</a>
-        <a onclick="newClassListForm()" class="btn btn-primary">Upload Classlist</a>
-        <a onclick="newUserForm()" class="btn btn-primary">New Student</a>
-    </div>
-    <h2>View Students</h2>
-    <table class="table table-striped table-bordered persist-area">
-        <thead class="persist-thead">
-            <tr>
-                <td width="4%"></td>
-                <td width="15%">Registration Section</td>
-                <td width="20%" style="text-align: left">User ID</td>
-                <td width="30%" colspan="2">Name</td>
-                <td width="15%">Rotating Section</td>
-                <td width="13%">Manual Registration</td>
-                <td width="3%"></td>
-            </tr>
-        </thead>
-HTML;
-        if (count($students) > 0) {
-            $count = 1;
-            $tbody_open = false;
-            foreach ($students as $student) {
-                $registration = ($student->getRegistrationSection() === null) ? "NULL" : $student->getRegistrationSection();
-                if($section !== $student->getRegistrationSection()) {
-                    $count = 1;
-                    if ($tbody_open) {
-                        $return .= <<<HTML
-
-        </tbody>
-HTML;
-                    }
-                    $return .= <<<HTML
-
-        <tr class="info persist-header">
-            <td colspan="8" style="text-align: center">Students Enrolled in Registration Section {$registration}</td>
-        </tr>
-        <tbody id="section-{$registration}">
-HTML;
-                    $tbody_open = true;
-                    $section = $student->getRegistrationSection();
-                }
-                $manual = ($student->isManualRegistration()) ? "TRUE" : "FALSE";
-                $rotating_section = ($student->getRotatingSection() === null) ? "NULL" : $student->getRotatingSection();
-                $style = "";
-                if ($student->accessGrading()) {
-                    $style = "style='background: #7bd0f7;'";
-                }
-                $return .= <<<HTML
-
-            <tr id="user-{$student->getId()}" {$style}>
-                <td>{$count}</td>
-                <td>{$registration}</td>
-                <td style="text-align: left">{$student->getId()}</td>
-                <td style="text-align: left">{$student->getDisplayedFirstName()}</td>
-                <td style="text-align: left">{$student->getLastName()}</td>
-                <td>{$rotating_section}</td>
-                <td>{$manual}</td>
-                <td><a onclick="editUserForm('{$student->getId()}');"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
-            </tr>
-HTML;
-                $count++;
-            }
-            $return .= <<<HTML
-
-        </tbody>
-HTML;
-        }
-        else {
-            $return .= <<<HTML
-        <tr>
-            <td colspan="8">No students found</td>
-        </tr>
-HTML;
+        //Assemble students into sections
+        $sections = [];
+        foreach ($students as $student) {
+            $registration = ($student->getRegistrationSection() === null) ? "NULL" : $student->getRegistrationSection();
+            $sections[$registration][] = $student;
         }
 
-        $return .= <<<HTML
-
-    </table>
-</div>
-HTML;
-
-        return $return;
+        return $this->core->getOutput()->renderTwigTemplate("admin/users/StudentList.twig", [
+            "sections" => $sections
+        ]);
     }
 
     /**
