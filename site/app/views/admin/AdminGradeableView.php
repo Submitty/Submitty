6a00b8ca6ee4395ce314d3a09eab3713516eba5c
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-05-24 09:37:32 -0400

    Simplified action conditionals and php variables

--- site/app/views/admin/AdminGradeableView.php
+++ site/app/views/admin/AdminGradeableView.php
@@ -11,31 +11,31 @@ class AdminGradeableView extends AbstractView {
      */
 	public function show_add_gradeable($type_of_action, AdminGradeable $admin_gradeable) {
 
-        $action           = "upload_new_gradeable"; //decides how the page's data is displayed
+        $action           = "add"; //decides how the page's data is displayed
         $button_string    = "Add";
-        $label_message    = "TEST";
-        $have_old         = false;
-        $edit             = json_encode($type_of_action === "edit");
+        $submit_text      = "Submit";
+        $label_message    = "";
         $gradeables_array = array();
 
         foreach ($admin_gradeable->getTemplateList() as $g_id_title) { //makes an array of gradeable ids for javascript
             array_push($gradeables_array, $g_id_title['g_id']);
         }
-        $js_gradeables_array = json_encode($gradeables_array);
 
         // //if the user is editing a gradeable instead of adding
         if ($type_of_action === "edit") {
-            $have_old      = true;
-            $action        = "upload_edit_gradeable";
+            $action        = "edit";
             $button_string = "Save changes to";
+            $submit_text   = "Save Changes";
             $label_message = ($admin_gradeable->getHasGrades()) ? "<span style='color: red;'>(Grading has started! Edit Questions At Own Peril!)</span>" : "";
         }
 
         return $this->core->getOutput()->renderTwigTemplate('admin/admin_gradeable/AdminGradeableBase.twig', [
+            "submit_url"      => $this->core->buildUrl(array('component' => 'admin', 'page' => 'admin_gradeable', 'action' => $action)),
+            "js_gradeables_array"=> json_encode($gradeables_array),
             "admin_gradeable" => $admin_gradeable,
             "label_message"   => $label_message,
-            "type_of_action"  => $type_of_action,
-            "mode"            => "create",
+            "action"          => $action,
+            "submit_text"     => $submit_text,
             "nav_tab"         => "0",    // TODO: allow the request of a particular tab (so save-and-continue on first page doesn't jump back to first page)
         ]);
     }
