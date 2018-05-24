2a93202088a8e7b728afa555f2d4c2ac1725f911
Glenn Smith <couleeapps@gmail.com>
2018-05-24 16:46:25 -0400

    Original names for these

--- site/app/views/admin/ExtensionsView.php
+++ site/app/views/admin/ExtensionsView.php
@@ -5,7 +5,7 @@ namespace app\views\admin;
 use app\views\AbstractView;
 
 class ExtensionsView extends AbstractView {
-    public function displayExtensions($gradeables) {
+    public function displayExtensions($gradeable_ids) {
         $students = $this->core->getQueries()->getAllUsers();
         $student_full = array();
         foreach ($students as $student) {
@@ -15,7 +15,7 @@ class ExtensionsView extends AbstractView {
         $student_full = json_encode($student_full);
 
         return $this->core->getOutput()->renderTwigTemplate("admin/Extensions.twig", [
-            "gradeables" => $gradeables,
+            "gradeable_ids" => $gradeable_ids,
             "student_full" => $student_full
         ]);
     }
