d5bfdcae501fb8930df448c4778a4a515ea8cd0e
Glenn Smith <couleeapps@gmail.com>
2018-05-25 11:18:53 -0400

    Organize

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -191,11 +191,11 @@ HTML;
                 Section {$key}: {$graders}<br />
 HTML;
                 }
-                $viewed_percent = number_format(($viewed_grade / $total_students) * 100, 1);
                 $return .= <<<HTML
             </div>
 HTML;
                 if ($gradeable->taGradesReleased()) {
+                    $viewed_percent = number_format(($viewed_grade / $total_students) * 100, 1);
                     $return .= <<<HTML
             <br />
             Number of students who have viewed their grade: {$viewed_grade} / {$total_students} ({$viewed_percent}%)
