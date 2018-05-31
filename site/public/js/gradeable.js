3d26dba21ac79b99f599b3acce00db2f9d6aceb2
Glenn Smith <couleeapps@gmail.com>
2018-05-31 15:56:09 -0400

    Twig comments!

--- site/public/js/gradeable.js
+++ site/public/js/gradeable.js
@@ -1,4 +1,8 @@
-
+/**
+ * Asynchronously render a gradeable using the passed data
+ * @param grading_data Gradeable data structure (see Gradeable.php/getGradedData())
+ * @returns {Promise} Will fire when the template is loaded and rendered
+ */
 function renderGradeable(grading_data) {
     return new Promise(function(accept, reject) {
         Twig.twig({
