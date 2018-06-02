170337bedec5ffca62cb8ccfe368b2d25d49c0db
Peter Bailie <pbailie@users.noreply.github.com>
2018-06-01 21:37:29 -0400

    Sysadmin tools: bugfixes & remove support for SVN accounts (#2079)
    * Update
    
    * Remove SVN accounts
    
    Advised by @bmcutler that SVN is no longer used at RPI.
    
    * bugfix
    
    Last minute bugfix

--- sysadmin_tools/student_auto_feed/accounts.php
+++ sysadmin_tools/student_auto_feed/accounts.php
@@ -29,27 +29,18 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
-//List of courses that also need SVN accounts as serialized array.
-//Serializing the array allows it to be defined as a constant.
-//NOTE: If there are no courses using SVN, the serialized array must still be
-//      defined, but make it an empty array.
-define('SVN_LIST', serialize( array (
-'cs1000',
-'cs2000',
-'cs3000',
-'cs4000',
-)));
-
 //Database access
 define('DB_LOGIN',  'hsdbu');
 define('DB_PASSWD', 'hsdbu_pa55w0rd');
 define('DB_HOST',   'localhost');
+define('DB_NAME',   'submitty');
 
 //Location of accounts creation error log file
 define('ERROR_LOG_FILE', '/var/local/submitty/bin/accounts_errors.log');
 
 //Where to email error messages so they can get more immediate attention.
-define('ERROR_E_MAIL', 'sysadmins@lists.myuniversity.edu');
+//Set to null to not send email.
+define('ERROR_EMAIL', 'sysadmins@lists.myuniversity.edu');
 
 /* SUGGESTED SETTINGS FOR TIMEZONES IN USA -------------------------------------
  *
@@ -83,85 +74,41 @@ function main() {
 	//Check for semester among CLI arguments.
 	$semester = cli_args::parse_args();
 	if ($semester === false) {
-	    exit(1);
+		exit(1);
 	}
 
-	$courses = determine_courses($semester);
-
-	foreach($courses as $course) {
-		if (array_search($course, unserialize(SVN_LIST)) !== false) {
-			//Create both auth account and SVN account
-			//First make sure SVN repo exists
-			if (!file_exists("/var/lib/svn/{$course}")) {
-				mkdir("/var/lib/svn/{$course}");
-			}
-			$user_list = get_user_list_from_course_db($semester, $course);
-			foreach($user_list as $user) {
-				//Let's make sure SVN account doesn't already exist before making it.
-				if (!file_exists("/var/lib/svn/{$course}/{$user}")) {
-					system ("/usr/sbin/adduser --quiet --home /tmp --gecos 'RCS auth account' --no-create-home --disabled-password --shell /usr/sbin/nologin {$user} > /dev/null 2>&1");
-					system ("svnadmin create /var/lib/svn/{$course}/{$user}");
-					system ("touch /var/lib/svn/{$course}/{$user}/db/rep-cache.db");
-					system ("chmod g+w /var/lib/svn/{$course}/{$user}/db/rep-cache.db");
-					system ("chmod 2770 /var/lib/svn/{$course}/{$user}");
-					system ("chown -R www-data:svn-{$course} /var/lib/svn/{$course}/{$user}");
-					system ("ln -s /var/lib/svn/hooks/pre-commit /var/lib/svn/{$course}/{$user}/hooks/pre-commit");
-				}
-			}
-			//Restart Apache
-			system ("/root/bin/regen.apache > /dev/null 2>&1");
-			system ("/usr/sbin/apache2ctl -t > /dev/null 2>&1");
-		} else {
-			//Only create auth account
-			$user_list = get_user_list_from_course_db($semester, $course);
-			foreach($user_list as $user) {
-				//We don't care if user already exists as adduser will skip over any account that already exists.
-				system ("/usr/sbin/adduser --quiet --home /tmp --gecos 'RCS auth account' --no-create-home --disabled-password --shell /usr/sbin/nologin {$user} > /dev/null 2>&1");
-			}
+	$user_list = get_user_list($semester);
+	if ($user_list !== false) {
+		foreach($user_list as $user) {
+			//We don't care if user already exists as adduser will skip over any account that already exists.
+			system ("/usr/sbin/adduser --quiet --home /tmp --gecos 'RCS auth account' --no-create-home --disabled-password --shell /usr/sbin/nologin {$user} > /dev/null 2>&1");
 		}
 	}
 }
 
 /**
- * Retrieve list of active courses
+ * Retrieve user list from courses_users
  *
  * @param string $semester
- * @return array
+ * @return array of user ids on success, boolean false on failure.
  */
- function determine_courses($semester) {
- 	//Retrieve course list from file system (each course has its own folder).
-	$path = "/var/local/submitty/courses/{$semester}/";
-	$courses = scandir($path);
-	if ($courses === false) {
-		log_it("Submitty Auto Account Creation: Cannot parse {$path}, CANNOT MAKE ACCOUNTS");
-		exit(1);
-	}
-	//remove ".", "..", and hidden file entries from courses list and return.
-	return array_filter($courses, function($elem) {return preg_match("~^[^\.]+~", $elem);});
-}
-
-/**
- * Retrieve user list from a course database
- *
- * @param string $semester
- * @param string $course
- * @return array
- */
-function get_user_list_from_course_db($semester, $course) {
+function get_user_list($semester) {
 	$db_user = DB_LOGIN;
 	$db_pass = DB_PASSWD;
 	$db_host = DB_HOST;
-	$db_name = "submitty_{$semester}_{$course}";
+	$db_name = DB_NAME;
 	$db_conn = pg_connect("host={$db_host} dbname={$db_name} user={$db_user} password={$db_pass}");
 	if ($db_conn === false) {
-		log_it("Submitty Auto Account Creation: Cannot connect to DB {$db_name}, skipping...");
-		return array();
+		log_it("Submitty Auto Account Creation: Cannot connect to DB {$db_name}.");
+		return false;
 	}
-	$db_query = pg_query($db_conn, "SELECT user_id FROM users;");
+
+	$db_query = pg_query_params($db_conn, "SELECT DISTINCT user_id FROM courses_users WHERE semester=$1;", array($semester));
 	if ($db_query === false) {
-        log_it("Submitty Auto Account Creation: Cannot read user list for {$course}, skipping...");
-		return array();
+		log_it("Submitty Auto Account Creation: Cannot read user list from {$db_name}.");
+		return false;
 	}
+
 	$user_list = pg_fetch_all_columns($db_query, 0);
 	pg_close($db_conn);
 	return $user_list;
@@ -174,8 +121,11 @@ function get_user_list_from_course_db($semester, $course) {
  */
 function log_it($msg) {
     $msg = date('m/d/y H:i:s : ', time()) . $msg . PHP_EOL;
-    error_log($msg, 1, ERROR_E_MAIL);
    	error_log($msg, 3, ERROR_LOG_FILE);
+
+   	if (!is_null(ERROR_EMAIL)) {
+	    error_log($msg, 1, ERROR_EMAIL);
+	}
 }
 
 /** @static class to parse command line arguments */
