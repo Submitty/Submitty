6f9e084c7c2317ef89f6e8d0345e8125355a21be
Matthew Peveler <matt.peveler@gmail.com>
2018-06-02 17:10:25 -0400

    Fix up various bugs with migrator

--- migration/migrator.py
+++ migration/migrator.py
@@ -16,28 +16,31 @@ import sys
 import psycopg2
 
 
-DIR_PATH = Path(__file__).resolve()
-MIGRATIONS_PATH = DIR_PATH.parent / 'migrations'
+DIR_PATH = Path(__file__).parent.resolve()
+MIGRATIONS_PATH = DIR_PATH / 'migrations'
 ENVIRONMENTS = [path.name for path in MIGRATIONS_PATH.iterdir()]
 
 
 def parse_args():
     parser = ArgumentParser(description='Migration script for upgrading/downgrading the database')
-    parser.add_argument('--environment', '-e', dest='environments', choices=ENVIRONMENTS,
-                        action='append', default=ENVIRONMENTS)
+    parser.add_argument('--environment', '-e', dest='environments', choices=ENVIRONMENTS, action='append')
     config_path = Path(DIR_PATH, '..', '..', 'config')
     default_config = config_path.resolve() if config_path.exists() else None
-    parser.add_argument('--config', '-c', dest='config_path', type=str, default=default_config)
+    required = False if default_config is not None else True
+    parser.add_argument('--config', '-c', dest='config_path', type=str, required=required, default=default_config)
     subparsers = parser.add_subparsers(metavar='command', dest='command')
     sub = subparsers.add_parser('create', help='Create migration')
     sub.add_argument('name', help='Name of argument')
     sub = subparsers.add_parser('migrate', help='Run migrations')
-    sub.add_argument('--fake', action='store_true', default=False,
+    sub.add_argument('--fake', action='store_true', default=False, dest='set_fake',
                      help='Mark migrations as run without actually running them')
     sub.add_argument('--initial', action='store_true', default=False,
                      help='Only run the first migration and then mark the rest as done without running them.')
     subparsers.add_parser('rollback', help='Rollback the previously done migration')
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.environments is None:
+        args.environments = ENVIRONMENTS
+    return args
 
 
 def main():
@@ -86,7 +89,6 @@ def handle_migration(args):
                 'password': database['database_password']
             }
 
-            args.use_environment = args.environment
             with psycopg2.connect(**params) as connection:
                 migrate_environment(connection, environment, args)
 
@@ -96,7 +98,7 @@ def handle_migration(args):
                 'user': database['database_user'],
                 'password': database['database_password']
             }
-            args.use_environment = 'course'
+
             for semester in os.listdir(os.path.join(config['submitty_data_dir'], 'courses')):
                 for course in os.listdir(os.path.join(config['submitty_data_dir'], 'courses', semester)):
                     params['dbname'] = 'submitty_{}_{}'.format(semester, course)
@@ -113,11 +115,10 @@ def migrate_environment(connection, environment, args):
 
     if not exists:
         with connection.cursor() as cursor:
-            cursor.execute("""
-    CREATE TABLE migrations_{}} (
+            cursor.execute("""CREATE TABLE migrations_{} (
       id VARCHAR(100) PRIMARY KEY NOT NULL,
       commit_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      status NUMERIC(1) DEFAULT 0 NOT NULL,
+      status NUMERIC(1) DEFAULT 0 NOT NULL
     );""".format(environment))
         connection.commit()
 
@@ -125,8 +126,10 @@ def migrate_environment(connection, environment, args):
     migrations = load_migrations(MIGRATIONS_PATH / environment)
 
     with connection.cursor() as cursor:
-        cursor.execute('SELECT migration, commit_time, status FROM migrations_{} ORDER BY id'.format(environment))
+        cursor.execute('SELECT id, commit_time, status FROM migrations_{} ORDER BY id'.format(environment))
         for migration in cursor.fetchall():
+            # fetchall returns things as a tuple which is a bit unwieldy
+            migration = {'id': migration[0], 'commit_time': migration[1], 'status': migration[2]}
             if migration['id'] in migrations:
                 migrations[migration['id']].update({
                     'commit_time': migration['commit_time'],
@@ -137,16 +140,17 @@ def migrate_environment(connection, environment, args):
                 missing_migrations[migration['id']] = migration
 
     if len(missing_migrations) > 0:
-        print('Removing {} missing migrations...'.format(len(missing_migrations)))
+        print('Removing {} missing migrations:'.format(len(missing_migrations)))
         for migration in missing_migrations:
             remove_migration(connection, migration, environment, args)
 
+    print("Running {} migrations for {}:".format(args.direction, environment))
+    args.fake = args.set_fake
     if args.direction == 'up':
         keys = list(migrations.keys())
         if args.initial is True:
             key = keys.pop(0)
             run_migration(connection, migrations[key], environment, args)
-            # we don't want to actually run any of the other migrations here
             args.fake = True
         for key in keys:
             if migrations[key]['status'] == 0:
@@ -159,6 +163,7 @@ def migrate_environment(connection, environment, args):
 
 
 def remove_migration(connection, migration, environment, args):
+    print("  {}".format(migration['id']))
     file_name = migration['id'] + '_' + migration['name'] + '.py'
     file_path = Path(args.install_dir, 'migrations', environment, file_name)
     if file_path.exists():
@@ -181,19 +186,20 @@ def noop(_):
 
 
 def run_migration(connection, migration, environment, args):
+    print("  {}".format(migration['id']))
     if not args.fake:
         call_func(getattr(migration['module'], args.direction, noop), connection, environment)
-    status = 1 if migration['status'] == 0 else 0
+    status = 1 if args.direction == 'up' else 0
     with connection.cursor() as cursor:
         if migration['db']:
-            cursor.execute('UPDATE migrations_{} SET commit_time=CURRENT_TIMESTAMP, status=%d WHERE id=%s'.format(environment), (status, migration['id'],))
+            cursor.execute('UPDATE migrations_{} SET commit_time=CURRENT_TIMESTAMP, status=%s WHERE id=%s'.format(environment), (status, migration['id'],))
         else:
-            cursor.execute('INSERT INTO migrations_{} (id, status) VALUES(%s, %d)', (migration['id'], status,))
+            cursor.execute('INSERT INTO migrations_{} (id, status) VALUES(%s, %s)'.format(environment), (migration['id'], status,))
     connection.commit()
 
 
 def load_migration_module(name, path):
-    spec = importlib.util.spec_from_file_location('module.{}'.format(name), path)
+    spec = importlib.util.spec_from_file_location('module.{}'.format(name), str(path))
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
