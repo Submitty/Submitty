f41cb73840668b9ee07298cb3d48ca6e2c3bd606
Glenn Smith <couleeapps@gmail.com>
2018-06-05 11:31:19 -0400

    LocalStorage saving

--- site/public/js/ta-grading-keymap.js
+++ site/public/js/ta-grading-keymap.js
@@ -7,26 +7,6 @@ var remapping = {
     index: 0
 };
 
-function eventToKeyCode(e) {
-    var codeName = e.code;
-
-    //Apply modifiers to code name in reverse alphabetical order so they come out alphabetical
-    if (e.shiftKey && (e.code !== "ShiftLeft" && e.code !== "ShiftRight")) {
-        codeName = "Shift " + codeName;
-    }
-    if (e.metaKey && (e.code !== "MetaLeft" && e.code !== "MetaRight")) {
-        codeName = "Meta " + codeName;
-    }
-    if (e.ctrlKey && (e.code !== "ControlLeft" && e.code !== "ControlRight")) {
-        codeName = "Control " + codeName;
-    }
-    if (e.altKey && (e.code !== "AltLeft" && e.code !== "AltRight")) {
-        codeName = "Alt " + codeName;
-    }
-
-    return codeName;
-}
-
 window.onkeyup = function(e) {
     if (remapping.active) {
         remapFinish(e);
@@ -58,11 +38,18 @@ window.onkeydown = function(e) {
  * @param {Function} fn Function / callable
  */
 function registerKeyHandler(name, code, fn) {
+    var originalCode = code;
+
+    //Check local storage
+    if (remapGetLS(name) !== null) {
+        code = remapGetLS(name);
+    }
+
     keymap.push({
         name: name,
         code: code,
         fn: fn,
-        originalCode: code
+        originalCode: originalCode
     });
 }
 
@@ -100,6 +87,9 @@ Twig.twig({
     async: true
 });
 
+/**
+ * Generate list of hotkeys on the ui
+ */
 function generateHotkeysList() {
     var parent = $("#hotkeys-list");
 
@@ -110,6 +100,10 @@ function generateHotkeysList() {
     }));
 }
 
+/**
+ * Start rebinding a hotkey
+ * @param {int} i Index of hotkey to rebind
+ */
 function remapHotkey(i) {
     if (remapping.active) {
         return;
@@ -126,15 +120,92 @@ function remapHotkey(i) {
     button.addClass("btn-success");
 }
 
+/**
+ * Called when remapping has finished and should save (or discard) a pressed key
+ * @param {KeyboardEvent} event Event from onkeydown
+ */
 function remapFinish(event) {
     if (event.code === "Escape") {
         keymap[remapping.index].code = keymap[remapping.index].originalCode;
     } else {
         keymap[remapping.index].code = eventToKeyCode(event);
     }
+
+    remapSetLS(keymap[remapping.index].name, eventToKeyCode(event));
+
     remapping.active = false;
     generateHotkeysList();
 
     $(".remap-button").attr("disabled", null);
     $("#settings-close").attr("disabled", null);
+}
+
+/**
+ * Get the keycode for a hotkey binding from localStorage
+ * @param {string} mapName Name of hotkey binding
+ * @returns {string|null} Code value if exists, else null
+ */
+function remapGetLS(mapName) {
+    var lsKeymap = localStorage.getItem("keymap");
+    if (lsKeymap === null) {
+        return null;
+    }
+    try {
+        lsKeymap = JSON.parse(lsKeymap);
+    } catch (e) {
+        return null;
+    }
+    if (!lsKeymap.hasOwnProperty(mapName)) {
+        return null;
+    }
+    return lsKeymap[mapName].code;
+}
+
+/**
+ * Save a hotkey binding to localStorage
+ * @param {string} mapName Name of hotkey binding
+ * @param {string} code New binding code
+ */
+function remapSetLS(mapName, code) {
+    var lsKeymap = localStorage.getItem("keymap");
+    if (lsKeymap === null) {
+        lsKeymap = {};
+    } else {
+        try {
+            lsKeymap = JSON.parse(lsKeymap);
+        } catch (e) {
+            lsKeymap = {};
+        }
+    }
+    if (!lsKeymap.hasOwnProperty(mapName)) {
+        lsKeymap[mapName] = {};
+    }
+    lsKeymap[mapName].code = code;
+
+    localStorage.setItem("keymap", JSON.stringify(lsKeymap));
+}
+
+/**
+ * Convert a KeyboardEvent into a (generally) user-readable string
+ * @param {KeyboardEvent} e Event
+ * @returns {string} String form of the event
+ */
+function eventToKeyCode(e) {
+    var codeName = e.code;
+
+    //Apply modifiers to code name in reverse alphabetical order so they come out alphabetical
+    if (e.shiftKey && (e.code !== "ShiftLeft" && e.code !== "ShiftRight")) {
+        codeName = "Shift " + codeName;
+    }
+    if (e.metaKey && (e.code !== "MetaLeft" && e.code !== "MetaRight")) {
+        codeName = "Meta " + codeName;
+    }
+    if (e.ctrlKey && (e.code !== "ControlLeft" && e.code !== "ControlRight")) {
+        codeName = "Control " + codeName;
+    }
+    if (e.altKey && (e.code !== "AltLeft" && e.code !== "AltRight")) {
+        codeName = "Alt " + codeName;
+    }
+
+    return codeName;
 }
