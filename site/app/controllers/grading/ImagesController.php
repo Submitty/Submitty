9ec67f2857709c9ae6530711cfe28b5145d256fd
Joanna Ng <jn7070@gmail.com>
2018-06-08 14:43:07 -0400

    Changes added to Images files

---
+++ site/app/controllers/grading/ImagesController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\controllers\grading;
+
+use app\controllers\AbstractController;
+use app\libraries\FileUtils;
+
+class ImagesController extends AbstractController {
+	public function run() {
+        switch ($_REQUEST['action']) {
+            case 'view_images_page':
+            		$this->view_images_page();
+                break;
+            default:
+                $this->viewPage();
+                break;
+        }
+    }
+
+		public function view_images_page() {
+				$target_dir = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "config_upload");
+				$all_files = FileUtils::getAllFiles($target_dir);
+				$this->core->getOutput()->renderOutput(array('grading', 'Images'), 'listStudentImages', $target_dir, $all_files);
+		}
+
+		public function process_images_upload() {
+				if (!isset($_POST['csrf_token']) || !$this->core->checkCsrfToken($_POST['csrf_token'])) {
+						$this->core->addErrorMessage("Upload failed: Invalid CSRF token");
+						$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'images',
+								'action' => 'view_images_page')));
+				}
+
+				if (empty($_FILES) || !isset($_FILES['config_upload'])) {
+						$this->core->addErrorMessage("Upload failed: No file to upload");
+						$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'images',
+								'action' => 'view_images_page')));
+				}
+
+				$upload = $_FILES['config_upload'];
+				if (!isset($upload['tmp_name']) || $upload['tmp_name'] === "") {
+						$this->core->addErrorMessage("Upload failed: Empty tmp name for file");
+						$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'images',
+								'action' => 'view_images_page')));
+				}
+
+				$target_dir = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "config_upload");
+				$target_dir = FileUtils::joinPaths($target_dir, count(scandir($target_dir))-1);
+				FileUtils::createDir($target_dir);
+
+				if (FileUtils::getMimeType($upload["tmp_name"]) == "application/zip") {
+						$zip = new \ZipArchive();
+						$res = $zip->open($upload['tmp_name']);
+						if ($res === true) {
+								$zip->extractTo($target_dir);
+								$zip->close();
+						}
+						else {
+								FileUtils::recursiveRmdir($target_dir);
+								$error_message = ($res == 19) ? "Invalid or uninitialized Zip object" : $zip->getStatusString();
+								$this->core->addErrorMessage("Upload failed: {$error_message}");
+								$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'gradeable',
+										'action' => 'upload_config')));
+						}
+				}
+				else {
+						if (!@copy($upload['tmp_name'], FileUtils::joinPaths($target_dir, $upload['name']))) {
+								FileUtils::recursiveRmdir($target_dir);
+								$this->core->addErrorMessage("Upload failed: Could not copy file");
+								$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'gradeable',
+										'action' => 'upload_config')));
+						}
+				}
+				$this->core->addSuccessMessage("Gradeable config uploaded");
+				$this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'gradeable',
+						'action' => 'upload_config')));
+		}
+}
---
+++ site/app/views/grading/ImagesView.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace app\views\grading;
+
+use app\models\User;
+use app\views\AbstractView;
+
+class ImagesView extends AbstractView {
+    /**
+     * @param User[] $students
+     * @return string
+     */
+    public function listStudentImages($students) {
+        //Assemble students into sections
+        $sections = [];
+        foreach ($students as $student) {
+            $registration = ($student->getRegistrationSection() === null) ? "NULL" : $student->getRegistrationSection();
+            $sections[$registration][] = $student;
+        }
+
+        return $this->core->getOutput()->renderTwigTemplate("grading/Images.twig", [
+            "sections" => $sections
+        ]);
+    }
+
+    /**
+     * @param User[] $graders
+     * @param array  $reg_sections associative array representing registration sections in the system
+     * @param array  $rot_sections associative array representing rotating sections in the system
+     * @param bool   $use_database
+     * @return string
+     */
+    public function listGraders($graders, $reg_sections, $rot_sections, $use_database=false) {
+        return $this->core->getOutput()->renderTwigTemplate("admin/users/GraderList.twig", [
+            "graders" => $graders,
+            "groups" => [
+                0 => [
+                    "name" => "Developer",
+                    "all_sections" => true
+                ],
+                1 => [
+                    "name" => "Instructor",
+                    "all_sections" => true
+                ],
+                2 => [
+                    "name" => "Full Access Grader (Grad TA)",
+                    "all_sections" => false
+                ],
+                3 => [
+                    "name" => "Limited Access Grader (Mentor)",
+                    "all_sections" => false
+                ]
+            ],
+            "reg_sections" => $reg_sections,
+            "rot_sections" => $rot_sections,
+            "use_database" => $use_database
+        ]);
+    }
+
+    /**
+     * Creates the user form box to be displayed when creating or editing a user on the students/graders pages
+     * @param array  $reg_sections associative array representing registration sections in the system
+     * @param array  $rot_sections associative array representing rotating sections in the system
+     * @param string $action what action to go to after hitting the submit button (different for student vs grader page)
+     * @param bool   $use_database
+     * @return string
+     */
+    public function userForm($reg_sections, $rot_sections, $action, $use_database=false) {
+        return $this->core->getOutput()->renderTwigTemplate("admin/users/UserForm.twig", [
+            "reg_sections" => $reg_sections,
+            "rot_sections" => $rot_sections,
+            "action" => $action,
+            "use_database" => $use_database
+        ]);
+    }
+
+
+    /**
+     * Creates the form box to be displayed when copying or downloading emails on the students/graders pages
+     * @param string $code to specify whether it is grader tab or student tab
+     * @param User[] $students
+     * @param User[] $graders
+     * @param array  $reg_sections associative array representing registration sections in the system
+     * @param bool   $use_database
+     * @return string
+     */
+    public function downloadForm($code, $students, $graders, $reg_sections, $use_database=false) {
+        $download_info = array();
+        if ($code === 'user') {
+            foreach ($students as $student) {
+                $rot_sec = ($student->getRotatingSection() === null) ? 'NULL' : $student->getRotatingSection();
+                $reg_sec = ($student->getRegistrationSection() === null) ? 'NULL' : $student->getRegistrationSection();
+                $grp = "";
+                switch ($student->getGroup()) {
+                    case 0:
+                        $grp = 'Developer';
+                        break;
+                    case 1:
+                        $grp = 'Instructor';
+                        break;
+                    case 2:
+                        $grp = 'Full Access Grader (Grad TA)';
+                        break;
+                    case 3:
+                        $grp = 'Limited Access Grader (Mentor)';
+                        break;
+                    default:
+                        $grp = 'Student';
+                        break;
+                }
+                $first_name = str_replace("'", "&#039;", $student->getDisplayedFirstName());
+                $last_name = str_replace("'", "&#039;", $student->getLastName());
+                array_push($download_info, ['first_name' => $first_name, 'last_name' => $last_name, 'user_id' => $student->getId(), 'email' => $student->getEmail(), 'reg_section' => "$reg_sec", 'rot_section' => "$rot_sec", 'group' => "$grp"]);
+            }
+        }
+        else if ($code === 'grader') {
+            foreach ($graders as $grader) {
+                $rot_sec = ($grader->getRotatingSection() === null) ? 'NULL' : $grader->getRotatingSection();
+                switch ($grader->getGroup()) {
+                    case 0:
+                        $reg_sec = 'All';
+                        $grp = 'Developer';
+                        break;
+                    case 1:
+                        $reg_sec = 'All';
+                        $grp = 'Instructor';
+                        break;
+                    case 2:
+                        $grp = 'Full Access Grader (Grad TA)';
+                        $reg_sec = implode(',', $grader->getGradingRegistrationSections());
+                        break;
+                    case 3:
+                        $grp = 'Limited Access Grader (Mentor)';
+                        $reg_sec = implode(',', $grader->getGradingRegistrationSections());
+                        break;
+                    default:
+                        $grp = 'UNKNOWN';
+                        $reg_sec = "";
+                        break;
+                }
+                $first_name = str_replace("'", "&#039;", $grader->getDisplayedFirstName());
+                $last_name = str_replace("'", "&#039;", $grader->getLastName());
+                array_push($download_info, ['first_name' => $first_name, 'last_name' => $last_name, 'user_id' => $grader->getId(), 'email' => $grader->getEmail(), 'reg_section' => "$reg_sec", 'rot_section' => "$rot_sec", 'group' => $grp]);
+            }
+        }
+        $download_info_json = json_encode($download_info);
+        return $this->core->getOutput()->renderTwigTemplate("admin/users/DownloadForm.twig", [
+            "reg_sections" => $reg_sections,
+            "use_database" => $use_database,
+            "code" => $code,
+            "download_info_json" => $download_info_json
+        ]);
+    }
+
+    public function rotatingSectionsForm($students, $reg_sections, $not_null_counts, $null_counts, $max_section) {
+        $reg_sections_count = array();
+        foreach ($students as $student) {
+            $registration = ($student->getRegistrationSection() === null) ? "NULL" : $student->getRegistrationSection();
+            if (array_key_exists($registration, $reg_sections_count)) {
+                $reg_sections_count[$registration] = $reg_sections_count[$registration]+1;
+            }
+            else {
+                $reg_sections_count[$registration] = 1;
+            }
+        }
+
+        return $this->core->getOutput()->renderTwigTemplate("admin/users/RotatingSectionsForm.twig", [
+            "students" => $students,
+            "reg_sections" => $reg_sections,
+            "reg_sections_count" => $reg_sections_count,
+            "not_null_counts" => $not_null_counts,
+            "null_counts" => $null_counts,
+            "max_section" => $max_section
+        ]);
+    }
+}
