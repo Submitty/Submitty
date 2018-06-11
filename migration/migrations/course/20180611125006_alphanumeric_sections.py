0d21a05508b76316c348bad838500f0196c9bca8
Matthew Peveler <matt.peveler@gmail.com>
2018-06-11 13:02:34 -0400

    add in typcast in down method

--- migration/migrations/course/20180611125006_alphanumeric_sections.py
+++ migration/migrations/course/20180611125006_alphanumeric_sections.py
@@ -1,19 +1,20 @@
 def up(conn):
     with conn.cursor() as cursor:
         # disable foreign key contraints while we typecast the columns
-        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL;")
+        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL")
 
+        # typecast from integer to character varying
         cursor.execute("ALTER TABLE ONLY sections_registration ALTER COLUMN sections_registration_id SET DATA TYPE character varying(255) USING sections_registration_id::varchar(255)")
         cursor.execute("ALTER TABLE ONLY grading_registration ALTER COLUMN sections_registration_id SET DATA TYPE character varying(255) USING sections_registration_id::varchar(255)")
         cursor.execute("ALTER TABLE ONLY users ALTER COLUMN registration_section SET DATA TYPE character varying(255) USING registration_section::varchar(255)")
         cursor.execute("ALTER TABLE ONLY gradeable_teams ALTER COLUMN registration_section SET DATA TYPE character varying(255) USING registration_section::varchar(255)")
 
         # renable the foreign key contraints as we're done typecasting
-        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL;")
+        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL")
 
         # add missing foreign key contraints
         cursor.execute("ALTER TABLE ONLY gradeable_teams ADD CONSTRAINT gradeable_teams_registration_section_fkey FOREIGN KEY (registration_section) REFERENCES sections_registration(sections_registration_id)")
@@ -27,11 +28,17 @@ def down(conn):
         cursor.execute("ALTER TABLE ONLY gradeable_teams DROP CONSTRAINT gradeable_teams_rotating_section_fkey")
 
         # disable foreign key contraints while we typecast the columns
-        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL;")
+        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL")
+
+        # typecast from integer to character varying
+        cursor.execute("ALTER TABLE ONLY sections_registration ALTER COLUMN sections_registration_id SET DATA TYPE integer USING sections_registration_id::integer")
+        cursor.execute("ALTER TABLE ONLY grading_registration ALTER COLUMN sections_registration_id SET DATA TYPE integer USING sections_registration_id::integer")
+        cursor.execute("ALTER TABLE ONLY users ALTER COLUMN registration_section SET DATA TYPE integer USING registration_section::integer")
+        cursor.execute("ALTER TABLE ONLY gradeable_teams ALTER COLUMN registration_section SET DATA TYPE integer USING registration_section::integer")
 
         # renable the foreign key contraints as we're done typecasting
-        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL;")
-        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL;")
+        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL")
