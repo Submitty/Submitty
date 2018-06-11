f019cab2563c3138fbe3f2bf9f616a17505a08ff
Matthew Peveler <matt.peveler@gmail.com>
2018-06-11 14:23:37 -0400

    fix bad migration file

--- migration/migrations/course/20180611125006_alphanumeric_sections.py
+++ migration/migrations/course/20180611125006_alphanumeric_sections.py
@@ -1,9 +1,8 @@
 def up(conn):
     with conn.cursor() as cursor:
-        # disable foreign key contraints while we typecast the columns
-        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL")
+        # drop foreign key contraints as the foreign keys also store type of columns in their definitions
+        cursor.execute("ALTER TABLE ONLY grading_registration DROP CONSTRAINT grading_registration_sections_registration_id_fkey")
+        cursor.execute("ALTER TABLE ONLY users DROP CONSTRAINT users_registration_section_fkey")
 
         # typecast from integer to character varying
         cursor.execute("ALTER TABLE ONLY sections_registration ALTER COLUMN sections_registration_id SET DATA TYPE character varying(255) USING sections_registration_id::varchar(255)")
@@ -12,9 +11,8 @@ def up(conn):
         cursor.execute("ALTER TABLE ONLY gradeable_teams ALTER COLUMN registration_section SET DATA TYPE character varying(255) USING registration_section::varchar(255)")
 
         # renable the foreign key contraints as we're done typecasting
-        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE ONLY grading_registration ADD CONSTRAINT grading_registration_sections_registration_id_fkey FOREIGN KEY (sections_registration_id) REFERENCES sections_registration(sections_registration_id)")
+        cursor.execute("ALTER TABLE ONLY users ADD CONSTRAINT users_registration_section_fkey FOREIGN KEY (registration_section) REFERENCES sections_registration(sections_registration_id)")
 
         # add missing foreign key contraints
         cursor.execute("ALTER TABLE ONLY gradeable_teams ADD CONSTRAINT gradeable_teams_registration_section_fkey FOREIGN KEY (registration_section) REFERENCES sections_registration(sections_registration_id)")
@@ -23,14 +21,13 @@ def up(conn):
 
 def down(conn):
     with conn.cursor() as cursor:
-        # remove missing foreign key contraints
+        # drop foreign key contraints while we typecast the columns
         cursor.execute("ALTER TABLE ONLY gradeable_teams DROP CONSTRAINT gradeable_teams_registration_section_fkey")
         cursor.execute("ALTER TABLE ONLY gradeable_teams DROP CONSTRAINT gradeable_teams_rotating_section_fkey")
 
         # disable foreign key contraints while we typecast the columns
-        cursor.execute("ALTER TABLE grading_registration DISABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE users DISABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE gradeable_teams DISABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE ONLY grading_registration DROP CONSTRAINT grading_registration_sections_registration_id_fkey")
+        cursor.execute("ALTER TABLE ONLY users DROP CONSTRAINT users_registration_section_fkey")
 
         # typecast from integer to character varying
         cursor.execute("ALTER TABLE ONLY sections_registration ALTER COLUMN sections_registration_id SET DATA TYPE integer USING sections_registration_id::integer")
@@ -39,6 +36,5 @@ def down(conn):
         cursor.execute("ALTER TABLE ONLY gradeable_teams ALTER COLUMN registration_section SET DATA TYPE integer USING registration_section::integer")
 
         # renable the foreign key contraints as we're done typecasting
-        cursor.execute("ALTER TABLE grading_registration ENABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE users ENABLE TRIGGER ALL")
-        cursor.execute("ALTER TABLE gradeable_teams ENABLE TRIGGER ALL")
+        cursor.execute("ALTER TABLE ONLY grading_registration ADD CONSTRAINT grading_registration_sections_registration_id_fkey FOREIGN KEY (sections_registration_id) REFERENCES sections_registration(sections_registration_id)")
+        cursor.execute("ALTER TABLE ONLY users ADD CONSTRAINT users_registration_section_fkey FOREIGN KEY (registration_section) REFERENCES sections_registration(sections_registration_id)")
