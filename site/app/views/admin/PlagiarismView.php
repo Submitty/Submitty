387b472690217fe3305a36598ed08bab140de670
Glenn Smith <couleeapps@gmail.com>
2018-06-12 13:10:09 -0400

    Revert "Admin plagiarism view"
    This reverts commit 9b4075719f4869fbe0f3d2cf9f1bd63ec8a8a37b.

--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -10,27 +10,55 @@ class PlagiarismView extends AbstractView {
         if (strpos($assignment, '.') || strpos($assignment, '/')) throw new \InvalidArgumentException("Invalid assignment");
         if (strpos($studenta, '.') || strpos($studenta, '/')) throw new \InvalidArgumentException("Invalid assignment");
         if (strpos($studentb, '.') || strpos($studentb, '/')) throw new \InvalidArgumentException("Invalid assignment");
-
-        $contents = file_get_contents("/var/local/submitty/courses/$semester/$course/plagiarism/report/var/local/submitty/courses/$semester/$course/submissions/$assignment/compare/" . $studenta . "_" . $studentb . ".html");
-        return $this->core->getOutput()->renderTwigTemplate("admin/plagiarism/Compare.twig", [
-            "contents" => $contents
-        ]);
+        $return = "";
+        $return .= <<<HTML
+<div class="content" style="height: 85vh">
+HTML;
+        $return .= file_get_contents("/var/local/submitty/courses/$semester/$course/plagiarism/report/var/local/submitty/courses/$semester/$course/submissions/$assignment/compare/" . $studenta . "_" . $studentb . ".html");
+        $return .= <<<HTML
+</div>
+HTML;
+        return $return;
     }
 
     public function plagiarismIndex($semester, $course, $assignment) {
         if (strpos($semester, '.') || strpos($semester, '/')) throw new \InvalidArgumentException("Invalid semester");
         if (strpos($course, '.') || strpos($course, '/')) throw new \InvalidArgumentException("Invalid course");
         if (strpos($assignment, '.') || strpos($assignment, '/')) throw new \InvalidArgumentException("Invalid assignment");
-
-        $contents = file_get_contents("/var/local/submitty/courses/$semester/$course/plagiarism/report/var/local/submitty/courses/$semester/$course/submissions/$assignment/index.html");
-        return $this->core->getOutput()->renderTwigTemplate("admin/plagiarism/Index.twig", [
-            "contents" => $contents
-        ]);
+        $return = "";
+        $return .= <<<HTML
+<div class="content">
+<h1 class="centered">Plagiarism Detection - $assignment</h1>
+<br>
+HTML;
+        $return .= file_get_contents("/var/local/submitty/courses/$semester/$course/plagiarism/report/var/local/submitty/courses/$semester/$course/submissions/$assignment/index.html");
+        $return .= <<<HTML
+</div>
+HTML;
+        return $return;
     }
 
     public function plagiarismTree($semester, $course, $assignments) {
-        return $this->core->getOutput()->renderTwigTemplate("admin/plagiarism/Tree.twig", [
-            "assignments" => $assignments
-        ]);
+        $return = "";
+        $return .= <<<HTML
+<div class="content">
+<h1 style="text-align: center">Plagiarism Detection</h1>
+<br>
+HTML;
+        if ($assignments) {
+            $return .= '<ul>';
+                foreach ($assignments as $assignment) {
+                    $return .= "<li><a href=\"{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'index', 'assignment' => $assignment))}\">$assignment</a></li>";
+                }
+            $return .= '</ul>';
+        } else {
+            $return .= <<<HTML
+<p>It looks like you have yet to run plagiarism detection on any assignment. See <a href="http://submitty.org/instructor/plagiarism">http://submitty.org/instructor/plagiarism</a> for details.</p>
+HTML;
+        }
+        $return .= <<<HTML
+</div>
+HTML;
+        return $return;
     }
 }
