b2cb2e29381c8d4cb68266212af7e41f7a5c2555
Barb Cutler <bmcutler@gmail.com>
2018-06-13 00:29:25 -0400

    print debug

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -516,6 +516,8 @@ min_lichen_version=v.18.06.00
 pushd ${lichen_repo_dir} > /dev/null
 git merge-base --is-ancestor "${min_lichen_version}" HEAD
 if [ $? -ne 0 ]; then
+    git status
+    git log
     echo -e "ERROR: Submitty/Lichen repository history does not contain version ${min_lichen_version}"
     echo -e "   Run 'git fetch' to get the tags from github."
     echo -e "   Also check to be sure your current branch is up-to-date."
