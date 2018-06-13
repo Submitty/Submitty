cd41f3f48222b296f129ee96d00d44115771e408
tushargr <tushar.gurjar1999@gmail.com>
2018-06-14 03:43:41 +0530

    store plagiarism form value in json that will be used by cronjob to run plagiarism

--- site/app/controllers/admin/PlagiarismController.php
+++ site/app/controllers/admin/PlagiarismController.php
@@ -15,8 +15,11 @@ class PlagiarismController extends AbstractController {
             case 'index':
                 $this->plagiarismIndex();
                 break;
+            case 'plagiarism_form':
+                $this->plagiarismForm();
+                break;    
             case 'run_plagiarism':
-                $this->runPlagiarismForm();
+                $this->runPlagiarism();
                 break;    
             default:
                 $this->core->getOutput()->addBreadcrumb('Plagiarism Detection');
@@ -52,7 +55,7 @@ class PlagiarismController extends AbstractController {
         $this->core->getOutput()->renderOutput(array('admin', 'Plagiarism'), 'plagiarismTree', $semester, $course, $assignments);  
     }
 
-    public function runPlagiarismForm() {
+    public function plagiarismForm() {
         $semester = $_REQUEST['semester'];
         $course = $_REQUEST['course'];
         $gradeable_ids = array_diff(scandir("/var/local/submitty/courses/$semester/$course/submissions/"), array('.', '..'));
@@ -64,6 +67,159 @@ class PlagiarismController extends AbstractController {
         }
         $all_sem_gradeables = FileUtils::getAllSemesterGradeables();
 
-        $this->core->getOutput()->renderOutput(array('admin', 'Plagiarism'), 'runPlagiarismForm', $gradeable_ids_titles, $all_sem_gradeables);
+        $this->core->getOutput()->renderOutput(array('admin', 'Plagiarism'), 'plagiarismForm', $gradeable_ids_titles, $all_sem_gradeables);
+    }
+
+    public function runPlagiarism() {
+
+        $semester = $_REQUEST['semester'];
+        $course = $_REQUEST['course'];
+        $return_url = $this->core->buildUrl(array('component'=>'admin', 'page' => 'plagiarism', 'course' => $course, 'semester' => $semester, 'action' => 'plagiarism_form'));
+        
+        if (!$this->core->checkCsrfToken($_POST['csrf_token'])) {
+            $this->core->addErrorMessage("Invalid CSRF token");
+            $this->core->redirect($return_url);
+        }
+
+        $prev_gradeable_number = $_POST['prev_gradeables_number'];
+        $ignore_submission_number = $_POST['ignore_submission_number'];
+        $gradeable = $_POST['gradeable_id'];
+        $version_option = $_POST['version_option'];
+        if ($version_option == "active_version") {
+            $version_option = "active_version";
+        }
+        else {
+            $version_option = "all_version";
+        }
+
+        $file_option = $_POST['file_option'];
+        if ($file_option == "regrex_matching_files") {
+            $file_option = "matching_regrex";
+        }
+        else {
+            $file_option = "all";
+        }
+        if($file_option == "matching_regrex") {
+            if( isset($_POST['regrex_to_select_files']) && $_POST['regrex_to_select_files'] !== '') {
+                $regrex_for_selecting_files = $_POST['regrex_to_select_files'];
+            }
+            else {
+                $this->core->addErrorMessage("No regrex provided for selecting files");
+                $this->core->redirect($return_url);
+            }    
+        }
+
+        $language= $_POST['language'];
+        if( isset($_POST['threshold']) && $_POST['threshold'] !== '') {
+            $threshold = $_POST['threshold'];
+        }
+        else {
+            $this->core->addErrorMessage("No input provided for threshold");
+            $this->core->redirect($return_url);
+        } 
+        if( isset($_POST['sequence_length']) && $_POST['sequence_length'] !== '') {
+            $sequence_length = $_POST['sequence_length'];
+        }
+        else {
+            $this->core->addErrorMessage("No input provided for sequence length");
+            $this->core->redirect($return_url);
+        } 
+
+        $prev_term_gradeables = array();
+        for( $i = 0; $i < $prev_gradeable_number; $i++ ) {
+            if($_POST['prev_sem_'.$i]!= "" && $_POST['prev_course_'.$i]!= "" && $_POST['prev_gradeable_'.$i]!= "") {
+                array_push($prev_term_gradeables, "/var/local/submitty/course/".$_POST['prev_sem_'.$i]."/".$_POST['prev_course_'.$i]."/submissions/".$_POST['prev_gradeable_'.$i]);
+            }
+        }
+
+        $ignore_submissions = array();
+        $ignore_submission_option = $_POST['ignore_submission_option'];
+        if ($ignore_submission_option == "ignore") {
+            for( $i = 0; $i < $ignore_submission_number; $i++ ) {
+                if(isset($_POST['ignore_submission_'.$i]) && $_POST['ignore_submission_'.$i] !== '') {
+                    array_push($ignore_submissions, $_POST['ignore_submission_'.$i]);
+                }
+            }    
+        }
+        
+        $gradeable_path =  FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "submissions", $gradeable);
+        $provided_code_option = $_POST['provided_code_option'];
+        if($provided_code_option == "code_provided") {
+            $instructor_provided_code= true;
+        }
+        else {
+            $instructor_provided_code= false;
+        }
+
+        if($instructor_provided_code == true) {
+            if (empty($_FILES) || !isset($_FILES['provided_code_file'])) {
+                $this->core->addErrorMessage("Upload failed: Instructor code not provided");
+                $this->core->redirect($return_url);
+            }
+            if (!isset($_FILES['provided_code_file']['tmp_name']) || $_FILES['provided_code_file']['tmp_name'] == "") {
+                $this->core->addErrorMessage("Upload failed: Instructor code not provided");
+                $this->core->redirect($return_url);
+            }
+
+            else {
+                $upload = $_FILES['provided_code_file'];
+                $target_dir = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "lichen/provided_code", $gradeable);
+                if (!is_dir($target_dir)) {
+                    FileUtils::createDir($target_dir);    
+                }
+                $target_dir = FileUtils::joinPaths($target_dir, count(scandir($target_dir))-1);
+                FileUtils::createDir($target_dir);
+                $instructor_provided_code_path = $target_dir;
+
+                if (FileUtils::getMimeType($upload["tmp_name"]) == "application/zip") {
+                    $zip = new \ZipArchive();
+                    $res = $zip->open($upload['tmp_name']);
+                    if ($res === true) {
+                        $zip->extractTo($target_dir);
+                        $zip->close();
+                    }
+                    else {
+                        FileUtils::recursiveRmdir($target_dir);
+                        $error_message = ($res == 19) ? "Invalid or uninitialized Zip object" : $zip->getStatusString();
+                        $this->core->addErrorMessage("Upload failed: {$error_message}");
+                        $this->core->redirect($return_url);
+                    }
+                }
+                else {
+                    if (!@copy($upload['tmp_name'], FileUtils::joinPaths($target_dir, $upload['name']))) {
+                        FileUtils::recursiveRmdir($target_dir);
+                        $this->core->addErrorMessage("Upload failed: Could not copy file");
+                        $this->core->redirect($return_url);
+                    }
+                }
+            }
+        }
+
+        $json_file = "/usr/local/submitty/Lichen/to_be_run/".$semester."__".$course."__".$gradeable.".json";
+        $json_data = array("semester" =>    $semester,
+                            "course" =>     $course,
+                            "gradeable" =>  $gradeable_path,
+                            "version" =>    $version_option,
+                            "file_option" =>$file_option,
+                            "language" =>   $language,
+                            "threshold" =>  $threshold,
+                            "sequence_length"=> $sequence_length,
+                            "prev_term_gradeables" => $prev_term_gradeables,
+                            "ignore_submissions" =>   $ignore_submissions,
+                            "instructor_provided_code" =>   $instructor_provided_code,
+                                        );
+
+        if($file_option == "matching_regrex") {
+            $json_data["regrex"] = $regrex_for_selecting_files;
+        }
+        if($instructor_provided_code == true) {
+            $json_data["instructor_provided_code_path"] = $instructor_provided_code_path;   
+        }
+        die(json_encode($json_data, JSON_PRETTY_PRINT));
+        // if (file_put_contents($json_file, json_encode($json_data, JSON_PRETTY_PRINT)) === false) {
+        //   die("Failed to write file {$json_file}");
+        // }
+
+        // $this->core->redirect($this->core->buildUrl(array('component'=>'admin', 'page' => 'plagiarism', 'course' => $course, 'semester' => $semester)));
     }
 }
--- site/app/views/admin/PlagiarismView.php
+++ site/app/views/admin/PlagiarismView.php
@@ -47,7 +47,7 @@ HTML;
 HTML;
         $return .= <<<HTML
         <div class="nav-buttons">
-            <a class="btn btn-primary" href="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'run_plagiarism'))}">Run Lichen Plagiarism Detector</a>
+            <a class="btn btn-primary" href="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'plagiarism_form'))}">Run Lichen Plagiarism Detector</a>
         </div><br /><br />        
 HTML;
         if ($assignments) {
@@ -68,8 +68,10 @@ HTML;
         return $return;
     }
 
-    public function runPlagiarismForm($gradeable_ids_titles, $all_sem_gradeables) {
+    public function plagiarismForm($gradeable_ids_titles, $all_sem_gradeables) {
         $all_sem_gradeables_json = json_encode($all_sem_gradeables);
+        $semester = $this->core->getConfig()->getSemester();
+        $course = $this->core->getConfig()->getCourse();
         $return = "";
         $return .= <<<HTML
 <div class="content">
@@ -78,9 +80,10 @@ HTML;
 HTML;
         $return .= <<<HTML
 <div id="run-plagiarism-form">
-    <form method="post" action="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'action' => 'run_plagiarism'))}" enctype="multipart/form-data">
+    <form method="post" action="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'course' => $course, 'semester' => $semester, 'action' => 'run_plagiarism'))}" enctype="multipart/form-data">
         <input type="hidden" name="csrf_token" value="{$this->core->getCsrfToken()}" />
         <input type="hidden" name="prev_gradeables_number" value="1" />
+        <input type="hidden" name="ignore_submission_number" value="1" />
         <br /><div style="width:100%;">
             Select Gradeable: 
             <select name="gradeable_id">
@@ -97,24 +100,24 @@ HTML;
         </div><br /><br />
         <div style="width:100%;">
             Instructor Provided Code: 
-            <input type="radio" id="no_code_provided_id" name="provided_code_option" checked >
+            <input type="radio" id="no_code_provided_id" value="no_code_provided" name="provided_code_option" checked >
             <label for="no_code_provided_id">No</label>
-            <input type="radio" id="code_provided_id" name="provided_code_option" >
+            <input type="radio" id="code_provided_id" value="code_provided" name="provided_code_option" >
             <label for="code_provided_id">Yes</label><br />
             <input type="file" name="provided_code_file">
         </div><br /><br />
         <div style="width:100%;">
             Version: 
-            <input type="radio" id="all_version_id" name="version_option" checked >
+            <input type="radio" id="all_version_id" value="all_version" name="version_option" checked >
             <label for="all_version_id">All Version</label>
-            <input type="radio" id="active_version_id" name="version_option" >
+            <input type="radio" id="active_version_id" value="active_version" name="version_option" >
             <label for="active_version_id">Only Active Version</label><br />
         </div><br /><br />
         <div style="width:100%;">
             Files to be Compared: 
-            <input type="radio" id="all_files_id" name="file_option[]" checked>
+            <input type="radio" id="all_files_id" value="all_files" name="file_option" checked>
             <label for="all_files_id">All Files</label>
-            <input type="radio" id="regrex_matching_files_id" name="file_option[]" >
+            <input type="radio" id="regrex_matching_files_id" value="regrex_matching_files" name="file_option" >
             <label for="regrex_matching_files_id">Regrex matching files</label><br /><br />
             <input type="text" name="regrex_to_select_files" />
         </div><br /><br />
@@ -158,8 +161,20 @@ HTML;
             <i class="fa fa-plus-square" aria-hidden="true" ></i>Add more
         </span>    
         <br /><br />
+        <div name="ignore_submission_div" style="width:100%;">
+            Are there any submissions that should be ignored? 
+            <input type="radio" id="ignore_none_id" value="no_ignore" name="ignore_submission_option" checked >
+            <label for="ignore_none_id">No</label>
+            <input type="radio" id="ignore_id" value="ignore" name="ignore_submission_option" >
+            <label for="ignore_id">Yes</label><br />
+            <input type="text" name="ignore_submission_0" />
+        </div>
+        <span style="cursor:pointer;" name="add_more_ignore">
+            <i class="fa fa-plus-square" aria-hidden="true" ></i>Add more
+        </span> 
+        <br /><br />
         <div style="float: right; width: auto; margin-top: 10px">
-            <a href="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism'))}" class="btn btn-danger">Cancel</a>
+            <a href="{$this->core->buildUrl(array('component' => 'admin', 'page' => 'plagiarism', 'course' => $course, 'semester' => $semester))}" class="btn btn-danger">Cancel</a>
             <input class="btn btn-primary" type="submit" value="Run Lichen Plagiarism Detector" />
         </div>
     </form>
@@ -173,6 +188,11 @@ HTML;
     });
     $('[name="add_more_prev_gradeable"]', form).on('click', function(){
         addMorePrevGradeable(all_sem_gradeables);
+    });
+    $('[name="add_more_ignore"]', form).on('click', function(){
+        var ignore_submission_number = $('[name="ignore_submission_number"]', form).val();
+        $('[name="ignore_submission_div"]', form).append('<br /><input type="text" name="ignore_submission_'+ ignore_submission_number +'" />');
+        $('[name="ignore_submission_number"]', form).val(parseInt(ignore_submission_number)+1);
     });
 </script>
 HTML;
