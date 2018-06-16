cce917e68880ab9174adb32c2a2f28d85f08e0da
Matthew Peveler <matt.peveler@gmail.com>
2018-06-15 23:17:13 -0400

    Use bash/python3 for calling things directly in git

--- .setup/travis/setup.sh
+++ .setup/travis/setup.sh
@@ -59,7 +59,7 @@ America/New_York
 http://localhost
 http://localhost/git
 
-${AUTH_METHOD}" | sudo ${SUBMITTY_REPOSITORY}/.setup/CONFIGURE_SUBMITTY.py --debug
+${AUTH_METHOD}" | sudo python3 ${SUBMITTY_REPOSITORY}/.setup/CONFIGURE_SUBMITTY.py --debug
 
 
 mkdir -p ${SUBMITTY_DATA_DIR}/instructors
@@ -79,4 +79,4 @@ sudo usermod -a -G travis hwphp
 # necessary to pass config path as submitty_repository is a symlink
 sudo python3 ${SUBMITTY_REPOSITORY}/migration/migrator.py -e master -e system migrate --initial
 
-sudo /usr/local/submitty/.setup/INSTALL_SUBMITTY.sh clean
+sudo bash /usr/local/submitty/.setup/INSTALL_SUBMITTY.sh clean
