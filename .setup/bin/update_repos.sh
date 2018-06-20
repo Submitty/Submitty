8c4d70a296606ee835109e95f171be17a4c77467
Barb Cutler <bmcutler@users.noreply.github.com>
2018-06-20 01:30:41 -0400

    Extra package installation for Lichen Python & C/C++ tokenization  (#2215)

--- .setup/bin/update_repos.sh
+++ .setup/bin/update_repos.sh
@@ -26,7 +26,7 @@ SUBMITTY_INSTALL_DIR=/usr/local/submitty
 # dependencies between versions.
 
 min_AnalysisTools_version=v.18.06.00
-min_Lichen_version=v.18.06.00
+min_Lichen_version=v.18.06.01
 min_RainbowGrades_version=v.18.06.00
 min_Tutorial_version=v.18.06.00
 
