df06f2b055e1265dc49a85649741a5bc1d703b17
Barb Cutler <bmcutler@gmail.com>
2018-06-19 23:54:32 -0400

    cleanup repo in INSTALL_HELPER

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -525,21 +525,6 @@ fi
 #####################################
 # Build & Install Lichen Modules
 
-lichen_repo_dir=${SUBMITTY_INSTALL_DIR}/GIT_CHECKOUT/Lichen
-min_lichen_version=v.18.06.00
-
-pushd ${lichen_repo_dir} > /dev/null
-git merge-base --is-ancestor "${min_lichen_version}" HEAD
-if [ $? -ne 0 ]; then
-    git status
-    git log
-    echo -e "ERROR: Submitty/Lichen repository history does not contain version ${min_lichen_version}"
-    echo -e "   Run 'git fetch' to get the tags from github."
-    echo -e "   Also check to be sure your current branch is up-to-date."
-    exit 1
-fi
-popd > /dev/null
-
 /bin/bash ${lichen_repo_dir}/install_lichen.sh
 
 
