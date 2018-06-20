d8a20786ddf5befbd966e40d4a4e2e0bafcfdd69
unknown <hultoj@rpi.edu>
2018-06-20 15:46:12 -0400

    bug fix

--- site/public/js/ta-grading-mark.js
+++ site/public/js/ta-grading-mark.js
@@ -466,6 +466,8 @@ function addMark(me, num) {
             parent.append(getMarkView(num, id2, id2, 1));
            // saveMark(me, true);
             window.location.reload();
+            toggleEditMode();
+            openMark(me);
             // Add new mark and then update
             // ajaxAddNewMark(gradeable_id, user_id, question_id, note, points, function() {
             //     updateMarksOnPage(num, background, min, max, precision, gradeable_id, user_id, get_active_version, question_id, your_user_id);
