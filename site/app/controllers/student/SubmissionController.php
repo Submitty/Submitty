06b3ea3adc6b4d6d5b46b127c0fa11bf8d32f8ba
ropern123 <ropern123@gmail.com>
2018-06-25 12:08:58 -0400

    Closes submission permissions loopholes (#2223)
    * Closes permissions loopholes
    
    * Prevent conflict with full-access-upload
    
    * Added PHPUnit testcases for fixed issues

--- site/app/controllers/student/SubmissionController.php
+++ site/app/controllers/student/SubmissionController.php
@@ -339,9 +339,9 @@ class SubmissionController extends AbstractController {
         }
 
         $max_size = $gradeable->getMaxSize();
-	if ($max_size < 10000000) {
-	    $max_size = 10000000;
-	}
+    if ($max_size < 10000000) {
+        $max_size = 10000000;
+    }
         // Error checking of file name
         $file_size = 0;
         if (isset($uploaded_file)) {
@@ -447,6 +447,13 @@ class SubmissionController extends AbstractController {
             return $this->uploadResult("Invalid CSRF token.", false);
         }
 
+        // make sure is admin
+        if (!$this->core->getUser()->accessAdmin()) {
+            $msg = "You do not have access to that page.";
+            $this->core->addErrorMessage($msg);
+            return $this->uploadResult($msg, false);
+        }
+
         $merge_previous = false;
         if(isset($_REQUEST['merge'])){
             if($_REQUEST['merge']  === "true"){
@@ -466,13 +473,6 @@ class SubmissionController extends AbstractController {
         }
         if (!isset($_POST['path'])) {
             return $this->uploadResult("Invalid path.", false);
-        }
-
-        // make sure is admin
-        if (!$this->core->getUser()->accessAdmin()) {
-            $msg = "You do not have access to that page.";
-            $this->core->addErrorMessage($msg);
-            return $this->uploadResult($msg, false);
         }
 
         $gradeable_id = $_REQUEST['gradeable_id'];
@@ -692,6 +692,13 @@ class SubmissionController extends AbstractController {
     private function ajaxDeleteSplitItem() {
         if (!isset($_POST['csrf_token']) || !$this->core->checkCsrfToken($_POST['csrf_token'])) {
             return $this->uploadResult("Invalid CSRF token.", false);
+        }
+
+        // make sure is grader
+        if (!$this->core->getUser()->accessGrading()) {
+            $msg = "You do not have access to that page.";
+            $this->core->addErrorMessage($msg);
+            return $this->uploadResult($msg, false);
         }
         
         $gradeable_list = $this->gradeables_list->getSubmittableElectronicGradeables();
@@ -1214,7 +1221,14 @@ class SubmissionController extends AbstractController {
     }
     
     private function updateSubmissionVersion() {
-        if (isset($_REQUEST['ta'])){
+        if (isset($_REQUEST['ta'])) {
+            // make sure is full grader
+            if (!$this->core->getUser()->accessFullGrading()) {
+                $msg = "You do not have access to that page.";
+                $this->core->addErrorMessage($msg);
+                $this->core->redirect($this->core->getConfig()->getSiteUrl());
+                return array('error' => true, 'message' => $msg);
+            }
             $ta = $_REQUEST['ta'];
             $who = $_REQUEST['who'];
             $mylist = new GradeableList($this->core, $this->core->getQueries()->getUserById($who));
