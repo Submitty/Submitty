024e8938f9b11337b5a40b011d0269d4cd6ad3d8
Glenn Smith <couleeapps@gmail.com>
2018-06-25 15:24:41 -0400

    Twig regrade panel

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -772,19 +772,9 @@ HTML;
      * @return string
      */
     public function renderRegradePanel(Gradeable $gradeable) {
-        $return = <<<HTML
-<div id="regrade_info" class = "draggable rubric_panel" style="right: 15px; bottom: 40px;width: 48%; height: 30%">
-    <div class = "draggable_content">
-        <div class = "inner-container" style="padding:20px;">
-HTML;
-        $return .= $this->core->getOutput()->renderTemplate('submission\Homework', 'showRequestForm', $gradeable);
-        $return .= $this->core->getOutput()->renderTemplate('submission\Homework', 'showRegradeDiscussion', $gradeable);
-        $return .= <<<HTML
-        </div>
-    </div>
-</div>
-HTML;
-        return $return;
+        return $this->core->getOutput()->renderTwigTemplate("grading/electronic/RegradePanel.twig", [
+            "gradeable" => $gradeable
+        ]);
     }
 
     public function popupStudents() {
