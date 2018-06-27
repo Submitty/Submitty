524b561e94941d5998c84b18b9a62f1d8a3ed301
pbailie <pbailie@users.noreply.github.com>
2018-06-27 18:29:00 -0400

    Changes to be committed: modified:   migration/data/submitty_db.sql modified:   migration/migrations/master/20180622203658_auto_feed_registration_sections.py
    Includes requested code change to submitty_db.sql

--- migration/migrations/master/20180622203658_auto_feed_registration_sections.py
+++ migration/migrations/master/20180622203658_auto_feed_registration_sections.py
@@ -5,20 +5,19 @@ CREATE TABLE public.courses_registration_sections (
     semester character varying(255) NOT NULL,
     course character varying(255) NOT NULL,
     registration_section_id character varying(255) NOT NULL,
-    PRIMARY KEY (semester, course, registration_section_id),
-    FOREIGN KEY (semester, course) REFERENCES public.courses(semester, course) ON UPDATE CASCADE);""")
+    CONSTRAINT courses_registration_sections_pkey PRIMARY KEY (semester, course, registration_section_id),
+    CONSTRAINT courses_registration_sections_fkey FOREIGN KEY (semester, course) REFERENCES courses(semester, course) ON UPDATE CASCADE);""")
 
         cursor.execute("""
-CREATE FUNCTION public.sync_registration_section() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
+CREATE OR REPLACE FUNCTION sync_registration_section() RETURNS trigger AS
+-- TRIGGER function to INSERT registration sections to course DB, as needed.
+$$
 DECLARE
   registration_row RECORD;
   db_conn VARCHAR;
   query_string TEXT;
 BEGIN
   FOR registration_row IN SELECT semester, course FROM courses_registration_sections WHERE registration_section_id=NEW.registration_section_id LOOP
-    RAISE NOTICE 'Semester: %, Course: %, Registration Section: %', registration_row.semester, registration_row.course, NEW.registration_section_id;
     db_conn := format('dbname=submitty_%s_%s', registration_row.semester, registration_row.course);
     query_string := 'INSERT INTO sections_registration VALUES(' || quote_literal(NEW.registration_section_id) || ') ON CONFLICT DO NOTHING';
     -- Need to make sure that query_string was set properly as dblink_exec will happily take a null and then do nothing
@@ -31,12 +30,12 @@ BEGIN
   -- All done.
   RETURN NULL;
 END;
-$$;""")
+$$ LANGUAGE plpgsql;""")
 
-        cursor.execute('CREATE TRIGGER registration_sync_registration_id AFTER INSERT ON public.courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE public.sync_registration_section();')
+        cursor.execute('CREATE TRIGGER registration_sync_registration_id AFTER INSERT ON courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE sync_registration_section();')
 
 def down(conn):
     with conn.cursor() as cursor:
         cursor.execute('DROP TRIGGER registration_sync_registration_id;')
-        cursor.execute('DROP FUNCTION public.sync_registration_section();')
-        cursor.execute('DROP TABLE public.courses_registration_sections;')
+        cursor.execute('DROP FUNCTION sync_registration_section();')
+        cursor.execute('DROP TABLE courses_registration_sections;')
