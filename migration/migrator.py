b0c37b4809e448456e61430360f607ed3e558979
Matthew Peveler <matt.peveler@gmail.com>
2018-06-29 00:20:54 -0400

    bump migrator version

--- migration/migrator.py
+++ migration/migrator.py
@@ -16,7 +16,7 @@ import sys
 
 import psycopg2
 
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 DIR_PATH = Path(__file__).parent.resolve()
 MIGRATIONS_PATH = DIR_PATH / 'migrations'
 ENVIRONMENTS = ['master', 'system', 'course']
