9c8254c3ec38a3682ff43d50b3313ce0530a5ff4
Glenn Smith <couleeapps@gmail.com>
2018-07-02 11:31:46 -0400

    Selenium downloads, GET via parts, assert not error page

--- tests/e2e/base_testcase.py
+++ tests/e2e/base_testcase.py
@@ -1,9 +1,16 @@
 from __future__ import print_function
+
+import shutil
+import tempfile
 from datetime import date
 import os
 import unittest
 
-from selenium import webdriver
+import sys
+
+from urllib.parse import urlencode
+
+from selenium.webdriver import Chrome as WebDriver
 from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -38,6 +45,16 @@ class BaseTestCase(unittest.TestCase):
         self.options.add_argument('--hide-scrollbars')
         self.options.add_argument('--disable-gpu')
         self.options.add_argument('--no-proxy-server')
+
+        self.download_dir = tempfile.mkdtemp(prefix="vagrant-submitty")
+        # https://stackoverflow.com/a/26916386/214063
+        profile = {
+            'download.prompt_for_download': False,
+            'download.default_directory': self.download_dir,
+            'download.directory_upgrade': True,
+            'plugins.plugins_disabled': ['Chrome PDF Viewer']
+        }
+        self.options.add_experimental_option('prefs', profile)
         self.user_id = user_id if user_id is not None else BaseTestCase.USER_ID
         self.user_name = user_name if user_name is not None else BaseTestCase.USER_NAME
         if user_password is None and user_id is not None:
@@ -48,17 +65,27 @@ class BaseTestCase(unittest.TestCase):
         self.use_log_in = log_in
 
     def setUp(self):
-        self.driver = webdriver.Chrome(options=self.options)
+        self.driver = WebDriver(options=self.options)
+        self.enable_download_in_headless_chrome(self.download_dir)
         if self.use_log_in:
             self.log_in()
 
     def tearDown(self):
         self.driver.quit()
+        shutil.rmtree(self.download_dir)
 
-    def get(self, url):
+    def get(self, url=None, parts=None):
+        if url is None:
+            # Can specify parts = [('semester', 's18'), ...]
+            self.assertIsNotNone(parts)
+            url = "/index.php?" + urlencode(parts)
+
         if url[0] != "/":
             url = "/" + url
         self.driver.get(self.test_url + url)
+
+        # Frog robot
+        self.assertNotEqual(self.driver.title, "Submitty - Error", "Got Error Page")
 
     def log_in(self, url=None, title="Submitty", user_id=None, user_password=None, user_name=None):
         """
@@ -138,3 +165,11 @@ class BaseTestCase(unittest.TestCase):
         if today.month < 7:
             semester = "s" + str(today.year)[-2:]
         return semester
+
+    # https://stackoverflow.com/a/47366981/214063
+    def enable_download_in_headless_chrome(self, download_dir):
+        # add missing support for chrome "send_command"  to selenium webdriver
+        self.driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+
+        params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+        command_result = self.driver.execute("send_command", params)
