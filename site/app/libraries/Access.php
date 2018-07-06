9f976758a09a607b2d4cc1c10142ffbfa84369de
Glenn Smith <couleeapps@gmail.com>
2018-07-06 11:36:02 -0400

    s/mentor/grader/g

--- site/app/libraries/Access.php
+++ site/app/libraries/Access.php
@@ -10,24 +10,24 @@ use app\models\User;
 class Access {
     const USER_GROUP_INSTRUCTOR = 1;
     const USER_GROUP_TA         = 2;
-    const USER_GROUP_MENTOR     = 3;
+    const USER_GROUP_GRADER     = 3;
     const USER_GROUP_STUDENT    = 4;
 
     // Bit field for various access control options
     const ALLOW_INSTRUCTOR              = 1 << 0;
     const ALLOW_TA                      = 1 << 1;
-    const ALLOW_MENTOR                  = 1 << 2;
+    const ALLOW_GRADER                  = 1 << 2;
     const ALLOW_STUDENT                 = 1 << 3;
     const ALLOW_LOGGED_OUT              = 1 << 4;
     const CHECK_GRADEABLE_MIN_GROUP     = 1 << 5;
-    const CHECK_GRADING_SECTION_MENTOR  = 1 << 6;
+    const CHECK_GRADING_SECTION_GRADER  = 1 << 6;
     const CHECK_PEER_ASSIGNMENT_STUDENT = 1 << 7;
     const CHECK_HAS_SUBMISSION          = 1 << 8;
     const CHECK_CSRF                    = 1 << 9;
 
     //
-    const ALLOW_MIN_STUDENT    = self::ALLOW_INSTRUCTOR | self::ALLOW_TA | self::ALLOW_MENTOR | self::ALLOW_STUDENT;
-    const ALLOW_MIN_MENTOR     = self::ALLOW_INSTRUCTOR | self::ALLOW_TA | self::ALLOW_MENTOR;
+    const ALLOW_MIN_STUDENT    = self::ALLOW_INSTRUCTOR | self::ALLOW_TA | self::ALLOW_GRADER | self::ALLOW_STUDENT;
+    const ALLOW_MIN_GRADER     = self::ALLOW_INSTRUCTOR | self::ALLOW_TA | self::ALLOW_GRADER;
     const ALLOW_MIN_TA         = self::ALLOW_INSTRUCTOR | self::ALLOW_TA;
     const ALLOW_MIN_INSTRUCTOR = self::ALLOW_INSTRUCTOR;
 
@@ -41,14 +41,14 @@ class Access {
         $this->core = $core;
 
         $this->permissions["grading.details"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP;
-        $this->permissions["grading.grade"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR | self::CHECK_PEER_ASSIGNMENT_STUDENT;
-        $this->permissions["grading.show_hidden_cases"] = self::ALLOW_MIN_MENTOR | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR;
-        $this->permissions["grading.save_one_component"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR | self::CHECK_PEER_ASSIGNMENT_STUDENT | self::CHECK_HAS_SUBMISSION;
-        $this->permissions["grading.save_general_comment"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR | self::CHECK_PEER_ASSIGNMENT_STUDENT | self::CHECK_HAS_SUBMISSION;
-        $this->permissions["grading.get_mark_data"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR | self::CHECK_PEER_ASSIGNMENT_STUDENT;
-        $this->permissions["grading.get_gradeable_comment"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR | self::CHECK_PEER_ASSIGNMENT_STUDENT;
-        $this->permissions["grading.add_one_new_mark"] = self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR;
-        $this->permissions["grading.delete_one_mark"] = self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_MENTOR;
+        $this->permissions["grading.grade"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER | self::CHECK_PEER_ASSIGNMENT_STUDENT;
+        $this->permissions["grading.show_hidden_cases"] = self::ALLOW_MIN_GRADER | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER;
+        $this->permissions["grading.save_one_component"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER | self::CHECK_PEER_ASSIGNMENT_STUDENT | self::CHECK_HAS_SUBMISSION;
+        $this->permissions["grading.save_general_comment"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER | self::CHECK_PEER_ASSIGNMENT_STUDENT | self::CHECK_HAS_SUBMISSION;
+        $this->permissions["grading.get_mark_data"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER | self::CHECK_PEER_ASSIGNMENT_STUDENT;
+        $this->permissions["grading.get_gradeable_comment"] = self::ALLOW_MIN_STUDENT | self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER | self::CHECK_PEER_ASSIGNMENT_STUDENT;
+        $this->permissions["grading.add_one_new_mark"] = self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER;
+        $this->permissions["grading.delete_one_mark"] = self::CHECK_GRADEABLE_MIN_GROUP | self::CHECK_GRADING_SECTION_GRADER;
         $this->permissions["grading.import_teams"] = self::ALLOW_MIN_INSTRUCTOR | self::CHECK_CSRF;
         $this->permissions["grading.export_teams"] = self::ALLOW_MIN_INSTRUCTOR;
         $this->permissions["grading.submit_team_form"] = self::ALLOW_MIN_INSTRUCTOR;
@@ -77,7 +77,7 @@ class Access {
         $group = $user->getGroup();
         if ($group === self::USER_GROUP_STUDENT && !($checks & self::ALLOW_STUDENT)) {
             return false;
-        } else if ($group === self::USER_GROUP_MENTOR && !($checks & self::ALLOW_MENTOR)) {
+        } else if ($group === self::USER_GROUP_GRADER && !($checks & self::ALLOW_GRADER)) {
             return false;
         } else if ($group === self::USER_GROUP_TA && !($checks & self::ALLOW_TA)) {
             return false;
@@ -106,7 +106,7 @@ class Access {
             }
         }
 
-        if ($group === self::USER_GROUP_MENTOR && ($checks & self::CHECK_GRADING_SECTION_MENTOR)) {
+        if ($group === self::USER_GROUP_GRADER && ($checks & self::CHECK_GRADING_SECTION_GRADER)) {
             //Check their grading section
             if (!$this->checkGradingSection($gradeable)) {
                 return false;
