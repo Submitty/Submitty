d77ee0fd60b0ab843557dae3036deae0bf05f1fd
Barb Cutler <bmcutler@gmail.com>
2018-07-06 23:16:38 -0400

    str convert

--- sbin/submitty_daemon_jobs/submitty_jobs/__init__.py
+++ sbin/submitty_daemon_jobs/submitty_jobs/__init__.py
@@ -7,13 +7,12 @@ import json
 from pathlib import Path
 
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', '..', 'config')
-print ("CONFIG PATH "+CONFIG_PATH)
-os.system ("ls -lta "+str(CONFIG_PATH))
-with open(Path(CONFIG_PATH, 'submitty.json'),'r') as open_file:
+
+with open(str(Path(CONFIG_PATH, 'submitty.json'))) as open_file:
     JSON_FILE = json.load(open_file)
 DATA_DIR = JSON_FILE['submitty_data_dir']
 QUEUE_DIR = Path(DATA_DIR, 'submitty_daemon_job_queue')
 
-with open(Path(CONFIG_PATH, 'submitty_users.json')) as open_file:
+with open(str(Path(CONFIG_PATH, 'submitty_users.json'))) as open_file:
     JSON_FILE = json.load(open_file)
 DAEMON_USER = JSON_FILE['daemon_user']
