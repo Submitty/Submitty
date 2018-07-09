c488f407838312814bf0810112b9c3f6a7e97e0d
Glenn Smith <couleeapps@gmail.com>
2018-07-09 12:23:07 -0400

    Twig cleanup (#2338)
    * Remove manual loop counters
    
    * for/if replaced with for-if
    
    * Consistency
    
    * Nuke {{ core.getConfig.. }}
    
    * Query stats that don't generate html in the model
    
    * Taking HTML out of PHP
    
    * Error check
    
    * Getting the view out of the model
    
    * Array accessor syntax
    
    * {{ consistency }}
    
    * snake_case
    
    * Couple more cores
    
    * Don't crash if logged out
    
    * Fancier
    
    * Fancier?
    
    * The This
    
    * Escape it right
    
    * Whoops

--- site/app/views/AuthenticationView.php
+++ site/app/views/AuthenticationView.php
@@ -6,6 +6,7 @@ class AuthenticationView extends AbstractView {
     public function loginForm() {
         $old = $_REQUEST['old'] ?? [];
         return $this->core->getOutput()->renderTwigTemplate("Authentication.twig", [
+            "site_url" => $this->core->getConfig()->getSiteUrl(),
             "old" => $old
         ]);
     }
