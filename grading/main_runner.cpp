cfd076762a35e2b26fb4ac7476cf81d32c35fc6d
emaicus <emaicus@skidmore.edu>
2018-07-12 21:00:49 -0400

    Moves Iteration over Testcases from C++ to Python (#2383)
    * initial commit
    
    * continued work
    
    * wip
    
    * finished initial action refactor
    
    * fixed bug in delay
    
    * Added scrot screenshotting program to xenial install
    
    * initial commit
    
    * Testcase loop moved to grade_item_main_runner.py
    
    * fixes

--- grading/main_runner.cpp
+++ grading/main_runner.cpp
@@ -21,13 +21,17 @@ int main(int argc, char *argv[]) {
   std::string rcsid = "";
   int subnum = -1;
   std::string time_of_submission = "";
-
+  //If test_case_to_run isn't passed in as a parameter, all testcases are run.
+  int test_case_to_run = -1;
   // Check command line arguments
-  if (argc == 5) {
+  if (argc >= 5) {
     hw_id = argv[1];
     rcsid = argv[2];
     subnum = atoi(argv[3]);
     time_of_submission = argv[4];
+    if (argc == 6){
+      test_case_to_run = atoi(argv[5]);
+    }
   }
   else if (argc != 1) {
     std::cerr << "INCORRECT ARGUMENTS TO RUNNER" << std::endl;
@@ -37,11 +41,11 @@ int main(int argc, char *argv[]) {
   // LOAD HW CONFIGURATION JSON
   nlohmann::json config_json = LoadAndProcessConfigJSON(rcsid);
 
-  nlohmann::json grading_parameters = config_json.value("grading_parameters",nlohmann::json::object());
-  int AUTO_POINTS         = grading_parameters.value("AUTO_POINTS",0);
-  int EXTRA_CREDIT_POINTS = grading_parameters.value("EXTRA_CREDIT_POINTS",0);
-  int TA_POINTS           = grading_parameters.value("TA_POINTS",0);
-  int TOTAL_POINTS        = grading_parameters.value("TOTAL_POINTS",AUTO_POINTS+TA_POINTS);
+  // nlohmann::json grading_parameters = config_json.value("grading_parameters",nlohmann::json::object());
+  // int AUTO_POINTS         = grading_parameters.value("AUTO_POINTS",0);
+  // int EXTRA_CREDIT_POINTS = grading_parameters.value("EXTRA_CREDIT_POINTS",0);
+  // int TA_POINTS           = grading_parameters.value("TA_POINTS",0);
+  // int TOTAL_POINTS        = grading_parameters.value("TOTAL_POINTS",AUTO_POINTS+TA_POINTS);
 
   // necessary since the untrusted user does not have a home directory
   setenv("DYNAMORIO_CONFIGDIR", ".", 1);
@@ -58,15 +62,29 @@ int main(int argc, char *argv[]) {
 
   nlohmann::json::iterator tc = config_json.find("testcases");
   assert (tc != config_json.end());
+
+  if(test_case_to_run != -1){
+    assert (test_case_to_run < tc->size());
+  }else{
+    std::cout << "Running all testcases in a single run." << std::endl;
+  }
+
   for (unsigned int i = 0; i < tc->size(); i++) {
+
+    TestCase my_testcase(config_json,i);
+
+    if (my_testcase.isFileCheck() || my_testcase.isCompilation()){
+      continue;
+    }
+
+    if(test_case_to_run != -1 &&  test_case_to_run != i){
+      continue;
+    }
 
     std::cout << "========================================================" << std::endl;
     std::cout << "TEST #" << i+1 << std::endl;
 
-    TestCase my_testcase(config_json,i);
 
-    if (my_testcase.isFileCheck()) continue;
-    if (my_testcase.isCompilation()) continue;
     std::vector<std::string> commands = stringOrArrayOfStrings((*tc)[i],"command");
     std::vector<nlohmann::json> actions  = mapOrArrayOfMaps((*tc)[i],"actions");
     assert (commands.size() > 0);
@@ -97,9 +115,9 @@ int main(int argc, char *argv[]) {
                             config_json.value("resource_limits",nlohmann::json()),
                             config_json,
                             windowed); 
-      
+
     }
-    
+
     std::vector<std::vector<std::string>> filenames = my_testcase.getFilenames();
     assert (filenames.size() > 0);
     assert (filenames.size() == my_testcase.numFileGraders());
@@ -124,11 +142,9 @@ int main(int argc, char *argv[]) {
         }
       }
     }
+    std::cout << "========================================================" << std::endl;
+    std::cout << "FINISHED TEST #" << i+1 << std::endl;
   }
-  
-  std::cout << "========================================================" << std::endl;
-  std::cout << "FINISHED ALL TESTS" << std::endl;
-  
   return 0;
 }
 
