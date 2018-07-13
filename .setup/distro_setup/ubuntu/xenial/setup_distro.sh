7528c5d0cfe7fabbd7b762f21259176bc6a36eb6
Matthew Peveler <matt.peveler@gmail.com>
2018-07-12 23:10:36 -0400

    Remove pip2 install clang installation line (#2403)
    * remove pip2 install clang
    
    * Some fixes to make install_system.sh --vagrant rerunable

--- .setup/distro_setup/ubuntu/xenial/setup_distro.sh
+++ .setup/distro_setup/ubuntu/xenial/setup_distro.sh
@@ -95,7 +95,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -qqy
 apt-get install -qqy -y docker-ce
-systemctl status docker
+systemctl status docker | head -n 100
 
 
 if [ ${VAGRANT} == 1 ]; then
