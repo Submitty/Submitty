9c8e1783605ea49c2401480440a236266195c1e5
Matthew Peveler <matt.peveler@gmail.com>
2018-07-12 21:44:36 -0400

    Add set flag to travis bash scripts (#2402)
    * Add set flag to called travis scripts
    
    * fix export usage in before_install
    
    * prune down emma commands
    
    * Add sudo flag to command
    
    * remove set -ev flag from before_install.sh as it messes up travis log and it already uses set -e
    
    * add sudo in front of chpasswd
    
    * inline the password for submitty_dbuser
    
    * use useradd instead of adduser
    
    * add psycopg2 depedency to integration testing
    
    * Create database and database user for integration test
    
    * Set postgresql version to be the same as e2e tests

--- .setup/travis/setup_test_suite.sh
+++ .setup/travis/setup_test_suite.sh
@@ -13,6 +13,14 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 source ${DIR}/../common/common_env.sh
 
+# this script must be run by root or sudo
+if [[ "$UID" -ne "0" ]] ; then
+    echo "ERROR: This script must be run by root or sudo"
+    exit 1
+fi
+
+set -ev
+
 echo "Setting up auto-grader test suite"
 
 mkdir -p ${SUBMITTY_INSTALL_DIR}/test_suite
