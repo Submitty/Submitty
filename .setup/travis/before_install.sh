55d0663100e662dcf86f02d90122d676d5a007c2
Matthew Peveler <matt.peveler@gmail.com>
2018-07-13 13:34:18 -0400

    Turn off env_reset in Travis sudoers

--- .setup/travis/before_install.sh
+++ .setup/travis/before_install.sh
@@ -14,4 +14,9 @@ pyenv global ${PY_VERSION_3}
 
 mkdir -p ~/.local/bin
 export PATH="${HOME}/.local/bin:${PATH}:${HOME}/.composer/vendor/bin:/usr/bin"
+
+# Travis has a really bizarre setup of how its Python/PHP stuff isn't really accessible to the "root" user
+# and so we have to do some janky workarounds to make it so that the travis, root, and submitty users all
+# refer to the same thing when doing "python3", "pip3", "composer", etc.
+sudo sed -i -e "s/env_reset/\!env_reset/g" /etc/sudoers
 sudo sed -e "s?secure_path=\"?secure_path=\"/home/travis/.phpenv/shims:/opt/python/${PY_VERSION_3}/bin:${PATH}:?g" --in-place /etc/sudoers
