6346faa9e10a97f40861e148241422400fa4cdac
Glenn Smith <couleeapps@gmail.com>
2018-07-16 15:40:55 -0400

    Abort recreate_sample_courses.sh if any command fails
    Otherwise it tries to keep going and causes a bunch of confusion

--- .setup/bin/recreate_sample_courses.sh
+++ .setup/bin/recreate_sample_courses.sh
@@ -4,6 +4,9 @@
 
 ########################################################################
 
+# If any command fails, we need to bail
+set -e
+
 # this script must be run by root or sudo
 if [[ "$UID" -ne "0" ]] ; then
     echo "ERROR: This script must be run by root or sudo"
