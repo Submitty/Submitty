58d6e83def57381d78e44ceb7db0ab4f12804453
Nick Roper <ropern123@gmail.com>
2018-07-16 12:49:58 -0400

    Added ability to delete uploaded html files

--- site/app/controllers/admin/WrapperController.php
+++ site/app/controllers/admin/WrapperController.php
@@ -4,13 +4,15 @@ namespace app\controllers\admin;
 
 use app\controllers\AbstractController;
 use app\libraries\FileUtils;
-use phpDocumentor\Reflection\File;
+use app\libraries\Utils;
 
 class WrapperController extends AbstractController {
 	public function run() {
 		switch($_REQUEST['action']) {
             case 'process_upload_html':
                 $this->processUploadHTML();
+            case 'delete_uploaded_html':
+                $this->deleteUploadedHTML();
 			case 'show_page':
 			default:
 				$this->uploadWrapperPage();
@@ -49,9 +51,52 @@ class WrapperController extends AbstractController {
             $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
                 'action' => 'show_page')));
         }
+        $upload = $_FILES['wrapper_upload'];
 
-        $target_dir = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'site');
+        if(!Utils::endsWith($upload['name'],'.html')) {
+            $this->core->addErrorMessage("Upload failed: Invalid file: Not an html file");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+                'action' => 'show_page')));
+        }
 
+        if(!isset($_POST['location']) && ($_POST['location'] !== 'upper-left' || $_POST['location'] !== 'upper-right' || $_POST['location'] !== 'lower-left')) {
+            $this->core->addErrorMessage("Upload failed: Invalid location");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+                'action' => 'show_page')));
+        }
+        $filename = $_POST['location'].'.html';
+
+        if (!@copy($upload['tmp_name'], FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'site', $filename))) {
+            $this->core->addErrorMessage("Upload failed: Could not copy file");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+                'action' => 'show_page')));
+        }
+
+        $this->core->addSuccessMessage("Uploaded ".$upload['name']." as ".$filename);
+        $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+            'action' => 'show_page')));
+    }
+
+    private function deleteUploadedHTML() {
+
+        if(!isset($_REQUEST['filename']) && ($_REQUEST['filename'] !== 'upper-left' || $_REQUEST['filename'] !== 'upper-right' || $_REQUEST['filename'] !== 'lower-left')) {
+            $this->core->addErrorMessage("Upload failed: Invalid filename");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+                'action' => 'show_page')));
+        }
+        $filename = $_REQUEST['filename'].'.html';
+
+	    $filepath = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'site', $filename);
+
+	    if(!@unlink($filepath)) {
+	        $this->core->addErrorMessage("Deletion failed: Could not unlink file");
+            $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+                'action' => 'show_page')));
+        }
+
+        $this->core->addSuccessMessage("Deleted ".$filename);
+        $this->core->redirect($this->core->buildUrl(array('component' => 'admin', 'page' => 'wrapper',
+            'action' => 'show_page')));
     }
 
 }
