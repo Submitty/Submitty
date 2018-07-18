ded34a95a20c8e65d39a2caadedb64e4c373a1e4
Joanna Ng <jn7070@gmail.com>
2018-07-18 14:42:25 -0400

    collapsible folders and replace message

--- site/app/views/course/CourseMaterialsView.php
+++ site/app/views/course/CourseMaterialsView.php
@@ -14,7 +14,32 @@ class CourseMaterialsView extends AbstractView {
      * @return string
      */
     public function listCourseMaterials($instructor_permission) {
-        //array to hold all materials filenames
+
+        function add_files(&$files, $expected_path, $course_materials_array, $start_dir_name) {
+            $files[$start_dir_name] = array();
+            $working_dirRoot = &$files[$start_dir_name];
+			
+            $arrlength = count($course_materials_array);
+
+            foreach($course_materials_array as $file) {
+                $path = explode('/', $file);
+                $working_dir = &$files[$start_dir_name];
+                $filename = array_pop($path);
+                foreach($path as $dir) {
+                    if (!isset($working_dir[$dir])) {
+                        $working_dir[$dir] = array();
+                    }
+                    $working_dir = &$working_dir[$dir];
+                }
+				
+                $expected_file_path = FileUtils::joinPaths($expected_path, $file);
+				
+                $working_dir[$filename] = $expected_file_path;
+            }
+        }
+		 
+        $submissions = array();
+
         $course_materials_array = array();
 
         //Get the expected course materials path and files
@@ -22,9 +47,13 @@ class CourseMaterialsView extends AbstractView {
         $path_length = strlen($expected_path)+1;
         $course_materials_array = FileUtils::getAllFilesTrimSearchPath($expected_path, $path_length);
         usort($course_materials_array, 'strnatcasecmp');
+
+        add_files($submissions, $expected_path, $course_materials_array, 'course_materials');
+
         return $this->core->getOutput()->renderTwigTemplate("course/CourseMaterials.twig", [
             "courseMaterialsArray" => $course_materials_array,
             "folderPath" => $expected_path,
+            "submissions" => $submissions,
             "hasInstructorPermission" => $instructor_permission
         ]);
     }
