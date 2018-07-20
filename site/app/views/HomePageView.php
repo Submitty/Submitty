a0706d562f0a5994b739801c333e4d1769bc1d92
ropern123 <ropern123@gmail.com>
2018-07-20 01:28:40 -0400

    Change display of archived courses for instructor (#2462)
    * Ordering courses by group, then semester, then course name
    
    * Fix
    
    * Added section for archived courses for instructors
    
    * Fixed display of courses
    
    * Minor ordering fix
    
    * Fixed potential crash and extra newline on homepage
    
    * archived courses have status=2

--- site/app/views/HomePageView.php
+++ site/app/views/HomePageView.php
@@ -13,7 +13,9 @@ class HomePageView extends AbstractView {
     /*
     *@param List of courses the student is in.
     */
-    public function showHomePage($user, $courses = array(), $changeNameText) {
+    public function showHomePage($user, $unarchived_courses = array(), $archived_courses = array(), $changeNameText) {
+        $statuses = array();
+        $course_types = [$unarchived_courses, $archived_courses];
         $rankTitles = [
             0 => "Developer:",
             1 => "Instructor:",
@@ -21,29 +23,34 @@ class HomePageView extends AbstractView {
             3 => "Grader:",
             4 => "Student:"
         ];
-        $ranks = array();
 
-        //Create rank lists
-        for ($i = 0; $i < 5; $i++){
-            $ranks[$i] = [];
-            $ranks[$i]["title"] = $rankTitles[$i];
-            $ranks[$i]["courses"] = [];
+        foreach($course_types as $course_type) {
+            $ranks = array();
+
+            //Create rank lists
+            for ($i = 0; $i < 5; $i++){
+                $ranks[$i] = [];
+                $ranks[$i]["title"] = $rankTitles[$i];
+                $ranks[$i]["courses"] = [];
+            }
+
+            //Assemble courses into rank lists
+            foreach ($course_type as $course) {
+                $rank = $this->core->getQueries()->getGroupForUserInClass($course->getSemester(), $course->getTitle(), $user->getId());
+                array_push($ranks[$rank]["courses"], $course);
+            }
+
+            //Filter any ranks with no courses
+            $ranks = array_filter($ranks, function($rank) {
+                return count($rank["courses"]) > 0;
+            });
+            $statuses[] = $ranks;
         }
 
-        //Assemble courses into rank lists
-        foreach ($courses as $course) {
-            $rank = $this->core->getQueries()->getGroupForUserInClass($course->getSemester(), $course->getTitle(), $user->getId());
-            array_push($ranks[$rank]["courses"], $course);
-        }
-
-        //Filter any ranks with no courses
-        $ranks = array_filter($ranks, function($rank) {
-            return count($rank["courses"]) > 0;
-        });
 
         return $this->core->getOutput()->renderTwigTemplate('HomePage.twig', [
             "user" => $user,
-            "ranks" => $ranks,
+            "statuses" => $statuses,
             "change_name_text" => $changeNameText,
             "show_change_password" => $this->core->getAuthentication() instanceof DatabaseAuthentication
         ]);
