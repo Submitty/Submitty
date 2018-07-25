ea07a6fba4bca51ff10ca3a081f37dd545904488
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-07-25 09:59:36 -0400

    Removed mark/component deletion from AdminGradeableController

--- site/app/controllers/admin/AdminGradeableController.php
+++ site/app/controllers/admin/AdminGradeableController.php
@@ -442,8 +442,6 @@ class AdminGradeableController extends AbstractController {
 
         /** @var Component[] $new_components */
         $new_components = [];
-        /** @var Component[] $delete_components */
-        $delete_components = [];
 
         $update_component_peer = function (Component $component, $peer_grading_complete_score) {
             $component->setPoints([
@@ -522,8 +520,6 @@ class AdminGradeableController extends AbstractController {
                     self::parseEgComponent($old_component, $details, $x);
                     $old_component->setOrder($x);
                     $new_components[] = $old_component;
-                } else if ($num_old_components > $num_questions) {
-                    $delete_components[] = $old_component;
                 }
                 $x++;
             }
@@ -547,7 +543,6 @@ class AdminGradeableController extends AbstractController {
             foreach ($new_components as $comp) {
                 $marks = $comp->getMarks();
                 $new_marks = [];
-                $delete_marks = [];
 
                 if ($comp->getOrder() == -1) {
                     continue;
@@ -581,24 +576,11 @@ class AdminGradeableController extends AbstractController {
                             }
                         }
                     }
-
-                    //delete marks marked for deletion
-                    $gcm_ids_deletes = explode(",", $details['component_deleted_marks_' . $index]);
-                    foreach ($gcm_ids_deletes as $gcm_id_to_delete) {
-                        foreach ($marks as $mark) {
-                            if ($gcm_id_to_delete == $mark->getId()) {
-                                $delete_marks[] = $mark;
-                            }
-                        }
-                    }
                 }
                 $index++;
 
                 // Finally, set the new marks ...
                 $comp->setMarks($new_marks);
-
-                // ... And delete the ones marked for deletion
-                $this->core->getQueries()->deleteMarks($delete_marks);
             }
         } else if ($gradeable->getType() === GradeableType::CHECKPOINTS) {
             if (!isset($details['checkpoints'])) {
@@ -615,8 +597,6 @@ class AdminGradeableController extends AbstractController {
                     self::parseCheckpoint($old_component, $details['checkpoints'][$x]);
                     $old_component->setOrder($x);
                     $new_components[] = $old_component;
-                } else if ($num_old_components > $num_checkpoints) {
-                    $delete_components[] = $old_component;
                 }
                 $x++;
             }
@@ -666,8 +646,6 @@ class AdminGradeableController extends AbstractController {
                     $old_numeric->setOrder($x);
                     $new_components[] = $old_numeric;
                     $start_index_numeric++;
-                } else if ($num_old_numerics > $num_numeric) {
-                    $delete_components[] = $old_numeric;
                 }
                 $x++;
             }
@@ -689,8 +667,6 @@ class AdminGradeableController extends AbstractController {
                     $old_text->setOrder($z + $x);
                     $new_components[] = $old_text;
                     $start_index_text++;
-                } else if ($num_old_texts > $num_text) {
-                    $delete_components[] = $old_text;
                 }
                 $x++;
             }
@@ -707,9 +683,6 @@ class AdminGradeableController extends AbstractController {
 
         // Finally, Set the components and update the gradeable
         $gradeable->setComponents($new_components);
-
-        // Delete the old ones
-        $this->core->getQueries()->deleteComponents($delete_components);
 
         // Save to the database
         $this->core->getQueries()->updateGradeable($gradeable);
