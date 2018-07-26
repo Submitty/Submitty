196dc08c26901b53acde974ee73b081e43bbd3c0
Nick Roper <ropern123@gmail.com>
2018-07-26 18:24:16 -0400

    Autosaving course settings page

--- site/app/controllers/admin/ConfigurationController.php
+++ site/app/controllers/admin/ConfigurationController.php
@@ -71,55 +71,49 @@ class ConfigurationController extends AbstractController {
 
     public function updateConfiguration() {
         if (!$this->core->checkCsrfToken($_POST['csrf_token'])) {
-            $this->core->addErrorMessage("Invalid CSRF token. Try again.");
-            $_SESSION['request'] = $_POST;
-            $this->core->redirect($this->core->buildUrl(array('component' => 'admin',
-                                                              'page' => 'configuration',
-                                                              'action' => 'view')));
+            $response = array('status' => 'fail', 'message' => 'Invalid CSRF token');
+            $this->core->getOutput()->renderJson($response);
+            return $response;
         }
 
-        if (!isset($_POST['course_name']) || $_POST['course_name'] == "") {
-            $this->core->addErrorMessage("Course name can not be blank");
-            $_SESSION['request'] = $_POST;
-            $this->core->redirect($this->core->buildUrl(array('component' => 'admin',
-                                                              'page' => 'configuration',
-                                                              'action' => 'view')));
+        if(!isset($_POST['name'])) {
+            $response = array('status' => 'fail', 'message' => 'Name of config value not provided');
+            $this->core->getOutput()->renderJson($response);
+            return $response;
+        }
+        $name = $_POST['name'];
+
+        if(!isset($_POST['entry'])) {
+            $response = array('status' => 'fail', 'message' => 'Name of config entry not provided');
+            $this->core->getOutput()->renderJson($response);
+            return $response;
+        }
+        $entry = $_POST['entry'];
+
+        if($name === "course_name" && $entry === "") {
+            $response = array('status' => 'fail', 'message' => 'Course name cannot be blank');
+            $this->core->getOutput()->renderJson($response);
+            return $response;
         }
 
-        foreach (array('default_hw_late_days', 'default_student_late_days') as $key) {
-            $_POST[$key] = (isset($_POST[$key])) ? intval($_POST[$key]) : 0;
+        if(in_array($name, array('default_hw_late_days', 'default_student_late_days'))) {
+            $entry = $entry === "" ? intval($entry) : 0;
         }
 
-        foreach (array('zero_rubric_grades', 'keep_previous_files', 'display_rainbow_grades_summary', 'display_custom_message', 'forum_enabled', 'regrade_enabled') as $key) {
-            $_POST[$key] = (isset($_POST[$key]) && $_POST[$key] == "true") ? true : false;
+        if(in_array($name, array('zero_rubric_grades', 'keep_previous_files', 'display_rainbow_grades_summary', 'display_custom_message', 'forum_enabled', 'regrade_enabled'))) {
+            $entry = $entry === "true" ? true : false;
         }
 
-        $save_array = array(
-            'course_details' => array(
-                'course_name'                    => $_POST['course_name'],
-                'course_home_url'                => $_POST['course_home_url'],
-                'default_hw_late_days'           => $_POST['default_hw_late_days'],
-                'default_student_late_days'      => $_POST['default_student_late_days'],
-                'zero_rubric_grades'             => $_POST['zero_rubric_grades'],
-                'upload_message'                 => nl2br($_POST['upload_message']),
-                'keep_previous_files'            => $_POST['keep_previous_files'],
-                'display_rainbow_grades_summary' => $_POST['display_rainbow_grades_summary'],
-                'display_custom_message'         => $_POST['display_custom_message'],
-                'course_email'                   => $_POST['course_email'],
-                'vcs_base_url'                   => $_POST['vcs_base_url'],
-                'vcs_type'                       => $_POST['vcs_type'],
-                'forum_enabled'				     => $_POST['forum_enabled'],
-                'regrade_enabled'                => $_POST['regrade_enabled'],
-                'regrade_message'                => $_POST['regrade_message'],
-                'private_repository'             => $_POST['private_repository']
-            )
-        );
+        if($name === 'upload_message') {
+            $entry = nl2br($entry);
+        }
 
-        $this->core->getConfig()->saveCourseIni($save_array);
+        $config_ini = $this->core->getConfig()->readCourseIni();
+        $config_ini['course_details'][$name] = $entry;
+        $this->core->getConfig()->saveCourseIni($config_ini['course_details']);
 
-        $this->core->addSuccessMessage("Site configuration updated");
-        $this->core->redirect($this->core->buildUrl(array('component' => 'admin',
-                                                          'page' => 'configuration',
-                                                          'action' => 'view')));
+        $response = array('status' => 'success', 'message' => 'Config updated');
+        $this->core->getOutput()->renderJson($response);
+        return $response;
     }
 }
