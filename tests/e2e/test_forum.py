db542e99a0a7680aaf75e12bddbec78cfac37bce
Gagan Kumar <gagan1kumar.cs@gmail.com>
2018-07-29 17:49:52 +0530

    Minor patch for e2e

--- tests/e2e/test_forum.py
+++ tests/e2e/test_forum.py
@@ -13,12 +13,12 @@ class TestForum(BaseTestCase):
 
     def init_and_enable_discussion(self):
         self.driver.find_element_by_id(self.get_current_semester() + '_sample').click()
-        if len(self.driver.find_elements_by_xpath("//a[contains(text(),'Discussion Forum')]")) == 0:
+        if len(self.driver.find_elements_by_xpath("//a[contains(string(),'Discussion Forum')]")) == 0:
             self.driver.find_element_by_xpath("//a[contains(text(),'Course Settings')]").click()
             self.driver.find_element_by_name("forum_enabled").click()
             self.driver.find_element_by_xpath("//button[@form = 'configForm']").click()
             self.driver.find_element_by_xpath("//a[contains(text(),'sample')]").click()
-        self.driver.find_element_by_xpath("//a[contains(text(),'Discussion Forum')]").click()
+        self.driver.find_element_by_xpath("//a[contains(string(),'Discussion Forum')]").click()
         self.forum_page_url = self.driver.current_url
 
     def switch_to_page_create_thread(self):
