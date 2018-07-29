26e14fac113a2e5670ae6aa0e4bd4b7081ca30cc
Gagan Kumar <gagan1kumar.cs@gmail.com>
2018-07-30 01:54:58 +0530

    Added migration. Recommended only for up

---
+++ migration/migrations/course/20180729161731_update_old_merged_thread.py
@@ -0,0 +1,7 @@
+def up(config, conn, semester, course):
+    with conn.cursor() as cursor:
+        cursor.execute("UPDATE threads SET deleted = false WHERE merged_thread_id <> -1")
+
+def down(config, conn, semester, course):
+    with conn.cursor() as cursor:
+        cursor.execute("UPDATE threads SET deleted = true WHERE merged_thread_id <> -1")
