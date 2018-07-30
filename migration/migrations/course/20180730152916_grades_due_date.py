ce5f0267e5c09b0ae9d932c37b773387a46c8c35
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-07-30 16:55:42 -0400

    better default for migration

--- migration/migrations/course/20180730152916_grades_due_date.py
+++ migration/migrations/course/20180730152916_grades_due_date.py
@@ -4,7 +4,7 @@ def up(config, conn, semester, course):
         colnames = [desc[0] for desc in cursor.description]
         if 'g_grade_due_date' not in colnames:
             cursor.execute('ALTER TABLE gradeable ADD COLUMN g_grade_due_date timestamp(6) with time zone')
-            cursor.execute('UPDATE gradeable SET g_grade_due_date = g_grade_start_date')
+            cursor.execute('UPDATE gradeable SET g_grade_due_date = g_grade_released_date')
             cursor.execute('ALTER TABLE gradeable ALTER COLUMN g_grade_due_date SET NOT NULL')
         
         cursor.execute('ALTER TABLE gradeable DROP CONSTRAINT g_grade_start_date')
