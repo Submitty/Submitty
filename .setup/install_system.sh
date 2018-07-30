3826fe2b8eab9f275e03ee236c75f85786cff6dd
Victor Nazzaro <victor741naruto@gmail.com>
2018-07-30 15:18:55 -0400

    init

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -613,6 +613,13 @@ if [ ${WORKER} == 0 ]; then
     fi
 fi
 
+#############
+#NPM install
+#############
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 #################################################################
 # DOCKER SETUP
 #################
