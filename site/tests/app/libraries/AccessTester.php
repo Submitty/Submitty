a6063a9705843496e01243baf95f0e99592fd5b0
Glenn Smith <couleeapps@gmail.com>
2018-07-31 18:02:04 -0400

    Quotes

--- site/tests/app/libraries/AccessTester.php
+++ site/tests/app/libraries/AccessTester.php
@@ -30,25 +30,17 @@ class AccessTester extends BaseUnitTest {
         $this->access = new Access($this->core);
     }
 
-    public function testIsGradedGradeableInGradingSections() {
-
-    }
-
-    public function testIsSectionInGradingSections() {
-
-    }
-
     public function testIsGradedGradeableInPeerAssignment() {
         $user1 = $this->createMockModel(User::class);
         $user2 = $this->createMockModel(User::class);
         $user3 = $this->createMockModel(User::class);
-        $user1->method('getId')->willReturn("user1");
-        $user2->method('getId')->willReturn("user2");
-        $user3->method('getId')->willReturn("user3");
+        $user1->method("getId")->willReturn("user1");
+        $user2->method("getId")->willReturn("user2");
+        $user3->method("getId")->willReturn("user3");
 
         /* @var MockObject $queries */
         $queries = $this->core->getQueries();
-        $queries->method('getPeerAssignment')->will(
+        $queries->method("getPeerAssignment")->will(
             $this->returnValueMap([
                 ["g1", "user1", ["user2", "user3"]],
                 ["g1", "user2", ["user1"]],
@@ -62,24 +54,24 @@ class AccessTester extends BaseUnitTest {
         $g1->method("getUser")->willReturn($user1);
         $g1->method("getTeam")->willReturn(null);
         $g1->method("getPeerGrading")->willReturn(true);
-        $g1->method('beenTAGraded')->willReturn(true);
-        $g1->method('beenAutograded')->willReturn(true);
+        $g1->method("beenTAGraded")->willReturn(true);
+        $g1->method("beenAutograded")->willReturn(true);
 
         $g2 = $this->createMockModel(Gradeable::class);
         $g2->method("getId")->willReturn("g1");
         $g2->method("getUser")->willReturn($user2);
         $g2->method("getTeam")->willReturn(null);
         $g2->method("getPeerGrading")->willReturn(true);
-        $g2->method('beenTAGraded')->willReturn(true);
-        $g2->method('beenAutograded')->willReturn(true);
+        $g2->method("beenTAGraded")->willReturn(true);
+        $g2->method("beenAutograded")->willReturn(true);
 
         $g3 = $this->createMockModel(Gradeable::class);
         $g3->method("getId")->willReturn("g1");
         $g3->method("getUser")->willReturn($user3);
         $g3->method("getTeam")->willReturn(null);
         $g3->method("getPeerGrading")->willReturn(true);
-        $g3->method('beenTAGraded')->willReturn(true);
-        $g3->method('beenAutograded')->willReturn(true);
+        $g3->method("beenTAGraded")->willReturn(true);
+        $g3->method("beenAutograded")->willReturn(true);
 
         //New model
         $su1 = new Submitter($this->core, $user1);
@@ -162,13 +154,13 @@ class AccessTester extends BaseUnitTest {
         $user1 = $this->createMockModel(User::class);
         $user2 = $this->createMockModel(User::class);
         $user3 = $this->createMockModel(User::class);
-        $user1->method('getId')->willReturn("user1");
-        $user2->method('getId')->willReturn("user2");
-        $user3->method('getId')->willReturn("user3");
+        $user1->method("getId")->willReturn("user1");
+        $user2->method("getId")->willReturn("user2");
+        $user3->method("getId")->willReturn("user3");
 
         $team1 = $this->createMockModel(Team::class);
-        $team1->method('getId')->willReturn("team1");
-        $team1->method('hasMember')->willReturnCallback(function($test) {
+        $team1->method("getId")->willReturn("team1");
+        $team1->method("hasMember")->willReturnCallback(function($test) {
             return in_array($test, ["user1", "user2"]);
         });
 
@@ -176,8 +168,8 @@ class AccessTester extends BaseUnitTest {
         $g1 = $this->createMockModel(Gradeable::class);
         $g1->method("getUser")->willReturn($user1);
         $g1->method("getTeam")->willReturn(null);
-        $g1->method('beenTAGraded')->willReturn(true);
-        $g1->method('beenAutograded')->willReturn(true);
+        $g1->method("beenTAGraded")->willReturn(true);
+        $g1->method("beenAutograded")->willReturn(true);
         self::assertTrue($this->access->isGradedGradeableByUser($g1, $user1));
         self::assertFalse($this->access->isGradedGradeableByUser($g1, $user2));
         self::assertFalse($this->access->isGradedGradeableByUser($g1, $user3));
@@ -185,8 +177,8 @@ class AccessTester extends BaseUnitTest {
         $g2 = $this->createMockModel(Gradeable::class);
         $g2->method("getUser")->willReturn($user1);
         $g2->method("getTeam")->willReturn($team1);
-        $g2->method('beenTAGraded')->willReturn(true);
-        $g2->method('beenAutograded')->willReturn(true);
+        $g2->method("beenTAGraded")->willReturn(true);
+        $g2->method("beenAutograded")->willReturn(true);
         self::assertTrue($this->access->isGradedGradeableByUser($g2, $user1));
         self::assertTrue($this->access->isGradedGradeableByUser($g2, $user2));
         self::assertFalse($this->access->isGradedGradeableByUser($g2, $user3));
@@ -212,13 +204,13 @@ class AccessTester extends BaseUnitTest {
         $user1 = $this->createMockModel(User::class);
         $user2 = $this->createMockModel(User::class);
         $user3 = $this->createMockModel(User::class);
-        $user1->method('getId')->willReturn("user1");
-        $user2->method('getId')->willReturn("user2");
-        $user3->method('getId')->willReturn("user3");
+        $user1->method("getId")->willReturn("user1");
+        $user2->method("getId")->willReturn("user2");
+        $user3->method("getId")->willReturn("user3");
 
         $team1 = $this->createMockModel(Team::class);
-        $team1->method('getId')->willReturn("team1");
-        $team1->method('hasMember')->willReturnCallback(function($test) {
+        $team1->method("getId")->willReturn("team1");
+        $team1->method("hasMember")->willReturnCallback(function($test) {
             return in_array($test, ["user1", "user2"]);
         });
 
@@ -231,8 +223,8 @@ class AccessTester extends BaseUnitTest {
         $g1 = $this->createMockModel(Gradeable::class);
         $g1->method("getUser")->willReturn($user1);
         $g1->method("getTeam")->willReturn(null);
-        $g1->method('beenTAGraded')->willReturn(true);
-        $g1->method('beenAutograded')->willReturn(true);
+        $g1->method("beenTAGraded")->willReturn(true);
+        $g1->method("beenAutograded")->willReturn(true);
         self::assertTrue($this->access->isGradedGradeableBySubmitter($g1, $su1));
         self::assertFalse($this->access->isGradedGradeableBySubmitter($g1, $su2));
         self::assertFalse($this->access->isGradedGradeableBySubmitter($g1, $su3));
@@ -241,8 +233,8 @@ class AccessTester extends BaseUnitTest {
         $g2 = $this->createMockModel(Gradeable::class);
         $g2->method("getUser")->willReturn($user1);
         $g2->method("getTeam")->willReturn($team1);
-        $g2->method('beenTAGraded')->willReturn(true);
-        $g2->method('beenAutograded')->willReturn(true);
+        $g2->method("beenTAGraded")->willReturn(true);
+        $g2->method("beenAutograded")->willReturn(true);
         self::assertTrue($this->access->isGradedGradeableBySubmitter($g2, $su1));
         self::assertTrue($this->access->isGradedGradeableBySubmitter($g2, $su2));
         self::assertFalse($this->access->isGradedGradeableBySubmitter($g2, $su3));
@@ -267,28 +259,28 @@ class AccessTester extends BaseUnitTest {
     public function testResolveNewGradeable() {
         //Ungraded old model Gradeable
         $g1 = $this->createMock(Gradeable::class);
-        $g1->method('beenTAGraded')->willReturn(false);
-        $g1->method('beenAutograded')->willReturn(false);
+        $g1->method("beenTAGraded")->willReturn(false);
+        $g1->method("beenAutograded")->willReturn(false);
         //Graded old model Gradeable
         $g2 = $this->createMock(Gradeable::class);
-        $g2->method('beenTAGraded')->willReturn(true);
-        $g2->method('beenAutograded')->willReturn(true);
+        $g2->method("beenTAGraded")->willReturn(true);
+        $g2->method("beenAutograded")->willReturn(true);
 
-        $this->assertEquals([$g1, null, null, null], $this->invokeMethod($this->access, 'resolveNewGradeable', $g1));
-        $this->assertEquals([$g2, $g2, null, null], $this->invokeMethod($this->access, 'resolveNewGradeable', $g2));
+        $this->assertEquals([$g1, null, null, null], $this->invokeMethod($this->access, "resolveNewGradeable", $g1));
+        $this->assertEquals([$g2, $g2, null, null], $this->invokeMethod($this->access, "resolveNewGradeable", $g2));
 
         //New model Gradeable and *GradedGradeable
         $ng = $this->createMock(\app\models\gradeable\Gradeable::class);
         $ngg = $this->createMock(GradedGradeable::class);
         $tgg = $this->createMock(TaGradedGradeable::class);
         $agg = $this->createMock(AutoGradedGradeable::class);
-        $ngg->method('getGradeable')->willReturn($ng);
-        $tgg->method('getGradedGradeable')->willReturn($ngg);
-        $agg->method('getGradedGradeable')->willReturn($ngg);
+        $ngg->method("getGradeable")->willReturn($ng);
+        $tgg->method("getGradedGradeable")->willReturn($ngg);
+        $agg->method("getGradedGradeable")->willReturn($ngg);
 
-        $this->assertEquals([null, null, $ng, null], $this->invokeMethod($this->access, 'resolveNewGradeable', $ng));
-        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, 'resolveNewGradeable', $ngg));
-        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, 'resolveNewGradeable', $tgg));
-        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, 'resolveNewGradeable', $agg));
+        $this->assertEquals([null, null, $ng, null], $this->invokeMethod($this->access, "resolveNewGradeable", $ng));
+        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, "resolveNewGradeable", $ngg));
+        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, "resolveNewGradeable", $tgg));
+        $this->assertEquals([null, null, $ng, $ngg], $this->invokeMethod($this->access, "resolveNewGradeable", $agg));
     }
 }
