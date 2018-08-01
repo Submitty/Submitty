a921108256e064316a119de5e080632f97a69dcd
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-08-01 12:40:50 -0400

    Removed zombie code from merge

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -173,22 +173,6 @@ class ElectronicGraderController extends GradingController {
         $this->core->getOutput()->useHeader(false);
         $this->core->getOutput()->renderOutput(array('grading', 'PDFAnnotation'), 'showAnnotationPage');
     }
-      
-    private function fetchGradeable($gradeable_id, $who_id) {
-        // TODO: this is bad, but its the only way to do it until the new model
-        $users = [$who_id];
-        $team = $this->core->getQueries()->getTeamById($who_id);
-        if ($team !== null) {
-            $users = array_merge($team->getMembers(), $users);
-        }
-        $gradeables = $this->core->getQueries()->getGradeables($gradeable_id, $users);
-        $gradeable = null;
-        foreach ($gradeables as $g) {
-            // Either this is the user requsted (non-team case) or its the gradeable instance for me or access grading
-            if ($g->getUser() === $who_id || $g->getUser()->getId() === $this->core->getUser()->getId() || $this->core->getUser()->accessGrading()) {
-                $gradeable = $g;
-                break;
-            }
 
     /**
      * Checks that a given diff viewer type is valid using DiffViewer::isValidType
