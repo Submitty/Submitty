86571504cea820b1d13f723e1cc29314a65b74f3
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-08-03 16:35:18 -0400

    Drop not null constraint on column on down
    This means that creating gradeables after rollback won't fail because of the not null constraint.

--- migration/migrations/course/20180730152916_grades_due_date.py
+++ migration/migrations/course/20180730152916_grades_due_date.py
@@ -4,8 +4,9 @@ def up(config, conn, semester, course):
         colnames = [desc[0] for desc in cursor.description]
         if 'g_grade_due_date' not in colnames:
             cursor.execute('ALTER TABLE gradeable ADD COLUMN g_grade_due_date timestamp(6) with time zone')
-            cursor.execute('UPDATE gradeable SET g_grade_due_date = g_grade_released_date')
-            cursor.execute('ALTER TABLE gradeable ALTER COLUMN g_grade_due_date SET NOT NULL')
+
+        cursor.execute('UPDATE gradeable SET g_grade_due_date = g_grade_released_date WHERE g_grade_due_date IS NULL')
+        cursor.execute('ALTER TABLE gradeable ALTER COLUMN g_grade_due_date SET NOT NULL')
         
         cursor.execute('ALTER TABLE gradeable DROP CONSTRAINT g_grade_start_date')
         cursor.execute('ALTER TABLE gradeable ADD CONSTRAINT g_grade_start_date CHECK ((g_grade_start_date <= g_grade_due_date))')
@@ -14,6 +15,7 @@ def up(config, conn, semester, course):
 
 def down(config, conn, semester, course):
     with conn.cursor() as cursor:
+        cursor.execute('ALTER TABLE gradeable ALTER COLUMN g_grade_due_date DROP NOT NULL')
         cursor.execute('ALTER TABLE gradeable DROP CONSTRAINT g_grade_due_date')
         cursor.execute('ALTER TABLE gradeable DROP CONSTRAINT g_grade_start_date')
         cursor.execute('ALTER TABLE gradeable ADD CONSTRAINT g_grade_start_date CHECK ((g_grade_start_date <= g_grade_released_date))')
