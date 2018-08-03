556723ea37b83856114858ea4481bba73b4a6b6b
Victor Nazzaro <victor741naruto@gmail.com>
2018-08-03 12:44:10 -0400

    push back download pdf

--- site/app/models/PDFGenerator.php
+++ site/app/models/PDFGenerator.php
@@ -10,9 +10,9 @@ use setasign\Fpdi\Fpdi;
 class PDFGenerator extends AbstractModel {
     public function __construct(Core $core){
         parent::__construct($core);
-        $pdf = new Fpdi();
-        $test = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'submissions/open_homework/instructor/6/upload.pdf');
-        $pageCount = $pdf->setSourceFile($test);
+//        $pdf = new Fpdi();
+//        $test = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), 'submissions/open_homework/instructor/6/upload.pdf');
+//        $pageCount = $pdf->setSourceFile($test);
 
     }
 }
