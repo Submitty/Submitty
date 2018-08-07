e8ff89fea02b3135a5ef71d451438c983bcc7ee8
Barb Cutler <bmcutler@users.noreply.github.com>
2018-08-06 23:10:12 -0400

    Debugging the installation on production machine (#2664)
    * rename
    
    * submitty_dbuser role & master database creation
    
    * user/role

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -540,8 +540,22 @@ fi
 # Create and setup database for non-workers
 if [ ${WORKER} == 0 ]; then
     dbuser_password=`cat ${SUBMITTY_INSTALL_DIR}/.setup/submitty_conf.json | jq .database_password | tr -d '"'`
-    PGPASSWORD=${dbuser_password} psql -d postgres -h localhost -U ${DB_USER} -c "CREATE DATABASE submitty"
-    python3 ${SUBMITTY_REPOSITORY}/migration/migrator.py -e master -e system migrate --initial
+
+    # create the submitty_dbuser role in postgres (if it does not yet exist
+    su postgres -c "psql -c \"DO \\\$do\\\$ BEGIN IF NOT EXISTS ( SELECT FROM  pg_catalog.pg_roles WHERE  rolname = '${DB_USER}') THEN  CREATE ROLE ${DB_USER} LOGIN PASSWORD '${dbuser_password}'; END IF; END \\\$do\\\$;\""
+
+    # check to see if a submitty master database exists
+    DB_EXISTS=`su -c 'psql -lqt | cut -d \| -f 1 | grep -w submitty' postgres`
+
+    if [ "$DB_EXISTS" == "" ]; then
+	echo "Submitty master database does not yet exist"
+	PGPASSWORD=${dbuser_password} psql -d postgres -h localhost -U ${DB_USER} -c "IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'submitty') THEN CREATE DATABASE submitty"
+	python3 ${SUBMITTY_REPOSITORY}/migration/migrator.py -e master -e system migrate --initial
+    else
+	echo "Submitty master database already exists"
+    fi
+
+
 fi
 
 echo Beginning Install Submitty Script
