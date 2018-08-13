461a06457acdafdf239c4009537101fdbbaf91de
Matthew Peveler <matt.peveler@gmail.com>
2018-08-12 23:38:37 -0400

    Add down path for git migration

--- migration/migrations/system/20180812120356_daemoncgi_group.py
+++ migration/migrations/system/20180812120356_daemoncgi_group.py
@@ -1,18 +1,22 @@
-from collections import OrderedDict
 import json
 from pathlib import Path
 from subprocess import DEVNULL, STDOUT, check_call
 
 
+DAEMONCGI_GROUP = 'submitty_daemoncgi'
+
+
 def up(config):
-    daemoncgi_group = 'submitty_daemoncgi'
-    submitty_users = Path(config.submitty['submitty_install_dir'], 'config', 'submitty_users.json')
-    with submitty_users.open() as open_file:
-        users_json = json.load(open_file, object_pairs_hook=OrderedDict)
-    if 'daemoncgi_group' not in users_json:
-        check_call(['addgroup', daemoncgi_group], stdout=DEVNULL, stderr=STDOUT)
-        check_call(['usermod', '-a', '-G', daemoncgi_group, users_json['cgi_user']])
-        check_call(['usermod', '-a', '-G', daemoncgi_group, users_json['daemon_user']])
-        users_json['daemoncgi_group'] = daemoncgi_group
-        with submitty_users.open('w') as open_file:
-            json.dump(users_json, open_file, indent=2)
+    check_call(['addgroup', DAEMONCGI_GROUP], stdout=DEVNULL, stderr=STDOUT)
+    check_call(['usermod', '-a', '-G', DAEMONCGI_GROUP, config.submitty_users['cgi_user']])
+    check_call(['usermod', '-a', '-G', DAEMONCGI_GROUP, config.submitty_users['daemon_user']])
+    config.submitty_users['daemoncgi_group'] = DAEMONCGI_GROUP
+    with Path(config.config_path, 'submitty_users.json').open('w') as open_file:
+        json.dump(config.submitty_users, open_file, indent=2)
+
+
+def down(config):
+    check_call(['groupdel', DAEMONCGI_GROUP])
+    del config.submitty_users['daemoncgi_group']
+    with Path(config.config_path, 'submitty_users.json').open('w') as open_file:
+        json.dump(config.submitty_users, open_file, indent=2)
