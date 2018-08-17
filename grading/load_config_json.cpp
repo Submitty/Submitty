03039a942fb60f17f27fa1f3287d5751623273ab
Evan Maicus <emaicus@outlook.com>
2018-08-17 11:08:21 -0400

    wip

--- grading/load_config_json.cpp
+++ grading/load_config_json.cpp
@@ -44,56 +44,74 @@ void AddAutogradingConfiguration(nlohmann::json &whole_config) {
 
 void AddDockerConfiguration(nlohmann::json &whole_config) {
   
-  if (!whole_config["docker_enabled"].is_boolean()){
-    whole_config["docker_enabled"] = false;
-  }
+  // if (!whole_config["docker_enabled"].is_boolean()){
+  //   whole_config["docker_enabled"] = false;
+  // }
+  
+  // std::cout << "docker enabled " << std::endl;
+  // nlohmann::json::iterator tc = whole_config.find("testcases");
+  // assert (tc != whole_config.end());
+  
+  // std::cout << "assertion passed " << std::endl;
 
-
-  // for each testcase{
-  //   nlohmann::json this_testcase = whole_config["testcases"][i];
-  //   std::vector<std::string> commands;
+  // int testcase_num = 0;
+  // for (typename nlohmann::json::iterator itr = tc->begin(); itr != tc->end(); itr++,testcase_num++){
+  //   std::cout << "TESTCASE " << testcase_num <<std::endl;
+  //   nlohmann::json this_testcase = whole_config["testcases"][testcase_num];
+  //   nlohmann::json commands = nlohmann::json::array();
 
   //   // if "command" exists in whole_config, we must wrap it in a container.
   //   if(this_testcase["command"]){
+  //     std::cout << "Found command " << std::endl;
   //     if (this_testcase["command"].is_array()){
+  //       std::cout << "It was an array " << std::endl;
   //       commands = this_testcase["command"];
   //     }
   //     else{
+  //       std::cout << "Pushing back. " << std::endl;
   //       commands.push_back(this_testcase["command"]);
   //     }
+  //     std::cout << "Erasing " << std::endl;
 
-  //     this_testcase.remove("command");
+  //     this_testcase.erase("command");
   //   }
 
-  //   assert (this_testcase["containers"].is_null() || commans.size() == 0);
-    
+  //   assert (this_testcase["containers"].is_null() || commands.size() == 0);
+  //   std::cout << "Passed second assertion " << std::endl;
+
   //   if(!this_testcase["containers"].is_null()){
-  //     assert(this_testcase["containers"]is_structured());
+  //     std::cout << "Found containers " << std::endl;
+  //     assert(this_testcase["containers"].is_array());
   //   }
 
   //   if(this_testcase["containers"].is_null()){
-  //     this_testcase["containers"] = json::object();
+  //     std::cout << "Didn't find containers " << std::endl;
+  //     this_testcase["containers"] = nlohmann::json::array();
+  //     std::cout << "This far " << std::endl;
   //     //commands may have to be a json::array();
-  //     this_testcase["containers"]["commands"] = commands;
+  //     this_testcase["containers"][0] = nlohmann::json::object();
+  //     this_testcase["containers"][0]["commands"] = commands;
   //   }
 
-  //   assert(this_testcase["containers"]["commands"].size() > 0);
+  //   for (int container_num = 0; container_num < this_testcase["containers"].size(); container_num++){
+  //     assert(this_testcase["containers"][container_num]["commands"].size() > 0);
 
-  //   if(this_testcase["containers"]["container_name"].is_null()){
-  //     //pad this out correctly?
-  //     this_testcase["containers"]["container_name"] = "container" + testcase_num; 
+  //     if(this_testcase["containers"][container_num]["container_name"].is_null()){
+  //       //pad this out correctly?
+  //       this_testcase["containers"][container_num]["container_name"] = "container" + testcase_num; 
+  //     }
+
+  //     if(this_testcase["containers"][container_num]["outgoing_connections"].is_null()){
+  //       this_testcase["containers"][container_num]["outgoing_connections"] = nlohmann::json::array();
+  //     }
+
+  //     if(this_testcase["containers"][container_num]["container_image"].is_null()){
+  //       //TODO: store the default system image somewhere and fill it in here.
+  //       this_testcase["containers"][container_num]["container_image"] = "ubuntu:custom";
+  //     }    
   //   }
-
-  //   if(this_testcase["containers"]["outgoing_connections"].is_null()){
-  //     this_testcase["containers"]["outgoing_connections"] = json::array();
-  //   }
-
-  //   if(this_testcase["containers"]["container_image"].is_null()){
-  //     //TODO: store the default system image somewhere and fill it in here.
-  //     this_testcase["containers"]["container_image"] = "ubuntu:custom";
-  //   }
-
-  //   whole_config["testcases"][i] = this_testcase;
+  //   std::cout << "Done " << std::endl;
+  //   whole_config["testcases"][testcase_num] = this_testcase;
   // }
   
 }
