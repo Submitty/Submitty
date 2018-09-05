a06cfe79d2edc3be8122e1f67d06dfeebb21e17f
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-09-05 13:12:33 -0400

    Migration sets dates to soft limit

--- migration/migrations/course/20180904185348_max_date_constraint.py
+++ migration/migrations/course/20180904185348_max_date_constraint.py
@@ -1,16 +1,16 @@
 def up(config, conn, semester, course):
     with conn.cursor() as cursor:
         # First, re-clamp the dates similar to the "clamp_dates" migration
-        cursor.execute("UPDATE gradeable SET g_ta_view_start_date = LEAST(g_ta_view_start_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE gradeable SET g_grade_start_date = LEAST(g_grade_start_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE gradeable SET g_grade_due_date = LEAST(g_grade_due_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE gradeable SET g_grade_released_date = LEAST(g_grade_released_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE gradeable SET g_grade_locked_date = LEAST(g_grade_locked_date, '9999-03-01 00:00:00.000000')")
+        cursor.execute("UPDATE gradeable SET g_ta_view_start_date = LEAST(g_ta_view_start_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE gradeable SET g_grade_start_date = LEAST(g_grade_start_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE gradeable SET g_grade_due_date = LEAST(g_grade_due_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE gradeable SET g_grade_released_date = LEAST(g_grade_released_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE gradeable SET g_grade_locked_date = LEAST(g_grade_locked_date, '9999-02-01 00:00:00.000000')")
 
-        cursor.execute("UPDATE electronic_gradeable SET eg_team_lock_date = LEAST(eg_team_lock_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE electronic_gradeable SET eg_submission_open_date = LEAST(eg_submission_open_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE electronic_gradeable SET eg_submission_due_date = LEAST(eg_submission_due_date, '9999-03-01 00:00:00.000000')")
-        cursor.execute("UPDATE electronic_gradeable SET eg_regrade_request_date = LEAST(eg_regrade_request_date, '9999-03-01 00:00:00.000000')")
+        cursor.execute("UPDATE electronic_gradeable SET eg_team_lock_date = LEAST(eg_team_lock_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE electronic_gradeable SET eg_submission_open_date = LEAST(eg_submission_open_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE electronic_gradeable SET eg_submission_due_date = LEAST(eg_submission_due_date, '9999-02-01 00:00:00.000000')")
+        cursor.execute("UPDATE electronic_gradeable SET eg_regrade_request_date = LEAST(eg_regrade_request_date, '9999-02-01 00:00:00.000000')")
 
         # Now, add constraints to prevent them from being this high again
         # Note: due to existing constraints, only a few of the columns need the new constraint
