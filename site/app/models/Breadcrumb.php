195fe256b8f121b6d89c615b72383e9e4388cdd6
Matthew Peveler <matt.peveler@gmail.com>
2018-09-14 13:43:33 -0400

    Add tests for the breadcrumb model

--- site/app/models/Breadcrumb.php
+++ site/app/models/Breadcrumb.php
@@ -4,37 +4,32 @@ namespace app\models;
 use app\libraries\Core;
 
 /**
- * Breadcrumb header navigation item
+ * Breadcrumb navigation item
  *
- * @method string getString()
- * @method null getUrl()
- * @method bool isTop()
- * @method bool isIcon()
+ * @method string getTitle()
+ * @method string|null getUrl()
+ * @method bool isExternalLink()
 */
 class Breadcrumb extends AbstractModel {
-    /** @property string $title */
+    /** @property string */
     protected $title;
-    /** @property null $url */
+    /** @property string|null */
     protected $url = null;
-    /** @property bool $top */
-    protected $top = false;
-    /** @property bool $icon */
-    protected $icon = false;
+    /** @property string|null */
+    protected $external_url = false;
 
     /**
      * Breadcrumb constructor.
      * @param Core $core
      * @param string $title
-     * @param null $url
-     * @param bool $top
-     * @param bool $icon
+     * @param string|null $url
+     * @param string|null $external_url
      */
-    public function __construct(Core $core, string $title, $url = null, bool $top = false, bool $icon = false) {
+    public function __construct(Core $core, string $title, $url = null, $external_url = null) {
         parent::__construct($core);
         $this->title = $title;
         $this->url = $url;
-        $this->top = $top;
-        $this->icon = $icon;
+        $this->external_url = $external_url;
     }
 
 }
