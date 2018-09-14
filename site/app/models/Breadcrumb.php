2ecfbd2c2c27dc91159fc1958f209d816895a2f7
Matthew Peveler <matt.peveler@gmail.com>
2018-09-14 13:59:00 -0400

    Update documentation on model

--- site/app/models/Breadcrumb.php
+++ site/app/models/Breadcrumb.php
@@ -6,9 +6,17 @@ use app\libraries\Core;
 /**
  * Breadcrumb navigation item
  *
+ * These are used to give a user a sense of where they are in the hierarchy of
+ * the site. Each breadcrumb has a title, and then optionally a link and an
+ * external link. The links are used to provide an anchor tag on the title, while
+ * if there's an external link, then it shows a fa-external-link icon that has
+ * an anchor tag pointing to the external url.
+ *
+ * @link https://fontawesome.com/v4.7.0/icon/external-link
+ *
  * @method string getTitle()
  * @method string|null getUrl()
- * @method bool isExternalLink()
+ * @method string|null getExternalLink()
 */
 class Breadcrumb extends AbstractModel {
     /** @property string */
