329c1a983708ca5597ba383914e32f76fed3c860
andrewaikens87 <andrewaikens87@gmail.com>
2018-09-24 10:34:37 -0400

    Remove column creation

--- migration/migrations/course/20180920191016_add_anon_notifications.py
+++
@@ -1,6 +0,0 @@
-def up(config, conn, semester, course):
-	with conn.cursor() as cursor:
-		cursor.execute('ALTER TABLE notifications ADD COLUMN IF NOT EXISTS anonymous BOOLEAN DEFAULT FALSE NOT NULL')
-
-def down(config, conn, semester, course):
-	pass
