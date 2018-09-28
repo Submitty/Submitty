6d5b8846a21a7608aec7b114b206dccf1880118e
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-09-28 10:42:11 -0400

    Mark 'publish' setting properly extracted in grade mode
    While this value can't change, since we store the model in the DOM, we must be able to reliably fetch the data

--- site/public/js/ta-grading-rubric.js
+++ site/public/js/ta-grading-rubric.js
@@ -1232,7 +1232,7 @@ function getMarkFromDOM(mark_id) {
             points: parseFloat(domElement.find('input[type=number]').val()),
             title: domElement.find('input[type=text]').val(),
             deleted: domElement.hasClass('mark-deleted'),
-            publish: domElement.find('.mark-publish input[type=checkbox]').is(':checked')
+            publish: domElement.find('.mark-publish-container input[type=checkbox]').is(':checked')
         };
     } else {
         if (mark_id === 0) {
@@ -1242,7 +1242,7 @@ function getMarkFromDOM(mark_id) {
             id: parseInt(domElement.attr('data-mark_id')),
             points: parseFloat(domElement.find('.mark-points').attr('data-points')),
             title: domElement.find('.mark-title').attr('data-title'),
-            publish: domElement.attr('data-publish')
+            publish: domElement.attr('data-publish') === 'true',
         };
     }
 }
