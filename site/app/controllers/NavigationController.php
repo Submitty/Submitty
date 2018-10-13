4d6c25c7056cfb484c996d5e7956e36d6a5eb9f7
Kevin Mackenzie <dev.kjmack305@gmail.com>
2018-10-12 20:56:55 -0400

    Better Electronic Gradeable Presets and Settings (#2833)
    * Updated model, migration, db loading/saving
    
    * Fixed missing placeholder
    
    * Updated Create page
    
    * Added scanned exam to page 2
    
    * Better defaults for student permissions
    
    * Improved auto-setting feature
    
    * Better button behavior
    
    * Front-page changes with scanned exam option
    
    * Improved spacing
    
    * Updated course tables
    
    * change wording (more edits to come)
    
    * more work-in-progress edits
    
    * Improved saving message responsiveness
    
    * Initial work towards ui changes
    
    * Updated boolean conversion in controller
    
    * Fixed preset settings
    
    * Hide backing inputs
    
    * Updated labeling for new layout
    
    * Initial work on student view after grades
    
    * Fixed student view after grades assignment
    
    * Imroved style of 'Autograding' tab
    
    * Misc display and layout fixes
    
    * Indicate non-team-assignments are so
    
    * Correct autograding config for scanned exam
    
    * Navigation filters view-after-grades-released
    
    * Adjusted student view permissions in case of url hacking
    
    * Added row to course_tables.sql
    
    * Fixed behavior of radio buttons & saving
    
    * Code consolidation
    
    * Add print to debug travis
    
    * Fix missing `eg_` prefix
    
    * Remove debug print from test_basecase.py
    
    * Scanned Exam default allows download active version

--- site/app/controllers/NavigationController.php
+++ site/app/controllers/NavigationController.php
@@ -117,6 +117,11 @@ class NavigationController extends AbstractController {
             return false;
         }
 
+        // if student view is true and they can only view after grades are released, filter appropriately
+        if ($gradeable->isStudentView() && $gradeable->isStudentViewAfterGrades() && !$user->accessGrading()) {
+            return $gradeable->isTaGradeReleased();
+        }
+
         //If we're not instructor and this is not open to TAs
         $date = $this->core->getDateTimeNow();
         if ($gradeable->getTaViewStartDate() > $date && !$user->accessAdmin()) {
