0dc583d9f0866fbbfe489a3513f28b5cdc366507
pbailie <pbailie@users.noreply.github.com>
2018-10-18 18:40:43 -0400

    Changes to be committed: modified:   migration/data/submitty_db.sql modified:   migration/migrations/master/20181011175809_reg_section_sync.py
    WIP

--- migration/data/submitty_db.sql
+++ migration/data/submitty_db.sql
@@ -281,15 +281,13 @@ DECLARE
     db_conn VARCHAR;
     query_string TEXT;
 BEGIN
-    FOR registration_row IN SELECT semester, course FROM courses_registration_sections WHERE registration_section_id=NEW.registration_section_id LOOP
-        db_conn := format('dbname=submitty_%s_%s', registration_row.semester, registration_row.course);
-        query_string := 'INSERT INTO sections_registration VALUES(' || quote_literal(NEW.registration_section_id) || ') ON CONFLICT DO NOTHING';
-        -- Need to make sure that query_string was set properly as dblink_exec will happily take a null and then do nothing
-        IF query_string IS NULL THEN
-            RAISE EXCEPTION 'query_string error in trigger function sync_insert_registration_section()';
-        END IF;
-        PERFORM dblink_exec(db_conn, query_string);
-    END LOOP;
+    db_conn := format('dbname=submitty_%s_%s', NEW.semester, NEW.course);
+    query_string := 'INSERT INTO sections_registration VALUES(' || quote_literal(NEW.registration_section_id) || ') ON CONFLICT DO NOTHING';
+    -- Need to make sure that query_string was set properly as dblink_exec will happily take a null and then do nothing
+    IF query_string IS NULL THEN
+        RAISE EXCEPTION 'query_string error in trigger function sync_insert_registration_section()';
+    END IF;
+    PERFORM dblink_exec(db_conn, query_string);
 
     -- All done.
     RETURN NULL;
@@ -303,22 +301,20 @@ DECLARE
     db_conn VARCHAR;
     query_string TEXT;
 BEGIN
-    FOR registration_row IN SELECT semester, course FROM courses_registration_sections WHERE registration_section_id=OLD.registration_section_id LOOP
-        db_conn := format('dbname=submitty_%s_%s', registration_row.semester, registration_row.course);
-        query_string := 'DELETE FROM sections_registration WHERE sections_registration_id = ' || quote_literal(OLD.registration_section_id);
-        -- Need to make sure that query_string was set properly as dblink_exec will happily take a null and then do nothing
-        IF query_string IS NULL THEN
-            RAISE EXCEPTION 'query_string error in trigger function sync_delete_registration_section()';
-        END IF;
-        PERFORM dblink_exec(db_conn, query_string, true);
-    END LOOP;
+    db_conn := format('dbname=submitty_%s_%s', OLD.semester, OLD.course);
+    query_string := 'DELETE FROM sections_registration WHERE sections_registration_id = ' || quote_literal(OLD.registration_section_id);
+    -- Need to make sure that query_string was set properly as dblink_exec will happily take a null and then do nothing
+    IF query_string IS NULL THEN
+        RAISE EXCEPTION 'query_string error in trigger function sync_delete_registration_section()';
+    END IF;
+    PERFORM dblink_exec(db_conn, query_string);
 
     -- All done.  As this is a BEFORE DELETE trigger, RETURN OLD allows original triggering DELETE query to proceed.
     RETURN OLD;
 
 -- Trying to delete a registration section while users are still enrolled will raise an integrity constraint violation exception.
 -- We should catch this exception and stop execution with no rows processed.
--- No rows processed will indicate to the UsersController that there was a problem.
+-- No rows processed will indicate to the UsersController that deletion had an error and did not occur.
 EXCEPTION WHEN integrity_constraint_violation THEN
     RAISE NOTICE 'Users are still enrolled in registration section ''%''', OLD.registration_section_id;
     -- Return NULL so we do not proceed with original triggering DELETE query.
@@ -332,5 +328,5 @@ CREATE TRIGGER user_sync_courses_users AFTER INSERT OR UPDATE ON courses_users F
 CREATE TRIGGER user_sync_users AFTER UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE sync_user();
 
 -- INSERT and DELETE triggers for syncing registration sections happen on different instances of TG_WHEN (after vs before).
-CREATE TRIGGER insert_registration_sync_registration_id AFTER INSERT ON courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE sync_insert_registration_section();
-CREATE TRIGGER delete_registration_sync_registration_id BEFORE DELETE ON courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE sync_delete_registration_section();
+CREATE TRIGGER insert_sync_registration_id AFTER INSERT OR UPDATE ON courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE sync_insert_registration_section();
+CREATE TRIGGER delete_sync_registration_id BEFORE DELETE ON courses_registration_sections FOR EACH ROW EXECUTE PROCEDURE sync_delete_registration_section();
