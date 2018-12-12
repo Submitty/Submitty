7c2d2ceab0d14f329469daabf2c3b16fce354826
Kasra Sheik <sheikk@rpi.edu>
2018-12-11 19:42:56 -0500

    testing send mail without the prepended note

--- sbin/sendEmail.py
+++ sbin/sendEmail.py
@@ -76,9 +76,14 @@ def sendAnnouncement():
 	print("Attempting to Send an Email Announcement. Course: {}, Semester: {}, Announcement Title: {}".format(course, semester, thread_title))
 
 	class_list = getClassList(semester, course)
+	# class_list = ['sheikk@rpi.edu']
 	emailCount = 0 
 	for student_email in class_list:
-		announcement_email = constructAnnouncementEmail(student_email, thread_title, thread_content)
+		announcement_email = constructMailString(student_email, thread_title, thread_content)
+		# print("email sender: " + str(EMAIL_SENDER))
+		# print("email_to: " + str(student_email))
+		# print("announcement_email: " + str(announcement_email))
+
 		mail_client.sendmail(EMAIL_SENDER, student_email, announcement_email)
 
 		#Sleep if we reach a certain sending threshold
