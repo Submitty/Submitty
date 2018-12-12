b34ac230c66b82526b058e2c87b56ee06e19d2f6
Kasra Sheik <sheikk@rpi.edu>
2018-12-12 15:55:03 -0500

    Send Email on Instructor Forum Announcement  (#3136)
    * basic email functionality on instructor announcements
    
    * Adding email sending functionality to submitty job queue
    
    * Adding send email radio button and more descriptive logging
    
    * clean up for PR
    
    * clean up for PR
    
    * Adding naive solution to batch bulk sends
    
    * pushing updated config
    
    * tweak
    
    * update send email script permissions
    
    * attempting to fix malfromed email string
    
    * attempting to fix malfromed email string part 2
    
    * attempting to fix malfromed email string part - removing unneeded print statements
    
    * attempting to fix malfromed email string part - adding class list
    
    * testing send mail without the prepended note
    
    * fixing null group section and adding malformed email string

--- sbin/submitty_daemon_jobs/submitty_jobs/jobs.py
+++ sbin/submitty_daemon_jobs/submitty_jobs/jobs.py
@@ -58,6 +58,7 @@ class DeleteLichenResult(AbstractJob):
         course = self.job_details['course']
         gradeable = self.job_details['gradeable']
 
+
         lichen_output = os.path.join(DATA_DIR, 'courses', semester, course, 'lichen', 'lichen_job_output.txt')
 
         with open(lichen_output, "w") as output_file:
@@ -70,3 +71,20 @@ class DeleteLichenResult(AbstractJob):
             subprocess.call("rm"+ " -rf /var/local/submitty/courses/" +semester+"/"+course+ "/lichen/matches/"+gradeable, stdout=output_file, stderr=output_file, shell=True)
             subprocess.call("echo"+ " Deleted lichen plagiarism results and saved config for "+gradeable, stdout=op, stderr=op,shell=True)
 
+
+class SendEmail(AbstractJob):
+    def run_job(self):
+        email_type = self.job_details['email_type']
+        semester = self.job_details['semester']
+        course = self.job_details['course']
+
+        email_script = '/usr/local/submitty/sbin/sendEmail.py'
+
+        thread_title = self.job_details['thread_title']
+        thread_content = self.job_details['thread_content']
+
+        try:
+            with open('email_job_logs.txt', "a") as output_file:
+                subprocess.call([email_script, email_type, semester, course, thread_title, thread_content], stdout=output_file)
+        except PermissionError:
+            print ("error, could not open "+output_file+" for writing")
