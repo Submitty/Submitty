fd6ead59cc68652552414939f9b07a9a46818af5
Barb Cutler <bmcutler@users.noreply.github.com>
2019-02-07 13:49:21 -0500

    Reorganize installation location of java tools jars & install soot (#3259)
    * reorganize installation location of java tools jars & install soot
    
    * typo
    
    * fix travis
    
    * specific older version of soot

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -357,9 +357,9 @@ fi
 echo -e "Build the junit test runner"
 
 # copy the file from the repo
-rsync -rtz ${SUBMITTY_REPOSITORY}/junit_test_runner/TestRunner.java ${SUBMITTY_INSTALL_DIR}/JUnit/TestRunner.java
+rsync -rtz ${SUBMITTY_REPOSITORY}/junit_test_runner/TestRunner.java ${SUBMITTY_INSTALL_DIR}/java_tools/JUnit/TestRunner.java
 
-pushd ${SUBMITTY_INSTALL_DIR}/JUnit > /dev/null
+pushd ${SUBMITTY_INSTALL_DIR}/java_tools/JUnit > /dev/null
 # root will be owner & group of the source file
 chown  root:root  TestRunner.java
 # everyone can read this file
