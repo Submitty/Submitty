6955b47e8f57f77bb48622144d7e9383fa0d27ad
Shail Patel <shailpatel67@gmail.com>
2019-02-12 23:26:45 -0500

    Add Bulk Upload QR suffix and handle QR URLs (#3288)
    * initial work
    
    * handle URLs

--- site/app/controllers/student/SubmissionController.php
+++ site/app/controllers/student/SubmissionController.php
@@ -546,13 +546,13 @@ class SubmissionController extends AbstractController {
         // Open a cURL connection
         $semester = $this->core->getConfig()->getSemester();
         $course = $this->core->getConfig()->getCourse();
-        $qr_prefix = $_POST['qr_prefix'];
-
+        $qr_prefix = rawurlencode($_POST['qr_prefix']);
+        $qr_suffix = rawurlencode($_POST['qr_suffix']);
         $ch = curl_init();
         if($_POST['use_qr_codes'] === "false"){
             curl_setopt($ch, CURLOPT_URL, $this->core->getConfig()->getCgiUrl()."pdf_check.cgi?&num={$num_pages}&sem={$semester}&course={$course}&g_id={$gradeable_id}&ver={$current_time}");
         }else{
-            curl_setopt($ch, CURLOPT_URL, $this->core->getConfig()->getCgiUrl()."pdf_check_qr.cgi?&sem={$semester}&course={$course}&g_id={$gradeable_id}&ver={$current_time}&qr_prefix={$qr_prefix}");
+            curl_setopt($ch, CURLOPT_URL, $this->core->getConfig()->getCgiUrl()."pdf_check_qr.cgi?&sem={$semester}&course={$course}&g_id={$gradeable_id}&ver={$current_time}&qr_prefix={$qr_prefix}&qr_suffix={$qr_suffix}");
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
