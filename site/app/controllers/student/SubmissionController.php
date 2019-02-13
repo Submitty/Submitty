193067f110090b56a0cf70a63b04123f13e879a2
Matthew Peveler <matt.peveler@gmail.com>
2019-02-13 09:54:49 -0500

    Make recommended changes

--- site/app/controllers/student/SubmissionController.php
+++ site/app/controllers/student/SubmissionController.php
@@ -788,15 +788,14 @@ class SubmissionController extends AbstractController {
         }
 
         $upload_time_string_tz = $timestamp . " " . $this->core->getConfig()->getTimezone()->getName();
-       
-        $bulk_upload_data = array("submit_timestamp" =>  $current_time_string_tz,
-                                  "upload_timestamp" =>  $upload_time_string_tz,
-                                  "filepath" => $uploaded_file);
         
-        $bulk_upload_data_json = json_encode($bulk_upload_data, JSON_PRETTY_PRINT);
-        
-        if (!@file_put_contents(FileUtils::joinPaths($version_path, "bulk_upload_data.json"), $bulk_upload_data_json)) { 
+        $bulk_upload_data = [
+            "submit_timestamp" =>  $current_time_string_tz,
+            "upload_timestamp" =>  $upload_time_string_tz,
+            "filepath" => $uploaded_file
+        ];
 
+        if (FileUtils::writeJsonFile(FileUtils::joinPaths($version_path, "bulk_upload_data.json"), $bulk_upload_data)) {
             return $this->uploadResult("Failed to create bulk upload file for this submission.", false);
         }
         
