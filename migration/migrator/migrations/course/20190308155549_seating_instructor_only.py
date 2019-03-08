7215dd78bce7860a2aea942997edd2be3d569f51
Matthew Peveler <matt.peveler@gmail.com>
2019-03-08 15:56:50 -0500

    Add migration for seating only for instructors

---
+++ migration/migrator/migrations/course/20190308155549_seating_instructor_only.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+import configparser
+
+
+def up(config, database, semester, course):
+    course_dir = Path(config.submitty['submitty_data_dir'], 'courses', semester, course)
+    config_file = Path(course_dir, 'config', 'config.ini')
+
+    if config_file.is_file():
+        config = configparser.ConfigParser()
+        config.read(str(config_file))
+
+        if not config.has_option('course_details', 'seating_only_for_instructor'):
+            config.set('course_details', 'seating_only_for_instructor', 'false')
+
+        with open(str(config_file), 'w') as configfile:
+            config.write(configfile)
