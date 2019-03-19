2d4eec6e6ad446f0a7653677925fd668ebb84e82
Leon Montealegre <leonm99@gmail.com>
2019-03-19 05:54:40 -0400

    Updated migration

--- migration/migrator/migrations/course/20190211143300_vcs_host_type.py
+++ migration/migrator/migrations/course/20190211143300_vcs_host_type.py
@@ -1,8 +1,6 @@
-def up(config, conn, semester, course):
-    with conn.cursor() as cursor:
-        cursor.execute("ALTER TABLE electronic_gradeable ADD COLUMN eg_vcs_host_type INTEGER DEFAULT 0")
+def up(config, database, semester, course):
+    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN eg_vcs_host_type INTEGER DEFAULT 0")
 
 
-def down(config, conn, semester, course):
-    with conn.cursor() as cursor:
-        cursor.execute("ALTER TABLE ONLY electronic_gradeable DROP COLUMN eg_vcs_host_type")
+def down(config, database, semester, course):
+    database.execute("ALTER TABLE ONLY electronic_gradeable DROP COLUMN eg_vcs_host_type")
