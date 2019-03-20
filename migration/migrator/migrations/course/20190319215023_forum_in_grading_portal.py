c2fa5b3f71454c8c8349cc7d7bfb6a06626373e1
andrewaikens87 <andrewaikens87@gmail.com>
2019-03-19 21:58:43 -0400

    fix table

--- migration/migrator/migrations/course/20190319215023_forum_in_grading_portal.py
+++ migration/migrator/migrations/course/20190319215023_forum_in_grading_portal.py
@@ -1,6 +1,6 @@
 def up(config, database, semester, course):
-    database.execute("ALTER TABLE gradeable ADD COLUMN IF NOT EXISTS thread_ids json DEFAULT '{}' NOT NULL")
-    database.execute("ALTER TABLE gradeable ADD COLUMN IF NOT EXISTS has_discussion boolean DEFAULT FALSE NOT NULL")
+    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN IF NOT EXISTS thread_ids json DEFAULT '{}' NOT NULL")
+    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN IF NOT EXISTS has_discussion boolean DEFAULT FALSE NOT NULL")
 
 def down(config, database, semester, course):
     pass
