5239c5e2da039eb25dad1bc51afbef5887d08ed7
zjxiaohan <zjchacha@qq.com>
2019-03-22 13:20:42 -0700

    fix tooltip of sidebar

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -2594,6 +2594,9 @@ $(document).ready(function() {
         position: { my: "right+0 bottom+0" },
         content: function () {
             if($("#sidebar").hasClass("collapsed")) {
+                if ($(this).attr("title") === "Collapse Sidebar") {
+                    return "Expand Sidebar";
+                }
                 return $(this).attr("title")
             }
             else {
@@ -2601,7 +2604,6 @@ $(document).ready(function() {
             }
         }
     });
-    $("#nav-sidebar-collapse.collapse-icon").attr("title", "Expand Sidebar");
 
     //Remember sidebar preference
     if (localStorage.sidebar !== "") {
