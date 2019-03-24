9512657b4f9b069082d80b69d1c76c24298b2581
emaicus <emaicus@outlook.com>
2019-03-24 10:08:54 -0400

    more flake updates

--- sbin/restart_shipper_and_all_workers.py
+++ sbin/restart_shipper_and_all_workers.py
@@ -5,12 +5,12 @@ shippers in the correct order.
 '''
 import time
 import subprocess
-import os 
+import os
 from os import path
 import sys
 
 SYSTEMCTL_WRAPPER_SCRIPT = path.join(path.dirname(path.realpath(__file__)),
-                                      'shipper_utils','systemctl_wrapper.py')
+  'shipper_utils', 'systemctl_wrapper.py')
 
 if __name__ == '__main__':
 
@@ -21,34 +21,33 @@ if __name__ == '__main__':
     delay_in_seconds = 5
     print('Stopping the local shipper daemon...')
     subprocess.call(["python3", SYSTEMCTL_WRAPPER_SCRIPT, "stop",
-                      "--daemon", "shipper", "--target", "primary"])
+                    "--daemon", "shipper", "--target", "primary"])
 
     print('Stopping all local worker daemon...')
     subprocess.call(["python3", SYSTEMCTL_WRAPPER_SCRIPT, "stop",
-                        "--daemon", "worker", "--target", "primary"])
+                    "--daemon", "worker", "--target", "primary"])
 
     print('Stopping all remote worker daemons...')
     cmd = 'python3 {0} stop --daemon worker --target perform_on_all_workers'
-                                            .format(SYSTEMCTL_WRAPPER_SCRIPT)
+          .format(SYSTEMCTL_WRAPPER_SCRIPT)
 
     subprocess.call(["su", "-", "submitty_daemon", "-c",  cmd])
 
     print('Delaying {0} seconds to allow the system to stabilize...'
-                                            .format(delay_in_seconds))
+          .format(delay_in_seconds))
     time.sleep(delay_in_seconds)
 
     print('Starting the local shipper daemon...')
     subprocess.call(["python3", SYSTEMCTL_WRAPPER_SCRIPT, "start", "--daemon",
-                                              "shipper", "--target", "primary"])
-
+                    "shipper", "--target", "primary"])
 
     print('Starting the local worker daemon...')
     subprocess.call(["python3", SYSTEMCTL_WRAPPER_SCRIPT, "start", "--daemon",
-                                               "worker", "--target", "primary"])
+                    "worker", "--target", "primary"])
 
     print('Starting all worker daemons...')
     cmd = 'python3 {0} start --daemon worker --target perform_on_all_workers'
-                                              .format(SYSTEMCTL_WRAPPER_SCRIPT)
+          .format(SYSTEMCTL_WRAPPER_SCRIPT)
 
     subprocess.call(["su", "-", "submitty_daemon", "-c", cmd])
 
