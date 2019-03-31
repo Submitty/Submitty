cd8cedc02cddac79ae50db7bdbf5fa88f28370ff
Kasra Sheik <sheikk@rpi.edu>
2019-03-31 15:16:31 -0400

    resolving requested changes

--- site/app/controllers/admin/EmailRoomSeatingController.php
+++ site/app/controllers/admin/EmailRoomSeatingController.php
@@ -9,8 +9,8 @@ use app\libraries\FileUtils;
 
 
 class EmailRoomSeatingController extends AbstractController {
-	const DEFAULT_EMAIL_SUBJECT = '[Submitty {$course_name}]: Seating Assignment for {$gradeable_id}';
-	const DEFAULT_EMAIL_BODY =
+    const DEFAULT_EMAIL_SUBJECT = '[Submitty {$course_name}]: Seating Assignment for {$gradeable_id}';
+    const DEFAULT_EMAIL_BODY =
 'Hello,
 
 Listed below is your seating assignment for the upcoming exam {$gradeable_id} on {$exam_date} at {$exam_time}.
@@ -23,89 +23,79 @@ Seat: {$exam_seat}
 
 Please email your instructor with any questions or concerns.';
 
- public function __construct(Core $core) {
+    public function __construct(Core $core) {
         parent::__construct($core);
     }
 
-	public function run() {
+    public function run() {
 
-		switch($_REQUEST['action']) {
-				case 'send_email':
-						$this->emailSeatingAssignments();
-				case 'show_page':
+        switch($_REQUEST['action']) {
+		        case 'send_email':
+                    $this->emailSeatingAssignments();
+                    break;
 				default:
-						$this->renderEmailTemplate();
-						break;
-		}
-	}
+                    $this->renderEmailTemplate();
+                    break;
+        }
+    }
 
-	private function renderEmailTemplate(){
-		$this->core->getOutput()->renderOutput(array('admin', 'EmailRoomSeating'), 'displayPage', EmailRoomSeatingController::DEFAULT_EMAIL_SUBJECT, EmailRoomSeatingController::DEFAULT_EMAIL_BODY);
-	}
+    private function renderEmailTemplate(){
+        $this->core->getOutput()->renderOutput(array('admin', 'EmailRoomSeating'), 'displayPage', EmailRoomSeatingController::DEFAULT_EMAIL_SUBJECT, EmailRoomSeatingController::DEFAULT_EMAIL_BODY);
+    }
+    
+    public function emailSeatingAssignments() {
+        $seating_assignment_subject = $_POST["room_seating_email_subject"];
+        $seating_assignment_body = $_POST["room_seating_email_body"];
 
-	public function emailSeatingAssignments() {
-		$seating_assignment_subject = $_POST["room_seating_email_subject"];
-		$seating_assignment_body = $_POST["room_seating_email_body"];
+        $gradeable_id = $this->core->getConfig()->getRoomSeatingGradeableId();
+        $course =  $this->core->getConfig()->getCourse();
+        $seating_assignments_path = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "reports", "seating", $gradeable_id);
 
-		$gradeable_id = $this->core->getConfig()->getRoomSeatingGradeableId();
-		$course =  $this->core->getConfig()->getCourse();
-		$seating_assignments_path = FileUtils::joinPaths($this->core->getConfig()->getCoursePath(), "reports", "seating", $gradeable_id);
+        $classList = $this->core->getQueries()->getClassEmailListWithIds();
 
-		$classList = $this->core->getQueries()->getClassEmailListWithIds();
+        foreach($classList as $user) {
+            $user_id = $user['user_id'];
+            $user_email = $user['user_email'];
 
-		foreach($classList as $user) {
-			$user_id = $user['user_id'];
-			$user_email = $user['user_email'];
+            $room_seating_file = FileUtils::joinPaths($seating_assignments_path, "$user_id.json");
+            $room_seating_json = FileUtils::readJsonFile($room_seating_file);
 
-			$room_seating_file = FileUtils::joinPaths($seating_assignments_path, "$user_id.json");
-			$room_seating_json = FileUtils::readJsonFile($room_seating_file);
+            if($room_seating_json === false){
+                continue;
+            }
 
-			if($room_seating_json === false){
-				continue;
-			}
-
-			$email_data = [
-						"subject" => $this->replaceSeatingAssignmentMessagePlaceholders($seating_assignment_subject, $room_seating_json),
+            $email_data = [
+                "subject" => $this->replaceSeatingAssignmentMessagePlaceholders($seating_assignment_subject, $room_seating_json),
 						"body" => $this->replaceSeatingAssignmentMessagePlaceholders($seating_assignment_body, $room_seating_json)
-				];
+            ];
 
-				$this->core->getQueries()->createEmail($email_data, $user_email);
-		}
+            $this->core->getQueries()->createEmail($email_data, $user_email);
+        }
 
-			$this->core->addSuccessMessage("Seating assignments have been sucessfully emailed!");
-			return $this->core->redirect($this->core->buildUrl());
-	}
+        $this->core->addSuccessMessage("Seating assignments have been sucessfully emailed!");
+        return $this->core->redirect($this->core->buildUrl());
+    }
 
-	private function replaceSeatingAssignmentMessagePlaceholders($seatingAssignmentMessage, $seatingAssignmentData) {
-		if(array_key_exists("gradeable", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$gradeable_id}', $seatingAssignmentData["gradeable"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("date", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_date}', $seatingAssignmentData["date"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("time", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_time}', $seatingAssignmentData["time"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("building", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_building}', $seatingAssignmentData["building"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("room", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_room}', $seatingAssignmentData["room"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("zone", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_zone}', $seatingAssignmentData["zone"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("row", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_row}', $seatingAssignmentData["row"], $seatingAssignmentMessage);
-		}
-		if(array_key_exists("seat", $seatingAssignmentData)){
-			$seatingAssignmentMessage = str_replace('{$exam_seat}', $seatingAssignmentData["seat"], $seatingAssignmentMessage);
-		}
+    private function replaceSeatingAssignmentMessagePlaceholders($seatingAssignmentMessage, $seatingAssignmentData) {
+        
+        $replaces = [
+            'gradeable' => 'gradeable_id',
+            'date' => 'exam_date',
+            'time' => 'exam_time',
+            'building' => 'exam_building',
+            'room' => 'exam_room',
+            'zone' => 'exam_zone',
+            'row' => 'exam_row',
+            'seat' => 'exam_seat',
+        ];   
 
-		$seatingAssignmentMessage = str_replace('{$course_name}', $this->core->getConfig()->getCourse(), $seatingAssignmentMessage);
-		return $seatingAssignmentMessage;
-	}
+        foreach($replaces as $key => $variable) {
+        	if(isset($seatingAssignmentData[$key])) {
+        		$seatingAssignmentMessage = str_replace('{$' . $variable . '}', $seatingAssignmentData[$key], $seatingAssignmentMessage); 
+        	}
+        }
 
-
+        return $seatingAssignmentMessage; 
+    }
 
 }
--- site/app/views/admin/EmailRoomSeatingView.php
+++ site/app/views/admin/EmailRoomSeatingView.php
@@ -3,7 +3,6 @@
 namespace app\views\admin;
 
 use app\views\AbstractView;
-//TODO: add this as a general view in the future..
 class EmailRoomSeatingView extends AbstractView {
     public function displayPage($defaultSubject, $defaultBody) {
         return $this->core->getOutput()->renderTwigTemplate("admin/EmailRoomSeating.twig", [
