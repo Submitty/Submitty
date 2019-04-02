a921a7168da0dbc8caa9e8a0eb4e1eccf9e6e583
Matthew Peveler <matt.peveler@gmail.com>
2019-04-02 09:09:14 -0400

    Fix detecting config path in run_migrator

--- migration/run_migrator.py
+++ migration/run_migrator.py
@@ -1,9 +1,9 @@
 """Run the migrator tool through its CLI."""
 from pathlib import Path
 import sys
-from migrator import get_dir_path, cli
+from migrator import cli
 
 if __name__ == '__main__':
-    config_path = Path(get_dir_path(), '..', '..', '..', 'config')
+    config_path = Path(Path(__file__).parent.resolve(), '..', '..', '..', 'config')
     config_path = config_path.resolve() if config_path.exists() else None
     cli.run(sys.argv[1:], config_path)
