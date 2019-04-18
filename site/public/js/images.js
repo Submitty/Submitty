f8d491b6ce01d12e8cc184f614135953eacf0714
Fenn-CS <fenn25.fn@gmail.com>
2019-04-18 14:49:25 +0100

    enhanced to used unique but fixed name harses for each student
    Signed-off-by: Fenn-CS <fenn25.fn@gmail.com>

--- site/public/js/images.js
+++ site/public/js/images.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
 
 $('.no-image-available').each(function(i, obj) {
+	var student_name = $(obj).data('student-name');
+	// Identicon requires a harsh of at least 15 chars
+	// So to prevent short names from breaking it, the name is salted.
+	// This would ensure resulting harshes are longer than 15 chars.
+	student_name = student_name+'0123456789_abcdefghijklmnopqrstuvwxyz';
     // create a base64 encoded PNG
-    var data = new Identicon(randomString(32), 420).toString();
-    // write to a data URI
+    var data = new Identicon(uniqueHarsh(student_name), 420).toString();
+    // update no image tag.
     $(obj).replaceWith('<img width=210 height=210 src="data:image/png;base64,' + data + '">');
 });
 
-function randomString(len, charSet) {
-    charSet = charSet || '0123456789abcdefghijklmn0123456789opqrstuvwxyz0123456789';
-    var randomString = '';
-    for (var i = 0; i < len; i++) {
-        var randomPoz = Math.floor(Math.random() * charSet.length);
-        randomString += charSet.substring(randomPoz,randomPoz+1);
-    }
-    return randomString;
+function uniqueHarsh(string) {
+    return btoa(string);
 }
 
 });
