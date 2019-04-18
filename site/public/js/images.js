c2f49ed0ff42351c10320160ba202b1db7d42be0
Fenn-CS <fenn25.fn@gmail.com>
2019-04-18 15:17:35 +0100

    code structured enhanced, spellings corrected
    Signed-off-by: Fenn-CS <fenn25.fn@gmail.com>

--- site/public/js/images.js
+++ site/public/js/images.js
@@ -2,18 +2,14 @@ $(document).ready(function(){
 
 $('.no-image-available').each(function(i, obj) {
 	var student_name = $(obj).data('student-name');
-	// Identicon requires a harsh of at least 15 chars
+	// Identicon requires a hash of at least 15 chars
 	// So to prevent short names from breaking it, the name is salted.
-	// This would ensure resulting harshes are longer than 15 chars.
+	// This would ensure resulting hashes are longer than 15 chars.
 	student_name = student_name+'0123456789_abcdefghijklmnopqrstuvwxyz';
     // create a base64 encoded PNG
-    var data = new Identicon(uniqueHarsh(student_name), 420).toString();
+    var data = new Identicon(btoa(student_name), 210).toString();
     // update no image tag.
     $(obj).replaceWith('<img width=210 height=210 src="data:image/png;base64,' + data + '">');
 });
-
-function uniqueHarsh(string) {
-    return btoa(string);
-}
 
 });
