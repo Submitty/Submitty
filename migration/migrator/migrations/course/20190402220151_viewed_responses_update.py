94408921bf5b753a06a9456ebbc27439c21fac75
Matthew Peveler <matt.peveler@gmail.com>
2019-04-24 22:50:25 -0400

    Update 20190402220151_viewed_responses_update.py

--- migration/migrator/migrations/course/20190402220151_viewed_responses_update.py
+++ migration/migrator/migrations/course/20190402220151_viewed_responses_update.py
@@ -1,4 +1,5 @@
 def up(config, database, semester, course):
+    database.execute("DELETE FROM viewed_responses AS a USING viewed_responses AS b WHERE a.timestamp < b.timestamp AND a.thread_id = b.thread_id AND a.user_id = b.user_id")
     database.execute("ALTER TABLE viewed_responses DROP CONSTRAINT IF EXISTS viewed_responses_pkey")
     database.execute("ALTER TABLE viewed_responses ADD PRIMARY KEY(thread_id, user_id)")
     pass
