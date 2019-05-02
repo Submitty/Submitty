a21f9ddbfe82c32f3202a4b7478fa5428a328f93
Andrew Aikens <andrewaikens87@gmail.com>
2019-05-02 14:46:37 -0400

    Fix typo
    Fix typo

--- migration/migrator/migrations/course/20190415092504_null_email_address.py
+++ migration/migrator/migrations/course/20190415092504_null_email_address.py
@@ -1,5 +1,5 @@
 def up(config, database, semester, course):
-    database.excecute("ALTER TABLE only users ALTER COLUMN user_email DROP NOT NULL")
+    database.execute("ALTER TABLE only users ALTER COLUMN user_email DROP NOT NULL")
     pass
 
 
