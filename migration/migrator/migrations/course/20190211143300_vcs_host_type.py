7cdf58cf445f01459d0a45cdb0120f8a0200fc21
Barb Cutler <bmcutler@gmail.com>
2019-05-07 16:37:24 -0400

    migrations shouldnt destroy data

--- migration/migrator/migrations/course/20190211143300_vcs_host_type.py
+++ migration/migrator/migrations/course/20190211143300_vcs_host_type.py
@@ -1,6 +1,6 @@
 def up(config, database, semester, course):
-    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN eg_vcs_host_type INTEGER DEFAULT 0")
+    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN IF NOT EXISTS eg_vcs_host_type INTEGER DEFAULT 0")
 
 
 def down(config, database, semester, course):
-    database.execute("ALTER TABLE ONLY electronic_gradeable DROP COLUMN eg_vcs_host_type")
+    pass
