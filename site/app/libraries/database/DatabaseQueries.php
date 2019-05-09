5aaa2020ee99f8284f434c15520b805050b8f5ff
Victor Li <victorli718@gmail.com>
2019-05-10 07:30:38 +0800

    Enter excused absense by date (relative to due date) (#3505)
    * show due days on excused extension
    
    * Add Date Picker
    
    * Fix small syntax
    
    * Update Extensions.twig
    
    * Update server.js
    
    * Small syntax changes
    
    * little changes to query
    
    * Un-rename function
    
    * Negative
    
    * Added dateClear when inserting days or switching gradeables
    
    * flatpickr in vendor folder

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -1682,10 +1682,11 @@ WHERE gcm_id=?", $params);
      */
     public function getAllElectronicGradeablesIds() {
         $this->course_db->query("
-          SELECT g_id, g_title
-          FROM gradeable INNER JOIN electronic_gradeable USING (g_id)
-          WHERE eg_scanned_exam=FALSE and eg_has_due_date=TRUE
-          ORDER BY eg_submission_due_date ASC
+            SELECT gradeable.g_id, g_title, eg_submission_due_date
+            FROM gradeable INNER JOIN electronic_gradeable 
+                ON gradeable.g_id = electronic_gradeable.g_id
+            WHERE g_gradeable_type=0 and eg_scanned_exam=FALSE and eg_has_due_date=TRUE
+            ORDER BY g_grade_released_date DESC
         ");
         return $this->course_db->rows();
     }
