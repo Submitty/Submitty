f32f1e1d148741d704d2ed122518e7fcfb56fef0
sak6lab <sdargula@gmail.com>
2019-06-11 12:53:24 -0400

    removed pass from migration file

--- migration/migrator/migrations/course/20190608032611_grade_inquiry_with_timezone.py
+++ migration/migrator/migrations/course/20190608032611_grade_inquiry_with_timezone.py
@@ -4,10 +4,8 @@
 def up(config, database, semester, course):
     database.execute("ALTER TABLE regrade_discussion ALTER COLUMN timestamp TYPE TIMESTAMP WITH TIME ZONE;")
     database.execute("ALTER TABLE regrade_requests ALTER COLUMN timestamp TYPE TIMESTAMP WITH TIME ZONE;")
-    pass
 
 
 def down(config, database, semester, course):
     database.execute("ALTER TABLE regrade_discussion ALTER COLUMN timestamp TYPE TIMESTAMP;")
     database.execute("ALTER TABLE regrade_requests ALTER COLUMN timestamp TYPE TIMESTAMP;")
-    pass
