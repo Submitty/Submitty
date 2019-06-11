bfc5c91b36548816bad13d9d3090dc578e387038
sak6lab <sdargula@gmail.com>
2019-06-11 10:21:19 -0400

    changed regrade_discussion's and regrade_requests' timestamp column to timestamp with timezone #3594 and #3137

---
+++ migration/migrator/migrations/course/20190608032611_grade_inquiry_with_timezone.py
@@ -0,0 +1,13 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    database.execute("ALTER TABLE regrade_discussion ALTER COLUMN timestamp TYPE TIMESTAMP WITH TIME ZONE;")
+    database.execute("ALTER TABLE regrade_requests ALTER COLUMN timestamp TYPE TIMESTAMP WITH TIME ZONE;")
+    pass
+
+
+def down(config, database, semester, course):
+    database.execute("ALTER TABLE regrade_discussion ALTER COLUMN timestamp TYPE TIMESTAMP;")
+    database.execute("ALTER TABLE regrade_requests ALTER COLUMN timestamp TYPE TIMESTAMP;")
+    pass
