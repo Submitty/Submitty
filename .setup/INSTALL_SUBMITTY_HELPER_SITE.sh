f413e9b2b12213982ca9d5c038776df7561725f3
Barb Cutler <bmcutler@users.noreply.github.com>
2019-06-18 16:22:45 -0400

    zone images is deprecated

--- .setup/INSTALL_SUBMITTY_HELPER_SITE.sh
+++ .setup/INSTALL_SUBMITTY_HELPER_SITE.sh
@@ -109,13 +109,6 @@ cp -R "${NODE_FOLDER}/@submitty/pdf-annotate.js/dist" ${VENDOR_FOLDER}/pdf-annot
 mkdir ${VENDOR_FOLDER}/twigjs
 cp ${NODE_FOLDER}/twig/twig.min.js ${VENDOR_FOLDER}/twigjs/
 
-# TEMPORARY (until we have generalized code for generating charts in html)
-# copy the zone chart images
-mkdir -p ${SUBMITTY_INSTALL_DIR}/site/public/zone_images/
-if [ -d ${SUBMITTY_INSTALL_DIR}/zone_images ]; then
-    cp ${SUBMITTY_INSTALL_DIR}/zone_images/* ${SUBMITTY_INSTALL_DIR}/site/public/zone_images/
-fi
-
 # set the permissions of all files
 # $PHP_USER can read & execute all directories and read all files
 # "other" can cd into all subdirectories
