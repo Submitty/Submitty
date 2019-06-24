cb28f28c110d967db4ec9e729fef1ed91a401e5f
Kevin-MM <kevin.m.mckinney@gmail.com>
2019-06-24 16:27:41 -0400

    Created bulk of rainbow grades automation script

--- sbin/auto_rainbow_grades.py
+++ sbin/auto_rainbow_grades.py
@@ -4,44 +4,70 @@ import os
 import shutil
 
 # Constants
-RAINBOW_GRADES_DIR = "/usr/local/submitty/GIT_CHECKOUT/RainbowGrades"
+RAINBOW_GRADES_PATH = '/usr/local/submitty/GIT_CHECKOUT/RainbowGrades'
+COURSES_PATH = '/var/local/submitty/courses'
 
 # Collect path information
-courses_path = "/var/local/submitty/courses"
 semester = sys.argv[1]
 course = sys.argv[2]
+user = sys.argv[3]
 
 # TODO: Validate passed in parameters
 
-# Get contents of grades reports directory
-reports_path = os.path.join(courses_path, semester, course, "reports", "all_grades")
-reports = os.listdir(reports_path)
+# Generate path information
+rg_course_path = os.path.join(COURSES_PATH, semester, course, 'rainbow_grades')
 
-# Ensure the grades report directory isn't empty
-# If it is empty then grade reports haven't been generated and rainbow grades cannot run
-if not reports:
-    print("The grade reports directory was empty")
-    print("Ensure grade reports have been generated before attempting to run this script")
-    exit("Exiting...")
+# Setup course specific rainbow grades directory if ones does not exist
+if not os.path.exists(rg_course_path):
 
-# Auto rainbow grades files should reside inside
-# /var/local/submitty/courses/<semester>/<course>/uploads/auto_rainbow_grades
-auto_path = os.path.join(courses_path, semester, course, "uploads", "auto_rainbow_grades")
+    # Create the rainbow grades directory for the course
+    print('Creating new directory: {}'.format(rg_course_path))
+    os.mkdir(rg_course_path, 0o770)
 
-# If this directory exists then remove it
-if os.path.exists(auto_path):
+    # Set folder owner
+    print('Setting ownership to {}'.format(user))
+    shutil.chown(rg_course_path, user)
 
-    # Directory found remove it
-    print("Found old auto_rainbow_grades directory")
-    print("Deleting {}".format(auto_path))
-    shutil.rmtree(auto_path)
+    # Copy Makefile and customization.json file from master rainbow grades directory to course specific directory
+    print('Copying initial files')
+    shutil.copyfile(RAINBOW_GRADES_PATH + '/SAMPLE_Makefile', rg_course_path + '/Makefile')
+    shutil.copyfile(RAINBOW_GRADES_PATH + '/SAMPLE_customization.json', rg_course_path + '/customization.json')
 
-# Create the auto_rainbow_grades directory for the course
-print("Creating new directory: {}".format(auto_path))
-os.mkdir(auto_path, 0o770)
+    # Setup Makefile
+    print('Configuring Makefile')
+    makefile_path = os.path.join(rg_course_path, 'Makefile')
 
-raw_data_path = os.path.join(auto_path, "raw_data")
+    # Read in the file
+    with open(makefile_path, 'r') as file:
+        filedata = file.read()
 
-# Copy grade reports directory to auto_rainbow_grades (rename as raw_data)
-# shutil.copytree(reports_path, )
+    # Replace the target string
+    filedata = filedata.replace('username', user)
+    filedata = filedata.replace('/<PATH_TO_SUBMITTY_REPO>/RainbowGrades', RAINBOW_GRADES_PATH)
+    filedata = filedata.replace('submitty.cs.rpi.edu', 'localhost')
+    filedata = filedata.replace('<SEMESTER>/<COURSE>', '{}/{}'.format(semester, course))
+
+    # Write the file out again
+    with open(makefile_path, 'w') as file:
+        file.write(filedata)
+
+# Change directory to course specific directory
+os.chdir(rg_course_path)
+
+# Run make pull_test (command outputs capture in cmd_output for debugging)
+print('Pulling in grade reports')
+cmd_output = os.popen('make pull_test').read()
+
+# Run make
+print('Compiling rainbow grades')
+cmd_output = os.popen('make').read()
+
+# Run make push_test
+print('Exporting to summary_html')
+cmd_output = os.popen('make push_test').read()
+
+# Change more file permissions
+print('Setting ownership of all rainbow grades files to {}'.format(user))
+os.chdir('..')
+cmd_output = os.popen('chown {}:sample_tas_www rainbow_grades/ -R'.format(user)).read()
 
