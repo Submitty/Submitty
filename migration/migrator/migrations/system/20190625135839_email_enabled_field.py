80e3b14638dd1512edb5efbcaa9c43ff90cbeb30
sak6lab <sdargula@gmail.com>
2019-06-25 17:53:02 -0400

    removed chmods from migration

--- migration/migrator/migrations/system/20190625135839_email_enabled_field.py
+++ migration/migrator/migrations/system/20190625135839_email_enabled_field.py
@@ -9,8 +9,6 @@ import os
 
 def up(config):
     email_filename = str(Path(config.submitty['submitty_install_dir'], 'config', 'email.json'))
-    # change file permissions so root can read and write
-    #os.chmod(email_filename, 0o740)
     # read json and add email_enabled field
     with open(email_filename,'r') as open_file:
         email_json = json.load(open_file)
@@ -20,8 +18,6 @@ def up(config):
     # write file again with new json
     with open(email_filename, 'w') as open_file:
         json.dump(email_json, open_file, indent=2)
-    #change file permissions back so root can only read
-    #os.chmod(email_filename, 0o440)
 
 # no need for down as email_enabled is not used in previous builds
 def down(config):
