10e5fce1bcfb89e63aa442ddced5481a2e6e1433
Kevin-MM <kevin.m.mckinney@gmail.com>
2019-06-28 10:37:32 -0400

    Did work on auto_rainbow_grades script and moved the code that generates the rainbow_grades folder from an old migrator to a new one.

---
+++ migration/migrator/migrations/course/20190628100043_create_rainbow_grades_dir.py
@@ -0,0 +1,30 @@
+import os
+import grp
+from pathlib import Path
+
+
+def up(config, conn, semester, course):
+    course_dir = Path(config.submitty['submitty_data_dir'], 'courses', semester, course)
+    course_customization_dir = Path(course_dir, 'rainbow_grades')
+
+    # create the directories
+    os.makedirs(str(course_customization_dir), exist_ok=True)
+
+    php_user = config.submitty_users['php_user']
+
+    # get course group
+    stat_info = os.stat(str(course_dir))
+    course_group_id = stat_info.st_gid
+    course_group = grp.getgrgid(course_group_id)[0]
+
+    # set the owner/group/permissions
+    os.system("chown -R "+php_user+":"+course_group+" "+str(course_customization_dir))
+    os.system("chmod -R u+rwx  "+str(course_customization_dir))
+    os.system("chmod -R g+rwxs "+str(course_customization_dir))
+    os.system("chmod -R o-rwx  "+str(course_customization_dir))
+
+    pass
+
+
+def down(config, conn, semester, course):
+    pass
