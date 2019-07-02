577fbbaae5ff51fac594910f98e9a0790d6e8cae
Evan Maicus <emaicus@outlook.com>
2019-07-02 09:49:34 -0400

    wip

--- json_schemas/sample_unit_tests.py
+++ json_schemas/sample_unit_tests.py
@@ -1,45 +1,34 @@
 import unittest
 import schema_validator
+import traceback
 import json
 import os
 
 # TODO: ADD pip3 install jsonschema migration!
 
+test_directories = ['tutorial_configs', 'more_autograding_examples']
 
 with open('complete_config_json_schema.json', 'r') as infile:
     SCHEMA = json.load(infile)
 
-test_files = [
-    'complete_config_01_simple_python',
-    'complete_config_02_simple_cpp',
-    'complete_config_03_multipart',
-    'complete_config_04_python_static_analysis',
-    'complete_config_05_cpp_static_analysis',
-    'complete_config_06_loop_types',
-    'complete_config_07_loop_depth',
-    'complete_config_08_memory_debugging',
-    'complete_config_09_java_testing',
-    'complete_config_10_java_coverage',
-    'complete_config_11_resources',
-    'complete_config_12_system_calls',
-    'complete_config_13_cmake_compilation'
-]
-
 
 class TestSchema(unittest.TestCase):
-    def test_validate_schema(self):
-        for test_file in test_files:
-            with self.subTest(msg='Testing {0}'.format(test_file),
-                              test_file=test_file):
-                p = os.path.join('tutorial_configs',
-                                 '{0}.json'.format(test_file))
-                with open(p, 'r') as infile:
-                    config_json = json.load(infile)
-                try:
-                    schema_validator.complete_config_validator(config_json,
-                                                               SCHEMA)
-                    success = True
-                except Exception as e:
-                    print(e)
-                    success = False
-                self.assertTrue(success)
+    def test_tutorial_configs(self):
+        for directory in test_directories:
+            for file in os.listdir(directory):
+                test_file = os.path.join(directory, file)
+                print('{0}: '.format(test_file))
+                print()
+                with self.subTest(msg='Testing {0}'.format(test_file),
+                                  test_file=test_file):
+                    with open(test_file, 'r') as infile:
+                        config_json = json.load(infile)
+                    try:
+                        schema_validator.complete_config_validator(config_json,
+                                                                   SCHEMA, show_warnings=False)
+                        success = True
+                    except Exception:
+                        traceback.print_exc()
+                        success = False
+                    self.assertTrue(success)
+                print()
--- json_schemas/schema_validator.py
+++ json_schemas/schema_validator.py
@@ -15,24 +15,28 @@ def parse_arguments():
                         help='The path to the configuration to be validated')
     parser.add_argument('--schema', required=True,
                         help='The path to the schema to be validated against')
+    parser.add_argument('-w', '--show_warnings', required=False,
+                        action='store_true', help='Display warnings')
     return parser.parse_args()
 
 
-def complete_config_validator(j_, s_):
+def complete_config_validator(j_, s_, show_warnings=True):
     """
     Given a complete config and a complete config schema, validate
     the complete config piecemeal
     """
+    warn = show_warnings
     s_prop = s_['properties']
-    submitty_validate_schema(j_, s_prop, 'autograding_options', 'global')
-    submitty_validate_schema(j_, s_prop, 'autograding_method', 'global')
-    submitty_validate_schema(j_, s_prop, 'container_options', 'global')
-    submitty_validate_schema(j_, s_prop, 'resource_limits', 'global')
+    validate_schema(j_, s_prop, 'autograding', 'global', warn=warn)
+    validate_schema(j_, s_prop, 'autograding_method', 'global', warn=warn)
+    validate_schema(j_, s_prop, 'container_options', 'global', warn=warn)
+    validate_schema(j_, s_prop, 'resource_limits', 'global', warn=warn)
 
     # test all of the global pieces of the config.
     testcases = j_["testcases"]
     p_s = s_['definitions']['testcase']['properties']
     sub_limit_schema = s_['definitions']['submission_limit']
+    filecheck_schema = s_['definitions']['filecheck']
     c_schema = s_['definitions']['container']
     abs_v_schema = s_['definitions']['abstract_validation_object']
     spec_v_schema = s_['definitions']['validator_definitions']
@@ -40,53 +44,60 @@ def complete_config_validator(j_, s_):
     testcase_num = 1
     # validate each testcase one piece at a time.
     for t in testcases:
-        try:
-            # A hack because a testcase can be either a submission_limit or a
-            # regular testcase. We check sub-limit first and short circuit.
-            submitty_validate_schema(t, sub_limit_schema, required=False)
-            testcase_num += 1
+        # A hack because a testcase can be either a submission_limit, a
+        # filecheck, or a regular testcase. We check sub-limit first and
+        # short circuit.
+        success = False
+        for extra_schema in [sub_limit_schema, filecheck_schema]:
+            try:
+                validate_schema(t, extra_schema, required=False, warn=warn)
+                testcase_num += 1
+                success = True
+            except Exception:
+                pass
+        if success:
             continue
-        except Exception:
-            pass
 
         # validate all of the different chunks of the testcase
         t_name = 'testcase_{0}'.format(testcase_num)
-        submitty_validate_schema(t, p_s, 'dispatcher_actions', t_name)
-        submitty_validate_schema(t, p_s, 'actions', t_name)
-        submitty_validate_schema(t, p_s, 'points', t_name)
-        submitty_validate_schema(t, p_s, 'type', t_name)
-        submitty_validate_schema(t, p_s, 'pre_commands', t_name)
-        submitty_validate_schema(t, p_s, 'single_port_per_container', t_name)
-        submitty_validate_schema(t, p_s, 'use_router', t_name)
-        submitty_validate_schema(t, p_s, 'title', t_name)
-
+        validate_schema(t, p_s, 'dispatcher_actions', t_name, warn=warn)
+        validate_schema(t, p_s, 'actions', t_name, warn=warn)
+        validate_schema(t, p_s, 'points', t_name, warn=warn)
+        validate_schema(t, p_s, 'type', t_name, warn=warn)
+        validate_schema(t, p_s, 'pre_commands', t_name, warn=warn)
+        validate_schema(t, p_s, 'single_port_per_container', t_name, warn=warn)
+        validate_schema(t, p_s, 'use_router', t_name, warn=warn)
+        validate_schema(t, p_s, 'title', t_name, warn=warn)
         containers = t['containers']
         # validate each container in the testcase
         for c in containers:
-            submitty_validate_schema(c, c_schema, prefix=t_name)
+            validate_schema(c, c_schema, prefix=t_name, warn=warn)
 
         validators = t['validation']
         validator_num = 0
         # Validate each validation object in the testcase
         for v in validators:
-            submitty_validate_schema(v, abs_v_schema, prefix=t_name)
-            submitty_validate_schema(v, spec_v_schema, prefix=t_name)
+            validate_schema(v, abs_v_schema, prefix=t_name, warn=warn)
+            validate_schema(v, spec_v_schema, prefix=t_name, warn=warn)
             validator_num += 1
 
         testcase_num += 1
     # Finally, validate the config as a whole.
-    submitty_validate_schema(j_, s_, prefix='whole schema')
+    validate_schema(j_, s_, prefix='whole schema', warn=warn)
 
 
-def submitty_validate_schema(j_, s_, key='', prefix='', required=True):
+def validate_schema(j_, s_, key='', prefix='', required=True, warn=False):
     """
     We validate a key within a config and a schema.
     If no key is given, we evaluate the whole config or schema.
     """
     descriptive_title = '{0} {1}'.format(prefix, key)
-    my_json_chunk = j_.get(key, None) if j_ is not None else j_
-    my_schema = s_.get(key, None) if s_ is not None else s_
-
+    if key != '':
+        my_json_chunk = j_.get(key, None) if j_ is not None else j_
+        my_schema = s_.get(key, None) if s_ is not None else s_
+    else:
+        my_json_chunk = j_
+        my_schema = s_
     # If attempting to validate an item not specified in the schema, fail.
     if my_schema is None:
         raise Exception(("ERROR: There is no specification for "
@@ -94,27 +105,23 @@ def submitty_validate_schema(j_, s_, key='', prefix='', required=True):
                          "add a specification.").format(key))
 
     if my_json_chunk is None:
-        print(('WARNING: could not identify ',
-               '{0} ({1})'.format(descriptive_title, key)))
+        if warn:
+            print(("WARNING: could not identify "
+                   "{0} ({1})").format(descriptive_title, key))
         return
     try:
         # validate.
         validate(instance=my_json_chunk, schema=my_schema)
     except Exception:
-        # If the item was required, print an error. Otherwise,
-        # the exception will be suppressed later, so don't be loud.
-        if required:
-            traceback.print_exc()
-        if descriptive_title != '':
-            print(('ERROR: {0} was not '.format(descriptive_title),
-                   'properly formatted.'.format(descriptive_title)))
+        if descriptive_title.strip() != '':
+            print(('ERROR: {0} was not '
+                   'properly formatted.').format(descriptive_title))
         raise
 
 
 def main():
 
     args = parse_arguments()
-
     with open(args.schema, 'r') as infile:
         schema = json.load(infile)
 
@@ -122,13 +129,13 @@ def main():
         config_json = json.load(infile)
 
     try:
-        complete_config_validator(config_json, schema)
+        complete_config_validator(config_json, schema,
+                                  show_warnings=args.show_warnings)
         print()
-        print('SUCCESS: your configuration conforms to the Submitty\
-              configuration JSON schema.')
-    except Exception as e:
-        print('ERROR: {0}'.format(e))
-        pass
+        print(("SUCCESS: your configuration conforms to the Submitty"
+               "configuration JSON schema."))
+    except Exception:
+        traceback.print_exc()
 
 
 if __name__ == '__main__':
