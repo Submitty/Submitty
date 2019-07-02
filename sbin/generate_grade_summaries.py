adca635abbe6c019c6c5c03ddaec0ae07d542706
zjxiaohan <zjxiaohan@gmail.com>
2019-07-02 14:28:22 +0800

    lint fix

--- sbin/generate_grade_summaries.py
+++ sbin/generate_grade_summaries.py
@@ -9,7 +9,9 @@ Usage:
 import argparse
 import requests
 
+
 def main():
+    """Automatically call Generate Grade Summaries API"""
     parser = argparse.ArgumentParser(
         description='Automatically call Generate Grade Summaries API'
     )
@@ -21,7 +23,10 @@ def main():
     token = args.token
 
     courses = list()
-    course_response = requests.get('{}/api/courses'.format(base_url), headers={'Authorization': token})
+    course_response = requests.get(
+        '{}/api/courses'.format(base_url),
+        headers={'Authorization': token}
+    )
     if course_response.status_code == 200:
         course_response = course_response.json()
         if course_response["status"] == 'success':
@@ -34,15 +39,26 @@ def main():
         exit(-1)
 
     for course in courses:
-        grade_generation_response = requests.post('{}/api/{}/{}/reports/summaries'.format(base_url, course['semester'], course['title']), headers={'Authorization': token})
+        grade_generation_response = requests.post(
+            '{}/api/{}/{}/reports/summaries'.format(base_url, course['semester'], course['title']),
+            headers={'Authorization': token}
+        )
         if grade_generation_response.status_code == 200:
             grade_generation_response = grade_generation_response.json()
             if grade_generation_response["status"] == 'success':
-                print("Successfully generated rainbow grades for {}.{}".format(course['semester'], course['title']))
+                print("Successfully generated rainbow grades for {}.{}".format(
+                    course['semester'],
+                    course['title']
+                ))
             else:
-                print("ERROR: Failed to generate rainbow grades for {}.{}.\nReason:{}".format(course['semester'], course['title'], grade_generation_response["message"]))
+                print("ERROR: Failed to generate rainbow grades for {}.{}.\nReason:{}".format(
+                    course['semester'],
+                    course['title'],
+                    grade_generation_response["message"]
+                ))
         else:
             print("ERROR: Submitty Service Unavailable.")
+
 
 if __name__ == "__main__":
     main()
