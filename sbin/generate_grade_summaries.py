7e1459603bf955f600106378645b9123cd364e3c
zjxiaohan <zjxiaohan@gmail.com>
2019-07-03 23:33:48 +0800

    generate grade reports for only one course

--- sbin/generate_grade_summaries.py
+++ sbin/generate_grade_summaries.py
@@ -3,11 +3,12 @@
 Script to trigger the generate grade summaries.
 
 Usage:
-./generate_grade_summaries.py <base_url> <token>
+./generate_grade_summaries.py <semester> <course> <base_url> <token>
 """
 
 import argparse
 import requests
+from sys import stderr
 
 
 def main():
@@ -15,49 +16,43 @@ def main():
     parser = argparse.ArgumentParser(
         description='Automatically call Generate Grade Summaries API.'
     )
+    parser.add_argument('semester')
+    parser.add_argument('course')
     parser.add_argument('base_url')
     parser.add_argument('token')
     args = parser.parse_args()
 
+    semester = args.semester
+    course = args.course
     base_url = args.base_url.rstrip('/')
     token = args.token
 
-    courses = list()
-    course_response = requests.get(
-        '{}/api/courses'.format(base_url),
-        headers={'Authorization': token}
-    )
-    if course_response.status_code == 200:
-        course_response = course_response.json()
-        if course_response["status"] == 'success':
-            courses = course_response['data']['unarchived_courses']
-        else:
-            print("ERROR: {}".format(course_response["message"]))
-            exit(-1)
-    else:
-        print("ERROR: Submitty Service Unavailable.")
-        exit(-1)
-
-    for course in courses:
+    try:
         grade_generation_response = requests.post(
             '{}/api/{}/{}/reports/summaries'.format(
-                base_url, course['semester'], course['title']
+                base_url, semester, course
             ),
             headers={'Authorization': token}
         )
-        if grade_generation_response.status_code == 200:
-            grade_generation_response = grade_generation_response.json()
-            if grade_generation_response["status"] == 'success':
-                print("Successfully generated grade reports for {}.{}".format(
-                    course['semester'], course['title']
-                ))
-            else:
-                print("ERROR: Failed to generate grade reports for {}.{}.".format(
-                    course['semester'], course['title'],
-                ))
-                print("Reason:{}".format(grade_generation_response["message"]))
+    except:
+        print("ERROR: Invalid arguments.", file=stderr)
+        exit(-1)
+
+    if grade_generation_response.status_code == 200:
+        grade_generation_response = grade_generation_response.json()
+        if grade_generation_response["status"] == 'success':
+            print("Successfully generated grade reports for {}.{}".format(
+                semester, course
+            ))
         else:
-            print("ERROR: Submitty Service Unavailable.")
+            print("ERROR: Failed to generate grade reports for {}.{}.".format(
+                semester, course
+            ), file=stderr)
+            print("Reason:{}".format(
+                grade_generation_response["message"]
+            ), file=stderr)
+    else:
+        print("ERROR: Submitty Service Unavailable.", file=stderr)
 
 
 if __name__ == "__main__":
