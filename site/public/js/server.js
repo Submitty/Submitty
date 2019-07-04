5ac0870b9d0bac6991405a9e6c719bd275914c52
Xiao Han <zjchacha@qq.com>
2019-07-05 04:50:05 +0800

    [Router] New URL for CourseMaterialsController (#3962)
    * New URL for CourseMaterialsController
    
    * combine folder and non-folder deletion functions

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -320,7 +320,8 @@ function displayCloseSubmissionsWarning(form_action,gradeable_name) {
     form.css("display", "block");
 }
 
-function newDeleteCourseMaterialForm(form_action, file_name) {
+function newDeleteCourseMaterialForm(path, file_name) {
+    let url = buildNewCourseUrl(["course_materials", "delete"]) + "?path=" + path;
     var current_y_offset = window.pageYOffset;
     document.cookie = 'jumpToScrollPostion='+current_y_offset;
 
@@ -341,7 +342,7 @@ function newDeleteCourseMaterialForm(form_action, file_name) {
     var form = $("#delete-course-material-form");
     $('[name="delete-course-material-message"]', form).html('');
     $('[name="delete-course-material-message"]', form).append('<b>'+file_name+'</b>');
-    $('[name="delete-confirmation"]', form).attr('action', form_action);
+    $('[name="delete-confirmation"]', form).attr('action', url);
     form.css("display", "block");
 }
 
@@ -1247,7 +1248,7 @@ function downloadFileWithAnyRole(file_name, path) {
 }
 
 function downloadCourseMaterialZip(dir_name, path) {
-    window.location = buildUrl({'component': 'grading', 'page': 'course_materials', 'action': 'download_course_material_zip', 'dir_name': dir_name, 'path': path});
+    window.location = buildNewCourseUrl(['course_materials', 'download_zip']) + '?dir_name=' + dir_name + '&path=' + path;
 }
 
 function checkColorActivated() {
@@ -1678,7 +1679,7 @@ function escapeHTML(str) {
 
 function changePermission(filename, checked) {
     // send to server to handle file permission change
-    var url = buildUrl({'component': 'grading', 'page': 'course_materials', 'action': 'modify_course_materials_file_permission', 'filename': encodeURIComponent(filename), 'checked': checked});
+    let url = buildNewCourseUrl(['course_materials', 'modify_permission']) + '?filename=' + encodeURIComponent(filename) + '&checked=' + checked;
 
     $.ajax({
         url: url,
@@ -1691,7 +1692,7 @@ function changePermission(filename, checked) {
 
 function changeNewDateTime(filename, newdatatime) {
     // send to server to handle file permission change
-    var url = buildUrl({'component': 'grading', 'page': 'course_materials', 'action': 'modify_course_materials_file_time_stamp', 'filename': encodeURIComponent(filename), 'newdatatime': encodeURIComponent(newdatatime)});
+    let url = buildNewCourseUrl(['course_materials', 'modify_timestamp']) + '?filename=' + encodeURIComponent(filename) + '&newdatatime=' + newdatatime;
 
     $.ajax({
         url: url,
