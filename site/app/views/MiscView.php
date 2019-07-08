3102674dbfc055d56cf22e1f1f67860f10a4f7d7
mktung <32647488+mktung@users.noreply.github.com>
2019-07-08 17:17:21 -0400

    [Bugfix:TAGrading] Fixed css and js link in code.twig (#4023)

--- site/app/views/MiscView.php
+++ site/app/views/MiscView.php
@@ -11,20 +11,25 @@ class MiscView extends AbstractView {
     }
 
     public function displayCode($file_type, $filename, $file_contents) {
-        $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('codemirror', 'codemirror.css'));
-        $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('codemirror', 'theme', 'eclipse.css'));
-        $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('codemirror', 'theme', 'monokai.css'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('jquery', 'jquery.min.js'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('codemirror', 'codemirror.js'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('codemirror', 'mode', 'clike', 'clike.js'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('codemirror', 'mode', 'python', 'python.js'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('codemirror', 'mode', 'shell', 'shell.js'));
+        $code_css = array();
+        $code_css[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'codemirror.css'), 'vendor');
+        $code_css[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'theme', 'eclipse.css'), 'vendor');
+        $code_css[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'theme', 'monokai.css'), 'vendor');
+        
+        $code_js = array();
+        $code_js[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('jquery', 'jquery.min.js'), 'vendor');
+        $code_js[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'codemirror.js'), 'vendor');
+        $code_js[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'mode', 'clike', 'clike.js'), 'vendor');
+        $code_js[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'mode', 'python', 'python.js'), 'vendor');
+        $code_js[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('codemirror', 'mode', 'shell', 'shell.js'), 'vendor');
 
         return $this->core->getOutput()->renderTwigTemplate("misc/Code.twig", [
             "filename" => $filename,
             "file_contents" => $file_contents,
             "file_type" => $file_type,
-            "base_url" => $this->core->getConfig()->getBaseUrl()
+            "base_url" => $this->core->getConfig()->getBaseUrl(),
+            "code_css" => $code_css,
+            "code_js" => $code_js
         ]);
 	}
 }
