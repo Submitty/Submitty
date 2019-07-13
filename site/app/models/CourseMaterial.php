dc949333854fc710d4d6ed246855c5e2f7a0822d
Kevin McKinney <kevin.m.mckinney@gmail.com>
2019-07-12 21:39:17 -0400

    [Bugfix:CourseMaterials] Prevent students from accessing course materials prior to release (#4071)
    * Added checks to make sure we are not displaying files to students if those files have not yet been released.
    
    * Added checks to make sure we are not allowing students to download files that have not yet been released.

---
+++ site/app/models/CourseMaterial.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace app\models;
+
+
+use app\exceptions\FileNotFoundException;
+use app\libraries\Core;
+
+class CourseMaterial extends AbstractModel
+{
+    /**
+     * Determine if a course materials file has been released
+     *
+     * @param Core $core Application core
+     * @param string $path_to_file Full path to the file that we would like to check is released or not
+     * @return bool Indicates if the file has been released or not
+     * @throws FileNotFoundException The course_materials_file_data.json was not found
+     */
+    public static function isMaterialReleased(Core $core, string $path_to_file)
+    {
+        // Before students are allowed to view or download a course materials file we must ensure
+        // it has been released.  To return true the file metadata must be found in course_materials_file_data.json
+        // and the current time must be greater than the release_datetime
+
+        // Get path to the meta data json
+        $meta_data_json = $core->getConfig()->getCoursePath() . '/uploads/course_materials_file_data.json';
+
+        if(!is_file($meta_data_json))
+        {
+            throw new FileNotFoundException('Unable to locate the course_materials_file_data.json file');
+        }
+
+        $meta_data = json_decode(file_get_contents($meta_data_json));
+
+        // If file path does not exist as key in $meta_data then it has not been released
+        if(!property_exists($meta_data, $path_to_file))
+        {
+            return false;
+        }
+        // Else key does exist
+        else
+        {
+            $current_time = new \DateTime('now');
+            $release_time = \DateTime::createFromFormat('Y-m-d H:i:s', $meta_data->$path_to_file->release_datetime);
+
+            // If current time is greater than release time return true, else return false
+            $current_time > $release_time ? $retVal = true : $retVal = false;
+
+            return $retVal;
+        }
+    }
+}
