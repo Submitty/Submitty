d6a44223e7cde2ab04d615bc20b2a6b7c284bb7b
zjxiaohan <zjxiaohan@gmail.com>
2019-07-14 13:58:16 +0800

    split the `update` function

--- site/app/controllers/admin/LateController.php
+++ site/app/controllers/admin/LateController.php
@@ -4,27 +4,24 @@ namespace app\controllers\admin;
 
 use app\controllers\AbstractController;
 use app\libraries\DateUtils;
-use app\libraries\FileUtils;
 
 class LateController extends AbstractController {
     public function run() {
         switch ($_REQUEST['action']) {
             case 'view_late':
-                $this->core->getOutput()->addBreadcrumb('Late Days Allowed');
                 $this->viewLateDays();
                 break;
             case 'view_extension':
-                $this->core->getOutput()->addBreadcrumb('Excused Absence Extensions');
                 $this->viewExtensions();
                 break;
             case 'update_late':
-                $this->update("late", false);
+                $this->updateLateDays();
                 break;
             case 'delete_late':
-                $this->update("late", true);
+                $this->deleteLateDays();
                 break;
             case 'update_extension':
-                $this->update("extension", false);
+                $this->updateExtension();
                 break;
             case 'get_extension_details':
                 $this->getExtensions($_REQUEST['g_id']);
@@ -41,55 +38,80 @@ class LateController extends AbstractController {
     }
 
     public function viewExtensions() {
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('flatpickr', 'flatpickr.min.js'));
-        $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('flatpickr', 'flatpickr.min.css'));
-        $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('flatpickr', 'plugins', 'shortcutButtons', 'shortcut-buttons-flatpickr.min.js'));
-        $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('flatpickr', 'plugins', 'shortcutButtons', 'themes', 'light.min.css'));
         $e_gradeables = $this->core->getQueries()->getAllElectronicGradeablesIds();
         $this->core->getOutput()->renderOutput(array('admin', 'Extensions'), 'displayExtensions', $e_gradeables);
     }
 
-    public function update($type, $delete) {
-        if ($delete) {
-            if ($type != 'late') {
-                $error = "Deletion implemented only for Late Days";
-                return $this->core->getOutput()->renderJsonFail($error);
-            }
-        }
-        //Check to see if a CSV file was submitted.
+    public function updateLateDays() {
         $data = array();
-        if (isset($_FILES['csv_upload']) && (file_exists($_FILES['csv_upload']['tmp_name'])) && !$delete) {
-            //Validate
-            if (!($this->parseAndValidateCsv($_FILES['csv_upload']['tmp_name'], $data, $type))) {
+        if (isset($_FILES['csv_upload']) && (file_exists($_FILES['csv_upload']['tmp_name']))) {
+            if (!($this->parseAndValidateCsv($_FILES['csv_upload']['tmp_name'], $data, "late"))) {
                 $error = "Something is wrong with the CSV you have chosen. Try again.";
                 return $this->core->getOutput()->renderJsonFail($error);
             }
             else {
-                //Validate OK.  Process CSV.
-                //$_REQUEST['csv_option'] is determined by value attribute of radio buttons in site/app/views/admin/LateDayView.php
                 $csv_option = isset($_REQUEST['csv_option']) ? $_REQUEST['csv_option'] : null;
-				for ($i = 0; $i < count($data); $i++){
-					if ($type == "late"){
-						$this->core->getQueries()->updateLateDays($data[$i][0], $data[$i][1], $data[$i][2], $csv_option);
-					}
-					else {
-						$this->core->getQueries()->updateExtensions($data[$i][0], $data[$i][1], $data[$i][2]);
-					}
-				}
-				if ($type == "late"){
-					$this->getLateDays();
-				}
-				else {
-					$this->getExtensions($data[0][1]);
-				}
+                for ($i = 0; $i < count($data); $i++){
+                    $this->core->getQueries()->updateLateDays($data[$i][0], $data[$i][1], $data[$i][2], $csv_option);
+                }
+                $this->getLateDays();
             }
         }
-        else{ // not CSV, it's an individual
-            if (!$this->core->checkCsrfToken($_POST['csrf_token'])) {
-                $error = "Invalid CSRF token. Try again.";
+        else { // not CSV, it's an individual
+            $user = $this->core->getQueries()->getSubmittyUser($_POST['user_id']);
+            $isUserNotInCourse = empty($this->core->getQueries()->getUsersById(array($_POST['user_id'])));
+            if (!isset($_POST['user_id']) || $_POST['user_id'] == "" || $isUserNotInCourse || $user->getId() !== $_POST['user_id']) {
+                $error = "Invalid Student ID";
                 return $this->core->getOutput()->renderJsonFail($error);
             }
-            if ((!isset($_POST['g_id']) || $_POST['g_id'] == "" ) && $type == 'extension') {
+            if (!isset($_POST['datestamp']) || !DateUtils::validateTimestamp($_POST['datestamp'])) {
+                $error = "Datestamp must be mm/dd/yy";
+                return $this->core->getOutput()->renderJsonFail($error);
+            }
+            if (((!isset($_POST['late_days'])) || $_POST['late_days'] == "" || (!ctype_digit($_POST['late_days'])))) {
+                $error = "Late Days must be a nonnegative integer";
+                return $this->core->getOutput()->renderJsonFail($error);
+            }
+            $this->core->getQueries()->updateLateDays($_POST['user_id'], $_POST['datestamp'], $_POST['late_days']);
+            return $this->getLateDays();
+        }
+    }
+
+    public function deleteLateDays() {
+        $user = $this->core->getQueries()->getSubmittyUser($_POST['user_id']);
+        $isUserNotInCourse = empty($this->core->getQueries()->getUsersById(array($_POST['user_id'])));
+        if (!isset($_POST['user_id']) || $_POST['user_id'] == "" || $isUserNotInCourse || $user->getId() !== $_POST['user_id']) {
+            $error = "Invalid Student ID";
+            return $this->core->getOutput()->renderJsonFail($error);
+        }
+        if ((!isset($_POST['datestamp']) || !DateUtils::validateTimestamp($_POST['datestamp']))) {
+            $error = "Datestamp must be mm/dd/yy";
+            return $this->core->getOutput()->renderJsonFail($error);
+        }
+        $this->core->getQueries()->deleteLateDays($_POST['user_id'], $_POST['datestamp']);
+
+        return $this->getLateDays();
+    }
+
+    public function updateExtension() {
+        //Check to see if a CSV file was submitted.
+        $data = array();
+        if (isset($_FILES['csv_upload']) && (file_exists($_FILES['csv_upload']['tmp_name']))) {
+            //Validate
+            if (!($this->parseAndValidateCsv($_FILES['csv_upload']['tmp_name'], $data, "extension"))) {
+                $error = "Something is wrong with the CSV you have chosen. Try again.";
+                return $this->core->getOutput()->renderJsonFail($error);
+            }
+            else {
+                for ($i = 0; $i < count($data); $i++){
+                    $this->core->getQueries()->updateExtensions($data[$i][0], $data[$i][1], $data[$i][2]);
+                }
+
+                $this->getExtensions($data[0][1]);
+            }
+        }
+        else { // not CSV, it's an individual
+            if ((!isset($_POST['g_id']) || $_POST['g_id'] == "" )) {
                 $error = "Please choose a gradeable_id";
                 return $this->core->getOutput()->renderJsonFail($error);
             }
@@ -99,58 +121,42 @@ class LateController extends AbstractController {
                 $error = "Invalid Student ID";
                 return $this->core->getOutput()->renderJsonFail($error);
             }
-            if ((!isset($_POST['datestamp']) || !DateUtils::validateTimestamp($_POST['datestamp'])) && $type == 'late') {
-                $error = "Datestamp must be mm/dd/yy";
-                return $this->core->getOutput()->renderJsonFail($error);
-            }
-            if (((!isset($_POST['late_days'])) || $_POST['late_days'] == "" || (!ctype_digit($_POST['late_days']))) && !$delete) {
+            if ((!isset($_POST['late_days'])) || $_POST['late_days'] == "" || (!ctype_digit($_POST['late_days']))) {
                 $error = "Late Days must be a nonnegative integer";
                 return $this->core->getOutput()->renderJsonFail($error);
             }
-            if($type == "late"){
-                if ($delete) {
-                    $this->core->getQueries()->deleteLateDays($_POST['user_id'], $_POST['datestamp']);
+            $team = $this->core->getQueries()->getTeamByGradeableAndUser($_POST['g_id'], $_POST['user_id']);
+            //0 is for single submission, 1 is for team submission
+            $option = isset($_POST['option']) ? $_POST['option'] : -1;
+            if($team != NULL && $team->getSize() > 1){
+                if($option == 0){
+                    $this->core->getQueries()->updateExtensions($_POST['user_id'], $_POST['g_id'], $_POST['late_days']);
+                    $this->getExtensions($_POST['g_id']);
+                } else if($option == 1){
+                    $team_member_ids = explode(", ", $team->getMemberList());
+                    for($i = 0; $i < count($team_member_ids); $i++){
+                        $this->core->getQueries()->updateExtensions($team_member_ids[$i], $_POST['g_id'], $_POST['late_days']);
+                    }
+                    $this->getExtensions($_POST['g_id']);
+                } else {
+                    $this->core->getOutput()->useHeader(false);
+                    $this->core->getOutput()->useFooter(false);
+                    $team_member_ids = explode(", ", $team->getMemberList());
+                    $team_members = array();
+                    for($i = 0; $i < count($team_member_ids); $i++){
+                        $team_members[$team_member_ids[$i]] = $this->core->getQueries()->getUserById($team_member_ids[$i])->getDisplayedFirstName() . " " .
+                            $this->core->getQueries()->getUserById($team_member_ids[$i])->getDisplayedLastName();
+                    }
+                    $return = $this->core->getOutput()->renderTwigTemplate("admin/users/MoreExtensions.twig",
+                        ['g_id' => $_POST['g_id'],
+                            'member_list' => $team_members]);
+                    echo json_encode(array('is_team' => true, 'popup' => $return));
                 }
-                else {
-                    $this->core->getQueries()->updateLateDays($_POST['user_id'], $_POST['datestamp'], $_POST['late_days']);
-                }
-                $this->getLateDays();
-            }
-            else{
-            	$team = $this->core->getQueries()->getTeamByGradeableAndUser($_POST['g_id'], $_POST['user_id']);
-            	//0 is for single submission, 1 is for team submission
-            	$option = isset($_POST['option']) ? $_POST['option'] : -1;
-            	if($team != NULL && $team->getSize() > 1){
-            		if($option == 0){
-						$this->core->getQueries()->updateExtensions($_POST['user_id'], $_POST['g_id'], $_POST['late_days']);
-						$this->getExtensions($_POST['g_id']);
-					} else if($option == 1){
-						$team_member_ids = explode(", ", $team->getMemberList());
-						for($i = 0; $i < count($team_member_ids); $i++){
-							$this->core->getQueries()->updateExtensions($team_member_ids[$i], $_POST['g_id'], $_POST['late_days']);
-						}
-						$this->getExtensions($_POST['g_id']);
-					} else {
-						$this->core->getOutput()->useHeader(false);
-						$this->core->getOutput()->useFooter(false);
-						$team_member_ids = explode(", ", $team->getMemberList());
-						$team_members = array();
-						for($i = 0; $i < count($team_member_ids); $i++){
-							$team_members[$team_member_ids[$i]] = $this->core->getQueries()->getUserById($team_member_ids[$i])->getDisplayedFirstName() . " " .
-								$this->core->getQueries()->getUserById($team_member_ids[$i])->getDisplayedLastName();
-						}
-						$return = $this->core->getOutput()->renderTwigTemplate("admin/users/MoreExtensions.twig",
-							['g_id' => $_POST['g_id'],
-								'member_list' => $team_members]);
-						echo json_encode(array('is_team' => true, 'popup' => $return));
-					}
-				} else {
-					$this->core->getQueries()->updateExtensions($_POST['user_id'], $_POST['g_id'], $_POST['late_days']);
-					$this->getExtensions($_POST['g_id']);
-				}
+            } else {
+                $this->core->getQueries()->updateExtensions($_POST['user_id'], $_POST['g_id'], $_POST['late_days']);
+                $this->getExtensions($_POST['g_id']);
             }
         }
-
     }
 
     function getLateDays(){
