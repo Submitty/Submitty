b658e4353d287d9dde58c05d6cf691a9fb2933db
sak6lab <sdargula@gmail.com>
2019-07-15 15:16:08 -0400

    added migration for team notifications

---
+++ migration/migrator/migrations/course/20190715150737_team_notification_settings.py
@@ -0,0 +1,37 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("ALTER TABLE notification_settings ADD COLUMN IF NOT EXISTS team_invite BOOLEAN DEFAULT FALSE NOT NULL")
+    database.execute("ALTER TABLE notification_settings ADD COLUMN IF NOT EXISTS team_member_submission BOOLEAN DEFAULT FALSE NOT NULL")
+    database.execute("ALTER TABLE notification_settings ADD COLUMN IF NOT EXISTS team_invite_email BOOLEAN DEFAULT FALSE NOT NULL")
+    database.execute("ALTER TABLE notification_settings ADD COLUMN IF NOT EXISTS team_member_submission_email BOOLEAN DEFAULT FALSE NOT NULL")
+    pass
+
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    pass
