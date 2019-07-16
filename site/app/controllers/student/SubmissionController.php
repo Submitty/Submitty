207d029453e41d76d707fe210ce1eb062c50a0cb
sak6lab <sdargula@gmail.com>
2019-07-16 10:13:04 -0400

    notification links can use new router

--- site/app/controllers/student/SubmissionController.php
+++ site/app/controllers/student/SubmissionController.php
@@ -1736,7 +1736,7 @@ class SubmissionController extends AbstractController {
           }
 
           // make graders' notifications and emails
-          $metadata = json_encode(array(array('component' => 'grading', 'page' => 'electronic', 'action' => 'grade', 'gradeable_id' => $gradeable_id, 'who_id' => $submitter->getId())));
+          $metadata = json_encode(array(array('component' => 'grading', 'page' => 'electronic', 'action' => 'grade', 'gradeable_id' => $gradeable_id, 'who_id' => $submitter->getId()),null,false));
           foreach ($graders as $grader) {
               if ($grader->accessFullGrading() && $grader->getId() != $user_id){
                   $details = ['component' => 'grading', 'metadata' => $metadata, 'content' => $n_content, 'body' => $email_body, 'subject' => $email_subject, 'sender_id' => $user_id, 'to_user_id' => $grader->getId()];
@@ -1746,7 +1746,7 @@ class SubmissionController extends AbstractController {
           }
 
           // make students' notifications and emails
-          $metadata = json_encode(array(array('component' => 'student', 'gradeable_id' => $gradeable_id)));
+          $metadata = json_encode(array(array('component' => 'student', 'gradeable_id' => $gradeable_id),null,false));
           if($submitter->isTeam()){
               $submitting_team = $submitter->getTeam()->getMemberUsers();
               foreach($submitting_team as $submitting_user){
