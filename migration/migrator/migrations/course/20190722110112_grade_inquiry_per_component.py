3b670d820dc691a93b21543c1c0949322d135d4e
sak6lab <sdargula@gmail.com>
2019-07-22 15:29:11 -0400

    removed down migration and added if not exists

--- migration/migrator/migrations/course/20190722110112_grade_inquiry_per_component.py
+++ migration/migrator/migrations/course/20190722110112_grade_inquiry_per_component.py
@@ -3,19 +3,19 @@
 
 def up(config, database, semester, course):
     # add gc_id (gradeable compoenent id) foriegn key column to regrade discussion table
-    database.execute("ALTER TABLE regrade_discussion ADD COLUMN gc_id integer")
-    database.execute("ALTER TABLE regrade_discussion ADD CONSTRAINT gradeable_component_id_fk FOREIGN KEY (gc_id) REFERENCES gradeable_component (gc_id)")
+    database.execute("ALTER TABLE regrade_discussion ADD COLUMN IF NOT EXISTS gc_id integer")
+    database.execute("""
+        ALTER TABLE regrade_discussion DROP CONSTRAINT IF EXISTS gradeable_component_id_fk;
+        ALTER TABLE regrade_discussion ADD CONSTRAINT gradeable_component_id_fk FOREIGN KEY (gc_id) REFERENCES gradeable_component (gc_id);
+        """)
 
     # add eg_is_gi_per_component column to electronic_gradeable table
-    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN eg_is_gi_per_component boolean DEFAULT false NOT NULL")
+    database.execute("ALTER TABLE electronic_gradeable ADD COLUMN IF NOT EXISTS eg_is_gi_per_component boolean DEFAULT false NOT NULL")
     # add a check constraint to make sure that eg_regrade_allowed is true
-    database.execute("ALTER TABLE electronic_gradeable ADD CONSTRAINT regrade_allowed_true_check CHECK (eg_regrade_allowed is true or eg_is_gi_per_component is false)")
+    database.execute("""
+        ALTER TABLE electronic_gradeable DROP CONSTRAINT IF EXISTS regrade_allowed_true_check;
+        ALTER TABLE electronic_gradeable ADD CONSTRAINT regrade_allowed_true_check CHECK (eg_regrade_allowed is true or eg_is_gi_per_component is false);
+    """)
 
 def down(config, database, semester, course):
-    # remove gc_id column from regrade_discusion table
-    database.execute("ALTER TABLE regrade_discussion DROP CONSTRAINT gradeable_component_id_fk")
-    database.execute("ALTER TABLE regrade_discussion DROP COLUMN gc_id")
-
-    # remove eg_is_gi_per_component column from electronic_gradeable table
-    database.execute("ALTER TABLE electronic_gradeable DROP CONSTRAINT regrade_allowed_true_check")
-    database.execute("ALTER TABLE electronic_gradeable DROP COLUMN eg_is_gi_per_component")
+    pass
