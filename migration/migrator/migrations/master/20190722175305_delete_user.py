d56de308d96c82f15923ce0ed761b30012c31a92
pbailie <pbailie@users.noreply.github.com>
2019-07-23 19:59:05 -0400

    Update 20190722175305_delete_user.py
    - Better trigger handling of user ID cleanup
    - Exception handling for creating new triggers (so duplicate triggers don't crash the migrator)

--- migration/migrator/migrations/master/20190722175305_delete_user.py
+++ migration/migrator/migrations/master/20190722175305_delete_user.py
@@ -29,11 +29,6 @@ BEGIN
     END IF;
     PERFORM dblink_exec(db_conn, query_string);
 
-    SELECT COUNT(*) INTO user_courses FROM courses_users WHERE user_id = OLD.user_id;
-    IF user_courses = 1 THEN
-        DELETE FROM users WHERE user_id = OLD.user_id;
-    END IF;
-
     -- All done.  As this is a BEFORE DELETE trigger, RETURN OLD allows original triggering DELETE query to proceed.
     RETURN OLD;
 
@@ -47,7 +42,42 @@ EXCEPTION WHEN integrity_constraint_violation THEN
 END;
 $$ LANGUAGE plpgsql;""")
 
-    database.execute("CREATE TRIGGER before_delete_sync_delete_user BEFORE DELETE ON courses_users FOR EACH ROW EXECUTE PROCEDURE sync_delete_user();")
+    database.execute("""
+CREATE OR REPLACE FUNCTION sync_delete_user_cleanup() RETURNS TRIGGER AS $$
+DECLARE
+    user_courses INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO user_courses FROM courses_users WHERE user_id = OLD.user_id;
+    IF user_courses = 0 THEN
+        DELETE FROM users WHERE user_id = OLD.user_id;
+    END IF;
+    RETURN NULL;
+-- The SELECT Count(*) / If check should prevent this exception, but this
+-- exception handling is provided 'just in case' so process isn't interrupted.
+EXCEPTION WHEN integrity_constraint_violation THEN
+    RAISE NOTICE 'Integrity constraint prevented user ''%'' from being deleted from master.users table.', OLD.user_id;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;""")
+
+    database.execute("""
+DO $$
+BEGIN
+    CREATE TRIGGER before_delete_sync_delete_user BEFORE DELETE ON courses_users FOR EACH ROW EXECUTE PROCEDURE sync_delete_user();
+EXCEPTION WHEN duplicate_object THEN
+    RAISE NOTICE 'BEFORE DELETE trigger for function sync_delete_user() already exists, skipping.';
+END;
+$$ LANGUAGE plpgsql;""")
+
+database.execute("""
+DO $$
+BEGIN
+    CREATE TRIGGER after_delete_sync_delete_user_cleanup AFTER DELETE ON courses_users FOR EACH ROW EXECUTE PROCEDURE sync_delete_user_cleanup();
+EXCEPTION WHEN duplicate_object THEN
+    RAISE NOTICE 'AFTER DELETE trigger for function sync_delete_user_cleanup() already exists, skipping.';
+END;
+$$ LANGUAGE plpgsql;""")
+
 
 def down(config, database):
     """
@@ -58,6 +88,7 @@ def down(config, database):
     :param database: Object for interacting with given database for environment
     :type database: migrator.db.Database
     """
+    database.execute("DROP TRIGGER IF EXISTS after_delete_sync_user_delete_cleanup ON courses_users;")
     database.execute("DROP TRIGGER IF EXISTS before_delete_sync_user_delete ON courses_users;")
     database.execute("DROP FUNCTION IF EXISTS sync_delete_user();")
     database.execute("ALTER TABLE ONLY emails DROP CONSTRAINT IF EXISTS emails_user_id_fk;")
