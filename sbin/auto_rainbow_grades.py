b60981be6edaf3ea4cad15cb92b643b7fd2755e1
Kevin-MM <kevin.m.mckinney@gmail.com>
2019-07-24 08:25:52 -0400

    Corrected python linting error

--- sbin/auto_rainbow_grades.py
+++ sbin/auto_rainbow_grades.py
@@ -169,7 +169,7 @@ if response_json['status'] != 'success':
 
     print('Failed to obtain an auth token.', flush=True)
     print('Check submitty_admin.json contains valid credentials', flush=True)
-    print('Attempting to continue with previously generated grade summaries', 
+    print('Attempting to continue with previously generated grade summaries',
           flush=True)
 
     # We may still continue execution if grade summaries had been previously manually
