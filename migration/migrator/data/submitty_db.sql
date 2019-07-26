62e9f2055d0b752015807673be63d1ee094e968c
pbailie <pbailie@users.noreply.github.com>
2019-07-25 20:10:55 -0400

    Update submitty_db.sql and delete_user.py migration
    WIP

--- migration/migrator/data/submitty_db.sql
+++ migration/migrator/data/submitty_db.sql
@@ -356,7 +356,7 @@ END;
 $generate_api_key$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION sync_delete_user() RETURNS TRIGGER AS $$
-›-- BEFORE DELETE trigger function to DELETE users from course DB.
+-- BEFORE DELETE trigger function to DELETE users from course DB.
 DECLARE
     db_conn VARCHAR;
     query_string TEXT;
@@ -376,13 +376,17 @@ BEGIN
 -- We should catch this exception and stop execution with no rows processed.
 -- No rows processed will indicate that deletion had an error and did not occur.
 EXCEPTION WHEN integrity_constraint_violation THEN
-    RAISE NOTICE 'User still has existing data in course DB ''%''', substring(db_conn FROM 7);
+    -- Show that an exception occurred, and what was the exception.
+    RAISE NOTICE 'User still has existing data in course DB ''%''', substring(db_conn FROM 8);
+    RAISE NOTICE '%', SQLERRM;
     -- Return NULL so we do not proceed with original triggering DELETE query.
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION sync_delete_user_cleanup() RETURNS TRIGGER AS $$
+-- AFTER DELETE trigger function removes user from master.users if they have no
+-- existing course enrollment.  (i.e. no entries in courses_users)
 DECLARE
     user_courses INTEGER;
 BEGIN
@@ -391,10 +395,13 @@ BEGIN
         DELETE FROM users WHERE user_id = OLD.user_id;
     END IF;
     RETURN NULL;
+
 -- The SELECT Count(*) / If check should prevent this exception, but this
 -- exception handling is provided 'just in case' so process isn't interrupted.
 EXCEPTION WHEN integrity_constraint_violation THEN
+    -- Show that an exception occurred, and what was the exception.
     RAISE NOTICE 'Integrity constraint prevented user ''%'' from being deleted from master.users table.', OLD.user_id;
+    RAISE NOTICE '%', SQLERRM;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
--- migration/migrator/migrations/master/20190722175305_delete_user.py
+++ migration/migrator/migrations/master/20190722175305_delete_user.py
@@ -35,7 +35,9 @@ BEGIN
 -- We should catch this exception and stop execution with no rows processed.
 -- No rows processed will indicate that deletion had an error and did not occur.
 EXCEPTION WHEN integrity_constraint_violation THEN
-    RAISE NOTICE 'User still has existing data in course DB ''%''', substring(db_conn FROM 7);
+    -- Show that an exception occurred, and what was the exception.
+    RAISE NOTICE 'User still has existing data in course DB ''%''', substring(db_conn FROM 8);
+    RAISE NOTICE '%', SQLERRM;
     -- Return NULL so we do not proceed with original triggering DELETE query.
     RETURN NULL;
 END;
@@ -43,6 +45,8 @@ $$ LANGUAGE plpgsql;""")
 
     database.execute("""
 CREATE OR REPLACE FUNCTION sync_delete_user_cleanup() RETURNS TRIGGER AS $$
+-- AFTER DELETE trigger function removes user from master.users if they have no
+-- existing course enrollment.  (i.e. no entries in courses_users)
 DECLARE
     user_courses INTEGER;
 BEGIN
@@ -51,14 +55,19 @@ BEGIN
         DELETE FROM users WHERE user_id = OLD.user_id;
     END IF;
     RETURN NULL;
+
 -- The SELECT Count(*) / If check should prevent this exception, but this
 -- exception handling is provided 'just in case' so process isn't interrupted.
 EXCEPTION WHEN integrity_constraint_violation THEN
+    -- Show that an exception occurred, and what was the exception.
     RAISE NOTICE 'Integrity constraint prevented user ''%'' from being deleted from master.users table.', OLD.user_id;
+    RAISE NOTICE '%', SQLERRM;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;""")
 
+    # Oddly, IF NOT EXISTS is a syntax error when coupled with CREATE TRIGGER.
+    # We'll check for a duplicate trigger with exception handling.
     database.execute("""
 DO $$
 BEGIN
@@ -68,6 +77,8 @@ EXCEPTION WHEN duplicate_object THEN
 END;
 $$ LANGUAGE plpgsql;""")
 
+    # Oddly, IF NOT EXISTS is a syntax error when coupled with CREATE TRIGGER.
+    # We'll check for a duplicate trigger with exception handling.
     database.execute("""
 DO $$
 BEGIN
