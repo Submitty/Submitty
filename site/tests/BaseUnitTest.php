3ce21c2b1ee0b7304981ca7fcb81b182b547c666
zjxiaohan <zjxiaohan@gmail.com>
2019-07-31 21:10:58 +0800

    fix tests

--- site/tests/BaseUnitTest.php
+++ site/tests/BaseUnitTest.php
@@ -50,7 +50,7 @@ class BaseUnitTest extends \PHPUnit\Framework\TestCase {
         }
 
         if (isset($config_values['course_home_url'])) {
-            $config->method('buildCourseUrl')->willReturn($config_values['course_home_url']);
+            $core->method('buildCourseUrl')->willReturn($config_values['course_home_url']);
             $config->method('getCourseHomeUrl')->willReturn($config_values['course_home_url']);
         }
 
