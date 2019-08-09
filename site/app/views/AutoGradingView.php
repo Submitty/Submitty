46b04b8c0a03ca1e5e56a8fa5b2d997298453e03
thef1stofhername <aorosz@outlook.com>
2019-08-09 17:05:35 -0400

    styling concepts in place, not rendering

--- site/app/views/AutoGradingView.php
+++ site/app/views/AutoGradingView.php
@@ -225,7 +225,7 @@ class AutoGradingView extends AbstractView {
         }
 
         $popup_css_file = $this->core->getOutput()->timestampResource($popup_css_file, 'css');
-
+       // $this->core->getOutput()->addInternalCss('sequence.css');
         return $this->core->getOutput()->renderTwigTemplate("autograding/AutoChecks.twig", [
             "gradeable_id" => $gradeable->getId(),
             "checks" => $checks,
