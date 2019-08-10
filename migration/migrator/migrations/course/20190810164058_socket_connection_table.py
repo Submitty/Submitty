8184c5c2df4f5cfaffe14cd913f02c29d9a6182d
xprilion <xprilion@gmail.com>
2019-08-11 02:19:37 +0530

    update migration

--- migration/migrator/migrations/course/20190810164058_socket_connection_table.py
+++ migration/migrator/migrations/course/20190810164058_socket_connection_table.py
@@ -3,7 +3,7 @@
 
 def up(config, database, semester, course):
     if not database.has_table('socket_connections'):
-        database.execute("CREATE TABLE socket_connections ( connection_id SERIAL NOT NULL, user_id VARCHAR(500), client_id VARCHAR(100), last_active TIMESTAMP, CONSTRAINT socket_connections_pk PRIMARY KEY (connection_id), CONSTRAINT socket_connections_users_user_id_fk FOREIGN KEY (user_id) REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE );")
+        database.execute("CREATE TABLE socket_connections ( connection_id SERIAL NOT NULL, user_id VARCHAR(500), client_id VARCHAR(100), last_active TIMESTAMP, CONSTRAINT socket_connections_pk PRIMARY KEY (connection_id), CONSTRAINT socket_connections_users_user_id_fk FOREIGN KEY (user_id) REFERENCES users ON UPDATE CASCADE ON DELETE CASCADE )")
 
 def down(config, database, semester, course):
     pass
