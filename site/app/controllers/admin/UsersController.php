00a8d0a68041fb138642793a09d6183ad4b15eb6
zjxiaohan <zjxiaohan@gmail.com>
2019-08-27 21:07:59 +0800

    Put user into course db before updating

--- site/app/controllers/admin/UsersController.php
+++ site/app/controllers/admin/UsersController.php
@@ -332,8 +332,8 @@ class UsersController extends AbstractController {
                 $this->core->addSuccessMessage("New Submitty user '{$user->getId()}' added");
             }
             else {
-                $this->core->getQueries()->updateUser($user, $this->core->getConfig()->getSemester(), $this->core->getConfig()->getCourse());
                 $this->core->getQueries()->insertCourseUser($user, $this->core->getConfig()->getSemester(), $this->core->getConfig()->getCourse());
+                $this->core->getQueries()->updateUser($user, $this->core->getConfig()->getSemester(), $this->core->getConfig()->getCourse());
                 $this->core->addSuccessMessage("Existing Submitty user '{$user->getId()}' added");
             }
 
