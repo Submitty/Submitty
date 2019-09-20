bd73884c0b1c86d3f19660dc04accecac73e7340
pbailie <pbailie@users.noreply.github.com>
2019-09-19 21:27:44 -0400

    Changes to be committed: modified:   migration/migrator/migrations/system/20190918171152_preferred_name_logging.py
    WIP

--- migration/migrator/migrations/system/20190918171152_preferred_name_logging.py
+++ migration/migrator/migrations/system/20190918171152_preferred_name_logging.py
@@ -1,8 +1,9 @@
 """Migration for the Submitty system."""
+import re
 import os
 import pwd
+import shutil
 from pathlib import Path
-from pprint import pprint
 
 def up(config):
     """
@@ -12,6 +13,10 @@ def up(config):
     :type config: migrator.config.Config
     """
 
+    # User 'postgres' uid and gid (needed to permit postgresql access to files)
+    postgres_uid = pwd.getpwnam('postgres').pw_uid
+    postgres_gid = pwd.getpwnam('postgres').pw_gid
+
     # Update repos to ensure we have the latest SysadminTools release
     update_repos_script = str(Path(config.submitty['submitty_repository'], '.setup', 'bin', 'update_repos.sh'))
     os.system('bash ' + update_repos_script)
@@ -19,7 +24,7 @@ def up(config):
     # New log folders
     psql_log_folder = str(Path(config.submitty['submitty_data_dir'], 'logs', 'psql'))
     pfn_log_folder = str(Path(config.submitty['submitty_data_dir'], 'logs', 'preferred_names'))
-    postgres_uid = pwd.getpwnam('postgres').pw_uid
+
     try:
         os.mkdir(psql_log_folder)
         os.chown(psql_log_folder, postgres_uid, config.submitty_users['daemon_gid'])
@@ -32,23 +37,48 @@ def up(config):
     except FileExistsError:
         pass
 
-    # Postgresql configuration (FIX ME)
+    # Postgresql configuration
     psql_version = os.popen("psql -V | grep -m 1 -o '[0-9]\+' | head -1").read().translate({0x0a: None})
-    process = f"""\
-sed -i "s~^#*[ tab]*log_destination[ tab]*=[ tab]*'[a-z]\+'~log_destination = 'csvlog'~;
-        s~^#*[ tab]*logging_collector[ tab]*=[ tab]*[a-z01]\+~logging_collector = on~;
-        s~^#*[ tab]*log_directory[ tab]*=[ tab]*'[^][(){{}}<>|:;&#=!'?\*\~\$\"\` tab]\+'~log_directory = '{config.submitty['submitty_data_dir']}/logs/psql'~;
-        s~^#*[ tab]*log_filename[ tab]*=[ tab]*'[-a-zA-Z0-9_%\.]\+'~log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log'~;
-        s~^#*[ tab]*log_file_mode[ tab]*=[ tab]*[0-9]\+~log_file_mode = 0640~;
-        s~^#*[ tab]*log_rotation_age[ tab]*=[ tab]*[a-z0-9]\+~log_rotation_age = 1d~;
-        s~^#*[ tab]*log_rotation_size[ tab]*=[ tab]*[a-zA-Z0-9]\+~log_rotation_size = 10MB~;
-        s~^#*[ tab]*log_min_messages[ tab]*=[ tab]*[a-z]\+~log_min_messages = log~;
-        s~^#*[ tab]*log_min_duration_statement[ tab]*=[ tab]*[-0-9]\+~log_min_duration_statement = 0~;
-        s~^#*[ tab]*log_line_prefix[ tab]*=[ tab]*'.\+'~log_line_prefix = '%t '~" /etc/postgresql/{psql_version}/main/postgresql.conf"""
-    subprocess.run(process)
+    psql_conf_file = open(f'/etc/postgresql/{psql_version}/main/postgresql.conf', mode='r')
+    psql_config = psql_conf_file.read()
+    psql_conf_file.close()
 
-    #TO DO: copy preferred_name_logging.php to sbin and set ownership+permissions
+    # Regexs used to adjust Postgresql's configuration via match and substitute.
+    # Dict key is regex, value is associated replacement text.
+    # Third pattern:  x5b = '[', x5d = ']', x22 = double quotes
+    # Fourth pattern: x2d = '-', x2e = '.'
+    # Ninth pattern:  x2d = '-'
+    patterns = {r"^#*\s*log_destination\s*=\s*'[a-z]+'":                                "log_destination = 'csvlog'",
+                r"^#*\s*logging_collector\s*=\s*[a-z01]+":                              "logging_collector = on",
+                r"^#*\s*log_directory\s*=\s*'[^(){}<>|:;&#=!'~?*$`\x5b\x5d\x22\s]+'":  f"log_directory = '{config.submitty['submitty_data_dir']}/logs/psql",
+                r"^#*\s*log_filename\s*=\s*'[a-zA-Z0-9_%\x2d\x2e]+'":                   "log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log'",
+                r"^#*\s*log_file_mode\s*=\s*[0-9]+":                                    "log_file_mode = 0640",
+                r"^#*\s*log_rotation_age\s*=\s*[a-z0-9]+":                              "log_rotation_age = 1d",
+                r"^#*\s*log_rotation_size\s*=\s*[a-zA-Z0-9]+":                          "log_rotation_size = 10MB",
+                r"^#*\s*log_min_messages\s*=\s*[a-z]+":                                 "log_min_messages = log",
+                r"^#*\s*log_min_duration_statement\s*=\s*[0-9\x2d]+":                   "log_min_duration_statement = 0",
+                r"^#*\s*log_line_prefix\s*=\s*.+":                                      "log_line_prefix = '%t '"}
 
+    for regex, replacement in patterns.items():
+        psql_config = re.sub(regex, replacement, psql_config, count = 1, flags = re.MULTILINE)
+
+    psql_conf_file = open(f'/etc/postgresql/{psql_version}/main/postgresql.conf', mode='w')
+    psql_conf_file.write(psql_config)
+    psql_conf_file.close()
+    os.chown(postgres_uid, postgres_gid, psql_conf_file)
+
+    # Copy preferred_name_logging.php from SysadminTools to sbin
+    src =  str(Path(config.submitty['submitty_repository'], '..', 'SysadminTools', 'preferred_name_logging', 'preferred_name_logging.php')
+    dest = str(Path(config.submitty['submitty_install_dir'], 'sbin', 'preferred_name_logging.php')
+
+    try:
+        shutil.copyfile(src, dest)
+        os.chown(dest, 0, config.submitty_users['daemon_group'])
+        os.chmod(dest, 0o0550)
+    except shutil.SameFileError:
+        pass
+    except: OSError:
+        pass
 
 
 def down(config):
