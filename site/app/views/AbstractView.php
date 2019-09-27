c7eb2f12f3984e19806eff93ac6ccd239fbd74e8
Matthew Peveler <matt.peveler@gmail.com>
2019-09-27 06:22:01 -0300

    [Refactor] Rewrite handling of views in Output

--- site/app/views/AbstractView.php
+++ site/app/views/AbstractView.php
@@ -3,12 +3,16 @@
 namespace app\views;
 
 use app\libraries\Core;
+use app\libraries\Output;
 
 abstract class AbstractView {
     /** @var Core */
     protected $core;
+    /** @var Output */
+    protected $output;
 
-    public function __construct($core) {
+    public function __construct(Core $core, Output $output) {
         $this->core = $core;
+        $this->output = $output;
     }
 }
