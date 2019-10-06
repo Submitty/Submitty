0dd979ba4306cae502ac96cc1dea35503ceccf92
Matthew Peveler <matt.peveler@gmail.com>
2019-10-06 10:12:39 -0300

    [Refactor:PHP] Rewrite handling of views in Output to be more decoupled from Core (#4551)

--- site/app/views/admin/ConfigurationView.php
+++ site/app/views/admin/ConfigurationView.php
@@ -5,19 +5,18 @@ namespace app\views\admin;
 use app\views\AbstractView;
 
 class ConfigurationView extends AbstractView {
-    public function viewConfig($fields, $gradeable_seating_options) {
-        $this->core->getOutput()->addInternalJs("course-settings.js");
-        $this->core->getOutput()->addInternalJs("configuration.js");
-        $this->core->getOutput()->addInternalCss("configuration.css");
-        $this->core->getOutput()->addBreadcrumb('Course Settings');
-        return $this->core->getOutput()->renderTwigTemplate("admin/Configuration.twig", [
+    public function viewConfig($fields, $gradeable_seating_options, $email_enabled, $csrf_token) {
+        $this->output->addInternalJs("configuration.js");
+        $this->output->addInternalCss("configuration.css");
+        $this->output->addBreadcrumb('Course Settings');
+        return $this->output->renderTwigTemplate("admin/Configuration.twig", [
             "fields" => $fields,
             "gradeable_seating_options" => $gradeable_seating_options,
             "theme_url" => $this->core->buildCourseUrl(['theme']),
             "email_room_seating_url" => $this->core->buildCourseUrl(['email_room_seating']),
             "manage_categories_url" => $this->core->buildCourseUrl(['forum', 'categories']),
-            "csrf_token" => $this->core->getCsrfToken(),
-            "email_enabled" => $this->core->getConfig()->isEmailEnabled()
+            "csrf_token" => $csrf_token,
+            "email_enabled" => $email_enabled
         ]);
     }
 }
