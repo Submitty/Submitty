dd14111293e4a51080973212355a866d2b0cc056
sak6lab <sdargula@gmail.com>
2019-10-12 20:00:44 -0400

    added two more tests

--- tests/e2e/test_grade_inquiry.py
+++ tests/e2e/test_grade_inquiry.py
@@ -31,22 +31,71 @@ class TestGradeInquiry(BaseTestCase):
         # navigate back to gradeable page
         self.driver.find_element_by_xpath("//a[@id='nav-sidebar-submitty']").click()
 
+    # TA GRADING INTERFACE TESTS
     def test_normal_submission_grade_inquiry_panel(self):
         gradeable_id = 'grades_released_homework'
         grade_inquiry_deadline_date = "9998-01-01 00:00:00"
 
         # login as instructor
-        self.log_in(user_id='instructor', user_password='instructor', user_name='instructor')
+        self.log_in(user_id='instructor')
         self.click_class('sample')
         self.set_grade_inquiries_for_course(True)
-        self.set_grade_inquiries_for_gradeable(gradeable_id, True, grade_inquiry_deadline_date)
+        self.set_grade_inquiries_for_gradeable(gradeable_id, grade_inquiry_deadline_date, allowed=True)
 
-        # navigate to TA grading interface
+        # navigate to TA grading interface of student with normal submission
         self.driver.find_element_by_xpath("//div[@id='"+gradeable_id+"']//a[contains(@class,'btn-nav-grade')]").click()
         self.driver.find_element_by_xpath("//a[contains(text(),'Grading Index')]").click()
         self.driver.find_element_by_xpath("//a[contains(@href,'grading/grade?who_id=bauchg')]").click()
 
-        # make sure submit button is present and disabled because no text
+        # make sure submit button is present
+        buttons = self.driver.find_elements_by_xpath("//button[contains(@class,'gi-submit')]")
+        assert len(buttons) == 1
+        assert buttons[0].text == "Submit Grade Inquiry"
+
+    def test_no_submission_grade_inquiry_panel(self):
+        gradeable_id = 'grades_released_homework'
+
+        # login as instructor
+        self.log_in(user_id='instructor')
+        self.click_class('sample')
+        self.set_grade_inquiries_for_course(True)
+        self.set_grade_inquiries_for_gradeable(gradeable_id,allowed=True)
+
+        # navigate to TA grading interface of student with no submission
+        self.driver.find_element_by_xpath("//div[@id='"+gradeable_id+"']//a[contains(@class,'btn-nav-grade')]").click()
+        self.driver.find_element_by_xpath("//a[contains(text(),'Grading Index')]").click()
+        self.driver.find_element_by_xpath("//a[contains(@href,'grading/grade?who_id=lakinh')]").click()
+
+        try:
+           self.driver.find_element_by_xpath("//div[@id='regrade_info']//span[text()='No Submission']")
+        except NoSuchElementException:
+                assert False
+        assert True
+        buttons = self.driver.find_elements_by_xpath("//button[contains(@class,'gi-submit')]")
+        assert len(buttons) == 0
+
+     # STUDENT SUBMISSION TESTS
+    def test_normal_submission_student_grade_inquiry_box(self):
+        gradeable_id = 'grades_released_homework'
+        grade_inquiry_deadline_date = "9998-01-01 00:00:00"
+
+        self.log_in(user_id='instructor')
+        self.click_class('sample')
+        self.set_grade_inquiries_for_course(True)
+        self.set_grade_inquiries_for_gradeable(gradeable_id,allowed=True)
+
+        self.log_out()
+        self.log_in(user_id='bauchg')
+        self.click_class('sample')
+
+        self.driver.find_element_by_xpath("//div[@id='"+gradeable_id+"']//a[contains(text(),'VIEW GRADE')]").click()
+
+        assert not self.driver.find_element_by_id("regradeBoxSection").is_displayed()
+        open_grade_inquiry_button = self.driver.find_element_by_xpath("//button[contains(text(),'Open Grade Inquiry')]")
+        open_grade_inquiry_button.click()
+        assert not open_grade_inquiry_button.is_displayed()
+
+        # make sure submit button is present
         buttons = self.driver.find_elements_by_xpath("//button[contains(@class,'gi-submit')]")
         assert len(buttons) == 1
         assert buttons[0].text == "Submit Grade Inquiry"
