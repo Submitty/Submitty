5d5c910ac7bb982edb3b84bf081ddc250e1b3cf4
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2019-10-23 12:21:40 -0400

    [Bugfix:Email] Handle unmatched variables in exam seating email (#4632)
    * Handle unmatched variables in seating email
    
    * Expand placeholder regex
    
    * Get rid of tabs on 111
    
    * Insert "SEE INSTRUCTOR" when an entry is missing
    
    * Undo unreplaced variable substitution.
    
    * Drop `if` statement

--- site/app/controllers/admin/EmailRoomSeatingController.php
+++ site/app/controllers/admin/EmailRoomSeatingController.php
@@ -103,12 +103,11 @@ Please email your instructor with any questions or concerns.';
         ];
 
         foreach($replaces as $key => $variable) {
-            if(isset($data[$key])) {
-                $message = str_replace('{$' . $variable . '}', $data[$key], $message);
-            }
+            $message = str_replace('{$' . $variable . '}', $data[$key] ?? 'SEE INSTRUCTOR', $message);
         }
 
         $message = str_replace('{$course_name}', $this->core->getConfig()->getCourse(), $message);
+
         return $message;
     }
 
