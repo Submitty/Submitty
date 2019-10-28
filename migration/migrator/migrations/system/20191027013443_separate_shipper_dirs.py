41c91258bae38cd1ea6ca171d226b9511f9cb42c
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2019-10-28 00:30:17 -0400

    Create migrator

---
+++ migration/migrator/migrations/system/20191027013443_separate_shipper_dirs.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+IN_PROGRESS_PATH = '/var/local/submitty/grading'
+
+
+def up(config):
+    if not os.path.exists(IN_PROGRESS_PATH):
+        os.mkdir(IN_PROGRESS_PATH)
+    shutil.chown(IN_PROGRESS_PATH, 'submitty_daemon', 'submitty_daemonphp')
+    os.chmod(IN_PROGRESS_PATH, 0o770)
