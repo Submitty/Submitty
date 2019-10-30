337f340cc1c66e2a8c812234a15e82c5f0eb8b07
Matthew Peveler <matt.peveler@gmail.com>
2019-10-29 21:51:05 -0300

    [Testing:PHP] Add baseline for phpstan for higher level (#4664)
    * [Testing:PHP] Add baseline for phpstan for higher level
    
    * fix array offset error with Ds\Set
    
    * update baseline

--- site/app/controllers/GlobalController.php
+++ site/app/controllers/GlobalController.php
@@ -27,7 +27,7 @@ class GlobalController extends AbstractController {
         $js = $this->core->getOutput()->getJs();
 
         if (array_key_exists('override.css', $wrapper_urls)) {
-            $css[] = $wrapper_urls['override.css'];
+            $css->add($wrapper_urls['override.css']);
         }
 
         $unread_notifications_count = null;
@@ -355,7 +355,7 @@ class GlobalController extends AbstractController {
         //else if(...){}
         //more Holidays go here!
 
-        return $this->core->getOutput()->renderTemplate('Global', 'header', $breadcrumbs, $wrapper_urls, $sidebar_buttons, $unread_notifications_count, $css, $js, $duck_img);
+        return $this->core->getOutput()->renderTemplate('Global', 'header', $breadcrumbs, $wrapper_urls, $sidebar_buttons, $unread_notifications_count, $css->toArray(), $js->toArray(), $duck_img);
     }
 
     public function footer() {
