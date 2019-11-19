ebe04b99a5ed1683f20e08456b6baa63f3783556
Ben Kelly <benjaminkelly@me.com>
2019-11-19 10:25:36 -0500

    [Refactor:PHP] Combine DatabaseQueries.php and PostgresqlDatabaseQueries.php into single file (#4763)
    * Removed implementation in PostGres and fixed implementation in DBQ
    
    * migrated missing functions over
    
    * finished migrations and removed duplicate messages
    
    * removed PostGresql database queries object from testing
    
    * fixed linting issues
    
    * fixed (hopefully) last linter error
    
    * more lints
    
    * ran auto linter
    
    * fixed merge
    
    * fixed merge pt 2
    
    * ran phpcbf
    
    * finally sorted out linter
    
    * fix phpstan errors
    
    * style change
    
    * fixed various issues with comments and inconsistencies
    
    * fixed getAverageComponentScores mismatch with postgres file
    
    * fix phpstan error

--- site/app/libraries/database/DatabaseFactory.php
+++ site/app/libraries/database/DatabaseFactory.php
@@ -68,13 +68,6 @@ class DatabaseFactory {
      * @return DatabaseQueries
      */
     public function getQueries(Core $core) {
-        switch ($this->driver) {
-            case 'pgsql':
-            case 'postgres':
-            case 'postgresql':
-                return new PostgresqlDatabaseQueries($core);
-            default:
-                return new DatabaseQueries($core);
-        }
+        return new DatabaseQueries($core);
     }
 }
