97f03df5e57acc5c0324179f3028a1c882ae66c7
mukul-kmr-jha <jimickeyji@gmail.com>
2019-12-29 11:58:20 +0530

    "Rollback Available" TA grading

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -1042,6 +1042,29 @@ class ElectronicGraderController extends AbstractController {
         else {
             $late_status = $ldi->getStatus();
         }
+        $rollbackSubmission = -1;
+        $previousVersion = $display_version-1;
+        // check for rollback submission only if the Active version is greater than 1 and that too is late.
+        if ($previousVersion && $late_status !== LateDayInfo::STATUS_GOOD) {
+            while($previousVersion) {
+                $graded_gradeable->getAutoGradedGradeable()->setActiveVersion($previousVersion);
+                $prevVersionInstance = $graded_gradeable->getAutoGradedGradeable()->getAutoGradedVersionInstance($previousVersion);
+                $lateInfo = LateDays::fromUser($this->core, $late_days_user)->getLateDayInfoByGradeable($gradeable);
+                $newLateInfo =  LateDays::fromUser($this->core, $late_days_user)->getLateDaysRemainingByContext($prevVersionInstance->getSubmissionTime());
+                $mkj =$graded_gradeable->getAutoGradedGradeable()->getActiveVersion();
+                echo 'checking for version'.$previousVersion.$lateInfo->getStatus().'<-late status , new status->'.$newLateInfo.'\n';
+                echo  'Active version is ' . $graded_gradeable->getAutoGradedGradeable()->getActiveVersion();
+                if ($lateInfo == null or ($lateInfo->getStatus($newLateInfo) == LateDayInfo::STATUS_GOOD) ) {
+                    echo 'found good one\n';
+                    $rollbackSubmission = $previousVersion;
+                    break;
+                }
+                // check the same thing for previous version.
+                $previousVersion-=1;
+            }
+        }
+        // set the active version back to what it was
+        $graded_gradeable->getAutoGradedGradeable()->setActiveVersion($display_version);
 
         $logger_params = array(
             "course_semester" => $this->core->getConfig()->getSemester(),
@@ -1059,7 +1082,7 @@ class ElectronicGraderController extends AbstractController {
         $this->core->getOutput()->addInternalCss('grade-inquiry.css');
         $this->core->getOutput()->addInternalJs('grade-inquiry.js');
         $show_hidden = $this->core->getAccess()->canI("autograding.show_hidden_cases", ["gradeable" => $gradeable]);
-        $this->core->getOutput()->renderOutput(array('grading', 'ElectronicGrader'), 'hwGradingPage', $gradeable, $graded_gradeable, $display_version, $progress, $show_hidden, $can_inquiry, $can_verify, $show_verify_all, $show_silent_edit, $late_status, $sort, $direction, $who_id);
+        $this->core->getOutput()->renderOutput(array('grading', 'ElectronicGrader'), 'hwGradingPage', $gradeable, $graded_gradeable, $display_version, $progress, $show_hidden, $can_inquiry, $can_verify, $show_verify_all, $show_silent_edit, $late_status, $rollbackSubmission, $sort, $direction, $who_id);
         $this->core->getOutput()->renderOutput(array('grading', 'ElectronicGrader'), 'popupStudents');
         $this->core->getOutput()->renderOutput(array('grading', 'ElectronicGrader'), 'popupMarkConflicts');
         $this->core->getOutput()->renderOutput(array('grading', 'ElectronicGrader'), 'popupSettings');
--- site/app/models/gradeable/LateDayInfo.php
+++ site/app/models/gradeable/LateDayInfo.php
@@ -105,21 +105,25 @@ class LateDayInfo extends AbstractModel {
 
     /**
      * Gets the late status of the gradeable
+     * @param int $relative_days optional
      * @return int One of self::STATUS_NO_ACTIVE_VERSION, self::STATUS_BAD, self::STATUS_LATE, or self::STATUS_GOOD
      */
-    public function getStatus() {
+    public function getStatus(int $relative_days = null) {
+
         // No late days info, so NO_SUBMISSION
         if (!$this->hasLateDaysInfo()) {
             return self::STATUS_NO_ACTIVE_VERSION;
         }
 
+        $days = $relative_days !==null ? $relative_days : $this->getDaysLate();
+
         // If the number of days late is more than the number allowed, then its BAD
-        if ($this->getDaysLate() > $this->getLateDaysAllowed()) {
+        if ($days > $this->getLateDaysAllowed()) {
             return self::STATUS_BAD;
         }
 
         // if the student submitted after the deadline (plus extensions) then its late
-        if ($this->getDaysLate() > $this->getLateDayException()) {
+        if ($days > $this->getLateDayException()) {
             return self::STATUS_LATE;
         }
 
