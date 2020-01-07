6e144b2b0764072f86883f965c60d2620728d7ec
Barb Cutler <bmcutler@gmail.com>
2020-01-07 11:36:51 -0500

    update sysadmintools version

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -9,7 +9,7 @@ export AnalysisTools_Version=v.18.06.00
 export Lichen_Version=v19.12.00
 export RainbowGrades_Version=v19.07.00
 export Tutorial_Version=v.19.02.00
-export SysadminTools_Version=v19.12.00
+export SysadminTools_Version=v20.01.00
 
 # JAVA
 export JUNIT_VERSION=4.12
