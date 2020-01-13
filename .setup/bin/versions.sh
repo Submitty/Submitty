a44c4db2d8205ab40ee61b1435305c36ad06efb9
Barb Cutler <bmcutler@gmail.com>
2020-01-13 17:16:54 -0500

    update tutorial version

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -8,7 +8,7 @@
 export AnalysisTools_Version=v.18.06.00
 export Lichen_Version=v19.12.00
 export RainbowGrades_Version=v19.07.00
-export Tutorial_Version=v.19.02.00
+export Tutorial_Version=v20.01.00
 export SysadminTools_Version=v20.01.00
 
 # JAVA
