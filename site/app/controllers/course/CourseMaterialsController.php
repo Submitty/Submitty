9e19dfd2fc684064e1d58229a2b07ab4ff44b39c
mukul-kmr-jha <jimickeyji@gmail.com>
2020-01-18 23:34:44 +0530

    Course Materials Download Folder Behavior Using Section-specific Materials

--- site/app/controllers/course/CourseMaterialsController.php
+++ site/app/controllers/course/CourseMaterialsController.php
@@ -105,47 +105,73 @@ class CourseMaterialsController extends AbstractController {
         // makes a random zip file name on the server
         $temp_name = uniqid($this->core->getUser()->getId(), true);
         $zip_name = $temp_dir . "/" . $temp_name . ".zip";
+        // replacing any whitespace with underscore char.
         $zip_file_name = preg_replace('/\s+/', '_', $dir_name) . ".zip";
+        $file_data = $this->core->getConfig()->getCoursePath() . '/uploads/course_materials_file_data.json';
+        $json = FileUtils::readJsonFile($file_data);
 
         $zip = new \ZipArchive();
         $zip->open($zip_name, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 
-        if (!$this->core->getUser()->accessGrading()) {
-            // if the user is not the instructor
-            // download all accessible files according to course_materials_file_data.json
-            $file_data = $this->core->getConfig()->getCoursePath() . '/uploads/course_materials_file_data.json';
-            $json = FileUtils::readJsonFile($file_data);
-            foreach ($json as $path => $file) {
-                if (!CourseMaterial::isSectionAllowed($this->core, $path, $this->core->getUser())) {
-                    $this->core->getOutput()->showError("Your section may not access this file.");
-                    return false;
-                }
+//        if (!$this->core->getUser()->accessGrading()) {
+//            // if the user is not the instructor
+//            // download all accessible files according to course_materials_file_data.json
+//            $file_data = $this->core->getConfig()->getCoursePath() . '/uploads/course_materials_file_data.json';
+//            $json = FileUtils::readJsonFile($file_data);
+//            foreach ($json as $path => $file) {
+//                if (!CourseMaterial::isSectionAllowed($this->core, $path, $this->core->getUser())) {
+//                    continue;
+//                }
+//
+//                // check if the file is in the requested folder
+//                if (!Utils::startsWith(realpath($path), $root_path) && $file['release_datetime'] < $this->core->getDateTimeNow()->format("Y-m-d H:i:sO") ) {
+//
+//                    $file_path = $file->getRealPath();
+//                    $relativePath = substr($file_path, strlen($root_path) + 1);
+//
+//                    $zip->addFile($file_path, $relativePath);
+//                    $relative_path = substr($path, strlen($root_path) + 1);
+//                    $zip->addFile($path, $relative_path);
+//                    continue;
+//                }
+//            }
+//        }
+//        else {
+//            // if the user is an instructor
+//            // download all files
+//            $files = new \RecursiveIteratorIterator(
+//                new \RecursiveDirectoryIterator($root_path),
+//                \RecursiveIteratorIterator::LEAVES_ONLY
+//            );
+//
+//            foreach ($files as $name => $file) {
+//                if (!$file->isDir()) {
+//                    $file_path = $file->getRealPath();
+//                    $relativePath = substr($file_path, strlen($root_path) + 1);
+//
+//                    $zip->addFile($file_path, $relativePath);
+//                }
+//            }
+//        }
 
-                // check if the file is in the requested folder
-                if (!Utils::startsWith(realpath($path), $root_path)) {
-                    continue;
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($root_path),
+            \RecursiveIteratorIterator::LEAVES_ONLY
+        );
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $file_path = $file->getRealPath();
+                if (!$this->core->getUser()->accessGrading()) {
+                    if (CourseMaterial::isSectionAllowed($this->core, $file_path, $this->core->getUser()) && $json[$file_path]['release_datetime'] < $this->core->getDateTimeNow()->format("Y-m-d H:i:sO")) {
+                        $relativePath = substr($file_path, strlen($root_path) + 1);
+                        $zip->addFile($file_path, $relativePath);
+                    }
                 }
-                if ($file['release_datetime'] < $this->core->getDateTimeNow()->format("Y-m-d H:i:sO")) {
-                    $relative_path = substr($path, strlen($root_path) + 1);
-                    $zip->addFile($path, $relative_path);
-                }
-            }
-        }
-        else {
-            // if the user is an instructor
-            // download all files
-            $files = new \RecursiveIteratorIterator(
-                new \RecursiveDirectoryIterator($root_path),
-                \RecursiveIteratorIterator::LEAVES_ONLY
-            );
-
-            foreach ($files as $name => $file) {
-                if (!$file->isDir()) {
-                    $file_path = $file->getRealPath();
+                else {
                     $relativePath = substr($file_path, strlen($root_path) + 1);
-
                     $zip->addFile($file_path, $relativePath);
                 }
+
             }
         }
 
