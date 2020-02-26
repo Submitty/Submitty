4d96673a9aa78c394dc970982b9fb666d63ed5ec
Matthew Peveler <matt.peveler@gmail.com>
2020-02-26 14:23:03 -0500

    [Feature:Vagrant] Set vagrant to use localhost to access site instead of ip address

--- .setup/bin/recreate_sample_courses.sh
+++ .setup/bin/recreate_sample_courses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Automated regeneration of sample course data 
+# Automated regeneration of sample course data
 
 ########################################################################
 
@@ -23,7 +23,7 @@ fi
 cd ../../
 
 python3 ./.setup/bin/partial_reset.py
-python3 ./.setup/bin/setup_sample_courses.py --no_submissions --submission_url http://192.168.56.111
+python3 ./.setup/bin/setup_sample_courses.py
 
 PHP_VERSION=$(php -r 'print PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
 service php${PHP_VERSION}-fpm restart
