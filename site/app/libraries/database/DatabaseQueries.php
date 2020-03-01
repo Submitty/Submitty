a03df52d8275c9742d26c889827b8de2cc47da5d
DefinitiveAbove <definitiveabove@gmail.com>
2020-02-29 19:13:32 -0500

    [Feature:Forum] Added split post functionality (#5031)
    * [Feature:Forum] Added split post functionality
    
    Allows for a non top level post to be split into another thread.
    
    * Added in line about post title
    
    * Actually fixed linting
    
    * Actually actually fixed linting
    
    * Made requested changes
    
    * Added back insert
    
    * More accurate todo
    
    * Made more requested changes
    
    Co-authored-by: Andrew Aikens <andrewaikens87@gmail.com>

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -452,6 +452,62 @@ WHERE status = 1"
         return $categories_list;
     }
 
+    public function splitPost($post_id, $title, $categories_ids) {
+        $old_thread_id = -1;
+        $thread_id = -1;
+        $post = $this->core->getQueries()->getPost($post_id);
+        // Safety measure in case the database is bad for some reason
+        $counted_posts = array();
+        if (!empty($post)) {
+            if ($post["parent_id"] != -1) {
+                $old_thread_id = $post["thread_id"];
+                $this->course_db->query("SELECT id from threads where merged_post_id = ?", array($post_id));
+                $thread_id = $this->course_db->rows();
+                if (count($thread_id) > 0) {
+                    $thread_id = $thread_id[0]["id"];
+                    $this->course_db->query("UPDATE threads set merged_thread_id=-1, merged_post_id=-1 where id=?", array($thread_id));
+                    $this->course_db->query("DELETE FROM thread_categories where thread_id=?", array($thread_id));
+                }
+                else {
+                    //TODO: Update AbstractDatabase.php to work with returning syntax
+                    $this->course_db->query("INSERT INTO threads (title, created_by, is_visible, lock_thread_date) VALUES (?, ?, ?, ?)", array($title, $post["author_user_id"], true, null));
+                    $this->course_db->query("SELECT MAX(id) as max_id from threads where title=? and created_by=?", array($title, $post["author_user_id"]));
+                    $thread_id = $this->course_db->rows()[0]["max_id"];
+                }
+                $str = "";
+                $arr = array();
+                foreach ($categories_ids as $id) {
+                    if (!empty($str)) {
+                        $str .= ", ";
+                    }
+                    $str .= "({$thread_id}, ?)";
+                    array_push($arr, $id);
+                }
+                $this->course_db->query("INSERT INTO thread_categories (thread_id, category_id) VALUES {$str}", $arr);
+                $posts = array($post);
+                while (count($posts) > 0) {
+                    $check_posts = array();
+                    $str = "";
+                    foreach ($posts as $check_post) {
+                        if (!in_array($check_post["id"], $counted_posts)) {
+                            $check_posts[] = $check_post["id"];
+                            $str .= "?, ";
+                            $counted_posts[] = $check_post["id"];
+                            $this->course_db->query("UPDATE posts set thread_id = ? where parent_id = ?", array($thread_id, $check_post["id"]));
+                        }
+                    }
+                    if (strlen($str) > 0) {
+                        $str = substr($str, 0, -2);
+                    }
+                    $this->course_db->query("SELECT id from posts where parent_id in (" . $str . ")", $check_posts);
+                    $posts = $this->course_db->rows();
+                }
+                $this->course_db->query("UPDATE posts set thread_id=?, parent_id=? where id=?", array($thread_id, -1, $post_id));
+            }
+        }
+        return array($old_thread_id, $thread_id, $counted_posts);
+    }
+
     public function createPost($user, $content, $thread_id, $anonymous, $type, $first, $hasAttachment, $markdown, $parent_post = -1) {
         if (!$first && $parent_post == 0) {
             $this->course_db->query("SELECT MIN(id) as id FROM posts where thread_id = ?", array($thread_id));
@@ -531,6 +587,19 @@ WHERE status = 1"
     public function getPostHistory($post_id) {
         $this->course_db->query("SELECT * FROM forum_posts_history where post_id = ? ORDER BY edit_timestamp DESC", array($post_id));
         return $this->course_db->rows();
+    }
+
+    public function getPostOldThread($post_id) {
+        $this->course_db->query("SELECT id, merged_thread_id, title FROM threads WHERE merged_thread_id <> -1 AND merged_post_id = ?", array($post_id));
+        $rows = $this->course_db->rows();
+        if (count($rows) > 0) {
+            return $rows[0];
+        }
+        else {
+            $rows = array();
+            $rows["merged_thread_id"] = -1;
+            return $rows;
+        }
     }
 
     public function getDeletedPostsByUser($user) {
--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -358,6 +358,7 @@ class ForumThreadView extends AbstractView {
                 "edit_url" => $this->core->buildCourseUrl(['forum', 'posts', 'modify']) . '?' . http_build_query(['modify_type' => '1']),
                 "search_url" => $this->core->buildCourseUrl(['forum', 'search']),
                 "merge_url" => $this->core->buildCourseUrl(['forum', 'threads', 'merge']),
+                "split_url" => $this->core->buildCourseUrl(['forum', 'posts', 'split']),
                 "post_content_limit" => $post_content_limit
             ]);
         }
@@ -381,6 +382,7 @@ class ForumThreadView extends AbstractView {
                 "activeThreadTitle" => $generatePostContent["activeThreadTitle"],
                 "post_box_id" => $generatePostContent["post_box_id"],
                 "merge_url" => $this->core->buildCourseUrl(['forum', 'threads', 'merge']),
+                "split_url" => $this->core->buildCourseUrl(['forum', 'posts', 'split']),
                 "post_content_limit" => $post_content_limit
             ]);
 
@@ -529,7 +531,8 @@ class ForumThreadView extends AbstractView {
                 "activeThreadTitle" => $activeThreadTitle,
                 "post_box_id" => $post_box_id,
                 "total_attachments" => $totalAttachments,
-                "merge_url" => $this->core->buildCourseUrl(['forum', 'threads', 'merge'])
+                "merge_url" => $this->core->buildCourseUrl(['forum', 'threads', 'merge']),
+                "split_url" => $this->core->buildCourseUrl(['forum', 'posts', 'split'])
             ]);
         }
         else {
@@ -828,7 +831,7 @@ class ForumThreadView extends AbstractView {
 
         $post_user_info = [];
 
-
+        $merged_thread = false;
         if ($this->core->getUser()->getGroup() <= 2) {
             $info_name = $first_name . " " . $last_name . " (" . $post['author_user_id'] . ")";
             $visible_user_json = json_encode($visible_username);
@@ -866,6 +869,13 @@ class ForumThreadView extends AbstractView {
                     "ud_button_title" => $ud_button_title,
                     "ud_button_icon" => $ud_button_icon
                 ];
+
+                if ($this->core->getUser()->accessGrading()) {
+                    $merged_thread_query = $this->core->getQueries()->getPostOldThread($post_id);
+                    if ($merged_thread_query["merged_thread_id"] != -1) {
+                        $merged_thread = true;
+                    }
+                }
 
                 $shouldEditThread = null;
 
@@ -958,7 +968,8 @@ class ForumThreadView extends AbstractView {
             "post_box_id" => $post_box_id,
             "thread_id" => $thread_id,
             "parent_id" => $post_id,
-            "render_markdown" => $markdown
+            "render_markdown" => $markdown,
+            "thread_previously_merged" => $merged_thread
         ];
     }
 
