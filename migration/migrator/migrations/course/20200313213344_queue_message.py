1d6d709c8fcc26838a545a3f46081de0e836ef6d
Eli Schiff <eli@elischiff.org>
2020-03-16 17:17:03 -0400

    added down migration and removed default message

--- migration/migrator/migrations/course/20200313213344_queue_message.py
+++ migration/migrator/migrations/course/20200313213344_queue_message.py
@@ -23,7 +23,7 @@ def up(config, database, semester, course):
             j = json.load(in_file)
 
         if 'queue_message' not in j['course_details']:
-            j['course_details']['queue_message'] = 'Welcome to the office hours/lab queue. Choose a queue name and enter the code secret code you have been provided.'
+            j['course_details']['queue_message'] = ''
 
         with open(config_file, 'w') as out_file:
             json.dump(j, out_file, indent=4)
@@ -42,4 +42,16 @@ def down(config, database, semester, course):
     :param course: Code of course being migrated
     :type course: str
     """
-    pass
+
+    course_dir = Path(config.submitty['submitty_data_dir'], 'courses', semester, course)
+    # add boolean to course config
+    config_file = Path(course_dir, 'config', 'config.json')
+    if config_file.is_file():
+        with open(config_file, 'r') as in_file:
+            j = json.load(in_file)
+
+        if 'queue_message' in j['course_details']:
+            del j['course_details']['queue_message']
+
+        with open(config_file, 'w') as out_file:
+            json.dump(j, out_file, indent=4)
