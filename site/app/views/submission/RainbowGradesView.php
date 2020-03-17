be95b5d7fb46724540be31022771f6c57c8acc33
Eli Schiff <eli@elischiff.org>
2020-03-17 15:21:31 -0400

    fixed bugs with rainbow grades, and phone menu

--- site/app/views/submission/RainbowGradesView.php
+++ site/app/views/submission/RainbowGradesView.php
@@ -8,6 +8,7 @@ class RainbowGradesView extends AbstractView {
     public function showGrades($grade_file) {
         $display_rainbow_grades_summary = $this->core->getConfig()->displayRainbowGradesSummary();
         $this->core->getOutput()->addBreadcrumb('Rainbow Grades');
+        $this->core->getOutput()->addInternalCss('rainbow-grades.css');
         return $this->core->getOutput()->renderTwigTemplate("submission/RainbowGrades.twig", [
             "show_summary" => $display_rainbow_grades_summary && $grade_file !== null,
             "grade_file" => $grade_file
