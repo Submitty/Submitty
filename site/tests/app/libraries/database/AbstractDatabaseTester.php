631e3353c58a49af2ad58dacc00e0fd0660e5a56
Eli Schiff <eli@elischiff.org>
2020-03-29 23:32:51 -0400

    added missing timezone

--- site/tests/app/libraries/database/AbstractDatabaseTester.php
+++ site/tests/app/libraries/database/AbstractDatabaseTester.php
@@ -323,7 +323,7 @@ SELECT * FROM test ORDER BY pid");
         $this->assertEquals(1, $database->getRowCount());
         $database->query("SELECT * FROM test");
         $this->assertEquals($database->rows()[0]["val_bool"], true);
-        $this->assertEquals($database->rows()[0]["val_date"], $now->format("Y-m-d H:i:sO"));
+        $this->assertEquals($database->rows()[0]["val_date"], $now->format("Y-m-d H:i:sO") . " " . $now->getTimezone()->getName());
         $database->disconnect();
     }
 
