175d56365205285b868073962a097fba0bc89a17
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2020-03-31 20:03:22 -0400

    Get Submitty data directory from config

--- migration/migrator/migrations/system/20191027013443_separate_shipper_dirs.py
+++ migration/migrator/migrations/system/20191027013443_separate_shipper_dirs.py
@@ -1,10 +1,10 @@
 import os
 import shutil
 
-IN_PROGRESS_PATH = '/var/local/submitty/grading'
-
 
 def up(config):
+    IN_PROGRESS_PATH = os.path.join(config.submitty['submitty_data_dir'], 'grading')
+
     os.makedirs(IN_PROGRESS_PATH, exist_ok=True)
     shutil.chown(IN_PROGRESS_PATH, 'submitty_daemon', 'submitty_daemonphp')
     os.chmod(IN_PROGRESS_PATH, 0o770)
