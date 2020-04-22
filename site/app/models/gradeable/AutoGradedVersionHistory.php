299ee6f81d29396a320bfc83a073d45e3209b866
Barb Cutler <bmcutler@gmail.com>
2020-04-22 17:10:44 -0400

    bugfix

--- site/app/models/gradeable/AutoGradedVersionHistory.php
+++ site/app/models/gradeable/AutoGradedVersionHistory.php
@@ -52,7 +52,7 @@ class AutoGradedVersionHistory extends AbstractModel {
         $this->grading_finished = DateUtils::parseDateTime($details['grading_finished'], $timezone);
         $this->batch_regrade = $details['batch_regrade'] === true;
         $this->first_access_time = null;
-        if (array_key_exists('first_access_time',$details))
+        if (array_key_exists('first_access_time', $details))
           $this->first_access_time = DateUtils::parseDateTime($details['first_access_time'], $timezone);
         $this->access_duration = $details['access_duration'] ?? '-1';
         $this->wait_time = $details['wait_time'];
