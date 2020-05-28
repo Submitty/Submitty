4771298b5a922ca0b59cafc892e77f1ed39b3b96
Shail Patel <shailpatel67@gmail.com>
2020-05-28 18:53:02 -0400

    [Refactor:Submission] Refactor Notebooks to use models (#5380)
    * initial work
    
    * fix submission_controller
    
    * progress on updating testcases after parsing a usespecific notebook
    
    * requested changes
    
    * update baseline

--- site/app/models/notebook/AbstractNotebookInput.php
+++ site/app/models/notebook/AbstractNotebookInput.php
@@ -1,21 +1,16 @@
 <?php
 
-namespace app\models\gradeable;
+namespace app\models\notebook;
 
 use app\libraries\Core;
 use app\models\AbstractModel;
 
 /**
- * Class AbstractGradeableInput
- * @package app\models\gradeable
- *
- * Information required to load gradeable input submissions on the submission page
- *
  * @method string getFileName()
  * @method array[] getImages()
  * @method string getLabel()
  */
-class AbstractGradeableInput extends AbstractModel {
+class AbstractNotebookInput extends AbstractModel {
 
     /** @prop @var string The name of the file to save text box data to */
     protected $file_name;
