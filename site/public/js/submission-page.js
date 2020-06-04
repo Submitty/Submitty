910ca3498acb5dca8e1c9e0319cde5e7b837570f
Evan Maicus <emaicus@outlook.com>
2020-06-04 08:55:41 -0400

    Add missing let
    Co-authored-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/public/js/submission-page.js
+++ site/public/js/submission-page.js
@@ -1,6 +1,6 @@
 
 function openActionsPopup(popup_css, element_id) {
-  elem_html = `<link rel="stylesheet" type="text/css" href="${popup_css}" />`;
+  let elem_html = `<link rel="stylesheet" type="text/css" href="${popup_css}" />`;
   elem_html += document.getElementById(element_id).innerHTML;
   my_window = window.open("", "_blank", "status=1,width=750,height=500");
   my_window.document.write(elem_html);
