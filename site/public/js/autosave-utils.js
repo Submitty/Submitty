32d0a1219d0ccba58fe0b215005c2b10b73d1c6f
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2020-06-07 17:46:35 -0400

    Move common autosave functions into separate file

---
+++ site/public/js/autosave-utils.js
@@ -0,0 +1,101 @@
+/**
+ * This value will be true if autosave is enabled for this session.
+ * 
+ * Specifically, we try to set a test value in localStorage and immediately 
+ * remove the test value. If all goes well, then localStorage is available and
+ * we can auto-save to there. Otherwise, we simply disable auto-saving.
+ * 
+ * In particular, setting an item can fail in one of two scenarios:
+ * * The local storage quota is full
+ * * The `localStorage` variable simply doesn't exist and therefore the browser doesn't support it.
+ * 
+ * The first point isn't too relevant as we don't expect to be storing a ton of
+ * data; however, Safari on iOS in Private Mode sets the quota to zero, meaning 
+ * that every attempt to write to localStorage will throw an exception. 
+ * Throwing an exception every keypress isn't great, hence the primary 
+ * motivation for this block here.
+ * 
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage|MDN} on detecting localStorage.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem|Storage.setItem()} documentation.
+ */
+const autosaveEnabled = (() => {
+    try {
+        localStorage.setItem('TEST', 'TEST');
+        localStorage.removeItem('TEST');
+        return true;
+    } catch (e) {
+        return false;
+    }
+})();
+
+function msToDays(ms) {
+    return ms / (1000 * 60 * 60 * 24);
+}
+
+/**
+ * Scans localStorage data and deletes any autosave entries older than thirty days.
+ *  
+ * Data is classified as autosave if its key ends in the provided suffix. Each
+ * autosave entry is expected to be a JSON object with a "timestamp" key. If
+ * an autosave entry could not be parsed, or if it does not have a "timestamp"
+ * key, then it is assumed to be corrupted and will therefore be deleted.
+ * 
+ * @param {String} suffix - Suffix of autosave entries to delete.
+ */
+function cleanupAutosaveHistory(suffix) {
+    if (autosaveEnabled) {
+        const toDelete = [];
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (!key.endsWith(suffix)) {
+                continue;
+            }
+
+            try {
+                const { timestamp } = JSON.parse(localStorage.getItem(key));
+                
+                if (msToDays(Date.now() - timestamp) > 30) {
+                    toDelete.push(key);
+                }
+            } catch (e) {
+                // This item has gotten corrupted somehow; let's delete it 
+                // instead of letting it linger around and taking up space.
+                toDelete.push(key);
+            }
+        }
+        toDelete.forEach(localStorage.removeItem, localStorage);
+    }
+}
+
+/**
+ * Event handler for the `onbeforeunload` event; saves unsaved changes to 
+ * localStorage (if enabled) and prompts the "Are you sure you want to exit?" 
+ * dialog box.
+ * @param {Event} e Event object for this event
+ */
+function saveAndWarnUnsubmitted(e) {
+    saveNotebookToLocal();
+    // For Firefox
+    e.preventDefault();
+    // For Chrome
+    e.returnValue = '';
+    return true;
+}
+
+let isSaveScheduled = false;
+/**
+ * Schedules a save action for ten seconds from now if no save has been 
+ * scheduled yet. This way we don't re-write minor changes when modifying a
+ * text box answer.
+ * 
+ * @param {() => void} saveCallback - Callback that triggers the save action.
+ */
+function deferredSave(saveCallback) {
+    if (autosaveEnabled && !isSaveScheduled) {
+        setTimeout(() => {
+            saveCallback();
+            isSaveScheduled = false;
+        }, 10 * 1000);
+        isSaveScheduled = true;
+    }
+}
