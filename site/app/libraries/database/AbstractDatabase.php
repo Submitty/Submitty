db5016691d42b98fa5e264e4aae26c9bcceecb70
Matthew Peveler <matt.peveler@gmail.com>
2020-06-09 19:00:42 +0300

    [Refactor:PHP] Fix static analysis issues in app/libraries/database (#5482)
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/app/libraries/database/AbstractDatabase.php
+++ site/app/libraries/database/AbstractDatabase.php
@@ -8,7 +8,7 @@ use app\libraries\Utils;
 abstract class AbstractDatabase {
 
     /**
-     * @var \PDO
+     * @var \PDO|null
      */
     protected $link = null;
 
@@ -194,15 +194,15 @@ abstract class AbstractDatabase {
      * run it through the query() function which will just return a boolean on if the function suceeded or
      * not. In all cases, it will throw a {@see DatabaseException} on an invalid query.
      *
-     * @param $query
-     * @param $parameters
-     * @param $callback
+     * @param string $query
+     * @param array $parameters
+     * @param callable|null $callback
      *
      * @return DatabaseRowIterator|bool
      *
      * @throws \app\exceptions\DatabaseException
      */
-    public function queryIterator($query, $parameters = [], $callback = null) {
+    public function queryIterator(string $query, array $parameters = [], $callback = null) {
         $lower = trim(strtolower($query));
         if (!Utils::startsWith($lower, "select")) {
             return $this->query($query, $parameters);
@@ -237,12 +237,12 @@ abstract class AbstractDatabase {
     }
 
     /**
-     * @param $result
-     * @param $columns
+     * @param array $result
+     * @param array $columns
      *
      * @return mixed
      */
-    public function transformResult($result, $columns) {
+    public function transformResult(array $result, array $columns) {
         foreach ($result as $col => $value) {
             if (isset($columns[$col])) {
                 $column = $columns[$col];
@@ -262,7 +262,7 @@ abstract class AbstractDatabase {
      * Start a DB transaction, turning off autocommit mode. Queries won't be
      * actually commited to the database till Database::commit() is called.
      */
-    public function beginTransaction() {
+    public function beginTransaction(): void {
         if (!$this->transaction) {
             $this->transaction = $this->link->beginTransaction();
         }
@@ -271,14 +271,14 @@ abstract class AbstractDatabase {
     /**
      * Actually commit/execute all queries to the database since we began the transaction.
      */
-    public function commit() {
+    public function commit(): void {
         if ($this->transaction) {
             $this->link->commit();
             $this->transaction = false;
         }
     }
 
-    public function rollback() {
+    public function rollback(): void {
         if ($this->transaction) {
             $this->link->rollBack();
             $this->transaction = false;
@@ -306,7 +306,7 @@ abstract class AbstractDatabase {
      *
      * @return array
      */
-    public function rows() {
+    public function rows(): array {
         if ($this->results !== null && count($this->results) > 0) {
             return $this->results;
         }
@@ -324,20 +324,18 @@ abstract class AbstractDatabase {
      *
      * @return int
      */
-    public function getRowCount() {
+    public function getRowCount(): int {
         return $this->row_count;
     }
 
     /**
      * Return count of total queries run against current PDO connection
-     *
-     * @return int
      */
-    public function getQueryCount() {
+    public function getQueryCount(): int {
         return count($this->all_queries);
     }
 
-    public function getQueries() {
+    public function getQueries(): array {
         return $this->all_queries;
     }
 
@@ -346,7 +344,7 @@ abstract class AbstractDatabase {
      *
      * @return string[]
      */
-    public function getPrintQueries() {
+    public function getPrintQueries(): array {
         $print = [];
         foreach ($this->all_queries as $query) {
             foreach ($query[1] as $parameter) {
@@ -360,7 +358,7 @@ abstract class AbstractDatabase {
     /**
      * @return bool
      */
-    public function inTransaction() {
+    public function inTransaction(): bool {
         return $this->transaction;
     }
 
@@ -382,9 +380,9 @@ abstract class AbstractDatabase {
      * booleans, we generally use a tinyint(1) and just use 0/1 to represent it else we convert it to whatever
      * works for that given database.
      *
-     * @param $value
+     * @param mixed $value
      *
-     * @return string
+     * @return mixed
      */
     public function convertBoolean($value) {
         return ($value === true) ? 1 : 0;
--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -2335,8 +2335,6 @@ VALUES(?, ?, ?, ?, 0, 0, 0, 0, ?)",
      * @param string $session_id
      * @param string $user_id
      * @param string $csrf_token
-     *
-     * @return string
      */
     public function newSession($session_id, $user_id, $csrf_token) {
         $this->submitty_db->query(
@@ -3904,10 +3902,9 @@ AND gc_id IN (
     }
 
     public function getRegradeRequestStatus($user_id, $gradeable_id) {
-        $row = $this->course_db->query("SELECT * FROM regrade_requests WHERE user_id = ? AND g_id = ? ", [$user_id, $gradeable_id]);
-        return ($this->course_db->row()) ? $row['status'] : 0;
+        $this->course_db->query("SELECT * FROM regrade_requests WHERE user_id = ? AND g_id = ? ", [$user_id, $gradeable_id]);
+        return ($this->course_db->getRowCount() > 0) ? $this->course_db->row()['status'] : 0;
     }
-
 
     public function insertNewRegradeRequest(GradedGradeable $graded_gradeable, User $sender, string $initial_message, $gc_id) {
         $params = [$graded_gradeable->getGradeableId(), $graded_gradeable->getSubmitter()->getId(), RegradeRequest::STATUS_ACTIVE, $gc_id];
@@ -3924,11 +3921,13 @@ AND gc_id IN (
             throw $dbException;
         }
     }
+
     public function getNumberGradeInquiries($gradeable_id, $is_grade_inquiry_per_component_allowed = true) {
         $grade_inquiry_all_only_query = !$is_grade_inquiry_per_component_allowed ? ' AND gc_id IS NULL' : '';
         $this->course_db->query("SELECT COUNT(*) AS cnt FROM regrade_requests WHERE g_id = ? AND status = -1" . $grade_inquiry_all_only_query, [$gradeable_id]);
         return ($this->course_db->row()['cnt']);
     }
+
     public function getRegradeDiscussions(array $grade_inquiries) {
         if (count($grade_inquiries) == 0) {
             return [];
@@ -3967,6 +3966,7 @@ AND gc_id IN (
         $this->course_db->query("DELETE FROM regrade_discussion WHERE regrade_id = ?", $regrade_id);
         $this->course_db->query("DELETE FROM regrade_requests WHERE id = ?", $regrade_id);
     }
+
     public function deleteGradeable($g_id) {
         $this->course_db->query("DELETE FROM gradeable WHERE g_id=?", [$g_id]);
     }
