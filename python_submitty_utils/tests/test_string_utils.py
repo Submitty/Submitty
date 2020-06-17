8dc246dc274498915fe3673d37cbace546521eb6
Barb Cutler <bmcutler@users.noreply.github.com>
2020-06-16 20:17:04 -0400

    Apply suggestions from code review
    Co-authored-by: Matthew Peveler <matt.peveler@gmail.com>

--- python_submitty_utils/tests/test_string_utils.py
+++ python_submitty_utils/tests/test_string_utils.py
@@ -5,15 +5,13 @@ from submitty_utils import string_utils
 
 class TestUser(unittest.TestCase):
     def testNegativeLength(self):
-        for i in range(-100, 0):
-            self.assertEqual(string_utils.generate_random_string(i), '')
+        self.assertEqual(string_utils.generate_random_string(-1), '')
 
     def testZeroLength(self):
         self.assertEqual(string_utils.generate_random_string(0), '')
 
-    def testPositiveLengths(self):
-        for i in range(1, 100):
-            self.assertEqual(len(string_utils.generate_random_string(i)), i)
+    def testPositiveLength(self):
+        self.assertEqual(len(string_utils.generate_random_string(1)), 1)
 
     def testRandom(self):
         # Very low chance of generating the same string twice.
