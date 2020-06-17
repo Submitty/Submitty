ced0de397e347e4713c4e6b530afca22ebef02ea
Matthew Peveler <matt.peveler@gmail.com>
2020-06-17 11:37:59 -0400

    [Refactor:SubmittyUtils] Cleanup dateutils.write_submitty_date to be more consistent
    This updates the write_submitty_date function to be a bit more consistent and with less
    potential foot-guns. First, instead of printing an error to console, but still returning
    if given an invalid type, this function will now throw an exception, so that if someone
    were to somehow get an integer into this function, we should not return an integer back
    and have some other crash later in an unrelated system. Scanning through the codebase,
    we always use this function with datetimes at the moment at least. Second, remove the
    space between the milliseconds and timezone if using milliseconds to match the format of
    the non-milliseconds datetime string, as well as just generally where someone would expect
    the "-0400" string. Third, the optional parameter to add milliseconds to the datetime
    string was named "microseconds", and so it was renamed to "milliseconds" to be consistent
    with what it was actually doing.
    
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- python_submitty_utils/submitty_utils/dateutils.py
+++ python_submitty_utils/submitty_utils/dateutils.py
@@ -41,31 +41,29 @@ def get_current_time():
     return datetime.now(get_timezone())
 
 
-def write_submitty_date(d=None, microseconds=False):
+def write_submitty_date(d=None, milliseconds=False):
     """
     Converts a datetime object to a string with a timezone. If the datetime object
     does not have a timezone, it'll use the server's timezone.
 
     :param d: datetime object you want to convert to string
+    :param milliseconds: add milliseconds to returned string
     :return:
-
-    FIXME: We should not be printing anything out here
     """
     if d is None:
         d = get_current_time()
     if not isinstance(d, datetime):
-        print("ERROR:  ", d, " is not a datetime object, it is of type ", type(d))
-        return d
-    my_timezone = d.tzinfo
-    if my_timezone is None:
-        print("ERROR: NO TIMEZONE ", d, " assuming local timezone")
-        my_timezone = get_timezone()
-        d = my_timezone.localize(d)
-    answer = d.strftime("%Y-%m-%d %H:%M:%S%z")
-    if microseconds:
-        mlsec = d.strftime("%f")
-        mlsec = mlsec[0:3]
-        answer = d.strftime("%Y-%m-%d %H:%M:%S.{} %z".format(mlsec))
+        raise TypeError(
+            f"Invalid type. Expected datetime or datetime string, got {type(d)}."
+        )
+    if d.tzinfo is None:
+        d = get_timezone().localize(d)
+
+    if milliseconds:
+        mlsec = d.strftime("%f")[0:3]
+        answer = d.strftime(f"%Y-%m-%d %H:%M:%S.{mlsec}%z")
+    else:
+        answer = d.strftime("%Y-%m-%d %H:%M:%S%z")
     return answer
 
 
