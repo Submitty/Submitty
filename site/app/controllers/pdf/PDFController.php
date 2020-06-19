bc99eee091d1814914b21ece8266d7c49fcbcd92
unknown <hultoj@rpi.edu>
2020-06-19 04:59:04 -0400

    fixes

--- site/app/controllers/pdf/PDFController.php
+++ site/app/controllers/pdf/PDFController.php
@@ -178,7 +178,7 @@ class PDFController extends AbstractController {
         $annotation_body["annotations"] = $_POST['annotation_layer'];                    
         
         $annotation_json = json_encode($annotation_body);
-                        
+                                
         file_put_contents(FileUtils::joinPaths($annotation_version_path, md5($annotation_info["file_path"])) . "_" . $grader_id . '.json', $annotation_json);
         $this->core->getOutput()->renderJsonSuccess('Annotation saved successfully!');
         return true;
