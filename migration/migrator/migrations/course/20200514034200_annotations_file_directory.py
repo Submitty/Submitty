f8d5c76770d84b76cdb266ab0d29d90ffbb30ad0
unknown <hultoj@rpi.edu>
2020-06-22 16:30:50 -0400

    fixed migration

--- migration/migrator/migrations/course/20200514034200_annotations_file_directory.py
+++ migration/migrator/migrations/course/20200514034200_annotations_file_directory.py
@@ -8,7 +8,6 @@ def up(config, database, semester, course):
       
     course_dir = Path(config.submitty['submitty_data_dir'], 'courses', semester, course)
     annotations_dir = Path(course_dir, 'annotations')
-    
     # create the directories
     php_user = config.submitty_users['php_user']
 
@@ -25,14 +24,18 @@ def up(config, database, semester, course):
                     for name in files:
                         if "_" in name:
                             [file_name, grader_id] = name.split('_', 1)
-                            annotation_full_path_sub = annotation_full_path.replace("annotations", "submissions");
+                            submissions_full_path = annotation_full_path.replace("annotations", "submissions");
+                            md5_file_name = hashlib.md5((submissions_full_path + '/' + file_name + '.pdf').encode())
                             #Hash folder + file_name + grader_id where folder is the directory structure after the version directory
                             json_data = {};
+                            for submission_root, submission_dirs, submission_files in os.walk(submissions_full_path):
+                                for submission_file in submission_files:
+                                    if(submission_file[:-4] == file_name):
+                                        md5_file_name = hashlib.md5((os.path.abspath(os.path.join(submission_root, submission_file))).encode())
                             with open(Path(annotation_full_path,name)) as initial_file:
-                                json_data['annotations'] = json.dump(json.load(initial_file))
+                                json_data['annotations'] = json.dumps(json.load(initial_file))
                             json_data['grader_id'] = grader_id[:-5]
                             json_data['file_name'] = file_name
-                            md5_file_name = hashlib.md5((annotation_full_path_sub + '/'+ file_name + '.pdf').encode())
                             file_path = Path(annotations_dir, gradeable_level_dir, user_level_dir, version_level_dir)
                             new_json_file = open(Path(annotation_full_path, md5_file_name.hexdigest() + "_" + grader_id), 'x')
                             json.dump(json_data, new_json_file)
