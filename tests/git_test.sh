bab70f37b9a6830ce3df0da6d553a001b8ed6f29
Matthew Peveler <matt.peveler@gmail.com>
2020-06-24 01:54:51 -0400

    [Testing:Git] expand functional tests
    The current functional tests for Git in Submitty was limited to just
    grabbing repo as instructor and then minimally testing that grabbing
    a repo with invalid permissions would generally fail, though without
    verifying that it failed correctly. This worked well enough for
    verifying apache + sbin/authentication.py was working, but did not
    provide adaquete coverage for when the PHP code errored, such as in
    the result of #5473. The error on the latter testcase was now a 500
    error instead of an unauthorized error, but we did not catch this.
    
    To improve things going forward, this commit adds testing to cover:
    
    1. instructor checking out student repository
    2. student checkout out student repository
    3. student checking out instructor repository fails with specific stderr
       output
    
    This provides more broader coverage of cases to exercise the PHP code,
    as well as make sure we do error appropriately.
    
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- tests/git_test.sh
+++ tests/git_test.sh
@@ -2,19 +2,20 @@ SEMESTER=$(python3 -c 'from datetime import datetime; today = datetime.today(); 
 
 test_git() {
     random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-    git clone http://${1}:${1}@localhost/git/${SEMESTER}/sample/open_homework/$2 ${random_string}_open_homework
-    cd ${random_string}_open_homework
-    touch test.txt
+    git clone http://${1}:${1}@localhost/git/${SEMESTER}/sample/open_homework/$2 open_homework
+    cd open_homework
+    echo "foo" >> test.txt
     git add .
     git commit -m "First commit"
     git push
-    touch test2.txt
+    echo "bar" >> test2.txt
     git add .
     git commit -m "Second commit"
     git push
     git reset --hard HEAD~1
     git pull
     cd ..
+    rm -rf open_homework
 }
 
 cleanup() {
@@ -40,10 +41,9 @@ test_git instructor student
 test_git student student
 
 EXIT_CODE=0
-git clone http://student:student@localhost/git/${SEMESTER}/sample/open_homework/instructor 2> /tmp/submitty_git/git_log || EXIT_CODE=$?
+git clone http://student:student@localhost/git/${SEMESTER}/sample/open_homework/instructor > /tmp/submitty_git/git_stdout 2> /tmp/submitty_git/git_stderr || EXIT_CODE=$?
 test ${EXIT_CODE} -ne 0
-cat /tmp/submitty_git/git_log
-test "fatal: Authentication failed for 'http://localhost/git/s20/sample/open_homework/instructor/'" = "${</tmp/submitty_git/git_log}"
+test "$(echo -e "Cloning into 'instructor'...\nfatal: Authentication failed for 'http://localhost/git/s20/sample/open_homework/instructor/'")" = "$(</tmp/submitty_git/git_stderr)"
 
 cleanup
 popd
