d5634316399be241d831d68bc3535ca07abea59b
josharsh <harsh.joshi.pth@gmail.com>
2020-06-24 22:15:46 +0530

    Lint Fix

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -107,7 +107,7 @@ class ElectronicGraderController extends AbstractController {
             $gradeable->setRandomPeerGradersList($final_grading_info);
             return JsonResponse::getSuccessResponse($final_grading_info);
         }
-        $graded_array = $student_array; 
+        $graded_array = $student_array;
           /*n_array_peers : An Array of arrays that holds information on to be graded peers
           [ [A,B,C,D,E,F], [E,F,A,B,C,D], [C,D,E,F,A,B] ]
           A grades C and E and is graded by C and E.
@@ -125,7 +125,7 @@ class ElectronicGraderController extends AbstractController {
         for ($i = 0; $i < $number_to_grade; ++$i) {
             $random_offset = rand(1, $max_offset);
             if (in_array($random_offset, $offset_array)) {
-                $random_offset = rand($random_offset+1, $max_offset);
+                $random_offset = rand($random_offset + 1, $max_offset);
             }
             array_push($offset_array, $random_offset);
         }
