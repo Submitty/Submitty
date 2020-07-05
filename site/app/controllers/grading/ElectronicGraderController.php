248f80ddc6fa425d67c9c50fe9199e8aeaabacba
josharsh <harsh.joshi.pth@gmail.com>
2020-07-05 19:49:11 +0530

    Lint Fix

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -239,7 +239,7 @@ class ElectronicGraderController extends AbstractController {
         $total_indvidual_students = 0;
         $viewed_grade = 0;
         $num_gradeables = 1;
-        $peers_to_grade=0;
+        $peers_to_grade = 0;
 
         $regrade_requests = $this->core->getQueries()->getNumberGradeInquiries($gradeable_id, $gradeable->isGradeInquiryPerComponentAllowed());
         if ($peer) {
