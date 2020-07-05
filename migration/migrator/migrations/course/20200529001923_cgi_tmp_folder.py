894c920ceec0f34c9f12ac7ead519e5c0fbd9926
mukul-kmr-jha <jimickeyji@gmail.com>
2020-07-05 18:38:41 +0100

    remove unused vars

--- migration/migrator/migrations/course/20200529001923_cgi_tmp_folder.py
+++ migration/migrator/migrations/course/20200529001923_cgi_tmp_folder.py
@@ -13,10 +13,10 @@ def up(config, database, semester, course):
     daemoncgi_group = config.submitty_users['daemoncgi_group']
 
     # set the owner/group/permissions
-    chown_res = subprocess.check_output(["chown", "-R", php_user + ":" + daemoncgi_group + " ", str(tmp_cgi_dir)])
-    chmod_user_res = subprocess.check_output(["chmod", "-R", "u+rwx", str(tmp_cgi_dir)])
-    chmod_group_res = subprocess.check_output(["chmod", "-R", "g+rwxs", str(tmp_cgi_dir)])
-    chmod_other_res = subprocess.check_output(["chmod", "-R", "o-rwx", str(tmp_cgi_dir)])
+    subprocess.check_output(["chown", "-R", php_user + ":" + daemoncgi_group + " ", str(tmp_cgi_dir)])
+    subprocess.check_output(["chmod", "-R", "u+rwx", str(tmp_cgi_dir)])
+    subprocess.check_output(["chmod", "-R", "g+rwxs", str(tmp_cgi_dir)])
+    subprocess.check_output(["chmod", "-R", "o-rwx", str(tmp_cgi_dir)])
 
 def down(config, database, semester, course):
     pass
