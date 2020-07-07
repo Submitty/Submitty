0c9ffe12d05c5fab37498ed9bb9d84f0a5fd9a87
Kevin-MM <kevin.m.mckinney@gmail.com>
2020-07-07 13:49:04 -0400

    Only reorderable widgets will be re-rendered with form changes.

--- site/public/js/notebook_builder/notebook-builder.js
+++ site/public/js/notebook_builder/notebook-builder.js
@@ -6,28 +6,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 class NotebookBuilder {
     constructor() {
-        this.widgets = [];
+        this.reorderable_widgets = [];
         this.selector = new SelectorWidget();
         this.form_options = new FormOptionsWidget();
+
+        const main_div = document.getElementById('notebook-builder');
+        main_div.appendChild(this.selector.render());
+        main_div.appendChild(this.form_options.render());
     }
 
     /**
-     * Display all widgets the notebook builder is keeping track of.
+     * Re-render reorderable widgets
      */
     render() {
-        // Get a handle on the main div
-        const main_div = document.getElementById('notebook-builder');
+        // Get a handle on the widgets div
+        const widgets_div = document.getElementById('reorderable-widgets');
 
         // Clear
-        main_div.innerHTML = '';
+        widgets_div.innerHTML = '';
 
         // Draw widgets
-        this.widgets.forEach(widget => {
-            main_div.appendChild(widget.render());
+        this.reorderable_widgets.forEach(widget => {
+            widgets_div.appendChild(widget.render());
         });
-
-        main_div.appendChild(this.selector.render());
-        main_div.appendChild(this.form_options.render());
     }
 
     /**
@@ -36,7 +37,7 @@ class NotebookBuilder {
     getJSON() {
         const notebook_array = [];
 
-        this.widgets.forEach(widget => {
+        this.reorderable_widgets.forEach(widget => {
             // Ensure we got something back before adding to the notebook_array
             const widget_json = widget.getJSON();
             if (widget_json) {
@@ -56,7 +57,7 @@ class NotebookBuilder {
      * @param {Widget} widget
      */
     widgetAdd(widget) {
-        this.widgets.push(widget);
+        this.reorderable_widgets.push(widget);
         this.render();
     }
 
@@ -66,8 +67,8 @@ class NotebookBuilder {
      * @param {Widget} widget
      */
     widgetRemove(widget) {
-        const index = this.widgets.indexOf(widget);
-        this.widgets.splice(index, 1);
+        const index = this.reorderable_widgets.indexOf(widget);
+        this.reorderable_widgets.splice(index, 1);
         this.render();
     }
 
@@ -77,15 +78,15 @@ class NotebookBuilder {
      * @param {Widget} widget
      */
     widgetUp(widget) {
-        const index = this.widgets.indexOf(widget);
+        const index = this.reorderable_widgets.indexOf(widget);
 
         // If index is 0 then do nothing
         if (index === 0) {
             return;
         }
 
-        this.widgets.splice(index, 1);
-        this.widgets.splice(index - 1, 0, widget);
+        this.reorderable_widgets.splice(index, 1);
+        this.reorderable_widgets.splice(index - 1, 0, widget);
         this.render();
     }
 
@@ -95,15 +96,15 @@ class NotebookBuilder {
      * @param {Widget} widget
      */
     widgetDown(widget) {
-        const index = this.widgets.indexOf(widget);
+        const index = this.reorderable_widgets.indexOf(widget);
 
         // If widget is already at the end of the form then do nothing
-        if (index === this.widgets.length - 1) {
+        if (index === this.reorderable_widgets.length - 1) {
             return;
         }
 
-        this.widgets.splice(index, 1);
-        this.widgets.splice(index + 1, 0, widget);
+        this.reorderable_widgets.splice(index, 1);
+        this.reorderable_widgets.splice(index + 1, 0, widget);
         this.render();
     }
 }
