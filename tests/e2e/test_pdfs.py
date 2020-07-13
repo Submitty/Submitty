5bfe8ec65ace571b30aa7adc5a85e15e3c90fc10
unknown <hultoj@rpi.edu>
2020-07-13 16:42:38 -0400

    changes to sample.yml

--- tests/e2e/test_pdfs.py
+++ tests/e2e/test_pdfs.py
@@ -6,7 +6,7 @@ class TestPDFs(BaseTestCase):
     def __init__(self, testname):
         super().__init__(testname, log_in=False)
 
-    def test_pdf_instructor(self):
+    def test_pdf_instructor_basic_access(self):
         self.log_in(user_id="instructor", user_name="Quinn")
         self.click_class('sample')
         self.driver.find_element_by_xpath('//a[@href="http://localhost:1501/courses/s20/sample/gradeable/grading_homework/grading/status"]').click()
@@ -17,7 +17,6 @@ class TestPDFs(BaseTestCase):
         file_url = "/var/local/submitty/courses/s20/sample/submissions/grading_homework/dbFCC0LPYC3bAjc/1/part1_buggy2.py"
         self.driver.find_element_by_xpath('//a[@file-url="'+file_url+'"]').click()
         self.driver.find_element_by_id('pageContainer1')
-
 
 if __name__ == "__main__":
     import unittest
