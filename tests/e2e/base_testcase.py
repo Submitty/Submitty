c3ca592918c793ebeadd768ebb46624416ac50bd
Marwan Atef <marwanatef97@gmail.com>
2020-07-13 02:46:13 +0200

    fix e2e import error

--- tests/e2e/base_testcase.py
+++ tests/e2e/base_testcase.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-from python_submitty_utils.submitty_utils import dateutils
+from submitty_utils import dateutils
 
 
 # noinspection PyPep8Naming
