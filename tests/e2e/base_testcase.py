5f5c9632670a8b5fc5395a2e02f24ebe69fe1d79
unknown <hultoj@rpi.edu>
2020-07-14 11:51:01 -0400

    working test

--- tests/e2e/base_testcase.py
+++ tests/e2e/base_testcase.py
@@ -62,7 +62,7 @@ class BaseTestCase(unittest.TestCase):
         if user_password is None and user_id is not None:
             user_password = user_id
         self.user_password = user_password if user_password is not None else BaseTestCase.USER_PASSWORD
-        self.semester = "s20"
+        self.semester = "f20"
         self.full_semester = BaseTestCase.get_display_semester(self.semester)
         self.logged_in = False
         self.use_log_in = log_in
@@ -144,7 +144,7 @@ class BaseTestCase(unittest.TestCase):
         if course_name is None:
             course_name = course
         course_name = course_name.title()
-        self.driver.find_element(By.ID, "s20" + '_' + course).click()
+        self.driver.find_element(By.ID, "f20" + '_' + course).click()
         # print(self.driver.page_source)
         WebDriverWait(self.driver, BaseTestCase.WAIT_TIME).until(EC.title_is('Submitty ' + course_name + ' Gradeables'))
 
--- tests/e2e/test_pdfs.py
+++ tests/e2e/test_pdfs.py
@@ -9,13 +9,12 @@ class TestPDFs(BaseTestCase):
     def test_pdf_instructor_basic_access(self):
         self.log_in(user_id="instructor", user_name="Quinn")
         self.click_class('sample')
-        self.driver.find_element_by_xpath('//a[@href="http://localhost:1501/courses/s20/sample/gradeable/grading_homework/grading/status"]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_peer/grading/status")]').click()
         self.driver.find_element_by_link_text("Grading Index").click()
-        student_url = "http://localhost:1501/courses/s20/sample/gradeable/grading_homework/grading/grade?who_id=aufded&sort=id&direction=ASC"
-        self.driver.find_element_by_xpath('//a[@href="'+student_url+'"]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_peer/grading/grade?who_id=bechta&sort=id&direction=ASC")]').click()
         self.driver.find_element_by_id('submissions').click()
-        file_url = "/var/local/submitty/courses/s20/sample/submissions/grading_homework/dbFCC0LPYC3bAjc/1/part1_buggy2.py"
-        self.driver.find_element_by_xpath('//a[@file-url="'+file_url+'"]').click()
+        self.driver.find_element_by_xpath('//a[contains(@file-url,"words_249.pdf")]').click()
+        self.driver.implicitly_wait(10)
         self.driver.find_element_by_id('pageContainer1')
 
 if __name__ == "__main__":
