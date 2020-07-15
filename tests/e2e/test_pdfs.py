1a1cf22686e878b324428185956f44acbdcfb6c8
unknown <hultoj@rpi.edu>
2020-07-15 13:02:06 -0400

    changes to tests

--- tests/e2e/test_pdfs.py
+++ tests/e2e/test_pdfs.py
@@ -16,6 +16,17 @@ class TestPDFs(BaseTestCase):
         self.driver.find_element_by_xpath('//a[contains(@file-url,"words_249.pdf")]').click()
         self.driver.implicitly_wait(10)
         self.driver.find_element_by_id('pageContainer1')
+        
+    def test_peer_basic_access(self):
+        self.log_in(user_id="student", user_name="Student")
+        self.click_class('sample')
+        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_pdf/grading/status")]').click()
+        self.driver.find_element_by_link_text("Grading Index").click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_pdf/grading/grade?who_id=bechta&sort=id&direction=ASC")]').click()
+        self.driver.find_element_by_id('submissions').click()
+        self.driver.find_element_by_xpath('//a[contains(@file-url,"words_249.pdf")]').click()
+        self.driver.implicitly_wait(10)
+        self.driver.find_element_by_id('pageContainer1')
 
 if __name__ == "__main__":
     import unittest
