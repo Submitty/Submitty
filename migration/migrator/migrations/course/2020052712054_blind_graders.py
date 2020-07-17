a6b33013dae5c5523b5f72b153bc9c5a1c674a85
unknown <hultoj@rpi.edu>
2020-07-17 13:05:56 -0400

    migration fix

--- migration/migrator/migrations/course/2020052712054_blind_graders.py
+++ migration/migrator/migrations/course/2020052712054_blind_graders.py
@@ -1,6 +1,6 @@
 def up(config, database, semester, course):
     
-    if not database.table_has_column('electronic_gradeable', 'limited_access_blind'):
+    if not database.table_has_column('electronic_gradeable', 'eg_limited_access_blind'):
         database.execute('ALTER TABLE electronic_gradeable ADD COLUMN eg_limited_access_blind INTEGER DEFAULT 1')
         database.execute('ALTER TABLE electronic_gradeable ADD COLUMN eg_peer_blind INTEGER DEFAULT 3')
 
