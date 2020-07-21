5eb49484b51e7b9119c23074b0d91427824b1212
unknown <hultoj@rpi.edu>
2020-07-21 16:52:41 -0400

    formatting fix

--- site/app/views/AutoGradingView.php
+++ site/app/views/AutoGradingView.php
@@ -440,7 +440,6 @@ class AutoGradingView extends AbstractView {
                 $overall_comments[$display_name] = $comment;
             }
         }
-
         return $this->core->getOutput()->renderTwigTemplate('autograding/TAResults.twig', [
             'files' => $files,
             'been_ta_graded' => $ta_graded_gradeable->isComplete(),
@@ -661,7 +660,6 @@ class AutoGradingView extends AbstractView {
         
         $this->core->getOutput()->addInternalCss('admin-gradeable.css');
         $this->core->getOutput()->addInternalCss('ta-grading.css');
-        
         return $this->core->getOutput()->renderTwigTemplate('autograding/PeerResults.twig', [
             'files' => $files,
             'been_ta_graded' => $ta_graded_gradeable->isComplete(),
