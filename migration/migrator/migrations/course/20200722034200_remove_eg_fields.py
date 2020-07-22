9e1a0900b4e66618edd9ffa6fc0f474aaed94976
unknown <hultoj@rpi.edu>
2020-07-22 15:05:44 -0400

    migration fix

---
+++ migration/migrator/migrations/course/20200722034200_remove_eg_fields.py
@@ -0,0 +1,45 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+
+    database.execute(
+        """
+        ALTER TABLE electronic_gradeable
+            DROP COLUMN IF EXISTS eg_peer_grading,
+            DROP COLUMN IF EXISTS eg_peer_grade_set
+        """
+    )
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute(
+        """
+        ALTER TABLE electronic_gradeable
+            ADD COLUMN eg_peer_grading boolean DEFAULT false NOT NULL,
+            ADD COLUMN eg_peer_grade_set integer DEFAULT 0 NOT NULL
+        """
+    )
