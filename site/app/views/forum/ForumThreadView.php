0208eb4090d30b5bdd7e1f21e63ed16dcc2adc99
mukul-kmr-jha <jimickeyji@gmail.com>
2020-07-26 20:09:52 +0100

    remove action btns from side bar thread view

--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -830,7 +830,7 @@ class ForumThreadView extends AbstractView {
                     "is_anon" => $first_post["anonymous"],
                     "render_markdown" => $first_post["render_markdown"],
                     "author_info" => $author_info,
-                    "title" =>  $thread['title'],
+                    "title" =>  "(" . $thread['id'] . ") " . $thread['title'],
                     "deleted" => $first_post['deleted'],
                     "content" => $first_post_content
                 ]);
