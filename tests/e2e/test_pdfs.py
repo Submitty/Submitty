8fc0dcb93254a5d305636074c7bb4f8a4e258c50
unknown <hultoj@rpi.edu>
2020-07-29 11:32:11 -0400

    fixes for tests

--- tests/e2e/test_pdfs.py
+++ tests/e2e/test_pdfs.py
@@ -10,14 +10,14 @@ class TestPDFs(BaseTestCase):
     @unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")
     def test_pdf_basic_access(self):
         self.switch_settings("Limited Access Grader")
-        self.pdf_access("instructor", "Quinn", "aphacker", "grading_hw_pdf", "words_1463.pdf")
-        self.pdf_access("ta", "Jill", "aphacker", "grading_hw_pdf", "words_1463.pdf")
-        self.pdf_access("grader", "Tim", "jastm", "grading_hw_pdf", "words_1463.pdf")
+        self.pdf_access("instructor", "Quinn", "aphacker", "grading_homework_pdf", "words_1463.pdf")
+        self.pdf_access("ta", "Jill", "aphacker", "grading_homework_pdf", "words_1463.pdf")
+        self.pdf_access("grader", "Tim", "jastm", "grading_homework_pdf", "words_1463.pdf")
     
     def test_pdf_basic_no_access(self):
         self.switch_settings("Instructor")
-        self.pdf_no_access("ta", "Jill", "aphacker", "grading_hw_pdf", "words_1463.pdf")
-        self.pdf_no_access("grader", "Tim", "jastm", "grading_hw_pdf", "words_1463.pdf")
+        self.pdf_no_access("ta", "Jill", "aphacker", "grading_homework_pdf", "words_1463.pdf")
+        self.pdf_no_access("grader", "Tim", "jastm", "grading_homework_pdf", "words_1463.pdf")
         
     def test_peer_basic_access(self):
         self.log_in(user_id="student", user_name="Student")
@@ -37,7 +37,7 @@ class TestPDFs(BaseTestCase):
         self.log_out()
         self.log_in(user_id="instructor", user_name="Quinn")
         self.click_class('sample')
-        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_hw_pdf/update")]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_pdf/update")]').click()
         self.driver.find_element_by_id('page_3_nav').click()
         select_element = Select(self.driver.find_element_by_id("minimum_grading_group"))
         select_element.select_by_visible_text(setting);
@@ -51,7 +51,9 @@ class TestPDFs(BaseTestCase):
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/'+gradeable_id+'/grading/status")]').click()
         self.driver.find_element_by_link_text("Grading Index").click()
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/'+gradeable_id+'/grading/grade?who_id='+student_id+'&sort=id&direction=ASC")]').click()
+        self.driver.implicitly_wait(10)
         self.driver.find_element_by_id('submissions').click()
+        self.driver.implicitly_wait(10)
         self.driver.find_element_by_xpath('//a[contains(@file-url,"'+pdf_name+'")]').click()
         self.driver.implicitly_wait(10)
         self.driver.find_element_by_id('pageContainer1')
