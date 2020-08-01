5067b1356ef57d43ddde5a2c3581372e3a23b728
unknown <hultoj@rpi.edu>
2020-08-01 11:21:15 -0400

    selenium fix

--- tests/e2e/test_navigation_page_non_student.py
+++ tests/e2e/test_navigation_page_non_student.py
@@ -61,7 +61,7 @@ class TestNavigationPageNonStudent(BaseTestCase):
                                 .find_element(By.ID, 'items_being_graded-section')
                                 .find_elements(By.CLASS_NAME, "gradeable-row")))
         self.assertEqual("graded", elements[4].get_attribute('id'))
-        self.assertEqual(9, len(self.driver
+        self.assertEqual(13, len(self.driver
                                 .find_element(By.ID, 'graded-section')
                                 .find_elements(By.CLASS_NAME, "gradeable-row")))
 
