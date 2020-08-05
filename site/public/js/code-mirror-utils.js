f4892bd7996a0154f9c8db179bc1f051d49a26e9
Kevin-MM <kevin.m.mckinney@gmail.com>
2020-08-05 09:08:43 -0400

    Created codemirror wrapper function.

--- site/public/js/code-mirror-utils.js
+++ site/public/js/code-mirror-utils.js
@@ -1,4 +1,24 @@
 /**
+ * Generate a 'submitty' codemirror.  This is simply a regular codemirror except we've bound accessibility keys to
+ * make keyboard navigation better.  It also adds a small instructional message above the codemirror.
+ *
+ * @param {HTMLElement} attachment_elem The instructional message and codemirror will be appended to this element.
+ * @param {Object} codemirror_config A javascript object which defines the configuration the codemirror should be
+ *                                   instantiated with.
+ * @returns {CodeMirror}
+ */
+function getSubmittyCodeMirror(attachment_elem, codemirror_config) {
+    const accessibility_msg = document.createElement('i');
+    accessibility_msg.innerText = 'Press TAB to indent. Press ESC to advance from answer area.';
+    accessibility_msg.style.fontSize = '75%';
+    attachment_elem.appendChild(accessibility_msg);
+
+    const cm = CodeMirror(attachment_elem, codemirror_config);
+    makeCodeMirrorAccessible(cm);
+    return cm;
+}
+
+/**
  * Make codemirror boxes more accessible.  This function binds some extra key listeners to the passed in codemirror
  * object.  If the codemirror has focus, pressing escape will advance to the next focusable element outside the
  * codemirror, pressing 'Shift-Tab' will move to the previous.
