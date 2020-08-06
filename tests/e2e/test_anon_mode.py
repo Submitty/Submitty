f91d4e7d89999d453751ec8fc2b45c8e8b0e47b8
unknown <hultoj@rpi.edu>
2020-08-06 14:17:52 -0400

    adding waits

--- tests/e2e/test_anon_mode.py
+++ tests/e2e/test_anon_mode.py
@@ -10,11 +10,14 @@ class TestAnonMode(BaseTestCase):
         self.log_in(user_id="student", user_name="Joe")
         self.click_class('sample')
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_pdf_peer_homework/grading/status")]').click()
+        self.wait_for_element((By.LINK_TEXT, "Grading Index"))
         self.driver.find_element_by_link_text("Grading Index").click()
+        self.wait_for_element((By.ID, "details-table"))
         page_source = self.driver.page_source
         page_source_processed = page_source[0:page_source.index("page-info")]
         self.assertFalse("towneo" in page_source_processed)
-        (self.driver.find_element_by_xpath("//tbody[@class='details-content panel-content-active']/tr[5]/td[5]").find_elements_by_tag_name("a")[0]).click()
+        (self.driver.find_element_by_xpath("//tbody[@class='details-content panel-content-active']/tr[6]/td[5]").find_elements_by_tag_name("a")[0]).click()
+        self.wait_for_element((By.ID, "submission_browser"))
         page_source = self.driver.page_source
         page_source_processed = page_source[0:page_source.index("page-info")]
         self.assertFalse("towneo" in page_source_processed)
@@ -23,13 +26,16 @@ class TestAnonMode(BaseTestCase):
         self.log_in(user_id="student", user_name="Joe")
         self.click_class('sample')
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_pdf_peer_team_homework/grading/status")]').click()
+        self.wait_for_element((By.LINK_TEXT, "Grading Index"))
         self.driver.find_element_by_link_text("Grading Index").click()
+        self.wait_for_element((By.ID, "details-table"))
         page_source = self.driver.page_source
         page_source_processed = page_source[0:page_source.index("page-info")]
         self.assertFalse("thompc" in page_source_processed)
         self.assertFalse("turnej" in page_source_processed)
         self.assertFalse("uptonr" in page_source_processed)
-        (self.driver.find_element_by_xpath("//tbody[@class='details-content panel-content-active']/tr[5]/td[5]").find_elements_by_tag_name("a")[0]).click()
+        (self.driver.find_element_by_xpath("//tbody[@class='details-content panel-content-active']/tr[6]/td[5]").find_elements_by_tag_name("a")[0]).click()
+        self.wait_for_element((By.ID, "submission_browser"))
         page_source = self.driver.page_source
         page_source_processed = page_source[0:page_source.index("page-info")]
         self.assertFalse("thompc" in page_source_processed)
