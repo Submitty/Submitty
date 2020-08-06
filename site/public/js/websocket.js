101c2cef5cf42c08bf374e7297c7987aeafc7762
Marwan Atef <marwanatef97@gmail.com>
2020-08-06 17:37:52 +0200

    fix js linting

--- site/public/js/websocket.js
+++ site/public/js/websocket.js
@@ -30,18 +30,6 @@ class WebSocketClient {
         this.onopen = null;
         this.onmessage = null;
     }
-    this.url = url;
-    console.log('WebSocket: connecting...');
-    this.client = new WebSocket(this.url);
-    this.client.onopen = () => {
-      console.log('WebSocket: Connected!');
-      if (this.onopen) {
-        this.onopen();
-      }
-      let course = $('.breadcrumb').eq(1).text().trim();
-      let page = $('.breadcrumb').eq(2).text().trim().toLowerCase().split(' ').join('_');
-      this.client.send(JSON.stringify({'type': 'new_connection', 'page': course +'-'+ page}));
-    };
 
     open(url) {
         if (!url) {
@@ -55,7 +43,9 @@ class WebSocketClient {
             if (this.onopen) {
                 this.onopen();
             }
-            this.client.send(JSON.stringify({'type': 'new_connection', 'course': document.body.dataset.courseUrl.split('/').pop()}));
+            const course = $('.breadcrumb').eq(1).text().trim();
+            const page = $('.breadcrumb').eq(2).text().trim().toLowerCase().split(' ').join('_');
+            this.client.send(JSON.stringify({'type': 'new_connection', 'page': `${course}-${page}`}));
         };
 
         this.client.onmessage = (event) => {
