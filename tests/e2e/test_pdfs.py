d1facca32b6261e91bef3a04b8c883364e48613c
unknown <hultoj@rpi.edu>
2020-08-12 21:55:15 -0400

    full test suite

--- tests/e2e/test_pdfs.py
+++ tests/e2e/test_pdfs.py
@@ -15,28 +15,27 @@ class TestPDFs(BaseTestCase):
         self.pdf_access("instructor", "3", "8" ,"grading_homework_pdf", "words_1463.pdf", "2")
         self.pdf_access("ta", "3", "8", "grading_homework_pdf", "words_1463.pdf", "2")
         self.pdf_access("grader", "2", "8", "grading_homework_pdf", "words_249.pdf", "1")
-    #@unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")
-    #def test_pdf_team_access(self):
-    #    self.switch_settings("Limited Access Grader")
-    #    self.pdf_access("instructor", "1", "8", "grading_homework_team_pdf", "words_881.pdf", "1")
-    #    self.pdf_access("ta", "1", "6", "grading_homework_team_pdf", "words_881.pdf", "1")
-    #    self.pdf_access("grader","1", "6", "grading_homework_team_pdf", "words_881.pdf", "1")
-    #@unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")    
-    #def test_pdf_peer_access(self):
-    #    self.switch_settings("Limited Access Grader")
-    #    self.pdf_access("instructor", "3", "8", "grading_pdf_peer_homework", "words_1463.pdf", "1")
-    #    self.pdf_access("ta", "3", "8", "grading_pdf_peer_homework", "words_1463.pdf", "1")
-    #    self.pdf_access("grader","2", "8", "grading_pdf_peer_homework", "words_249.pdf", "1")
-    #    self.pdf_access("student","2", "5", "grading_pdf_peer_homework", "words_249.pdf", "1")
-    #@unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")    
-    #def test_pdf_peer_team_access(self):
-    #    self.switch_settings("Limited Access Grader")
-    #    self.pdf_access("instructor", "2", "8", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
-    #    self.pdf_access("ta", "2", "6", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
-    #    self.pdf_access("grader", "2", "6", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
-    #    self.pdf_access("bauchg", "1", "5", "grading_pdf_peer_team_homework", "words_881.pdf", "1")
+    @unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")
+    def test_pdf_team_access(self):
+        self.switch_settings("Limited Access Grader")
+        self.pdf_access("instructor", "1", "8", "grading_homework_team_pdf", "words_881.pdf", "1")
+        self.pdf_access("ta", "1", "6", "grading_homework_team_pdf", "words_881.pdf", "1")
+        self.pdf_access("grader","1", "6", "grading_homework_team_pdf", "words_881.pdf", "1")
+    @unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")    
+    def test_pdf_peer_access(self):
+        self.switch_settings("Limited Access Grader")
+        self.pdf_access("instructor", "3", "8", "grading_pdf_peer_homework", "words_1463.pdf", "1")
+        self.pdf_access("ta", "3", "8", "grading_pdf_peer_homework", "words_1463.pdf", "1")
+        self.pdf_access("grader","2", "8", "grading_pdf_peer_homework", "words_249.pdf", "1")
+        self.pdf_access("student","2", "5", "grading_pdf_peer_homework", "words_249.pdf", "1")
+    @unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")    
+    def test_pdf_peer_team_access(self):
+        self.switch_settings("Limited Access Grader")
+        self.pdf_access("instructor", "2", "8", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
+        self.pdf_access("ta", "2", "6", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
+        self.pdf_access("grader", "2", "6", "grading_pdf_peer_team_homework", "words_1463.pdf", "1")
+        self.pdf_access("bauchg", "1", "5", "grading_pdf_peer_team_homework", "words_881.pdf", "1")
     def pdf_access(self, user_id, tr_number, td_number, gradeable_id, pdf_name, version):
-        self.log_out()
         self.log_in(user_id=user_id)
         self.click_class('sample')
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/'+gradeable_id+'/grading/status")]').click()
@@ -51,31 +50,28 @@ class TestPDFs(BaseTestCase):
             self.driver.find_element_by_id('open_file_'+pdf_name).click()
             new_window = self.driver.window_handles[1]
             self.driver.switch_to.window(new_window)
-            time.sleep(5)
-            print(self.driver.page_source)
             try:
-                print("CHECK1")
-                self.driver.find_element_by_xpath('//embed[contains@type,"application/pdf"]')
-                print("CHECK2")
+                self.driver.find_element_by_xpath('//embed[contains(@type,"application/pdf")]')
             except NoSuchElementException:
                 return False
-            print("CHECK3")
             self.driver.close()
             self.driver.switch_to.window(current_window)
-            time.sleep(5)
         self.driver.find_element_by_xpath('//a[contains(@file-url,"'+pdf_name+'")]').click()
-        self.driver.implicitly_wait(20)
+        self.wait_for_element((By.ID, "pageContainer1"))
         self.driver.find_element_by_id('pageContainer1')
-    def switch_settings(self, setting):
+        self.driver.find_element_by_xpath('//a[contains(@onclick,"collapseFile()")]').click()
+        self.driver.maximize_window()
+        self.wait_for_element((By.ID, "logout"))
         self.log_out()
+    def switch_settings(self, setting):
         self.log_in(user_id="instructor")
         self.click_class('sample')
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework_pdf/update")]').click()
         self.driver.find_element_by_id('page_3_nav').click()
         select_element = Select(self.driver.find_element_by_id("minimum_grading_group"))
         select_element.select_by_visible_text(setting)
+        self.wait_for_element((By.ID, "logout"))
         self.log_out()
-        self.driver.implicitly_wait(20)
 
 if __name__ == "__main__":
     import unittest
