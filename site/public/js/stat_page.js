36faca583173abb0a80c4b23e429b9b9cdc67ba8
zeez <36535968+zeez2030@users.noreply.github.com>
2020-08-14 03:51:00 +0200

    Update stat_page.js

--- site/public/js/stat_page.js
+++ site/public/js/stat_page.js
@@ -1,69 +1,70 @@
-$("td").click(function () {
-  var table_id = 0;
-  switch ($(this).attr('id')) {
-    case "user_sort":
-      table_id = 0;
-      break;
-    case "total_posts_sort":
-      table_id = 1;
-      break;
-    case "total_threads_sort":
-      table_id = 2;
-      break;
-    case "total_deleted_sort":
-      table_id = 3;
-      break;
-    default:
-      table_id = 0;
-  }
+$('td').click(function () {
+    let table_id = 0;
+    switch ($(this).attr('id')) {
+        case 'user_sort':
+            table_id = 0;
+            break;
+        case 'total_posts_sort':
+            table_id = 1;
+            break;
+        case 'total_threads_sort':
+            table_id = 2;
+            break;
+        case 'total_deleted_sort':
+            table_id = 3;
+            break;
+        default:
+            table_id = 0;
+    }
 
-  if ($(this).html().indexOf(' ↓') > -1) {
-    sortTable(table_id, true);
-  } else {
-    sortTable(table_id, false);
-  }
+    if ($(this).html().indexOf(' ↓') > -1) {
+        sortTable(table_id, true);
+    } else {
+        sortTable(table_id, false);
+    }
 });
 
-$("button").click(function () {
+$('button').click(function () {
 
-  var action = $(this).data('action');
-  var posts = $(this).data('posts');
-  var ids = $(this).data('id');
-  var timestamps = $(this).data('timestamps');
-  var thread_ids = $(this).data('thread_id');
-  var thread_titles = $(this).data('thread_titles');
-  if (action == "expand") {
+    let action = $(this).data('action');
+    let posts = $(this).data('posts');
+    let ids = $(this).data('id');
+    let timestamps = $(this).data('timestamps');
+    let thread_ids = $(this).data('thread_id');
+    let thread_titles = $(this).data('thread_titles');
+    if (action == 'expand') {
 
 
-    for (var i = 0; i < posts.length; i++) {
-      var post_string = posts[i];
-      post_string = escapeSpecialChars(post_string);
-      var thread_title = thread_titles[i]["title"];
-      thread_title = escapeSpecialChars(thread_title);
-      $(this).parent().parent().parent().append('<tr id="' + ids[i] + '"><td></td><td>' + timestamps[i] + '</td><td style = "cursor:pointer;" data-type = "thread" data-thread_id="' + thread_ids[i] + '"><pre class="pre_forum" style="white-space: pre-wrap;">' + thread_title + '</pre></td><td colspan = "2" style = "cursor:pointer;" align = "left" data-type = "post" data-thread_id="' + thread_ids[i] + '"><pre class="pre_forum" style="white-space: pre-wrap;">' + post_string + '</pre></td></tr> ');
+        for (let i = 0; i < posts.length; i++) {
+            let post_string = posts[i];
+            post_string = escapeSpecialChars(post_string);
+            let thread_title = thread_titles[i]["title"];
+            thread_title = escapeSpecialChars(thread_title);
+            $(this).parent().parent().parent().append('<tr id="' + ids[i] + '"><td></td><td>' + timestamps[i] + '</td><td style = "cursor:pointer;" data-type = "thread" data-thread_id="' + thread_ids[i] + '"><pre class="pre_forum" style="white-space: pre-wrap;">' + thread_title + '</pre></td><td colspan = "2" style = "cursor:pointer;" align = "left" data-type = "post" data-thread_id="' + thread_ids[i] + '"><pre class="pre_forum" style="white-space: pre-wrap;">' + post_string + '</pre></td></tr> ');
 
+        }
+        $(this).html('Collapse');
+        $(this).data('action', 'collapse');
+        $('td').click(function () {
+
+        if ($(this).data('type') == 'post' || $(this).data('type') == 'thread') {
+            let id = $(this).data('thread_id');
+            let url = buildCourseUrl(['forum', 'threads', id]);
+            window.open(url);
+        }
+        
+        });
+    } 
+    else {
+        for (let j = 0; j < ids.length; j++) {
+            let item = document.getElementById(ids[j]);
+            item.remove();
+        }
+
+        $(this).html('Expand');
+        $(this).data('action', 'expand');
     }
-    $(this).html("Collapse");
-    $(this).data('action', "collapse");
-    $("td").click(function () {
-
-      if ($(this).data('type') == "post" || $(this).data('type') == "thread") {
-        var id = $(this).data('thread_id');
-        var url = buildCourseUrl(['forum', 'threads', id]);
-        window.open(url);
-      }
-
-    });
-  } else {
-    for (var i = 0; i < ids.length; i++) {
-      var item = document.getElementById(ids[i]);
-      item.remove();
-    }
-
-    $(this).html("Expand");
-    $(this).data('action', "expand");
-  }
 
 
-  return false;
+    return false;
 });
