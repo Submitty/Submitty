e891a4f5be35a9d44bbc72cf18a4ce1611baf2ce
mukul-kmr-jha <jimickeyji@gmail.com>
2020-08-18 21:07:42 +0100

    lint-2

--- site/public/js/resizable-panels.js
+++ site/public/js/resizable-panels.js
@@ -17,64 +17,64 @@
  * @param {resizeCallback} callback
  */
 function initializeResizablePanels (panelSel, dragBarSel, isHorizontalResize= false, callback = null) {
-  // Select all the DOM elements for dragging in two-panel-mode
-  const panelEle = document.querySelector(panelSel);
-  const panelCont = panelEle.parentElement;
-  const dragbar = document.querySelector(dragBarSel);
+    // Select all the DOM elements for dragging in two-panel-mode
+    const panelEle = document.querySelector(panelSel);
+    const panelCont = panelEle.parentElement;
+    const dragbar = document.querySelector(dragBarSel);
 
-  let xPos = 0, yPos = 0, panelHeight = 0, panelWidth = 0;
+    let xPos = 0, yPos = 0, panelHeight = 0, panelWidth = 0;
 
-  // Width of left side
-  const mouseDownHandler = (e) => {
-    // Get the current mouse position
-    xPos = e.clientX;
-    yPos = e.clientY;
-    panelHeight = panelEle.getBoundingClientRect().height;
-    panelWidth = panelEle.getBoundingClientRect().width;
+    // Width of left side
+    const mouseDownHandler = (e) => {
+        // Get the current mouse position
+        xPos = e.clientX;
+        yPos = e.clientY;
+        panelHeight = panelEle.getBoundingClientRect().height;
+        panelWidth = panelEle.getBoundingClientRect().width;
 
-    // Attach the listeners to `document`
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-  };
+        // Attach the listeners to `document`
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
 
-  const mouseUpHandler = () => {
-    // remove the dragging CSS props to go back to initial styling
-    dragbar.style.removeProperty('cursor');
-    document.body.style.removeProperty('cursor');
-    document.body.style.removeProperty('user-select');
-    document.body.style.removeProperty('pointer-events');
-    dragbar.style.removeProperty('filter');
+    const mouseUpHandler = () => {
+        // remove the dragging CSS props to go back to initial styling
+        dragbar.style.removeProperty('cursor');
+        document.body.style.removeProperty('cursor');
+        document.body.style.removeProperty('user-select');
+        document.body.style.removeProperty('pointer-events');
+        dragbar.style.removeProperty('filter');
 
-    // Remove the handlers of `mousemove` and `mouseup`
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-  };
+        // Remove the handlers of `mousemove` and `mouseup`
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
 
-  const mouseMoveHandler = (e) => {
-    let updateValue;
-    if (isHorizontalResize) {
-      const dy = e.clientY - yPos;
-      updateValue = (panelHeight - dy) * 100 / panelCont.getBoundingClientRect().height;
-      panelEle.style.height = `${updateValue}%`;
-    }
-    else {
-      const dx = e.clientX - xPos;
-      updateValue = (panelWidth + dx) * 100 / panelCont.getBoundingClientRect().width;
-      panelEle.style.width = `${updateValue}%`;
-    }
+    const mouseMoveHandler = (e) => {
+        let updateValue;
+        if (isHorizontalResize) {
+            const dy = e.clientY - yPos;
+            updateValue = (panelHeight - dy) * 100 / panelCont.getBoundingClientRect().height;
+            panelEle.style.height = `${updateValue}%`;
+        }
+        else {
+            const dx = e.clientX - xPos;
+            updateValue = (panelWidth + dx) * 100 / panelCont.getBoundingClientRect().width;
+            panelEle.style.width = `${updateValue}%`;
+        }
 
-    // consistent mouse pointer during dragging
-    document.body.style.cursor = 'col-resize';
-    // Disable text selection when dragging
-    document.body.style.userSelect = 'none';
-    document.body.style.pointerEvents = 'none';
-    // Add blurry effect on drag-bar
-    dragbar.style.filter = 'blur(5px)';
+        // consistent mouse pointer during dragging
+        document.body.style.cursor = 'col-resize';
+        // Disable text selection when dragging
+        document.body.style.userSelect = 'none';
+        document.body.style.pointerEvents = 'none';
+        // Add blurry effect on drag-bar
+        dragbar.style.filter = 'blur(5px)';
 
-    // Callback function
-    if (typeof callback === 'function') {
-      callback(`${updateValue}%`, isHorizontalResize);
-    }
-  };
-  dragbar.addEventListener('mousedown', mouseDownHandler);
+        // Callback function
+        if (typeof callback === 'function') {
+            callback(`${updateValue}%`, isHorizontalResize);
+        }
+    };
+    dragbar.addEventListener('mousedown', mouseDownHandler);
 }
