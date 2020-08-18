07a28153b185d985c96fff42673c7550dfafa1e8
josharsh <harsh.joshi.pth@gmail.com>
2020-08-18 22:26:03 +0530

    Lint Fix

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -1946,7 +1946,7 @@ class ElectronicGraderController extends AbstractController {
         }
 
         // get the user
-        if ($this->core->getUser()->getGroup() === User::GROUP_STUDENT){
+        if ($this->core->getUser()->getGroup() === User::GROUP_STUDENT) {
             $this->core->getOutput()->renderJsonFail('Insufficient permissions for custom marks');
             return;
         }
