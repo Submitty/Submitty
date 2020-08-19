7f7c4e997d57db24ad273476f85f403ad8d78e8a
mukul-kmr-jha <jimickeyji@gmail.com>
2020-08-19 09:06:18 +0100

    update migration file

--- migration/migrator/migrations/course/20200806032906_solution_ta_notes.py
+++ migration/migrator/migrations/course/20200806032906_solution_ta_notes.py
@@ -10,11 +10,12 @@ def up(config, database, semester, course):
         edited_at timestamp with time zone NOT NULL
     )""")
     database.execute("ALTER TABLE IF EXISTS ONLY solution_ta_notes DROP CONSTRAINT IF EXISTS solution_ta_notes_g_id_fk;")
+    database.execute("ALTER TABLE IF EXISTS ONLY solution_ta_notes DROP CONSTRAINT IF EXISTS solution_ta_notes_component_id_fk;")
     database.execute("ALTER TABLE IF EXISTS ONLY solution_ta_notes DROP CONSTRAINT IF EXISTS solution_ta_notes_author_fk;")
-    database.execute('ALTER TABLE solution_ta_notes ADD CONSTRAINT solution_ta_notes_g_id_fk FOREIGN KEY (g_id) REFERENCES gradeable(g_id)')
-    database.execute('ALTER TABLE solution_ta_notes ADD CONSTRAINT solution_ta_notes_author_fk FOREIGN KEY (author) REFERENCES users(user_id)')
+    database.execute('ALTER TABLE IF EXISTS ONLY solution_ta_notes ADD CONSTRAINT solution_ta_notes_g_id_fk FOREIGN KEY (g_id) REFERENCES gradeable(g_id) ON DELETE CASCADE')
+    database.execute("ALTER TABLE IF EXISTS ONLY solution_ta_notes ADD CONSTRAINT solution_ta_notes_component_id_fk FOREIGN KEY (component_id) REFERENCES gradeable_component(gc_id) ON DELETE CASCADE")
+    database.execute('ALTER TABLE IF EXISTS ONLY solution_ta_notes ADD CONSTRAINT solution_ta_notes_author_fk FOREIGN KEY (author) REFERENCES users(user_id)')
 
 
 def down(config, database, semester, course):
-    database.execute("DROP TABLE solution_ta_notes")
     pass
