e01f69ff4fe70cbe1893094bbba61a1a5d348870
mukul-kmr-jha <jimickeyji@gmail.com>
2020-08-19 08:37:21 +0100

    compact migration code

--- migration/migrator/migrations/course/20200815082257_link_itempool_component.py
+++ migration/migrator/migrations/course/20200815082257_link_itempool_component.py
@@ -1,8 +1,11 @@
 """Migration for a given Submitty course database."""
 
 def up(config, database, semester, course):
+    # Link Itempool with the rubric-component
     database.execute("ALTER TABLE IF EXISTS gradeable_component ADD COLUMN IF NOT EXISTS gc_is_itempool_linked BOOL NOT NULL DEFAULT FALSE")
     database.execute("ALTER TABLE IF EXISTS gradeable_component ADD COLUMN IF NOT EXISTS gc_itempool varchar(100) NOT NULL DEFAULT ''")
+    # Link Itempool with Solution/Ta notes panels
+    database.execute("ALTER TABLE IF EXISTS solution_ta_notes ADD COLUMN IF NOT EXISTS itempool_item VARCHAR(100) NOT NULL DEFAULT ''")
 
 
 def down(config, database, semester, course):
