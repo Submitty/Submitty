12857d99b37bf7d2f303d45d58a6ae2c8791927d
mukul-kmr-jha <jimickeyji@gmail.com>
2020-08-20 20:17:38 +0100

    image flag fix

--- tests/e2e/test_my_profile.py
+++ tests/e2e/test_my_profile.py
@@ -98,6 +98,50 @@ class TestMyProfile(BaseTestCase):
     def test_upload_profile_photo(self):
         self.setup_test_start()
 
+        # upload user image
+        self.upload_profile_photo()
+
+        # Look for success message
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "success-js-1")))
+
+        # edit-profile-photo form should be out-of-the screen
+        self.assertFalse(self.driver.find_element(By.ID, "edit-profile-photo-form").is_displayed())
+
+        # Assert that image is added and alt-tag value is updated correctly
+        alt_tag_val = self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img").get_attribute('alt')
+        self.assertTrue(self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img").is_displayed())
+        self.assertEqual("{} {}".format(self.student_first_name, self.student_last_name), alt_tag_val)
+
+    def test_flagged_profile_photo(self):
+        self.setup_test_start()
+        # upload user image
+        self.upload_profile_photo()
+        # student logout from the submitty
+        self.log_out()
+
+        # flag student profile photo
+        self.change_image_state()
+
+        # login as student and go to my-profile page
+        self.setup_test_start()
+        # No image element and just an empty span element stating photo is 'N/A'
+        try:
+           self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img")
+           not_found = False
+        except NoSuchElementException:
+           not_found = True
+        self.assertTrue(not_found)
+        self.assertEqual('N/A', self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/span[@class='center-img-tag']").text)
+
+        # look for flagged image message
+        self.assertEqual('Your preferred image was flagged as inappropriate.', self.driver.find_element(By.ID, "flagged-message").text)
+        self.log_out()
+
+        # Reset the image state unflag
+        self.change_image_state(False)
+
+
+    def upload_profile_photo(self):
         # click on the upload-profile-photo link
         self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/span/a").click()
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "edit-profile-photo-form")))
@@ -115,46 +159,23 @@ class TestMyProfile(BaseTestCase):
         self.driver.find_element(By.ID, "user-image-button").send_keys(image_path)
         self.driver.find_element(By.XPATH, "//div[@id='edit-profile-photo-form']/form/div/div/div[2]/div[2]/div/input").click()
 
-        # Look for success message
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "success-js-1")))
-
-        # edit-profile-photo form should be out-of-the screen
-        self.assertFalse(self.driver.find_element(By.ID, "edit-profile-photo-form").is_displayed())
-
-        # Assert that image is added and alt-tag value is updated correctly
-        alt_tag_val = self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img").get_attribute('alt')
-        self.assertTrue(self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img").is_displayed())
-        self.assertEqual("{} {}".format(self.student_first_name, self.student_last_name), alt_tag_val)
-
-    def test_flagged_profile_photo(self):
+    def change_image_state(self, flag = True):
         # Login as instructor and go to student photos page
         self.log_in(user_id='instructor', user_name='Quinn')
         self.click_class("sample", "SAMPLE")
         self.driver.find_element(By.ID, "nav-sidebar-photos").click()
 
-        # find Joe(Student) image and flag it
+        # find Joe (Student) image and flag it
         self.driver.find_element(By.XPATH, "//td[@class='{}-image-container']/div[@class='name']/a".format(self.student_id)).click()
-        WebDriverWait(self.driver, 2).until(EC.alert_is_present(), "You are flagging {}'s preferred image as inappropriate.\nThis should be done if the image is not a recognizable passport style photo.\n\nDo you wish to proceed?".format(self.student_id))
+        if not flag:
+            WebDriverWait(self.driver, 2).until(EC.alert_is_present(), "You are flagging {}'s preferred image as inappropriate.\nThis should be done if the image is not a recognizable passport style photo.\n\nDo you wish to proceed?".format(self.student_id))
+        else:
+            WebDriverWait(self.driver, 2).until(EC.alert_is_present(), "{0}'s preferred image has be flagged as inappropriate.\nThis was done because the image is not a recognizable, passport style photo.\n\nYou are reverting to ${0}'s preferred image.\nDo you wish to proceed?".format(self.student_id))
         # accept the popup
         self.driver.switch_to.alert.accept()
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "success-js-0")))
-        # logout from the submitty
+        # Instructor logout from the submitty
         self.log_out()
-
-        # login as student and go to my-profile page
-        self.setup_test_start()
-
-        # No image element and just an empty span element stating photo is 'N/A'
-        try:
-           self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/img")
-           not_found = False
-        except NoSuchElementException:
-           not_found = True
-        self.assertTrue(not_found)
-        self.assertEqual('N/A', self.driver.find_element(By.XPATH, "//div[@id='user-card-img']/div/span[@class='center-img-tag']").text)
-
-        # look for flagged image message
-        self.assertEqual('Your preferred image was flagged as inappropriate.', self.driver.find_element(By.ID, "flagged-message").text)
 
 if __name__ == "__main__":
     import unittest
