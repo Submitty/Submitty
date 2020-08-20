fedbfa5d19d5613af11d571c1694ef1e5c155de4
unknown <hultoj@rpi.edu>
2020-08-19 20:20:24 -0400

    test_fix

--- tests/e2e/test_stats.py
+++ tests/e2e/test_stats.py
@@ -12,13 +12,13 @@ class TestStats(BaseTestCase):
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grading_homework/grading/status")]').click()
         numerical_data_text = self.driver.find_element_by_id("numerical-data").text
         if full_access:
-            self.assertTrue("Students who have submitted: 72 / 101 (71.3%)" in numerical_data_text)
-            self.assertTrue("Current percentage of grading done: 43.75 / 72 (60.8%)" in numerical_data_text)
-            self.assertTrue("Section 1: 4 / 12 (33.3%)" in numerical_data_text)
+            self.assertTrue("Students who have submitted: 68 / 101 (67.3%)" in numerical_data_text)
+            self.assertTrue("Current percentage of grading done: 43.25 / 68 (63.6%)" in numerical_data_text)
+            self.assertTrue("Section 1: 4 / 9 (44.4%)" in numerical_data_text)
         else:
-            self.assertTrue("Students who have submitted: 15 / 20 (75%)" in numerical_data_text)
-            self.assertTrue("Current percentage of grading done: 9 / 15 (60.0%)" in numerical_data_text)
-            self.assertTrue("Section 4: 4 / 8 (50.0%)" in numerical_data_text)
+            self.assertTrue("Students who have submitted: 13 / 20 (65%)" in numerical_data_text)
+            self.assertTrue("Current percentage of grading done: 8 / 13 (61.5%)" in numerical_data_text)
+            self.assertTrue("Section 4: 6 / 6 (100.0%)" in numerical_data_text)
         self.log_out()
     def individual_released_stats_test_helper(self, user_id, full_access):
         self.log_in(user_id=user_id)
@@ -26,15 +26,15 @@ class TestStats(BaseTestCase):
         self.driver.find_element_by_xpath('//a[contains(@href,"/sample/gradeable/grades_released_homework/grading/status")]').click()
         numerical_data_text = self.driver.find_element_by_id("numerical-data").text
         if full_access:
-            self.assertTrue("Students who have submitted: 71 / 101 (70.3%)" in numerical_data_text)
-            self.assertTrue("Current percentage of grading done: 71 / 71 (100.0%)" in numerical_data_text)
-            self.assertTrue("Section 1: 8 / 8 (100.0%)" in numerical_data_text)
-            self.assertTrue("Number of students who have viewed their grade: 53 / 71 (74.6%)" in numerical_data_text)
+            self.assertTrue("Students who have submitted: 73 / 101 (72.3%)" in numerical_data_text)
+            self.assertTrue("Current percentage of grading done: 73 / 73 (100.0%)" in numerical_data_text)
+            self.assertTrue("Section 1: 12 / 12 (100.0%)" in numerical_data_text)
+            self.assertTrue("Number of students who have viewed their grade: 50 / 73 (68.5%)" in numerical_data_text)
         else:
             self.assertTrue("Students who have submitted: 13 / 20 (65%)" in numerical_data_text)
             self.assertTrue("Current percentage of grading done: 13 / 13 (100.0%)" in numerical_data_text)
-            self.assertTrue("Section 4: 5 / 5 (100.0%)" in numerical_data_text)
-            self.assertTrue("Number of students who have viewed their grade: 12 / 13 (92.3%)" in numerical_data_text)            
+            self.assertTrue("Section 4: 8 / 8 (100.0%)" in numerical_data_text)
+            self.assertTrue("Number of students who have viewed their grade: 8 / 13 (61.5%)" in numerical_data_text)            
         self.log_out()
     def team_grading_stats_test_helper(self, user_id, full_access):
         self.log_in(user_id=user_id)
@@ -44,13 +44,13 @@ class TestStats(BaseTestCase):
         if full_access:
             self.assertTrue("Students on a team: 101/101 (100%)" in numerical_data_text)
             self.assertTrue("Number of teams: 36" in numerical_data_text)
-            self.assertTrue("Teams who have submitted: 28 / 36 (77.8%)" in numerical_data_text)
-            self.assertTrue("Section 1: 4 / 5 (80.0%)" in numerical_data_text)
+            self.assertTrue("Teams who have submitted: 30 / 36 (83.3%)" in numerical_data_text)
+            self.assertTrue("Section 1: 1.5 / 3 (50.0%)" in numerical_data_text)
         else:
             self.assertTrue("Students on a team: 20/20 (100%)" in numerical_data_text)
             self.assertTrue("Number of teams: 8" in numerical_data_text)
-            self.assertTrue("Teams who have submitted: 5 / 8 (62.5%)" in numerical_data_text)
-            self.assertTrue("Section 4: 2 / 3 (66.7%)" in numerical_data_text)
+            self.assertTrue("Teams who have submitted: 6 / 8 (75%)" in numerical_data_text)
+            self.assertTrue("Section 4: 0 / 3 (0.0%)" in numerical_data_text)
         self.log_out()
     @unittest.skipUnless(os.environ.get('TRAVIS_BUILD_DIR') is None, "cannot run in Travis-CI")
     def test_team_grading_stats(self):
