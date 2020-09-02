a941b39b9a59d175c8e5fade3477d1d6f8bd76c2
Evan Maicus <emaicus@outlook.com>
2020-09-02 12:59:44 -0400

    temporarily replace websocketclient with mock

--- site/public/js/websocket.js
+++ site/public/js/websocket.js
@@ -22,84 +22,98 @@
  * get called on the first connect and then subsequent reconnects.
  */
 
-/* exported WebSocketClient */
 class WebSocketClient {
     constructor() {
-        this.number = 0;
-        this.autoReconnectInterval = 5 * 1000;
-        this.onopen = null;
-        this.onmessage = null;
-        this.url = `${document.body.dataset.baseUrl.replace('http', 'ws')}ws/`;
+        console.log("instantiating mock websocketclient");
     }
 
     open(page) {
-        console.log(`WebSocket: connecting to ${this.url}`);
-        this.client = new WebSocket(this.url);
-        this.client.onopen = () => {
-            console.log('WebSocket: connected');
-            if (this.onopen) {
-                this.onopen();
-            }
-            const term_course_arr = document.body.dataset.courseUrl.split('/');
-            const course = term_course_arr.pop();
-            const term = term_course_arr.pop();
-            this.client.send(JSON.stringify({'type': 'new_connection', 'page': `${term}-${course}-${page}`}));
-        };
-
-        this.client.onmessage = (event) => {
-            this.number++;
-            if (this.onmessage) {
-                try {
-                    this.onmessage(JSON.parse(event.data));
-                }
-                catch (exc) {
-                    console.error(`error on message: ${exc}`);
-                }
-            }
-        };
-
-        this.client.onclose = (event) => {
-            switch (event.code) {
-                case 1000:
-                    console.log('WebSocket: Closed');
-                    break;
-                default:
-                    this.reconnect();
-                    break;
-            }
-            //this.onclose(event);
-        };
-
-        this.client.onerror = (error) => {
-            switch (error.code) {
-                case 'ECONNREFUSED':
-                    this.reconnect();
-                    break;
-                default:
-                    //console.log(`WebSocket: Error - ${error.code}`);
-                    //this.onerror(error);
-                    break;
-            }
-        };
+        console.log("mock websocketclient: requested page open " + page);
     }
 
     send(data) {
-        this.client.send(JSON.stringify(data));
-    }
-
-    removeClientListeners() {
-        this.client.onopen = null;
-        this.client.onmessage = null;
-        this.client.onclose = null;
-        this.client.onerror = null;
-    }
-
-    reconnect() {
-        console.log(`WebSocketClient: Retry in ${this.autoReconnectInterval}ms`);
-        this.removeClientListeners();
-        setTimeout(() => {
-            console.log('WebSocketClient: Reconnecting...');
-            this.open(this.url);
-        }, this.autoReconnectInterval);
+        console.log('Mock client asked to send data.');
     }
 }
+
+/* exported WebSocketClient */
+// class WebSocketClient {
+//     constructor() {
+//         this.number = 0;
+//         this.autoReconnectInterval = 5 * 1000;
+//         this.onopen = null;
+//         this.onmessage = null;
+//         this.url = `${document.body.dataset.baseUrl.replace('http', 'ws')}ws/`;
+//     }
+
+//     open(page) {
+//         console.log(`WebSocket: connecting to ${this.url}`);
+//         this.client = new WebSocket(this.url);
+//         this.client.onopen = () => {
+//             console.log('WebSocket: connected');
+//             if (this.onopen) {
+//                 this.onopen();
+//             }
+//             const term_course_arr = document.body.dataset.courseUrl.split('/');
+//             const course = term_course_arr.pop();
+//             const term = term_course_arr.pop();
+//             this.client.send(JSON.stringify({'type': 'new_connection', 'page': `${term}-${course}-${page}`}));
+//         };
+
+//         this.client.onmessage = (event) => {
+//             this.number++;
+//             if (this.onmessage) {
+//                 try {
+//                     this.onmessage(JSON.parse(event.data));
+//                 }
+//                 catch (exc) {
+//                     console.error(`error on message: ${exc}`);
+//                 }
+//             }
+//         };
+
+//         this.client.onclose = (event) => {
+//             switch (event.code) {
+//                 case 1000:
+//                     console.log('WebSocket: Closed');
+//                     break;
+//                 default:
+//                     this.reconnect();
+//                     break;
+//             }
+//             //this.onclose(event);
+//         };
+
+//         this.client.onerror = (error) => {
+//             switch (error.code) {
+//                 case 'ECONNREFUSED':
+//                     this.reconnect();
+//                     break;
+//                 default:
+//                     //console.log(`WebSocket: Error - ${error.code}`);
+//                     //this.onerror(error);
+//                     break;
+//             }
+//         };
+//     }
+
+//     send(data) {
+//         this.client.send(JSON.stringify(data));
+//     }
+
+//     removeClientListeners() {
+//         this.client.onopen = null;
+//         this.client.onmessage = null;
+//         this.client.onclose = null;
+//         this.client.onerror = null;
+//     }
+
+//     reconnect() {
+//         console.log(`WebSocketClient: Retry in ${this.autoReconnectInterval}ms`);
+//         this.removeClientListeners();
+//         setTimeout(() => {
+//             console.log('WebSocketClient: Reconnecting...');
+//             this.open(this.url);
+//         }, this.autoReconnectInterval);
+//     }
+// }
