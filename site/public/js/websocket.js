e70f792aad4996ef606c7fbccd8599cfef2ce41a
Evan Maicus <emaicus@outlook.com>
2020-09-13 13:39:11 -0400

    Fix websocket port

--- site/public/js/websocket.js
+++ site/public/js/websocket.js
@@ -31,7 +31,7 @@ class WebSocketClient {
         this.onmessage = null;
         // We do string replacement here so that http -> ws, https -> wss.
         let my_url = new URL(document.body.dataset.baseUrl.replace('http', 'ws'));
-        my_url.port = 8334;
+        my_url.port = 8443;
         my_url.pathname = "ws";
         this.url = my_url.href;
     }
