3cbbb24c335550900b1c0375969b87041f36a51b
DefinitiveAbove <definitiveabove@gmail.com>
2020-10-04 18:19:27 -0400

    [Feature:Autograding] release_hidden_details (#5932)
    * Added in release_hidden_details
    
    Added in release_hidden_details
    - requires "hidden" to be enabled
    - reveals details on grade release about a hidden testcase if true
    - default is false
    
    * fixed show_hidden for non-ta gradeables
    
    * remove test case
    
    * added test case
    
    * add in tests for cpp_hidden_tests
    
    * added in parens and removed redundant equals

--- site/app/controllers/student/SubmissionController.php
+++ site/app/controllers/student/SubmissionController.php
@@ -173,7 +173,7 @@ class SubmissionController extends AbstractController {
             // Only show hidden test cases if the display version is the graded version (and grades are released)
             $show_hidden = false;
             if ($graded_gradeable != null) {
-                $show_hidden = $version == $graded_gradeable->getOrCreateTaGradedGradeable()->getGradedVersion(false) && $gradeable->isTaGradeReleased();
+                $show_hidden = (!$gradeable->isTaGrading() || $version == $graded_gradeable->getOrCreateTaGradedGradeable()->getGradedVersion(false)) && $gradeable->isTaGradeReleased();
                 // can this user access grade inquiries for this graded_gradeable
                 $can_inquiry = $this->core->getAccess()->canI("grading.electronic.grade_inquiry", ['graded_gradeable' => $graded_gradeable]);
             }
