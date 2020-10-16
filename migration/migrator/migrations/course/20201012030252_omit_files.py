7f79a8d4857a0c40aeee058b672e84b6b03867ba
unknown <hultoj@rpi.edu>
2020-10-16 17:46:02 -0400

    migration fix

--- migration/migrator/migrations/course/20201012030252_omit_files.py
+++ migration/migrator/migrations/course/20201012030252_omit_files.py
@@ -1,7 +1,9 @@
 def up(config, database, semester, course):
-    database.execute("CREATE TABLE electronic_gradeable_hidden_files (g_id character varying(255) NOT NULL, file_wildcard character varying(255) NOT NULL, lowest_access_group INTEGER NOT NULL DEFAULT 3);")
-    database.execute("ALTER TABLE ONLY electronic_gradeable_hidden_files ADD CONSTRAINT electronic_gradeable_hidden_files_pk PRIMARY KEY (g_id, file_wildcard);")
-    database.execute("ALTER TABLE ONLY electronic_gradeable_hidden_files ADD CONSTRAINT electronic_gradeable_hidden_files_fk FOREIGN KEY (g_id) REFERENCES electronic_gradeable(g_id) ON DELETE CASCADE;")
+    database.execute("""CREATE TABLE IF NOT EXISTS electronic_gradeable_hidden_files (g_id character varying(255) NOT NULL,
+     file_wildcard character varying(255) NOT NULL, 
+     lowest_access_group INTEGER NOT NULL DEFAULT 3, 
+     CONSTRAINT electronic_gradeable_hidden_files_pk PRIMARY KEY (g_id, file_wildcard), 
+     CONSTRAINT electronic_gradeable_hidden_files_fk FOREIGN KEY (g_id) REFERENCES electronic_gradeable(g_id) ON DELETE CASCADE);""")
 
 def down(config, database, semester, course):
 	pass
