e521bf89013c907d656ee173cd7ee726a2da53a0
unknown <hultoj@rpi.edu>
2020-10-26 03:15:08 -0400

    temp fix

--- migration/migrator/migrations/course/20201012030252_omit_files.py
+++ migration/migrator/migrations/course/20201012030252_omit_files.py
@@ -2,5 +2,6 @@ def up(config, database, semester, course):
     database.execute("""ALTER TABLE electronic_gradeable ADD eg_hidden_files character varying(1024);""")
 
 def down(config, database, semester, course):
-	database.execute("""DROP TABLE electronic_gradeable_hidden_files;""")
-	database.execute("""ALTER TABLE electronic_gradeable DROP COLUMN eg_hidden_files;""")
+	#database.execute("""DROP TABLE electronic_gradeable_hidden_files;""")
+	#database.execute("""ALTER TABLE electronic_gradeable DROP COLUMN eg_hidden_files;""")
+	pass
