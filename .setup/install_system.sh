aa88b578cbefa84d1d82a13fc5a470842769c3f9
Shail Patel <shailpatel@Shails-MacBook-Pro.local>
2020-11-02 23:34:28 -0500

    only clobber www-data file

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -484,7 +484,7 @@ EOF
         fi
     fi
 
-    cp ${SUBMITTY_REPOSITORY}/.setup/php-fpm/pool.d/submitty.conf /etc/php/${PHP_VERSION}/fpm/pool.d/submitty.conf
+    cp -n ${SUBMITTY_REPOSITORY}/.setup/php-fpm/pool.d/submitty.conf /etc/php/${PHP_VERSION}/fpm/pool.d/submitty.conf
     cp ${SUBMITTY_REPOSITORY}/.setup/apache/www-data /etc/apache2/suexec/www-data
     chmod 0640 /etc/apache2/suexec/www-data
 
