30b949f30f99b08556c28da6ca325d8670088b48
Barb Cutler <bmcutler@gmail.com>
2020-12-25 16:41:55 -0500

    stupid lint2

--- .github/workflows/pr_title_tester.py
+++ .github/workflows/pr_title_tester.py
@@ -2,9 +2,7 @@ import re
 
 
 # paste this from pr_title.yml
-exp = "^(\[SYSADMIN ACTION\])?(\[(Bugfix|Feature|Refactor|Testing|Documentation|\
-VPAT|UI\/UX):(Submission|Autograding|Forum|Notifications|TAGrading|InstructorUI|\
-RainbowGrades|System|Developer|API)\] .{2,40}$|\[(DevDependency|Dependency)\] .{2,70})$"
+exp = r"^(\[SYSADMIN ACTION\])?(\[(Bugfix|Feature|Refactor|Testing|Documentation|VPAT|UI\/UX):(Submission|Autograding|Forum|Notifications|TAGrading|InstructorUI|RainbowGrades|System|Developer|API)\] .{2,40}$|\[(DevDependency|Dependency)\] .{2,70})$"
 
 
 # should pass
@@ -16,7 +14,7 @@ assert re.search(exp, "[SYSADMIN ACTION][Refactor:Autograding] xxxx")
 assert re.search(exp, "[DevDependency] xxxx")
 assert re.search(exp, "[Dependency] xxxx")
 assert re.search(exp, "[Bugfix:Submission] 0123456789012345678901234567890123456789")
-assert re.search(exp, "[Dependency] 012345678901234567890123456789"+
+assert re.search(exp, "[Dependency] 012345678901234567890123456789" +
                  "0123456789012345678901234567890123456789")
 
 # should fail
@@ -28,5 +26,5 @@ assert not re.search(exp, "[Dependency:Autograding] xxxx")
 assert not re.search(exp, "[SYSADMINACTION][Refactor:Autograding] xxxx")
 assert not re.search(exp, "[DevDependency:Autograding] xxxx")
 assert not re.search(exp, "[Bugfix:Submission] 01234567890123456789012345678901234567890")
-assert not re.search(exp, "[Dependency] 012345678901234567890123456789"+
+assert not re.search(exp, "[Dependency] 012345678901234567890123456789" +
                      "01234567890123456789012345678901234567890")
