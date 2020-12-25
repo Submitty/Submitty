53cd0dd7afcfd13a3aa0e0ed7893d2986f749297
Barb Cutler <bmcutler@gmail.com>
2020-12-25 16:38:41 -0500

    stupid lint

--- .github/workflows/pr_title_tester.py
+++ .github/workflows/pr_title_tester.py
@@ -2,7 +2,9 @@ import re
 
 
 # paste this from pr_title.yml
-exp = '^(\[SYSADMIN ACTION\])?(\[(Bugfix|Feature|Refactor|Testing|Documentation|VPAT|UI\/UX):(Submission|Autograding|Forum|Notifications|TAGrading|InstructorUI|RainbowGrades|System|Developer|API)\] .{2,40}$|\[(DevDependency|Dependency)\] .{2,70})$'
+exp = "^(\[SYSADMIN ACTION\])?(\[(Bugfix|Feature|Refactor|Testing|Documentation|\
+VPAT|UI\/UX):(Submission|Autograding|Forum|Notifications|TAGrading|InstructorUI|\
+RainbowGrades|System|Developer|API)\] .{2,40}$|\[(DevDependency|Dependency)\] .{2,70})$"
 
 
 # should pass
@@ -14,7 +16,8 @@ assert re.search(exp, "[SYSADMIN ACTION][Refactor:Autograding] xxxx")
 assert re.search(exp, "[DevDependency] xxxx")
 assert re.search(exp, "[Dependency] xxxx")
 assert re.search(exp, "[Bugfix:Submission] 0123456789012345678901234567890123456789")
-assert re.search(exp, "[Dependency] 0123456789012345678901234567890123456789012345678901234567890123456789")
+assert re.search(exp, "[Dependency] 012345678901234567890123456789"+
+                 "0123456789012345678901234567890123456789")
 
 # should fail
 assert not re.search(exp, "[UI//UX:API] xxxx")
@@ -25,4 +28,5 @@ assert not re.search(exp, "[Dependency:Autograding] xxxx")
 assert not re.search(exp, "[SYSADMINACTION][Refactor:Autograding] xxxx")
 assert not re.search(exp, "[DevDependency:Autograding] xxxx")
 assert not re.search(exp, "[Bugfix:Submission] 01234567890123456789012345678901234567890")
-assert not re.search(exp, "[Dependency] 01234567890123456789012345678901234567890123456789012345678901234567890")
+assert not re.search(exp, "[Dependency] 012345678901234567890123456789"+
+                     "01234567890123456789012345678901234567890")
