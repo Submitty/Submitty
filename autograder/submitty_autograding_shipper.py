dbb03fb4b81d756fbadb35b7286a2c2bb13e6a3f
Barb Cutler <bmcutler@gmail.com>
2021-01-16 18:49:44 -0500

    lint

--- autograder/submitty_autograding_shipper.py
+++ autograder/submitty_autograding_shipper.py
@@ -32,16 +32,16 @@ from autograder import packer_unpacker
 from autograder import config as submitty_config
 
 
-import sys
-sys.path.append('/usr/local/submitty/sbin/shipper_utils/')
-import ssh_jump_proxy
-
-
 INTERACTIVE_QUEUE = ''
 IN_PROGRESS_PATH = ''
 JOB_ID = '~SHIP~'
 
 
+"""
+This function is copied, would rather use the version in this file instead.
+import sys
+sys.path.append('/usr/local/submitty/sbin/shipper_utils/')
+import ssh_jump_proxy
 """
 def ssh_connection_allowing_jump_proxy(target_user, target_host):
 
@@ -55,7 +55,7 @@ def ssh_connection_allowing_jump_proxy(target_user, target_host):
         conf.parse(open(config_file))
         o = conf.lookup(target_host)
         if 'proxyjump' in o:
-            (intermediate_user,intermediate_host) = str(o['proxyjump']).split('@')
+            (intermediate_user, intermediate_host) = str(o['proxyjump']).split('@')
         if 'identityfile' in o:
             my_key_filename = o['identityfile'][0]
 
@@ -67,9 +67,9 @@ def ssh_connection_allowing_jump_proxy(target_user, target_host):
             intermediate_connection = paramiko.SSHClient()
             intermediate_connection.get_host_keys()
             intermediate_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            intermediate_connection.connect(hostname=intermediate_host, 
-                                            username = intermediate_user,
-                                            timeout=60, 
+            intermediate_connection.connect(hostname=intermediate_host,
+                                            username=intermediate_user,
+                                            timeout=60,
                                             key_filename=my_key_filename)
             my_sock = intermediate_connection.get_transport().open_channel(
                 'direct-tcpip', (target_host, 22), ('', 0)
@@ -78,18 +78,17 @@ def ssh_connection_allowing_jump_proxy(target_user, target_host):
         target_connection = paramiko.SSHClient()
         target_connection.get_host_keys()
         target_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        target_connection.connect(hostname = target_host, 
-                                  username = target_user, 
-                                  timeout=60, 
-                                  sock=my_sock, 
+        target_connection.connect(hostname=target_host,
+                                  username=target_user,
+                                  timeout=60,
+                                  sock=my_sock,
                                   key_filename=my_key_filename)
         # return both connections (so they can be cleaned up)
-        return (target_connection,intermediate_connection)
+        return (target_connection, intermediate_connection)
 
     except Exception as e:
         print("ERROR: could not open a connection to the target host: "+str(e))
         raise e
-"""
 
 
 def instantiate_global_variables(config):
@@ -372,7 +371,8 @@ def establish_ssh_connection(
     retry_delay = .1
     while not connected:
         try:
-            (target_connection,intermediate_connection) = ssh_jump_proxy.ssh_connection_allowing_jump_proxy(user,host)
+            (target_connection,intermediate_connection) = ssh_connection_allowing_jump_proxy(user,host)
+            #(target_connection,intermediate_connection) = ssh_jump_proxy.ssh_connection_allowing_jump_proxy(user,host)
             connected = True
         except Exception:
             if only_try_once:
