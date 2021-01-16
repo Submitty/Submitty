a446b77079dffbacf17ee02cbc23c06c70d0fc19
Barb Cutler <bmcutler@gmail.com>
2021-01-16 16:58:24 -0500

    edit

--- sbin/shipper_utils/systemctl_wrapper.py
+++ sbin/shipper_utils/systemctl_wrapper.py
@@ -6,53 +6,7 @@ from os import path
 import sys
 import json
 import paramiko
-#import ssh_jump_proxy
-
-
-
-
-
-
-def ssh_connection_allowing_jump_proxy(target_user, target_host):
-
-    # load and parse the ssh config
-    my_key_filename = None
-    intermediate_host = None
-    intermediate_user = None
-    config_file = os.path.expanduser("~")+"/.ssh/config"
-    if (os.path.exists(config_file)):
-        conf = paramiko.SSHConfig()
-        conf.parse(open(config_file))
-        o = conf.lookup(target_host)
-        if 'proxyjump' in o:
-            (intermediate_user,intermediate_host) = str(o['proxyjump']).split('@')
-        if 'identityfile' in o:
-            my_key_filename = o['identityfile'][0]
-
-    try:
-        # if this connection has a jump proxy, open that connection first
-        my_sock = None
-        intermediate_connection = None
-        if intermediate_host:
-            intermediate_connection = paramiko.SSHClient()
-            intermediate_connection.get_host_keys()
-            intermediate_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            intermediate_connection.connect(hostname = intermediate_host, username = intermediate_user, timeout=60, key_filename=my_key_filename)
-            my_sock = intermediate_connection.get_transport().open_channel(
-                'direct-tcpip', (target_host, 22), ('', 0)
-            )
-        # open the actual connection
-        target_connection = paramiko.SSHClient()
-        target_connection.get_host_keys()
-        target_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        target_connection.connect(hostname = target_host, username = target_user, timeout=60, sock=my_sock, key_filename=my_key_filename)
-        # return both connections (so they can be cleaned up)
-        return (target_connection,intermediate_connection)
-
-    except Exception as e:
-        print("ERROR: could not open a connection to the target host: "+str(e))
-        raise e
-
+import ssh_jump_proxy
 
 
 CONFIG_PATH = path.join(path.dirname(path.realpath(__file__)), '..', '..','config')
@@ -140,7 +94,7 @@ def perform_systemctl_command_on_worker(daemon, mode, target):
   script_directory = os.path.join(INSTALL_DIR, 'sbin', 'shipper_utils', 'systemctl_wrapper.py')
   command = "sudo {0} {1} --daemon {2}".format(script_directory, mode, daemon)
   try:
-      (target_connection,intermediate_connection) = ssh_connection_allowing_jump_proxy(user,host)
+      (target_connection,intermediate_connection) = ssh_jump_proxy.ssh_connection_allowing_jump_proxy(user,host)
   except Exception as e:
       print("ERROR: could not ssh to {0}@{1} due to following error: {2}".format(user, host,str(e)))
       return EXIT_CODES['failure']
--- sbin/shipper_utils/update_and_install_workers.py
+++ sbin/shipper_utils/update_and_install_workers.py
@@ -9,48 +9,7 @@ import subprocess
 import docker
 import traceback
 import argparse
-#import ssh_jump_proxy
-
-
-def ssh_connection_allowing_jump_proxy(target_user, target_host):
-
-    # load and parse the ssh config
-    my_key_filename = None
-    intermediate_host = None
-    intermediate_user = None
-    config_file = os.path.expanduser("~")+"/.ssh/config"
-    if (os.path.exists(config_file)):
-        conf = paramiko.SSHConfig()
-        conf.parse(open(config_file))
-        o = conf.lookup(target_host)
-        if 'proxyjump' in o:
-            (intermediate_user,intermediate_host) = str(o['proxyjump']).split('@')
-        if 'identityfile' in o:
-            my_key_filename = o['identityfile'][0]
-
-    try:
-        # if this connection has a jump proxy, open that connection first
-        my_sock = None
-        intermediate_connection = None
-        if intermediate_host:
-            intermediate_connection = paramiko.SSHClient()
-            intermediate_connection.get_host_keys()
-            intermediate_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            intermediate_connection.connect(hostname = intermediate_host, username = intermediate_user, timeout=60, key_filename=my_key_filename)
-            my_sock = intermediate_connection.get_transport().open_channel(
-                'direct-tcpip', (target_host, 22), ('', 0)
-            )
-        # open the actual connection
-        target_connection = paramiko.SSHClient()
-        target_connection.get_host_keys()
-        target_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        target_connection.connect(hostname = target_host, username = target_user, timeout=60, sock=my_sock, key_filename=my_key_filename)
-        # return both connections (so they can be cleaned up)
-        return (target_connection,intermediate_connection)
-
-    except Exception as e:
-        print("ERROR: could not open a connection to the target host: "+str(e))
-        raise e
+import ssh_jump_proxy
 
 
 CONFIG_PATH = path.join(path.dirname(path.realpath(__file__)), '..', '..','config')
@@ -118,7 +77,7 @@ def run_commands_on_worker(user, host, commands, operation='unspecified operatio
     else:
         success = False
         try:
-            (target_connection,intermediate_connection) = ssh_connection_allowing_jump_proxy(user,host)
+            (target_connection,intermediate_connection) = ssh_jump_proxy.ssh_connection_allowing_jump_proxy(user,host)
         except Exception as e:
             print(f"ERROR: could not ssh to {user}@{host} due to following error: {str(e)}")
             return False
