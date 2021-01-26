ae40074f6421ef3d20cfcb1e7e9fe0a68efe3c73
Evan Maicus <emaicus@outlook.com>
2021-01-26 09:44:30 -0500

    Remove stray comma

--- autograder/autograder/grade_item.py
+++ autograder/autograder/grade_item.py
@@ -52,7 +52,7 @@ def get_testcases(
         for item in complete_config_obj['item_pool']:
             testcase_specs += item['testcases']
 
-    is_vcs = queue_obj['vcs_checkout'],
+    is_vcs = queue_obj['vcs_checkout']
 
     # Construct the testcase objects
     for t in testcase_specs:
