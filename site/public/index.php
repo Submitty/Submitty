4fd94a451c97c743d0735159f70592f79d6a0462
Shail Patel <shailpatel67@gmail.com>
2021-02-03 18:56:59 -0500

    test

--- site/public/index.php
+++ site/public/index.php
@@ -60,9 +60,6 @@ set_exception_handler("exception_handler");
 
 function error_handler() {
     $error = error_get_last();
-    if($error === NULL){
-        return;
-    }
     if ($error['type'] === E_ERROR) {
         exception_handler(new BaseException("Fatal Error: " . $error['message'] . " in file
         " . $error['file'] . " on line " . $error['line']));
