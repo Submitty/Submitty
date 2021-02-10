90435be0f816805a447061ce366166985e56ffdd
Vikram Srikishan <vikrsri@gmail.com>
2021-02-10 01:49:34 -0500

    modified migration and ran course dump

--- migration/migrator/migrations/course/20210204140110_modify_subsection.py
+++ migration/migrator/migrations/course/20210204140110_modify_subsection.py
@@ -1,3 +1,7 @@
 
 def up(config, database, semester, course):
+    database.execute("UPDATE users SET registration_subsection = '' WHERE registration_subsection IS NULL")
     database.execute('ALTER TABLE users ALTER COLUMN registration_subsection SET DEFAULT '' NOT NULL')
+
+def down(config, database, semester, course):
+    database.execute('ALTER TABLE users ALTER COLUMN registration_subsection SET DEFAULT null')
