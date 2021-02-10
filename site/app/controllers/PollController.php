362237730a4ab381e29afe0cbe5997d595bc754a
Ben Kelly <benjaminkelly@me.com>
2021-02-10 15:27:37 -0500

    Update site/app/controllers/PollController.php
    Co-authored-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/app/controllers/PollController.php
+++ site/app/controllers/PollController.php
@@ -378,7 +378,7 @@ class PollController extends AbstractController {
             $this->core->addErrorMessage("Invalid Poll ID");
             return new RedirectResponse($this->core->buildCourseUrl(['polls']));
         }
-        $image_path = $this->core->getQueries()->getPoll($_POST["poll_id"]);
+        $image_path = $this->core->getQueries()->getPoll($_POST["poll_id"])->getImagePath();
         if ($image_path != null) {
             unlink($image_path);
         }
