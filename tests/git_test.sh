70593951c244d9add3b82a76cdce3292c7568a40
Shail Patel <shailpatel67@gmail.com>
2021-02-15 19:09:25 -0500

    cleanup git tests

--- tests/git_test.sh
+++ tests/git_test.sh
@@ -2,8 +2,7 @@ SEMESTER=$(python3 -c 'from datetime import datetime; today = datetime.today(); 
 
 test_git() {
     random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-    echo http://${1}:${1}@localhost/git/${SEMESTER}/sample/open_homework/$2 open_homework
-    git clone --progress --verbose http://${1}:${1}@localhost/git/${SEMESTER}/sample/open_homework/$2 open_homework
+    git clone http://${1}:${1}@localhost/git/${SEMESTER}/sample/open_homework/$2 open_homework
     cd open_homework
     echo "foo" >> test.txt
     git add .
@@ -29,15 +28,13 @@ err_message() {
 }
 
 # Display our error message if something fails below
-# trap 'err_message' ERR
+trap 'err_message' ERR
 
-# set -ev
+set -ev
 
 mkdir /tmp/submitty_git
 pushd /tmp/submitty_git
 
-echo $SEMESTER
-git clone --progress --verbose http://instructor:$instructor@localhost/git/${SEMESTER}/sample/open_homework/$2 open_homework
 test_git instructor instructor
 test_git instructor student
 test_git student student
