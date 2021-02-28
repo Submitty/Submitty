cd8d3c03ab25e4ba3bd6e9fbbdf6e02b6a527b14
Vikram Srikishan <vikrsri@gmail.com>
2021-02-28 04:38:11 -0500

    more migration changes

--- migration/migrator/migrations/course/20210204140110_modify_subsection.py
+++ migration/migrator/migrations/course/20210204140110_modify_subsection.py
@@ -1,7 +1,6 @@
 
 def up(config, database, semester, course):
     database.execute("UPDATE users SET registration_subsection = '' WHERE registration_subsection IS NULL")
-    database.execute('ALTER TABLE users ALTER COLUMN registration_subsection SET DEFAULT '' NOT NULL')
     database.execute('ALTER TABLE users ALTER COLUMN registration_subsection SET DEFAULT  '', ALTER COLUMN registration_subsection SET NOT NULL')
 
 def down(config, database, semester, course):
