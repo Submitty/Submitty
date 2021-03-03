7bfdf2c4256020e09818752f81dd728124217105
Shail Patel <shailpatel67@gmail.com>
2021-03-03 14:51:07 -0500

    [Bugfix:TAGrading] add grade inquiry checks (#6215)
    * initial work
    
    * phplint
    
    * suggested changes
    
    * lint

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -4302,7 +4302,10 @@ AND gc_id IN (
     }
 
     public function getRegradePost($post_id) {
-        $this->course_db->query("SELECT * FROM regrade_discussion WHERE id = ?", [$post_id]);
+        $this->course_db->query(
+            "SELECT * FROM regrade_discussion WHERE id = ?",
+            [$post_id]
+        );
         return $this->course_db->row();
     }
 
