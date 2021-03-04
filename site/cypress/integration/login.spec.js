4825292ef92391ef6336b7f5f504fa7c071e7e0f
Shail Patel <shailpatel67@gmail.com>
2021-03-03 22:39:20 -0500

    requested changes

--- site/cypress/integration/login.spec.js
+++ site/cypress/integration/login.spec.js
@@ -1,17 +1,12 @@
-import {buildUrl} from '../support/commands.js';
+import {buildUrl} from '../support/utils.js';
 
 
 describe('Test cases revolving around the logging in functionality of the site', () => {
-
-	describe('foo', () => {
-		cy.visit('/');
-	});
-
 	it('should log in through root endpoint', () => {
 		const user = 'instructor';
 
 		//should hit the login form
-		cy.visit('/');
+		cy.visit([]);
 		cy.url().should('eq', `${Cypress.config('baseUrl')}/authentication/login` );
 
 		//fill out the login form and hit submit
@@ -32,12 +27,14 @@ describe('Test cases revolving around the logging in functionality of the site',
 
 
 	it('should login through login endpoint', () => {
-		const url = '/authentication/login';
+		const user = 'instructor';
 
-		cy.visit(url);
+		cy.visit(['authentication', 'login']);
 		cy.url().should('eq', `${Cypress.config('baseUrl')}/authentication/login` );
+		cy.get('input[name=user_id]').type(user);
+		cy.get('input[name=password]').type(user);
+		cy.get('input[name=login]').click();
 
-		cy.login();
 		cy.get('#logout .icon-title').should((val) => {
 			expect( val.text().trim() ).to.equal('Logout Quinn');
 		});
@@ -45,7 +42,7 @@ describe('Test cases revolving around the logging in functionality of the site',
 
 
 	it('should reject bad passwords', () => {
-		cy.visit('/')
+		cy.visit([]);
 
 		cy.get('input[name=user_id]').type('instructor');
 		cy.get('input[name=password]').type('bad-password');
@@ -58,7 +55,7 @@ describe('Test cases revolving around the logging in functionality of the site',
 
 
 	it('should reject bad usernames', () => {
-		cy.visit('/')
+		cy.visit([]);
 
 		cy.get('input[name=user_id]').type('bad-username');
 		cy.get('input[name=password]').type('instructor');
@@ -72,11 +69,10 @@ describe('Test cases revolving around the logging in functionality of the site',
 
 	it('should redirect after logging in', () => {
 		//try to visit a page not logged in, then log in and see where we are
-		const url = buildUrl(['sample', 'gradeable', 'open_homework']); 
 		const full_url = buildUrl(['sample', 'gradeable', 'open_homework'], true);
 		const user = 'instructor';
 
-		cy.visit(url);
+		cy.visit(['sample', 'gradeable', 'open_homework']);
 		cy.url().should('eq', `${Cypress.config('baseUrl')}/authentication/login?old=${encodeURIComponent(full_url)}`);
 
 		cy.get('input[name=user_id]').type(user);
@@ -88,10 +84,8 @@ describe('Test cases revolving around the logging in functionality of the site',
 
 
 	it('should check if you can access a course', () => {
-		const url = buildUrl(['sample']);
-
 		cy.login('pearsr');
-		cy.visit(url);
+		cy.visit(['sample']);
 
 		cy.get('.content').should('have.text', "\n    You don't have access to this course. \n    This is sample for Spring 2021. \n    If you think this is a mistake, please contact your instructor to gain access. \n    click  here  to back to homepage and see your courses list.\n");
 	})
--- site/cypress/support/commands.js
+++ site/cypress/support/commands.js
@@ -23,43 +23,8 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-
-//Adding a cypress function requires each function to yield a value instead of returning it directly
-//These functions are defined in normal JS and can be imported instead
-
-
-/**
-* Generate a 3 letter semester code e.g s21, f20 based on today's data
-* This functions the same as the submitty python util's get_current_semester
-*
-* @returns {String}
-*/
-export function getCurrentSemester(){
-	const today = new Date();
-	const year = today.getFullYear().toString().slice(2,4);	//get last two digits
-	const semester = ((today.getMonth() + 1) < 7) ? "s" : "f";	//first half of year 'spring' rest is fall
-
-	return semester + year;
-}
-
-/**
-* Build a courseURL based on an array of 'parts', e.g [foo, bar] -> courses/s21/foo/bar
-* 
-* @param {String[]} [parts=[]] array of parts to string together
-* @param {Boolean} [include_base=false] whether to include the url base (e.g. http://localhost:1501/) or not
-* @returns {String}
-*/
-export function buildUrl(parts = [], include_base = false){
-	let url = "";
-	if(include_base){
-		url = Cypress.config('baseUrl') + '/';
-	}
-
-	return `${url}courses/${getCurrentSemester()}/${parts.join('/')}`
-}
-
-
-//These functions can be called like "cy.foo(...)" and will yeild a result
+import {buildUrl} from './utils.js';
+//These functions can be called like "cy.login(...)" and will yeild a result
 
 /**
 * Log into Submitty, assumes no one is logged in already
@@ -67,8 +32,23 @@ export function buildUrl(parts = [], include_base = false){
 * @param {String} [username=instructor] - username & password of who to log in as
 */
 Cypress.Commands.add("login", (username="instructor") => { 
-	cy.visit('/');
+	cy.visit([]);
 	cy.get('input[name=user_id]').type(username);
 	cy.get('input[name=password]').type(username);
 	cy.get('input[name=login]').click();
+});
+
+
+Cypress.Commands.overwrite("visit", (originalFn, options) => { 
+	let url = '';
+
+	if(Array.isArray(options)){
+		url = buildUrl(options);
+	}else if((typeof options) === 'string'){
+		url = buildUrl([]) + options;
+	}else{
+		url = buildUrl([]);
+	}
+
+	originalFn(url);
 });
