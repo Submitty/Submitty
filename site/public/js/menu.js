f3acf9834e72f6409397777dc6e987cbf99a6b89
Matthew Peveler <matt.peveler@gmail.com>
2021-04-15 08:08:35 -0400

    [Refactor:Developer] Fix eslint errors in ignored files (#6336)
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/public/js/menu.js
+++ site/public/js/menu.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(() => {
     function openMenu() {
-        $("body").css("overflow", "hidden");
+        $('body').css('overflow', 'hidden');
         $(document.body).scrollTop(0);
         $(document.body).scrollLeft(0);
-        $("#menu-overlay").addClass("mobile-block");
-        $("#mobile-menu").addClass("mobile-block");
-    }
-    
-    function closeMenu() {
-        $("body").css("overflow", "unset");
-        $("#menu-overlay").removeClass("mobile-block");
-        $("#mobile-menu").removeClass("mobile-block");
+        $('#menu-overlay').addClass('mobile-block');
+        $('#mobile-menu').addClass('mobile-block');
     }
 
-    $("#menu-button").click(openMenu);
-    $("#menu-exit").click(closeMenu);
-    $("#menu-overlay").click(closeMenu);
-});  
+    function closeMenu() {
+        $('body').css('overflow', 'unset');
+        $('#menu-overlay').removeClass('mobile-block');
+        $('#mobile-menu').removeClass('mobile-block');
+    }
+
+    $('#menu-button').click(openMenu);
+    $('#menu-exit').click(closeMenu);
+    $('#menu-overlay').click(closeMenu);
+});
