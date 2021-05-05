7dcc03b4692e9e1020d0248a006225b33c40a33e
Shail Patel <shailpatel67@gmail.com>
2021-05-04 21:03:23 -0400

    eslint

--- site/cypress/integration/api/auth.spec.js
+++ site/cypress/integration/api/auth.spec.js
@@ -1,90 +1,88 @@
-import {buildUrl} from '../../support/utils.js';
-
 function buildPostRequest(url, body, auth = null){
-	let header = {'Content-Type': 'application/json'};
-	if(auth !== null){
-		header['Authorization'] = auth;
-	}
+    const header = {'Content-Type': 'application/json'};
+    if (auth !== null){
+        header['Authorization'] = auth;
+    }
 
-	return {
-		'method' : 'POST',
-		'url' : url,
-		'headers' : header,
-		'body' : JSON.stringify(body)
-	};
+    return {
+        'method' : 'POST',
+        'url' : url,
+        'headers' : header,
+        'body' : JSON.stringify(body),
+    };
 }
 
 function buildGetRequest(url, auth = null){
-	let header = {'Content-Type': 'application/json'};
-	if(auth !== null){
-		header['Authorization'] = auth;
-	}
+    const header = {'Content-Type': 'application/json'};
+    if (auth !== null){
+        header['Authorization'] = auth;
+    }
 
-	return {
-		'method' : 'POST',
-		'url' : url,
-		'headers' : header
-	};
+    return {
+        'method' : 'POST',
+        'url' : url,
+        'headers' : header,
+    };
 }
 
 describe('Test cases revolving around the API', () => {
 
-	it('should authenticate a user', () => {
-		cy.request(buildPostRequest('api/token', {
-			'user_id' : 'instructor',
-			'password': 'instructor'
-		})).should((response) => {
-			expect(response).to.have.property('body');
+    it('should authenticate a user', () => {
+        cy.request(buildPostRequest('api/token', {
+            'user_id' : 'instructor',
+            'password': 'instructor',
+        })).should((response) => {
+            expect(response).to.have.property('body');
 
-			const response_json = JSON.parse(response.body);
-			expect(response_json['status']).to.equal('success');
-			expect(response_json['data']).to.have.property('token');
-		});
-	});
-
-
-	it('should require a user_id and password', () => {
-		cy.request(buildPostRequest('api/token', {
-			'foo' : 'bar'
-		})).should((response) => {
-			const data = JSON.parse(response.body);
-			expect(data['status']).to.equal('fail');
-			expect(data['message']).to.equal('Cannot leave user id or password blank');
-		});
-	});
+            const response_json = JSON.parse(response.body);
+            expect(response_json['status']).to.equal('success');
+            expect(response_json['data']).to.have.property('token');
+        });
+    });
 
 
-	it.only('should invalidate older tokens on request', () => {
+    it('should require a user_id and password', () => {
+        cy.request(buildPostRequest('api/token', {
+            'foo' : 'bar',
+        })).should((response) => {
+            const data = JSON.parse(response.body);
+            expect(data['status']).to.equal('fail');
+            expect(data['message']).to.equal('Cannot leave user id or password blank');
+        });
+    });
 
-		cy.request(buildPostRequest('api/token', {
-			'user_id' : 'instructor',
-			'password': 'instructor'
-		})).as('old_response');
 
-	
-		cy.get('@old_response').then((old) => {
-			cy.request(buildPostRequest('api/token/invalidate', 'POST', {
-				'user_id' : 'instructor',
-				'password': 'instructor'
-			}));
+    it.only('should invalidate older tokens on request', () => {
 
-			cy.request(buildPostRequest('api/token', {
-				'user_id' : 'instructor',
-				'password': 'instructor'
-			})).should((response) => {
-				const data = JSON.parse(response.body);
-				const old_data = JSON.parse(old.body);
-				expect(data['data']['token']).to.not.equal(old_data['data']['token']);
+        cy.request(buildPostRequest('api/token', {
+            'user_id' : 'instructor',
+            'password': 'instructor',
+        })).as('old_response');
 
-				cy.request(buildGetRequest('api/courses', old_data['data']['token']))
-				.should((response) => {
-					const data = JSON.parse(response.body);
-					expect(data['status']).to.equal('fail');
-					expect(data['message']).to.equal('Unauthenticated access. Please log in.');
-				});
-			});
 
-		});
+        cy.get('@old_response').then((old) => {
+            cy.request(buildPostRequest('api/token/invalidate', 'POST', {
+                'user_id' : 'instructor',
+                'password': 'instructor',
+            }));
 
-	});
+            cy.request(buildPostRequest('api/token', {
+                'user_id' : 'instructor',
+                'password': 'instructor',
+            })).should((response) => {
+                const data = JSON.parse(response.body);
+                const old_data = JSON.parse(old.body);
+                expect(data['data']['token']).to.not.equal(old_data['data']['token']);
+
+                cy.request(buildGetRequest('api/courses', old_data['data']['token']))
+                    .should((response) => {
+                        const data = JSON.parse(response.body);
+                        expect(data['status']).to.equal('fail');
+                        expect(data['message']).to.equal('Unauthenticated access. Please log in.');
+                    });
+            });
+
+        });
+
+    });
 });
