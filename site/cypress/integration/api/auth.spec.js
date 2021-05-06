4097a4829b3dff46d4edaef0cb79e6ccbd7f8b63
Matthew Peveler <matt.peveler@gmail.com>
2021-05-06 11:13:53 +0000

    Update auth.spec.js
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/cypress/integration/api/auth.spec.js
+++ site/cypress/integration/api/auth.spec.js
@@ -1,10 +1,9 @@
 import {buildPostRequest, buildGetRequest} from '../../support/utils.js';
 
 describe('Test cases revolving around the API', () => {
-
     it('should authenticate a user', () => {
         cy.request(buildPostRequest('api/token', {
-            'user_id' : 'instructor',
+            'user_id': 'instructor',
             'password': 'instructor',
         })).should((response) => {
             expect(response).to.have.property('body');
@@ -15,38 +14,34 @@ describe('Test cases revolving around the API', () => {
         });
     });
 
-
-    it('should require a user_id and password', () => {
-        [
-            {'foo' : 'bar'},
-            {'user_id' : 'instructor'},
-            {'password' : 'instructor'},
-        ].forEach((postBody) => {
-            cy.request(buildPostRequest('api/token', postBody)).should((response) => {
-                const data = JSON.parse(response.body);
-                expect(data['status']).to.equal('fail');
-                expect(data['message']).to.equal('Cannot leave user id or password blank');
-            });
+    [
+      ['no user_id or password', {'foo': 'bar'}],
+      ['no password', { 'user_id': 'instructor'}],
+      ['no user_id', {'password': 'instructor'}],
+    ].forEach(([title, postBody]) => {
+      it(`should require a user_id and password - ${title}`, () => {
+        cy.request(buildPostRequest('api/token', postBody)).should((response) => {
+            const data = JSON.parse(response.body);
+            expect(data['status']).to.equal('fail');
+            expect(data['message']).to.equal('Cannot leave user id or password blank');
         });
+      });
     });
 
-
     it('should invalidate older tokens on request', () => {
-
         cy.request(buildPostRequest('api/token', {
-            'user_id' : 'instructor',
+            'user_id': 'instructor',
             'password': 'instructor',
         })).as('old_response');
 
-
         cy.get('@old_response').then((old) => {
             cy.request(buildPostRequest('api/token/invalidate', 'POST', {
-                'user_id' : 'instructor',
+                'user_id': 'instructor',
                 'password': 'instructor',
             }));
 
             cy.request(buildPostRequest('api/token', {
-                'user_id' : 'instructor',
+                'user_id': 'instructor',
                 'password': 'instructor',
             })).should((response) => {
                 const data = JSON.parse(response.body);
@@ -60,8 +55,6 @@ describe('Test cases revolving around the API', () => {
                         expect(data['message']).to.equal('Unauthenticated access. Please log in.');
                     });
             });
-
         });
-
     });
 });
