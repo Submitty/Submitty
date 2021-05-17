4a6b215692f10d32da93daa4c15c9e682ff909c3
Alayn Arambula <arambula.alayn@gmail.com>
2021-05-17 10:38:58 -0500

    Initializes data when migrating.

--- migration/migrator/migrations/course/20210320080101_add_pause_timer.py
+++ migration/migrator/migrations/course/20210320080101_add_pause_timer.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+native_utc_dt = datetime.utcnow();
+utc_string = native_utc_dt.strftime("%Y-%m-%d %H:%M:%S") + " UTC";
 
 def up(config, database, semester, course):
     database.execute('ALTER TABLE queue ADD COLUMN IF NOT EXISTS time_paused INTEGER;')
     database.execute('ALTER TABLE queue ADD COLUMN IF NOT EXISTS time_paused_start TIMESTAMP WITH TIME ZONE;')
+    database.execute('UPDATE queue SET time_paused = 0;')
+    database.execute('UPDATE queue SET time_paused_start = \'' + utc_string + '\' WHERE paused = true;')
 
 def down(config, database, semester, course):
     database.execute('ALTER TABLE queue DROP COLUMN IF EXISTS time_paused;')
