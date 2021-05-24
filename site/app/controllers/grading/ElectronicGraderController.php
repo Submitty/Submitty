4832e2cd840df4e70885530e7f8130c711cb4753
Miles Ednie <edniem@rpi.edu>
2021-05-24 14:07:01 -0400

    made checking for the cookie much cleaner

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -811,12 +811,7 @@ class ElectronicGraderController extends AbstractController {
             $this->core->addErrorMessage("You do not have permission to grade {$gradeable->getTitle()}");
             $this->core->redirect($this->core->buildCourseUrl());
         }
-        $anon_mode = false;
-        if (array_key_exists('anon_mode', $_COOKIE)) {
-            if ($_COOKIE['anon_mode'] === 'on') {
-                $anon_mode = true;
-            }
-        }
+        $anon_mode = isset($_COOKIE['anon_mode']) && $_COOKIE['anon_mode'] === 'on';
         //Checks to see if the Grader has access to all users in the course,
         //Will only show the sections that they are graders for if not TA or Instructor
         $can_show_all = $this->core->getAccess()->canI("grading.electronic.details.show_all");
