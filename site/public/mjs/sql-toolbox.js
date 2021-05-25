3e6d7439cce28c07a9e82adacb7f3f4c4b8f2bc7
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2021-05-24 21:52:54 -0400

    [Feature:InstructorUI] CSV Export of SQL Toolbox Data (#6414)

--- site/public/mjs/sql-toolbox.js
+++ site/public/mjs/sql-toolbox.js
@@ -1,8 +1,8 @@
-
 import { buildCourseUrl, getCsrfToken } from './server.js';
 
 export async function runSqlQuery() {
     document.getElementById('query-results').style.display = 'block';
+    document.getElementById('download-sql-btn').disabled = true;
 
     const form_data = new FormData();
     form_data.append('csrf_token', getCsrfToken());
@@ -68,6 +68,7 @@ export async function runSqlQuery() {
             body.appendChild(bodyRow);
         });
         table.appendChild(body);
+        document.getElementById('download-sql-btn').disabled = false;
     }
     catch (exc) {
         console.error(exc);
@@ -75,8 +76,46 @@ export async function runSqlQuery() {
     }
 }
 
+export function generateCSV(id){
+    const results = document.getElementById(id);
+    let csv = '';
+    //Add headers to CSV string
+    const header = results.children.item(0);
+    const row = header.children.item(0);
+    for (let i = 1; i < row.children.length; i++){
+        csv += `"${row.children.item(i).textContent.split('"').join('""')}",`;
+    }
+    csv += '\n';
+
+    //Add data to CSV string
+    const data = results.children.item(1);
+    for (let i = 0; i < data.children.length; i++){
+        const row = data.children.item(i);
+        for (let j = 1; j < row.children.length; j++){
+            csv += `"${row.children.item(j).textContent.split('"').join('""')}",`;
+        }
+        csv += '\n';
+    }
+    return csv;
+}
+
+export async function downloadSqlResult(id){
+    const csv = generateCSV(id);
+    //Encode and download the CSV string
+    const address = `data:text/csv;charset=utf-8,${encodeURIComponent(csv)}`;
+    const filename = 'submitty.csv';
+    const temp_element = document.createElement('a');
+    temp_element.setAttribute('href', address);
+    temp_element.setAttribute('download', filename);
+    temp_element.style.display = 'none';
+    document.body.appendChild(temp_element);
+    temp_element.click();
+    document.body.removeChild(temp_element);
+}
+
 export function init() {
     document.getElementById('run-sql-btn').addEventListener('click', () => runSqlQuery());
+    document.getElementById('download-sql-btn').addEventListener('click', () => downloadSqlResult('query-results'));
 }
 
 /* istanbul ignore next */
