6dd4b27bc66dd7187d8f8ad51a44f69f44aabaef
Matthew Peveler <matt.peveler@gmail.com>
2021-05-26 13:55:42 -0400

    [Bugfix:System] Add guard on constraint drop

--- migration/migrator/migrations/course/20210521231204_team_id_constraint.py
+++ migration/migrator/migrations/course/20210521231204_team_id_constraint.py
@@ -6,10 +6,5 @@ one grade per individual.
 
 
 def up(config, database, semester, course):
-    database.execute("ALTER TABLE public.gradeable_data DROP CONSTRAINT g_id_gd_team_id_unique;");
+    database.execute("ALTER TABLE public.gradeable_data DROP CONSTRAINT IF EXISTS g_id_gd_team_id_unique;");
     database.execute("ALTER TABLE ONLY public.gradeable_data ADD CONSTRAINT g_id_gd_team_id_unique UNIQUE (g_id, gd_team_id);");
-    pass
-
-
-def down(config, database, semester, course):
-    pass
