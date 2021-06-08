04aa74e6d67ec9c5254bcaa9ae2e552c3cff611a
wusatosi <26424577+wusatosi@users.noreply.github.com>
2021-06-08 13:43:57 +0800

    Fix path again

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -183,10 +183,10 @@ source ${CURRENT_DIR}/distro_setup/setup_distro.sh
 apt-get install libzbar0 --yes
 
 pip install --upgrade pip
-pip3 install -r ${CURRENT_DIR}/.setup/pip/system_requirements.txt
+pip3 install -r ${SUBMITTY_REPOSITORY}/.setup/pip/system_requirements.txt
 
 if [ ${VAGRANT} == 1 ]; then
-    pip3 install -r ${CURRENT_DIR}/.setup/pip/vagrant_requirements.txt
+    pip3 install -r ${SUBMITTY_REPOSITORY}/.setup/pip/vagrant_requirements.txt
 fi
 
 #################################################################
