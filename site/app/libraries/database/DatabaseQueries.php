a29dbf9dda25c6374ca41d11b78191f1c60a1336
Miles Ednie <edniem@rpi.edu>
2021-06-11 14:39:54 -0400

    condensed query

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -5703,19 +5703,13 @@ AND gc_id IN (
     public function getHasCustomMarks($gradeable_id) {
         //first get the gc_id's for all components associated with the gradeable
         $this->course_db->query(
-            'SELECT gc_id FROM gradeable_component WHERE g_id=?',
+            'SELECT gc.gc_id FROM gradeable_component AS gc
+                   INNER JOIN gradeable_component_data AS gcd ON gc.gc_id=gcd.gc_id
+                   WHERE gc.g_id=?',
             [$gradeable_id]
         );
-        $rows = $this->course_db->rows();
-        //then check each component for a custom mark
-        foreach ($rows as $row) {
-            $this->course_db->query(
-                'SELECT * FROM gradeable_component_data WHERE gc_id=?',
-                [$row['gc_id']]
-            );
-            if ($this->course_db->row()) {
-                return true;
-            }
+        if (count($this->course_db->rows()) > 0) {
+            return true;
         }
         return false;
     }
