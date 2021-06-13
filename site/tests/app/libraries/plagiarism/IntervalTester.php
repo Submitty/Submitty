6d9392944b6ca0cbd8bf0a90c3c8a0a3b305e4cc
williamjallen <dalearn@icloud.com>
2021-06-13 14:23:08 -0400

    Remove broken/obsolete tests
    A followup PR will be created which adds proper tests.  The existing merging algorithm was incorrect when applied to more than two users and the entirety of the testing code depended upon the invalid assumption that two regions can be merged, regardless of the state of the matching positions

--- site/tests/app/libraries/plagiarism/IntervalTester.php
+++ site/tests/app/libraries/plagiarism/IntervalTester.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace tests\app\libraries\plagiarism;
 
 use app\libraries\plagiarism\Interval;
-use app\libraries\plagiarism\Submission;
 
 class IntervalTester extends \PHPUnit\Framework\TestCase {
     public function testInterval() {
@@ -17,19 +16,9 @@ class IntervalTester extends \PHPUnit\Framework\TestCase {
         $interval->updateEnd(5);
         $this->assertSame(3, $interval->getStart());
         $this->assertSame(5, $interval->getEnd());
-        /** @var Submission[] */
-        $users = [];
-        $users[] = new Submission('a', 1, [['start' => 3, 'end' => 4]], 3, 5);
-        $interval->addUser($users[0]);
-        $this->assertSame($users, $interval->getUsers());
-        $users[] = new Submission('b', 1, [['start' => 2, 'end' => 3]], 5, 6);
-        $interval->addUser($users[1]);
-        $this->assertSame($users, $interval->getUsers());
-        $users[] = new Submission('a', 2, [['start' => 1, 'end' => 2]], 10, 11);
-        $interval->addUser($users[2]);
-        $this->assertSame($users, $interval->getUsers());
-        $users[0]->mergeMatchingPositions([['start' => 2, 'end' => 3]]);
-        $interval->addUser(new Submission('a', 1, [['start' => 2, 'end' => 3]], 20, 30));
-        $this->assertSame($users, $interval->getUsers());
+
+        $this->assertSame("match", $interval->getType());
+        $interval->updateType("specific-match");
+        $this->assertSame("specific-match", $interval->getType());
     }
 }
--- site/tests/app/libraries/plagiarism/PlagiarismUtilsTester.php
+++ site/tests/app/libraries/plagiarism/PlagiarismUtilsTester.php
@@ -8,11 +8,12 @@ use app\libraries\FileUtils;
 use app\libraries\plagiarism\Interval;
 use app\libraries\plagiarism\PlagiarismUtils;
 use app\libraries\plagiarism\Stack;
-use app\libraries\plagiarism\Submission;
 use app\libraries\Utils;
 
 class PlagiarismUtilsTester extends \PHPUnit\Framework\TestCase {
-    public function testConstructIntervals(): void {
+    public function testConstructIntervals(): void
+    {
+        // TODO: Write *correct* tests for the PlagiarismUtils file
         $testData = [
             [
                 "end" => 5,
@@ -85,146 +86,5 @@ class PlagiarismUtilsTester extends \PHPUnit\Framework\TestCase {
                 "type" => "match"
             ],
         ];
-
-        try {
-            $randomDir = FileUtils::joinPaths(sys_get_temp_dir(), Utils::generateRandomString());
-            FileUtils::createDir($randomDir);
-            $testFile = FileUtils::joinPaths($randomDir, 'test.json');
-            FileUtils::writeJsonFile($testFile, $testData);
-
-            $expected = [
-                new Interval(1, 5, "match"),
-                new Interval(2, 6, "match"),
-                new Interval(3, 7, "match"),
-                new Interval(23, 27, "match")
-            ];
-            $expected[0]->addUser(new Submission(
-                'persona',
-                2,
-                [["end" => 5, "start" => 1]],
-                1,
-                5
-            ));
-            $expected[1]->addUser(new Submission(
-                'persona',
-                2,
-                [['end' => 6, 'start' => 2]],
-                2,
-                6
-            ));
-            $expected[2]->addUser(new Submission(
-                'persona',
-                2,
-                [['end' => 7, 'start' => 3]],
-                3,
-                7
-            ));
-            $expected[2]->addUser(new Submission(
-                'personb',
-                2,
-                [['end' => 9, 'start' => 5]],
-                3,
-                7
-            ));
-            $expected[2]->addUser(new Submission(
-                'personc',
-                2,
-                [['end' => 7, 'start' => 3]],
-                3,
-                7
-            ));
-            $expected[3]->addUser(new Submission(
-                'personc',
-                2,
-                [['end' => 7, 'start' => 3]],
-                23,
-                27
-            ));
-            $this->assertEquals(
-                $expected,
-                PlagiarismUtils::constructIntervals($testFile)
-            );
-        }
-        finally {
-            FileUtils::recursiveRmdir($randomDir);
-        }
-    }
-
-    public function testMergeIntervals(): void {
-        $intervalArray = [
-            new Interval(1, 5, "match"),
-            new Interval(2, 6, "match"),
-            new Interval(3, 7, "match"),
-            new Interval(23, 27, "match")
-        ];
-        $intervalArray[0]->addUser(new Submission(
-            'persona',
-            2,
-            [["end" => 5, "start" => 1]],
-            1,
-            5
-        ));
-        $intervalArray[1]->addUser(new Submission(
-            'persona',
-            2,
-            [['end' => 6, 'start' => 2]],
-            2,
-            6
-        ));
-        $intervalArray[2]->addUser(new Submission(
-            'persona',
-            2,
-            [['end' => 7, 'start' => 3]],
-            3,
-            7
-        ));
-        $intervalArray[2]->addUser(new Submission(
-            'personb',
-            2,
-            [['end' => 9, 'start' => 5]],
-            3,
-            7
-        ));
-        $intervalArray[2]->addUser(new Submission(
-            'personc',
-            2,
-            [['end' => 7, 'start' => 3]],
-            3,
-            7
-        ));
-        $intervalArray[3]->addUser(new Submission(
-            'personc',
-            2,
-            [['end' => 7, 'start' => 3]],
-            23,
-            27
-        ));
-
-        $expected = [];
-        $expected[] = $intervalArray[3];
-        $interval = new Interval(1, 7, "match");
-        $interval->addUser(new Submission(
-            'persona',
-            2,
-            [['end' => 5, 'start' => 1], ['end' => 6, 'start' => 2], ['end' => 7, 'start' => 3]],
-            1,
-            5
-        ));
-        $interval->addUser(new Submission(
-            'personb',
-            2,
-            [['end' => 9, 'start' => 5]],
-            3,
-            7
-        ));
-        $interval->addUser(new Submission(
-            'personc',
-            2,
-            [['end' => 7, 'start' => 3]],
-            3,
-            7
-        ));
-        $expected[] = $interval;
-        $this->assertEquals($expected, PlagiarismUtils::mergeIntervals($intervalArray)->toArray());
     }
 }
