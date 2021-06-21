c72f2f5d86d1630bec636a2448d603b2fb16b164
sbelsk <shellybelsky02@gmail.com>
2021-06-21 09:10:00 -0400

    Cleanup migration

--- migration/migrator/migrations/course/20210607131242_poll_question_type.py
+++ migration/migrator/migrations/course/20210607131242_poll_question_type.py
@@ -30,4 +30,4 @@ def down(config, database, semester, course):
     :param course: Code of course being migrated
     :type course: str
     """
-    database.execute("ALTER TABLE polls DROP COLUMN IF EXISTS question_type;")
+    pass
