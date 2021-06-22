2f6cd109b8e140edf5870187557efd4c55f3f2a1
HenrikLam <henrikhl131@gmail.com>
2021-06-22 17:03:48 -0400

    some linting

--- site/app/views/admin/StudentActivityDashboardView.php
+++ site/app/views/admin/StudentActivityDashboardView.php
@@ -2,35 +2,23 @@
 
 namespace app\views\admin;
 
-
 use app\views\AbstractView;
 
-class StudentActivityDashboardView extends AbstractView{
+class StudentActivityDashboardView extends AbstractView {
 
-        public function createTable($data_dump){
+        public function createTable($data_dump) {
             $this->core->getOutput()->addInternalCss('table.css');
             $this->core->getOutput()->addInternalCss('activity-dashboard.css');
             $this->core->getOutput()->addInternalJs('activity-dashboard.js');
             $this->core->getOutput()->addInternalCss('flatpickr.min.css');
-
-            
-        //var_dump($data_dump);
-
-
-
-        return $this->core->getOutput()->renderTwigTemplate("admin/users/StudentActivityDashboard.twig", [
-            "data" => $data_dump,
-            "download_link" => $this->core->buildCourseUrl(['activity', 'download'])
-        ]);
-
+            //var_dump($data_dump);
+            return $this->core->getOutput()->renderTwigTemplate("admin/users/StudentActivityDashboard.twig", [
+                "data" => $data_dump,
+                "download_link" => $this->core->buildCourseUrl(['activity', 'download'])
+            ]);
         }
 
-        public function downloadFile($file_url){
+        public function downloadFile($file_url) {
             readfile($file_url);
         }
-
-
-
-
-
 }
