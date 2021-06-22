345720e305fcad2f6eb5601b33ab386d7217e461
HenrikLam <henrikhl131@gmail.com>
2021-06-22 16:44:53 -0400

    some linting

--- site/public/js/activity-dashboard.js
+++ site/public/js/activity-dashboard.js
@@ -16,8 +16,8 @@ function downloadData() {
 
 /* exported sortTable */
 function sortTable(n, flag) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("data-table");
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById('data-table');
     for (i = 0; i < 10; i++){
         document.getElementById(i).style.color = 'white';
     }
@@ -25,7 +25,9 @@ function sortTable(n, flag) {
     switching = true;
     // Set the sorting direction to ascending:
     dir = 'asc';
-    if (flag){dir = 'desc'};
+    if (flag){
+        dir = 'desc';
+    }
     /* Make a loop that will continue until
     no switching has been done: */
     while (switching) {
@@ -39,38 +41,39 @@ function sortTable(n, flag) {
             shouldSwitch = false;
             /* Get the two elements you want to compare,
             one from current row and one from the next: */
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
+            x = rows[i].getElementsByTagName('TD')[n];
+            y = rows[i + 1].getElementsByTagName('TD')[n];
             /* Check if the two rows should switch place,
             based on the direction, asc or desc: */
-            if (dir == "asc") {
+            if (dir == 'asc') {
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
                 }
-            } else if (dir == "desc") {
+            }
+            else if (dir == 'desc') {
                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
                 }
             }
-      }
-      if (shouldSwitch) {
-          /* If a switch has been marked, make the switch
-          and mark that a switch has been done: */
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
-          // Each time a switch is done, increase this count by 1:
-          switchcount ++;
-      } else {
-          /* If no switching has been done AND the direction is "asc",
-          set the direction to "desc" and run the while loop again. */
-          if (switchcount == 0 && dir == "asc") {
-              dir = "desc";
-              switching = true;
-          }
-      }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount ++;
+        } else {
+            /* If no switching has been done AND the direction is 'asc',
+            set the direction to 'desc' and run the while loop again. */
+            if (switchcount == 0 && dir == 'asc') {
+                dir = 'desc';
+                switching = true;
+            }
+        }
     }
 }
