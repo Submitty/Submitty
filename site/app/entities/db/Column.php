ce15cac00370b1de9efd52a86df00aed3004697f
Matthew Peveler <matt.peveler@gmail.com>
2021-06-21 22:29:07 -0400

    fix indentation

--- site/app/entities/db/Column.php
+++ site/app/entities/db/Column.php
@@ -13,49 +13,49 @@ class Column {
    * @ORM\Id
    * @ORM\Column(name="table_catalog",type="string")
    */
-  protected $database;
+    protected $database;
   /**
    * @ORM\Id
    * @ORM\Column(name="table_schema",type="string")
    */
-  protected $schema;
+    protected $schema;
   /**
    * @ORM\Id
    * @ORM\Column(name="table_name",type="string")
    */
-  protected $table_name;
+    protected $table_name;
 
   /**
    * @ORM\Id
    * @ORM\Column(name="column_name",type="string")
    */
-  protected $name;
+    protected $name;
 
   /**
    * @ORM\Column(name="ordinal_position",type="integer")
    */
-  protected $position;
+    protected $position;
 
   /**
    * @ORM\Column(name="data_type",type="string")
    */
-  protected $type;
+    protected $type;
 
   /**
    * @ORM\ManyToOne(targetEntity="\app\entities\db\Table",inversedBy="columns")
    * @ORM\JoinColumns({
-   *    @ORM\JoinColumn(name="table_catalog", referencedColumnName="table_catalog"),
-   *    @ORM\JoinColumn(name="table_schema", referencedColumnName="table_schema"),
-   *    @ORM\JoinColumn(name="table_name", referencedColumnName="table_name")
+   * @ORM\JoinColumn(name="table_catalog", referencedColumnName="table_catalog"),
+   * @ORM\JoinColumn(name="table_schema", referencedColumnName="table_schema"),
+   * @ORM\JoinColumn(name="table_name", referencedColumnName="table_name")
    * })
    */
-  protected $table;
+    protected $table;
 
-  public function getName() {
-    return $this->name;
-  }
+    public function getName() {
+        return $this->name;
+    }
 
-  public function getType() {
-    return $this->type;
-  }
+    public function getType() {
+        return $this->type;
+    }
 }
--- site/app/entities/db/Table.php
+++ site/app/entities/db/Table.php
@@ -14,41 +14,41 @@ class Table {
    * @ORM\Id
    * @ORM\Column(name="table_catalog",type="string")
    */
-  protected $database;
+    protected $database;
   /**
    * @ORM\Id
    * @ORM\Column(name="table_schema",type="string")
    */
-  protected $schema;
+    protected $schema;
   /**
    * @ORM\Id
    * @ORM\Column(name="table_name",type="string")
    */
-  protected $name;
+    protected $name;
 
   /**
    * @ORM\OneToMany(targetEntity="\app\entities\db\Column",mappedBy="table")
    * @ORM\JoinColumns({
-   *    @ORM\JoinColumn(name="table_catalog", referencedColumnName="table_catalog"),
-   *    @ORM\JoinColumn(name="table_schema", referencedColumnName="table_schema"),
-   *    @ORM\JoinColumn(name="table_name", referencedColumnName="table_name")
+   * @ORM\JoinColumn(name="table_catalog", referencedColumnName="table_catalog"),
+   * @ORM\JoinColumn(name="table_schema", referencedColumnName="table_schema"),
+   * @ORM\JoinColumn(name="table_name", referencedColumnName="table_name")
    * })
    * @ORM\OrderBy({"position" = "ASC"})
    */
-  protected $columns;
+    protected $columns;
 
-  public function __construct() {
-    $this->columns = new ArrayCollection();
-  }
+    public function __construct() {
+        $this->columns = new ArrayCollection();
+    }
 
-  public function getName() {
-    return $this->name;
-  }
+    public function getName() {
+        return $this->name;
+    }
 
   /**
    * @return ArrayCollection<Column>
    */
-  public function getColumns() {
-    return $this->columns;
-  }
+    public function getColumns() {
+        return $this->columns;
+    }
 }
