5c265ed30d0f1b093645c014b36f72759319579b
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-06-28 15:57:18 -0400

    multiple versions stacked, removed some highlighting

--- site/public/js/system-update.js
+++ site/public/js/system-update.js
@@ -36,18 +36,38 @@ async function getReleases(current_tag) {
 * @param {String} current_tag - tag Submitty is currently running on
 */
 function updateReleaseNotes(data, current_tag){
+    current_tag = "v20.10.00";
+    let updates = '';
+    let i = 0;
+    while (i < data.length && data[i].tag_name !== current_tag) {
+        updates += (`# ${data[i].tag_name}\n${data[i].body}-END-\n`);
+        i++;
+    }
+
+    console.log('updates', updates);
+
+    $('.release-notes').remove();
+
     const latest = data[0];
 
     $.ajax({
         url: buildUrl(['markdown']),
         type: 'POST',
         data: {
-            content: addPRLinks(latest["body"]),
+            content: addPRLinks(updates),
             csrf_token: csrfToken
         },
         success: function(markdown_data){
-            $('#release-notes').css('display', 'block');
-            $('#release-notes').html(injectStyling(markdown_data));
+            const release_notes = markdown_data.split('-END-');
+            console.log('release_notes', release_notes);
+
+            for(let release of release_notes) {
+                //have to wrap the output in <div class="markdown"></div> since we are using 1 ajax call to process multiple payloads
+                //that way they all come back at the same time
+                $('.content').append(`<div class="box"><div class="markdown">${injectStyling(release)}</div></div>`);
+            }
+
+            //$('#release-notes').html(injectStyling(markdown_data));
             $('div.update').each( (index, div) => {
                 if ($(div).text().includes('None')) {
                     $(div).addClass('no-content');
@@ -68,7 +88,7 @@ function updateReleaseNotes(data, current_tag){
 }
 
 function addPRLinks(raw_str) {
-    return raw_str.replace(/#(\d+)/g, '[#$1](https://github.com/Submitty/Submitty/pulls/$1)');
+    return raw_str.replace(/#(\d+)/g, '[#$1](https://github.com/Submitty/Submitty/pull/$1)');
 }
 
 function injectStyling(markdown_data) {
@@ -76,15 +96,56 @@ function injectStyling(markdown_data) {
     markdown_data = markdown_data.replace(/<\/ul>|<\/em><\/p>/g, '$&<hr>');
     //replace normal li contents with spans with classes
     markdown_data = markdown_data.replace(/\[\w+:(\w+)\].+(?=<\/li>)|\[([^\]]+)\].+(?=<\/li>)/g, '<span class="update update-$1$2">$&</span>');
+    //add class to version headers
+    markdown_data = markdown_data.replace(/<h1/g, '$& class="version-header"');
+    //add content wrapper
+    //markdown_data = markdown_data.replace(/<\/h1>([\s\S]+?)(?=<hr>)/g)
+    
     // //special highlighting for SYSADMIN updates only if there are any
     // if ((markdown_data.match(/\[SYSADMIN ACTION\]/g) || []).length) {
     //     markdown_data = markdown_data.replace(/<p>SYSADMIN[\s\S]+?(?:<hr>)/g, '<div class="update-SYSADMIN">$&</div>');
+    // }
+
+    // if ((markdown_data.match(/\[SECURITY\]/g) || []).length) {
+    //     markdown_data = markdown_data.replace(/<p>SECURITY[\s\S]+?(?:<hr>)/g, '<div class="update-SECURITY">$&</div>');
     // }
     // //special highlighting for SECURITY updates only if there are any
     // if ((markdown_data.match(/\[SECURITY\]/g) || []).length) {
     markdown_data = markdown_data.replace(/<p>([^<\n]+)[\s\S]+?(?:<hr>)/g, '<div class="update update-$1">$&</div>');
     //}
     return markdown_data;
+}
+
+function mergeReleaseNotes(accumulated, current) {
+    let split = current.body.split('\r\n\r\n').slice(1);
+    let last_section = split[0];
+    for (let i = 0; i < split.length; i++) {
+        if(!accumulated.hasOwnProperty(last_section)) {
+            accumulated[last_section] = [];
+        }
+        while(isReleaseItem(split[i])) {
+            if(!split[i].includes("None")){
+                accumulated[last_section].push(split[i]);
+            }
+            i++;
+        }
+        last_section = split[i];
+    }
+    return accumulated;
+}
+
+function isReleaseItem(string) {
+    return string && string[0] === '*';
+}
+
+function updatesToNotes(merged_updates) {
+    let release_notes = '';
+    release_notes += `${merged_updates["SYSADMIN ACTION / BREAKING CHANGE"]}\n\n${merged_updates["SYSADMIN ACTION / BREAKING CHANGE"].join('\n')}\n\n`;
+    release_notes += `${merged_updates["SECURITY"]}\n\n${merged_updates["SECURITY"].join('\n')}\n\n`;
+    for(let section in merged_updates) {
+        release_notes += `${section}\n\n${merged_updates[section].join('\n')}\n\n`;
+    }
+    return release_notes;
 }
 
 function filterReleaseNotes(filter) {
