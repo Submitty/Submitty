cd8f396a02b536fde8a7d03660e457f85e3d6746
Shail Patel <shailpatel67@gmail.com>
2021-06-30 01:06:51 -0400

    add more tests

--- site/cypress/integration/courseMaterials.spec.js
+++ site/cypress/integration/courseMaterials.spec.js
@@ -11,38 +11,127 @@ describe('Test cases revolving around course material uploading and access contr
 		cy.visit(['sample', 'course_materials']);
 	});
 
-	describe('Test upload and viewing files' , () => {
-		it('Should upload a file and be able to view and download it', () => {
-			cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
-			cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
-			cy.get('#submit-materials').click();
+	afterEach(() => {
+		cy.reload();
+		cy.logout();
+		cy.login();
+		cy.visit(['sample', 'course_materials']);
+	})
 
-			cy.get('.file-viewer').contains('file1.txt');
-			
-			const fileTgt = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/file1.txt`;
+	it('Should upload a file and be able to view and download it', () => {
+		cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+		cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
+		cy.get('#submit-materials').click();
 
-			cy.visit(fileTgt);
-			cy.get('pre').should('have.text', 'a\n');
-			cy.visit(['sample', 'course_materials']);
+		cy.get('.file-viewer').contains('file1.txt');
+		
+		const fileTgt = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/file1.txt`;
 
-			//a href tags should be for navigation only, workaround to prevent cypress from expecting a page change
-			//https://github.com/cypress-io/cypress/issues/14857
-			//TODO: handle download
+		cy.visit(fileTgt);
+		cy.get('pre').should('have.text', 'a\n');
+		cy.visit(['sample', 'course_materials']);
 
-			cy.get('.fa-trash').click();
-			cy.get('.btn-danger').click();
-			cy.get('.file-viewer').should('not.exist');
-		});
+		//a href tags should be for navigation only, workaround to prevent cypress from expecting a page change
+		//https://github.com/cypress-io/cypress/issues/14857
+		//TODO: handle download
+
+		cy.get('.fa-trash').click();
+		cy.get('.btn-danger').click();
+		cy.get('.file-viewer').should('not.exist');
 	});
 
+	it('Support optional file locations', () => {
+		cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+		cy.get('#input-provide-full-path').type('option1');
+		cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
+		cy.get('#submit-materials').click();
 
-	describe('Test adding course material links', () => {
-		it.only('Should allow uploading links', () => {
-			cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
-			cy.get('#url_selection_radio').click();
-			cy.get('#url_title').type('Test URL');
-			cy.get('#url_url').type(buildUrl(['sample', 'users'], true));
-			cy.get('#submit-materials').click();
-		})
+		cy.get('.file-viewer').contains('file1.txt');
+		const fileTgt = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/option1/file1.txt`;
+
+		cy.visit(fileTgt);
+		cy.get('pre').should('have.text', 'a\n');
+		cy.visit(['sample', 'course_materials']);
+
+		cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+		cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
+
+		const fpath = 'option1/1234/!@#$%^&*()/';
+		cy.get('#input-provide-full-path').type(fpath);
+		cy.get('#submit-materials').click();
+		cy.get('.file-viewer').should('have.length', 2);
+
+		const fileTgt2 = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/${encodeURIComponent(fpath)}/file1.txt`;
+		cy.visit(fileTgt2);
+		cy.get('pre').should('have.text', 'a\n');
+		cy.visit(['sample', 'course_materials']);
+
+		cy.get('.div-viewer .fa-trash').first().click();
+		cy.get('.btn-danger').click();
+	});
+
+	it('Should allow uploading links', () => {
+		cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+		cy.get('#url_selection_radio').click();
+		cy.get('#url_title').type('Test URL');
+		cy.get('#url_url').type(buildUrl(['sample', 'users'], true));
+		cy.get('#submit-materials').click();
+
+		cy.get('.file-viewer > [href="http://localhost:1511/courses/s21/sample/users"]').click();
+		cy.location().should((loc) => {
+			expect(loc.href).to.eq(buildUrl(['sample', 'users'],true));
+		});
+
+		cy.visit(['sample', 'course_materials']);
+		cy.get('.key_to_click > .fa-trash').click();
+		cy.get('.btn-danger').click();
+		cy.get('.file-viewer').should('not.exist');
+	});
+
+	it('Should release course materials by date', () => {
+		const date = '2021-06-29 21:37:53';
+		cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+		cy.get('#upload_picker').clear().type(date);
+		cy.get('#upload1').attachFile(['file1.txt', 'file2.txt'] , { subjectType: 'drag-n-drop' });
+		cy.get('#submit-materials').click();
+
+		cy.get('#date_to_release_sd1f1').should('have.value', date);
+		cy.get('#date_to_release_sd1f1').should('have.value', date);
+
+		cy.reload() //dom elements become detatched after uploading?
+
+		cy.get('.fa-pencil-alt').first().click();
+		cy.get('#edit-picker').clear().type('9998-01-01 00:00:00');
+		cy.get('#submit-edit').click({force: true}); //div covering button
+		cy.get('#date_to_release_sd1f1').should('have.value', '9998-01-01 00:00:00');
+
+		cy.reload();
+
+		cy.logout();
+		cy.login('aphacker');
+		cy.visit(['sample', 'course_materials']);
+		cy.get('.file-viewer').should('have.length', 1);
+
+		const fileTgt = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/file2.txt`;
+
+		cy.visit(fileTgt);
+		cy.get('pre').should('have.text', 'b\n');
+		cy.visit(['sample', 'course_materials']);
+
+		const fileTgt2 = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/file1.txt`;
+		cy.visit(fileTgt2);
+		cy.get('.content').contains(`Reason: You may not access this file until it is released`);
+
+		cy.logout();
+		cy.login();
+
+		cy.visit(['sample', 'course_materials']);
+		cy.reload();
+
+		cy.get('.fa-trash').first().click();
+		cy.get('.btn-danger').click();
+
+		cy.get('.fa-trash').click();
+		cy.get('.btn-danger').click();
 	});
 });
