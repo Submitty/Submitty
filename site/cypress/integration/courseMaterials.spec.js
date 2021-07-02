c95329bb40df94c4d83e3239fa492b477f116e8a
Shail Patel <shailpatel67@gmail.com>
2021-07-01 22:23:17 -0400

    add more tests

--- site/cypress/integration/courseMaterials.spec.js
+++ site/cypress/integration/courseMaterials.spec.js
@@ -5,16 +5,17 @@ const defaultFilePath = `/var/local/submitty/courses/${coursePath}`;
 
 describe('Test cases revolving around course material uploading and access control', () => {
     before(() => {
+        console.log(buildUrl(['sample', 'course_materials']));
         cy.visit('/');
         cy.login();
         cy.visit(['sample', 'course_materials']);
     });
 
     afterEach(() => {
-    	cy.reload();
-    	cy.logout();
-    	cy.login();
-    	cy.visit(['sample', 'course_materials']);
+        cy.reload();
+        cy.logout();
+        cy.login();
+        cy.visit(['sample', 'course_materials']);
     });
 
     it('Should upload a file and be able to view and download it', () => {
@@ -39,7 +40,7 @@ describe('Test cases revolving around course material uploading and access contr
         cy.get('.file-viewer').should('not.exist');
     });
 
-    it('Support optional file locations', () => {
+    it('Should support optional file locations', () => {
         cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
         cy.get('#input-provide-full-path').type('option1');
         cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
@@ -76,7 +77,7 @@ describe('Test cases revolving around course material uploading and access contr
         cy.get('#url_url').type(buildUrl(['sample', 'users'], true));
         cy.get('#submit-materials').click();
 
-        cy.get('.file-viewer > [href="http://localhost:1511/courses/s21/sample/users"]').click();
+        cy.get(`.file-viewer > [href="${buildUrl(['sample', 'users'],true)}"]`).click();
         cy.location().should((loc) => {
             expect(loc.href).to.eq(buildUrl(['sample', 'users'],true));
         });
@@ -177,7 +178,7 @@ describe('Test cases revolving around course material uploading and access contr
         cy.get('.file-viewer').should('not.exist');
     });
 
-    it('should upload and unzip zip files', () => {
+    it('Should upload and unzip zip files', () => {
         cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
         cy.get('#expand-zip-checkbox').check();
         cy.get('#upload1').attachFile('zip.zip' , { subjectType: 'drag-n-drop' });
@@ -200,5 +201,153 @@ describe('Test cases revolving around course material uploading and access contr
         for (let i = 17; i < 22; i++){
             cy.get('[name="release_date"]').eq(i).should('have.value', '2021-06-29 21:37:53');
         }
+
+        cy.logout();
+        cy.login('aphacker');
+        cy.visit(['sample','course_materials']);
+
+        cy.get('.file-viewer').should('have.length', 6);
+
+        const fileTgt = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/zip/2/3/8/4/3/1/1_1.txt`;
+        cy.visit(fileTgt);
+        cy.get('body').should('have.text','');
+
+        const fileTgt2 = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/zip/1_1.txt`;
+        cy.visit(fileTgt2);
+        cy.get('.content').contains('Reason: You may not access this file until it is released');
+
+        cy.logout();
+        cy.login();
+
+        cy.visit(['sample', 'course_materials']);
+        cy.get('.fa-trash').first().click();
+        cy.get('.btn-danger').click();
+        cy.get('.file-viewer').should('not.exist');
+    });
+
+    it.only('Should restrict course materials by section', () => {
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#all_Sections_Showing_yes').click();
+        cy.get('#upload1').attachFile(['file1.txt', 'file2.txt'] , { subjectType: 'drag-n-drop' });
+        cy.get('#section-1').check();
+        cy.get('#upload_picker').clear().type('2021-06-29 21:37:53');
+        cy.get('#submit-materials').click();
+
+        cy.reload();
+        cy.get('.fa-pencil-alt').last().click();
+        cy.get('#section-edit-2').check();
+        cy.get('#submit-edit').click();
+
+        cy.reload();
+        cy.logout();
+        cy.login('aphacker');
+        cy.visit(['sample', 'course_materials']);
+
+        cy.get('.file-viewer').should('have.length', 2);
+
+        cy.logout();
+        cy.login('browna');
+        cy.visit(['sample', 'course_materials']);
+
+        cy.get('.file-viewer').should('have.length', 1);
+
+        const fileTgt2 = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/file1.txt`;
+        cy.visit(fileTgt2);
+
+        cy.reload();
+        cy.get('.content').contains('Reason: Your section may not access this file');
+
+        cy.logout();
+        cy.login();
+
+        cy.visit(['sample', 'course_materials']);
+        cy.get('.fa-trash').first().click();
+        cy.get('.btn-danger').click();
+
+        cy.get('.fa-trash').click();
+        cy.get('.btn-danger').click();
+        cy.get('.file-viewer').should('not.exist');
+
+    });
+
+    it.only('Should restrict course materials within folders', () => {
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#all_Sections_Showing_yes').click();
+        cy.get('#upload1').attachFile('zip.zip' , { subjectType: 'drag-n-drop' });
+        cy.get('#section-1').check();
+        cy.get('#upload_picker').clear().type('2021-06-29 21:37:53');
+        cy.get('#expand-zip-checkbox').check();
+        cy.get('#submit-materials').click();
+
+        cy.reload();
+        cy.get('[onclick=\'setCookie("foldersOpen",openAllDivForCourseMaterials());\']').click();
+        cy.get('.fa-pencil-alt').eq(9).click();
+        cy.get('#all-sections-showing-yes').click();
+        cy.get('#section-edit-2').check();
+        cy.get('#submit-edit').click();
+
+        cy.reload();
+        cy.logout();
+        cy.login('browna');
+        cy.visit(['sample', 'course_materials']);
+
+        cy.get('.file-viewer').should('have.length', 1);
+        const fileTgt2 = `${buildUrl(['sample', 'display_file'])}?dir=course_materials&path=${encodeURIComponent(defaultFilePath)}/zip/1_1.txt`;
+        cy.visit(fileTgt2);
+
+        cy.reload();
+        cy.get('.content').contains('Reason: Your section may not access this file');
+        cy.logout();
+
+        cy.login();
+        cy.visit(['sample', 'course_materials']);
+        cy.get('.fa-trash').first().click();
+        cy.get('.btn-danger').click();
+    });
+
+    it('Should sort course materials', () => {
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#input-provide-full-path').type('a');
+        cy.get('#upload1').attachFile('file1.txt' , { subjectType: 'drag-n-drop' });
+        cy.get('#upload_sort').clear().type('50000');
+        cy.get('#submit-materials').click();
+        cy.reload();
+
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#input-provide-full-path').type('a');
+        cy.get('#upload1').attachFile('file2.txt' , { subjectType: 'drag-n-drop' });
+        cy.get('#upload_sort').clear().type('10');
+        cy.get('#submit-materials').click();
+        cy.reload();
+
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#input-provide-full-path').type('a');
+        cy.get('#upload1').attachFile('file3.txt' , { subjectType: 'drag-n-drop' });
+        cy.get('#upload_sort').clear().type('5.5');
+        cy.get('#submit-materials').click();
+        cy.reload();
+
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#input-provide-full-path').type('a');
+        cy.get('#upload1').attachFile('file4.txt' , { subjectType: 'drag-n-drop' });
+        cy.get('#upload_sort').clear().type('5.4');
+        cy.get('#submit-materials').click();
+        cy.reload();
+
+        cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+        cy.get('#input-provide-full-path').type('a');
+        cy.get('#upload1').attachFile('file5.txt' , { subjectType: 'drag-n-drop' });
+        cy.get('#upload_sort').clear().type('0');
+        cy.get('#submit-materials').click();
+        cy.reload();
+        cy.get('[onclick=\'setCookie("foldersOpen",openAllDivForCourseMaterials());\']').click();
+
+
+        for (let i = 5; i > 0; i--){
+            cy.get(`:nth-child(${6-i}) > .file-viewer`).contains(`file${i}.txt` );
+        }
+
+        cy.get('.fa-trash').first().click();
+        cy.get('.btn-danger').click();
     });
 });
