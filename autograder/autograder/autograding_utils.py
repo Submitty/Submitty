631ed659525829695d19c851b4c08e566f319280
Barb Cutler <bmcutler@gmail.com>
2021-07-06 14:40:50 -0400

    typo

--- autograder/autograder/autograding_utils.py
+++ autograder/autograder/autograding_utils.py
@@ -732,9 +732,8 @@ def zip_my_directory(path, zipfilename):
     for root, _dirs, files in os.walk(path):
         for my_file in files:
             relpath = root[len(path)+1:]
-            zipf.write(os.path.join(root, my_file), os.path.join(relpath, my_file))
             try:
-                zipf.write(os.path.join(root,my_file), os.path.join(relpath, my_file))
+                zipf.write(os.path.join(root, my_file), os.path.join(relpath, my_file))
             except FileNotFoundError as e:
                 # this can happen if we have a broken symlink
                 pass
