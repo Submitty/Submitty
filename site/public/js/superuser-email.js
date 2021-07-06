6dc93d1afff1354f6dc2090c57705ef0370eac34
HenrikLam <henrikhl131@gmail.com>
2021-07-06 16:50:08 -0400

    Revert "commit before reverting"
    This reverts commit 5caf424a3904006ffbd51a0d075225e42a06060c.

--- site/public/js/superuser-email.js
+++ site/public/js/superuser-email.js
@@ -1,16 +1,31 @@
 /* exported sendEmail */
 /* eslint no-undef: "off" */
 
+
 function sendEmail(url) {
-    const options = $("[name='email-options']");
-    console.log(options);
+    const emailContent = $('#email-content').val();
+    const emailSubject = $('#email-subject').val();
+    // Check checkboxes for options
+    const emailInstructor = $('#email-instructor').is(':checked');
+    const emailFullAcess = $('#email-full-access').is(':checked');
+    const emailLimitedAccess = $('#email-limited-access').is(':checked');
+    const emailStudent = $('#email-student').is(':checked');
+    const emailToSecondary = $('#email-to-secondary').is(':checked');
+    const emailFaculty = $('#email-faculty').is(':checked');
     $('#email-content').prop('disabled', true);
     $('#send-email').prop('disabled', true);
     $.ajax({
         url: url,
         type: 'POST',
         data: {
-            'optons': options,
+            'emailContent': emailContent,
+            'emailSubject': emailSubject,
+            'emailFullAccess': emailFullAcess,
+            'emailLimitedAccess': emailLimitedAccess,
+            'emailInstructor': emailInstructor,
+            'emailStudent': emailStudent,
+            'emailToSecondary': emailToSecondary,
+            'emailFaculty': emailFaculty,
             csrf_token: csrfToken,
         },
         cache: false,
