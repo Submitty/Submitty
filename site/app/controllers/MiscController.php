fc17a81168719161270ca828ceb39d80d7ab5474
Chris Reed <cjreed121@users.noreply.github.com>
2021-07-12 10:55:59 -0400

    Some suggested changes

--- site/app/controllers/MiscController.php
+++ site/app/controllers/MiscController.php
@@ -238,7 +238,7 @@ class MiscController extends AbstractController {
     /**
      * @Route("/courses/{_semester}/{_course}/gradeable/{gradeable_id}/downloadTestCaseResult")
      */
-    public function downloadTestCaseResult($gradeable_id, $version, $test_case, $file_name, $user_id) {
+    public function downloadTestCaseResult(string $gradeable_id, int $version, int $test_case, string $file_name, string $user_id) {
         $gradeable = $this->tryGetGradeable($gradeable_id);
         $graded_gradeable = $this->tryGetGradedGradeable($gradeable, $user_id, false);
         if ($user_id !== $this->core->getUser()->getId()) {
@@ -251,14 +251,24 @@ class MiscController extends AbstractController {
 
         $name = "details/test" . sprintf("%02d", $test_case) . "/$file_name";
         $files = $autograde->getResultsFiles();
-        $file_path = $files[$name]['path'];
-        if (file_exists($file_path)) {
-            $this->core->getOutput()->useHeader(false);
-            $this->core->getOutput()->useFooter(false);
-            header('Content-Type: application/octet-stream');
-            header("Content-Transfer-Encoding: Binary");
-            header("Content-disposition: attachment; filename=\"{$file_name}\"");
-            readfile($file_path);
+        if (isset($files[$name])) {
+            $file_path = $files[$name]['path'];
+            if (file_exists($file_path)) {
+                $this->core->getOutput()->useHeader(false);
+                $this->core->getOutput()->useFooter(false);
+                header('Content-Type: application/octet-stream');
+                header("Content-Transfer-Encoding: Binary");
+                header("Content-disposition: attachment; filename=\"{$file_name}\"");
+                readfile($file_path);
+            }
+            else {
+                $this->core->addErrorMessage("That file does not seem to exist");
+                return new RedirectResponse($this->core->buildCourseUrl(['gradeable', $gradeable_id]));
+            }
+        }
+        else {
+            $this->core->addErrorMessage("That file does not seem to exist");
+            return new RedirectResponse($this->core->buildCourseUrl(['gradeable', $gradeable_id]));
         }
     }
 
