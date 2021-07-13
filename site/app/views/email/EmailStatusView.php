8c12febc2721785afd1919da1000f8bc38a761d5
HenrikLam <henrikhl131@gmail.com>
2021-07-13 17:37:25 -0400

    refactored the view file

--- site/app/views/email/EmailStatusView.php
+++ site/app/views/email/EmailStatusView.php
@@ -10,25 +10,21 @@ use app\models\EmailStatusModel;
 class EmailStatusView extends AbstractView {
     public function showEmailStatus($emailStatus) {
         $this->core->getOutput()->addBreadcrumb("Email Statuses", $this->core->buildCourseUrl(["email"]));
-        $this->core->getOutput()->addInternalCss('email-status.css');
-        return $this->core->getOutput()->renderTwigTemplate("EmailStatusPage.twig", [
-            "subjects" => $emailStatus->getSubjects(),
-            "pending" => $emailStatus->getPending(),
-            "successes" => $emailStatus->getSuccesses(),
-            "errors" => $emailStatus->getErrors(),
-            "courses" => $emailStatus->getCourses()
-        ]);
+        return $this->renderStatusPage($email_status);
     }
-
     public function showSuperuserEmailStatus($emailStatuses) {
         $this->core->getOutput()->addBreadcrumb("Superuser Email Statuses", $this->core->buildUrl(["superuser","email"]));
+        return $this->renderStatusPage($email_status);
+    }
+    private function renderStatusPage($email_status) {
+        $this->core->getOutput()->addVendorJs('bootstrap/js/bootstrap.bundle.min.js');
         $this->core->getOutput()->addInternalCss('email-status.css');
-        return $this->core->getOutput()->renderTwigTemplate("superuser/SuperuserEmailStatus.twig", [
-            "subjects" => $emailStatuses->getSubjects(),
-            "pending" => $emailStatuses->getPending(),
-            "successes" => $emailStatuses->getSuccesses(),
-            "errors" => $emailStatuses->getErrors(),
-            "courses" => $emailStatuses->getCourses()
+        return $this->core->getOutput()->renderTwigTemplate("EmailStatusPage.twig", [
+            "subjects" => $email_status->getSubjects(),
+            "pending" => $email_status->getPending(),
+            "successes" => $email_status->getSuccesses(),
+            "errors" => $email_status->getErrors(),
+            "courses" => $email_status->getCourses()
         ]);
     }
 }
