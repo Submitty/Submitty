5a55c7784adff27efe43c97af995dfb461357f62
HenrikLam <henrikhl131@gmail.com>
2021-07-13 16:19:21 -0400

    removed unnecessary js

--- site/app/views/email/EmailStatusView.php
+++ site/app/views/email/EmailStatusView.php
@@ -10,7 +10,6 @@ use app\models\EmailStatusModel;
 class EmailStatusView extends AbstractView {
     public function showEmailStatus($emailStatus) {
         $this->core->getOutput()->addBreadcrumb("Email Statuses", $this->core->buildCourseUrl(["email"]));
-        $this->core->getOutput()->addVendorJs('bootstrap/js/bootstrap.bundle.min.js');
         $this->core->getOutput()->addInternalCss('email-status.css');
         return $this->core->getOutput()->renderTwigTemplate("EmailStatusPage.twig", [
             "subjects" => $emailStatus->getSubjects(),
@@ -23,7 +22,6 @@ class EmailStatusView extends AbstractView {
 
     public function showSuperuserEmailStatus($emailStatuses) {
         $this->core->getOutput()->addBreadcrumb("Superuser Email Statuses", $this->core->buildUrl(["superuser","email"]));
-        $this->core->getOutput()->addVendorJs('bootstrap/js/bootstrap.bundle.min.js');
         $this->core->getOutput()->addInternalCss('email-status.css');
         return $this->core->getOutput()->renderTwigTemplate("superuser/SuperuserEmailStatus.twig", [
             "subjects" => $emailStatuses->getSubjects(),
