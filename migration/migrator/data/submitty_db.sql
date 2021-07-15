942c12ee2ab433437477e9fc1e1ff9f214e56039
Chris Reed <cjreed121@users.noreply.github.com>
2021-07-15 13:35:56 -0400

    Add owner and more constraints

--- migration/migrator/data/submitty_db.sql
+++ migration/migrator/data/submitty_db.sql
@@ -295,7 +295,10 @@ CREATE TABLE public.courses (
     semester character varying(255) NOT NULL,
     course character varying(255) NOT NULL,
     status smallint DEFAULT 1 NOT NULL,
-    group_name character varying(255) NOT NULL
+    group_name character varying(255) NOT NULL,
+    owner_name character varying(255) NOT NULL,
+    CONSTRAINT group_validate CHECK (((group_name)::text ~ '^[a-zA-Z0-9_-]*$'::text)),
+    CONSTRAINT owner_validate CHECK (((owner_name)::text ~ '^[a-zA-Z0-9_-]*$'::text))
 );
 
 
--- migration/migrator/migrations/master/20210714135438_add_course_groups.py
+++ migration/migrator/migrations/master/20210714135438_add_course_groups.py
@@ -13,18 +13,25 @@ def up(config, database):
     :type database: migrator.db.Database
     """
     database.execute("ALTER TABLE courses ADD COLUMN IF NOT EXISTS group_name varchar(255);")
+    database.execute("ALTER TABLE courses ADD COLUMN IF NOT EXISTS owner_name varchar(255);")
+    database.execute("ALTER TABLE courses DROP CONSTRAINT IF EXISTS group_validate;")
+    database.execute("ALTER TABLE courses DROP CONSTRAINT IF EXISTS owner_validate;")
+    database.execute("ALTER TABLE courses ADD CONSTRAINT group_validate CHECK (group_name ~ '^[a-zA-Z0-9_-]*$')")
+    database.execute("ALTER TABLE courses ADD CONSTRAINT owner_validate CHECK (owner_name ~ '^[a-zA-Z0-9_-]*$')")
     semester_dir = Path(config.submitty['submitty_data_dir'], 'courses')
     for semester in semester_dir.iterdir():
         for course in semester.iterdir():
-            query = "UPDATE courses SET group_name=:g WHERE semester = :s AND course = :c;"
+            query = "UPDATE courses SET group_name=:g, owner_name=:o WHERE semester = :s AND course = :c;"
             params = {
                 's': semester.name,
                 'c': course.name,
-                'g': course.group()
+                'g': course.group(),
+                'o': course.owner()
             }
             database.session.execute(text(query), params)
     # we should be able to force the not null constraint now
     database.execute("ALTER TABLE courses ALTER COLUMN group_name SET NOT NULL;")
+    database.execute("ALTER TABLE courses ALTER COLUMN owner_name SET NOT NULL;")
 
 
 def down(config, database):
