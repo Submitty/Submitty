2d8a717e84ad5670a6f71911b0619dc737148cce
HenrikLam <49821332+HenrikLam@users.noreply.github.com>
2021-07-16 11:11:15 -0400

    [Feature:Notifications] Add email pages for superusers (#6598)

--- site/app/models/EmailStatusModel.php
+++ site/app/models/EmailStatusModel.php
@@ -11,7 +11,8 @@ use app\libraries\DateUtils;
  * @method array     getSubjects()
  * @method array     getPending()
  * @method array     getSuccesses()
- * @method array     getError()
+ * @method array     getErrors()
+ * @method array     getCourses()
  */
 class EmailStatusModel extends AbstractModel {
     // A map of all unique subjects of emails to the time created
@@ -26,16 +27,21 @@ class EmailStatusModel extends AbstractModel {
     // A map of email subjects to the rows that resulted in an error in the database
     /** @prop-read array */
     protected $errors = [];
+    // A map of email subjects to the semester and course as one string
+    /** @prop-read array */
+    protected $courses = [];
 
     public function __construct(Core $core, $data) {
         parent::__construct($core);
         foreach ($data as $row) {
-            $this->subjects[$row["subject"]] = $row["created"];
             if (!array_key_exists($row["subject"], $this->subjects)) {
-                // initialize all the counter arrays
-                $this->pending[$row["subject"]] = [];
+                $this->subjects[$row["subject"]] = $row["created"];
                 $this->successes[$row["subject"]] = [];
                 $this->errors[$row["subject"]] = [];
+                $this->pending[$row["subject"]] = [];
+            }
+            if ($row["semester"] != null || $row["course"] != null) {
+                $this->courses[$row["subject"]] = $row["semester"] . ' ' . $row["course"];
             }
             if ($row["sent"] != null) {
                 $this->successes[$row["subject"]][] = $row;
