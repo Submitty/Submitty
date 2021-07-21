0f619caa590a4370526062b46dfba877c38dfe1c
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2021-07-21 02:36:52 -0400

    [Refactor:CourseMaterials] Migrate Course Materials Config to DB (#6599)
    * Initial DB migration and model
    
    * Add more functions to DatabaseQueries
    
    * Change DB and model to use int for type
    
    * Convert controller and view to use model
    
    * Remove access log and add tryGetCourseMaterial function in AbstractController
    
    * Fix PHP lint
    
    * Begin testing
    
    * Update PHP Unit tests
    
    * Remove down migration
    
    * Suggested changes
    
    * Fix unit tests
    
    * Check file existence
    
    * Suggested changes
    
    * Fix linting
    
    * Update query
    
    * Fix queries
    
    * Remove catches and update tests
    
    * Change precision to float
    
    * Edit SQL
    
    * Add model tests
    
    * WIP moving to doctrine
    
    * Suggested changes and full doctrine implemenation
    
    * Improve ORM
    
    * linting
    
    * Fix removing sections
    
    * Update controller tests and fix section bug
    
    * Fix model tests
    
    * Suggested changes
    
    * Folder mass edit support
    
    * Update tests
    
    * Update model test
    
    * Fix HTML validation issue and migration issue
    
    * Update code and tests to match
    
    * Fix PHP unit tests
    
    * Fix timezone issue
    
    * Remove authorized by allow list from twig
    
    * Timezone test
    
    * More timezone testing
    
    * Don't allow duplicate file paths
    
    * Fix set all release dates button
    
    * Fix unit tests
    
    * Change duplicate path message
    
    * Suggested changes
    
    * Remove down migration
    
    Co-authored-by: Chris Reed <cjreed121@users.noreply.github.com>
    Co-authored-by: Barb Cutler <bmcutler@users.noreply.github.com>

---
+++ site/app/entities/course/CourseMaterial.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\entities\course;
+
+use app\libraries\DateUtils;
+use app\models\User;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class CourseMaterial
+ * @package app\entities
+ * @ORM\Entity
+ * @ORM\Table(name="course_materials")
+ */
+class CourseMaterial {
+    const FILE = 0;
+    const LINK = 1;
+    const DIR = 2;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @ORM\Column(type="smallint")
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * @ORM\Column(type="datetimetz")
+     * @var \DateTime
+     */
+    protected $release_date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var boolean
+     */
+    protected $hidden_from_students;
+
+    /**
+     * @ORM\Column(type="float")
+     * @var float
+     */
+    protected $priority;
+
+    /**
+     * @ORM\OneToMany(targetEntity="\app\entities\course\CourseMaterialSection", mappedBy="course_material", fetch="EAGER", cascade={"persist"}, orphanRemoval=true)
+     * @var Collection<CourseMaterialSection>
+     */
+    protected $sections;
+
+    public function __construct(int $type, string $path, \DateTime $release_date, bool $hidden_from_students, float $priority) {
+        $this->setType($type);
+        $this->setPath($path);
+        $this->setReleaseDate($release_date);
+        $this->setHiddenFromStudents($hidden_from_students);
+        $this->setPriority($priority);
+        $this->sections = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<CourseMaterialSection>
+     */
+    public function getSections(): Collection {
+        return $this->sections;
+    }
+
+    public function getPath(): string {
+        return $this->path;
+    }
+
+    public function getType(): int {
+        return $this->type;
+    }
+
+    public function isFile(): bool {
+        return $this->type === self::FILE;
+    }
+
+    public function isLink(): bool {
+        return $this->type === self::LINK;
+    }
+
+    public function isDir(): bool {
+        return $this->type === self::DIR;
+    }
+
+    public function getPriority(): float {
+        return $this->priority;
+    }
+
+    public function getReleaseDate(): \DateTime {
+        return $this->release_date;
+    }
+
+    public function isHiddenFromStudents(): bool {
+        return $this->hidden_from_students;
+    }
+
+    public function isSectionAllowed(User $user): bool {
+        if ($this->sections->count() === 0) {
+            return true;
+        }
+        foreach ($this->sections->getValues() as $course_material_section) {
+            if ($user->getRegistrationSection() === $course_material_section->getSectionId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function setReleaseDate(\DateTime $release_date): void {
+        $this->release_date = $release_date;
+    }
+
+    public function setHiddenFromStudents(bool $hidden_from_students): void {
+        $this->hidden_from_students = $hidden_from_students;
+    }
+
+    public function setPriority(float $priority): void {
+        $this->priority = $priority;
+    }
+
+    public function setPath(string $path): void {
+        $this->path = $path;
+    }
+
+    public function setType(int $type): void {
+        $this->type = $type;
+    }
+
+    public function addSection(CourseMaterialSection $section): void {
+        $this->sections[] = $section;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function removeSection(CourseMaterialSection $section): void {
+        $this->sections->removeElement($section);
+    }
+}
