fbc38056b49ffd0b63c86b00bf16a2eedf30791c
HenrikLam <henrikhl131@gmail.com>
2021-07-22 13:01:25 -0400

    added functions to entity

--- site/app/entities/email/EmailEntity.php
+++ site/app/entities/email/EmailEntity.php
@@ -28,26 +28,86 @@ class EmailEntity
     private $id;
     /** 
      * @ORM\Column(type="string")
+     * @prop-read string
      */
     private $user_id;
     /**
      * @ORM\Column(type="text")
+     * @prop-read string
      */
     private $subject;
     /** 
      * @ORM\Column(type="text")
+     * @prop-read string
      */
     private $body;
-    /** @ORM\Column(type="datetime") */
+    /**
+     * @ORM\Column(type="datetime")
+     * @prop-read DateTime
+     */
     private $created;
-    /** @ORM\Column(type="datetime") */
+    /**
+     * @ORM\Column(type="datetime")
+     * @prop-read DateTime
+     */
     private $sent;
-    /** @ORM\Column(type="string") */
+    /** 
+     * @ORM\Column(type="string")
+     * @prop-read string
+     */
     private $error;
-    /** @ORM\Column(type="string", length=255) */
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @prop-read string
+     */
     private $email_address;
-    /** @ORM\Column(type="string") */
+    /** 
+     * @ORM\Column(type="string")
+     * @prop-read string
+     */
     private $semester;
-    /** @ORM\Column(type="string") */
+    /**
+     * @ORM\Column(type="string")
+     * @prop-read string
+     */
     private $course;
+
+    /**
+     * @return string
+     */
+    public function getUserId(): string {
+        return $this->user_id;
+    }
+
+    public function getSubject(): string {
+        return $this->subject;
+    }
+
+    public function getBody(): string {
+        return $this->body;
+    }
+
+    public function getCreated(): \DateTime {
+        return $this->created;
+    }
+
+    public function getSent(): \DateTime {
+        return $this->sent;
+    }
+
+    public function getError(): string {
+        return $this->error;
+    }
+
+    public function getEmailAddress(): string {
+        return $this->email_address;
+    }
+
+    public function getSemester(): string {
+        return $this->semester;
+    }
+
+    public function getCourse(): string {
+        return $this->course;
+    }
 }
