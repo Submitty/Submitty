be01df9de2d16c20681474413c005cb6ea0abca1
williamjallen <dalearn@icloud.com>
2021-07-26 09:54:59 -0400

    progress

--- site/cypress/integration/taGradingInterface.spec.js
+++ site/cypress/integration/taGradingInterface.spec.js
@@ -72,15 +72,14 @@ describe('Test cases relating to the grading of an assignment', () => {
         cy.get('#details-table').contains('aphacker').siblings(':nth-child(8)').click()
     });
 
-    describe('Test cases relating to the functionality of the control bar', () => {
-        it('Test that home button returns to the grading index page', () => {
-            cy.get('#main-page').click()
-            cy.url().should('include', buildUrl(['sample', 'gradeable', 'grading_homework', 'grading', 'details'], true))
-        });
-
-        it('Test that home button returns to the grading index page', () => {
-        });
-    });
+    // describe('Test cases relating to the functionality of the control bar', () => {
+    //     it('Test that home button returns to the grading index page', () => {
+    //         cy.get('#main-page').click()
+    //         cy.url().should('include', buildUrl(['sample', 'gradeable', 'grading_homework', 'grading', 'details'], true))
+    //     });
+    //
+    //     // TODO: Write more tests for the remaining buttons
+    // });
 
     // describe('Test cases relating to the "files" pane', () => {
     //     // it would be ideal to click on other types of folders too but unfortunately no autograding has been done at this point
@@ -98,4 +97,81 @@ describe('Test cases relating to the grading of an assignment', () => {
     //         cy.get('#file_viewer_sd1f1_iframe').should('be.visible')
     //     });
     // });
+
+    describe('Test cases relating to the "rubric" pane', () => {
+        // it('try clicking on a component and play around with the grade a bit', () => {
+        //     cy.get('#grading_rubric_btn').click()
+        //
+        //     cy.get('.component-container').first().within(() => {
+        //         cy.get('.graded-by').contains('Ungraded!')
+        //         cy.get('#grading_total').contains('− / 2')
+        //     });
+        //
+        //     cy.get('.component-container').first().within(() => {
+        //         // open the component
+        //         cy.get('#grading_total').click()
+        //
+        //         cy.get('#grading_total').contains('− / 2')
+        //
+        //         // wait for the component to load properly
+        //         cy.get('.mark-selector', { timeout: 4000 }).should('be.visible');
+        //
+        //         cy.get('.mark-selector').eq(0).click();
+        //         cy.get('#grading_total').contains('2 / 2')
+        //         cy.get('#grading_total').should('have.class', 'green-background')
+        //
+        //         cy.get('.mark-selector').eq(1).click();
+        //         cy.get('#grading_total').contains('1 / 2')
+        //         cy.get('#grading_total').should('have.class', 'yellow-background')
+        //
+        //         cy.get('.mark-selector').eq(1).click();
+        //         cy.get('#grading_total').contains('− / 2')
+        //
+        //         cy.get('.mark-selector').eq(2).click();
+        //         cy.get('#grading_total').contains('0 / 2')
+        //         cy.get('#grading_total').should('have.class', 'red-background')
+        //         cy.get('.mark-selector').eq(2).click();
+        //
+        //         // check increment/decrement
+        //         cy.get('.mark-note-custom').clear().type('Test mark')
+        //         cy.get('.plus-minus > .fa-caret-down').click()
+        //         cy.get('.plus-minus > .fa-caret-down').click()
+        //         cy.get('#grading_total').contains('1 / 2')
+        //         cy.get('.plus-minus > .fa-caret-up').click()
+        //         cy.get('#grading_total').contains('1.5 / 2')
+        //
+        //         // click the cancel button
+        //         cy.get('.save-tools-cancel').click()
+        //
+        //         cy.get('.mark-selector', { timeout: 4000 }).should('not.be.visible');
+        //
+        //         cy.get('.graded-by').contains('Ungraded!')
+        //         cy.get('#grading_total').contains('− / 2')
+        //     });
+        // });
+
+        it('save a mark and see that it is visible everywhere it should be visible', () => {
+            cy.get('#grading_rubric_btn').click()
+
+            cy.get('.component-container').first().within(() => {
+                // open the component
+                cy.get('#grading_total').click()
+
+                // wait for the component to load properly
+                cy.get('.mark-selector', { timeout: 4000 }).should('be.visible');
+
+                cy.get('.mark-selector').eq(1).click();
+                cy.get('#grading_total').contains('1 / 2')
+
+                // click the save button
+                cy.get('.save-tools-save').click()
+
+                // wait for the component to close
+                cy.get('.mark-selector', { timeout: 4000 }).should('not.be.visible');
+
+                cy.get('.graded-by').contains('Graded by instructor')
+                cy.get('#grading_total').contains('1 / 2')
+            });
+        });
+    });
 });
