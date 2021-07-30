89a8b714478ff165cae513fb1f3fcdbc0219ba36
Chris Reed <cjreed121@users.noreply.github.com>
2021-07-30 09:06:29 -0400

    SQL dump

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -183,6 +183,38 @@ CREATE TABLE public.course_materials (
 
 
 --
+-- Name: course_materials_access; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.course_materials_access (
+    id integer NOT NULL,
+    course_material_id integer NOT NULL,
+    user_id character varying(255) NOT NULL,
+    "timestamp" timestamp with time zone NOT NULL
+);
+
+
+--
+-- Name: course_materials_access_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.course_materials_access_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: course_materials_access_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.course_materials_access_id_seq OWNED BY public.course_materials_access.id;
+
+
+--
 -- Name: course_materials_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -1144,6 +1176,13 @@ ALTER TABLE ONLY public.course_materials ALTER COLUMN id SET DEFAULT nextval('pu
 
 
 --
+-- Name: course_materials_access id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.course_materials_access ALTER COLUMN id SET DEFAULT nextval('public.course_materials_access_id_seq'::regclass);
+
+
+--
 -- Name: gradeable_access id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -1748,11 +1787,19 @@ ALTER TABLE ONLY public.course_materials_sections
 
 
 --
--- Name: course_materials_sections fk_section_id; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: course_materials_access fk_user_id; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.course_materials_sections
-    ADD CONSTRAINT fk_section_id FOREIGN KEY (section_id) REFERENCES public.sections_registration(sections_registration_id) ON DELETE CASCADE;
+ALTER TABLE ONLY public.course_materials_access
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+--
+-- Name: gradeable_allowed_minutes_override fk_user_id; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.gradeable_allowed_minutes_override
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(user_id);
 
 
 --
