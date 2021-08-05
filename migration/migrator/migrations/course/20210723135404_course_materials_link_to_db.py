f2dc404bbaf9144d18051cec8be98c593324309a
Chris Reed <cjreed121@users.noreply.github.com>
2021-08-05 15:53:28 -0400

    Update migration

--- migration/migrator/migrations/course/20210723135404_course_materials_link_to_db.py
+++ migration/migrator/migrations/course/20210723135404_course_materials_link_to_db.py
@@ -1,6 +1,7 @@
 """Migration for a given Submitty course database."""
 
 import json
+import os
 from pathlib import Path
 
 def up(config, database, semester, course):
@@ -30,27 +31,24 @@ def up(config, database, semester, course):
                 for itemkey, itemvalue in data.items():
                     url = None
                     url_title = None
-                    if 'external_link' in itemvalue and itemvalue['external_link'] is True:
-                        try:
-                            f = open(itemkey)
-                            data = json.load(f)
-                            url = data['url']
-                            url_title = data['name']
-                            if url is not None and url_title is None:
-                                url_title = url
-                        except:
-                            print ("course materials migration - directory exception on {itemkey}")
-                    query = """
-                        UPDATE course_materials SET
-                        url = :url, url_title = :url_title
-                        WHERE path = :path
-                    """
-                    params = {
-                        'url': url,
-                        'url_title': url_title,
-                        'path': itemkey
-                    }
-                    database.session.execute(query, params)
+                    if 'external_link' in itemvalue and itemvalue['external_link'] is True and os.path.exists(itemkey):
+                        f = open(itemkey)
+                        data = json.load(f)
+                        url = data['url']
+                        url_title = data['name']
+                        if url is not None and url_title is None:
+                            url_title = url
+                        query = """
+                            UPDATE course_materials SET
+                            url = :url, url_title = :url_title
+                            WHERE path = :path
+                        """
+                        params = {
+                            'url': url,
+                            'url_title': url_title,
+                            'path': itemkey
+                        }
+                        database.session.execute(query, params)
 
 
 def down(config, database, semester, course):
