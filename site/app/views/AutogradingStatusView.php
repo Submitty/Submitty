3ce5bc08a714ed3f9133d6dec7115343828e80e0
HenrikLam <henrikhl131@gmail.com>
2021-08-13 16:35:03 -0400

    maybe add mobile-table support?

--- site/app/views/AutogradingStatusView.php
+++ site/app/views/AutogradingStatusView.php
@@ -10,6 +10,7 @@ class AutogradingStatusView extends AbstractView {
     public function displayPage($progress, $courses) {
         $this->core->getOutput()->addInternalJs("autograding-status.js");
         $this->core->getOutput()->addInternalCss("autograding-status.css");
+        $this->core->getOutput()->addInternalCss('table.css');
         return $this->core->getOutput()->renderTwigTemplate("AutogradingStatus.twig", [
             "progress" => $progress,
             "time" => date("H:i:s", time()),
