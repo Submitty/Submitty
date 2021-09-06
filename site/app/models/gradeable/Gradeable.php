56aabf96da73dda4212abc2efe6a71dd9a17c43b
Chris Reed <cjreed121@users.noreply.github.com>
2021-09-06 16:45:02 -0400

    Never coerce team lock date

--- site/app/models/gradeable/Gradeable.php
+++ site/app/models/gradeable/Gradeable.php
@@ -804,6 +804,7 @@ class Gradeable extends AbstractModel {
 
         // Blacklist the dates checked by validation
         $black_list = $this->getDateValidationSet($regrade_modified);
+        $black_list[] = "team_lock_date";
 
         // First coerce in the forward direction, then in the reverse direction
         return $coerce_dates(
