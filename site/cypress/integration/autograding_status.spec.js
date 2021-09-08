3c629c91d0c22814b55a83a79f3b895eecf872fb
HenrikLam <henrikhl131@gmail.com>
2021-09-07 21:27:36 -0400

    test

--- site/cypress/integration/autograding_status.spec.js
+++ site/cypress/integration/autograding_status.spec.js
@@ -4,8 +4,8 @@ import {skipOn} from '@cypress/skip-test';
 const autograding_status_path = 'autograding_status';
 
 // Assumes autograding workers have been turned off
-describe('Pre autograding test', () => {
-    skipOn(Cypress.env('autograding_shipper_off') === 'false', () => {
+skipOn(Cypress.env('run_area') === 'CI', () => {
+    describe('Pre autograding test', () => {
         before(() => {
             cy.visit('/');
             cy.login();
