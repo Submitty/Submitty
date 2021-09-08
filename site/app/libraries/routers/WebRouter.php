aee056fb0683e0175196d325e501cebbb6cc506a
DefinitiveAbove <definitiveabove@gmail.com>
2021-09-08 01:42:21 -0400

    Fix improper type

--- site/app/libraries/routers/WebRouter.php
+++ site/app/libraries/routers/WebRouter.php
@@ -28,7 +28,7 @@ class WebRouter {
     /** @var Request  */
     protected $request;
 
-    /** @var AnnotationReader */
+    /** @var PsrCachedReader */
     protected $reader;
 
     /** @var array */
