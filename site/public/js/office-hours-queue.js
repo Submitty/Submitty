eeee8db85172e239a34d9bbac91e785b06148018
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-09-26 20:06:43 -0400

    office hours queue

--- site/public/js/office-hours-queue.js
+++ site/public/js/office-hours-queue.js
@@ -1,11 +1,10 @@
 /* global previewMarkdown */
 /* exported previewQueueMarkdown */
 
-function previewQueueMarkdown(){
+function previewQueueMarkdown(mode){
     const announcement_textarea = $('#queue-announcement-message');
     const preview = $('#queue_announcement_message_preview');
-    const preview_button = $('button[title="Preview Markdown"]');
     const announcement_content = announcement_textarea.val();
 
-    previewMarkdown(announcement_textarea, preview, preview_button, { content: announcement_content });
+    previewMarkdown(mode, announcement_textarea, preview, { content: announcement_content });
 }
