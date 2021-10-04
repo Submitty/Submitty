4c7696460a0ef94f1209986d621b0d47b3723f8c
jenaybarela <jbarela775@gmail.com>
2021-10-04 18:01:25 -0400

    Adding visual cue for expiring threads

--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -282,6 +282,7 @@ class ForumThreadView extends AbstractView {
                 "userGroup" => $generatePostContent["userGroup"],
                 "activeThread" => $generatePostContent["activeThread"],
                 "activeThreadAnnouncement" => $generatePostContent["activeThreadAnnouncement"],
+                "expiring" => $generatePostContent["expiring"],
                 "isCurrentFavorite" => $generatePostContent["isCurrentFavorite"],
                 "display_option" => $generatePostContent["display_option"],
                 "post_data" => $generatePostContent["post_data"],
@@ -423,6 +424,7 @@ class ForumThreadView extends AbstractView {
 
         $activeThreadTitle = ($this->core->getUser()->accessFullGrading() ? "({$activeThread['id']}) " : '') . $activeThread['title'];
         $activeThreadAnnouncement = $activeThread['pinned_expiration'] > date("Y-m-d H:i:s");
+        $expiring = $activeThread['pinned_expiration'] <= date("Y-m-d H:i:s", strtotime("+7 day"));
 
         $thread_id = $activeThread['id'];
 
@@ -551,6 +553,7 @@ class ForumThreadView extends AbstractView {
                 "userGroup" => $this->core->getUser()->getGroup(),
                 "activeThread" => $activeThread,
                 "activeThreadAnnouncement" => $activeThreadAnnouncement,
+                "expiring" => $expiring,
                 "isCurrentFavorite" => $isCurrentFavorite,
                 "display_option" => $display_option,
                 "post_data" => $post_data,
@@ -574,6 +577,7 @@ class ForumThreadView extends AbstractView {
                 "userGroup" => $this->core->getUser()->getGroup(),
                 "activeThread" => $activeThread,
                 "activeThreadAnnouncement" => $activeThreadAnnouncement,
+                "expiring" => $expiring,
                 "isCurrentFavorite" => $isCurrentFavorite,
                 "display_option" => $display_option,
                 "post_data" => $post_data,
@@ -825,6 +829,7 @@ class ForumThreadView extends AbstractView {
                 "link" => $link,
                 "class" => $class,
                 "pinned" => $thread["pinned_expiration"] > date("Y-m-d H:i:s"),
+                "expiring" => $thread["pinned_expiration"] <= date("Y-m-d H:i:s", strtotime("+7 day")),
                 "favorite" => $favorite,
                 "merged_thread_id" => $thread['merged_thread_id'],
                 "status" => $thread["status"],
