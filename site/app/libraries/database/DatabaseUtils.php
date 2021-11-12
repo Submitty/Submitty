989ec84f939a7c83ee695038bf93ee978d76034e
William Allen <16820599+williamjallen@users.noreply.github.com>
2021-11-12 18:28:26 -0500

    Update site/app/libraries/database/DatabaseUtils.php
    Co-authored-by: Matthew Peveler <matt.peveler@gmail.com>

--- site/app/libraries/database/DatabaseUtils.php
+++ site/app/libraries/database/DatabaseUtils.php
@@ -7,9 +7,9 @@ namespace app\libraries\database;
 use app\libraries\DateUtils;
 
 class DatabaseUtils {
-    public static function formatQuery($sql, $params): string {
-        if (is_null($params)) {
-            return '';
+    public static function formatQuery(string $sql, ?array $params): string {
+        if ($params === null) {
+            return $sql;
         }
 
         foreach ($params as $param) {
