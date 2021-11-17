51d4d91aa51c82a3148e2ccb4a0c7fed7365c825
Javier Portorreal <portoj@rpi.edu>
2021-11-17 18:54:59 -0500

    [Feature:TAGrading] add table contraints

--- migration/migrator/migrations/course/20210928161700_add_late_day_cache_table.py
+++ migration/migrator/migrations/course/20210928161700_add_late_day_cache_table.py
@@ -21,21 +21,31 @@ def up(config, database, semester, course):
             team_id VARCHAR(255),
             g_title VARCHAR(255),
             late_day_date TIMESTAMP WITHOUT TIME zone NOT NULL,
-            late_days_remaining INTEGER,
+            late_days_remaining INTEGER NOT NULL,
             late_days_allowed INTEGER,
             submission_days_late INTEGER,
             late_day_exceptions INTEGER, 
             late_day_status INTEGER,
             late_days_change INTEGER NOT NULL,
-            CONSTRAINT ldc_user_team_id_check CHECK (((user_id IS NOT NULL) OR (team_id IS NOT NULL)))
+            CONSTRAINT ldc_user_team_id_check CHECK (
+                ((user_id IS NOT NULL) OR (team_id IS NOT NULL))
+                AND
+                NOT ((user_id IS NOT NULL) AND (team_id IS NOT NULL))
+            )
         );
     ''')
 
     database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT ldc_g_user_id_unique UNIQUE (g_id, user_id);')
     database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT ldc_g_team_id_unique UNIQUE (g_id, team_id);')
-    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_g_id FOREIGN KEY (g_id) REFERENCES gradeable(g_id);')
-    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_user FOREIGN KEY (user_id) REFERENCES users(user_id);')
-    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_team FOREIGN KEY (team_id) REFERENCES gradeable_teams(team_id);')
+    # Make sure that all gradeable info is present if g_id is not null
+    database.execute('''ALTER TABLE late_day_cache ADD CONSTRAINT ldc_gradeable_info CHECK (
+        g_id IS NULL
+        OR
+        ((g_title IS NOT NULL) AND (submission_days_late IS NOT NULL) AND (late_day_exceptions IS NOT NULL))
+    );''')
+    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_g_id FOREIGN KEY (g_id) REFERENCES gradeable(g_id) ON DELETE CASCADE;')
+    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;')
+    database.execute('ALTER TABLE late_day_cache ADD CONSTRAINT late_day_cache_team FOREIGN KEY (team_id) REFERENCES gradeable_teams(team_id) ON DELETE CASCADE ON UPDATE CASCADE;')
     pass
 
 
