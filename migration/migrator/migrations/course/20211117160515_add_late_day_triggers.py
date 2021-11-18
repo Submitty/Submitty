cd89ec280643115857b3e53c0e6f961c39a04ee7
Javier Portorreal <portoj@rpi.edu>
2021-11-18 10:17:21 -0500

    [Feature:Submission] COrrect migration and add dump

--- migration/migrator/migrations/course/20211117160515_add_late_day_triggers.py
+++ migration/migrator/migrations/course/20211117160515_add_late_day_triggers.py
@@ -1,12 +1,18 @@
 """Migration for the Submitty system."""
 
 
-def up(config):
+def up(config, database, semester, course):
     """
     Run up migration.
 
     :param config: Object holding configuration details about Submitty
     :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
     """
 
     # Drop triggers
@@ -337,12 +343,18 @@ def up(config):
     pass
 
 
-def down(config):
+def down(config, database, semester, course):
     """
     Run down migration (rollback).
 
     :param config: Object holding configuration details about Submitty
     :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
     """
     # Drop triggers
     database.execute("DROP TRIGGER IF EXISTS gradeable_version_change ON electronic_gradeable_version;")
