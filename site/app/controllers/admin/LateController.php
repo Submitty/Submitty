7e8d749d1eeacbeda05abc9d7c5d580ff7c0195c
Javier Portorreal <portoj@rpi.edu>
2021-12-08 17:45:13 -0500

    [Feature:Submission] remove import

--- site/app/controllers/admin/LateController.php
+++ site/app/controllers/admin/LateController.php
@@ -9,7 +9,6 @@ use app\libraries\response\RedirectResponse;
 use app\libraries\response\MultiResponse;
 use app\libraries\response\JsonResponse;
 use app\libraries\response\WebResponse;
-use app\models\gradeable\LateDays;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
