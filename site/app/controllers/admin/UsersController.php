dbd2ab0d0b4a68a2632f249de6d73d6fe8e00a59
listeph <20steph18@gmail.com>
2021-12-08 17:31:46 -0500

    suggested change

--- site/app/controllers/admin/UsersController.php
+++ site/app/controllers/admin/UsersController.php
@@ -941,7 +941,6 @@ class UsersController extends AbstractController {
             Registration section is optional for graders, so automatically validate if not set.*/
             if (isset($vals[$registration_section_idx]) && strtolower($vals[$registration_section_idx]) === "null") {
                 $vals[$registration_section_idx] = null;
-                $uploaded_data[$row_num][$registration_section_idx] = null;
             }
             $unset_grader_registration_section = ($list_type === 'graderlist' && empty($vals[$registration_section_idx]));
             if (!($unset_grader_registration_section || User::validateUserData('registration_section', $vals[$registration_section_idx]))) {
@@ -985,6 +984,7 @@ class UsersController extends AbstractController {
                     $bad_columns[] = 'user_preferred_lastname';
                 }
             }
+            $uploaded_data[$row_num] = $vals;
         }
 
         // $bad_rows will contain rows with errors.  No errors to report when empty.
