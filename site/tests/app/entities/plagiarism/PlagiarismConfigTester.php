548c4399904da5a6cfb362beb87f4a1dd9be2168
sbelsk <shellybelsky02@gmail.com>
2021-12-13 14:20:34 -0500

    update tests

--- site/tests/app/entities/plagiarism/PlagiarismConfigTester.php
+++ site/tests/app/entities/plagiarism/PlagiarismConfigTester.php
@@ -6,9 +6,10 @@ namespace tests\app\entities\plagiarism;
 
 use app\entities\plagiarism\PlagiarismConfig;
 use app\exceptions\ValidationException;
-use Exception;
+use app\exceptions\FileNotFoundException;
 use Doctrine\ORM\Mapping as ORM;
 use app\libraries\plagiarism\PlagiarismUtils;
+use app\libraries\DateUtils;
 use tests\BaseUnitTest;
 
 class PlagiarismConfigTester extends BaseUnitTest {
@@ -16,6 +17,8 @@ class PlagiarismConfigTester extends BaseUnitTest {
     private $my_config;
 
     public function setUp(): void {
+        DateUtils::setTimezone(new \DateTimeZone("America/New_York"));
+
         $this->my_config = new PlagiarismConfig(
             "homework_1",
             1,
@@ -28,6 +31,8 @@ class PlagiarismConfigTester extends BaseUnitTest {
             20,
             10,
             [],
+            [],
+            4,
             ["ta", "ta2"]
         );
     }
@@ -42,7 +47,7 @@ class PlagiarismConfigTester extends BaseUnitTest {
         $this->assertEquals($this->my_config->isRegexDirCheckoutSelected(), false);
         $this->assertEquals($this->my_config->getLanguage(), "plaintext");
         $this->assertEquals($this->my_config->getThreshold(), 20);
-        $this->assertEquals($this->my_config->getSequenceLength(), 10);
+        $this->assertEquals($this->my_config->getHashSize(), 10);
         $this->assertEquals($this->my_config->getOtherGradeables(), []);
         $this->assertEquals($this->my_config->getIgnoredSubmissions(), ["ta", "ta2"]);
     }
@@ -55,7 +60,7 @@ class PlagiarismConfigTester extends BaseUnitTest {
         try {
             $this->my_config->setVersionStatus("latest_version");
         }
-        catch (Exception $e) {
+        catch (ValidationException $e) {
             $exception_thrown = true;
         }
         $this->assertTrue($exception_thrown);
@@ -68,7 +73,7 @@ class PlagiarismConfigTester extends BaseUnitTest {
         try {
             $this->my_config->setRegexArray(["foo\..\secret_file.txt", "*_3.cpp"]);
         }
-        catch (Exception $e) {
+        catch (ValidationException $e) {
             $exception_thrown = true;
         }
         $this->assertTrue($exception_thrown);
@@ -93,7 +98,7 @@ class PlagiarismConfigTester extends BaseUnitTest {
         try {
             $this->my_config->setLanguage("swift");
         }
-        catch (Exception $e) {
+        catch (ValidationException $e) {
             $exception_thrown = true;
         }
         $this->assertTrue($exception_thrown);
@@ -106,36 +111,49 @@ class PlagiarismConfigTester extends BaseUnitTest {
         try {
             $this->my_config->setThreshold(-5);
         }
-        catch (Exception $e) {
+        catch (ValidationException $e) {
             $exception_thrown = true;
         }
         $this->assertTrue($exception_thrown);
         $this->assertEquals($this->my_config->getThreshold(), 25);
 
-        // sequence length
-        $this->my_config->setSequenceLength(7);
-        $this->assertEquals($this->my_config->getSequenceLength(), 7);
+        // hash size
+        $this->my_config->setHashSize(7);
+        $this->assertEquals($this->my_config->getHashSize(), 7);
         $exception_thrown = false;
         try {
-            $this->my_config->setSequenceLength(-3);
+            $this->my_config->setHashSize(-3);
         }
-        catch (Exception $e) {
+        catch (ValidationException $e) {
             $exception_thrown = true;
         }
         $this->assertTrue($exception_thrown);
-        $this->assertEquals($this->my_config->getSequenceLength(), 7);
+        $this->assertEquals($this->my_config->getHashSize(), 7);
 
         // other gradeables
         $this->my_config->setOtherGradeables([
-            "prior_semester" => "f16",
-            "prior_course" => "sample",
-            "prior_gradeable" => "example_gradeable"
+            "other_semester" => "f16",
+            "other_course" => "sample",
+            "other_gradeable" => "example_gradeable"
         ]);
         $this->assertEquals($this->my_config->getOtherGradeables(), [
-            "prior_semester" => "f16",
-            "prior_course" => "sample",
-            "prior_gradeable" => "example_gradeable"
+            "other_semester" => "f16",
+            "other_course" => "sample",
+            "other_gradeable" => "example_gradeable"
         ]);
+
+        // other gradeable paths
+        $this->assertFalse($this->my_config->hasOtherGradeablePaths());
+        $exception_thrown = false;
+        try {
+            $this->my_config->setOtherGradeablePaths(["/var/local/submitty/courses/f17/test_course/hw1","/my_documents/hw1"], 10);
+        }
+        catch (FileNotFoundException $e) {
+            $exception_thrown = true;
+        }
+        $this->assertTrue($exception_thrown);
+        $this->assertFalse($this->my_config->hasOtherGradeablePaths());
+        $this->assertEquals($this->my_config->getOtherGradeablePaths(), []);
 
         // ignored submissions
         $this->my_config->setIgnoredSubmissions([]);
