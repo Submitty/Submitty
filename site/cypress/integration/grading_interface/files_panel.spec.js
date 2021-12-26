8f3dd2ba8e3df44d3b88ad72ef5323a35fb6d8db
williamjallen <william.j.allen24@gmail.com>
2021-12-26 10:06:15 -0500

    lint

--- site/cypress/integration/grading_interface/files_panel.spec.js
+++ site/cypress/integration/grading_interface/files_panel.spec.js
@@ -1,76 +1,76 @@
 describe('Test cases involving the files panel', () => {
     function assertSubmissionsBrowserClosed() {
-        cy.get('#div_viewer_sd1').should('not.be.visible')
+        cy.get('#div_viewer_sd1').should('not.be.visible');
     }
 
     function assertSubmissionsBrowserOpen() {
-        cy.get('#div_viewer_sd1').should('be.visible')
+        cy.get('#div_viewer_sd1').should('be.visible');
     }
 
     function assertResultsBrowserClosed() {
-        cy.get('#div_viewer_rd1').should('not.be.visible')
+        cy.get('#div_viewer_rd1').should('not.be.visible');
     }
 
     function assertResultsBrowserOpen() {
-        cy.get('#div_viewer_rd1').should('be.visible')
+        cy.get('#div_viewer_rd1').should('be.visible');
     }
 
     beforeEach(() => {
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'details']);
         cy.login('instructor');
-        cy.get('.btn').contains('View All').click()
-        cy.get('#details-table').contains('aphacker').siblings().eq(6).click()
-        cy.get('#submission_browser_btn').click()
+        cy.get('.btn').contains('View All').click();
+        cy.get('#details-table').contains('aphacker').siblings().eq(6).click();
+        cy.get('#submission_browser_btn').click();
     });
 
     it('test the open/close submissions and results buttons', () => {
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
-        cy.get('#toggleSubmissionButton').click() // open submissions browser
-        assertSubmissionsBrowserOpen()
-        assertResultsBrowserClosed()
-        cy.get('#toggleSubmissionButton').click() // close submissions browser
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
+        cy.get('#toggleSubmissionButton').click(); // open submissions browser
+        assertSubmissionsBrowserOpen();
+        assertResultsBrowserClosed();
+        cy.get('#toggleSubmissionButton').click(); // close submissions browser
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
 
-        cy.get('#toggleResultButton').click() // open results browser
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserOpen()
-        cy.get('#toggleResultButton').click() // close results browser
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
+        cy.get('#toggleResultButton').click(); // open results browser
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserOpen();
+        cy.get('#toggleResultButton').click(); // close results browser
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
     });
 
     it('test the auto open checkbox', () => {
-        cy.get('#autoscroll_id').click()
-        cy.get('#autoscroll_id').should('be.checked')
+        cy.get('#autoscroll_id').click();
+        cy.get('#autoscroll_id').should('be.checked');
 
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
-        cy.get('#submissions').click() // open submissions browser
-        cy.get('#results').click() // open results browser
-        assertSubmissionsBrowserOpen()
-        assertResultsBrowserOpen()
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
+        cy.get('#submissions').click(); // open submissions browser
+        cy.get('#results').click(); // open results browser
+        assertSubmissionsBrowserOpen();
+        assertResultsBrowserOpen();
 
-        cy.get('#next-student').click()
-        assertSubmissionsBrowserOpen()
-        assertResultsBrowserOpen()
+        cy.get('#next-student').click();
+        assertSubmissionsBrowserOpen();
+        assertResultsBrowserOpen();
 
-        cy.get('#submissions').click() // close submissions browser
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserOpen()
+        cy.get('#submissions').click(); // close submissions browser
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserOpen();
 
-        cy.get('#prev-student').click()
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserOpen()
+        cy.get('#prev-student').click();
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserOpen();
 
-        cy.get('#results').click() // close results browser
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
+        cy.get('#results').click(); // close results browser
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
 
-        cy.get('#next-student').click()
-        assertSubmissionsBrowserClosed()
-        assertResultsBrowserClosed()
+        cy.get('#next-student').click();
+        assertSubmissionsBrowserClosed();
+        assertResultsBrowserClosed();
     });
 
     it('test the full panel view', () => {
@@ -78,13 +78,13 @@ describe('Test cases involving the files panel', () => {
         // displays an iframe which is inaccessible to Cypress for the most part
         // We just test that the full panel view opens, and that an iframe appears.
 
-        cy.get('#submissions').click()
-        cy.get('#file_viewer_full_panel_iframe').should('not.exist')
-        cy.get('i[title="Show file in full panel"]').first().click()
-        cy.get('#grading_file_name').should('contain', '.submit.timestamp')
-        cy.get('#file_viewer_full_panel_iframe').should('be.visible')
+        cy.get('#submissions').click();
+        cy.get('#file_viewer_full_panel_iframe').should('not.exist');
+        cy.get('i[title="Show file in full panel"]').first().click();
+        cy.get('#grading_file_name').should('contain', '.submit.timestamp');
+        cy.get('#file_viewer_full_panel_iframe').should('be.visible');
 
-        cy.get('[aria-label="Collapse File"]').click()
-        cy.get('#file_viewer_full_panel_iframe').should('not.exist')
+        cy.get('[aria-label="Collapse File"]').click();
+        cy.get('#file_viewer_full_panel_iframe').should('not.exist');
     });
 });
