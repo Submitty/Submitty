9df7ff62e488a264d47071664d1e6c7ff51a4c2c
DefinitiveAbove <definitiveabove@gmail.com>
2022-02-15 04:08:44 -0500

    linting (:

--- site/ts/grading/panel-student-info.ts
+++ site/ts/grading/panel-student-info.ts
@@ -1,10 +1,10 @@
 function checkTaVersionChange(){
-    let message = "You are overriding the student's chosen submission. Are you sure you want to continue?";
+    const message = "You are overriding the student's chosen submission. Are you sure you want to continue?";
     return confirm(message);
 }
 
 export function init() {
-    $("#student-info-ta-version-form").on("submit", () => {
+    $('#student-info-ta-version-form').on('submit', () => {
         return checkTaVersionChange();
-    })
+    });
 }
