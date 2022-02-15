2a4697a364abe61d83f68ae4f6a3abbf15cc118f
DefinitiveAbove <definitiveabove@gmail.com>
2022-02-15 04:06:38 -0500

    linting (:

--- site/ts/grading.ts
+++ site/ts/grading.ts
@@ -1,2 +1,2 @@
-export * as autograding from "./grading/panel-autograding";
-export * as studentInfo from "./grading/panel-student-info";
+export * as autograding from './grading/panel-autograding';
+export * as studentInfo from './grading/panel-student-info';
--- site/ts/grading/panel-autograding.ts
+++ site/ts/grading/panel-autograding.ts
@@ -1,30 +1,30 @@
-import { getCsrfToken } from "../utils/server";
+import { getCsrfToken } from '../utils/server';
 
-var HIGHEST_VERSION: number;
-var GRADEABLE_ID: string;
-var USER_ID: string;
+let HIGHEST_VERSION: number;
+let GRADEABLE_ID: string;
+let USER_ID: string;
 
 function regrade(single_regrade: number) {
     //if only regrading active version, late day fields left as 0 because they are irrelevant for regrading
     if (single_regrade) {
-        window.handleRegrade(HIGHEST_VERSION, getCsrfToken(), GRADEABLE_ID, USER_ID, true)
+        window.handleRegrade(HIGHEST_VERSION, getCsrfToken(), GRADEABLE_ID, USER_ID, true);
     }
     //regrading all versions
     else {
-        window.handleRegrade(HIGHEST_VERSION, getCsrfToken(), GRADEABLE_ID, USER_ID, false, true)
+        window.handleRegrade(HIGHEST_VERSION, getCsrfToken(), GRADEABLE_ID, USER_ID, false, true);
     }
 }
 
 // expand all outputs in Auto-Grading Testcases section
 function openAllAutoGrading() {
     // show all divs whose id starts with testcase_
-    let clickable_divs = $("[id^='tc_']");
+    const clickable_divs = $("[id^='tc_']");
 
     for (let i = 0; i < clickable_divs.length; i++) {
-        let clickable_div = clickable_divs[i];
-        let num = clickable_div.id.split("_")[1];
-        let content_div = $('#testcase_' + num);
-        if (content_div.css("display") == "none") {
+        const clickable_div = clickable_divs[i];
+        const num = clickable_div.id.split('_')[1];
+        const content_div = $(`#testcase_${num}`);
+        if (content_div.css('display') == 'none') {
             clickable_div.click();
         }
     }
@@ -34,8 +34,8 @@ function openAllAutoGrading() {
 function closeAllAutoGrading() {
     // hide all divs whose id starts with testcase_
     $("[id^='testcase_']").hide();
-    $("[id^='details_tc_']").find("span").hide();
-    $("[id^='details_tc_']").find(".loading-tools-show").show();
+    $("[id^='details_tc_']").find('span').hide();
+    $("[id^='details_tc_']").find('.loading-tools-show').show();
 }
 
 export function init(highest_version: number, gradeable_id: string, user_id: string) {
@@ -43,16 +43,16 @@ export function init(highest_version: number, gradeable_id: string, user_id: str
     GRADEABLE_ID = gradeable_id;
     USER_ID = user_id;
 
-    $("#autograding-results-open-all").on("click", () => {
+    $('#autograding-results-open-all').on('click', () => {
         openAllAutoGrading();
     });
-    $("#autograding-results-close-all").on("click", () => {
+    $('#autograding-results-close-all').on('click', () => {
         closeAllAutoGrading();
     });
-    $("#autograding-results-regrade-active").on("click", () => {
+    $('#autograding-results-regrade-active').on('click', () => {
         regrade(1);
     });
-    $("#autograding-results-regrade-all").on("click", () => {
+    $('#autograding-results-regrade-all').on('click', () => {
         regrade(0);
-    })
+    });
 }
