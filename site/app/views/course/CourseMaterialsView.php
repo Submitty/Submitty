4da14b23bea2a94d3cb1d71c7b8a1e19cc6c9b81
DefinitiveAbove <definitiveabove@gmail.com>
2022-02-16 15:25:53 -0500

    Fix issue with CourseMaterials hiding empty folders

--- site/app/views/course/CourseMaterialsView.php
+++ site/app/views/course/CourseMaterialsView.php
@@ -155,17 +155,16 @@ class CourseMaterialsView extends AbstractView {
     }
 
     private function removeEmptyFolders(array &$course_materials): bool {
+        $is_empty = true;
         foreach ($course_materials as $path => $course_material) {
-            if (is_array($course_material)) {
-                if ($this->removeEmptyFolders($course_material)) {
-                    unset($course_materials[$path]);
-                }
+            if (is_array($course_material) && $this->removeEmptyFolders($course_material)) {
+                unset($course_materials[$path]);
             }
             else {
-                return false;
+                $is_empty = false;
             }
         }
-        return true;
+        return $is_empty;
     }
 
     private function setSeen(array $course_materials, array &$seen, string $cur_path): bool {
