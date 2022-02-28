ab7acf8043a987d9dcd92cc8facbc00961559f71
wusatosi <26424577+wusatosi@users.noreply.github.com>
2022-02-28 04:26:01 -0500

    add some documentation to the e2e test

--- tests/e2e/test_redirect.py
+++ tests/e2e/test_redirect.py
@@ -2,27 +2,27 @@ from .base_testcase import BaseTestCase
 
 
 class RedirectTest(BaseTestCase):
+
     def __init__(self, testname):
         super().__init__(testname, log_in=True)
 
-    def test_redirect(self):
+    def test_trailing_slash_removal(self):
+        """ Test for redirection that removes trailing slash in url. """
         url_template = [
             '/courses/{}/{}',
             '/courses/{}/{}/gradeable',
             '/courses/{}/{}/forum',
             '/courses/{}/{}/course_materials'
         ]
-
-        url_formatted = [url.format(self.semester, 'sample') for url in url_template]        
+        url_formatted = [url.format(self.semester, 'sample') for url in url_template]
 
         for url in url_formatted:
-            redirect_from = url + '/'
-            self.get(redirect_from)
+            self.get(url + '/')
+            # /courses/s22/sample/ -> /courses/s22/sample by redirect
             self.assertEqual(
-                self.test_url + url, 
-                self.driver.current_url, 
+                self.test_url + url,
+                self.driver.current_url,
                 "Should redirect to remove trailing slash"
             )
 
         pass
-
