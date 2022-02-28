733c160cb7abb6fe7389f6e2205b47ede0a67fcd
wusatosi <26424577+wusatosi@users.noreply.github.com>
2022-02-27 22:22:28 -0500

    redirect e2e draft

---
+++ tests/e2e/test_redirect.py
@@ -0,0 +1,26 @@
+from .base_testcase import BaseTestCase
+
+
+class RedirectTest(BaseTestCase):
+    def __init__(self, testname):
+        super().__init__(testname, log_in=True)
+
+    def test_redirect(self):
+        url_template = [
+            '/courses/{}/{}',
+            '/courses/{}/{}/gradeable',
+            '/courses/{}/{}/forum',
+            '/courses/{}/{}/course_materials'
+        ]
+
+        url_formatted = [url.format(self.semester, 'sample') for url in url_template]        
+
+        for url in url_formatted:
+            redirect_from = url + '/'
+            self.get(url)
+            redirected_to = self.driver.getCurrentUrl()
+            msg = "Redirection to remove trailing slash not accomplished"
+            self.assertEqual(redirect_from, redirected_to, msg)
+
+        pass
+
