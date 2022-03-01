c5c4eb2f3a8b01bb2e2effe10a9d04994130c642
wusatosi <26424577+wusatosi@users.noreply.github.com>
2022-03-01 13:09:04 -0500

    lint

--- site/cypress/integration/url_flexibility.spec.js
+++ site/cypress/integration/url_flexibility.spec.js
@@ -1,19 +1,20 @@
-import {getCurrentSemester} from '../support/utils.js'
+import {getCurrentSemester} from '../support/utils.js';
 
 describe('Tests for auto removal of trailing slash in url', () => {
-    before(() => {cy.login()});
+    before(() => { cy.login(); });
 
-    const BASE_URL = `/courses/${getCurrentSemester()}/sample`;
+    const BASE_URL = `/courses/${ getCurrentSemester() }/sample`;
 
     [
-        BASE_URL, 
-        `${BASE_URL}/gradeable`, 
+        BASE_URL,
+        `${BASE_URL}/gradeable`,
         `${BASE_URL}/forum`,
-        `${BASE_URL}/course_materials`, 
-        `${BASE_URL}/office_hours_queue`
-    ].forEach((url) => 
+        `${BASE_URL}/course_materials`,
+        `${BASE_URL}/office_hours_queue`,
+    ].forEach((url) =>
         it(`removes trailing slash for ${url}`, () => {
-            cy.visit(url + '/');
+            cy.visit(`${ url }`);
             cy.location('pathname').should('eq', url);
-    }));
-})
+        });
+    );
+});
