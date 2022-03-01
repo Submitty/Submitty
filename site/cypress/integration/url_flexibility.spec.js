e5cbdb3f3829232de3c8804e67405a44df8c6fd1
wusatosi <26424577+wusatosi@users.noreply.github.com>
2022-03-01 13:04:13 -0500

    add cypress test

---
+++ site/cypress/integration/url_flexibility.spec.js
@@ -0,0 +1,19 @@
+import {getCurrentSemester} from '../support/utils.js'
+
+describe('Tests for auto removal of trailing slash in url', () => {
+    before(() => {cy.login()});
+
+    const BASE_URL = `/courses/${getCurrentSemester()}/sample`;
+
+    [
+        BASE_URL, 
+        `${BASE_URL}/gradeable`, 
+        `${BASE_URL}/forum`,
+        `${BASE_URL}/course_materials`, 
+        `${BASE_URL}/office_hours_queue`
+    ].forEach((url) => 
+        it(`removes trailing slash for ${url}`, () => {
+            cy.get(url + '/');
+            cy.location('pathname').should('eq', url);
+    }));
+})
