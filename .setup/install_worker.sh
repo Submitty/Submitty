8ac58289b684594bca6ae82e62cf08e1c46b804a
DefinitiveAbove <definitiveabove@gmail.com>
2022-03-09 14:01:39 -0500

    Requested changes

--- .setup/install_worker.sh
+++ .setup/install_worker.sh
@@ -8,7 +8,6 @@
 
 GIT_PATH=/usr/local/submitty/GIT_CHECKOUT/Submitty
 SUPERVISOR_USER=submitty
-NO_RPI=0
 
 echo "checking ${SUPERVISOR_USER} user"
 # Create the submitty user here on the worker machine
@@ -23,10 +22,6 @@ fi
 bash ${GIT_PATH}/.setup/install_system.sh --worker --vagrant ${@} 2>&1 | tee ${GIT_PATH}/.vagrant/install_worker_system.log
 echo "--- FINISHED INSTALLING SYSTEM ---"
 echo "installing worker..."
-
-if [ ${NO_RPI} == 0 ]; then
-    sudo bash /usr/local/submitty/GIT_CHECKOUT/Submitty/.setup/distro_setup/ubuntu/rpi.sh
-fi
 
 sudo usermod -a -G submitty_daemon ${SUPERVISOR_USER}
 sudo usermod -a -G submitty_daemonphp ${SUPERVISOR_USER}
