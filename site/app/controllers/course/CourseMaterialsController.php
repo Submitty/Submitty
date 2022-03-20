c3b0cee845fa1a4e6f3f484b612ed4231f3d6c9f
poorna <poorna2152@gmail.com>
2022-03-20 11:12:47 +0530

    Fix not selecting section

--- site/app/controllers/course/CourseMaterialsController.php
+++ site/app/controllers/course/CourseMaterialsController.php
@@ -461,6 +461,9 @@ class CourseMaterialsController extends AbstractController {
         if (isset($_POST['sections']) && $sections_lock) {
             $sections = $_POST['sections'];
             $sections_exploded = @explode(",", $sections);
+            if (empty($sections_exploded)) {
+                return JsonResponse::getErrorResponse("Select at least one section");
+            }
             $details['sections'] = $sections_exploded;
         }
         else {
