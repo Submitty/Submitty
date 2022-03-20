0d6b8dcbd9ce744e3fa5629fcbe31de441df2993
poorna <poorna2152@gmail.com>
2022-03-20 22:34:30 +0530

    Add e2e test case

--- site/cypress/integration/courseMaterials.spec.js
+++ site/cypress/integration/courseMaterials.spec.js
@@ -281,6 +281,18 @@ describe('Test cases revolving around course material uploading and access contr
 
         });
 
+        it('Should display warning alert when no section selected for restrict course materials', () => {
+            cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
+            cy.get('#all_Sections_Showing_yes').click();
+            cy.get('#upload1').attachFile(['file1.txt', 'file2.txt'] , { subjectType: 'drag-n-drop' });
+            cy.get('#upload_picker').clear().type('2021-06-29 21:37:53');
+            cy.get('#cm_path').click();
+            cy.get('#submit-materials').click();
+            cy.on('window:alert', (alert) => {
+                expect(alert).eq("Select at least one section");
+            });
+        });
+
         it('Should restrict course materials within folders', () => {
             cy.get('[onclick="newUploadCourseMaterialsForm()"]').click();
             cy.get('#all_Sections_Showing_yes').click();
