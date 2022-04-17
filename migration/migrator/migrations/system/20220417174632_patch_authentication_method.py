1f2c325a852e63adb11e0dde5ba4f1b9d366b5d0
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-04-17 18:00:59 -0400

    Patch authentication json

---
+++ migration/migrator/migrations/system/20220417174632_patch_authentication_method.py
@@ -0,0 +1,29 @@
+"""Migration for the Submitty system."""
+from collections import OrderedDict
+import json
+
+def up(config):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    """
+    authentication_file = config.config_path / 'authentication.json'
+    with authentication_file.open('r+') as auth_file:
+        auth_info = json.load(auth_file, object_pairs_hook=OrderedDict)
+        if auth_info['ldap_options'] == []:
+            auth_info['ldap_options'] = {}
+            auth_file.seek(0)
+            auth_file.truncate()
+            auth_file.write(json.dumps(auth_info, indent=4))
+
+
+def down(config):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    """
+    pass
