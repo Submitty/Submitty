ef9b9bdbd09abdb72c88f2e82d7084f20659a841
Akshat Batra <akshatbatra25@gmail.com>
2022-05-25 16:05:25 +0530

    Replaced deprecated usage of PyPDF2 with latest recommendations in test_notes_upload and test_notes_upload_3page

--- more_autograding_examples/test_notes_upload_3page/config/test_input/separate_pages.py
+++ more_autograding_examples/test_notes_upload_3page/config/test_input/separate_pages.py
@@ -7,9 +7,9 @@ from shutil import copyfile
 inputpdf = PdfFileReader(open("student_file.pdf", "rb"), strict=False)
 
 # separate the pages of the student input file
-for i in range(inputpdf.numPages):
+for i in range(len(inputpdf.pages)):
     output = PdfFileWriter()
-    output.addPage(inputpdf.getPage(i))
+    output.add_page(inputpdf.pages[i])
     with open("student_file_page_%s.pdf" % i, "wb") as outputStream:
         output.write(outputStream)
 
@@ -18,5 +18,5 @@ max = int(sys.argv[1])
 
 # if the student submitted fewer than the max number of pages, create
 # a copy of the blank page as a substitute
-for i in range(inputpdf.numPages,max):
+for i in range(len(inputpdf.pages),max):
     copyfile ("blank_page.pdf","student_file_page_%s.pdf" % i)
