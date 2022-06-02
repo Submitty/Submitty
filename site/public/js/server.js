3cad99a6881a6046cc1dbb575e25087d8fee45c5
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2022-06-02 09:45:44 -0400

    Update site/public/js/server.js
    Changed var xhr to const xhr
    
    Co-authored-by: William Allen <16820599+williamjallen@users.noreply.github.com>

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -183,7 +183,7 @@ function loadTestcaseOutput(div_name, gradeable_id, who_id, index, version = '')
     {
         // Checks if xhr is defined and has not succeeded yet
         // If so, abort loading
-        var xhr = output_xhrs.get(orig_div_name);
+        const xhr = output_xhrs.get(orig_div_name);
         if(xhr && xhr.readyState != 4){
             xhr.abort();
             output_xhrs.delete(orig_div_name);
