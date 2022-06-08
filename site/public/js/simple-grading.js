28a2dcd29755cb31e8ea23c5d3e388caab2f3b85
Evan Shi <79226246+Eceptonsu@users.noreply.github.com>
2022-06-08 12:53:53 -0400

    Wording change

--- site/public/js/simple-grading.js
+++ site/public/js/simple-grading.js
@@ -434,8 +434,8 @@ function setupNumericTextCells() {
             }
             // Input greater than the max_clamp for the component is not allowed
             else {
-                if(elem.data("maxclamp") && elem.data("maxclamp") <= this.value) {
-                    alert('Score should be less than the max clamp value: ' + elem.data("maxclamp"));
+                if(elem.data("maxclamp") && elem.data("maxclamp") < this.value) {
+                    alert('Score should be less than or equal to the max clamp value: ' + elem.data("maxclamp"));
                     this.value = 0;
                 }
             }
