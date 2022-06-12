93251ddd2012098c182a1d32b15456eb4a101ebb
Akshat Batra <akshatbatra25@gmail.com>
2022-06-12 22:34:25 +0530

    Database migration

---
+++ migration/migrator/migrations/course/20220612122726_gradeable_specific_anon_ids.py
@@ -0,0 +1,33 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("CREATE TABLE IF NOT EXISTS public.gradeable_anon (user_id character varying NOT NULL, g_id character varying(255) NOT NULL, anon_id character varying(255))")
+    database.execute("INSERT INTO gradeable_anon (SELECT u.user_id, g_id, u.anon_id FROM gradeable g JOIN users u ON 1=1 WHERE NOT EXISTS (SELECT 1 FROM gradeable_anon WHERE user_id=u.user_id AND g_id=g.g_id))")
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    pass
