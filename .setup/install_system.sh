10296bc0ec7dd5d8100a31e81c9d8abd1c8fd4a1
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-06-14 23:20:14 -0400

    [SYSADMIN ACTION][SECURITY][Bugfix:System] Add Course User for Postgres (#8031)
    * Add postgres course user
    
    * Fix for new courses and systems
    
    * Fix superuser check
    
    * Remove down migration
    
    * Fix unit tests
    
    * Fix config test
    
    * Update test setup script
    
    * fix configure submitty
    
    * Update database connection in installer
    
    * Update submitty_ci.yml
    
    Co-authored-by: Barb Cutler <Barb Cutler>

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -194,6 +194,8 @@ fi
 COURSE_BUILDERS_GROUP=submitty_course_builders
 DB_USER=submitty_dbuser
 DATABASE_PASSWORD=submitty_dbuser
+DB_COURSE_USER=submitty_course_dbuser
+DB_COURSE_PASSWORD=submitty_dbuser
 
 #################################################################
 # DISTRO SETUP
@@ -663,6 +665,8 @@ else
         echo -e "/var/run/postgresql
 ${DB_USER}
 ${DATABASE_PASSWORD}
+${DB_COURSE_USER}
+${DB_COURSE_PASSWORD}
 America/New_York
 ${SUBMISSION_URL}
 
@@ -708,6 +712,8 @@ if [ ${WORKER} == 0 ]; then
     # create the submitty_dbuser role in postgres (if it does not yet exist)
     # SUPERUSER privilege is required to use dblink extension (needed for data sync between master and course DBs).
     su postgres -c "psql -c \"DO \\\$do\\\$ BEGIN IF NOT EXISTS ( SELECT FROM  pg_catalog.pg_roles WHERE  rolname = '${DB_USER}') THEN CREATE ROLE ${DB_USER} SUPERUSER LOGIN PASSWORD '${dbuser_password}'; END IF; END \\\$do\\\$;\""
+
+    su postgres -c "psql -c \"DO \\\$do\\\$ BEGIN IF NOT EXISTS ( SELECT FROM  pg_catalog.pg_roles WHERE  rolname = '${DB_COURSE_USER}') THEN CREATE ROLE ${DB_COURSE_USER} LOGIN PASSWORD '${dbcourse_user_password}'; END IF; END \\\$do\\\$;\""
 
     # check to see if a submitty master database exists
     DB_EXISTS=`su -c 'psql -lqt | cut -d \| -f 1 | grep -w submitty || true' postgres`
--- sbin/create_course.sh
+++ sbin/create_course.sh
@@ -29,6 +29,8 @@ DATABASE_HOST=$(jq -r '.database_host' "${CONF_DIR}/database.json")
 DATABASE_PORT=$(jq -r '.database_port' "${CONF_DIR}/database.json")
 DATABASE_USER=$(jq -r '.database_user' "${CONF_DIR}/database.json")
 DATABASE_PASS=$(jq -r '.database_password' "${CONF_DIR}/database.json")
+DATABASE_COURSE_USER=$(jq -r '.database_course_user' "${CONF_DIR}/database.json")
+echo $DATABASE_COURSE_USER
 
 ########################################################################################################################
 ########################################################################################################################
@@ -295,6 +297,18 @@ if [[ "$?" -ne "0" ]] ; then
     echo "ERROR: Failed to create tables within database ${DATABASE_NAME}"
     exit
 fi
+
+PGPASSWORD=${DATABASE_PASS} psql ${CONN_STRING} -d ${DATABASE_NAME} -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${DATABASE_COURSE_USER};"
+if [[ "$?" -ne "0" ]] ; then
+    echo "ERROR: Failed to grant table privileges to course database user"
+    exit
+fi
+PGPASSWORD=${DATABASE_PASS} psql ${CONN_STRING} -d ${DATABASE_NAME} -c "GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO ${DATABASE_COURSE_USER};"
+if [[ "$?" -ne "0" ]] ; then
+    echo "ERROR: Failed to grant sequence privileges to course database user"
+    exit
+fi
+
 PGPASSWORD=${DATABASE_PASS} psql ${CONN_STRING} -d submitty -c "INSERT INTO courses (semester, course, group_name, owner_name)
 VALUES ('${semester}', '${course}', '${ta_www_group}', '${instructor}');"
 if [[ "$?" -ne "0" ]] ; then
