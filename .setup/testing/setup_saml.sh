c03189b11fcf87b95829fa3a00b278e1e9f00445
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-06-16 13:13:19 -0400

    Reorder setup saml script

--- .setup/testing/setup_saml.sh
+++ .setup/testing/setup_saml.sh
@@ -5,8 +5,6 @@ set -ev
 docker run -p 7000:8080 --add-host host.docker.internal:host-gateway \
  -d submitty/docker-test-saml-idp
 
-/usr/local/submitty/sbin/saml_utils.php --add_users
-
 mkdir -p /usr/local/submitty/config/saml/certs
 
 openssl req -x509 -sha256 -days 365 -nodes -newkey rsa:4096 \
@@ -17,6 +15,8 @@ curl http://localhost:7000/simplesaml/saml2/idp/metadata.php \
  --output /usr/local/submitty/config/saml/idp_metadata.xml
 
 chown -R submitty_php:submitty_php /usr/local/submitty/config/saml
+
+/usr/local/submitty/sbin/saml_utils.php --add_users
 
 sed -i -e 's/"name": ""/"name": "SAML"/g' /usr/local/submitty/config/authentication.json
 sed -i -e 's/"username_attribute": ""/"username_attribute": "uid"/g' /usr/local/submitty/config/authentication.json
