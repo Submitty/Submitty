c6f339abcbf5e3a668b643295285649e414d040d
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2022-06-23 02:39:39 -0400

    test: add integration test

--- more_autograding_examples/c_malloc_not_allowed/submissions/sqbrackets.c
+++ more_autograding_examples/c_malloc_not_allowed/submissions/sqbrackets.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 
 int main() {
-    void* this_is_calloc[1] = { calloc(1, 1) };
+    void* this_is_malloc[1] = { malloc(1) };
     // and this is a comment containing malloc(), calloc() and alloca()
-    free (this_is_calloc[0]);
+    free (this_is_malloc[0]);
 
     return 0;
 }
---
+++ tests/integrationTests/tests/c_malloc_not_allowed/__init__.py
@@ -0,0 +1,104 @@
+# Necessary imports. Provides library functions to ease writing tests.
+from lib import prebuild, testcase, SUBMITTY_INSTALL_DIR
+
+import subprocess
+import os
+import glob
+import shutil
+import traceback
+
+############################################################################
+# COPY THE ASSIGNMENT FROM THE SAMPLE ASSIGNMENTS DIRECTORIES
+
+SAMPLE_ASSIGNMENT_CONFIG = os.path.join(SUBMITTY_INSTALL_DIR, "more_autograding_examples",
+                                        "c_malloc_not_allowed", "config")
+SAMPLE_SUBMISSIONS = os.path.join(SUBMITTY_INSTALL_DIR, "more_autograding_examples",
+                                  "c_malloc_not_allowed", "submissions")
+
+
+@prebuild
+def initialize(test):
+    try:
+        os.mkdir(os.path.join(test.testcase_path, "assignment_config"))
+    except OSError:
+        pass
+    subprocess.call(["cp",
+                     os.path.join(SAMPLE_ASSIGNMENT_CONFIG, "config.json"),
+                     os.path.join(test.testcase_path, "assignment_config")])
+
+
+def cleanup(test):
+    subprocess.call(["rm", "-rf",
+                     os.path.join(test.testcase_path, "data")])
+    os.mkdir(os.path.join(test.testcase_path, "data"))
+    os.mkdir(os.path.join(test.testcase_path, "data", "test_output"))
+    subprocess.call(["cp"] +
+                    glob.glob(os.path.join(SAMPLE_ASSIGNMENT_CONFIG, "test_output", "*.txt")) +
+                    [os.path.join(test.testcase_path, "data", "test_output")])
+
+
+@testcase
+def schema_validation(test):
+    cleanup(test)
+    config_path = os.path.join(test.testcase_path, 'assignment_config', 'complete_config.json')
+    try:
+        test.validate_complete_config(config_path)
+    except Exception:
+        traceback.print_exc()
+        raise
+
+
+@testcase
+def malloc(test):
+    cleanup(test)
+    subprocess.call(["cp",
+                     os.path.join(SAMPLE_SUBMISSIONS, "malloc.c"),
+                     os.path.join(test.testcase_path, "data", "malloc.c")])
+    test.run_compile()
+    test.run_run()
+    test.run_validator()
+    test.diff("grade.txt", "grade.txt.malloc", "-b")
+    subprocess.call(["rm", os.path.join(test.testcase_path, "data", "malloc.c")])
+    test.json_diff("results.json", "results.malloc.json")
+
+
+@testcase
+def calloc(test):
+    cleanup(test)
+    subprocess.call(["cp",
+                     os.path.join(SAMPLE_SUBMISSIONS, "calloc.c"),
+                     os.path.join(test.testcase_path, "data", "calloc.c")])
+    test.run_compile()
+    test.run_run()
+    test.run_validator()
+    test.diff("grade.txt", "grade.calloc.txt", "-b")
+    subprocess.call(["rm", os.path.join(test.testcase_path, "data", "calloc.c")])
+    test.json_diff("results.json", "results.calloc.json")
+
+
+@testcase
+def bracket(test):
+    cleanup(test)
+    subprocess.call(["cp",
+                     os.path.join(SAMPLE_SUBMISSIONS, "sqbrackets.c"),
+                     os.path.join(test.testcase_path, "data", "sqbrackets.c")])
+    test.run_compile()
+    test.run_run()
+    test.run_validator()
+    test.diff("grade.txt", "grade.sqbrackets.txt", "-b")
+    subprocess.call(["rm", os.path.join(test.testcase_path, "data", "sqbrackets.c")])
+    test.json_diff("results.json", "results.sqbrackets.json")
+
+
+@testcase
+def does_not_compile(test):
+    cleanup(test)
+    subprocess.call(["cp",
+                     os.path.join(SAMPLE_SUBMISSIONS, "compile_err.c"),
+                     os.path.join(test.testcase_path, "data", "compile_err.c")])
+    test.run_compile()
+    test.run_run()
+    test.run_validator()
+    test.diff("grade.txt", "grade.compile_err.txt", "-b")
+    subprocess.call(["rm", os.path.join(test.testcase_path, "data", "compile_err.c")])
+    test.json_diff("results.json", "results.compile_err.json")
