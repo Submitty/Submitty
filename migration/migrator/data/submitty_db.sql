6896e61d05e18118d03d67a1c9caabe9a8f0efce
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-06-25 11:13:27 -0400

    Update SQL dump

--- migration/migrator/data/submitty_db.sql
+++ migration/migrator/data/submitty_db.sql
@@ -65,9 +65,9 @@ CREATE FUNCTION public.saml_mapping_check() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
         BEGIN
-            IF (SELECT count(*) FROM saml_mapped_users WHERE NEW.user_id = user_id) = 1
+            IF (SELECT count(*) FROM saml_mapped_users WHERE NEW.user_id = user_id) = 2
             THEN
-                IF (SELECT count(*) FROM saml_mapped_users WHERE NEW.user_id = user_id AND user_id = saml_id) = 1
+                IF (SELECT count(*) FROM saml_mapped_users WHERE NEW.user_id = user_id AND user_id = saml_id) > 0
                 THEN
                     RAISE EXCEPTION 'SAML mapping already exists for this user';
                 end if;
@@ -698,7 +698,7 @@ CREATE TRIGGER insert_sync_registration_id AFTER INSERT OR UPDATE ON public.cour
 -- Name: saml_mapped_users saml_mapping_check_trigger; Type: TRIGGER; Schema: public; Owner: -
 --
 
-CREATE TRIGGER saml_mapping_check_trigger BEFORE INSERT OR UPDATE ON public.saml_mapped_users FOR EACH ROW EXECUTE PROCEDURE public.saml_mapping_check();
+CREATE TRIGGER saml_mapping_check_trigger AFTER INSERT ON public.saml_mapped_users FOR EACH ROW EXECUTE PROCEDURE public.saml_mapping_check();
 
 
 --
