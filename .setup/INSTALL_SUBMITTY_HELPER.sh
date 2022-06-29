c4135142ea64dae6a45cd318f3350495d8ade59b
Barb Cutler <Barb Cutler>
2022-06-29 01:15:51 -0400

    tweak

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -888,9 +888,9 @@ done
 # Cleanup Old Email
 
 # Will scan the emails table in the main Submitty database for email
-# receipts that were successfully sent (with no errors) and delete
-# them from the table.  A maximum of 10,000 email receipts will be
-# deleted.
+# receipts that were successfully sent at least 360 days ago, with no
+# errors, and delete them from the table.  A maximum of 10,000 email
+# receipts will be deleted.
 "${SUBMITTY_INSTALL_DIR}/sbin/cleanup_old_email.py" 360 10000
 
 
