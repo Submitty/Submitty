005df25bca6412b244bf04f4a1e7be13bd627b64
Akshat Batra <akshatbatra25@gmail.com>
2022-07-02 19:11:23 +0530

    updated selenium test

--- tests/e2e/test_sidebar.py
+++ tests/e2e/test_sidebar.py
@@ -215,8 +215,7 @@ class TestSidebar(BaseTestCase):
             [base_url + '/notifications', 'Notifications'],
             [base_url + '/office_hours_queue', 'Office Hours Queue'],
             [base_url + '/polls', 'Submini Polls'],
-            # sample course has no course materials to start, so this link will not appear
-            # [base_url + '/course_materials', 'Course Materials'],
+            [base_url + '/course_materials', 'Course Materials'],
             [base_url + '/forum', 'Discussion Forum'],
             [base_url + '/student_photos', 'Student Photos'],
             [base_url + '/late_table', 'My Late Days/Extensions'],
@@ -270,8 +269,7 @@ class TestSidebar(BaseTestCase):
             [base_url + '/notifications', 'Notifications'],
             [base_url + '/office_hours_queue', 'Office Hours Queue'],
             [base_url + '/polls', 'Submini Polls'],
-            # sample course has no course materials in start, so this link will not appear
-            # [base_url + '/course_materials', 'Course Materials'],
+            [base_url + '/course_materials', 'Course Materials'],
             [base_url + '/forum', 'Discussion Forum'],
             [base_url + '/late_table', 'My Late Days/Extensions'],
             [self.test_url + '/home', 'My Courses'],
