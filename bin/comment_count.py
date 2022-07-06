a0a0692c243ce1572e81cc18cdc4be70b51a05a6
poorna2152 <58025190+poorna2152@users.noreply.github.com>
2022-07-06 10:04:02 +0530

    Fix linting

--- bin/comment_count.py
+++ bin/comment_count.py
@@ -7,13 +7,13 @@ import glob
 nArgs = len(sys.argv)
 if (nArgs < 2):
     raise Exception("Need at least one file")
-files =[]
+files = []
 for i in range(1, nArgs):
     path = os.path.join(os.getcwd(), sys.argv[i])
     files.extend(glob.glob(path))
 if (not len(files)):
     raise Exception("No matching files found for the input pattern")
-command =  "cloc --csv --quiet " + " ".join(files)
+command = "cloc --csv --quiet " + " ".join(files)
 result = subprocess.check_output(command, shell=True).splitlines()
 if (len(result)):
     # last line of the output contains the comment lines count
