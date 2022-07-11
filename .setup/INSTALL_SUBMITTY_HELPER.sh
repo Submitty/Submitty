4e7a8ae51607ed242487ede7da7fb072142e34ed
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2022-07-11 16:44:16 -0400

    fix: vagrant up

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -45,6 +45,7 @@ SUBMITTY_REPOSITORY=$(jq -r '.submitty_repository' "${CONF_DIR}/submitty.json")
 SUBMITTY_INSTALL_DIR=$(jq -r '.submitty_install_dir' "${CONF_DIR}/submitty.json")
 WORKER=$([[ $(jq -r '.worker' "${CONF_DIR}/submitty.json") == "true" ]] && echo 1 || echo 0)
 
+export SUBMITTY_INSTALL_DIR
 source "${THIS_DIR}/bin/versions.sh"
 
 if [ "${WORKER}" == 0 ]; then
