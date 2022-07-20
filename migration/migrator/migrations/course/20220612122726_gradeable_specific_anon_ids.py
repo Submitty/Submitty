007142d7d48c5f2f85f4c0d41b9c3fa2f2e1b0e1
Akshat Batra <akshatbatra25@gmail.com>
2022-07-20 15:37:19 +0530

    drop anon_id from users

--- migration/migrator/migrations/course/20220612122726_gradeable_specific_anon_ids.py
+++ migration/migrator/migrations/course/20220612122726_gradeable_specific_anon_ids.py
@@ -14,8 +14,9 @@ def up(config, database, semester, course):
     :param course: Code of course being migrated
     :type course: str
     """
-    database.execute("CREATE TABLE IF NOT EXISTS gradeable_anon (user_id character varying NOT NULL, g_id character varying(255) NOT NULL, anon_id character varying(255))")
-    database.execute("INSERT INTO gradeable_anon (SELECT u.user_id, g_id, u.anon_id FROM gradeable g JOIN users u ON 1=1 WHERE NOT EXISTS (SELECT 1 FROM gradeable_anon WHERE user_id=u.user_id AND g_id=g.g_id))")
+    database.execute("CREATE TABLE IF NOT EXISTS gradeable_anon (user_id character varying NOT NULL, g_id character varying(255) NOT NULL, anon_id character varying(255));")
+    database.execute("INSERT INTO gradeable_anon (SELECT u.user_id, g_id, u.anon_id FROM gradeable g JOIN users u ON 1=1 WHERE NOT EXISTS (SELECT 1 FROM gradeable_anon WHERE user_id=u.user_id AND g_id=g.g_id));")
+    database.execute("ALTER TABLE users DROP COLUMN anon_id;")
 
 def down(config, database, semester, course):
     """
@@ -30,4 +31,4 @@ def down(config, database, semester, course):
     :param course: Code of course being migrated
     :type course: str
     """
-    database.execute('DROP TABLE IF EXISTS gradeable_anon;')
+    pass
