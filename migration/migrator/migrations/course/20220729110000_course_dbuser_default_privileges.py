2ea054ec01e3d38e6d12a811775f651191f61e47
Akshat Batra <akshatbatra25@gmail.com>
2022-08-01 21:32:38 +0530

    remove down migration

--- migration/migrator/migrations/course/20220729110000_course_dbuser_default_privileges.py
+++ migration/migrator/migrations/course/20220729110000_course_dbuser_default_privileges.py
@@ -25,18 +25,5 @@ def up(config, database, semester, course):
 def down(config, database, semester, course):
     """
     Run down migration (rollback).
-
-    :param config: Object holding configuration details about Submitty
-    :type config: migrator.config.Config
-    :param database: Object for interacting with given database for environment
-    :type database: migrator.db.Database
-    :param semester: Semester of the course being migrated
-    :type semester: str
-    :param course: Code of course being migrated
-    :type course: str
     """
-    database_file = config.config_path / 'database.json'
-    with(database_file).open('r') as db_file:
-        db_info = json.load(db_file, object_pairs_hook=OrderedDict)
-        database.execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT, INSERT, UPDATE, DELETE ON TABLES FROM {}".format(db_info['database_course_user']))
-        database.execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT, UPDATE ON SEQUENCES FROM {}".format(db_info['database_course_user']))
+    pass
