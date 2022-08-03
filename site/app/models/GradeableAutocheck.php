3ff0241e47789937a13900b22e8899d19058bf51
Evan Shi <79226246+Eceptonsu@users.noreply.github.com>
2022-08-03 02:32:12 -0400

    Php linting

--- site/app/models/GradeableAutocheck.php
+++ site/app/models/GradeableAutocheck.php
@@ -81,18 +81,14 @@ class GradeableAutocheck extends AbstractModel {
         }
 
         if (isset($details["use_expected_string"])) {
-            if (
-                isset($details["expected_file"]) 
-                && substr($details["expected_file"], 0, 11) == "test_output" 
-                && file_exists($results_path . "/details/test_output/" . end(explode("/", $details["expected_file"])))
-                ) {
+            if (isset($details["expected_file"])
+                && substr($details["expected_file"], 0, 11) == "test_output"
+                && file_exists($results_path . "/details/test_output/" . end(explode("/", $details["expected_file"])))) {
                 $expected_file = $results_path . "/details/test_output/" . end(explode("/", $details["expected_file"]));
-            }
-            else {
+            } else {
                 $this->core->addErrorMessage("Auto-generated expected file not found.");
             }
-        }
-        elseif (isset($details["expected_file"])) {
+        } elseif (isset($details["expected_file"])) {
             if (substr($details["expected_file"], 0, 11) == "test_output") {
                 if (file_exists($course_path . "/" . $details["expected_file"])) {
                     $expected_file = $course_path . "/" . $details["expected_file"];
