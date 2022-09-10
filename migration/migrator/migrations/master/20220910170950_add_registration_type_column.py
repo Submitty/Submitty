1b2229b8e1a1d27dd83bce7c86d45a3f87bca753
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2022-09-10 17:22:55 -0400

    started migration

---
+++ migration/migrator/migrations/master/20220910170950_add_registration_type_column.py
@@ -0,0 +1,31 @@
+"""Migration for the Submitty master database."""
+
+
+def up(config, database):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    database.execute("""
+ALTER TABLE public.courses_users
+ADD registration_type varying(255);
+""")
+
+
+def down(config, database):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    database.execute("""
+ALTER TABLE public.courses_users
+DROP COLUMN registration_type;
+""")
