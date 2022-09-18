d98a587218d1431756b788c1e20e8305546fc45d
William Allen <william.j.allen24@gmail.com>
2022-09-17 22:22:28 -0400

    Update plagiarism.js

--- site/public/js/plagiarism.js
+++ site/public/js/plagiarism.js
@@ -341,7 +341,13 @@ function requestAjaxData(url, f, es) {
     $.ajax({
         url: url,
         success: function(data) {
-            data = JSON.parse(data);
+            try {
+                data = JSON.parse(data);
+            }
+            catch (e) {
+                console.log(data);
+                throw e;
+            }
             if (data.status !== 'success') {
                 alert(data.message);
                 return;
