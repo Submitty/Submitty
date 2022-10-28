a9a9c223989d10d8e4af344dad6e597bc11dd642
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2022-10-28 19:37:06 -0400

    simple query to get the trigger to fire

---
+++ migration/migrator/migrations/master/20221028192745_registration_section_main_to_course.py
@@ -0,0 +1,27 @@
+"""Migration for the Submitty master database."""
+
+
+def up(config, database):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    database.execute("""
+UPDATE courses_users SET registration_type=registration_type;
+    """)
+
+
+def down(config, database):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    """
+    pass
