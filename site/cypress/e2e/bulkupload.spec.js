9c6419dd194938c48ad708fd935dbafaef56ad56
Evan Shi <79226246+Eceptonsu@users.noreply.github.com>
2022-11-07 17:02:06 -0500

    Remove bulk testing for normal gradeable

--- site/cypress/e2e/bulkupload.spec.js
+++ site/cypress/e2e/bulkupload.spec.js
@@ -11,11 +11,6 @@ describe('Test cases revolving around bulk uploading', () => {
             cy.get('#radio-student').click();
             cy.get('#user_id').should('be.visible');
             cy.get('#submission-mode-warning > .warning').should('have.text', 'Warning: Submitting files for a student!');
-
-            cy.get('#radio-bulk').click();
-            cy.get('#use-qr').should('be.visible');
-            cy.get('#num_pages').should('be.visible');
-            cy.get('#submission-mode-warning > .warning').should('have.text', 'Warning: Submitting files for bulk upload!');
         });
 
     });
