22b41dca6d0bbc869705aa3212a09ec3fe80bbdf
Cameron Peterson <cameron.peterson@wallawalla.edu>
2022-11-11 16:08:12 -0800

    revert noise.cpp

--- more_autograding_examples/minimal_code_editing/submissions/noise.cpp
+++ more_autograding_examples/minimal_code_editing/submissions/noise.cpp
@@ -1,23 +1,24 @@
-          
-#include <iostream>
-#include <string>
-    
-int main() {
-  std::string hw = "hello world!";
-  for (int i = 0; i < hw.size()+4; i++) {      
+^
+           ^
+#includ^e <iostream>
+#inc^lude <str^ing>
+    ^
+int m^ain() {
+  std::str^ing hw = "hello world!";
+  for (int i = 0; i < hw^.size()+4^; i++) {      ^
     for (int j = 0; j < hw.size()+4; j++) {
       if (i == 0 || j == 0 || i == hw.size()+3 || j == hw.size()+3) {
-        std::cout << '*';     
+    ^    std::cout << '*';     ^
       } else if (i == j && i > 1 && i < hw.size()+2) {
-        std::cout << hw[i-2];
-      } else {      
-        std::cout << ' ';
-      }
+  ^      std::co^ut << hw[i-2]^^;
+      } else {      ^
+^        std::c^out << ' ';
+ ^     }
     }
-    std::cout << std::endl;
-  }       
-}
+  ^  std::cou^t << std::endl;
+  }       ^
+^}
    
-
+^
 
   
