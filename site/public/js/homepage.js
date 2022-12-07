b8e7627e34f719ee1d0b45b6f07ea171acee9620
marymx <marymx@umich.edu>
2022-12-07 17:30:08 -0500

    comments

--- site/public/js/homepage.js
+++ site/public/js/homepage.js
@@ -13,11 +13,11 @@ function getCurrentUTCOffset() {
     return `${sign + hours}:00`;
 }
 
+// once the homepage loads, does a timezone check against the user's local timezone and
+//if the user set a timezone,  warning will pop up if there is a timezone mismatch
 window.addEventListener('load', function () {
     console.log("home page loaded");
-    console.log(buildUrl(['home', 'get_user_time_zone']));
-    //works
-    const temp = 1234;
+
     $.getJSON({
         type: 'GET',
         // eslint-disable-next-line no-undef
@@ -25,7 +25,7 @@ window.addEventListener('load', function () {
         data: {
             // eslint-disable-next-line no-undef
             csrf_token: csrfToken,
-            temp,
+            
 
         },
         success: function (response) {
@@ -33,31 +33,24 @@ window.addEventListener('load', function () {
             if (response.status === 'success') {
                 const users_utc = response.data.utc_offset;
                 const current_offset = getCurrentUTCOffset();
-                // $('#time_zone_selector_label').attr('data-user_time_zone', response.data.user_time_zone_with_offset);
-                // eslint-disable-next-line no-undef
-                // displaySuccessMessage('Time-zone updated succesfully!!');
-
+            
                 // Check user's current time zone, give a warning message if the user's current time zone differs from systems' time-zone
-                // const offset = getCurrentUTCOffset();
+                
                 if (users_utc !==current_offset) {
-                //console.log(offset);
+                
                 // eslint-disable-next-line no-undef
                 displayWarningMessage('The set time-zone on your profile does not match system time-zone. Please update to prevent any issues!');
                 
                  }
             }
             else {
-                console.log("error1");
                 console.log(response);
-                // eslint-disable-next-line no-undef
-                // displayErrorMessage('Time-zone is not updated!');
+            
             }
         },
         error: function (response) {
-            // console.error('Failed to parse response from server!');
+        
             console.log(response);
-            // eslint-disable-next-line no-undef
-            //displayErrorMessage('Failed to parse response from server!');
            
         },
     });
