3d94e79f5f4883eba6ff30eee5c84e8027aa30e8
William Allen <william.j.allen24@gmail.com>
2023-02-07 22:09:00 -0500

    Update baseline to ignore errors for columns without join columns specified

--- site/app/entities/course/CourseMaterialAccess.php
+++ site/app/entities/course/CourseMaterialAccess.php
@@ -21,7 +21,7 @@ class CourseMaterialAccess {
     protected $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="\app\entities\course\CourseMaterial", inversedBy="accesses", nullable=false)
+     * @ORM\ManyToOne(targetEntity="\app\entities\course\CourseMaterial", inversedBy="accesses")
      * @var CourseMaterial
      */
     protected $course_material;
--- site/app/entities/plagiarism/PlagiarismRunAccess.php
+++ site/app/entities/plagiarism/PlagiarismRunAccess.php
@@ -24,7 +24,7 @@ class PlagiarismRunAccess {
     protected $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="app\entities\plagiarism\PlagiarismConfig", inversedBy="access_times", nullable=false)
+     * @ORM\ManyToOne(targetEntity="app\entities\plagiarism\PlagiarismConfig", inversedBy="access_times")
      * @var PlagiarismConfig
      */
     protected $lichen_run;
