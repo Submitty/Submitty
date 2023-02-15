9cfd80d81d2e99817deb15cb4e1412b235dae431
jason490 <116290832+jason490@users.noreply.github.com>
2023-02-14 19:01:41 -0500

    Removed accidental php changes

--- site/app/controllers/admin/UsersController.php
+++ site/app/controllers/admin/UsersController.php
@@ -636,13 +636,7 @@ class UsersController extends AbstractController {
             }
             $curr_section_sizes = $this->core->getQueries()->getUsersCountByRotatingSections();
             $section_assignment_counts = array_map(function ($expected_size, $curr_size) {
-                if ($curr_size === NULL){
-                    $curr_size = 0;
-                    return $expected_size - $curr_size;
-                }else{
-                    return $expected_size - $curr_size['count'];
-                }
-                
+                return $expected_size - $curr_size['count'];
             }, $expected_section_sizes, $curr_section_sizes);
         }
         // distribute unassigned users to rotating sections using the $section_assigment_counts array
