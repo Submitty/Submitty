ec5ac3633c362c32fdef6036887bbc41d06f0b27
jason490 <116290832+jason490@users.noreply.github.com>
2023-02-14 19:11:56 -0500

    Prevents the data containing NULL values

--- site/app/controllers/admin/UsersController.php
+++ site/app/controllers/admin/UsersController.php
@@ -636,7 +636,11 @@ class UsersController extends AbstractController {
             }
             $curr_section_sizes = $this->core->getQueries()->getUsersCountByRotatingSections();
             $section_assignment_counts = array_map(function ($expected_size, $curr_size) {
-                return $expected_size - $curr_size['count'];
+                if ($curr_size === NULL) {
+                    return $expected_size;
+                }else {
+                    return $expected_size - $curr_size['count'];
+                }
             }, $expected_section_sizes, $curr_section_sizes);
         }
         // distribute unassigned users to rotating sections using the $section_assigment_counts array
