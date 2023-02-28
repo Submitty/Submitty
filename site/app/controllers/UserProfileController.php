36c5a58c9d9cd928c5fe3ff0eb2600f121622ecc
Mahi <pasarkarmahi@gmail.com>
2023-02-28 17:12:23 -0500

    [Feature:Students] Allow students to input blank pronouns

--- site/app/controllers/UserProfileController.php
+++ site/app/controllers/UserProfileController.php
@@ -106,25 +106,20 @@ class UserProfileController extends AbstractController {
      */
     public function changePronouns() {
         $user = $this->core->getUser();
-        if (!empty($_POST['pronouns'])) {
-            $newPronouns = trim($_POST['pronouns']);
+        $newPronouns = trim($_POST['pronouns']);
 
-            //validPronouns() checks for valid option
-            if ($user->validateUserData('user_pronouns', $newPronouns) === true) {
-                $user->setPronouns($newPronouns);
-                $user->setUserUpdated(true);
-                $this->core->getQueries()->updateUser($user);
-                return JsonResponse::getSuccessResponse([
-                    'message' => "Pronouns updated successfully",
-                    'pronouns' => $newPronouns
-                ]);
-            }
-            else {
-                return JsonResponse::getErrorResponse("Pronouns is not valid");
-            }
+        //validPronouns() checks for valid option
+        if ($user->validateUserData('user_pronouns', $newPronouns) === true) {
+            $user->setPronouns($newPronouns);
+            $user->setUserUpdated(true);
+            $this->core->getQueries()->updateUser($user);
+            return JsonResponse::getSuccessResponse([
+                'message' => "Pronouns updated successfully",
+                'pronouns' => $newPronouns
+            ]);
         }
         else {
-            return JsonResponse::getErrorResponse('Pronouns cannot be empty!');
+            return JsonResponse::getErrorResponse("Pronouns is not valid");
         }
     }
 
