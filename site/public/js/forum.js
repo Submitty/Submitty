aff1990a6b547b4c728f3a8043210e974cbe59a3
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-03-04 06:52:12 -0500

    Revert "attempt 2"
    This reverts commit 239a6f0d893fc290168a1f2b2f3d7a0c709ac3ab.

--- site/public/js/forum.js
+++ site/public/js/forum.js
@@ -674,7 +674,7 @@ function initSocketClient() {
                 break;
             case 'new_post':
                 // eslint-disable-next-line eqeqeq
-                $('data#current-thread').style.backgroundColor = "red";
+                $('data#current-thread').classList.add("new_thread");
                 if ($('data#current-thread').val() == msg.thread_id) {
                     socketNewOrEditPostHandler(msg.post_id, msg.reply_level, msg.post_box_id);
                 }
