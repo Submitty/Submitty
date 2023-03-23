b7126e55a4d0ef19977617449453ca44f8c13727
Mahi <pasarkarmahi@gmail.com>
2023-03-22 20:17:14 -0400

    [Feature:InstructorUI] created popup form for toggling columns
    Body of form not implemented yet

--- site/public/js/manage-students.js
+++ site/public/js/manage-students.js
@@ -2,12 +2,13 @@
 
 //opens modal with initial settings for new student
 function toggleColumnsForm() {
-    alert("toggle columns!");
-    /*
     $('.popup-form').css('display', 'none');
-    var form = $("#edit-user-form");
+    var form = $("#toggle-columns-form");
     form.css("display", "block");
     form.find('.form-body').scrollTop(0);
-    captureTabInModal("edit-user-form");
-    */
+    captureTabInModal("toggle-columns-form");
+}
+
+function updateManageStudentsColumns() {
+    alert("Update Columns");
 }
