5154fd5d15639ae231064ddce2e3309aeeedc4de
jason490 <116290832+jason490@users.noreply.github.com>
2023-03-23 21:13:26 -0400

    Added general solution
    Hides files by passing in a list of file names to hide.

--- site/app/views/submission/HomeworkView.php
+++ site/app/views/submission/HomeworkView.php
@@ -999,12 +999,34 @@ class HomeworkView extends AbstractView {
             }
 
             $files = $version_instance->getFiles();
+            $combinedfiles = array_merge($files['submissions'], $files['checkout']);
+            $notebook_data = null;
+            $showfiles = array();
+            if ($autograding_config->isNotebookGradeable()){
+                $notebook_model = $autograding_config->getUserSpecificNotebook($this->core->getUser()->getId());
 
+                $notebook = $notebook_model->getNotebook();
+                if ($graded_gradeable !== null) {
+                    $notebook_data = $notebook_model->getMostRecentNotebookSubmissions(
+                        $graded_gradeable->getAutoGradedGradeable()->getHighestVersion(),
+                        $notebook,
+                        $this->core->getUser()->getId(),
+                        $version_instance !== null ? $version_instance->getVersion() : 0,
+                        $graded_gradeable->getGradeableId()
+                    );
+                }
+                foreach($notebook_data as $note){
+                    if(array_key_exists('filename', $note)){
+                        array_push($showfiles,$note['filename']);
+                    }
+                }
+            }
             $param = array_merge($param, [
+                'notebook' => $showfiles,
                 'submission_time' => DateUtils::dateTimeToString($version_instance->getSubmissionTime()),
                 'days_late' => $version_instance->getDaysLate(),
                 'num_autogrades' => $version_instance->getHistoryCount(),
-                'files' => array_merge($files['submissions'], $files['checkout']),
+                'files' => $combinedfiles,
                 'display_version_days_late' => $version_instance->getDaysLate(),
                 'autograder_machine' => $version_instance->getAutograderMachine(),
             ]);
