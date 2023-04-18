4f693dfa867f134e0354aacd710a061e453a434e
jason490 <116290832+jason490@users.noreply.github.com>
2023-04-18 14:07:26 -0400

    Added more tests and fixed some of the tests

--- site/cypress/e2e/gradeables/edit_gradeable.spec.js
+++ site/cypress/e2e/gradeables/edit_gradeable.spec.js
@@ -2,10 +2,10 @@ describe('Tests cases revolving around modifying gradeables', () => {
     beforeEach(() => {
         cy.visit('/');
         cy.login('instructor');
-    });
-    it('Should make sure edit gradeables does not contain errors', () => {
         cy.visit(['sample','gradeable','open_peer_homework','update']);
-
+    });
+    
+    it('Should test settings page 0-2', () => {
         cy.get('#no_ta_grade').click();
         cy.get('#discussion_grading_enable_container').should('not.be.visible');
         cy.get('#yes_ta_grade').click();
@@ -16,10 +16,12 @@ describe('Tests cases revolving around modifying gradeables', () => {
         cy.get('#no_student_view').click();
         cy.get('#student_download_view').should('not.be.visible');
         cy.get('#student_submit_view').should('not.be.visible');
+        cy.get('#no_student_view_after_grades').click();
+
         cy.get('#yes_student_download').click();
         cy.get('#yes_student_submit').click();
 
-        cy.get('#gradeable-lock').contain('Select prerequisite gradeable (Off)');
+        cy.get('#gradeable-lock').contains('Select prerequisite gradeable (Off)');
 
         //Change to button when the link changes to button
         cy.get('.settings > a').click();
@@ -37,15 +39,17 @@ describe('Tests cases revolving around modifying gradeables', () => {
         cy.get('#pdf_page').should('not.be.visible');
         cy.get('[value="Add New Component"]').click();
 
-        cy.get('#grade_by_count_down_120').click();
-        cy.get('[value="Add New Mark"]').click();
+        // cy.get('#grade_by_count_down_118').click();
+        cy.get('[value="Add New Mark"]').eq(-1).click();
 
-        cy.get('[title="Delete this component"]').click();
-        cy.on('window:confirm', (str) =>{
-            expect(str).to.equal('Are you sure you want to delete this component?');
+        cy.get('[title="Delete this component"]').eq(-1).click().then(()=>{
+            cy.on('window:confirm', (str) =>{
+                expect(str).to.equal('Are you sure you want to delete this component?');
+            });
         });
-        cy.on('window:confirm', () => true);
+    });
 
+    it('Should test settings page 3-5', ()=>{
         cy.get('#page_3_nav').click();
         cy.get('#all_access').click();
         cy.get('#registration_section').click();
@@ -53,43 +57,103 @@ describe('Tests cases revolving around modifying gradeables', () => {
         cy.get('#page_4_nav').click();
         cy.get('#peer_graders_list').should('exist');
         
-        cy.get('#clear_peer_matrix').click();
-
-        cy.on('window:confirm', (str) =>{
-            expect(str).to.equal('Each student grades every other student! Continue?');
+        cy.get('#clear_peer_matrix').click().then(()=>{
+            cy.on('window:confirm', () => false);
         });
-        cy.on('window:confirm', () => false);
-
-        cy.on('window:confirm', (str) =>{
-            expect(str).to.equal('This will update peer matrix. Are you sure?');
-        });
-        cy.on('window:confirm', () => false);
 
         cy.get('#download_peer_csv').click();
 
-        const newFile = after.filter(file => !before.includes(file))[0]
-        cy.readFile('cypress/downloads/' + newFile);
         cy.get('#hidden_files').should('exist');
         cy.get('#add-peer-grader').click();
-        cy.get('.form-button-container > a').click();
-
-        cy.get('#has_due_date_no').click();
-
-        cy.get('#has_due_date_yes').click();
-
-        cy.get('has_release_date_no').click();
-        cy.get('has_release_date_yes').click();
-
-        cy.get('#no_late_submission').click();
-        cy.get('#yes_late_submission').click();
+        cy.get('.form-button-container > a').click({force:true,multiple:true});
 
         cy.get('#page_5_nav').click();
+        cy.get('#has_due_date_no').click();
+        //Check for hidden elements
+        cy.get('#has_due_date_yes').click();
+        //check if the hidden elements are back
 
+        cy.get('#has_release_date_yes').click();
+        //check for hidden elements
+        cy.get('#has_release_date_no').click();
+        //check for the hidden elements are back
+
+        cy.get('#no_late_submission').click();
+        //check for hidden elements
+        cy.get('#yes_late_submission').click();
+        //check for elements
         //Goes back to general
         cy.get('#page_0_nav').click();
     });
 
-    it('Should verify changed settings in edit gradeables', ()=> {
+    it('Should test the change of settings in student view', ()=> {
+        cy.get('#page_1_nav').click();
+        cy.get('#no_student_view').click();
+        //student should not be able to see open_peer_homework
+
+        cy.login('student');
+        cy.visit('sample');
+        cy.get('#open_peer_homework').should('not.exist');
+
+        cy.login('instructor');
+        cy.visit(['sample','gradeable','open_peer_homework','update']);
+        cy.get('#page_1_nav').click();
+        cy.get('#yes_student_view').click();
+
+        cy.login('student');
+        cy.visit(['sample','gradeable','open_peer_homework']);
+
+        cy.get('#upload1').should('not.exist');
+        cy.get('#submission-version-select').should('not.exist');
+        cy.contains('Submissions are no longer being accepted for this assignment').should('exist');
+        cy.contains('No submissions for this assignment.').should('exist');
+        
+        //Makes sure we can undo the setting change
+        cy.login('instructor');
+        cy.visit(['sample','gradeable','open_peer_homework','update']);
+        cy.get('#yes_student_download').click();
+        cy.get('#yes_student_submit').click();
+
+        cy.login('student');
+        cy.visit(['sample','gradeable','open_peer_homework']);
+        cy.get('#upload1').should('exist');
+        cy.get('#submission-version-select').should('exist');
+        cy.contains('Submissions are no longer being accepted for this assignment').should('not.exist');
+        cy.contains('No submissions for this assignment.').should('not.exist');
 
     });
+    
+    // it('Should test locking the gradeable',() =>{
+    //     //testing 
+    //     cy.login('instructor');
+    //     cy.visit(['sample','gradeable','open_peer_homework','update']);
+    //     cy.get('#page_1_nav').click();
+    //     //Locking the gradeable
+    //     cy.get('[value="open_homework"]').click();
+    //     //unlocking the gradeable
+    //     cy.get('[value=""]').click();
+    //     //Relocks the gradeable
+    //     cy.get('[value="open_homework"]').click();
+
+    //     ['student','grader','ta'].forEach((user) => {
+    //         cy.login(user);
+
+    //     });
+
+    //     cy.login('instructor');
+    //     cy.visit(['sample','gradeable','open_peer_homework','update']);
+    //     cy.get('#page_1_nav').click();
+    //     cy.get('[value=""]').click();
+        
+    //     ['student','grader','ta'].forEach((user) => {
+    //         cy.login(user);
+    //         if (user === 'ta'){
+
+    //         }
+    //         if (user === 'student'){
+
+    //         }
+    //     });
+
+    // });
 });
