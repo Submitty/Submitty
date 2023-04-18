1a42447511d524297b2f828fd84ec064cbcd6e16
Mahi <pasarkarmahi@gmail.com>
2023-04-18 18:05:20 -0400

    [Bugfix:UpdateUser] progress :(

--- site/public/js/manage-students.js
+++ site/public/js/manage-students.js
@@ -1,16 +1,31 @@
 
 
 //Data structure for active columns
+let activeColumns = new Array(10).fill(1);
 
 //opens modal with initial settings for new student
 function toggleColumnsForm() {
-    $('.popup-form').css('display', 'none');
+    //loadColumns();
+
+    //$('.popup-form').css('display', 'none');
     var form = $("#toggle-columns-form");
     form.css("display", "block");
-    form.find('.form-body').scrollTop(0);
-    captureTabInModal("toggle-columns-form");
+    //form.find('.form-body').scrollTop(0);
+    //captureTabInModal("toggle-columns-form");
 }
 
 function updateManageStudentsColumns() {
+    //saveColumns();
+
     alert("Update Columns");
+}
+
+//Cookies (loading and storing)
+function saveColumns() {
+    //$.cookie('activeColumns', JSON.stringify(activeColumns));
+    //document.cookie = `active_columns=${encodeURIComponent(JSON.stringify(activeColumns))}`;
+}
+
+function loadColumns() {
+    return JSON.parse($.cookie('activeColumns'));
 }
