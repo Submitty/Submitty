4f930bdf49738a2edf45c0cbd1e2f9f113e8507f
Cameron Peterson <reapernsgaming@gmail.com>
2023-05-14 14:07:50 -0700

    remove commented out and re-add master

--- autograder/tests/test_submitty_autograding_shipper.py
+++ autograder/tests/test_submitty_autograding_shipper.py
@@ -75,8 +75,8 @@ class TestAutogradingShipper(unittest.TestCase):
     def tearDownClass(cls):
         """ Tear down the mock environment for these testcases. """
         # Remove the test environment.
-        # with contextlib.suppress(FileNotFoundError):
-        #     shutil.rmtree(WORKING_DIR)
+        with contextlib.suppress(FileNotFoundError):
+            shutil.rmtree(WORKING_DIR)
 
     @classmethod
     def setUpClass(cls):
@@ -182,7 +182,7 @@ class TestAutogradingShipper(unittest.TestCase):
         # Initialize git homework directory
         create_git_repository = """
         cd {}/homework_01;
-        git init;
+        git init --initial-branch "master";
         git config user.email "test@email.com";
         git config user.name "Test Shipper";
         git add -A;
