f635e48e6a4a2956d130b5d795432d8e89c8822c
Mahi <pasarkarmahi@gmail.com>
2023-05-17 11:44:48 -0400

    Linting

--- .setup/bin/randomgen.py
+++ .setup/bin/randomgen.py
@@ -3,19 +3,25 @@
 class Rand48(object):
     def __init__(self, seed):
         self.n = seed
+
     def seed(self, seed):
         self.n = seed
+
     def srand(self, seed):
         self.n = (seed << 16) + 0x330e
+
     def next(self):
         self.n = (25214903917 * self.n + 11) & (2**48 - 1)
         return self.n
+
     def drand(self):
         return self.next() / 2**48
+
     def lrand(self):
         return self.next() >> 17
+
     def mrand(self):
         n = self.next() >> 16
         if n & (1 << 31):
             n -= 1 << 32
-        return n   
+        return n  
