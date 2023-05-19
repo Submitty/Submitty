4ac151ff976ca16c1dc37b963bcca67539aece28
Mahi <pasarkarmahi@gmail.com>
2023-05-19 09:59:26 -0400

    Fixed typo bug

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -2002,7 +2002,7 @@ ORDER BY {$u_or_t}.{$section_key}",
         $params = [$g_id,$g_id];
         $where = "";
         if (count($sections) > 0) {
-            $where = "WHERE {$section_key} IN " . $this->createParamaterList(count($sections));
+            $where = "WHERE {$section_key} IN " . $this->createParameterList(count($sections));
             $params = array_merge($params, $sections);
         }
         $this->course_db->query(
@@ -3754,7 +3754,7 @@ ORDER BY {$section_key}",
         if (count($sections) > 0) {
             // Expand out where clause
             $sections_keys = array_values($sections);
-            $placeholders = $this->createParamaterList(count($sections_keys));
+            $placeholders = $this->createParameterList(count($sections_keys));
             $where = "WHERE {$section_key} IN {$placeholders}";
             $params = array_merge($params, $sections_keys);
         }
