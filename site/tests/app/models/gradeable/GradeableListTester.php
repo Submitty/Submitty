20029889fd244e6cd111b818ba1da278eb0552e3
Cameron Peterson <46759635+IDzyre@users.noreply.github.com>
2023-05-22 11:29:11 -0700

    Remove unused variable pt 2

--- site/tests/app/models/gradeable/GradeableListTester.php
+++ site/tests/app/models/gradeable/GradeableListTester.php
@@ -777,7 +777,6 @@ class GradeableListTester extends BaseUnitTest {
             'autograding_config_path' => '/path/to/autograding',
             'vcs' => false,
             'vcs_subdirectory' => '',
-            'is_subdirectory' => false,
             'vcs_partial_path' => '',
             'vcs_host_type' => -1,
             'team_assignment' => false,
