f5239b2bfad1ab880f2f22b7925dbefc3bca33f9
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-02 08:05:00 -0700

    Revert "Merge branch 'main' into plagiarism-entity-test"
    This reverts commit f7f3f1453e15241d4bf118ae005e324c5486c53f, reversing
    changes made to 4809a14364b8f30cb92460264511de11dfcf6a68.
    
    Revert "Revert "Merge branch 'main' into plagiarism-entity-test""
    
    This reverts commit c5dcb7f0e7e74f7a87f0fb7833b6bb6a292b63c1.
    
    Revert "Merge branch 'main' into plagiarism-entity-test"
    
    This reverts commit f7f3f1453e15241d4bf118ae005e324c5486c53f, reversing
    changes made to 4809a14364b8f30cb92460264511de11dfcf6a68.

--- site/app/views/LateDaysTableView.php
+++ site/app/views/LateDaysTableView.php
@@ -10,6 +10,7 @@ class LateDaysTableView extends AbstractView {
         $preferred_name = $late_days->getUser()->getDisplayFullName();
         $table_data = $this->core->getOutput()->renderTwigTemplate('LateDaysTablePlugin.twig', [
             'late_days' => $late_days,
+            'grade_inquiry_enabled' => $this->core->getConfig()->isRegradeEnabled(),
             'highlight_gradeable' => $hightlight_gradeable,
         ]);
         $table_data = "<hr><h2>Late Day Usage by " . $preferred_name . " (" . $late_days->getUser()->getId() . ")</h2><br>" . $table_data;
@@ -23,6 +24,7 @@ class LateDaysTableView extends AbstractView {
         $this->core->getOutput()->enableMobileViewport();
         return $this->core->getOutput()->renderTwigTemplate('LateDaysTable.twig', [
             'late_days' => $late_days,
+            'grade_inquiry_enabled' => $this->core->getConfig()->isRegradeEnabled()
         ]);
     }
 
@@ -39,6 +41,7 @@ class LateDaysTableView extends AbstractView {
         $this->core->getOutput()->enableMobileViewport();
         return $this->core->getOutput()->renderTwigTemplate('LateDaysTableGrader.twig', [
             'late_days' => $late_days,
+            'grade_inquiry_enabled' => $this->core->getConfig()->isRegradeEnabled()
         ]);
     }
 }
