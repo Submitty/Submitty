da2b3c45225a2e9538369afc7a0fb1c123ec1fb7
Nia Heermance <niaheermance@gmail.com>
2023-06-05 16:44:21 -0400

    Undo Change REGRADE to GRADE INQUIRY

--- site/app/views/NavigationView.php
+++ site/app/views/NavigationView.php
@@ -654,7 +654,7 @@ class NavigationView extends AbstractView {
             if ($gradeable->anyActiveGradeInquiries()) {
                 //Open grade inquiries
                 return new Button($this->core, [
-                    "title" => "GRADE INQUIRY",
+                    "title" => "REGRADE",
                     "class" => "btn btn-danger btn-nav btn-nav-grade",
                     "href" => $href
                 ]);
@@ -705,7 +705,7 @@ class NavigationView extends AbstractView {
                 $date_time = $grades_released;
             }
             else {
-                $title = 'GRADE INQUIRY';
+                $title = 'REGRADE';
             }
 
             if ($gradeable->getType() === GradeableType::ELECTRONIC_FILE) {
@@ -713,9 +713,9 @@ class NavigationView extends AbstractView {
                     $TA_percent = $gradeable->getGradingProgress($this->core->getUser());
 
                     if ($TA_percent === 1) {
-                        //If they're done, change the text to GRADE INQUIRY
+                        //If they're done, change the text to REGRADE
                         $class = 'btn-default';
-                        $title = 'GRADE INQUIRY';
+                        $title = 'REGRADE';
                     }
                     else {
                         if (!is_nan($TA_percent) && $list_section === GradeableList::GRADED) {
