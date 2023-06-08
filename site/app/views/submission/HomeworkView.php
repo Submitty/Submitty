6c41f3e2e61d688ea205a5609f6338b364409d3f
IDzyre <reapernsgaming@gmail.com>
2023-06-08 09:36:30 -0700

    adjust UI

--- site/app/views/submission/HomeworkView.php
+++ site/app/views/submission/HomeworkView.php
@@ -454,11 +454,20 @@ class HomeworkView extends AbstractView {
         if (!is_null($graded_gradeable)) {
             $graded_gradeable->hasOverriddenGrades();
         }
+
+        $vcs_base_url = rtrim($this->core->getConfig()->getVcsBaseUrl(), '/');
+        $vcs_base_url = str_replace('{$vcs_type}', $this->core->getConfig()->getVcsType(), $vcs_base_url);
+        $vcs_base_url = str_replace('{$gradeable_id}', $gradeable->getId(), $vcs_base_url);
+        $vcs_base_url = str_replace('{$user_id}', $this->core->getUser()->getId(), $vcs_base_url);
+        if ($gradeable->isTeamAssignment()) {
+            $vcs_base_url = str_replace('{$team_id}', $gradeable->getSubmitter()->getTeam()->getTeamName(), $vcs_base_url);
+        }
+
         $recent_version_url = $graded_gradeable ? $this->core->buildCourseUrl(['gradeable', $gradeable->getId()]) . '/' . $graded_gradeable->getAutoGradedGradeable()->getHighestVersion() : null;
         $numberUtils = new NumberUtils();
         return $output . $this->core->getOutput()->renderTwigTemplate('submission/homework/SubmitBox.twig', [
             'vcs_subdirectory' => $gradeable->getVcsSubdirectory(),
-            'vcs_base_url' => rtrim($this->core->getConfig()->getVcsBaseUrl(), '/'),
+            'vcs_base_url' => $vcs_base_url,
             'vcs_partial_path' => $gradeable->getVcsPartialPath(),
             'gradeable_id' => $gradeable->getId(),
             'gradeable_name' => $gradeable->getTitle(),
