f9e35e82105b0b13047a71363424585789dd5920
Jaeseok Kang <ziesski@gmail.com>
2023-06-09 10:09:17 -0400

    Undo course_tables.sql

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -1791,7 +1791,6 @@ CREATE TABLE public.users (
     user_email character varying NOT NULL,
     user_group integer NOT NULL,
     registration_section character varying(255),
-    course_section_id character varying(255),
     rotating_section integer,
     user_updated boolean DEFAULT false NOT NULL,
     instructor_updated boolean DEFAULT false NOT NULL,
