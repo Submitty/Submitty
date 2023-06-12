6d9249fd33571ddcc36fa6067e075e13a8a78b38
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-12 09:06:50 -0700

    See if doing ts even works
    Update upload.ts
    
    Fixing the expand_zip
    
    Update UploadCourseMaterialsForm.twig
    
    Update EditCourseMaterialsForm.twig

--- site/public/js/drag-and-drop.js
+++ site/public/js/drag-and-drop.js
@@ -1146,41 +1146,6 @@ function handleDownloadImages(csrf_token) {
     });
 }
 
-function isValidFileName(file_name) {
-    if (file_name.indexOf("'") != -1 || file_name.indexOf("\"") != -1) {
-        alert("ERROR! You may not use quotes in your filename: " + file_name);
-        return false;
-    } 
-    else if (file_name.indexOf("\\") != -1 || file_name.indexOf("/") != -1) {
-        alert("ERROR! You may not use a slash in your filename: " + file_name);
-        return false;
-    }
-    else if (file_name.indexOf("<") != -1 || file_name.indexOf(">") != -1) {
-        alert("ERROR! You may not use angle brackets in your filename: " + file_name);
-        return false;
-    }
-    return true;
-}
-
-function shouldReplaceFileIfDup(file_name, target_path) {
-    var k = fileExists(target_path + "/" + file_name, 1);
-    if ( k[0] == 1 )
-    {
-        var skip_confirmation = false;
-        if (expand_zip == 'on') {
-            var extension = getFileExtension(file_name);
-            if (extension.toLowerCase() == "zip") {
-                skip_confirmation = true; // skip the zip if there is conflict when in expand zip choice.
-            }
-        }
-        // don't want to replace, so skip
-        if(!skip_confirmation && !confirm("Note: " + file_name + " already exists. Do you want to replace it?")){
-            return false;
-        }
-    }
-    return true;
-}
-
 
 /**
  * @param csrf_token
@@ -1224,7 +1189,7 @@ function handleUploadCourseMaterials(csrf_token, expand_zip, hide_from_students,
               if (!isValidFileName(file_array[i][j].name)){
                   return;
               }
-              if (!shouldReplaceFileIfDup(file_array[i][j].name, target_path)){
+              if (!shouldReplaceFileIfDup(file_array[i][j].name, target_path, expand_zip)){
                   continue;
               }
               formData.append('files' + (i + 1) + '[]', file_array[i][j], file_array[i][j].name);
