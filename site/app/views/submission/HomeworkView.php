ce5c45fcaad4b1bb99ade2a42dc49e18886b2ba4
Mahi <pasarkarmahi@gmail.com>
2023-06-14 15:41:06 -0400

    more edits

--- site/app/views/submission/HomeworkView.php
+++ site/app/views/submission/HomeworkView.php
@@ -1088,7 +1088,7 @@ class HomeworkView extends AbstractView {
             'ta_grades_released' => $gradeable->isTaGradeReleased(),
             'is_vcs' => $gradeable->isVcs(),
             'can_download' => $can_download,
-            'can_change_submissions' => $this->core->getUser()->accessGrading() || $gradeable->isStudentSubmit(),
+            'can_change_submissions' => $gradeable->isStudentSubmit(),
             'can_see_all_versions' => $gradeable->isStudentSubmit(),
             'active_same_as_graded' => $active_same_as_graded,
             'ta_grades_incomplete' => $gradeable->isTaGrading() && $gradeable->isTaGradeReleased() && !$graded_gradeable->isTaGradingComplete(),
