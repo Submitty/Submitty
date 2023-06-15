b6849aad06620b27ed8557d5cef14826aaf61f15
Jaeseok Kang <ziesski@gmail.com>
2023-06-15 12:14:39 -0400

    database to single transaction
    By enclosing the statements between BEGIN; and COMMIT;, the entire block will be executed as a single transaction. This ensures that either all the statements are successfully executed, or none of them are executed if an error occurs.

--- migration/migrator/migrations/master/20220811134614_user_session_management.py
+++ migration/migrator/migrations/master/20220811134614_user_session_management.py
@@ -9,18 +9,24 @@ def up(config, database):
     :param database: Object for interacting with given database for environment
     :type database: migrator.db.Database
     """
-    database.execute("ALTER TABLE sessions ALTER COLUMN session_expires TYPE timestamptz(0);")
+
     database.execute("""
+        BEGIN;
+
+        ALTER TABLE sessions ALTER COLUMN session_expires TYPE timestamptz(0);
+
         ALTER TABLE sessions
         ADD COLUMN IF NOT EXISTS session_created timestamp(0) with time zone DEFAULT NULL,
         ADD COLUMN IF NOT EXISTS browser_name character varying(50) DEFAULT 'Unknown',
         ADD COLUMN IF NOT EXISTS browser_version character varying(15) DEFAULT '',
         ADD COLUMN IF NOT EXISTS platform character varying(50) DEFAULT 'Unknown';
-    """)
-    database.execute("""
+
         ALTER TABLE users
         ADD COLUMN IF NOT EXISTS enforce_single_session boolean DEFAULT false;
+
+        COMMIT;
     """)
+
 
 def down(config, database):
     """
