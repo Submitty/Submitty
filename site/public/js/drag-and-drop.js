2672b18b44de1bc8a3dac0eb2a73a1c63f035301
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-16 08:58:02 -0700

    first js fixes

--- site/public/js/drag-and-drop.js
+++ site/public/js/drag-and-drop.js
@@ -1141,34 +1141,34 @@ function handleDownloadImages(csrf_token) {
 }
 
 function isValidFileName(file_name) {
-    if (file_name.indexOf("'") != -1 || file_name.indexOf("\"") != -1) {
-        alert("ERROR! You may not use quotes in your filename: " + file_name);
+    if (file_name.indexOf('\'') != -1 || file_name.indexOf('"') != -1) {
+        alert('ERROR! You may not use quotes in your filename: ' + file_name);
         return false;
     } 
-    else if (file_name.indexOf("\\") != -1 || file_name.indexOf("/") != -1) {
-        alert("ERROR! You may not use a slash in your filename: " + file_name);
+    else if (file_name.indexOf('\\') != -1 || file_name.indexOf('/') != -1) {
+        alert('ERROR! You may not use a slash in your filename: ' + file_name);
         return false;
     }
-    else if (file_name.indexOf("<") != -1 || file_name.indexOf(">") != -1) {
-        alert("ERROR! You may not use angle brackets in your filename: " + file_name);
+    else if (file_name.indexOf('<') != -1 || file_name.indexOf('>') != -1) {
+        alert('ERROR! You may not use angle brackets in your filename: ' + file_name);
         return false;
     }
     return true;
 }
 
 function shouldReplaceFileIfDup(file_name, target_path) {
-    var k = fileExists(target_path + "/" + file_name, 1);
+    var k = fileExists(target_path + '/' + file_name, 1);
     if ( k[0] == 1 )
     {
         var skip_confirmation = false;
         if (expand_zip == 'on') {
             var extension = getFileExtension(file_name);
-            if (extension.toLowerCase() == "zip") {
+            if (extension.toLowerCase() == 'zip') {
                 skip_confirmation = true; // skip the zip if there is conflict when in expand zip choice.
             }
         }
         // don't want to replace, so skip
-        if(!skip_confirmation && !confirm("Note: " + file_name + " already exists. Do you want to replace it?")){
+        if(!skip_confirmation && !confirm('Note: ' + file_name + ' already exists. Do you want to replace it?')){
             return false;
         }
     }
