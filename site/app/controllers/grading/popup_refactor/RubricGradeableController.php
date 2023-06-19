400e256f07127e5aaf8c9b3ce43280eebc85aff8
Nia Heermance <niaheermance@gmail.com>
2023-06-19 14:40:13 -0400

    Create member variables

--- site/app/controllers/grading/popup_refactor/RubricGradeableController.php
+++ site/app/controllers/grading/popup_refactor/RubricGradeableController.php
@@ -8,7 +8,7 @@
  * This class's functionNAME will eventually be called when a Rubric Gradeable's grading page is opened.
  * Currently, to access the page associated with this class, enter URL:
  * 
- *     /courses/{_semester}/{_course}/gradeable/{gradeable_id}/new_grading_beta
+ *     /courses/{_semester}/{_course}/gradeable/{gradeable_id}/new_grading_beta[?more_stuff_here_is_okay]
  * 
  * This class is also responsible for updating popup windows created.
  * 
@@ -30,7 +30,34 @@ class RubricGradeableController extends AbstractController {
 	# ---------------------------------
 
 	# Member Variables:
-	# <None Currently>
+
+	/**
+	 * @var current_student_id
+	 * The anonomous id of the student currently being grade.
+	 * This id can be set with setCurrentStudentId or when loading this page's URL
+	 * with ?who_id=INSERT_ID.
+	 */
+	private $current_student_id;
+
+	/**
+	 * @var sort_type
+	 * By what ordering are we sorting by.
+	 * Controls where next and prev arrows go.
+	 */
+	private $sort_type;
+
+	/**
+	 * @var sort_direction
+	 * For a given ordering, do we sort it ascending "ASC" or descending "DSC".
+	 * Controls where next and prev arrows go.
+	 */
+	private $sort_direction;
+
+	/**
+	 * @var navigate_assigned_students_only
+	 * Do we skip students that we are not assigned to when pressing next or prev arrows?
+	 */
+	private $navigate_assigned_students_only;
 
 	# ---------------------------------
 
@@ -43,25 +70,28 @@ class RubricGradeableController extends AbstractController {
 	# Functions:
 
 	/**
-     * INFORMATION
-     * 
+     * Displays the Rubric Grading page. 
      * @Route("/courses/{_semester}/{_course}/gradeable/{gradeable_id}/new_grading_beta/grade")
+     * 
      * @param string $gradeable_id - The id string of this gradeable, set in the gradeable's settings.
+     * @param string $who_id - The id of the student we should grade.
+     * @param string $sort - The current way we are sorting students. Determines who the next and prev students are.
+     * @param string $direction - Either "ASC" or "DESC" for ascending or descending sorting order.
+     * @param string $navigate_assigned_students_only - When going to the next student, this variable controls whether we skip students.
+     * 
      */
 	public function test_page_creation(
         $gradeable_id,
         $who_id = '',
-        $from = "",
-        $to = null,
-        $gradeable_version = null,
         $sort = "id",
         $direction = "ASC",
-        $component_id = "-1",
-        $anon_mode = false,
-        $filter = 'default',
         $navigate_assigned_students_only = "true"
     ) {
-		print "Hello World!\n$direction";
+
+		# Set Member Variables
+		$current_student_id = $who_id;
+		
+
 	}
 }
 
