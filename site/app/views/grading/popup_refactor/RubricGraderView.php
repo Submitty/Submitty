25ab465a91f61c2e861803d4a3f207bcfcb34ab4
Nia Heermance <niaheermance@gmail.com>
2023-06-20 15:07:52 -0400

    Fix style issues

--- site/app/views/grading/popup_refactor/RubricGraderView.php
+++ site/app/views/grading/popup_refactor/RubricGraderView.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
  * ---------------------
- * 
+ *
  * RubricGradearView.php
  * First Created By: Nia Heermance
- * 
+ *
  * This class creates the main display window for the Rubric Grader, featuring the
  * NavigationBar and the PanelBar.
- * 
+ *
  * The function createRubricGradeableView is called from createMainRubricGraderPage
  * of RubricGraderController.php.
- * 
+ *
  * ---------------------
  */
 
