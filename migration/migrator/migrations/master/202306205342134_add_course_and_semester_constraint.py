7135ddb3915e39ec259ae3942ccc4441d06df770
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-22 07:26:07 -0700

    Make lines shorter

--- migration/migrator/migrations/master/202306205342134_add_course_and_semester_constraint.py
+++ migration/migrator/migrations/master/202306205342134_add_course_and_semester_constraint.py
@@ -1,7 +1,9 @@
 def up(config, database):
-    sql = "ALTER TABLE courses ADD CONSTRAINT no_surrounding_spaces_course CHECK (course ~ '^[^ ].*[^ ]$'); ALTER TABLE terms ADD CONSTRAINT no_surrounding_spaces_semester CHECK (term_id ~ '^[^ ].*[^ ]$');"
+    sql = "ALTER TABLE courses ADD CONSTRAINT no_surrounding_spaces_course CHECK (course ~ '^[^ ].*[^ ]$');"
+    sql += "ALTER TABLE terms ADD CONSTRAINT no_surrounding_spaces_semester CHECK (term_id ~ '^[^ ].*[^ ]$');"
     database.execute(sql)
 
 def down(config, database):
-    sql = "ALTER TABLE courses DROP CONSTRAINT no_surrounding_spaces_course; ALTER TABLE terms DROP CONSTRAINT no_surrounding_spaces_semester;"
+    sql = "ALTER TABLE courses DROP CONSTRAINT no_surrounding_spaces_course;"
+    sql += "ALTER TABLE terms DROP CONSTRAINT no_surrounding_spaces_semester;"
     database.execute(sql)
