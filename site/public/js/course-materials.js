29b0711eb71d5d2d90c2d4d1fccb28a902ad0cbe
skara9 <89281036+skara9@users.noreply.github.com>
2023-06-23 13:42:28 -0400

    lint fix

--- site/public/js/course-materials.js
+++ site/public/js/course-materials.js
@@ -140,7 +140,7 @@ function setUpFolders(isInstructor = false) {
         });
     }
 
-    if (jumpToScrollPosition.length > 0 && jumpToScrollPosition != '-1') {
+    if (jumpToScrollPosition.length > 0 && jumpToScrollPosition !== '-1' && jumpToScrollPosition !== -1) {
         const cm_ids = (Cookies.get('cm_data') || '').split('|').filter(n => n.length);
         for (const cm_id of cm_ids) {
             toggleCMFolder(cm_id);
