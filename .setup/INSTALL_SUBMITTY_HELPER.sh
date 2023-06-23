3a9f34333daf87c3a2e8064f43b16b37833fdc57
Barb Cutler <Barb Cutler>
2023-06-23 09:43:43 -0400

    add comments & custom started file

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -465,6 +465,8 @@ chmod 644 "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_default.j
 
 # create allowed_autograding_commands_custom.json if doesnt exist
 if [[ ! -e "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json" ]]; then
+    rsync -tz "${SUBMITTY_REPOSITORY}/grading/allowed_autograding_commands_default.json" "${SUBMITTY_INSTALL_DIR}/config"
+    cp "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json"
     touch "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json"
 fi
 
