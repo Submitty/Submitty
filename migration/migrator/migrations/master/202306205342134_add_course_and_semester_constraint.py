95b82cc2fac5a5d941be3e81dbc78c93289795dc
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-23 09:33:03 -0700

    Add more restrictions and change to better check name

--- migration/migrator/migrations/master/202306205342134_add_course_and_semester_constraint.py
+++ migration/migrator/migrations/master/202306205342134_add_course_and_semester_constraint.py
@@ -1,9 +1,9 @@
 def up(config, database):
-    sql = "ALTER TABLE courses ADD CONSTRAINT no_surrounding_spaces_course CHECK (course ~ '^[^ ].*[^ ]$');"
-    sql += "ALTER TABLE terms ADD CONSTRAINT no_surrounding_spaces_semester CHECK (term_id ~ '^[^ ].*[^ ]$');"
+    sql = "ALTER TABLE courses ADD CONSTRAINT course_validate CHECK (course ~ '^[a-zA-Z0-9_-]*$');"
+    sql += "ALTER TABLE terms ADD CONSTRAINT term_id_validate CHECK (term_id ~ '^[a-zA-Z0-9_-]*$');"
     database.execute(sql)
 
 def down(config, database):
-    sql = "ALTER TABLE courses DROP CONSTRAINT no_surrounding_spaces_course;"
-    sql += "ALTER TABLE terms DROP CONSTRAINT no_surrounding_spaces_semester;"
+    sql = "ALTER TABLE courses DROP CONSTRAINT course_validate;"
+    sql += "ALTER TABLE terms DROP CONSTRAINT term_id_validate;"
     database.execute(sql)
