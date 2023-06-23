b0a29caeabf56248d31b9f7b820a20fc75ccc099
IDzyre <ReaperNSGaming@gmail.com>
2023-06-22 16:38:14 -0800

    change setup sample

--- .setup/bin/setup_sample_courses.py
+++ .setup/bin/setup_sample_courses.py
@@ -2092,13 +2092,9 @@ class Gradeable(object):
         if self.is_repository:
             form_json['date_due'] = dateutils.write_submitty_date(self.submission_due_date)
             form_json['upload_type'] = 'repository'
-            form_json['vcs_partial_path'] = '{$gradeable_id}/{$user_id}'
-            if self.using_subdirectory:
-                form_json['using_subdirectory'] = True
-                form_json['subdirectory'] = self.subdirectory
-            else:
-                form_json['using_subdirectory'] = False
-                form_json['subdirectory'] = ''
+            form_json['vcs_partial_path'] = self.vcs_partial_path
+            form_json['using_subdirectory'] = self.using_subdirectory
+            form_json['subdirectory'] = self.subdirectory
             return form_json
         form_json['gradeable_title'] = self.title
         form_json['gradeable_type'] = self.get_gradeable_type_text()
@@ -2117,7 +2113,7 @@ class Gradeable(object):
             form_json['section_type'] = self.get_submission_type()
             form_json['eg_late_days'] = self.late_days
             form_json['upload_type'] = self.get_upload_type()
-            form_json['upload_repo'] = self.vcs_partial_path
+            form_json['upload_repo'] = ''
             form_json['comment_title'] = []
             form_json['points'] = []
             form_json['eg_extra'] = []
