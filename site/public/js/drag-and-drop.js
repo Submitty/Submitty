7201f574acae8c8dc114a9b8d80d896466abbd88
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-06-23 09:01:10 -0700

    Move isValidFileName to ts
    Update upload.ts

--- site/public/js/drag-and-drop.js
+++ site/public/js/drag-and-drop.js
@@ -1399,20 +1399,3 @@ function initializeDropZone(id) {
     dropzone.addEventListener('dragleave', draghandle, false);
     dropzone.addEventListener('drop', drop, false);
 }
-
-
-function isValidFileName(file_name) {
-    if (file_name.indexOf('\'') !== -1 || file_name.indexOf('"') !== -1) {
-        alert(`ERROR! You may not use quotes in your filename: ${file_name}`);
-        return false;
-    }
-    else if (file_name.indexOf('\\') !== -1 || file_name.indexOf('/') !== -1) {
-        alert(`ERROR! You may not use a slash in your filename: ${file_name}`);
-        return false;
-    }
-    else if (file_name.indexOf('<') !== -1 || file_name.indexOf('>') !== -1) {
-        alert(`ERROR! You may not use angle brackets in your filename: ${file_name}`);
-        return false;
-    }
-    return true;
-}
