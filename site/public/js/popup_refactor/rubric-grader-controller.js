8fd3d9d972451981f1ae14650e3a02c779a43be9
Nia Heermance <niaheermance@gmail.com>
2023-06-28 10:28:05 -0400

    Create variables

---
+++ site/public/js/popup_refactor/rubric-grader-controller.js
@@ -0,0 +1,19 @@
+/**
+ * ---------------------
+ *
+ * rubric-grader-controller.twig
+ *
+ * The user-side controller for the new RubricGrader page.
+ * Loads in advance the previous and next students, and allows
+ * asyncronous writeback to the server.
+ *
+ * ---------------------
+ */
+
+// Variables
+
+/**
+ * The current submission being graded.
+ */
+let current_submission;
+
