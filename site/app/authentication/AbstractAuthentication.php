7c116c2907cd4b02364201f953add63e01b01891
Musaab Imran <96174078+musaabimran@users.noreply.github.com>
2023-07-02 13:15:47 +0500

    Update AbstractAuthentication.php

--- site/app/authentication/AbstractAuthentication.php
+++ site/app/authentication/AbstractAuthentication.php
@@ -39,7 +39,7 @@ abstract class AbstractAuthentication {
         $this->password = $password;
     }
 
-    public function getUserId(){
+    public function getUserId() {
         return $this->user_id;
     }
 }
