dedc87960a4f52297f39b8c00f9546091a03c62f
Mahi <pasarkarmahi@gmail.com>
2023-07-10 16:42:54 -0400

    Cancel loading and expand and collapse test cases

--- site/cypress/e2e/gradeables/gradeable_results.spec.js
+++ site/cypress/e2e/gradeables/gradeable_results.spec.js
@@ -2,8 +2,7 @@
 
 describe('Testing functionality of Autograder Results', () => {
     beforeEach(() => {
-        //cy.visit(['sample','gradeable','closed_team_homework']);
-        cy.visit('http://localhost:1511/courses/s23/sample/gradeable/closed_team_homework');
+        cy.visit(['sample','gradeable','closed_team_homework']);
         cy.login('student');
 
     });
@@ -20,8 +19,34 @@ describe('Testing functionality of Autograder Results', () => {
         cy.get('.loading-tools-show').eq(1).click();
         cy.get('#testcase_1').should('have.css', 'display').and('not.match', /none/);
         //Close
-        cy.get('.loading-tools-hide').eq(0).click();
+        cy.get('.loading-tools-hide').first().click();
         cy.get('#testcase_1').should('have.css', 'display').and('match', /none/);
+    });
+
+    it('Should expand and collapse all test cases', () => {
+        //Open all
+        cy.get('.loading-tools-show').first().click();
+        cy.get('#testcase_1').should('have.css', 'display').and('not.match', /none/);
+        cy.get('#testcase_2').should('have.css', 'display').and('not.match', /none/);
+        cy.get('#testcase_3').should('have.css', 'display').and('not.match', /none/);
+        cy.get('#testcase_6').should('have.css', 'display').and('not.match', /none/);
+        cy.get('#testcase_8').should('have.css', 'display').and('not.match', /none/);
+        //Close all
+        cy.get('.loading-tools-hide').first().click();
+        cy.get('#testcase_1').should('have.css', 'display').and('match', /none/);
+        cy.get('#testcase_2').should('have.css', 'display').and('match', /none/);
+        cy.get('#testcase_3').should('have.css', 'display').and('match', /none/);
+        cy.get('#testcase_6').should('have.css', 'display').and('match', /none/);
+        cy.get('#testcase_8').should('have.css', 'display').and('match', /none/);
+    });
+
+    it('Should cancel loading', () => {
+        //Open
+        cy.get('#testcase_1').should('have.css', 'display').and('match', /none/);
+        cy.get('.loading-tools-show').eq(1).click();
+        cy.get('.loading-tools-in-progress').first().click(); //cancel
+        cy.get('#testcase_1').should('have.css', 'display').and('match', /none/);
+
     });
 
 })
