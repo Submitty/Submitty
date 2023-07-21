6cf1cf0e33659b3653b5497e181bc4ebb1f1d19a
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2023-07-21 01:09:35 -0400

    update load triggers test for optional output

--- migration/tests/test_load_triggers.py
+++ migration/tests/test_load_triggers.py
@@ -49,24 +49,28 @@ class TestLoadTriggers(unittest.TestCase):
         args.config = SimpleNamespace()
         args.config.database = dict()
 
-        with patch.object(migrator.db, 'Database') as mock_class:
-            migrator.main.load_triggers(args)
+        for show_output in (True, False):
+            with patch.object(migrator.db, 'Database') as mock_class:
+                if show_output:
+                    migrator.main.load_triggers(args)
+                else:
+                    migrator.main.load_triggers(args, False)
 
-        self.assertEqual(1, mock_class.call_count)
-        masterdb = mock_class.return_value
-        self.assertEqual(len(self.data_master), masterdb.execute.call_count)
-        self.assertEqual(1, masterdb.commit.call_count)
-        self.assertEqual(1, masterdb.close.call_count)
+            self.assertEqual(1, mock_class.call_count)
+            masterdb = mock_class.return_value
+            self.assertEqual(len(self.data_master), masterdb.execute.call_count)
+            self.assertEqual(1, masterdb.commit.call_count)
+            self.assertEqual(1, masterdb.close.call_count)
 
-        data = [int(args[0][0]) for args in masterdb.execute.call_args_list]
-        self.assertEqual(set(self.data_master), set(data))
+            data = [int(args[0][0]) for args in masterdb.execute.call_args_list]
+            self.assertEqual(set(self.data_master), set(data))
 
-        self.assertEqual(
-            'Loading trigger functions to master...\n  {}\nDONE\n'
-            .format('\n  '.join(['test_fn_master_{}'
-                                 .format(self.data_master.index(n)) for n in data])),
-            sys.stdout.getvalue()
-        )
+            self.assertEqual(
+                'Loading trigger functions to master...\n  {}\nDONE\n'
+                .format('\n  '.join(['test_fn_master_{}'
+                                    .format(self.data_master.index(n)) for n in data])),
+                sys.stdout.getvalue()
+            )
 
     def test_course(self):
         args = Namespace()
@@ -74,54 +78,56 @@ class TestLoadTriggers(unittest.TestCase):
         args.config = SimpleNamespace()
         args.config.database = dict()
 
-        num_courses = random.randint(3, 9)
-
-        mock_instances = [Mock() for _ in range(0, num_courses+2)]
-
-        mock_instances[0].execute.return_value.all.return_value = [
-            {'semester': 's{}'.format(i), 'course': 'c{}'.format(i)}
-            for i in range(0, num_courses)
-        ]
-
-        with patch.object(migrator.db, 'Database') as mock_class:
-            mock_class.side_effect = mock_instances
-            migrator.main.load_triggers(args)
-
-        self.assertEqual(len(mock_instances)-1, mock_class.call_count)
-        self.assertEqual('submitty', mock_class.call_args_list[0][0][0]['dbname'])
-        self.assertEqual('master', mock_class.call_args_list[0][0][1])
-        for i in range(0, num_courses):
-            self.assertEqual('submitty_s{0}_c{0}'.format(i),
-                             mock_class.call_args_list[i+1][0][0]['dbname'])
-            self.assertEqual('course', mock_class.call_args_list[i+1][0][1])
-
-        masterdb = mock_instances[0]
-        self.assertEqual(1, masterdb.execute.call_count)
-        self.assertEqual(0, masterdb.commit.call_count)
-        self.assertEqual(1, masterdb.close.call_count)
-
         course_logs = ''
+        for show_output in (True, False):
+            num_courses = random.randint(3, 9)
+            mock_instances = [Mock() for _ in range(0, num_courses+2)]
+            mock_instances[0].execute.return_value.all.return_value = [
+                {'semester': 's{}'.format(i), 'course': 'c{}'.format(i)}
+                for i in range(0, num_courses)
+            ]
 
-        coursedbs = mock_instances[1:-1]
-        i = 0
-        for coursedb in coursedbs:
-            self.assertEqual(len(self.data_course), coursedb.execute.call_count)
-            self.assertEqual(1, coursedb.commit.call_count)
-            self.assertEqual(1, coursedb.close.call_count)
+            with patch.object(migrator.db, 'Database') as mock_class:
+                mock_class.side_effect = mock_instances
+                if show_output:
+                    migrator.main.load_triggers(args)
+                else:
+                    migrator.main.load_triggers(args, False)
 
-            data = [int(args[0][0]) for args in coursedb.execute.call_args_list]
-            self.assertEqual(set(self.data_course), set(data))
+            self.assertEqual(len(mock_instances)-1, mock_class.call_count)
+            self.assertEqual('submitty', mock_class.call_args_list[0][0][0]['dbname'])
+            self.assertEqual('master', mock_class.call_args_list[0][0][1])
+            for i in range(0, num_courses):
+                self.assertEqual('submitty_s{0}_c{0}'.format(i),
+                                mock_class.call_args_list[i+1][0][0]['dbname'])
+                self.assertEqual('course', mock_class.call_args_list[i+1][0][1])
 
-            course_logs += ('Loading trigger functions to s{0}.c{0}...\n  {1}\nDONE\n'
-                            .format(i, '\n  '.join(
-                                ['test_fn_course_{}'
-                                 .format(self.data_course.index(n)) for n in data]
-                            )))
-            i += 1
+            masterdb = mock_instances[0]
+            self.assertEqual(1, masterdb.execute.call_count)
+            self.assertEqual(0, masterdb.commit.call_count)
+            self.assertEqual(1, masterdb.close.call_count)
 
-        self.assertFalse(mock_instances[-1].called)
+            self.assertFalse(mock_instances[-1].called)
 
-        self.assertEqual(course_logs, sys.stdout.getvalue())
+            coursedbs = mock_instances[1:-1]
+            i = 0
+            if show_output:
+                for coursedb in coursedbs:
+                    self.assertEqual(len(self.data_course), coursedb.execute.call_count)
+                    self.assertEqual(1, coursedb.commit.call_count)
+                    self.assertEqual(1, coursedb.close.call_count)
+
+                    data = [int(args[0][0]) for args in coursedb.execute.call_args_list]
+                    self.assertEqual(set(self.data_course), set(data))
+
+                    course_logs += ('Loading trigger functions to s{0}.c{0}...\n  {1}\nDONE\n'
+                                    .format(i, '\n  '.join(
+                                        ['test_fn_course_{}'
+                                        .format(self.data_course.index(n)) for n in data]
+                                    )))
+                    i += 1
+
+            self.assertEqual(course_logs, sys.stdout.getvalue())
 
     def test_system(self):
         args = Namespace()
