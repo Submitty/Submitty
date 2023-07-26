d26fd8b4c982fbb5af5a8ae9f7abe68678677514
Yanli Wang <114595115+yanliw123@users.noreply.github.com>
2023-07-25 21:54:42 -0400

    Update submitty_db.sql

--- migration/migrator/data/submitty_db.sql
+++ migration/migrator/data/submitty_db.sql
@@ -578,8 +578,8 @@ CREATE TABLE public.users (
     user_pronouns character varying(255) DEFAULT ''::character varying,
     user_last_initial_format integer DEFAULT 0 NOT NULL,
     enforce_single_session boolean DEFAULT false,
-    display_pronouns boolean DEFAULT false,
     display_name_order character varying(255) DEFAULT 'GIVEN_F'::character varying NOT NULL,
+    display_pronouns boolean DEFAULT false,
     CONSTRAINT users_user_access_level_check CHECK (((user_access_level >= 1) AND (user_access_level <= 3))),
     CONSTRAINT users_user_last_initial_format_check CHECK (((user_last_initial_format >= 0) AND (user_last_initial_format <= 3)))
 );
