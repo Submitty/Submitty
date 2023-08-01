f5fa02e067c4bb90fbe54e7ba61a07995b41e472
musaabimran <musaabimran2001@gmail.com>
2023-08-01 18:32:40 +0500

    Registration section input validation

--- site/app/models/User.php
+++ site/app/models/User.php
@@ -611,8 +611,9 @@ class User extends AbstractModel {
                 return preg_match("~^[1-4]{1}$~", $data) === 1;
             case 'registration_section':
                 //Registration section must contain only alpha (upper and lower permitted), numbers, underscores, hyphens.
+                // AND between 0 and 20 chars.
                 //"NULL" registration section should be validated as a datatype, not as a string.
-                return preg_match("~^(?!^null$)[a-z0-9_\-]+$~i", $data) === 1 || is_null($data);
+                return preg_match("~^(?!^null$)[a-zA-Z0-9_\-]{1,20}$~", $data) === 1 || is_null($data);
             case 'course_section_id':
                 //Course Section Id section must contain only alpha (upper and lower permitted), numbers, underscores, hyphens.
                 return preg_match("~^(?!^null$)[a-z0-9_\-]+$~i", $data) === 1 || is_null($data);
