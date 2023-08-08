1c6d9bc5a17045293069fc00f944026fa296e091
Nia Heermance <niaheermance@gmail.com>
2023-08-08 18:11:58 -0400

    Revert "Update Mark.twig"
    This reverts commit 47f0c1463e183e00a28f0fd6cb1f9d3704aafeb8.

--- site/public/js/ta-grading-rubric.js
+++ site/public/js/ta-grading-rubric.js
@@ -2091,7 +2091,7 @@ function onClickCountUp(me) {
     setMarkTitle(mark_id, 'No Credit');
     $.get('Mark.twig',null,function(){
         $("input[id^='mark-editor-']").each(function () {
-            $(this).attr("title","No Credit");
+            $(this).attr("overall","No Credit");
             if(this.value<0){
                 this.style.backgroundColor = "var(--standard-vibrant-yellow)";
             }
@@ -2112,7 +2112,7 @@ function onClickCountDown(me) {
     setMarkTitle(mark_id, 'Full Credit');
     $.get('Mark.twig',null,function(){
         $("input[id^='mark-editor-']").each(function () {
-            $(this).attr("title","Full Credit");
+            $(this).attr("overall","Full Credit");
             if(this.value>0){
                 this.style.backgroundColor = "var(--standard-vibrant-yellow)";
             }
