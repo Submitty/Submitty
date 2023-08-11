a2ee40a5f257a51f10afcc7b0ee89e3e0dfa0d91
Nia Heermance <51209504+NiaHeermance@users.noreply.github.com>
2023-08-11 17:04:10 -0400

    [Bugfix:TAGrading] LimAccess blindness affecting FullAccess (#9629)
    ### What is the current behavior?
    On a Gradeable's configuration page, there's a tab called "Grader
    Assignment" where instructors can set permissions on those who can grade
    the Gradeable and how much can they see. One setting controls whether
    Limited Access Graders can see/are blind to who they are grading.
    However, this setting was also unintentionally affecting whether
    instructors and Full Access Graders can see who they are grading.
    
    Another issue is that the Limited Access Blindness settings does not
    blind Team members names on a Gradeable's Status page for limited access
    graders. Notice that the first issue mentioned above is about limited
    access settings affecting full access, while this bug is limited access
    settings not working fully as intended.
    
    ### What is the new behavior?
    The setting now only affects Limited Access Graders.
    
    ### Screenshots
    
    The setting in question:
    
    ![image](https://github.com/Submitty/Submitty/assets/51209504/126e3f3b-636e-440c-8345-b42997f97169)
    
    
    The setting was accidentally hiding the student information in the top
    left for instructors and Full Access Graders.
    
    
    ![image](https://github.com/Submitty/Submitty/assets/51209504/29c8ebfc-b132-4530-9a61-dc8c6fd907b0)
    
    
    A blinded Gradeable Status page for a team Gradeable.
    
    ![image](https://github.com/Submitty/Submitty/assets/51209504/be3a08b7-a892-4502-8cd9-785fc01cf9c5)
    
    ---------
    
    Co-authored-by: Youssef-Chip <131885371+Youssef-Chip@users.noreply.github.com>
    Co-authored-by: Cameron Peterson <46759635+IDzyre@users.noreply.github.com>

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -495,9 +495,15 @@ HTML;
         $columns = [];
         $columns[]             = ["width" => "2%",  "title" => "",                 "function" => "index"];
         $columns[]             = ["width" => "8%",  "title" => "Section",          "function" => "section"];
-        if ($peer || $anon_mode) {
+
+        $team_and_anon = ($this->core->getUser()->getGroup() === User::GROUP_LIMITED_ACCESS_GRADER &&
+            $gradeable->getLimitedAccessBlind() === 2);
+
+        if ($peer || $anon_mode || $team_and_anon) {
             if ($gradeable->isTeamAssignment()) {
-                if ($gradeable->getPeerBlind() === Gradeable::DOUBLE_BLIND_GRADING || $anon_mode) {
+                $peer_and_anon = ($this->core->getUser()->getGroup() === User::GROUP_STUDENT &&
+                    $gradeable->getPeerBlind() === Gradeable::DOUBLE_BLIND_GRADING);
+                if ($team_and_anon || $peer_and_anon || $anon_mode) {
                     $columns[] = ["width" => "30%", "title" => "Team Members",     "function" => "team_members_anon"];
                 }
                 else {
@@ -1195,7 +1201,10 @@ HTML;
     public function renderNavigationBar(GradedGradeable $graded_gradeable, float $progress, bool $peer, $sort, $direction, $from, $limited_access_blind, $anon_mode, $blind_grading) {
         $gradeable = $graded_gradeable->getGradeable();
         $isBlind = false;
-        if ($gradeable->getLimitedAccessBlind() == 2) {
+        if (
+            $this->core->getUser()->getGroup() == User::GROUP_LIMITED_ACCESS_GRADER
+            && $gradeable->getLimitedAccessBlind() == 2
+        ) {
             $isBlind = true;
         }
         $home_url = $this->core->buildCourseUrl(['gradeable', $graded_gradeable->getGradeableId(), 'grading', 'details']) . '?' . http_build_query(['sort' => $sort, 'direction' => $direction]);
