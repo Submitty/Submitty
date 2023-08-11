170ab8185c9b511b49719ed3568d0ee3bbb701d4
Nia Heermance <niaheermance@gmail.com>
2023-08-11 16:04:32 -0400

    Change sameSite for collapse_sidebar

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -1396,11 +1396,11 @@ $.fn.isInViewport = function() {                                        // jQuer
 
 function checkSidebarCollapse() {
     if ($(document.body).width() < 1150) {
-        Cookies.set('collapse_sidebar', 'true', { path: '/' });
+        Cookies.set('collapse_sidebar', 'true', { path: '/', sameSite: 'None' });
         $("aside").toggleClass("collapsed", true);
     }
     else{
-        Cookies.set('collapse_sidebar', 'false', { path: '/' });
+        Cookies.set('collapse_sidebar', 'false', { path: '/', sameSite: 'None' });
         $("aside").toggleClass("collapsed", false);
     }
 }
@@ -1449,7 +1449,7 @@ function toggleSidebar() {
     const sidebar = $("aside");
     const shown = sidebar.hasClass("collapsed");
 
-    Cookies.set('collapse_sidebar', !shown, { path: '/' });
+    Cookies.set('collapse_sidebar', !shown, { path: '/', sameSite: 'None' });
     sidebar.toggleClass("collapsed", !shown);
 }
 
