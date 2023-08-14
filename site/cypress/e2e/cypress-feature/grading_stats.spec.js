1189fb458ad4cfb4b8746325ef2956f43fe7aa63
Youssef-Chip <yhassan2004@gmail.com>
2023-08-14 15:42:01 -0400

    temporary changes for debugging purposes

--- site/cypress/e2e/cypress-feature/grading_stats.spec.js
+++ site/cypress/e2e/cypress-feature/grading_stats.spec.js
@@ -16,6 +16,21 @@ describe('Test cases for grading stats', () => {
             text.should('contain', 'Number of teams: 36');
             text.should('contain', 'Teams who have submitted: 27 / 36 (75%)');
             text.should('contain', 'Section 1: 2 / 4 (50.0%)');
+            cy.get('[data-testid="Grading Index"]').click();
+            cy.wait(2000);
+            cy.get('[data-testid="view-sections"]').then(($button) => {
+                if ($button.text().includes('View All')) {
+                    $button.click();
+                }
+            });
+            cy.get('table')
+                .find('tr')
+                .not(':contains(NULL)')
+                .filter(':contains(Too Many Days Late)')
+                .each((row) => {
+                    cy.wrap(row).children().eq(2).then((user_id) => {
+                    });
+                });
         });
 
         it(`${user} view should be accurate for grades.`, () => {
@@ -29,6 +44,21 @@ describe('Test cases for grading stats', () => {
             text.should('contain', 'Students who have submitted: 68 / 101 (67.3%)');
             text.should('contain', 'Current percentage of TA grading done: 33 / 68 (48.5%)');
             text.should('contain', 'Section 1: 4 / 9 (44.4%)');
+            cy.get('[data-testid="Grading Index"]').click();
+            cy.wait(2000);
+            cy.get('[data-testid="view-sections"]').then(($button) => {
+                if ($button.text().includes('View All')) {
+                    $button.click();
+                }
+            });
+            cy.get('table')
+                .find('tr')
+                .not(':contains(NULL)')
+                .filter(':contains(Too Many Days Late)')
+                .each((row) => {
+                    cy.wrap(row).children().eq(2).then((user_id) => {
+                    });
+                });
         });
 
         it(`${user} viewshould be accurate for released grades.`, () => {
@@ -43,6 +73,21 @@ describe('Test cases for grading stats', () => {
             text.should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
             text.should('contain', 'Section 1: 10 / 10 (100.0%)');
             text.should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
+            cy.get('[data-testid="Grading Index"]').click();
+            cy.wait(2000);
+            cy.get('[data-testid="view-sections"]').then(($button) => {
+                if ($button.text().includes('View All')) {
+                    $button.click();
+                }
+            });
+            cy.get('table')
+                .find('tr')
+                .not(':contains(NULL)')
+                .filter(':contains(Too Many Days Late)')
+                .each((row) => {
+                    cy.wrap(row).children().eq(2).then((user_id) => {
+                    });
+                });
         });
     });
 });
