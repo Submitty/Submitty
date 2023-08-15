31336b2b7dd20c61713ace28e695f8fd4937bdfb
Youssef-Chip <yhassan2004@gmail.com>
2023-08-15 11:52:46 -0400

    Added stats check for on time and all submissions

--- site/cypress/e2e/cypress-feature/grading_stats.spec.js
+++ site/cypress/e2e/cypress-feature/grading_stats.spec.js
@@ -1,4 +1,11 @@
+const ApplyFilter = () => {
+    cy.get('#filters').click();
+    cy.get('#bad_submissions').click();
+    cy.get('#apply_button').click();
+};
+
 describe('Test cases for grading stats', () => {
+
     ['instructor','ta','grader'].forEach((user) => {
         beforeEach(() => {
             cy.login(user);
@@ -6,99 +13,56 @@ describe('Test cases for grading stats', () => {
         });
         it(`${user} view should be accurate for teams.`, () => {
             cy.visit(['sample','gradeable','grading_team_homework','grading','status']);
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
 
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
+            const all_submissions = cy.get('#left-grading-stats');
+            all_submissions.should('contain', 'Students on a team: 101/101 (100%)');
+            all_submissions.should('contain', 'Number of teams: 36');
+            all_submissions.should('contain', 'Teams who have submitted on time: 25 / 36 (69.4%)');
+            all_submissions.should('contain', 'Section 1: 1 / 3 (33.3%)');
 
-            const text = cy.get('#left-grading-stats');
-            text.should('contain', 'Students on a team: 101/101 (100%)');
-            text.should('contain', 'Number of teams: 36');
-            text.should('contain', 'Teams who have submitted: 27 / 36 (75%)');
-            text.should('contain', 'Section 1: 2 / 4 (50.0%)');
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
-
-            cy.get('[data-testid="Grading Index"]').click();
-            cy.get('[data-testid="view-sections"]').then(($button) => {
-                if ($button.text().includes('View All')) {
-                    $button.click();
-                }
-            });
-            cy.scrollTo('bottom', { duration: 20000 });
+            ApplyFilter();
+            const on_time_submissions = cy.get('#left-grading-stats');
+            on_time_submissions.should('contain', 'Students on a team: 101/101 (100%)');
+            on_time_submissions.should('contain', 'Number of teams: 36');
+            on_time_submissions.should('contain', 'Teams who have submitted: 27 / 36 (75%)');
+            on_time_submissions.should('contain', 'Section 1: 2 / 4 (50.0%)');
+            //Change filters back to default
+            ApplyFilter();
         });
 
         it(`${user} view should be accurate for grades.`, () => {
             cy.visit(['sample','gradeable','grading_homework','grading','status']);
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
-            const text = cy.get('#left-grading-stats').should('exist');
-            text.should('contain', 'Students who have submitted: 68 / 101 (67.3%)');
-            text.should('contain', 'Current percentage of TA grading done: 33 / 68 (48.5%)');
-            text.should('contain', 'Section 1: 4 / 9 (44.4%)');
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
+            const all_submissions = cy.get('#left-grading-stats').should('exist');
+            all_submissions.should('contain', 'Students who have submitted on time: 59 / 101 (58.4%)');
+            all_submissions.should('contain', 'Current percentage of TA grading done: 30 / 59 (50.8%)');
+            all_submissions.should('contain', 'Section 1: 4 / 9 (44.4%)');
 
-            cy.get('[data-testid="Grading Index"]').click();
-            cy.get('[data-testid="view-sections"]').then(($button) => {
-                if ($button.text().includes('View All')) {
-                    $button.click();
-                }
-            });
-            cy.scrollTo('bottom', { duration: 20000 });
+            ApplyFilter();
+            const on_time_submissions = cy.get('#left-grading-stats').should('exist');
+            on_time_submissions.should('contain', 'Students who have submitted: 68 / 101 (67.3%)');
+            on_time_submissions.should('contain', 'Current percentage of TA grading done: 33 / 68 (48.5%)');
+            on_time_submissions.should('contain', 'Section 1: 4 / 9 (44.4%)');
+            //Change filters back to default
+            ApplyFilter();
+
         });
 
         it(`${user} viewshould be accurate for released grades.`, () => {
             cy.visit(['sample','gradeable','grades_released_homework','grading','status']);
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
-            const text = cy.get('#left-grading-stats');
-            text.should('contain', 'Students who have submitted: 71 / 101 (70.3%)');
-            text.should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
-            text.should('contain', 'Section 1: 10 / 10 (100.0%)');
-            text.should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
-            cy.get('#filters').click();
-            cy.get('#bad_submissions').click();
-            cy.get('#apply_button').click();
-            cy.wait(5000);
-            cy.scrollTo('bottom', { duration: 5000 });
-            cy.scrollTo('top', { duration: 10000 });
+            const all_submissions = cy.get('#left-grading-stats');
+            all_submissions.should('contain', 'Students who have submitted on time: 64 / 101 (63.4%)');
+            all_submissions.should('contain', 'Current percentage of TA grading done: 64 / 64 (100.0%)');
+            all_submissions.should('contain', 'Section 1: 10 / 10 (100.0%)');
+            all_submissions.should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
 
-            cy.get('[data-testid="Grading Index"]').click();
-            cy.get('[data-testid="view-sections"]').then(($button) => {
-                if ($button.text().includes('View All')) {
-                    $button.click();
-                }
-            });
-            cy.scrollTo('bottom', { duration: 20000 });
+            ApplyFilter();
+            const on_time_submissions = cy.get('#left-grading-stats').should('exist');
+            on_time_submissions.should('contain', 'Students who have submitted: 71 / 101 (70.3%)');
+            on_time_submissions.should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
+            on_time_submissions.should('contain', 'Section 1: 10 / 10 (100.0%)');
+            on_time_submissions.should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
+            //Change filters back to default
+            ApplyFilter();
         });
     });
 });
