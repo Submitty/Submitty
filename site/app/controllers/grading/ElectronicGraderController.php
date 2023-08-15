f1bf99e7ab72485949b5728bb685f667bb954d83
Youssef-Chip <yhassan2004@gmail.com>
2023-08-15 15:21:24 -0400

    Update ElectronicGraderController.php

--- site/app/controllers/grading/ElectronicGraderController.php
+++ site/app/controllers/grading/ElectronicGraderController.php
@@ -1749,6 +1749,7 @@ class ElectronicGraderController extends AbstractController {
         $non_late_graded = 0;
 
         // Assure late day cache is calculated
+        $this->core->getQueries()->generateLateDayCacheForUsers();
         $section_key = ($gradeable->isGradeByRegistration() ? 'registration_section' : 'rotating_section');
         if ($peer) {
             $total = $gradeable->getPeerGradeSet();
