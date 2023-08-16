bc8c519880c7f064ba91d6ec7b2bf3110feacf99
Jaeseok Kang <ziesski@gmail.com>
2023-08-16 15:31:19 -0400

    better path name

--- migration/migrator/migrations/system/20230815125306_add_git_safe_directory.py
+++ migration/migrator/migrations/system/20230815125306_add_git_safe_directory.py
@@ -3,7 +3,7 @@ import subprocess
 
 def up(config):
     DAEMON_USER = config.submitty_users['daemon_user']
-    RAINBOWGRADES_REPOSITORY = "/usr/local/submitty/GIT_CHECKOUT/RainbowGrades"
+    RAINBOWGRADES_REPOSITORY = os.path.join(config.submitty['submitty_install_dir'], 'GIT_CHECKOUT','RainbowGrades')
 
     gitconfig_path = f"/home/{DAEMON_USER}/.gitconfig"
 
@@ -13,11 +13,8 @@ def up(config):
     try:
         with open(gitconfig_path, "w") as gitconfig_file:
             gitconfig_file.write(gitconfig_content)
-            print(".gitconfig entry written successfully.")
-
-        # Set ownership and group for the .gitconfig file
         subprocess.run(["sudo", "chown", f"{DAEMON_USER}:{DAEMON_USER}", gitconfig_path], check=True)
-        print(".gitconfig ownership changed successfully.")
+
     except Exception as e:
         print("Error:", e)
         print("Failed to write .gitconfig entry or change ownership.")
