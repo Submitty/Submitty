f2c96d67a0b6e93e70882d688e4a1ea73bdb8885
Musaab Imran <96174078+musaabimran@users.noreply.github.com>
2023-08-22 02:10:03 +0500

    [SECURITY][Bugfix:System] Prevent caching the resource (#9693)
    ### What is the current behavior?
    Before adding the Cache-Control: private header, the current behavior
    involves the potential caching of the resource by both intermediaries,
    such as proxy servers, and the client's browser. Intermediary servers
    might cache the resource, which could lead to outdated content being
    served to subsequent users. Additionally, the client's browser might
    cache the resource to expedite subsequent visits.
    
    ### What is the new behavior?
    However, after incorporating the Cache-Control: private header, the new
    behavior emerges. In this updated scenario, intermediaries are
    explicitly prevented from caching the resource, ensuring that subsequent
    users receive the most current content. Although the resource can still
    be cached by the client's browser for quicker loading during subsequent
    visits, the risk of serving outdated content due to intermediary caching
    is mitigated by this header change.

--- site/public/index.php
+++ site/public/index.php
@@ -106,6 +106,9 @@ date_default_timezone_set($core->getConfig()->getTimezone()->getName());
 // Stops clickjacking on all pages
 header('X-Frame-Options: SAMEORIGIN');
 
+// Prevent intermediaries from caching the resource
+header('Cache-Control: private');
+
 // We only want to show notices and warnings in debug mode, as otherwise errors are important
 ini_set('display_errors', '1');
 if ($core->getConfig()->isDebug()) {
