ad2c9f25aba684949ab8527c03e0481f733402a0
Nia Heermance <niaheermance@gmail.com>
2023-08-28 21:50:37 -0400

    Create migration

---
+++ migration/migrator/migrations/course/20230828212638_update_past_materials_to_2000.py
@@ -0,0 +1,37 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("""
+        UPDATE course_materials
+        SET release_date =  make_timestamptz(2000, 1, 1, 0, 0, 0, 'America/New_York')
+        WHERE EXTRACT(year FROM release_date) < 2000;
+    """)
+
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    pass
