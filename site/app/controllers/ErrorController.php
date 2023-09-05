ca0bb3852f3951451e7b897e416b4b0735519780
Nia Heermance <niaheermance@gmail.com>
2023-09-05 16:44:50 -0400

    Create ErrorController for real

---
+++ site/app/controllers/ErrorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use app\controllers\AbstractController;
+use app\libraries\Core;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController {
+
+
+	/**
+     * @Route("/courses/{_semester}/{_course}/no_access")
+     */
+    public function noAccess() {
+        $this->core->getOutput()->renderOutput('Error', 'noAccessCourse');
+    }
+    
+}
