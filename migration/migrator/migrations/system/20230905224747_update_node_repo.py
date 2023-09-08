14524f01d4da1df3aef749316d40e0ad15333765
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2023-09-08 13:48:58 -0400

    Update migration

--- migration/migrator/migrations/system/20230905224747_update_node_repo.py
+++ migration/migrator/migrations/system/20230905224747_update_node_repo.py
@@ -9,7 +9,7 @@ def up(config):
     :type config: migrator.config.Config
     """
     os.system("npm -g uninstall npm")
-    os.system("apt-get purge nodejs -y")
+    os.system("apt-get purge nodejs libnode72 -y")
     os.system("rm -r /etc/apt/sources.list.d/nodesource.list")
     os.system("rm -r /etc/apt/keyrings/nodesource.gpg")
     os.system("curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg")
@@ -26,4 +26,9 @@ def down(config):
     :param config: Object holding configuration details about Submitty
     :type config: migrator.config.Config
     """
-    pass
+    os.system("apt-get purge nodejs libnode72 -y")
+    os.system("rm -r /etc/apt/sources.list.d/nodesource.list")
+    os.system("rm -r /etc/apt/keyrings/nodesource.gpg")
+    os.system("umask 0022 && curl -sL https://deb.nodesource.com/setup_16.x | bash -")
+    os.system("apt-get update")
+    os.system("apt-get install nodejs -y")
