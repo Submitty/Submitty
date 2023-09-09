addcc2e4b8f43f98f3644e50167ec9d0db06b0c7
IDzyre <ReaperNSGaming@gmail.com>
2023-09-08 18:32:46 -0800

    Shell check fix

--- .setup/vagrant_cloud.sh
+++ .setup/vagrant_cloud.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 VAGRANT_CLOUD_TOKEN=$1
-VAGRANT_USERNAME=reapernsgaming
-VAGRANT_BOX=$2
+VAGRANT_USERNAME=$2
+VAGRANT_BOX=$3
 
 VAGRANT_CLOUD_VERSION=$(curl \
   --request GET \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  https://app.vagrantup.com/api/v1/box/$VAGRANT_USERNAME/$VAGRANT_BOX | \
+  https://app.vagrantup.com/api/v1/box/"$VAGRANT_USERNAME"/"$VAGRANT_BOX" | \
   python3 -c \
   'import json,sys;obj=json.load(sys.stdin);version=obj["versions"][0]["version"].split(".");version[3]=str(int(version[3])+1).zfill(3);print(".".join(version))')
 
@@ -14,7 +14,7 @@ curl \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  https://app.vagrantup.com/api/v1/box/$VAGRANT_USERNAME/$VAGRANT_BOX/versions \
+  https://app.vagrantup.com/api/v1/box/"$VAGRANT_USERNAME"/"$VAGRANT_BOX"/versions \
   --data '
     {
       "version": {
@@ -28,12 +28,10 @@ curl \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  https://app.vagrantup.com/api/v1/box/$VAGRANT_USERNAME/$VAGRANT_BOX/version/$VAGRANT_CLOUD_VERSION/providers \
+  https://app.vagrantup.com/api/v1/box/"$VAGRANT_USERNAME"/"$VAGRANT_BOX"/version/"$VAGRANT_CLOUD_VERSION"/providers \
   --data '
     {
       "provider": {
-        "checksum": "a59e7332e8bbe896f11f478fc61fa8a6",
-        "checksum_type": "md5",
         "name": "virtualbox"
       }
     }
@@ -42,16 +40,15 @@ curl \
 response=$(curl \
     --request GET \
     --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-    https://app.vagrantup.com/api/v1/box/$VAGRANT_USERNAME/$VAGRANT_BOX/version/$VAGRANT_CLOUD_VERSION/provider/virtualbox/upload | \
+    https://app.vagrantup.com/api/v1/box/"$VAGRANT_USERNAME"/"$VAGRANT_BOX"/version/"$VAGRANT_CLOUD_VERSION"/provider/virtualbox/upload | \
     python3 -c \
     'import json,sys;obj=json.load(sys.stdin);print(obj["upload_path"])')
 
-curl --request PUT --upload-file submitty.box "${response}"
+curl --request PUT --upload-file "$VAGRANT_BOX".box "${response}"
 
 # Release the version
 
 curl \
   --request PUT \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-   https://app.vagrantup.com/api/v1/box/$VAGRANT_USERNAME/$VAGRANT_BOX/version/$VAGRANT_CLOUD_VERSION/release
-
+   https://app.vagrantup.com/api/v1/box/"$VAGRANT_USERNAME"/"$VAGRANT_BOX"/version/"$VAGRANT_CLOUD_VERSION"/release
