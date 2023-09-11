fbcec5f963dac25774cb8d1efb13f0a456d8ee42
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2023-09-11 13:18:37 -0400

    Bump node to 20

--- .setup/distro_setup/ubuntu/22.04/setup_distro.sh
+++ .setup/distro_setup/ubuntu/22.04/setup_distro.sh
@@ -74,7 +74,7 @@ jq libseccomp-dev libseccomp2 seccomp junit flex bison poppler-utils
 apt-get install -qqy ninja-build
 
 # NodeJS
-NODE_MAJOR=16
+NODE_MAJOR=20
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 chmod o+r /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
--- migration/migrator/migrations/system/20230905224747_update_node_repo.py
+++ migration/migrator/migrations/system/20230905224747_update_node_repo.py
@@ -14,7 +14,7 @@ def up(config):
     os.system("rm -r /etc/apt/keyrings/nodesource.gpg")
     os.system("curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg")
     os.system("chmod o+r /etc/apt/keyrings/nodesource.gpg")
-    os.system("echo 'deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main' > /etc/apt/sources.list.d/nodesource.list")
+    os.system("echo 'deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main' > /etc/apt/sources.list.d/nodesource.list")
     os.system("apt-get update")
     os.system("apt-get install nodejs -y")
 
