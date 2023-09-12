6e9df06ea7db9a490f82a7d52dfd822fdeccd6c8
Nia Heermance <niaheermance@gmail.com>
2023-09-12 17:54:31 -0400

    Add function comment

--- site/public/js/rejoin-class.js
+++ site/public/js/rejoin-class.js
@@ -4,8 +4,12 @@
 /**
  * rejoin-class.js
  * Functions for students self-readding themselves to courses when conditions are met.
- */ 
+ */
 
+/**
+ * Sends request for student to be readded to the course.
+ * param string readd_url Url to the readd PHP function.
+ */
 async function rejoinCourse(readd_url) {
     $.ajax({
         type: 'POST',
