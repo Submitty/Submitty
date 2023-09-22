7e1256f292bab16e0cbe4e52e0822de8206d098b
SubmittyBot <SubmittyBot@users.noreply.github.com>
2023-09-22 02:44:40 +0000

    Update Tutorial to v23.09.00

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -9,7 +9,7 @@ export AnalysisTools_Version=v22.03.00
 export AnalysisToolsTS_Version=v23.08.00
 export Lichen_Version=v23.09.00
 export RainbowGrades_Version=v23.09.00
-export Tutorial_Version=v23.05.00
+export Tutorial_Version=v23.09.00
 export SysadminTools_Version=v23.08.00
 export Localization_Version=v23.08.02
 
