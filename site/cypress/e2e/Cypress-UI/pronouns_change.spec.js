f7c99738bfec744b29ee3dc1cda1d31c76b74eca
liny18 <liny18@rpi.edu>
2023-09-26 17:02:48 -0400

    Revert styling changes
    Figured out that reformatting is a bad idea, therefore undoing the changes.

--- site/cypress/e2e/Cypress-UI/pronouns_change.spec.js
+++ site/cypress/e2e/Cypress-UI/pronouns_change.spec.js
@@ -1,60 +1,71 @@
-describe("Tests cases about changing user pronouns", () => {
-    let oldPronouns = "";
-    const newPronouns = "They/Them"; // Define constant for new pronouns
 
+describe('Tests cases abut changing user pronouns', () => {
+
+    let oldPronouns = '';
+    const newPronouns = 'They/Them'; // Define constant for new pronouns
+
+    //Set the stage by saving old pronouns and setting a desired pronoun to look for
     before(() => {
-        cy.visit("/user_profile");
-        cy.login("student");
+        cy.visit('/user_profile');
+        cy.login('student');
 
-        cy.get("#pronouns_val").as("pronounsVal").click(); // Alias for pronouns value
-        cy.get("#user-pronouns-change").as("pronounsInput"); // Alias for pronouns input
+        cy.get('#pronouns_val').as('pronounsVal').click(); // Alias for pronouns value
+        cy.get('#user-pronouns-change').as('pronounsInput'); // Alias for pronouns input
 
-        cy.get("@pronounsInput").then(($pronounsInput) => {
+        cy.get('@pronounsInput').then(($pronounsInput) => {
             oldPronouns = $pronounsInput.val();
         });
 
         cy.get('button[aria-label="Clear pronoun input"]').click();
-        cy.get("@pronounsInput").type(newPronouns);
-        cy.get("#edit-pronouns-submit").click();
+        cy.get('@pronounsInput').type(newPronouns);
+        cy.get('#edit-pronouns-submit').click();
 
-        cy.get("@pronounsVal").contains(newPronouns);
+        cy.get('@pronounsVal').contains(newPronouns);
 
         cy.logout();
     });
 
+    //restore pronouns to previous value at the end
     after(() => {
-        cy.visit("/user_profile");
-        cy.login("student");
+        cy.visit('/user_profile');
+        cy.login('student');
 
-        cy.get("@pronounsVal").click();
-        cy.get('button[aria-label="Clear pronoun input"]').click();
-        if (oldPronouns !== "") {
-            cy.get("@pronounsInput").type(oldPronouns);
+        //change back to old pronouns
+        cy.get('@pronounsVal').click();
+        cy.get('button[aria-label="Clear pronoun input"]').click(); //clear input using trash can
+        if (oldPronouns !== '') {
+            cy.get('@pronounsInput').type(oldPronouns);
         }
-        cy.get("#edit-pronouns-submit").first().click();
+        cy.get('#edit-pronouns-submit').first().click();
 
-        if (oldPronouns !== "") {
-            cy.get("@pronounsVal").contains(oldPronouns);
+        //ensure pronouns changed on page
+        if (oldPronouns !== '') {
+            cy.get('@pronounsVal').contains(oldPronouns);
         }
     });
 
-    it("Verifies changed pronouns as instructor in Manage Students", () => {
-        cy.visit(["sample", "users"]);
-        cy.login("instructor");
+    it('Verifies changed pronouns as instructor in Manage Students', () => {
+        cy.visit(['sample','users']);
+        cy.login('instructor');
 
-        cy.get("#toggle-columns").click();
-        cy.get("#toggle-pronouns").check();
-        cy.get("#toggle-student-col-submit").first().click();
+        //ensure pronouns column is on
+        cy.get('#toggle-columns').click(); //open toggle columns form
+        cy.get('#toggle-pronouns').check();
+        cy.get('#toggle-student-col-submit').first().click();
 
-        cy.get(".td-pronouns:eq( 12 )").should("have.text", newPronouns);
+        //Ensure correctness in table
+        cy.get('.td-pronouns:eq( 12 )').should('have.text', newPronouns);
+
     });
 
-    it("Verifies changed pronouns as instructor in Student Photos", () => {
-        cy.visit(["sample", "student_photos"]);
-        cy.login("instructor");
+    it('Verifies changed pronouns as instructor in Student Photos', () => {
 
-        cy.get(".student-image-container > .name")
-            .first()
-            .contains(newPronouns);
+        cy.visit(['sample','student_photos']);
+        cy.login('instructor');
+
+        //Select text from photo area and parse to get pronoun
+        cy.get('.student-image-container > .name').first().contains(newPronouns);
+
     });
+
 });
