94e74bcb5191478709524d6d8ea4ea5286e5e5b6
Cameron Peterson <reapernsgaming@gmail.com>
2023-09-26 14:25:27 -0700

    remove vides that didn't fail

--- site/cypress.config.ts
+++ site/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import fs from 'fs'
 
 export default defineConfig({
   video: true,
@@ -8,9 +9,26 @@ export default defineConfig({
     openMode: 0,
   },
   e2e: {
+    
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+        on(
+          'after:spec',
+          (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+            if (results && results.video) {
+              // Do we have failures for any retry attempts?
+              const failures = results.tests.some((test) =>
+                test.attempts.some((attempt) => attempt.state === 'failed')
+              )
+              if (!failures) {
+                // delete the video if the spec passed and no tests retried
+                fs.unlinkSync(results.video)
+              }
+            }
+          }
+        )
+        
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:1511',
