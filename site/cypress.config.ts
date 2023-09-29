d41d5d4cdebf83032a24a9fe80a54f5b659e1a71
IDzyre <reapernsgaming@gmail.com>
2023-09-29 14:56:03 -0700

    Fix import

--- site/cypress.config.ts
+++ site/cypress.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'cypress'
+import * as fs from 'fs'
+
 export default defineConfig({
   video: true,
-  videoCompression: 32,
   pageLoadTimeout: 120000,
   retries: {
     runMode: 2,
@@ -12,6 +13,18 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:1511',
