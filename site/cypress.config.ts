5cb08a862cd0e83e1e27ab6e4262ebe2d70ef6b1
Cameron Peterson <46759635+IDzyre@users.noreply.github.com>
2023-09-29 09:38:59 -0700

    add video compression instead of deleting it

--- site/cypress.config.ts
+++ site/cypress.config.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 
 export default defineConfig({
   video: true,
+  videoCompression: 15,
   pageLoadTimeout: 120000,
   retries: {
     runMode: 2,
@@ -13,23 +14,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-        on(
-          'after:spec',
-          (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-            if (results && results.video) {
-              // Do we have failures for any retry attempts?
-              const failures = results.tests.some((test) =>
-                test.attempts.some((attempt) => attempt.state === 'failed')
-              )
-              if (!failures) {
-                // delete the video if the spec passed and no tests retried
-                fs.unlinkSync(results.video)
-              }
-            }
-          }
-        )
-        
-      return require('./cypress/plugins/index.js', 'fs')(on, config)
+      return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:1511',
     specPattern: 'cypress/e2e/**/*.spec.js',
