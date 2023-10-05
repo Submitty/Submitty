6cdb7a0d8018599e7340896513fde04d1625e6d3
DarthNyan <tatewhiteberg@gmail.com>
2023-10-05 13:21:56 -0400

    migration: add attachment name columns

---
+++ migration/migrator/migrations/course/20231005130620_forum_attachment_names.py
@@ -0,0 +1,37 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("""
+        ALTER TABLE posts ADD COLUMN IF NOT EXISTS attachment_name character varying(255) DEFAULT '';
+        ALTER TABLE forum_posts_history ADD COLUMN IF NOT EXISTS has_attachment boolean DEFAULT false;
+        ALTER TABLE forum_posts_history ADD COLUMN IF NOT EXISTS attachment_name character varying(255) DEFAULT '';
+    """)
+
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    pass
