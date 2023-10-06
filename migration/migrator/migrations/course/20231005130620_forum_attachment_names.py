84e67b4fbc369ad1e4134f5958e5ffd7151c3df0
DarthNyan <tatewhiteberg@gmail.com>
2023-10-06 10:57:31 -0400

    migration adds attachment names of existing posts

--- migration/migrator/migrations/course/20231005130620_forum_attachment_names.py
+++ migration/migrator/migrations/course/20231005130620_forum_attachment_names.py
@@ -1,5 +1,5 @@
 """Migration for a given Submitty course database."""
-
+import os
 
 def up(config, database, semester, course):
     """
@@ -19,6 +19,23 @@ def up(config, database, semester, course):
         ALTER TABLE forum_posts_history ADD COLUMN IF NOT EXISTS has_attachment boolean DEFAULT false;
         ALTER TABLE forum_posts_history ADD COLUMN IF NOT EXISTS attachment_name character varying(255) DEFAULT '';
     """)
+
+    #set attachment names of existing posts
+    posts = database.execute("""
+        SELECT * FROM posts WHERE has_attachment = true;
+    """)
+    for post in posts:
+        post_id = post[0]
+        thread_id = post[1]
+        img_path = f"/var/local/submitty/courses/{semester}/{course}/forum_attachments/{thread_id}/{post_id}"
+        attachment_name = ""
+        all_imgs = os.listdir(img_path)
+        for img in all_imgs:
+            attachment_name = attachment_name + "\n" + img
+        database.execute(f"""
+                         UPDATE posts SET attachment_name = '{attachment_name}' WHERE id = {post_id};
+                         UPDATE forum_posts_history SET has_attachment = true, attachment_name = '{attachment_name}' WHERE post_id = {post_id};
+                         """)
 
 
 def down(config, database, semester, course):
