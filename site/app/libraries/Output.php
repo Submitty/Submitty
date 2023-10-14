63ed3365ff6026562e3a5e26395f3a48d98aa223
Oscar Li <lio.rpi.edu@gmail.com>
2023-10-14 18:46:02 -0400

    a
    a

--- site/app/libraries/Output.php
+++ site/app/libraries/Output.php
@@ -193,11 +193,11 @@ HTML;
         $this->addVendorJs(FileUtils::joinPaths('jquery', 'jquery.min.js'));
         $this->addVendorJs(FileUtils::joinPaths('jquery-ui', 'jquery-ui.min.js'));
         $this->addVendorJs(FileUtils::joinPaths('js-cookie', 'js.cookie.min.js'));
+        $this->addVendorJs(FileUtils::joinPaths('luxon', 'luxon.min.js'));
         $this->addInternalJs('diff-viewer.js');
         $this->addInternalJs('server.js');
         $this->addInternalJs('menu.js');
         $this->addInternalJs('testcase-output.js');
-        $this->addVendorJs(FileUtils::joinPaths('luxon', 'luxon.min.js'));
     }
 
     /**
