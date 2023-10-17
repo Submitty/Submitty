90dd2617ab47abe404ddd408bdee36d6e31d6fc5
Jaeseok Kang <ziesski@gmail.com>
2023-10-16 22:14:43 -0500

    include install_composer

--- .setup/SUBMITTY_TEST.sh
+++ .setup/SUBMITTY_TEST.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
+install_composer() {
+    if ! command -v composer &> /dev/null; then
+        echo "Composer not found. Installing Composer..."
+        EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        ACTUAL_SIGNATURE=$(php -r "echo hash_file('sha384', 'composer-setup.php');")
+
+        if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+            >&2 echo 'ERROR: Invalid installer signature'
+            rm composer-setup.php
+            exit 1
+        fi
+
+        php composer-setup.php --quiet
+        RESULT=$?
+        rm composer-setup.php
+        exit $RESULT
+    fi
+}
+
 pushd /usr/local/submitty/GIT_CHECKOUT/Submitty/site || {
     echo "Failed to change to the Submitty/site directory.
     Please check if you have /usr/local/submitty/GIT_CHECKOUT/Submitty/site and valid permission"
     exit 1
 } > /dev/null
 
+install_composer
+
+
 run_php_stan() {
-    php vendor/bin/phpstan analyze app public/index.php socket/index.php "${@:2}"
+    COMPOSER_ALLOW_SUPERUSER=1 composer run-script static-analysis "${@:2}" 2>/dev/null
 }
 
 run_php_cs() {
-    php vendor/bin/phpcs --extensions=php ./app
+    COMPOSER_ALLOW_SUPERUSER=1 composer run-script lint 2>/dev/null
 }
+
 
 submitty_test() {
     if [ "$1" == "phpstan" ]; then
@@ -23,7 +47,7 @@ submitty_test() {
         run_php_cs
         run_php_stan "$@"
     elif [ "$1" == "help" ]; then
-        echo "phpstan: php static analysis [option: --memory-limit=4G, --generate-baseline ...]
+        echo "phpstan: php static analysis [option: --memory-limit 4G, --generate-baseline ...]
               phpcs  : php CodeSniffer
               php-lint: phpcs & phpstan"
     else
