a82c1581324989837c81c8a36ce43e08f4571cc8
Jaeseok Kang <ziesski@gmail.com>
2023-10-21 17:25:52 -0400

    npm run

--- .setup/SUBMITTY_TEST.sh
+++ .setup/SUBMITTY_TEST.sh
@@ -15,7 +15,13 @@ run_php_cs() {
     COMPOSER_ALLOW_SUPERUSER=1 composer run-script lint 2>/dev/null
 }
 
+run_js_es() {
+    npm run eslint
+}
 
+run_css_style() {
+    npm run css-stylelint
+}
 if [ "$1" == "phpstan" ]; then
     run_php_stan "$@"
 elif [ "$1" == "phpcs" ]; then
@@ -23,6 +29,10 @@ elif [ "$1" == "phpcs" ]; then
 elif [ "$1" == "php-lint" ]; then
     run_php_cs
     run_php_stan "$@"
+elif [ "$1" == "js-lint" ]; then
+    run_js_es
+elif [ "$1" == "css-lint" ]; then
+    run_css_style
 elif [ "$1" == "help" ]; then
     echo "phpstan: php static analysis [option: --memory-limit 4G, --generate-baseline ...]
           phpcs  : php CodeSniffer
