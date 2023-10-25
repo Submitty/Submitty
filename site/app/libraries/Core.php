Submitty/Submitty

commit 23cca77eda652882b86601165e0e9cc25d5d2405

[Refactor:System] Reduce session handling overhead (#8620)
* Improve performance of session handling

* update tests

* Move stale session deletion logic to install script

* lint

* increase session expiration time to 6 weeks

* Remove index

* Fix cookie expiration and revert to 2-week expiration period

* simply logic to determine whether session should be updated

* update comment

site/app/libraries/Core.php
@@ -769,23 +769,23 @@ class Core {
                     $_COOKIE[$cookie_key]
                 );
                 $session_id = $token->claims()->get('session_id');
-                $expire_time = $token->claims()->get('expire_time');
                 $logged_in = $this->getSession($session_id, $token->claims()->get('sub'));
-                // make sure that the session exists and it's for the user they're claiming
-                // to be
+                // make sure that the session exists and it's for the user they're claiming to be
                 if (!$logged_in) {
                     // delete cookie that's stale
                     Utils::setCookie($cookie_key, "", time() - 3600);
                 }
                 else {
-                    if ($expire_time > 0) {
+                    // If more than a day has passed since we last updated the cookie, update it with the new timestamp
+                    if ($this->session_manager->shouldSessionBeUpdated()) {
+                        $new_token = TokenManager::generateSessionToken(
+                            $session_id,
+                            $token->claims()->get('sub')
+                        );
                         Utils::setCookie(
                             $cookie_key,
-                            TokenManager::generateSessionToken(
-                                $session_id,
-                                $token->claims()->get('sub')
-                            )->toString(),
-                            $expire_time
+                            $new_token->toString(),
+                            $new_token->claims()->get('expire_time')
                         );
                     }
                 }

site/app/libraries/SessionManager.php
@@ -2,6 +2,9 @@
 
 namespace app\libraries;
 
+use DateInterval;
+use DateTime;
+
 /**
  * Class SessionManager
  *
@@ -11,6 +14,8 @@ namespace app\libraries;
  * they logged in.
  */
 class SessionManager {
+    const SESSION_EXPIRATION = "2 weeks";
+
     /**
      * @var Core
      */
@@ -35,20 +40,30 @@ class SessionManager {
      * @return bool|string
      */
     public function getSession(string $session_id) {
-        $this->core->getQueries()->removeExpiredSessions();
         $this->session = $this->core->getQueries()->getSession($session_id);
         if (empty($this->session)) {
             return false;
         }
-        $this->core->getQueries()->updateSessionExpiration($session_id);
+
+        // Only refresh the session once per day
+        if ($this->shouldSessionBeUpdated()) {
+            $this->core->getQueries()->updateSessionExpiration($session_id);
+        }
 
         return $this->session['user_id'];
     }
 
     /**
+     * Sessions should only be updated once per day to reduce load on the server.
+     * Check whether a day has passed since we last updated this session
+     */
+    public function shouldSessionBeUpdated(): bool {
+        $day_before_expiration = (new DateTime())->add(DateInterval::createFromDateString(self::SESSION_EXPIRATION))->sub(DateInterval::createFromDateString('1 day'));
+        return new DateTime($this->session['session_expires']) < $day_before_expiration;
+    }
+
+    /**
      * Create a new session for the user
-     *
-     * @return string
      */
     public function newSession(string $user_id): string {
         if (!isset($this->session['session_id'])) {

site/tests/app/libraries/SessionManagerTester.php
@@ -12,7 +12,6 @@ class SessionManagerTester extends \PHPUnit\Framework\TestCase {
     public function testGetSessionInvalidId() {
         $core = new Core();
         $queries = $this->createMock(DatabaseQueries::class);
-        $queries->expects($this->once())->method('removeExpiredSessions');
         $queries->expects($this->once())->method('getSession')->with('id')->willReturn([]);
         $queries->expects($this->never())->method('updateSessionExpiration');
         $core->setQueries($queries);
@@ -24,11 +23,11 @@ class SessionManagerTester extends \PHPUnit\Framework\TestCase {
     public function testSessionManager() {
         $core = new Core();
         $queries = $this->createMock(DatabaseQueries::class);
-        $queries->expects($this->once())->method('removeExpiredSessions');
         $queries->expects($this->once())->method('getSession')->with('id')->willReturn([
             'session_id' => 'id',
             'user_id' => 'test',
-            'csrf_token' => 'token'
+            'csrf_token' => 'token',
+            'session_expires' => '2000-01-01'
         ]);
         $queries->expects($this->once())->method('updateSessionExpiration')->with('id');
         $queries->expects($this->once())->method('removeSessionById')->with('id');

.setup/INSTALL_SUBMITTY_HELPER.sh
@@ -880,6 +880,14 @@ if [ "${WORKER}" == 0 ]; then
 fi
 
 #############################################################################
+# Delete expired sessions
+
+# Deletes all expired sessions from the main Submitty database
+if [ "${WORKER}" == 0 ]; then
+    "${SUBMITTY_INSTALL_DIR}/sbin/delete_expired_sessions.py"
+fi
+
+#############################################################################
 # If the migrations have indicated that it is necessary to rebuild all
 # existing gradeables, do so.
 

sbin/cleanup_old_email.py
@@ -8,64 +8,10 @@ This script takes 2 optional arguments:
  - the maximum number of emails to delete per call to this script (default 1,000)
 """
 
-import json
-import os
+import database_queries
 import datetime
-from sqlalchemy import create_engine, MetaData, text
 import sys
-
-
-try:
-    CONFIG_PATH = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), '..', 'config')
-    with open(os.path.join(CONFIG_PATH, 'submitty.json')) as open_file:
-        SUBMITTY_CONFIG = json.load(open_file)
-    with open(os.path.join(CONFIG_PATH, 'database.json')) as open_file:
-        DATABASE_CONFIG = json.load(open_file)
-
-
-except Exception as config_fail_error:
-    print("[{}] ERROR: CORE SUBMITTY CONFIGURATION ERROR {}".format(
-        str(datetime.datetime.now()), str(config_fail_error)))
-    sys.exit(1)
-
-
-DATA_DIR_PATH = SUBMITTY_CONFIG['submitty_data_dir']
-EMAIL_LOG_PATH = os.path.join(DATA_DIR_PATH, "logs", "emails")
-TODAY = datetime.datetime.now()
-LOG_FILE = open(os.path.join(
-    EMAIL_LOG_PATH, "{:04d}{:02d}{:02d}.txt".format(TODAY.year, TODAY.month,
-                                                    TODAY.day)), 'a')
-
-
-try:
-    DB_HOST = DATABASE_CONFIG['database_host']
-    DB_USER = DATABASE_CONFIG['database_user']
-    DB_PASSWORD = DATABASE_CONFIG['database_password']
-
-except Exception as config_fail_error:
-    e = "[{}] ERROR: Database Configuration Failed {}".format(
-        str(datetime.datetime.now()), str(config_fail_error))
-    LOG_FILE.write(e+"\n")
-    print(e)
-    sys.exit(1)
-
-
-def setup_db():
-    """Set up a connection with the submitty database."""
-    db_name = "submitty"
-    # If using a UNIX socket, have to specify a slightly different connection string
-    if os.path.isdir(DB_HOST):
-        conn_string = "postgresql://{}:{}@/{}?host={}".format(
-            DB_USER, DB_PASSWORD, db_name, DB_HOST)
-    else:
-        conn_string = "postgresql://{}:{}@{}/{}".format(
-            DB_USER, DB_PASSWORD, DB_HOST, db_name)
-
-    engine = create_engine(conn_string)
-    db = engine.connect()
-    metadata = MetaData(bind=db)
-    return db, metadata
+from sqlalchemy import text
 
 
 def delete_old_emails(db, days_to_preserve, maximum_to_delete):
@@ -91,7 +37,7 @@ def delete_old_emails(db, days_to_preserve, maximum_to_delete):
     if unsent_count > 0:
         print(f"WARNING: {unsent_count} UNSENT emails in database without errors.")
 
-    last_week = str(TODAY - datetime.timedelta(days=days_to_preserve))
+    last_week = str(datetime.datetime.now() - datetime.timedelta(days=days_to_preserve))
 
     query = """SELECT count(*) FROM emails WHERE sent is not NULL
     AND sent < :format AND error = '';"""
@@ -118,7 +64,7 @@ def delete_old_emails(db, days_to_preserve, maximum_to_delete):
 
 def main():
     try:
-        db, metadata = setup_db()
+        db, metadata = database_queries.setup_db()
 
         print("\nChecking Submitty Database Emails Table")
 
@@ -143,7 +89,7 @@ def main():
     except Exception as email_send_error:
         e = "[{}] Error Sending Email: {}".format(
             str(datetime.datetime.now()), str(email_send_error))
-        LOG_FILE.write(e+"\n")
+        database_queries.LOG_FILE.write(e+"\n")
         print(e)
 
 

sbin/database_queries.py
@@ -1,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+This script creates a database connection to be used in other misc scripts
+"""
+
+import json
+import os
+import datetime
+from sqlalchemy import create_engine, MetaData
+import sys
+
+
+try:
+    CONFIG_PATH = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), '..', 'config')
+    with open(os.path.join(CONFIG_PATH, 'submitty.json')) as open_file:
+        SUBMITTY_CONFIG = json.load(open_file)
+    with open(os.path.join(CONFIG_PATH, 'database.json')) as open_file:
+        DATABASE_CONFIG = json.load(open_file)
+
+
+except Exception as config_fail_error:
+    print("[{}] ERROR: CORE SUBMITTY CONFIGURATION ERROR {}".format(
+        str(datetime.datetime.now()), str(config_fail_error)))
+    sys.exit(1)
+
+
+DATA_DIR_PATH = SUBMITTY_CONFIG['submitty_data_dir']
+EMAIL_LOG_PATH = os.path.join(DATA_DIR_PATH, "logs", "emails")
+TODAY = datetime.datetime.now()
+LOG_FILE = open(os.path.join(
+    EMAIL_LOG_PATH, "{:04d}{:02d}{:02d}.txt".format(TODAY.year, TODAY.month,
+                                                    TODAY.day)), 'a')
+
+
+try:
+    DB_HOST = DATABASE_CONFIG['database_host']
+    DB_USER = DATABASE_CONFIG['database_user']
+    DB_PASSWORD = DATABASE_CONFIG['database_password']
+
+except Exception as config_fail_error:
+    e = "[{}] ERROR: Database Configuration Failed {}".format(
+        str(datetime.datetime.now()), str(config_fail_error))
+    LOG_FILE.write(e+"\n")
+    print(e)
+    sys.exit(1)
+
+
+def setup_db():
+    """Set up a connection with the submitty database."""
+    db_name = "submitty"
+    # If using a UNIX socket, have to specify a slightly different connection string
+    if os.path.isdir(DB_HOST):
+        conn_string = "postgresql://{}:{}@/{}?host={}".format(
+            DB_USER, DB_PASSWORD, db_name, DB_HOST)
+    else:
+        conn_string = "postgresql://{}:{}@{}/{}".format(
+            DB_USER, DB_PASSWORD, DB_HOST, db_name)
+
+    engine = create_engine(conn_string)
+    db = engine.connect()
+    metadata = MetaData(bind=db)
+    return db, metadata

sbin/delete_expired_sessions.py
@@ -1,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Deletes all expired sessions from the sessions table in the main Submitty database
+"""
+
+import database_queries
+import datetime
+
+
+def delete_expired_sessions(db):
+    """Delete the sessions which have expired."""
+
+    return db.execute("""
+        WITH deleted AS (
+            DELETE FROM sessions
+            WHERE session_expires < current_timestamp
+            RETURNING *
+        ) SELECT COUNT(*) FROM deleted
+        """).fetchone()[0]
+
+
+def main():
+    try:
+        db, metadata = database_queries.setup_db()
+
+        print("Deleting expired sessions...")
+
+        result = delete_expired_sessions(db)  # returns a count of the number of sessions deleted
+
+        print(f"Successfully deleted {result} expired sessions\n")
+
+    except Exception as e:
+        e_str = f"[{datetime.datetime.now()}] Error while deleting sessions: {e}"
+        database_queries.LOG_FILE.write(e_str+"\n")
+        print(e_str)
+
+
+if __name__ == "__main__":
+    main()

site/app/libraries/TokenManager.php
@@ -49,7 +49,7 @@ class TokenManager {
         string $user_id,
         $persistent = true
     ): Token {
-        $expire_time = ($persistent) ? time() + (7 * 24 * 60 * 60) : 0;
+        $expire_time = $persistent ? (new \DateTime())->add(\DateInterval::createFromDateString(SessionManager::SESSION_EXPIRATION))->getTimestamp() : 0;
         return self::$configuration->builder()
             ->issuedAt(new \DateTimeImmutable())
             ->issuedBy(self::$issuer)

commit b58ba0c6264005159ddf1181bc91b35ad16be37b

[Bugfix:System] Fix session cookie not updating (#9925)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
When the session gets closer to expiration it will update on the
database but not the cookie. Thus the cookie will still expire and log
the user out after the session expiration time. The issue was introduced
in https://github.com/Submitty/Submitty/pull/8620.

### What is the new behavior?
The cookie will now update when the session updates. The user will only
get logged out after 2 weeks of inactivity.

site/app/libraries/Core.php
@@ -892,7 +892,7 @@ class Core {
                 }
                 else {
                     // If more than a day has passed since we last updated the cookie, update it with the new timestamp
-                    if ($this->session_manager->shouldSessionBeUpdated()) {
+                    if ($this->session_manager->checkAndUpdateSession()) {
                         $new_token = TokenManager::generateSessionToken(
                             $session_id,
                             $token->claims()->get('sub')

site/app/libraries/SessionManager.php
@@ -53,22 +53,23 @@ class SessionManager {
             return false;
         }
 
-        // Only refresh the session once per day
-        if ($this->shouldSessionBeUpdated()) {
-            $this->session->updateSessionExpiration($this->core->getDateTimeNow());
-            $em->flush();
-        }
-
         return $this->session->getUserId();
     }
 
     /**
      * Sessions should only be updated once per day to reduce load on the server.
-     * Check whether a day has passed since we last updated this session
+     * Updates the session if needed and returns if the update was performed.
      */
-    public function shouldSessionBeUpdated(): bool {
+    public function checkAndUpdateSession(): bool {
         $day_before_expiration = (new DateTime())->add(DateInterval::createFromDateString(self::SESSION_EXPIRATION))->sub(DateInterval::createFromDateString('1 day'));
-        return $this->session->getSessionExpires() < $day_before_expiration;
+        $needs_update = $this->session->getSessionExpires() < $day_before_expiration;
+
+        if ($needs_update) {
+            $this->session->updateSessionExpiration($this->core->getDateTimeNow());
+            $this->core->getSubmittyEntityManager()->flush();
+        }
+
+        return $needs_update;
     }
 
     /**

site/tests/app/libraries/SessionManagerTester.php
@@ -60,14 +60,6 @@ class SessionManagerTester extends BaseUnitTest {
             ->method('getActiveSessionById')
             ->with('id')
             ->willReturn($session);
-        $session->expects($this->once())
-            ->method('updateSessionExpiration')
-            ->with(
-                $this->callback(function (\DateTime $dt): bool {
-                    $this->assertInstanceOf(\DateTime::class, $dt);
-                    return true;
-                })
-            );
         $core->getSubmittyEntityManager()
             ->expects($this->exactly(0))
             ->method('persist');
@@ -82,7 +74,7 @@ class SessionManagerTester extends BaseUnitTest {
             ->expects($this->once())
             ->method('remove');
         $core->getSubmittyEntityManager()
-            ->expects($this->exactly(2))
+            ->expects($this->once())
             ->method('flush');
         $manager = new SessionManager($core);
         $this->assertEquals('test', $manager->getSession('id'));