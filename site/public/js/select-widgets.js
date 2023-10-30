30583dad1a3344271ff0102e41d5b77dc59bc34e
Nia Heermance <niaheermance@gmail.com>
2023-10-29 22:52:30 -0400

    Address js linting

--- site/public/js/select-widgets.js
+++ site/public/js/select-widgets.js
@@ -1,18 +1,18 @@
 /* exported registerSelect2Widgets */
 
-function registerSelect2Widget(widget_id, belongingModalId = "off") {
-	// Inspired by https://stackoverflow.com/a/30021059
-	options = {
+function registerSelect2Widget(widget_id, belongingModalId = 'off') {
+    // Inspired by https://stackoverflow.com/a/30021059
+    let options = {
         tags: true,
-        theme: "bootstrap-5",
+        theme: 'bootstrap-5',
         createTag: (params) => ({
             id: params.term,
-            text: params.term
-        })
+            text: params.term,
+        }),
     };
 
-    if (belongingModalId !== "off") {
-    	options.dropdownParent = $(`#${belongingModalId}`);
+    if (belongingModalId !== 'off') {
+        options.dropdownParent = $(`#${belongingModalId}`);
     }
 
     $(`#${widget_id}`).select2(options);
