6935e2664cbe615d33ddc008cbee27a9521d1f81
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-10-31 13:25:42 -0400

    William suggestion to fix query
    Co-authored-by: William Allen <16820599+williamjallen@users.noreply.github.com>

--- site/app/repositories/banner/BannerImageRepository.php
+++ site/app/repositories/banner/BannerImageRepository.php
@@ -12,7 +12,14 @@ class BannerImageRepository extends EntityRepository {
     public function getValidBannerImages(): array {
         $currentDate = new \DateTime();
 
-        return $this->_em->createQuery('SELECT b FROM app\entities\banner\BannerImage b WHERE b.release_date <= :currentDate AND :currentDate <= b.closing_date ORDER BY b.release_date DESC')
+        return $this->_em->createQuery('
+            SELECT b
+            FROM app\entities\banner\BannerImage b
+            WHERE
+                b.release_date <= :currentDate
+                AND :currentDate <= b.closing_date
+            ORDER BY b.release_date DESC
+        ')
             ->setParameter('currentDate', $currentDate)
             ->getResult();
     }
