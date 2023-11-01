fc00b4f9ee58d4f5437a39c5f1c19e58538538a0
DarthNyan <tatewhiteberg@gmail.com>
2023-11-01 17:49:05 -0400

    first cypress fixes

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -791,14 +791,16 @@ SQL;
             $this->course_db->query("SELECT MAX(id) as max_id from posts where thread_id=? and author_user_id=?", [$thread_id, $user]);
             $id = $this->course_db->rows()[0]["max_id"];
             $this->visitThread($user, $thread_id);
-            $rows = [];
-            $params = [];
-            foreach ($attachment_name as $img) {
-                $params[] = $this->createParameterList(4);
-                $rows = array_merge($rows, [$id, $img, 1, 0]);
+            if($hasAttachment) {
+                $rows = [];
+                $params = [];
+                foreach ($attachment_name as $img) {
+                    $params[] = $this->createParameterList(4);
+                    $rows = array_merge($rows, [$id, $img, 1, 0]);
+                }
+                $placeholders = implode(", ", $params);
+                $this->course_db->query("INSERT INTO forum_attachments (post_id, file_name, version_added, version_deleted) VALUES {$placeholders}", $rows);
             }
-            $placeholders = implode(", ", $params);
-            $this->course_db->query("INSERT INTO forum_attachments (post_id, file_name, version_added, version_deleted) VALUES {$placeholders}", $rows);
             return $id;
         }
         catch (DatabaseException $dbException) {
