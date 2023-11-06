fad49a03519bbe4a85c1c44b8fd1417eef1858a4
Tate Whiteberg <tatewhiteberg@gmail.com>
2023-11-06 03:01:48 -0500

    Permanent fix: use non-changing timezone

--- site/cypress/e2e/Cypress-UI/profile.spec.js
+++ site/cypress/e2e/Cypress-UI/profile.spec.js
@@ -86,7 +86,7 @@ describe('Test cases revolving around user profile page', () => {
     // Selenium test_time_zone_selection
     it('Time zone selector should work', () => {
         cy.get('[data-testid="time-zone-dropdown"]').should('contain.text', 'NOT_SET/NOT_SET');
-        cy.get('[data-testid="time-zone-dropdown"]').select('(UTC-05:00) America/New_York');
+        cy.get('[data-testid="time-zone-dropdown"]').select('(UTC-07:00) America/Phoenix');
         // Since the login success message is still up, we get the next message.
         cy.get('[data-testid="popup-message"]').next().should('contain.text', 'Time-zone updated successfully');
         cy.get('[data-testid="time-zone-dropdown"]').select('NOT_SET/NOT_SET');
