e8b4317fda115aaa7f96691793695c4bc64b0f5f
Thomas Orifici <thomas.orifici@gmail.com>
2023-11-09 16:40:23 -0500

    DB testing

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -416,7 +416,10 @@ CREATE FUNCTION public.grab_late_day_gradeables_for_user(user_id text) RETURNS S
 					WHEN lde.late_day_exceptions IS NULL THEN 0
 					ELSE lde.late_day_exceptions
 				END AS late_day_exceptions
-                lde.reason_for_exception
+                CASE
+					WHEN lde.reason_for_exception IS NULL THEN ''
+					ELSE lde.reason_for_exception
+				END AS reason_for_exception
 			FROM valid_gradeables vg
 			LEFT JOIN submitted_gradeables sg
 				ON vg.g_id=sg.g_id
@@ -432,6 +435,7 @@ CREATE FUNCTION public.grab_late_day_gradeables_for_user(user_id text) RETURNS S
 		returnrow.late_day_date = var_row.late_day_date;
 		returnrow.submission_days_late = var_row.submission_days_late;
 		returnrow.late_day_exceptions = var_row.late_day_exceptions;
+        returnrow.reason_for_exception = var_row.reason_for_exception;
 		RETURN NEXT returnrow;
         END LOOP;
         RETURN;	
