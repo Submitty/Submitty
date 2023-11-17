5d130453ab39d873cfea88129a5dbaa096c05f5c
ANS1514 <anson.decker@gmail.com>
2023-11-17 17:35:04 -0500

    proper code practice

--- site/public/js/forum.js
+++ site/public/js/forum.js
@@ -1,4 +1,4 @@
-/* global displaySuccessMessage */
+/* global displaySuccessMessage, hljs */
 
 // eslint-disable-next-line no-unused-vars
 function categoriesFormEvents() {
@@ -345,7 +345,6 @@ function socketNewOrEditPostHandler(post_id, reply_level, post_box_id=null, edit
                 // eslint-disable-next-line no-undef
                 file_array[post_box_id] = [];
                 uploadImageAttachments(`#${post_id}-reply .upload_attachment_box`);
-                // eslint-disable-next-line no-undef
                 hljs.highlightAll();
 
             }
@@ -2018,7 +2017,6 @@ function loadThreadHandler() {
                 saveScrollLocationOnRefresh('posts_list');
 
                 $('.post_reply_form').submit(publishPost);
-                // eslint-disable-next-line no-undef
                 hljs.highlightAll();
             },
             error: function() {
