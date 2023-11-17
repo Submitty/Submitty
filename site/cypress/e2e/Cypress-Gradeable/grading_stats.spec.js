a5ffd3b4df3735dcf3c91d7cea92a7ba80448c6e
Youssef-Chip <yhassan2004@gmail.com>
2023-11-17 17:32:08 -0500

    add more tests for bulk upload

--- site/cypress/e2e/Cypress-Gradeable/grading_stats.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/grading_stats.spec.js
@@ -72,7 +72,55 @@ describe('Test cases for grading stats', () => {
             //Default, omit both filter
             ApplyFilter(true, true);
         });
-
+        /*Currently the Bulk Upload gradeable on Sample course doesn't have any submissions.
+        Until sample course is modified to have a bulk upload gradeable released, 
+        another way of mimicking the behavior of these gradeables is to disable the option 
+        to submit past the due date in a released homework gradeable, that way when we omit or 
+        include bad submissions, we can make sure the statistics stay the same*/
+        it(`${user} view should be accurate for released bulk upload exams grades.`, () => {
+            cy.visit(['sample', 'gradeable', 'grades_released_homework', 'grades_released_homework', 'update?nav_tab=5']);
+            //Disable submissions after due date
+            cy.get('#no_late_submission').click();
+            cy.visit(['sample', 'gradeable', 'grades_released_homework', 'grading', 'status']);
+            cy.get('#left-grading-stats').as('left-chunk-stats');
+            cy.get('#right-grading-stats').as('right-chunk-stats');
+            cy.get('#grader-info').as('right-chunk-grader-info');
+            cy.get('@left-chunk-stats').should('contain', 'Students who have submitted on time: 71 / 101 (70.3%)');
+            cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Section 10: 4 / 4 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
+            cy.get('@right-chunk-stats').should('contain', 'Average: 5.05 / 10 (51%)');
+            cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 71 - avg 3.08 - stddev 2.13)');
+            //Include Bad Submissions only
+            ApplyFilter(true, false);
+            cy.get('@left-chunk-stats').should('contain', 'Students who have submitted: 71 / 101 (70.3%)');
+            cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Section 10: 4 / 4 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
+            cy.get('@right-chunk-stats').should('contain', 'Average: 5.05 / 10 (51%)');
+            cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 71 - avg 3.08 - stddev 2.13)');
+            //Omit Bad submissions and Include Null Section
+            ApplyFilter(true, true);
+            cy.get('@left-chunk-stats').should('contain', 'Students who have submitted on time: 98 / 139 (70.5%)');
+            cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 98 / 98 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Section NULL: 27 / 27 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 67 / 98 (68.4%)');
+            cy.get('@right-chunk-stats').should('contain', 'Average: 5.28 / 10 (53%)');
+            cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 98 - avg 3.15 - stddev 2.12)');
+            //Include Bad Submission with the Null section filter included
+            ApplyFilter(true, false);
+            cy.get('@left-chunk-stats').should('contain', 'Students who have submitted on time: 98 / 139 (70.5%)');
+            cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 98 / 98 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Section NULL: 27 / 27 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 67 / 98 (68.4%)');
+            cy.get('@right-chunk-stats').should('contain', 'Average: 5.28 / 10 (53%)');
+            cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 98 - avg 3.15 - stddev 2.12)');
+            //Default, omit both filter
+            ApplyFilter(true, true);
+            cy.visit(['sample', 'gradeable', 'grades_released_homework', 'grades_released_homework', 'update?nav_tab=5']);
+            //Enable back submissions after due date
+            cy.get('#yes_late_submission').click();
+        });
         it(`${user} view should be accurate for released grades.`, () => {
             cy.visit(['sample', 'gradeable', 'grades_released_homework', 'grading', 'status']);
             cy.get('#left-grading-stats').as('left-chunk-stats');
@@ -89,6 +137,7 @@ describe('Test cases for grading stats', () => {
             cy.get('@left-chunk-stats').should('contain', 'Students who have submitted: 71 / 101 (70.3%)');
             cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 71 / 71 (100.0%)');
             cy.get('@left-chunk-stats').should('contain', 'Section 10: 4 / 4 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 49 / 71 (69.0%)');
             cy.get('@right-chunk-stats').should('contain', 'Average: 5.05 / 10 (51%)');
             cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 71 - avg 3.08 - stddev 2.13)');
             //Omit Bad submissions and Include Null Section
@@ -104,11 +153,11 @@ describe('Test cases for grading stats', () => {
             cy.get('@left-chunk-stats').should('contain', 'Students who have submitted: 98 / 139 (70.5%)');
             cy.get('@left-chunk-stats').should('contain', 'Current percentage of TA grading done: 98 / 98 (100.0%)');
             cy.get('@left-chunk-stats').should('contain', 'Section NULL: 27 / 27 (100.0%)');
+            cy.get('@left-chunk-stats').should('contain', 'Number of students who have viewed their grade: 67 / 98 (68.4%)');
             cy.get('@right-chunk-stats').should('contain', 'Average: 5.28 / 10 (53%)');
             cy.get('@right-chunk-grader-info').should('contain', 'instructor (count 98 - avg 3.15 - stddev 2.12)');
             //Default, omit both filter
             ApplyFilter(true, true);
         });
-        //NEED A GRADEABLE IN SAMPLE COURSE WITH SUBMISSIONS FOR BULK UPLOAD THAT ARE GRADED
     });
 });
