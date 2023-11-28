5051f0e53a59f205ecc885c5c36e67c3e240e6fb
DarthNyan <tatewhiteberg@gmail.com>
2023-11-28 16:50:02 -0500

    preserve deletion info on migration rerun

--- migration/migrator/migrations/course/20231005130620_forum_attachment_names.py
+++ migration/migrator/migrations/course/20231005130620_forum_attachment_names.py
@@ -1,6 +1,5 @@
 """Migration for a given Submitty course database."""
 import os
-#import pdb; pdb.set_trace()
 
 def up(config, database, semester, course):
     """
@@ -16,70 +15,53 @@ def up(config, database, semester, course):
     :type course: str
     """
     database.execute("""
-        ALTER TABLE 
-                     posts 
-        ADD COLUMN IF NOT EXISTS 
-                     version_id int DEFAULT 1;
-        ALTER TABLE 
-                     forum_posts_history 
-        ADD COLUMN IF NOT EXISTS 
-                     has_attachment boolean DEFAULT false;
-        ALTER TABLE 
-                     forum_posts_history 
-        ADD COLUMN IF NOT EXISTS 
-                     version_id int;
+        ALTER TABLE posts 
+        ADD COLUMN IF NOT EXISTS version_id int DEFAULT 1;
+        
+        ALTER TABLE forum_posts_history 
+        ADD COLUMN IF NOT EXISTS has_attachment boolean DEFAULT false;
+        
+        ALTER TABLE forum_posts_history 
+        ADD COLUMN IF NOT EXISTS version_id int;
         WITH 
             versions AS (
-                SELECT 
-                post_id, edit_timestamp,
+                SELECT post_id, edit_timestamp,
                      ROW_NUMBER() OVER 
                      (
-                        PARTITION BY post_id ORDER BY edit_timestamp)
-                        AS 
-                           version_id 
-                        FROM 
-                            forum_posts_history
-                        )
-                     UPDATE 
-                        forum_posts_history 
-                     AS 
-                        p 
-                     SET 
-                        version_id = v.version_id 
-                     FROM 
-                        versions 
-                     AS 
-                        v 
-                     WHERE 
-                        p.post_id = v.post_id 
-                     AND 
-                        p.edit_timestamp = v.edit_timestamp;
-        UPDATE 
-            posts
-        SET 
-            version_id = t.countVersionID
+                        PARTITION BY post_id ORDER BY edit_timestamp
+                     )
+                    AS version_id 
+                    FROM forum_posts_history
+                )
+                UPDATE forum_posts_history AS p 
+                SET version_id = v.version_id 
+                FROM versions AS v 
+                WHERE p.post_id = v.post_id 
+                AND p.edit_timestamp = v.edit_timestamp;
+        
+        UPDATE posts
+        SET version_id = t.countVersionID
         FROM
-            (
-            SELECT 
-                fph.post_id, COUNT(fph.post_id) AS countVersionID
-            FROM 
-                forum_posts_history fph 
-            JOIN 
-                posts p ON p.id = fph.post_id 
-            GROUP BY 
-                fph.post_id
-            ) t
-        WHERE 
-            posts.id = t.post_id;
+        (
+            SELECT fph.post_id, COUNT(fph.post_id) AS countVersionID
+            FROM forum_posts_history fph 
+            JOIN posts p ON p.id = fph.post_id 
+            GROUP BY fph.post_id
+        ) t
+        WHERE posts.id = t.post_id;
                      
         CREATE TABLE IF NOT EXISTS forum_attachments ( 
                      post_id int NOT NULL,
                      file_name character varying NOT NULL,
                      version_added int NOT NULL DEFAULT 1,
                      version_deleted int NOT NULL DEFAULT 0
-                    )
+                    );
     """)
 
+    #check if forum_attachments was already created
+    img_table = database.execute("SELECT * FROM forum_attachments;").all()
+    if (len(img_table) > 0):
+        return
     #set attachment names of existing posts
     posts = database.execute("""
         SELECT * FROM posts WHERE has_attachment = true;
