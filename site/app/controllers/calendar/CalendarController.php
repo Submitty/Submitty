8e61ab4c19c80505bab3c2019a00e632a5fed49d
Yanli Wang <wang970753651@gmail.com>
2023-12-06 21:31:58 -0500

    Update CalendarController.php

--- site/app/controllers/calendar/CalendarController.php
+++ site/app/controllers/calendar/CalendarController.php
@@ -204,6 +204,22 @@ class CalendarController extends AbstractController {
             $this->core->addErrorMessage("Invalid or incorrect id");
             return new RedirectResponse($this->core->buildUrl(['calendar']));
         }
+        
+        if (isset($_POST['semester'])) {
+            $semester = $_POST['semester'];
+        }
+        else {
+            $this->core->addErrorMessage("Invalid semester");
+            return new RedirectResponse($this->core->buildUrl(['calendar']));
+        }
+
+        if (isset($_POST['course'])) {
+            $InputCourse = $_POST['course'];
+        }
+        else {
+            $this->core->addErrorMessage("Invalid course");
+            return new RedirectResponse($this->core->buildUrl(['calendar']));
+        }
 
         $instructor_courses = $this->core->getQueries()->getInstructorLevelUnarchivedCourses($this->core->getUser()->getId());
 
