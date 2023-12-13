ab441dd169a4d8a38f6600b32fd1c951dab36849
Chuanyi Pan <cy2631995926@gmail.com>
2023-12-13 02:19:48 -0500

    Destroy and Split the function with huge string pass

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -179,26 +179,6 @@ class DatabaseQueries {
     }
 
     /**
-     * Gets a list of given and family names from the database given an array of IDs.
-     */
-    public function getGrading(string $u_or_t, string $section_key, string $users_or_teams, string $user_or_team_id, string $verified, string $where): ?string {
-        return "
-        SELECT {$u_or_t}.{$section_key}, count({$u_or_t}.*) as cnt
-        FROM {$users_or_teams} AS {$u_or_t}
-        INNER JOIN (
-         SELECT * FROM gradeable_data AS gd
-         INNER JOIN (SELECT g_id, $user_or_team_id, max(active_version) as active_version FROM electronic_gradeable_version GROUP BY g_id, $user_or_team_id) AS egd on egd.g_id = gd.g_id AND egd.{$user_or_team_id} = gd.gd_{$user_or_team_id}
-         LEFT JOIN (
-         gradeable_component_data AS gcd
-         INNER JOIN gradeable_component AS gc ON gc.gc_id = gcd.gc_id AND gc.gc_is_peer = {$this->course_db->convertBoolean(false)}
-         )AS gcd ON gcd.gd_id = gd.gd_id WHERE gcd.g_id=?$verified
-        ) AS gd ON {$u_or_t}.{$user_or_team_id} = gd.gd_{$user_or_team_id}
-        {$where}
-        GROUP BY {$u_or_t}.{$section_key}
-        ORDER BY {$u_or_t}.{$section_key}";
-    }
-
-    /**
      * Retrieves all students from a course and their virtual attendance
      * information such as logs for the course materials page, and logs
      * for the discussion forum page.
@@ -2281,7 +2261,6 @@ ORDER BY {$orderby}",
          $u_or_t = "u";
         $users_or_teams = "users";
         $user_or_team_id = "user_id";
-        $verified = "";
         if ($is_team) {
             $u_or_t = "t";
             $users_or_teams = "gradeable_teams";
@@ -2294,8 +2273,25 @@ ORDER BY {$orderby}",
             $where = "WHERE active_version > 0 AND ({$section_key} IN " . $this->createParameterList(count($sections)) . ") IS NOT FALSE";
             $params = array_merge($params, $sections);
         }
-        $query = $this->getGrading($u_or_t, $section_key, $users_or_teams, $user_or_team_id, $verified, $where);
-        $this->course_db->query($query, $params);
+
+        $this->course_db->query(
+            "
+            SELECT {$u_or_t}.{$section_key}, count({$u_or_t}.*) as cnt
+            FROM {$users_or_teams} AS {$u_or_t}
+            INNER JOIN (
+            SELECT * FROM gradeable_data AS gd
+            INNER JOIN (SELECT g_id, $user_or_team_id, max(active_version) as active_version FROM electronic_gradeable_version GROUP BY g_id, $user_or_team_id) AS egd on egd.g_id = gd.g_id AND egd.{$user_or_team_id} = gd.gd_{$user_or_team_id}
+            LEFT JOIN (
+            gradeable_component_data AS gcd
+            INNER JOIN gradeable_component AS gc ON gc.gc_id = gcd.gc_id AND gc.gc_is_peer = {$this->course_db->convertBoolean(false)}
+            )AS gcd ON gcd.gd_id = gd.gd_id WHERE gcd.g_id=?
+            ) AS gd ON {$u_or_t}.{$user_or_team_id} = gd.gd_{$user_or_team_id}
+            {$where}
+            GROUP BY {$u_or_t}.{$section_key}
+            ORDER BY {$u_or_t}.{$section_key}
+        ", 
+            $params
+        );
         foreach ($this->course_db->rows() as $row) {
             if ($row[$section_key] === null) {
                 $row[$section_key] = "NULL";
@@ -2320,7 +2316,6 @@ ORDER BY {$orderby}",
         $u_or_t = "u";
         $users_or_teams = "users";
         $user_or_team_id = "user_id";
-        $verified = " AND GCD_VERIFIER_ID IS NOT NULL";
         if ($is_team) {
             $u_or_t = "t";
             $users_or_teams = "gradeable_teams";
@@ -2333,8 +2328,25 @@ ORDER BY {$orderby}",
             $where = "WHERE active_version > 0 AND ({$section_key} IN " . $this->createParameterList(count($sections)) . ") IS NOT FALSE";
             $params = array_merge($params, $sections);
         }
-        $query = $this->getGrading($u_or_t, $section_key, $users_or_teams, $user_or_team_id, $verified, $where);
-        $this->course_db->query($query, $params);
+
+        $this->course_db->query(
+            "
+            SELECT {$u_or_t}.{$section_key}, count({$u_or_t}.*) as cnt
+            FROM {$users_or_teams} AS {$u_or_t}
+            INNER JOIN (
+            SELECT * FROM gradeable_data AS gd
+            INNER JOIN (SELECT g_id, $user_or_team_id, max(active_version) as active_version FROM electronic_gradeable_version GROUP BY g_id, $user_or_team_id) AS egd on egd.g_id = gd.g_id AND egd.{$user_or_team_id} = gd.gd_{$user_or_team_id}
+            LEFT JOIN (
+            gradeable_component_data AS gcd
+            INNER JOIN gradeable_component AS gc ON gc.gc_id = gcd.gc_id AND gc.gc_is_peer = {$this->course_db->convertBoolean(false)}
+            )AS gcd ON gcd.gd_id = gd.gd_id WHERE gcd.g_id=? AND GCD_VERIFIER_ID IS NOT NULL
+            ) AS gd ON {$u_or_t}.{$user_or_team_id} = gd.gd_{$user_or_team_id}
+            {$where}
+            GROUP BY {$u_or_t}.{$section_key}
+            ORDER BY {$u_or_t}.{$section_key}
+        ", 
+            $params
+        );
         foreach ($this->course_db->rows() as $row) {
             if ($row[$section_key] === null) {
                 $row[$section_key] = "NULL";
