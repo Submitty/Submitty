0c00c34a75d79b2469684c5ae52a1dee2401aecd
Chuanyi Pan <cy2631995926@gmail.com>
2023-12-12 23:58:17 -0500

    Revert "trying fix php lint 2"
    This reverts commit 2512efb07ec38a61b4c8890664b6a2d42af039f8.

--- site/app/libraries/routers/WebRouter.php
+++ site/app/libraries/routers/WebRouter.php
@@ -97,7 +97,9 @@ class WebRouter {
                 $msg = "POST request exceeds maximum size of " . $max_post_length;
                 $this->core->addErrorMessage($msg);
 
-                return JsonResponse::getFailResponse($msg);
+                return MultiResponse::JsonResponse(
+                    JsonResponse::getFailResponse($msg)
+                );
             }
 
             return true;
@@ -147,7 +149,9 @@ class WebRouter {
             }
 
             if (!$router->checkFeatureFlag()) {
-                return JsonResponse::getFailResponse('Feature is not yet available.');
+                return MultiResponse::JsonResponse(
+                    JsonResponse::getFailResponse('Feature is not yet available.')
+                );
             }
 
             $check_post_max_size = $router->checkPostMaxSize($request);
@@ -185,7 +189,9 @@ class WebRouter {
             // Check if loadCourse returns true
             if (!$router->loadCourse()) {
                 // If loadCourse returns false, redirect to home page
-                return new RedirectResponse($core->buildUrl(['home']));
+                return MultiResponse::RedirectResponse(
+                    new RedirectResponse($core->buildUrl(['home']))
+                );
             }
 
             $logged_in = $core->isWebLoggedIn();
@@ -358,7 +364,9 @@ class WebRouter {
                 }
             }
             elseif (!str_ends_with($this->parameters['_controller'], 'AuthenticationController')) {
-                return new RedirectResponse($this->core->buildUrl(['authentication', 'login']));
+                return MultiResponse::RedirectResponse(
+                    new RedirectResponse($this->core->buildUrl(['authentication', 'login']))
+                );
             }
         }
 
