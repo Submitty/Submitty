7ae1fa3deaa26ad9b76ae45596971c6cc3bfcb7b
Chuanyi Pan <cy2631995926@gmail.com>
2023-12-13 06:27:51 -0500

    fix php lint 3

--- site/app/models/RainbowCustomizationJSON.php
+++ site/app/models/RainbowCustomizationJSON.php
@@ -304,9 +304,11 @@ class RainbowCustomizationJSON extends AbstractModel {
      * } $plagiarismEntry
      */
     public function addPlagiarismEntry(object $plagiarismEntry): void {
-        // $emptyObject = (object) [];
-        // if ($plagiarismEntry === $emptyObject) {
-        if (empty(get_object_vars($plagiarismEntry))) {
+        $emptyObject = new stdClass();
+        $emptyObject->user = "";
+        $emptyObject->gradeable = "";
+        $emptyObject->penalty = 0;
+        if ($plagiarismEntry === $emptyObject) {
             throw new BadArgumentException('Plagiarism entry may not be empty.');
         }
 
