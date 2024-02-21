18988b2b93088065c3155f5fa34a8adf16002096
IDzyre <reapernsgaming@gmail.com>
2024-02-20 19:28:46 -0800

    add autograding_config_path

--- site/app/controllers/admin/AdminGradeableController.php
+++ site/app/controllers/admin/AdminGradeableController.php
@@ -64,7 +64,8 @@ class AdminGradeableController extends AbstractController {
             'type' => GradeableType::typeToString($config->getType()),
             'id' => $config->getId(),
             'instructions_url' => $config->getInstructionsUrl(),
-            'syllabus_bucket' => $config->getSyllabusBucket()
+            'syllabus_bucket' => $config->getSyllabusBucket(), 
+            'autograding_config_path' => $config->getAutogradingConfigPath()
         ];
         if ($config->getType() === GradeableType::ELECTRONIC_FILE) {
             $return_json['bulk_upload'] = $config->isBulkUpload();
