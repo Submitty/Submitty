857c18b996dd8a3b53f4aa5bce63ebcce1feadf9
Ryan Varughese <ryvarughese@gmail.com>
2024-02-23 00:25:26 -0500

    Cleaning Up and Making Code More Concise
    Shortened the length of code by combining redundant if and else statements.

--- site/app/views/forum/ForumThreadView.php
+++ site/app/views/forum/ForumThreadView.php
@@ -1110,11 +1110,9 @@ class ForumThreadView extends AbstractView {
                 $visible_username = "Anonymous";
             }
         }
-        else if(!$post["anonymous"]){
-            if($first){
-                $visible_username = $visible_username . " [OP]";
-            }
-            else if($post["author_user_id"] == $first_post_start['author_user_id']){
+
+        else {
+            if ($post["author_user_id"] == $first_post_start['author_user_id']) {
                 $visible_username = $visible_username . " [OP]";
             }
         }
