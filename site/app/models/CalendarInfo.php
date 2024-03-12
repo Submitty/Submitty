69f7e117286459d29c3611bb70c40233fccc6845
Xiayucheng1212 <x1204585568@gmail.com>
2024-03-12 15:22:08 +0800

    Show beta section gradeable

--- site/app/models/CalendarInfo.php
+++ site/app/models/CalendarInfo.php
@@ -91,6 +91,7 @@ class CalendarInfo extends AbstractModel {
         // Get the gradeables from the GradeableList and group them by section
         $gradeable_list_sections = [
             GradeableList::FUTURE => $gradeable_list->getFutureGradeables(),
+            GradeableList::BETA => $gradeable_list->getBetaGradeables(),
             GradeableList::OPEN => $gradeable_list->getOpenGradeables(),
             GradeableList::GRADING => $gradeable_list->getGradingGradeables(),
             GradeableList::CLOSED => $gradeable_list->getClosedGradeables(),
