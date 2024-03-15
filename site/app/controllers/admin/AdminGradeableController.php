493e9d7439c7d888fe50e797ffba0a9b608bc572
IDzyre <reapernsgaming@gmail.com>
2024-03-14 20:48:12 -0700

    === vs ==

--- site/app/controllers/admin/AdminGradeableController.php
+++ site/app/controllers/admin/AdminGradeableController.php
@@ -160,7 +160,7 @@ class AdminGradeableController extends AbstractController {
             return JsonResponse::getSuccessResponse($_POST['id']);
         }
         catch (\Exception $e) {
-            if ($e->getMessage() == 'Date validation failed') {
+            if ($e->getMessage() === 'Date validation failed') {
                 return JsonResponse::getErrorResponse('Date validation has failed, ensure the dates are in the correct sequential order');
             }
             return JsonResponse::getErrorResponse($e->getMessage());
