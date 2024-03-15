7df7762e2736586d8c4c6bdc5ee44e996fad2f0b
rajputanuj31 <rajputanuj041@gmail.com>
2024-03-15 13:28:41 +0530

    added data-testid to components

--- site/cypress/e2e/Cypress-Gradeable/grade_inquiries.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/grade_inquiries.spec.js
@@ -7,19 +7,19 @@ describe('Test cases revolving around grade inquires', () => {
         cy.get(`#${gradeableId} .fa-pencil-alt`).click();
 
         if (allowed) {
-            cy.get('#yes_grade_inquiry_allowed').click();
+            cy.get('[data-testid="yes-grade-inquiry-allowed"]').click();
         }
         else {
-            cy.get('#no_grade_inquiry_allowed').click();
+            cy.get('[data-testid="no_grade_inquiry_allowed"]').click();
         }
 
         if (date) {
             cy.contains('Dates').click();
-            cy.get('#date_grade_inquiry_due').click();
+            cy.get('[data-testid="date_grade_inquiry_due"]').click();
 
-            cy.get('#date_grade_inquiry_due').should('be.visible');
+            cy.get('[data-testid="date_grade_inquiry_due"]').should('be.visible');
 
-            cy.get('#date_grade_inquiry_due').clear().type(date, { parseSpecialCharSequences: false, force: true });
+            cy.get('[data-testid="date_grade_inquiry_due"]').clear().type(date, { parseSpecialCharSequences: false, force: true });
         }
 
         cy.get('#nav-sidebar-submitty').click();
@@ -34,7 +34,7 @@ describe('Test cases revolving around grade inquires', () => {
         cy.get('[data-testid="view-sections"]').click();
         cy.get(':nth-child(4) > :nth-child(1) > :nth-child(8) > [data-testid="grade-button"]').click();
 
-        cy.get('.gi-submit').should('have.length', 1).and('contain', 'Submit Grade Inquiry');
+        cy.get('[data-testid= "submit"]').should('have.length', 1).and('contain', 'Submit Grade Inquiry');
 
     });
 });
