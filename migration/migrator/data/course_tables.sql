4bfa0256c068d5a3bb91e76b6f9cdd3dffe08e30
Jeffrey <JeffreyCorderoNY@gmail.com>
2024-03-29 14:58:15 -0400

    Database dump fix

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -1531,7 +1531,7 @@ CREATE TABLE public.polls (
     poll_id integer NOT NULL,
     name text NOT NULL,
     question text NOT NULL,
-    status text,
+    status text NOT NULL,
     release_date date NOT NULL,
     image_path text,
     question_type character varying(35) DEFAULT 'single-response-multiple-correct'::character varying,
