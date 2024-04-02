6fd3be3caed0d373a9c033ef3c9e150e4a9b8c9f
Jaeseok Kang <ziesski@gmail.com>
2024-04-02 13:13:02 -0400

    better in_array

--- site/app/models/RainbowCustomization.php
+++ site/app/models/RainbowCustomization.php
@@ -404,24 +404,19 @@ class RainbowCustomization extends AbstractModel {
      * @return array<int, array<string, bool|string>> multidimensional array of display option data
      */
     public function getDisplay(): array {
-        // Get allowed benchmarks
-        $display = RainbowCustomizationJSON::allowed_display;
-        $retArray = [];
+        $allowedDisplayOptions = RainbowCustomizationJSON::allowed_display;
+        $displayOptions = [];
 
-        // If json file available then collect used display option from that, else get empty array
-        !is_null($this->RCJSON) ?
-            $usedDisplay = $this->RCJSON->getDisplay() :
-            $usedDisplay = [];
+        $usedDisplayOptions = $this->RCJSON ? $this->RCJSON->getDisplay() : [];
 
-        // Add data into retArray
-        foreach ($display as $display_option) {
-            in_array($display_option, $usedDisplay) ? $isUsed = true : $isUsed = false;
-
-            // Add display to return array
-            $retArray[] = ['id' => $display_option, 'isUsed' => $isUsed];
+        foreach ($allowedDisplayOptions as $optionId) {
+            $displayOptions[] = [
+                'id' => $optionId,
+                'isUsed' => in_array($optionId, $usedDisplayOptions)
+            ];
         }
 
-        return $retArray;
+        return $displayOptions;
     }
 
     /**
