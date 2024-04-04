f456d0215c33217bb085db46fbadccdc7c26a2a8
Cameron Peterson <46759635+IDzyre@users.noreply.github.com>
2024-04-03 20:47:07 -0700

    Update comment

--- sbin/adduser_course.py
+++ sbin/adduser_course.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Use this script to add a user to courses. Any user added to a course
-will be an instructor.
+Use this script to add a user to courses. You can choose the group
+that the user belongs to.
 """
 
 import argparse
