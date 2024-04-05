307436d9d0334c3052fb992ac0d9bbdfa1e71e82
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2024-04-05 15:40:10 -0400

    php lint

--- site/app/models/User.php
+++ site/app/models/User.php
@@ -243,7 +243,6 @@ class User extends AbstractModel {
         if (isset($details['display_name_order'])) {
             $this->setDisplayNameOrder($details['display_name_order']);
         }
-        
         else {
             $this->setDisplayNameOrder("GIVEN_F");
         }
