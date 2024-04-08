60b24b71f657c6de34c14a8d2ae25d2e8b83c42a
manzil-infinity180 <rahulvs2809@gmail.com>
2024-04-09 01:40:06 +0530

    updated

--- site/cypress/e2e/Cypress-Gradeable/prev_next.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/prev_next.spec.js
@@ -1,13 +1,9 @@
 describe('Cyclic grading View Test', () => {
     ['ta', 'instructor'].forEach((user) => {
-        it(`${user} grading_homework test`, () => {
+        it(`${user} cyclic grading view testing for Grading Homework`, () => {
             cy.login(user);
             cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'status']);
             cy.get('[data-testid="Grading Index"]').should('exist').click();
-            /* when we login with "ta" (teaching assistant) we will get a pop up window
-               in which it is mentioned that you are assigned for grade (something like this)
-               so here we have to agree this if we close(Not cancel) it, it will pop up again
-               when we go for grade(i mean clicking on grade button)*/
             if (user === 'ta') {
                 cy.get('[data-testid="popup-window"]').should('exist');
                 cy.get('[data-testid="close-button"]').should('exist');
@@ -16,27 +12,23 @@ describe('Cyclic grading View Test', () => {
             }
             cy.get('[data-testid="view-sections"]').click();
             cy.get('[data-testid="grade-button"]').eq(12).click();
-            cy.get('[data-testid="student_info_btn"]').click();
+            cy.get('[data-testid="student-info-btn"]').click();
             cy.get('.rubric-title').should('contain', 'Joe Student');
             cy.get('#prev-student-navlink').click();
             cy.get('.rubric-title').should('contain', 'John Smith');
-            cy.get('#grading-setting-btn > .invisible-btn > .fas').click();
-            cy.get('#general-setting-list > tbody > :nth-child(2) > :nth-child(2) > .ta-grading-setting-option').select('Prev/Next Ungraded Student');
-            cy.get('#settings-popup > .popup-box > [data-testid="popup-window"] > .form-title > [data-testid="close-button"]').click();
+            cy.get('#grading-setting-btn').click(); // change
+            // cy.get('#general-setting-list').children().should('exist').first().children().should('exist').eq(1).children().should('exist').eq(1).find('.ta-grading-setting-option').select('Prev/Next Ungraded Student');
+            cy.get('#general-setting-list').find('.ta-grading-setting-option').first().select('Prev/Next Ungraded Student');
+            cy.get('.form-title > [data-testid="close-button"]').should('exist').eq(3).click();
             cy.get('#next-student-navlink').click();
             cy.get('.rubric-title').should('contain', 'Joe Student');
         });
     });
-    // cyclic grading view testing for Grading Team Homework
     ['ta', 'instructor'].forEach((user) => {
-        it(`${user} grading_team_homework test`, () => {
+        it(`${user} cyclic grading view testing for Grading Team Homework`, () => {
             cy.login(user);
             cy.visit(['sample', 'gradeable', 'grading_team_homework', 'grading', 'status']);
-            cy.get('[data-testid="Grading Index"]').click();
-            /* when we login with "ta" (teaching assistant) we will get a pop up window
-               in which it is mentioned that you are assigned for grade (something like this)
-               so here we have to agree this if we close(Not cancel) it, it will pop up again
-               when we go for grade(i mean clicking on grade button)*/
+            cy.get('[data-testid="Grading Index"]').should('exist').click();
             if (user === 'ta') {
                 cy.get('[data-testid="popup-window"]').should('exist');
                 cy.get('[data-testid="close-button"]').should('exist');
@@ -45,18 +37,17 @@ describe('Cyclic grading View Test', () => {
             }
             cy.get('[data-testid="view-sections"]').click();
             cy.get('[data-testid="grade-button"]').eq(1).click();
-            cy.get('[data-testid="student_info_btn"]').click();
+            cy.get('[data-testid="student-info-btn"]').click();
             cy.get('#grading-panel-student-name').should('exist');
-            cy.get('#grading-panel-student-name').children();
             cy.get('#grading-panel-student-name').children().should('exist');
             cy.get('#grading-scroll-message').should('exist');
-            cy.get('[data-testid="tab_bar_wrapper"').children().its('length').should('be.gte', 2);
+            cy.get('[data-testid="tab-bar-wrapper"').children().its('length').should('be.eq', 3);
             cy.get('#page_1_nav').should('contain', 'Ben Bitdiddle');
             cy.get('#page_2_nav').should('contain', 'Dannie Farrell');
             cy.get('#page_3_nav').should('contain',  'Adan Fisher');
             cy.get('#grading-panel-student-name').should('contain', 'Ben Bitdiddle').and('contain', 'Dannie Farrell').and('contain', 'Adan Fisher');
             cy.get('#next-student-navlink').click();
-            cy.get('[data-testid="tab_bar_wrapper"').children().its('length').should('be.gte', 2);
+            cy.get('[data-testid="tab-bar-wrapper"').children().its('length').should('be.gte', 2);
             cy.get('#grading-panel-student-name').should('contain', 'Eunice Hamill ').and('contain', 'Edison King').and('contain', 'Justice Kuhic');
         });
     });
