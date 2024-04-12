d81a6bb27a616cae24d4bffaaeed252d34f653c3
CC Hettiarachchi <68075099+CCWizma99@users.noreply.github.com>
2024-04-13 02:31:24 +0530

    Update server.js
    jslint fix

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -1965,19 +1965,18 @@ document.addEventListener('DOMContentLoaded', tzWarn);
  */
 function scorePillDark() {
     const html_element = document.querySelector('html');
-    const badges = document.querySelectorAll('span.badge');
-    
-    if (html_element.getAttribute('dark-theme') === 'dark' || html_element.getAttribute('data-black_mode') === 'black'){
+    const badges = document.querySelectorAll('span.badge');   
+    if (html_element.getAttribute('dark-theme') === 'dark' || html_element.getAttribute('data-black_mode') === 'black') {
         badges.forEach(badge => {
-            if (badge.classList.contains('green-background')){
+            if (badge.classList.contains('green-background')) {
                 badge.classList.remove('green-background');
                 badge.classList.add('dark-green-background');
             }
-            else if (badge.classList.contains('yellow-background')){
+            else if (badge.classList.contains('yellow-background')) {
                 badge.classList.remove('yellow-background');
                 badge.classList.add('dark-yellow-background');
             }
-            else if (badge.classList.contains('red-background')){
+            else if (badge.classList.contains('red-background')) {
                 badge.classList.remove('red-background');
                 badge.classList.add('dark-red-background');
             }
