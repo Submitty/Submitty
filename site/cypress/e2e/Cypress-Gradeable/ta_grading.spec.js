07daad0664fbfc8b6597f8703fb1c2038ce01fc6
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2024-04-12 14:37:50 -0400

    Remove commented lines

--- site/cypress/e2e/Cypress-Gradeable/ta_grading.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/ta_grading.spec.js
@@ -12,7 +12,6 @@ describe('Test cases for TA grading page', () => {
         cy.get('#overall-comment-instructor').should('have.value', 'Comment1');
 
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=aYl92mR3NvJYGrK&sort=id&direction=ASC']);
-        // cy.get('#grading_rubric_btn').click();
         cy.get('#overall-comment-instructor').should('have.value', '');
         cy.get('#overall-comment-instructor').type('Comment2');
         cy.get('#overall-comment-instructor').blur();
@@ -29,7 +28,6 @@ describe('Test cases for TA grading page', () => {
         cy.get('#overall-comment-instructor').should('have.value', '');
 
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=jKjodWaRdEV9pBb&sort=id&direction=ASC']);
-        // cy.get('#grading_rubric_btn').click();
         cy.get('#overall-comment-instructor').should('have.value', 'Comment1');
         cy.get('#overall-comment-instructor').clear();
         cy.get('#overall-comment-instructor').blur();
