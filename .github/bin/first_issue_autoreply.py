6a05999d91ee2e1341f6d988c02a416e79c3d3a5
michaelkrar <michael.krar1@gmail.com>
2024-04-12 17:57:34 -0400

    Fix indentation for pylint

--- .github/bin/first_issue_autoreply.py
+++ .github/bin/first_issue_autoreply.py
@@ -6,15 +6,15 @@ terminal_output = subprocess.check_output(pr_json, shell=True, text=True)
 json_output = json.loads(terminal_output)
 
 first_issue_comment = "Hi INSERT_MENTION_TO_GITHUB_USERNAME\n"\
-"Thank you for your interest in the Submitty open source project.\n"\
-"We welcome contributions from new developers!\n"\
-"However we do not use the Github issue 'assign' feature for first time prospective contributors.\n"\
-"Please read our documentation on [how to get started with Submitty](https://submitty.org/developer/getting_started/index), "\
-"specifically our pages on [setting up your development environment](https://submitty.org/developer/getting_started/vm_install_using_vagrant) "\
-"and [making a pull request](https://submitty.org/developer/getting_started/make_a_pull_request). "\
-"You do not need to be assigned to an issue to create a pull request that will be "\
-"reviewed by our team and then merged if it appropriately resolves the issue. "\
-"We also encourage you to join our [Zulip](https://submitty.org/index/contact) server to discuss technical questions."
+    "Thank you for your interest in the Submitty open source project.\n"\
+    "We welcome contributions from new developers!\n"\
+    "However we do not use the Github issue 'assign' feature for first time prospective contributors.\n"\
+    "Please read our documentation on [how to get started with Submitty](https://submitty.org/developer/getting_started/index), "\
+    "specifically our pages on [setting up your development environment](https://submitty.org/developer/getting_started/vm_install_using_vagrant) "\
+    "and [making a pull request](https://submitty.org/developer/getting_started/make_a_pull_request). "\
+    "You do not need to be assigned to an issue to create a pull request that will be "\
+    "reviewed by our team and then merged if it appropriately resolves the issue. "\
+    "We also encourage you to join our [Zulip](https://submitty.org/index/contact) server to discuss technical questions."
 
 for pr in json_output:
     num = str(pr['number'])
