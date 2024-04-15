2288520f0b61d9403310632a93d05868b5863ea5
manzil-infinity180 <rahulvs2809@gmail.com>
2024-04-16 01:34:58 +0530

    updated to data-testid

--- site/cypress/e2e/Cypress-Feature/test_forum.spec.js
+++ site/cypress/e2e/Cypress-Feature/test_forum.spec.js
@@ -49,21 +49,21 @@ const removeThread = (title) => {
 const editThread = (title) => {
     cy.get('.thread-left-cont > .thread-list-item').contains(title).click();
     cy.get('.create-post-head').should('contain', title);
-    cy.get('.first_post > .post-action-container > .edit-post-button').click();
+    cy.get('[data-testid="edit-post-button"]').first().click();
     cy.get('#upload1').find('[for="input-file1"]').selectFile('cypress/fixtures/sea_animals.png');
     cy.get('#file-upload-table-1').find('.file-label').should('contain', 'sea_animals.png');
-    cy.get('#file-upload-table-1').find('[aria-label="Press enter to remove file sea_animals.png"]').click();
+    cy.get('#file-upload-table-1').click();
     cy.get('#file-upload-table-1').should('not.contain');
     cy.get('[data-testid="cancel-forum-btn"').click();
 };
 const uploadAttachmentAndDelete = (title) => {
     cy.get('.thread-left-cont > .thread-list-item').contains(title).click();
     cy.get('.create-post-head').should('contain', title);
-    cy.get('.first_post > .post-action-container > .edit-post-button').click();
+    cy.get('[data-testid="edit-post-button"]').first().click();
     cy.get('#upload1').find('[for="input-file1"]').selectFile('cypress/fixtures/sea_animals.png');
     cy.get('#file-upload-table-1').find('.file-label').should('contain', 'sea_animals.png');
     cy.get('input[value="Update Post"]').click();
-    cy.get('.first_post > .post-action-container > .edit-post-button').click();
+    cy.get('[data-testid="edit-post-button"]').first().click();
     cy.get('[data-testid="mark-for-delete-btn"]').should('contain', 'Delete').last().click();
     cy.get('[data-testid="mark-for-delete-btn"]').last().should('contain', 'Keep');
     cy.get('input[value="Update Post"]').click();
