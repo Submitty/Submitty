9740f396bde7653f83ce45b6d393a811e68c16bc
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2024-04-16 12:50:33 -0400

    Remove system program check for docker

--- grading/execute.cpp
+++ grading/execute.cpp
@@ -45,16 +45,12 @@ extern const std::string GLOBAL_allowed_autograding_commands_custom_string;  // 
 // =====================================================================================
 // =====================================================================================
 
-bool command_available_in_env(const nlohmann::json &program_object, const bool &running_in_docker) {
-  bool onlyDocker = program_object.value("onlyDocker", false);
-  if (!running_in_docker && onlyDocker) {
-    return false;
-  }
-  return true;
-}
-
 bool system_program(const std::string &program, std::string &full_path_executable, const bool running_in_docker)
 {
+  // if we are docker, allow any program
+  if (running_in_docker) {
+    return true;
+  }
   // parse GLOBAL_allowed_autograding_commands_default_string into allowed_autograding_commands
   // ignore_comment flag set in parse function to ignore comments
   nlohmann::json allowed_autograding_commands =
@@ -70,21 +66,16 @@ bool system_program(const std::string &program, std::string &full_path_executabl
     for (auto& entry : allowed_autograding_commands.items()) {
       nlohmann::json program_obj = entry.value();
       if (program_obj["path"] == program) {
-        if (command_available_in_env(program_obj, running_in_docker)) {
-          full_path_executable = program;
-          return true;
-        }
-        return false;
+        full_path_executable = program;
+        return true;
       }
     }
   }
   else {
     if (allowed_autograding_commands.contains(program)) {
       nlohmann::json program_obj = allowed_autograding_commands[program];
-      if (command_available_in_env(program_obj, running_in_docker)) {
-        full_path_executable = program_obj["path"];
-        return true;
-      }
+      full_path_executable = program_obj["path"];
+      return true;
     }
   }
   return false;
