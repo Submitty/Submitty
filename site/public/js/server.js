f7a6c4b044c74b2c9e9462ef6a79a1753bd329e0
Raymond Reynoso <reynor2@rpi.edu>
2024-04-18 19:16:54 -0400

    Final

--- site/public/js/server.js
+++ site/public/js/server.js
@@ -482,7 +482,7 @@ function showPopup(selector) {
 
 /* eslint-disable-next-line no-unused-vars */
 function closePopup(id) {
-    $('#' + id).hide();
+    $(`#${id}`).hide();
     //Checking if the body contains the class no scroll making the page locked, then unlocking it
     if (document.body.classList.contains('no-scroll')) {
         document.body.classList.remove('no-scroll');
