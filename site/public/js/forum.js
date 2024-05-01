9794b18392c87ed810a5f0f72883e4071dc58112
Xiayucheng1212 <x1204585568@gmail.com>
2024-05-01 18:23:47 +0800

    remove unused lint-disable comment

--- site/public/js/forum.js
+++ site/public/js/forum.js
@@ -1986,8 +1986,7 @@ function markThreadUnread(thread_id) {
             // eslint-disable-next-line no-undef
             csrf_token: csrfToken,
         },
-        // eslint-disable-next-line no-unused-vars
-        success: function(data) {
+        success: function() {
             $(`#thread_box_link_${thread_id}`).children().addClass('new_thread');
             $('.post_box').removeClass('viewed_post').addClass('new_post');
         },
@@ -2011,8 +2010,7 @@ function markPostUnread(thread_id, post_id, last_viewed_timestamp) {
             // eslint-disable-next-line no-undef
             csrf_token: csrfToken,
         },
-        // eslint-disable-next-line no-unused-vars
-        success: function(data) {
+        success: function() {
             $(`#thread_box_link_${thread_id}`).children().addClass('new_thread');
             $(`#${post_id}`).removeClass('viewed_post').addClass('new_post');
         },
