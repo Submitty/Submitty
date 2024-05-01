aeab20c2b468ce7e6e05fba190fd473997260b8c
Xiayucheng1212 <x1204585568@gmail.com>
2024-05-01 17:29:39 +0800

    remove warning color

--- bin/generate_repos.py
+++ bin/generate_repos.py
@@ -21,8 +21,6 @@ import re
 from sqlalchemy import create_engine, MetaData, Table, bindparam
 
 from submitty_utils import db_utils
-WARNING = '\033[93m'
-DEFAULT = '\033[0m'
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'config')
 
 with open(os.path.join(CONFIG_PATH, 'submitty_users.json')) as open_file:
@@ -195,12 +193,12 @@ if eg is not None:
     is_team = eg.eg_team_assignment
     # gradeable is not a vcs gradeable
     if eg.eg_vcs_host_type == -1:
-        print((WARNING + "Warning: Semester '{}' and Course '{}' contains gradeable_id '{}' but it is not a VCS gradeable." + DEFAULT).format(args.semester, args.course, args.repo_name))
+        print(("Warning: Semester '{}' and Course '{}' contains gradeable_id '{}' but it is not a VCS gradeable.").format(args.semester, args.course, args.repo_name))
         print("exiting")
         sys.exit()
 # gradeable does not exist in the course
 elif not args.non_interactive:
-    print ((WARNING + "Warning: Semester '{}' and Course '{}' does not contain gradeable_id '{}'." + DEFAULT).format(args.semester, args.course, args.repo_name))
+    print (("Warning: Semester '{}' and Course '{}' does not contain gradeable_id '{}'.").format(args.semester, args.course, args.repo_name))
     # if the repo_name matches any repo_name in the different gradeables, we should ask the user if they want to continue
     gradeables = course_connection.execute(eg_table.select()).fetchall()
     is_repo_name_in_gradeables = False
@@ -215,7 +213,7 @@ elif not args.non_interactive:
                 sys.exit()
 
     if is_repo_name_in_gradeables == False:
-        print (WARNING + "Warning: Please make the gradeable before attempting to run this script!" + DEFAULT)
+        print ("Warning: Please make the gradeable before attempting to run this script!")
         print ("exiting")
         sys.exit()
 
