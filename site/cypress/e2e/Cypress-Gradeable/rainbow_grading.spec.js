f22a63e6684b27208f40fd6f2c9e24a575e70447
manzil-infinity180 <rahulvs2809@gmail.com>
2024-05-26 00:17:51 +0530

    modified the code and converted id to data-testid , reduced the repetition

--- site/cypress/e2e/Cypress-Gradeable/rainbow_grading.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/rainbow_grading.spec.js
@@ -1,70 +1,61 @@
-const checkRainbowGrades = (username, numericId, firstName, lastName) => {
-    cy.get('#rainbow-grades').should('contain', 'USERNAME');
-    cy.get('#rainbow-grades').should('contain', username);
-    cy.get('#rainbow-grades').should('contain', 'NUMERIC ID');
-    cy.get('#rainbow-grades').should('contain', numericId);
-    cy.get('#rainbow-grades').should('contain', 'FIRST');
-    cy.get('#rainbow-grades').should('contain', firstName);
-    cy.get('#rainbow-grades').should('contain', 'LAST');
-    cy.get('#rainbow-grades').should('contain', lastName);
-    cy.get('#rainbow-grades').should('contain', 'AVERAGE');
-    cy.get('#rainbow-grades').should('contain', 'OVERALL');
-    cy.get('#rainbow-grades').should('contain', 'FINAL GRADE');
-    cy.get('#rainbow-grades').should('contain', `Lecture Participation Polls for: ${username}`);
-};
-
-describe('Test that for sample- rainbow grades', () => {
-    it('can be configured automatically', () => {
-        // Enable
+describe('Test Rainbow Grading', () => {
+    beforeEach(() => {
+        cy.login('instructor');
         cy.visit(['sample', 'config']);
-        cy.login('instructor');
-        cy.get('#display-rainbow-grades-summary').then(($checkbox) => {
-            if (!$checkbox.prop('checked')) {
-                cy.get('#display-rainbow-grades-summary').click();
-            }
-        });
-        cy.get('#display-rainbow-grades-summary').should('be.checked');
 
-        // Check for student
-        cy.login('student');
-        cy.visit(['sample']);
-        cy.get('#nav-sidebar-grades').should('exist').click();
     });
-
-    it('can generate grade summaries and customize what to show', () => {
-        // Generate Grade summaries
-        cy.visit(['sample', 'reports']);
-        cy.login('instructor');
-        cy.get('#grade-summaries-button').click();
-
-        // Customize Rainbow grades
+    it('Enable viewing of rainbow grades and generating the rainbow grading', () => {
+        // enabled the rainbow grades
+        cy.get('[data-testid="display-rainbow-grades-summary"]').should('be.visible').and('not.be.checked');
+        cy.get('[data-testid="display-rainbow-grades-summary"]').should('not.be.checked'); // remove one of this
+        cy.visit(['sample', 'grades']);
+        cy.get('[data-testid="rainbow-grades"]').should('contain', 'No grades are available...');
+        cy.visit(['sample', 'config']);
+        cy.get('[data-testid="display-rainbow-grades-summary"]').check();
+        cy.get('[data-testid="display-rainbow-grades-summary"]').should('be.checked');
+        // generating the rainbow grading
         cy.visit(['sample', 'reports', 'rainbow_grades_customization']);
-        cy.get('#save_status', { timeout: 500000 }).should('contain', 'No changes to save');
         cy.get('#display_grade_summary').then(($checkbox) => {
             if (!$checkbox.prop('checked')) {
-                cy.get('#display_grade_summary').click();
+                cy.get('#display_grade_summary').check();
             }
         });
         cy.get('#display_grade_summary').should('be.checked');
-        cy.get('#save_status_button').click();
-
-        cy.get('#display_grade_summary').should('be.checked');
-        cy.get('#display_grade_details').click();
-        cy.get('#display_section').click();
-        cy.get('#display_benchmark_percent').click();
-        cy.get('#display_final_grade').click();
-        cy.get('#display_benchmarks_average').click();
-        cy.get('#save_status_button').click();
-        cy.get('#save_status', { timeout: 500000 }).should('contain', 'Rainbow grades successfully generated!');
-
-        // Check for instructor
+        cy.get('#display_benchmarks_average').check();
+        cy.get('#display_benchmarks_stddev').check();
+        cy.get('#display_benchmarks_perfect').check();
+        cy.get('[data-testid="save-status-button"]').click();
+        cy.get('[data-testid="save-status"]', { timeout: 500000 }).should('contain', 'Rainbow grades successfully generated!');
         cy.visit(['sample', 'grades']);
-        checkRainbowGrades('instructor', '801516157', 'Quinn', 'Instructor');
-        cy.logout();
-
-        // Check for student
-        cy.login('student');
-        cy.visit(['sample', 'grades']);
-        checkRainbowGrades('student', '410853871', 'Joe', 'Student');
+        ['USERNAME', 'NUMERIC ID', 'AVERAGE', 'STDDEV', 'PERFECT'].forEach((fields) => {
+            cy.get('[data-testid="rainbow-grades"]').should('contain', fields);
+        });
+        cy.get('[data-testid="rainbow-grades"]').should('contain', 'Information last updated');
+        ['instructor', 'ta', 'student', 'grader'].forEach((username) => {
+            cy.logout();
+            cy.login(username);
+            console.log(username);
+            cy.visit(['sample', 'grades']);
+            cy.get('[data-testid="rainbow-grades"]').should('contain', `Lecture Participation Polls for: ${username}`);
+            if (username==='instructor') {
+                checkRainbowGrades(801516157, 'Quinn');
+            }
+            else if (username === 'ta') {
+                checkRainbowGrades(281179137, 'Jill');
+            }
+            else if (username === 'student') {
+                checkRainbowGrades(410853871, 'Joe');
+            }
+            else if (username === 'grader') {
+                checkRainbowGrades(10306042, 'Tim');
+            }
+        });
     });
 });
+
+const checkRainbowGrades = (numericId, firstName) => {
+    [numericId, firstName].forEach((value) => {
+        cy.get('[data-testid="rainbow-grades"]').should('contain', value);
+    });
+};
+
