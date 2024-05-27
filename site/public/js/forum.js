3d13b3ddc05de1cb9e5738014b804ba28865176e
williamschen23 <chenwill2005@gmail.com>
2024-05-27 18:01:56 -0400

    remove extra print statement

--- site/public/js/forum.js
+++ site/public/js/forum.js
@@ -2006,7 +2006,6 @@ function toggleMarkdown(post_box_id, triggered) {
     $(`#markdown_input_${post_box_id}`).val($(`#markdown_input_${post_box_id}`).val() == 0 ? '1':'0');
     $(`#markdown-info-${post_box_id}`).toggleClass('disabled');
     Cookies.set('markdown_enabled', $(`#markdown_input_${post_box_id}`).val(), { path: '/', expires: 365 });
-    console.log($(`#markdown_input_${post_box_id}`).val());
 }
 
 // eslint-disable-next-line no-unused-vars
