710403af48aa49f2970f25b1bb55c9d469b8eb86
manzil-infinity180 <rahulvs2809@gmail.com>
2024-06-03 06:51:32 +0530

    updated setgradeinquiry function

--- site/cypress/e2e/Cypress-Gradeable/grade_inquiries.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/grade_inquiries.spec.js
@@ -1,30 +1,18 @@
 describe('Test cases revolving around grade inquires', () => {
-    const setGradeInquiriesForGradeable = (gradeableId, date = null, allowed = true) => {
+    const setGradeInquiriesForGradeable = (gradeableId, date = null) => {
         cy.visit(['sample', 'gradeable', gradeableId, 'update']);
-
-        if (allowed) {
-            cy.get('[data-testid="yes-grade-inquiry-allowed"]').click();
-        }
-        else {
-            cy.get('[data-testid="no-grade-inquiry-allowed"]').click();
-        }
-
-        if (date) {
-            cy.contains('Dates').click();
-            cy.get('[data-testid="date-grade-inquiry-due"]').click();
-
-            cy.get('[data-testid="date-grade-inquiry-due"]').should('be.visible');
-
-            cy.get('[data-testid="date-grade-inquiry-due"]').clear().type(date, { parseSpecialCharSequences: false, force: true });
-        }
+        cy.get('[data-testid="yes-grade-inquiry-allowed"]').click();
+        cy.contains('Dates').click();
+        cy.get('[data-testid="date-grade-inquiry-due"]').click();
+        cy.get('[data-testid="date-grade-inquiry-due"]').should('be.visible');
+        cy.get('[data-testid="date-grade-inquiry-due"]').clear().type(date, { parseSpecialCharSequences: false, force: true });
     };
 
     it('should test normal submission grade inquiry panel', () => {
-        cy.visit(['sample']);
         cy.login();
         const gradeableId = 'grades_released_homework';
         const gradeInquiryDeadlineDate = '9998-01-01 00:00:00';
-        setGradeInquiriesForGradeable(gradeableId, gradeInquiryDeadlineDate, true);
+        setGradeInquiriesForGradeable(gradeableId, gradeInquiryDeadlineDate);
         cy.visit(['sample', 'gradeable', gradeableId, 'grading', 'details']);
         cy.get('[data-testid="view-sections"]').click();
         cy.get('[data-testid="grade-button"]').first().click();
@@ -35,10 +23,8 @@ describe('Test cases revolving around grade inquires', () => {
         cy.get('[data-testid="markdown-mode-tab-preview"]').first().should('exist');
         cy.get('[data-testid="grade-inquiry-submit-button"]').should('contain', 'Submit Grade Inquiry').and('not.be.disabled');
     });
-    ['grader', 'ta'].forEach((user) => {
+    ['ta', 'grader'].forEach((user) => {
         it(`${user} can see grade inquiry panel`, () => {
-            cy.logout();
-            cy.visit(['sample']);
             cy.login(user);
             const gradeableId = 'grades_released_homework';
             cy.visit(['sample', 'gradeable', gradeableId, 'grading', 'details']);
