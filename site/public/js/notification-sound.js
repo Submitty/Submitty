dac1d494871a1d05fc0d1902e38a90a90e1286a4
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2024-06-19 15:05:47 -0400

    handle async

--- site/public/js/notification-sound.js
+++ site/public/js/notification-sound.js
@@ -3,14 +3,19 @@ class NotificationSound {
     constructor() {
         this.sound = document.createElement('audio');
 
-        if (this.sound.canPlayType('audio/mp3') === '') {
+        if (this.sound.canPlayType('audio/mpeg') === '') {
             throw 'Unable to produce notification sounds.  Browser doesn\'t support mp3 audio files.';
         }
 
-        this.sound.src = 'quack-alert.mp3';
+        this.sound.src = 'site/public/audio/quack-alert.mp3';
     }
 
-    play() {
-        this.sound.play();
+    async play() {
+        try {
+            await this.sound.play();
+        }
+        catch (error) {
+            console.error('Failed to play the notification sound: ', error);
+        }
     }
 }
