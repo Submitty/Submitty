5d3441f10c35c62ca3a5967ad3a7076e883143ed
Tushar <92613357+TusharPuri10@users.noreply.github.com>
2024-06-21 23:42:59 +0530

    [Feature:InstructorUI] Download Button for Rainbow Grades (#10079)
    fixes https://github.com/Submitty/Submitty/issues/10078
    closes #10213 
    
    ### What is the current behavior?
    => Currently the .csv of the grades from rainbow grades is located in
    /var/local/submitty/courses/SEMESTER/COURSE/rainbow_grades/output.csv
    
    ### What is the new behavior?
    => Added a blue "Download CSV of grades" button to get this file from
    the top of the "Gradebook" page.
    <img width="960" alt="image"
    src="https://github.com/Submitty/Submitty/assets/92613357/03d9ecd5-436f-4321-9032-44ceb1416708">
    
    
    
    ### Other information?
    There was PR with an minor update of RainbowGrades.twig made from PR
    #10213, which has been cherry picked to this PR, as well as the video
    demo. The work should be credited accordingly to @MUFFANUJ
    
    
    
    https://github.com/Submitty/Submitty/assets/151390316/06a29734-d22b-4074-9ffc-853bfa60783f
    
    
    Some useful feedbacks from #10213 was not carried over, please consider
    reviewing those comments as well when taking over this PR.
    
    ---------
    
    Signed-off-by: MUFFANUJ <anujsinghhero292@gmail.com>
    Co-authored-by: MUFFANUJ <anujsinghhero292@gmail.com>
    Co-authored-by: Jaeseok Kang <ziesski@gmail.com>
    Co-authored-by: dorian451 <112524240+dorian451@users.noreply.github.com>
    Co-authored-by: William Allen <16820599+williamjallen@users.noreply.github.com>
    Co-authored-by: Jaeseok Kang <123261952+ziesski@users.noreply.github.com>

--- site/app/controllers/admin/ReportController.php
+++ site/app/controllers/admin/ReportController.php
@@ -8,6 +8,7 @@ use app\libraries\DateUtils;
 use app\libraries\FileUtils;
 use app\libraries\GradeableType;
 use app\libraries\routers\AccessControl;
+use app\libraries\response\DownloadResponse;
 use app\libraries\response\MultiResponse;
 use app\libraries\response\JsonResponse;
 use app\libraries\response\RedirectResponse;
@@ -141,7 +142,6 @@ class ReportController extends AbstractController {
         if (!$this->core->getUser()->accessAdmin()) {
             $this->core->getOutput()->showError("This account cannot access admin pages");
         }
-
         $g_sort_keys = [
             'syllabus_bucket',
             'g_id',
@@ -768,7 +768,7 @@ class ReportController extends AbstractController {
     #[Route("/courses/{_semester}/{_course}/gradebook")]
     public function displayGradebook() {
         $grade_path = $this->core->getConfig()->getCoursePath() . "/rainbow_grades/output.html";
-
+        $grade_summaries_last_run = $this->getGradeSummariesLastRun();
         $grade_file = null;
         if (file_exists($grade_path)) {
             $grade_file = file_get_contents($grade_path);
@@ -778,8 +778,49 @@ class ReportController extends AbstractController {
             new WebResponse(
                 ['admin', 'Report'],
                 'showFullGradebook',
-                $grade_file
+                $grade_file,
+                $grade_summaries_last_run
             )
         );
+    }
+
+    /**
+     * Generate a custom filename for the downloaded CSV file
+     */
+    private function generateCustomFilename(): string {
+        $course = $this->core->getConfig()->getCourse();
+        $timestamp = DateUtils::getFileNameTimeStamp();
+        return "{$course}_rainbow_grades_{$timestamp}.csv";
+    }
+
+
+    /**
+     * Download CSV file for Rainbow Grades
+     */
+    #[Route("/courses/{_semester}/{_course}/reports/rainbow_grades_csv")]
+    public function downloadRainbowGradesCSVFile(): ?DownloadResponse {
+        // Path to the CSV file for Rainbow Grades
+        $csvFilePath = FileUtils::joinPaths(
+            '/var/local/submitty/courses',
+            $this->core->getConfig()->getTerm(),
+            $this->core->getConfig()->getCourse(),
+            'rainbow_grades',
+            'output.csv'
+        );
+
+
+        // Check if the file exists
+        if (file_exists($csvFilePath)) {
+            return DownloadResponse::getDownloadResponse(
+                file_get_contents($csvFilePath),
+                $this->generateCustomFilename(),
+                "application/csv"
+            );
+        }
+        else {
+            // Handle the case where the file does not exist
+            $this->core->getOutput()->showError($csvFilePath . " was not found or was not readable.\nMaybe you have not <a\thref='./rainbow_grades_customization'>generated the rainbow grades</a> yet?");
+            return null;
+        }
     }
 }
