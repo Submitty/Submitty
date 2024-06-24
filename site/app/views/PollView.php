a15846a8ce489bcc3f38c8ce5f8730384429db5b
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2024-06-24 18:03:34 -0400

    fix toggle-all

--- site/app/views/PollView.php
+++ site/app/views/PollView.php
@@ -91,11 +91,13 @@ class PollView extends AbstractView {
         $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('flatpickr', 'flatpickr.min.css'));
         $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('flatpickr', 'plugins', 'shortcutButtons', 'shortcut-buttons-flatpickr.min.js'));
         $this->core->getOutput()->addVendorCss(FileUtils::joinPaths('flatpickr', 'plugins', 'shortcutButtons', 'themes', 'light.min.css'));
+        $is_survey = $poll !== null ? $poll->isSurvey() : false;
         return $this->core->getOutput()->renderTwigTemplate("polls/PollForm.twig", [
             'csrf_token' => $this->core->getCsrfToken(),
             'base_url' => $this->core->buildCourseUrl() . '/polls',
             'poll' => $poll,
             'max_size' => Utils::returnBytes(ini_get('upload_max_filesize')),
+            'is_survey' => $is_survey,
           ]);
     }
 }
