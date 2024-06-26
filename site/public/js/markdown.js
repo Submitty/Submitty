8780d4bd8fb67a64381bad8a7fb1090db9d3817f
Zheyu Deng <sony1119266216@gmail.com>
2024-06-26 14:57:21 -0400

    fix minor bug

--- site/public/js/markdown.js
+++ site/public/js/markdown.js
@@ -25,23 +25,46 @@ $(document).ready(() => {
             targetTextarea.off('input').on('input', function() {
                 resizeTextarea(this);
             });
-
             resizeTextarea(targetTextarea.get(0));
+        });
+    };
+
+    const initializeMarkdownArea = (markdownAreaId) => {
+        enableTabsInTextArea(`#${markdownAreaId}`);
+        const markdown_area = $(`#${markdownAreaId}`).closest('.markdown-area');
+        markdown_area.find('[data-initialize-preview="1"]').trigger('click');
+    };
+
+    const applyMarkdownListeners = () => {
+        $('textarea[id^="reply_box"]').each(function() {
+            const targetTextarea = $(this);
+            const markdownAreaId = targetTextarea.attr('id');
+            initializeMarkdownArea(markdownAreaId);
+        });
+
+        $('.markdown-mode-tab').on('click', function() {
+            $(this).addClass('active');
+            const markdown_area = $(this).closest('.markdown-area');
+            markdown_area.find('.markdown-mode-tab').not(this).removeClass('active');
         });
     };
 
     $(document).ajaxComplete(() => {
         applyResizeListeners();
+        applyMarkdownListeners();
     });
 
     // Initial application of listeners when the page loads
     applyResizeListeners();
+    applyMarkdownListeners();
 
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             mutation.addedNodes.forEach((node) => {
                 if (node.nodeType === 1 && node.matches('textarea[id^="reply_box"]')) {
                     resizeTextarea(node);
+                    const markdownAreaId = node.id;
+                    initializeMarkdownArea(markdownAreaId);
                 }
             });
         });
