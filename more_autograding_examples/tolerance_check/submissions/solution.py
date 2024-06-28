fddef0850964da6811cc38dbe2e639b5d1e6e8f1
Barb Cutler <Barb Cutler>
2024-06-28 14:31:49 -0400

    update solution

--- more_autograding_examples/tolerance_check/submissions/solution.py
+++ more_autograding_examples/tolerance_check/submissions/solution.py
@@ -1,4 +1,5 @@
 import sys
+import decimal
 
 def is_positive_float(num):
     try:
@@ -23,11 +24,13 @@ def standard_deviation(lst, avg):
         v += (element - avg)**2
     return (v/(n - 1))**0.5
 
-def format_line(line_lst):
-    first = len(line_lst[0])
-    n_spaces = 8 - first
-    spaces = " "*n_spaces
-    return spaces.join(line_lst)
+
+def print_line(numneg, sd, avg):
+    if (numneg == 0):
+        print("| NONE |" + "%10.3f" % avg + " |" + "%10.3f" % sd + " |")
+    else:
+        print("|" + "%5d" % numneg + " |" + "%10.3f" % avg + " |" + "%10.3f" % sd + " |")
+
 
 args = len(sys.argv)
 
@@ -40,11 +43,18 @@ txt_file = open(file, "r")
 
 content_list = txt_file.readlines()
 
-print("     ".join(["AVG", "SD"]))
+print ("  #neg         AVG          SD  ")
+print ("+------------------------------+")
 
 for line in content_list:
+    tmp = list(filter(lambda x: not(is_positive_float(x)), line.rstrip().split(",")))
+    numneg = len(tmp)
+    
     lst = list(filter(lambda x: is_positive_float(x), line.rstrip().split(",")))
     lst = list(map(float, lst))
     avg = average(lst)
     sd = standard_deviation(lst, avg)
-    print(format_line([str(round(avg, 3)), str(round(sd, 3))]))
+
+    print_line(numneg,avg,sd)
+
+print ("+------------------------------+")
