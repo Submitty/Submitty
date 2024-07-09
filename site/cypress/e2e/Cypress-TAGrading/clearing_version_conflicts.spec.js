4aa7c9e8980f5b3d7a5c1c13b7cd0987a8a3b281
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-07-09 13:18:36 -0400

    add testing to make sure save button will resolve version conflicts

--- site/cypress/e2e/Cypress-TAGrading/clearing_version_conflicts.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/clearing_version_conflicts.spec.js
@@ -38,4 +38,47 @@ describe('Test cases for checking the clear version conflicts button in the TA g
         cy.reload();
         cy.get('.version-warning').should('exist');
     });
+
+    it('Clicking "save" on a component with a version conflict should resolve the conflict for that component', () => {
+        cy.login('instructor');
+        cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=K8jI3q4qpdCc1jw&sort=id&direction=ASC&gradeable_version=1']);
+        cy.get('#grading_rubric_btn').click();
+
+        // wait until page is fully loaded
+        cy.get('#component-list').children().should('have.length.least', 1);
+        cy.get('#component-list').children().eq(0).as('test-component');
+
+        cy.get('@test-component').within(() => {
+            cy.get('.version-warning').should('exist');
+            cy.get('.box-title').click();
+
+            cy.get('.save-tools-save').contains('Save (and resolve version conflict)');
+            cy.get('.save-tools-save').click();
+
+            cy.get('.version-warning', { timeout: 10000 }).should('not.exist');
+        });
+
+        cy.reload();
+
+        cy.get('#component-list').children().should('have.length.least', 1);
+        cy.get('#component-list').children().eq(0).as('test-component');
+        cy.get('@test-component').within(() => {
+            cy.get('.version-warning').should('not.exist');
+        });
+
+        // reset state
+        cy.get('@test-component').children().eq(0).then((el) => {
+            cy.window().then(async (win) => {
+                await win.ajaxChangeGradedVersion(win.getGradeableId(), win.getAnonId(), 2, [win.getComponentIdFromDOMElement(el[0])]);
+            });
+        });
+
+        cy.reload();
+
+        cy.get('#component-list').children().should('have.length.least', 1);
+        cy.get('#component-list').children().eq(0).as('test-component');
+        cy.get('@test-component').within(() => {
+            cy.get('.version-warning').should('exist');
+        });
+    });
 });
