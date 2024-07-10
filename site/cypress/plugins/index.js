74e77accc510cc07ebceef94b9c5d701f7d2bbfd
Rahul Vishwakarma <rahulvs2809@gmail.com>
2024-07-10 15:21:51 +0530

    Updated plugin file

--- site/cypress/plugins/index.js
+++ site/cypress/plugins/index.js
@@ -23,7 +23,3 @@ module.exports = (on, config) => {
     return getCompareSnapshotsPlugin(on, config);
 
 };
-export default function (/* on, config */) {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
-}
