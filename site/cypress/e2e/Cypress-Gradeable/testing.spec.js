6f36c22c13428bd65b5cbeb3a6e72032e676c240
IDzyre <reapernsgaming@gmail.com>
2024-07-10 11:40:29 -0800

    eslint fix

--- site/cypress/e2e/Cypress-Gradeable/testing.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/testing.spec.js
@@ -1,5 +1,4 @@
 describe('TA grading hotkey testing', () => {
-
     it('toggle keyboard shortcut', () => {
         cy.login();
         cy.visit(['testing', 'gradeable', 'open_homework', 'grading']);
