88f0ba0bea7b66d32cda0676bda20df449b7800f
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-10 13:36:04 +0530

    visual testing setup and test

---
+++ site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+describe('Visual testing', () => {
+    it('autograding markdown testing', () => {
+        cy.viewport(1100, 1050);
+        cy.login('instructor');
+        cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=jKjodWaRdEV9pBb&sort=id&direction=ASC']);
+        cy.get('#tc_expand_all').find('.loading-tools').click();
+        cy.get('.box-block').scrollIntoView().compareSnapshot('autograding_code',  1.0, {
+            capture: 'viewport',
+            clip: { x: 0, y: 0, width: 1100, height: 1050 },
+        });
+    })
+})
