f99748014f9809ff27cfcf1cd848d01c8f65f954
Zheyu Deng <sony1119266216@gmail.com>
2024-07-12 19:32:58 -0400

    simplified

--- site/public/js/grade-override.js
+++ site/public/js/grade-override.js
@@ -1,40 +1,25 @@
 function setCookie(name, value, days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    Cookies.set(name, value, { expires: days, path: '/' });
 }
 
 function getCookie(name) {
-    const cname = name + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(cname) === 0) {
-            return c.substring(cname.length, c.length);
-        }
-    }
-    return "";
+    return Cookies.get(name) || '';
 }
 
 function checkSelectedGradeable() {
-    const selectedGradable = getCookie("selectedGradable");
+    const selectedGradable = getCookie('selectedGradable');
     if (selectedGradable) {
         $('#g_id').val(selectedGradable);
         loadOverriddenGrades(selectedGradable);
     }
 }
 
-$(document).ready(function() {
+$(document).ready(() => {
     checkSelectedGradeable();
-    $('#g_id').change(function() {
+    $('#g_id').change(function() {  // Use function here to access 'this'
         const selectedGradable = $(this).val();
         if (selectedGradable) {
-            setCookie("selectedGradable", selectedGradable, 365);
+            setCookie('selectedGradable', selectedGradable, 365);
             loadOverriddenGrades(selectedGradable);
         }
     });
