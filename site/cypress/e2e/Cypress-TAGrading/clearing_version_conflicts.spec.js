2e017659349bacf01807dca03e21011458b7b6b2
dorian451 <112524240+dorian451@users.noreply.github.com>
2024-07-15 11:07:06 -0400

    change tests to use test ids

--- site/cypress/e2e/Cypress-TAGrading/clearing_version_conflicts.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/clearing_version_conflicts.spec.js
@@ -2,33 +2,33 @@ describe('Test cases for checking the clear version conflicts button in the TA g
     it('Button should not appear if there are no version conflicts', () => {
         cy.login('instructor');
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=LHmpGciilVzjcEJ&sort=id&direction=ASC']);
-        cy.get('#grading_rubric_btn').click();
+        cy.get('[data-testid="grading-rubric-btn"]').click();
         cy.get('[data-testid="change-graded-version"]').should('not.exist');
-        cy.get('.version-warning').should('not.exist');
+        cy.get('[data-testid="version-warning"]').should('not.exist');
     });
 
     it('Button should appear if there are version conflicts', () => {
         cy.login('instructor');
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=K8jI3q4qpdCc1jw&sort=id&direction=ASC&gradeable_version=1']);
-        cy.get('#grading_rubric_btn').click();
+        cy.get('[data-testid="grading-rubric-btn"]').click();
         cy.get('[data-testid="change-graded-version"]').should('exist');
-        cy.get('.version-warning').should('exist');
+        cy.get('[data-testid="version-warning"]').should('exist');
     });
 
     it('Clicking the button should resolve those version conflicts', () => {
         cy.login('instructor');
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=K8jI3q4qpdCc1jw&sort=id&direction=ASC&gradeable_version=1']);
-        cy.get('#grading_rubric_btn').click();
+        cy.get('[data-testid="grading-rubric-btn"]').click();
 
         // wait until page is fully loaded
-        cy.get('#component-list').children().should('have.length.least', 1);
+        cy.get('[data-testid="component-list"]').children().should('have.length.least', 1);
 
-        cy.get('.version-warning').should('exist');
+        cy.get('[data-testid="version-warning"]').should('exist');
 
         cy.get('[data-testid="change-graded-version"]').click();
         cy.get('[data-testid="change-graded-version"]', { timeout: 10000 }).should('not.be.visible');
 
-        cy.get('.version-warning').should('not.exist');
+        cy.get('[data-testid="version-warning"]').should('not.exist');
 
         // reset state
         cy.window().then(async (win) => {
@@ -36,6 +36,6 @@ describe('Test cases for checking the clear version conflicts button in the TA g
         });
 
         cy.reload();
-        cy.get('.version-warning').should('exist');
+        cy.get('[data-testid="version-warning"]').should('exist');
     });
 });
