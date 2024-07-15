f221afb2930593d456ea1a2c5581f6372466daad
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-16 00:49:33 +0530

    added one assertion

--- site/cypress/e2e/Cypress-TAGrading/zip_file_download.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/zip_file_download.spec.js
@@ -5,7 +5,8 @@ describe('download zip file', () => {
         cy.login();
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=hG1b13ljpDjKu32&sort=id&direction=ASC']);
         cy.get('body').type('{A}');
-        cy.get('body').type('{O}');
+        cy.get('body').type('{O}'); // Solution/Notes
+        cy.get('[data-testid="download-zip-file"]').should('contain', 'Download Zip File');
         cy.get('[data-testid="download-zip-file"]').click();
         const filename = path.join(downloadsFolder, 'grading_homework_hG1b13ljpDjKu32_v1.zip');
         cy.readFile(filename);
