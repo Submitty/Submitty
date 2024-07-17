a9fc3be68b640a36b2423c0c882e637e96fed15a
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-17 11:49:09 +0530

    changed the path for autograding test

--- site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
+++ site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
@@ -2,7 +2,7 @@ describe('Visual testing', () => {
     it('autograding markdown testing', () => {
         cy.viewport(1150, 900);
         cy.login('instructor');
-        cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=jKjodWaRdEV9pBb&sort=id&direction=ASC']);
+        cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
         cy.get('#nav-sidebar-collapse-sidebar').click();
         cy.get('[data-testid="details-tc-expand-all"]').click();
         cy.get('[data-testid="test-box-block"]').scrollIntoView().compareSnapshot('autograding_code', 1.0, {
