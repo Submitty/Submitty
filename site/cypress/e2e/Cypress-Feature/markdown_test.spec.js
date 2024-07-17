d95b0ca15cd2554bdbd5d9509c0f867afaf27782
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-18 00:01:46 +0530

    new image

--- site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
+++ site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
@@ -1,13 +1,14 @@
 describe('Visual testing', () => {
     it('autograding markdown testing', () => {
-        cy.viewport(1150, 900);
+        // cy.viewport(1150, 900);
         cy.login('instructor');
         cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
         cy.get('#nav-sidebar-collapse-sidebar').click();
         cy.get('[data-testid="details-tc-expand-all"]').click();
+        cy.wait(5000);
         cy.get('[data-testid="test-box-block"]').scrollIntoView().compareSnapshot('autograding_code', 1.0, {
-            capture: 'viewport',
-            clip: { x: 0, y: 0, width: 1150, height: 900 },
+            capture: 'fullPage',
+            clip: { x: 0, y: 20, width: 1150, height: 900 },
         });
     });
 });
