321310ae63ac76731bc4ea40961403cff59a7a69
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-23 18:14:19 +0530

    scroll into view

--- site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
+++ site/cypress/e2e/Cypress-Feature/markdown_test.spec.js
@@ -5,9 +5,8 @@ describe('Visual testing', () => {
         cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
         cy.get('#nav-sidebar-collapse-sidebar').click();
         cy.get('[data-testid="details-tc-expand-all"]').click();
-        cy.wait(5000);
-        cy.get('[data-testid="test-box-block"]').scrollIntoView().compareSnapshot('autograding_code', 1.0, {
-            capture: 'fullPage',
+        cy.get('[data-testid="test-box-block"]').scrollIntoView().should('be.visible');
+        cy.get('[data-testid="test-box-block"]').compareSnapshot('autograding_code', 1.0, {
             clip: { x: 0, y: 20, width: 1150, height: 900 },
         });
     });
