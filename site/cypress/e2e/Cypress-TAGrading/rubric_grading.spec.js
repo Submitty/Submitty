10f27985a796682cf5ecf4b94beea4ce86da100e
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-25 00:19:17 +0530

    changed data attributes

--- site/cypress/e2e/Cypress-TAGrading/rubric_grading.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/rubric_grading.spec.js
@@ -10,44 +10,45 @@ describe('Test cases for TA grading page', () => {
         cy.get('[data-testid="component-container"]').eq(1).should('contain', 'Coding Style');
         cy.get('[data-testid="component-container"]').eq(2).should('contain', 'Documentation');
         cy.get('[data-testid="component-container"]').eq(3).should('contain', 'Extra Credit');
-        cy.get('[data-testid="component-container"]').eq(0).click();
-        cy.get('[data-testid="component-container"]').eq(0)
+        cy.get('[data-testid="component-64"]').should('contain', 'Read Me');
+        cy.get('[data-testid="component-64"]').click();
+        cy.get('[data-testid="component-64"]')
             .should('contain', 'Full Credit')
             .should('contain', 'Minor errors in Read Me')
             .should('contain', 'Major errors in Read Me or Read Me missing');
         cy.get('body').type('{0}');
         cy.get('[data-testid="save-tools-save"]').should('contain', 'Save');
         cy.get('[data-testid="save-tools-save"]').click();
-        cy.get('[data-testid="component-container"]').eq(1).should('contain', 'Coding Style');
-        cy.get('[data-testid="component-container"]').eq(1).click();
-        cy.get('[data-testid="component-container"]').eq(1)
+        cy.get('[data-testid="component-65"]').should('contain', 'Coding Style');
+        cy.get('[data-testid="component-65"]').click();
+        cy.get('[data-testid="component-65"]')
             .should('contain', 'Full Credit')
             .should('contain', 'Code is unreadable')
             .should('contain', 'Code is very difficult to understand')
             .should('contain', 'Code is difficult to understand');
         cy.get('body').type('{3}');
         cy.get('[data-testid="save-tools-save"]').click();
-        cy.get('[data-testid="component-container"]').eq(2).should('contain', 'Documentation');
-        cy.get('[data-testid="component-container"]').eq(2).click();
-        cy.get('[data-testid="component-container"]').eq(2)
+        cy.get('[data-testid="component-66"]').should('contain', 'Documentation');
+        cy.get('[data-testid="component-66"]').click();
+        cy.get('[data-testid="component-66"]')
             .should('contain', 'Full Credit')
             .should('contain', 'No documentation')
             .should('contain', 'Very little documentation or documentation makes no sense')
             .should('contain', 'Way too much documentation and/or documentation makes no sense');
         cy.get('body').type('{2}');
         cy.get('[data-testid="save-tools-save"]').click();
-        cy.get('[data-testid="component-container"]').eq(3).should('contain', 'Extra Credit');
-        cy.get('[data-testid="component-container"]').eq(3).click();
-        cy.get('[data-testid="component-container"]').eq(3)
+        cy.get('[data-testid="component-67"]').should('contain', 'Extra Credit');
+        cy.get('[data-testid="component-67"]').click();
+        cy.get('[data-testid="component-67"]')
             .should('contain', 'Full Credit')
             .should('contain', 'Extra credit done poorly')
             .should('contain', 'Extra credit is acceptable');
         cy.get('body').type('{0}');
         cy.get('[data-testid="save-tools-save"]').click();
-        cy.get('[data-testid="grading_total"]').eq(1).should('contain', '2 / 2');
-        cy.get('[data-testid="grading_total"]').eq(2).should('contain', '2 / 5');
-        cy.get('[data-testid="grading_total"]').eq(3).should('contain', '2 / 5');
-        cy.get('[data-testid="grading_total"]').eq(4).should('contain', '0 / 0');
-        cy.get('[data-testid="score-total"]').eq(5).should('contain', '6 / 12').should('contain', 'Total');
+        cy.get('[data-testid="grading-total"]').eq(0).should('contain', '2 / 2');
+        cy.get('[data-testid="grading-total"]').eq(1).should('contain', '2 / 5');
+        cy.get('[data-testid="grading-total"]').eq(2).should('contain', '2 / 5');
+        cy.get('[data-testid="grading-total"]').eq(3).should('contain', '0 / 0');
+        cy.get('[data-testid="grading-total"]').eq(4).should('contain', '6 / 12');
     });
 });
