9c66dd36315e47e9a4bffd150fcd47c838d3785c
IDzyre <reapernsgaming@gmail.com>
2024-08-08 11:32:23 -0800

    Remove users edit, change to only unverified_users and dump database

--- migration/migrator/data/submitty_db.sql
+++ migration/migrator/data/submitty_db.sql
@@ -647,6 +647,27 @@ CREATE TABLE public.terms (
 
 
 --
+-- Name: unverified_users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.unverified_users (
+    user_id character varying NOT NULL,
+    user_numeric_id character varying,
+    user_givenname character varying NOT NULL,
+    user_preferred_givenname character varying,
+    user_password character varying,
+    user_familyname character varying NOT NULL,
+    user_email_secondary character varying(255) DEFAULT ''::character varying NOT NULL,
+    user_email_secondary_notify boolean DEFAULT false,
+    user_pronouns character varying(255) DEFAULT ''::character varying,
+    user_preferred_familyname character varying,
+    user_email character varying NOT NULL,
+    verification_code character varying(50) DEFAULT 'none'::character varying NOT NULL,
+    verification_expiration timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
--- migration/migrator/migrations/master/20240805165505_user_verification_code.py
+++ migration/migrator/migrations/master/20240805165505_user_verification_code.py
@@ -12,27 +12,21 @@ def up(config, database):
     """
 
     database.execute('''
-        ALTER TABLE users ADD COLUMN IF NOT EXISTS is_verified boolean default true,
-                    ADD COLUMN IF NOT EXISTS verification_code VARCHAR(50) default '',
-                    ADD COLUMN IF NOT EXISTS verification_expiration TIMESTAMP default CURRENT_TIMESTAMP;
-    ''')
-
-    database.execute('''
         CREATE TABLE if not exists public.unverified_users (
-    user_id character varying NOT NULL,
-    user_numeric_id character varying,
-    user_givenname character varying NOT NULL,
-    user_preferred_givenname character varying,
-    user_password character varying,
-    user_familyname character varying NOT NULL,
-    user_email_secondary character varying(255) DEFAULT ''::character varying NOT NULL,
-    user_email_secondary_notify boolean DEFAULT false,
-    user_pronouns character varying(255) DEFAULT ''::character varying,
-    user_preferred_familyname character varying,
-    user_email character varying NOT NULL,
-    verification_code character varying(50) NOT NULL DEFAULT 'none',
-    verification_expiration timestamp DEFAULT current_timestamp
-    )
+            user_id character varying NOT NULL,
+            user_numeric_id character varying,
+            user_givenname character varying NOT NULL,
+            user_preferred_givenname character varying,
+            user_password character varying,
+            user_familyname character varying NOT NULL,
+            user_email_secondary character varying(255) DEFAULT ''::character varying NOT NULL,
+            user_email_secondary_notify boolean DEFAULT false,
+            user_pronouns character varying(255) DEFAULT ''::character varying,
+            user_preferred_familyname character varying,
+            user_email character varying NOT NULL,
+            verification_code character varying(50) NOT NULL DEFAULT 'none',
+            verification_expiration timestamp DEFAULT current_timestamp
+        )
     ''')
     pass
 
