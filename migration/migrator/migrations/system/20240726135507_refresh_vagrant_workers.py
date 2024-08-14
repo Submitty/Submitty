fe6c3889cd594c74505c75741cc8141940c03d93
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2024-08-14 13:09:33 -0400

    fix alias migration

--- migration/migrator/migrations/system/20240726135507_refresh_vagrant_workers.py
+++ migration/migrator/migrations/system/20240726135507_refresh_vagrant_workers.py
@@ -8,7 +8,17 @@ def up(config):
     :param config: Object holding configuration details about Submitty
     :type config: migrator.config.Config
     """
-    os.system("alias refresh_vagrant_workers='python3 /usr/local/submitty/GIT_CHECKOUT/Submitty/.setup/bin/refresh_vagrant_workers.py'")
+    with open('/root/.bashrc', 'r') as f:
+        lines = f.readlines()
+
+    num = len(lines)
+    for i, line in enumerate(lines):
+        if line.startswith('alias '):
+            num = i + 1
+
+    lines.insert(num, "alias refresh_vagrant_workers='python3 /usr/local/submitty/GIT_CHECKOUT/Submitty/.setup/bin/refresh_vagrant_workers.py'\n")
+    with open('/root/.bashrc', 'w') as f:
+        f.writelines(lines)
 
 def down(config):
     """
