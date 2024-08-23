eff8c483f7ced9e9dd9204cb66e1cd6fcc48b92b
williamschen23 <chenwill2005@gmail.com>
2024-08-23 13:52:32 -0400

    use should not contains

--- site/cypress/e2e/Cypress-Feature/student_version_conflict.spec.js
+++ site/cypress/e2e/Cypress-Feature/student_version_conflict.spec.js
@@ -1,16 +1,14 @@
-import { buildUrl } from '/cypress/support/utils.js';
-
 const scores = [];
 describe('Test revolving around the student side and whether or not they should see a version conflict', () => {
     it('Student should have a version conflict', () => {
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'grades_released_homework_onlytaEC']);
 
-        cy.get('[data-testid="version-conflict-version-box"]').contains('Note: The version you have selected to be graded is not the version graded by the instructor/TAs.');
-        cy.get('[data-testid="version-conflict-version-box"]').contains('If the graded version does not match your selected version, a zero will be recorded in the gradebook.');
+        cy.get('[data-testid="version-conflict-version-box"]').should('contain', 'Note: The version you have selected to be graded is not the version graded by the instructor/TAs.');
+        cy.get('[data-testid="version-conflict-version-box"]').should('contain', 'If the graded version does not match your selected version, a zero will be recorded in the gradebook.');
 
         cy.get('[data-testid="version-conflict-ta"]').should('exist');
-        cy.get('[data-testid="version-conflict-ta"]').contains('has been detected in your submission. A grade of zero will be recorded in the gradebook. Please resolve as necessary.');
+        cy.get('[data-testid="version-conflict-ta"]').should('contain', 'has been detected in your submission. A grade of zero will be recorded in the gradebook. Please resolve as necessary.');
 
         cy.get('[data-testid="ta-results-box"').should('have.css', 'backgroundColor', 'rgb(217, 83, 79)');
 
@@ -23,7 +21,7 @@ describe('Test revolving around the student side and whether or not they should 
         });
 
         cy.get('[data-testid="ta-grade-results"]').each(($el, index) => {
-            cy.wrap($el).contains('For Version #2');
+            cy.wrap($el).should('contain', 'For Version #2');
         });
     });
 
