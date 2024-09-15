7b25418305b95012b11ed9a039464c0820615df5
Barb Cutler <Barb Cutler>
2024-09-14 22:01:53 -0400

    most of the shell lint

--- sbin/docker_cleanup.sh
+++ sbin/docker_cleanup.sh
@@ -12,8 +12,8 @@ if [[ "$UID" -ne "0" ]] ; then
 fi
 
 mkdir -p "/var/local/submitty/logs/docker_cleanup/"
-logfile_name="/var/local/submitty/logs/docker_cleanup/"`date "+%Y%m%d"`".txt"
-echo  "" >> $logfile_name
+logfile_name="/var/local/submitty/logs/docker_cleanup/$(date +%Y%m%d).txt"
+echo  "" >> "${logfile_name}"
 
 # =================================================================================
 function cleanup_dead_networks() {
@@ -25,8 +25,8 @@ function cleanup_dead_networks() {
         # if the autograding_tmp directory is non-empty,
         # there is active autograding for that worker
         if test -n "$(ls /var/local/submitty/autograding_tmp/${name})"; then
-            date_time=`date "+%Y%m%d %H:%M:%S"`
-            echo "${date_time} | ${name} has grading in progress" >> $logfile_name
+            date_time=$(date "+%Y%m%d %H:%M:%S")
+            echo "${date_time} | ${name} has grading in progress" >> "${logfile_name}"
             return
         fi
     fi
@@ -34,56 +34,56 @@ function cleanup_dead_networks() {
     # check for docker networks matching the worker name
     networks=$(docker network ls | grep ${name} | awk '{print $2}')
     if test -z "${networks}"; then
-        #echo "${date_time} | ${name} has no zombie docker network" >> $logfile_name
+        #echo "${date_time} | ${name} has no zombie docker network" >> "${logfile_name}"
         if [ $# -eq 1 ]; then
             echo "no docker networks to cleanup"
         fi
         return;
     fi
 
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | ${name} has a zombie docker network(s), force docker restart required" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | ${name} has a zombie docker network(s), force docker restart required" >> "${logfile_name}"
 
     # forceably cleanup the network(s) - restart seems to be necessary
     # NOTE: This will likely break docker autograding current in progress
     # by other workers on the same machine.
     systemctl restart docker
 
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | force docker restart complete" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | force docker restart complete" >> "${logfile_name}"
 
     for network in ${networks}; do
-        date_time=`date "+%Y%m%d %H:%M:%S"`
-        echo "${date_time} | removing zombie docker network ${network}" >> $logfile_name
+        date_time=$(date "+%Y%m%d %H:%M:%S")
+        echo "${date_time} | removing zombie docker network ${network}" >> "${logfile_name}"
         docker network rm ${network}
         if [ $# -eq 1 ]; then
             echo "killed zombie ${network}"
         fi
     done
 
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | force cleanup of zombie docker networks complete" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | force cleanup of zombie docker networks complete" >> "${logfile_name}"
 }
 # =================================================================================
 
 if [ $# -eq 0 ]; then
     # loop over all workers
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | docker_cleanup.sh started" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | docker_cleanup.sh started" >> "${logfile_name}"
     for dir in /var/local/submitty/autograding_tmp/*; do
         name=$(basename ${dir})
         cleanup_dead_networks ${name}
     done
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | docker_cleanup.sh finished" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | docker_cleanup.sh finished" >> "${logfile_name}"
 elif [ $# -eq 1 ]; then
     # only cleanup one worker
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | docker_cleanup.sh $1 started" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | docker_cleanup.sh $1 started" >> "${logfile_name}"
     cleanup_dead_networks $1  
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | docker_cleanup.sh $1 finished" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | docker_cleanup.sh $1 finished" >> "${logfile_name}"
 else
-    date_time=`date "+%Y%m%d %H:%M:%S"`
-    echo "${date_time} | docker_cleanup.sh - bad arguments" >> $logfile_name
+    date_time=$(date "+%Y%m%d %H:%M:%S")
+    echo "${date_time} | docker_cleanup.sh - bad arguments" >> "${logfile_name}"
 fi  
