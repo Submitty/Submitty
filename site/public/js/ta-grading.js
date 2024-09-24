ff4514b6680f2ea0dbf4a1148424146da7f6de4e
williamschen23 <chenwill2005@gmail.com>
2024-09-24 16:42:17 -0400

    fix

--- site/public/js/ta-grading.js
+++ site/public/js/ta-grading.js
@@ -647,10 +647,16 @@ function updateCookies() {
 // -----------------------------------------------------------------------------
 // Student navigation
 
-function waitForAllAjaxToComplete() {
-    if ($.active > 0) {
-        setTimeout(waitForAllAjaxToComplete, 100);
-    }
+function waitForAllAjaxToComplete(callback) {
+    const checkAjax = () => {
+        if ($.active > 0) {
+            setTimeout(checkAjax, 100);
+        }
+        else {
+            callback();
+        }
+    };
+    checkAjax();
 }
 
 function gotoMainPage() {
@@ -658,8 +664,9 @@ function gotoMainPage() {
 
     if (getGradeableId() !== '') {
         closeAllComponents(true).then(() => {
-            waitForAllAjaxToComplete();
-            window.location = window_location;
+            waitForAllAjaxToComplete(() => {
+                window.location = window_location;
+            });
         }).catch(() => {
             if (confirm('Could not save open component, go to main page anyway?')) {
                 window.location = window_location;
@@ -717,8 +724,9 @@ function gotoPrevStudent() {
 
     if (getGradeableId() !== '') {
         closeAllComponents(true).then(() => {
-            waitForAllAjaxToComplete();
-            window.location = window_location;
+            waitForAllAjaxToComplete(() => {
+                window.location = window_location;
+            });
         }).catch(() => {
             if (confirm('Could not save open component, change student anyway?')) {
                 window.location = window_location;
@@ -769,15 +777,15 @@ function gotoNextStudent() {
             window_location += `&component_id=${getFirstOpenComponentId()}`;
             break;
     }
-
     if (!navigate_assigned_students_only) {
         window_location += '&navigate_assigned_students_only=false';
     }
 
     if (getGradeableId() !== '') {
         closeAllComponents(true).then(() => {
-            waitForAllAjaxToComplete();
-            window.location = window_location;
+            waitForAllAjaxToComplete(() => {
+                window.location = window_location;
+            });
         }).catch(() => {
             if (confirm('Could not save open component, change student anyway?')) {
                 window.location = window_location;
