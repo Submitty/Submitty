08e193fcad0ca8cf4ec1265b95a790f5bd0403da
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-10-01 17:05:25 -0400

    Move ajaxGetMarkStats in TAGrading to async/await

--- site/public/js/ta-grading-rubric.js
+++ site/public/js/ta-grading-rubric.js
@@ -524,11 +524,14 @@ async function ajaxSaveMark(gradeable_id, component_id, mark_id, title, points, 
  * @param {string} gradeable_id
  * @param {int} component_id
  * @param {int} mark_id
- * @return {Promise} Rejects except when the response returns status 'success'
+ * @async
+ * @throws {Error} Throws except when the response returns status 'success'
+ * @return {Object}
  */
-function ajaxGetMarkStats(gradeable_id, component_id, mark_id) {
-    return new Promise((resolve, reject) => {
-        $.getJSON({
+async function ajaxGetMarkStats(gradeable_id, component_id, mark_id) {
+    let response;
+    try {
+        response = await $.getJSON({
             type: 'POST',
             async: AJAX_USE_ASYNC,
             url: buildCourseUrl(['gradeable', gradeable_id, 'components', 'marks', 'stats']),
@@ -537,21 +540,19 @@ function ajaxGetMarkStats(gradeable_id, component_id, mark_id) {
                 mark_id: mark_id,
                 csrf_token: csrfToken,
             },
-            success: function (response) {
-                if (response.status !== 'success') {
-                    console.error(`Something went wrong getting mark stats: ${response.message}`);
-                    reject(new Error(response.message));
-                }
-                else {
-                    resolve(response.data);
-                }
-            },
-            error: function (err) {
-                displayAjaxError(err);
-                reject(err);
-            },
         });
-    });
+    }
+    catch (err) {
+        displayAjaxError(err);
+        throw err;
+    }
+    if (response.status !== 'success') {
+        console.error(`Something went wrong getting mark stats: ${response.message}`);
+        throw new Error(response.message);
+    }
+    else {
+        return response.data;
+    }
 }
 
 /**
