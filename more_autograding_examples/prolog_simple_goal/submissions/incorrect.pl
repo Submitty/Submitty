13623ff2d6ab079c8fbd7c3fdb5ee1a63963e35f
Chris McCarthy <adsalor@gmail.com>
2024-10-31 15:52:49 -0400

    Minor tweaks
    removed unneeded syscall permissions and fixed file extension issues

--- more_autograding_examples/prolog_simple_goal/submissions/incorrect.pl
+++ more_autograding_examples/prolog_simple_goal/submissions/incorrect.pl
@@ -1,7 +1,3 @@
-% rename to main.pl before submitting
-% i'm working on seeing if there's a way to allow any name
-% but it might just need to be some grep silliness
-
 :- use_module(knowledge_base).
 
-goal(X) :- fact(X),fact2(X),fact3(X),fact4(X).
+goal(X) :- fact1(X),fact2(X),fact3(X),fact4(X).
