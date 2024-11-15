063bbfcb2e2d09582d1be1373369f7bded296e53
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-11-15 16:24:52 -0500

    Fix lint and ensure gradeable can be recreated

--- site/cypress/e2e/Cypress-Gradeable/deletion.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/deletion.spec.js
@@ -1,18 +1,21 @@
 describe('Test cases revolving around deletion of gradeables', () => {
     it('should delete gradeable', () => {
         cy.login('instructor');
-        cy.visit(['blank', 'gradeable']);
-        // Enter gradeable info
-        cy.get('[data-testid=create-gradeable-title]').type('TestGradeable');
-        cy.get('[data-testid=create-gradeable-id]').type('TestGradeable');
-        // force needed to click radio button
-        cy.get('[data-testid=radio-student-upload]').check({ force: true });
-        // Create Gradeable
-        cy.get('[data-testid=create-gradeable-btn]').click();
-        cy.visit(['blank']);
-        cy.get('[title="Delete Gradeable"]').click();
-        cy.get('[data-testid="confirm-delete-gradeable"]').click();
-        cy.get('[title="Delete Gradeable"]').should('not.exist');
+        // run twice to ensure data was fully deleted
+        cy.wrap(Array(2)).each(() => {
+            cy.visit(['sample', 'gradeable']);
+            // Enter gradeable info
+            cy.get('[data-testid=create-gradeable-title]').type('TestGradeable');
+            cy.get('[data-testid=create-gradeable-id]').type('TestGradeable');
+            // force needed to click radio button
+            cy.get('[data-testid=radio-student-upload]').check({ force: true });
+            // Create Gradeable
+            cy.get('[data-testid=create-gradeable-btn]').click();
+            cy.visit(['sample']);
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').click();
+            cy.get('[data-testid="confirm-delete-gradeable"]').click();
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').should('not.exist');
+        });
     });
     it('should be unable to delete gradeable with submissions', () => {
         cy.login('instructor');
