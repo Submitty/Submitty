d5aad03092f30d06696757863ac95925f3365704
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-11-19 11:26:00 -0500

    Add team deletion tests

--- site/cypress/e2e/Cypress-Gradeable/deletion.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/deletion.spec.js
@@ -1,5 +1,5 @@
 describe('Test cases revolving around deletion of gradeables', () => {
-    it('should delete gradeable', () => {
+    it('should delete basic electronic file gradeable', () => {
         cy.login('instructor');
         // run twice to ensure data was fully deleted
         cy.wrap(Array(2)).each(() => {
@@ -9,6 +9,44 @@ describe('Test cases revolving around deletion of gradeables', () => {
             cy.get('[data-testid=create-gradeable-id]').type('TestGradeable');
             // force needed to click radio button
             cy.get('[data-testid=radio-student-upload]').check({ force: true });
+            // Create Gradeable
+            cy.get('[data-testid=create-gradeable-btn]').click();
+            cy.visit(['sample']);
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').click();
+            cy.get('[data-testid="confirm-delete-gradeable"]').click();
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').should('not.exist');
+        });
+    });
+    it('should delete basic team gradeable', () => {
+        cy.login('instructor');
+        // run twice to ensure data was fully deleted
+        cy.wrap(Array(2)).each(() => {
+            cy.visit(['sample', 'gradeable']);
+            // Enter gradeable info
+            cy.get('[data-testid=create-gradeable-title]').type('TestGradeable');
+            cy.get('[data-testid=create-gradeable-id]').type('TestGradeable');
+            // force needed to click radio button
+            cy.get('#radio_ef_vcs_upload').check({ force: true });
+            cy.get('#team_yes_radio', { timeout: 20000 }).check();
+            // Create Gradeable
+            cy.get('[data-testid=create-gradeable-btn]').click();
+            cy.visit(['sample']);
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').click();
+            cy.get('[data-testid="confirm-delete-gradeable"]').click();
+            cy.get('[data-testid="TestGradeable"] [title="Delete Gradeable"]').should('not.exist');
+        });
+    });
+    it('should delete vcs team gradeable', () => {
+        cy.login('instructor');
+        // run twice to ensure data was fully deleted
+        cy.wrap(Array(2)).each(() => {
+            cy.visit(['sample', 'gradeable']);
+            // Enter gradeable info
+            cy.get('[data-testid=create-gradeable-title]').type('TestGradeable');
+            cy.get('[data-testid=create-gradeable-id]').type('TestGradeable');
+            // force needed to click radio button
+            cy.get('[data-testid=radio-student-upload]').check({ force: true });
+            cy.get('#team_yes_radio', { timeout: 20000 }).check();
             // Create Gradeable
             cy.get('[data-testid=create-gradeable-btn]').click();
             cy.visit(['sample']);
