7727c19ff09f793abe8277b617976fcecee923f1
fausta4303 <tonymatt13nicey@gmail.com>
2024-11-22 17:44:32 -0500

    Lint fix

--- site/public/js/config-upload.js
+++ site/public/js/config-upload.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     });
 });
 
-function openRenamePopup(file_path){
+function openRenamePopup(file_path) {
     let url = buildCourseUrl(['autograding_config', 'usage']) + '?config_path=' + encodeURIComponent(file_path);
     $('#alert_in_use').html('');
     $('#gradeables_using_config').empty();
@@ -13,7 +13,7 @@ function openRenamePopup(file_path){
         success(data){
             var gradeable_ids = JSON.parse(data)['data'];
             if(gradeable_ids.length > 0){
-                $('#alert_in_use').html("Note: Currently these gradeables are using this config, rename at your own risk");
+                $('#alert_in_use').html('Note: Currently these gradeables are using this config, rename at your own risk');
                 for(var i = 0; i < gradeable_ids.length; i++){
                     $('#gradeables_using_config').append("<li>" + gradeable_ids[i] + "</li>");
                 }
@@ -27,7 +27,7 @@ function openRenamePopup(file_path){
 function openDeletePopup(file_path) {
     let message = $('[data-name="delete-config-message"]');
     message.html('');
-    message.append('<b>'+file_path+'</b>');
+    message.append('<b>' + file_path + '</b>');
     $('[name="config_path"]').val(file_path);
-    $('#delete_config_popup').css("display", "block");
+    $('#delete_config_popup').css('display', 'block');
 }
