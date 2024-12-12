f9598040255785c54ad35dc06cf6433c1a5ea037
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-12-12 18:39:28 -0500

    add reasons

--- sbin/create_course.sh
+++ sbin/create_course.sh
@@ -307,8 +307,10 @@ chown "$instructor:$ta_www_group" "$course_dir/BUILD_${course}.sh"
 chmod 770 "$course_dir/BUILD_${course}.sh"
 replace_fillin_variables "$course_dir/BUILD_${course}.sh"
 
-#TODO: REMOVE THIS
+#TODO: REMOVE THIS https://github.com/Submitty/Submitty/actions/runs/11982612424/job/33410908260#step:14:95
 set +e
+# cp: cannot stat '/home/runner/work/Submitty/Submitty/branch/site/config/course_template.json': No such file or directory
+
 # copy the config file for TA grading & replace the variables
 cp "${SUBMITTY_INSTALL_DIR}/site/config/course_template.json" "${course_dir}/config/config.json"
 chown "${PHP_USER}:${ta_www_group}" "${course_dir}/config/config.json"
