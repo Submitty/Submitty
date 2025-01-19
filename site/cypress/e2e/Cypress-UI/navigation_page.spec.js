bc7ed7363f8db1e3474b65fcf81370b4a2879095
Soubhagya Mohapatra <ssm.allrounder@gmail.com>
2025-01-19 22:57:12 +0530

    added cypress test for team button

--- site/cypress/e2e/Cypress-UI/navigation_page.spec.js
+++ site/cypress/e2e/Cypress-UI/navigation_page.spec.js
@@ -170,6 +170,18 @@ describe('tests navigation buttons for each level of access', () => {
         );
         checkButtons('grades_released_lab', null, null);
     });
+
+    it('should show the correct alert message for team homework', () => {
+        cy.login('instructor');
+        cy.visit(['sample']);
+            
+        cy.get('[data-testid="open_team_homework"]').find('[data-testid="submit-btn"]').click();
+        
+        // Capture and verify the alert text
+        cy.on('window:alert', (alertText) => {
+            expect(alertText).to.equal('You must be on a team to submit to this gradeable.');
+        });
+    });
 });
 
 describe('navigation page', () => {
