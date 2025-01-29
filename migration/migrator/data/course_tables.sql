c18c4fec5ea3531e5b63f15df86967c232454aae
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-01-29 11:01:48 -0500

    Database dump

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -1048,7 +1048,7 @@ CREATE TABLE public.gradeable (
     g_syllabus_bucket character varying(255) NOT NULL,
     g_allowed_minutes integer,
     g_allow_custom_marks boolean DEFAULT true NOT NULL,
-    g_notification_state boolean DEFAULT false NOT NULL,
+    g_notification_state boolean DEFAULT true NOT NULL,
     CONSTRAINT g_grade_due_date CHECK ((g_grade_due_date <= g_grade_released_date)),
     CONSTRAINT g_grade_start_date CHECK ((g_grade_start_date <= g_grade_due_date)),
     CONSTRAINT g_ta_view_start_date CHECK ((g_ta_view_start_date <= g_grade_start_date))
