Submitty/Submitty

commit 419468fecc9419c73e15d3703da06f7f03645b7e

[Bugfix:TAGrading] allow removal of empty text items (#11360)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
Closes #11216 


https://github.com/user-attachments/assets/17eabd7d-d527-475a-8c58-742399f59026


### What is the new behavior?


https://github.com/user-attachments/assets/901e129c-964d-4cce-8a6f-90ce57e5871f


### Other information?

site/app/controllers/grading/SimpleGraderController.php
@@ -250,30 +250,26 @@ class SimpleGraderController extends AbstractController {
             $component_grade = $ta_graded_gradeable->getOrCreateGradedComponent($component, $grader, true);
             $component_grade->setGrader($grader);
 
+            if ($data === '' || (!$component->isText() && $data === '0')) {
+                $ta_graded_gradeable->deleteGradedComponent($component);
+                continue;
+            }
             if ($component->isText()) {
                 $component_grade->setComment($data);
             }
             else {
-                // This catches both the not-set and blank-data case for numeric cells
-                if ($data !== '') {
-                    if (
-                        !is_numeric($data)
-                        || $data < 0
-                    ) {
-                        return JsonResponse::getFailResponse("Save error: score must be a positive number");
-                    }
-                    if ($component->getUpperClamp() < $data) {
-                        return JsonResponse::getFailResponse("Save error: score must be a number less than the upper clamp");
-                    }
-                    $db_data = $component_grade->getTotalScore();
-                    if ($original_data != $db_data) {
-                        return JsonResponse::getFailResponse("Save error: displayed stale data (" . $original_data . ") does not match database (" . $db_data . ")");
-                    }
-                    $component_grade->setScore($data);
+                // Numeric case
+                if (!is_numeric($data) || $data < 0) {
+                    return JsonResponse::getFailResponse("Save error: score must be a positive number");
                 }
-                else {
-                    continue;
+                if ($component->getUpperClamp() < $data) {
+                    return JsonResponse::getFailResponse("Save error: score must be a number less than the upper clamp");
                 }
+                $db_data = $component_grade->getTotalScore();
+                if ($original_data != $db_data) {
+                    return JsonResponse::getFailResponse("Save error: displayed stale data (" . $original_data . ") does not match database (" . $db_data . ")");
+                }
+                $component_grade->setScore($data);
             }
             $component_grade->setGradeTime($this->core->getDateTimeNow());
             $return_data[$component->getId()] = $data;

site/public/js/simple-grading.js
@@ -423,9 +423,12 @@ function setupNumericTextCells() {
             }
             // Input greater than the max_clamp for the component is not allowed
             else {
-                if (elem.data('maxclamp') !== null && elem.data('maxclamp') < this.value) {
-                    alert(`Score should be less than or equal to the max clamp value: ${elem.data('maxclamp')}`);
-                    this.value = 0;
+                if (elem.data('maxclamp') !== undefined) {
+                    const maxClamp = elem.data('maxclamp');
+                    if (maxClamp < this.value || maxClamp === 0) {
+                        alert(`Score should be less than or equal to the max clamp value: ${maxClamp}`);
+                        this.value = 0;
+                    }
                 }
             }
         }

commit c93ddd04f2f396a55622149225ec4af6fbd713fa

[Bugfix:TAGrading] Set only updated scores in components (#11431)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
<!-- List issue if it fixes/closes/implements one using the "Fixes
#<number>" or "Closes #<number>" syntax -->
We have a problem with checkpoint (e.g., cs1 & ds lab) gradeables. I've
tested with both Mac Chrome and Mac Firefox.

When I click on 2 checkpoints for the same person, it displays both
checkpoints, but when I reload the page, only the 2nd checkpoint is
stored.

### What is the new behavior?
All checkpoints will persist the state that they should be in after
reloading.

### Other information?
<!-- Is this a breaking change? -->
<!-- How did you test -->
Issues caused by #11360
[Zulip discussion about
this](https://submitty.zulipchat.com/#narrow/channel/409312-RPI-Developers/topic/URGENT.20LAB.20GRADABLE.20BUGFIX.20NEEDED)

---------

Co-authored-by: Troy-bailan-King <97757033+Troy-bailan-King@users.noreply.github.com>

site/app/controllers/grading/SimpleGraderController.php
@@ -244,15 +244,23 @@ class SimpleGraderController extends AbstractController {
         $return_data = [];
 
         foreach ($gradeable->getComponents() as $component) {
-            $data = $_POST['scores'][$component->getId()] ?? '';
-            $original_data = $_POST['old_scores'][$component->getId()] ?? '';
+            if (!array_key_exists($component->getId(), $_POST['scores'])) {
+                continue;
+            }
+            $data = $_POST['scores'][$component->getId()];
+            if (!array_key_exists($component->getId(), $_POST['old_scores'])) {
+                return JsonResponse::getFailResponse("Save error: old score data missing");
+            }
+            $original_data = $_POST['old_scores'][$component->getId()];
 
-            $component_grade = $ta_graded_gradeable->getOrCreateGradedComponent($component, $grader, true);
-            $component_grade->setGrader($grader);
 
             if ($data === '' || (!$component->isText() && $data === '0')) {
                 $ta_graded_gradeable->deleteGradedComponent($component);
                 continue;
+            }
+            else {
+                $component_grade = $ta_graded_gradeable->getOrCreateGradedComponent($component, $grader, true);
+                $component_grade->setGrader($grader);
             }
             if ($component->isText()) {
                 $component_grade->setComment($data);