dbb6b3e80a7031d019b92a3d48befeece1f9f78c
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2025-02-06 16:00:35 -0500

    fix accessability issue

--- site/app/controllers/GlobalController.php
+++ site/app/controllers/GlobalController.php
@@ -431,13 +431,12 @@ class GlobalController extends AbstractController {
             ]);
         }
 
-        // --------------------------------------------------------------------------
-        // SUPERUSERS ONLY
-        if ($this->core->getUser()->getAccessLevel() === User::LEVEL_FACULTY) {
+        if ($this->core->getUser()->getAccessLevel() === User::LEVEL_SUPERUSER) {
             $sidebar_buttons[] = new NavButton($this->core, [
                 "href" => $this->core->buildUrl(['community_events']),
                 "title" => "Community Events",
-                "icon" => "fa-palette"
+                "icon" => "fa-palette",
+                "id" => "nav-sidebar-events"
             ]);
 
             $sidebar_buttons[] = new NavButton($this->core, [
