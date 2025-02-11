62dc63a350e61341125237c8bc59f3948bc88bd6
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-02-11 17:45:41 -0500

    Fix indentation

--- migration/migrator/migrations/master/20250121144600_make_empty_preferred_names_null.py
+++ migration/migrator/migrations/master/20250121144600_make_empty_preferred_names_null.py
@@ -38,7 +38,7 @@ def down(config, database):
     :param database: Object for interacting with given database for environment
     :type database: migrator.db.Database
     """
-    
+
     database.execute("""
     ALTER TABLE users DROP CONSTRAINT user_preferred_givenname_not_empty
     """)
