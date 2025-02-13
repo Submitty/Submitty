91cf1e2390a5fd007202807645ed52af39ab2cbb
zacharymnp <56311867+zacharymnp@users.noreply.github.com>
2025-02-13 05:46:12 -0500

    set nightly build to default
    Removes validation for nightly build checkbox, since customization.json is not needed.

--- site/app/controllers/admin/ConfigurationController.php
+++ site/app/controllers/admin/ConfigurationController.php
@@ -21,8 +21,8 @@ class ConfigurationController extends AbstractController {
     // The message that should be returned to the user if they fail the required validation to enable the nightly
     // rainbow grades build checkbox
     const FAIL_AUTO_RG_MSG = 'You may not enable automatic rainbow grades generation until you have supplied a ' .
-    'customization.json file.  To have one generated for you, you may use the Web-Based Rainbow Grades Generation inside the Grade ' .
-    'Reports tab.  You may also manually create the file and upload it to your course\'s rainbow_grades directory.';
+    'customization.json file.  To have one generated for you, you may use the Grades Configuration tab. ' .
+    'You may also manually create the file and upload it to your course\'s rainbow_grades directory.';
     const NO_SELF_REGISTER = 0; // Self registration disabled
     const REQUEST_SELF_REGISTER = 1; // Self registration allowed, users request and instructors can approve
     const ALL_SELF_REGISTER = 2; // Self registration allowed, and all users who register are automatically added
@@ -169,23 +169,7 @@ class ConfigurationController extends AbstractController {
             }
         }
         elseif ($name === 'auto_rainbow_grades') {
-            // Special validation for auto_rainbow_grades checkbox
-            // Get a new customization json object
-            $customization_json = new RainbowCustomizationJSON($this->core);
-
-            // If a manual_customization.json does not exist, then check for the presence of a regular one
-            if (!$customization_json->doesManualCustomizationExist()) {
-                // Attempt to populate it from the customization.json in the course rainbow_grades directory
-                // If no file exists do not allow user to enable this check mark until one is supplied
-                try {
-                    $customization_json->loadFromJsonFile();
-                }
-                catch (\Exception $e) {
-                    return MultiResponse::JsonOnlyResponse(
-                        JsonResponse::getFailResponse(ConfigurationController::FAIL_AUTO_RG_MSG)
-                    );
-                }
-            }
+            // Validation for auto_rainbow_grades checkbox
 
             $entry = $entry === "true";
         }
