0af31e73d12ff5e967c2e5361df317bb4cd01463
Soubhagya Mohapatra <ssm.allrounder@gmail.com>
2025-02-24 10:51:29 +0530

    don't use empty()

--- site/app/models/OfficeHoursQueueModel.php
+++ site/app/models/OfficeHoursQueueModel.php
@@ -107,7 +107,7 @@ class OfficeHoursQueueModel extends AbstractModel {
     }
 
     public function getName() {
-        if (!empty($this->core->getUser()->getPreferredGivenName()) && !empty($this->core->getUser()->getPreferredFamilyName())) {
+        if ($this->core->getUser()->getPreferredGivenName() && $this->core->getUser()->getPreferredFamilyName()) {
             return $this->core->getUser()->getPreferredGivenName() . " " . $this->core->getUser()->getPreferredFamilyName();
         }
         if (!isset($this->last_queue_details)) {
