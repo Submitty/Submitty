41e2e9eb6345b7701339704967ec3f2e6a1eedc2
williamschen23 <chenwill2005@gmail.com>
2025-03-17 15:06:31 -0400

    barb comments

--- site/public/js/admin-gradeable-updates.js
+++ site/public/js/admin-gradeable-updates.js
@@ -316,7 +316,7 @@ $(document).ready(() => {
 });
 
 function checkWarningBanners() {
-    $('#gradeable-dates-warnings').hide();
+    $('#gradeable-dates-warnings-banner').hide();
     if ($('#yes_grade_inquiry_allowed').is(':checked')) {
         const grade_inquiry_start_date = $('#date_grade_inquiry_start').val();
         const grade_inquiry_due_date = $('#date_grade_inquiry_due').val();
@@ -328,7 +328,18 @@ function checkWarningBanners() {
         }
         else {
             $('#grade-inquiry-dates-warning').hide();
-            $('#gradeable-dates-warnings-banner').hide();
+        }
+    }
+
+    if ($('#has_release_date_yes').is(':checked')) {
+        const release_date = $('#date_released').val();
+        const grade_inquiry_due_date = $('#date_grade_inquiry_due').val();
+        if (release_date > grade_inquiry_due_date) {
+            $('#no-grade-inquiry-warning').show();
+            $('#gradeable-dates-warnings-banner').show();
+        }
+        else {
+            $('#release-dates-warning').hide();
         }
     }
 }
