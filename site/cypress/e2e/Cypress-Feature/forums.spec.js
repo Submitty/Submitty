ea7529aec989e13c14cbc0b87c5a3fadbe0229ce
martig7 <martig7@rpi.edu>
2025-03-19 17:14:45 -0400

    fixed linting again

--- site/cypress/e2e/Cypress-Feature/forums.spec.js
+++ site/cypress/e2e/Cypress-Feature/forums.spec.js
@@ -62,7 +62,7 @@ const removeThread = (title) => {
         if (exists) {
             cy.get('[data-testid="thread-list-item"]').contains(title).click();
             cy.get('[data-testid="thread-dropdown"]').first().click();
-            cy.get('[data-testid="delete-post-button"]').first().click({ force : true });
+            cy.get('[data-testid="delete-post-button"]').first().click({ force: true });
             if (threadExists(title)) {
                 removeThread(title);
             }
