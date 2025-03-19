cff540c6b0639f29e816359f07eed9d9545d5fef
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-03-19 12:59:50 -0400

    [Refactor:Performance] Integrate performance warning into Vue component and update rendering logic

--- site/vue/src/components/performanceWarning.vue
+++ site/vue/src/components/performanceWarning.vue
@@ -1,14 +1,28 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+const props = defineProps<{
+    performanceWarning: boolean;
+    submittyQueries: string[];
+    courseQueries: string[];
+}>();
+const isVisible = ref(props.performanceWarning);
+</script>
+
 <template>
   <div
+    v-if="isVisible"
     id="performance-warning-system-message"
     class="system-message warning"
-    style="display: none;"
+    style="display: flex;"
   >
-    <span id="performance-warning-system-message-text" />
+    <span id="performance-warning-system-message-text">
+      Developer Warning: Excessive or duplicate database queries observed: {{ submittyQueries.length + courseQueries.length }} queries executed. Click "Show Page Details" at the bottom of the page for more info.
+    </span>
     <i
       id="close-performance-warning-system-message"
       class="fa fa-times"
       style="padding: 4px; cursor: pointer;"
+      @click="isVisible= false"
     />
   </div>
 </template>
--- site/vue/src/main.ts
+++ site/vue/src/main.ts
@@ -3,20 +3,15 @@ import Unknown from './components/Unknown.vue';
 
 const exports = {
     async render(target: string | Element, component: string, args: Record<string, unknown> = {}) {
-        const root_component = await (async () => {
+        const app = await (async () => {
             try {
-                return (await import(`./components/${component}.vue`) as { default: Parameters<typeof createApp>[0] }).default;
+                return createApp((await import(`./components/${component}.vue`) as { default: Parameters<typeof createApp>[0] }).default, args);
             }
             catch (e) {
                 console.error(`Could not find vue component ${component}:`, e);
-                return Unknown;
+                return createApp(Unknown, { component });
             }
         })();
-
-        const app = createApp(root_component);
-        for (const [key, value] of Object.entries(args)) {
-            app.provide(key, value);
-        }
 
         app.mount(target);
     },
