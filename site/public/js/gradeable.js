133b0e21e07933cb5424fca4947b94c2b8390a5f
Cameron Peterson <reapernsgaming@gmail.com>
2025-04-01 16:21:37 -0300

    Remove gradeable test message, maybe other PR

--- site/public/js/gradeable.js
+++ site/public/js/gradeable.js
@@ -360,10 +360,6 @@ function ajaxUploadGradeable() {
                 }
                 else {
                     closePopup('gradeable-json-form');
-                    if (!Object.prototype.hasOwnProperty.call(data, 'message')) {
-                        displayErrorMessage('An error has occurred. Check console log for more details');
-                        console.log(data);
-                    }
                     displayErrorMessage(data['message']);
                     return false;
                 }
