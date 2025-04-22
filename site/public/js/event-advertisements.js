09a2b2c34ba0b4a76f1db9d2851dff2b823b0b19
04vmatsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2025-04-22 17:33:21 -0400

    lint again

--- site/public/js/event-advertisements.js
+++ site/public/js/event-advertisements.js
@@ -20,8 +20,7 @@ function updateImageData(imageData) {
 }
 
 function setupLocalStorage() {
-    //incase the local storage for these variables are undefined
-
+    // incase the local storage for these variables are undefined
     if (localStorage.getItem(eventLS.open) === null) {
         localStorage.setItem(eventLS.open, 'false');
     }
@@ -41,8 +40,7 @@ function includesBanner(bannerArray, banner) {
 }
 
 function filterRemovedBanners(localStorageKey, newArray) {
-    //if the instructor removes arrays we don't want their data saved
-
+    // if the instructor removes arrays we don't want their data saved
     let currentArray = JSON.parse(localStorage.getItem(localStorageKey)) || [];
     currentArray = currentArray.filter((banner) => includesBanner(newArray, banner));
     return currentArray;
@@ -52,9 +50,7 @@ function updateLocalStorage(imageDataArray) {
     const bannerArray = filterRemovedBanners(eventLS.bannerArr, imageDataArray);
     const removedArray = filterRemovedBanners(eventLS.removedArr, imageDataArray);
 
-
-    //here new banners are added to the front of bannerArray so they are displayed first
-
+    // here new banners are added to the front of bannerArray so they are displayed first
     let updated = false;
 
     imageDataArray.forEach((item) => {
@@ -64,8 +60,7 @@ function updateLocalStorage(imageDataArray) {
         }
     });
 
-    //change index to 0 so that new events are shown first, and we want the ducktalk to be true to display the talking animation
-
+    // change index to 0 so that new events are shown first, and we want the ducktalk to be true to display the talking animation
     if (updated) {
         localStorage.setItem(eventLS.index, 0);
         localStorage.setItem(eventLS.duckTalk, 'true');
@@ -75,8 +70,7 @@ function updateLocalStorage(imageDataArray) {
     localStorage.setItem(eventLS.removedArr, JSON.stringify(removedArray));
 }
 
-//initiation script
-
+// initiation script
 function init() {
     setupLocalStorage();
     updateLocalStorage(imageDataArray);
