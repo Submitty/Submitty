ac184b7a01868fb34b3bd7a870542093791b03c0
williamschen23 <chenwill2005@gmail.com>
2025-04-25 14:56:31 -0400

    fix

--- site/cypress/e2e/Cypress-Gradeable/leaderboard_gradeable.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/leaderboard_gradeable.spec.js
@@ -3,7 +3,11 @@ describe('Tests leaderboard access', () => {
         cy.login('instructor');
         cy.visit(['sample', 'gradeable', 'leaderboard', 'update']);
         cy.get('#page_5_nav').click();
+        cy.get('[data-testid="submission-open-date"]').clear();
         cy.get('[data-testid="submission-open-date"]').type('2100-01-15 23:59:59');
+        // clicks out of the calendar and save
+        cy.get('body').click(0, 0);
+        cy.get('#save_status', { timeout: 10000 }).should('have.text', 'All Changes Saved');
     });
 
     it('Should check if leaderboard is accessible to users', () => {
