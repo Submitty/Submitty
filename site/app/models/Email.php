9c16b20fe7e898a8e3143bc71c578ea533369710
Justin Manion <jmanion32@gmail.com>
2025-05-29 13:55:09 -0400

    Update Email.php

--- site/app/models/Email.php
+++ site/app/models/Email.php
@@ -100,8 +100,8 @@ class Email extends AbstractModel {
         $body .= "\n\n--\nNOTE: This is an automated email notification, which is unable to receive replies.";
 
         // Adding the notifications settings link after the footer
-        if ($course != null || $term != null) {
-             $notifications_url = $base_url . "/courses/{$term}/{$course}/notifications/settings";
+        if ($course != null && $term != null) {
+            $notifications_url = $base_url . "/courses/{$term}/{$course}/notifications/settings";
             $body .= "\nPlease refer to the course syllabus for contact information for your teaching staff.\nUpdate your email notification settings for this course here: " . $notifications_url;
         }
         return $body;
