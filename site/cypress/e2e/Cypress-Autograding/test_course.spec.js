ccf36405a4fead661d7b4c4bccc585f458e8eb93
williamschen23 <chenwill2005@gmail.com>
2025-06-02 18:40:33 -0400

    verified that courses exist

--- site/cypress/e2e/Cypress-Autograding/test_course.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Test course should exist', () => {
-    it('Check test course exists', () => {
-        cy.login();
-        cy.visit(['development']);
-        // Use this file to create a test based around submissions
-        cy.get('jidsofjweiofjw');
-    });
-});
