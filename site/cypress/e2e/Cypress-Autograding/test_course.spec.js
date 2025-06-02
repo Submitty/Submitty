2c6cc9109773eecdff26eb3c23740824a7a7856c
williamschen23 <chenwill2005@gmail.com>
2025-06-02 16:16:14 -0400

    add cypress autograding directory

---
+++ site/cypress/e2e/Cypress-Autograding/test_course.spec.js
@@ -0,0 +1,8 @@
+describe('Test course should exist', () => {
+    it('Check test course exists', () => {
+        cy.login();
+        cy.visit(['development']);
+        // Use this file to create a test based around submissions
+        cy.get('jidsofjweiofjw');
+    });
+});
