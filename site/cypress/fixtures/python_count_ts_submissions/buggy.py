d01b10fc8dc3e71536ee2fd872879c879ecea23f
williamschen23 <chenwill2005@gmail.com>
2025-06-03 16:06:17 -0400

    remove python files

--- site/cypress/fixtures/python_count_ts_submissions/buggy.py
+++
@@ -1,9 +0,0 @@
-def answer(w, l):
-    return w+l
-
-lengths = [i for i in range(10)]
-widths  = [3*i for i in range(10)]
-
-for i in range(len(lengths)):
-    result = answer(lengths[i], widths[i])
-    print(f"Area: {result}.")
