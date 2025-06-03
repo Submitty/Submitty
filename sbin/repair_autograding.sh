71bdf1b0b218b78457e039d976b3c8e688095dc9
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-06-03 10:02:02 -0400

    Working restart primary / remote shippers and workers

--- sbin/repair_autograding.sh
+++ sbin/repair_autograding.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-RESTART_NEEDED=false
-COMPONENTS=("shipper" "worker")
-TARGETS=("primary" "perform_on_all_workers")
-STATUS_SCRIPT="/usr/local/submitty/sbin/shipper_utils/systemctl_wrapper.py"
-RESTART_SCRIPT="/usr/local/submitty/sbin/restart_shipper_and_all_workers.py"
+components=("shipper" "worker")
+targets=("primary" "perform_on_all_workers")
+status_codes=("inactive" "active" "failure" "bad_arguments" "io_error")
+status_script="/usr/local/submitty/sbin/shipper_utils/systemctl_wrapper.py"
 
-for component in "${COMPONENTS[@]}"; do
-  for target in "${TARGETS[@]}"; do
-    echo "Checking status of $component on $target..."
+for component in "${components[@]}"; do
+  for target in "${targets[@]}"; do
     if [[ "$target" == "primary" ]]; then
-      sudo python3 $STATUS_SCRIPT --daemon "$component" --target "$target" status
+      cmd_prefix="sudo"
     else
-      python3 -u submitty_daemon $STATUS_SCRIPT --daemon "$component" --target "$target" status
+      cmd_prefix="sudo -u submitty_daemon"
     fi
-    STATUS=$?
 
-    if [[ "$STATUS" -ne 1 ]]; then
-      echo "Detected issue with $component on $target (status code: $STATUS)"
-      RESTART_NEEDED=true
-      break 2
+    $cmd_prefix python3 "$status_script" --daemon "$component" --target "$target" status
+    status=$?
+
+    if [[ "$status" -ne 1 ]]; then
+        today=$(date +%Y%m%d)
+        log_file="/var/log/services/${today}.txt"
+
+        if [[ ! -f "${log_file}" ]]; then
+            sudo touch "${log_file}"
+        fi
+
+        echo -e "Restarting autograding ${component} for ${target}\n\nLast status: ${status_codes[$status]}" | sudo tee -a "${log_file}"
+        echo -e "\n----------------------------------------\n" | sudo tee -a "${log_file}"
+
+        $cmd_prefix python3 "$status_script" --daemon "$component" --target "$target" restart
     fi
   done
 done
-
-if $RESTART_NEEDED; then
-  echo "Restarting all shippers and workers..."
-  python3 $RESTART_SCRIPT
-else
-  echo "All daemons are healthy."
-fi
