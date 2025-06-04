e619198f865bc401f4ad97e4040a60685f5ea75f
Justin Manion <jmanion32@gmail.com>
2025-06-04 11:17:43 -0400

    Reverted Import, Created CSS file

--- site/app/views/submission/HomeworkView.php
+++ site/app/views/submission/HomeworkView.php
@@ -1093,6 +1093,7 @@ class HomeworkView extends AbstractView {
 
         $this->core->getOutput()->addInternalJs('confetti.js');
         $this->core->getOutput()->addInternalJs('submission-page.js');
+        $this->core->getOutput()->addInternalCss('autogradingresultsbox.css');
         return $this->core->getOutput()->renderTwigTemplate('submission/homework/AutogradingResultsBox.twig', $param);
     }
 
