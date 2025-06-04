7bbcd3292cc8aa5f944f12e2b84b952b35eefde4
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-06-04 09:22:17 -0400

    Remove space between timestamp and service identifier

--- sbin/repair_services.sh
+++ sbin/repair_services.sh
@@ -26,7 +26,7 @@ log_service_restart() {
         sudo touch "${log_file}"
     fi
 
-    echo -e "${timestamp}: ${services[$service]}\n" | sudo tee -a "${log_file}" > /dev/null
+    echo -e "${timestamp}:${services[$service]}\n" | sudo tee -a "${log_file}" > /dev/null
     echo -e "${message}\n\n${last_status}" | sudo tee -a "${log_file}" > /dev/null
     echo -e "\n----------------------------------------\n" | sudo tee -a "${log_file}" > /dev/null
 }
