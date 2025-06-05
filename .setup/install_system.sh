530e6a13c3a6721e920f23f94df69e9c402c6c91
Justin Manion <jmanion32@gmail.com>
2025-06-05 10:44:35 -0400

    Revert "Changed Orders"
    This reverts commit b7c3cb150f439cb4811b2e8b15344fb4f88b6c5b.

--- .setup/install_system.sh
+++ .setup/install_system.sh
@@ -705,7 +705,6 @@ ${DB_COURSE_USER}
 ${DB_COURSE_PASSWORD}
 America/New_York
 en_US
-100
 ${SUBMISSION_URL}
 
 
@@ -720,6 +719,7 @@ submitty@vagrant
 do-not-reply@vagrant
 localhost
 25
+100
 " | python3 ${SUBMITTY_REPOSITORY}/.setup/CONFIGURE_SUBMITTY.py --debug --setup-for-sample-courses --websocket-port ${WEBSOCKET_PORT}
 
         # Set these manually as they're not asked about during CONFIGURE_SUBMITTY.py
