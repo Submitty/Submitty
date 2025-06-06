579766f9d3ddf3da97e0a61f515be24a88d03fbb
Justin Manion <jmanion32@gmail.com>
2025-06-06 16:54:58 -0400

    [UI/UX:Submission] Improve Duplicate Submission Handling (#11640)
    ### What is the current behavior?
    When the user has not made any changes to their previous submission, the
    Submit button provides no visual feedback. Clicking it triggers a
    JavaScript alert: "Not a new submission", and the page reloads
    unnecessarily.
    
    ![main error alert submit
    button](https://github.com/user-attachments/assets/4dae0112-a2da-49a6-9f54-96fcbfe7d81c)
    
    ### What is the new behavior?
    The Submit button now appears visually disabled when no changes have
    been made to the previous submission. When clicked, it now triggers an
    inline warning popup displaying: "WARNING: Duplicate submission. No
    attempts used". The page no longer reloads.
    
    
    https://github.com/user-attachments/assets/c9f0b3ff-0b6d-45bc-8c23-f271b1a7b630
    
    ---------
    
    Co-authored-by: Alex Lavallee <alex@lavallee.one>

--- site/public/js/drag-and-drop.js
+++ site/public/js/drag-and-drop.js
@@ -1,7 +1,7 @@
 /* exported handleUploadBanner, initializeDropZone, handleEditCourseMaterials, handleUploadCourseMaterials, handleDownloadImages,
             handleSubmission, handleRegrade, handleBulk, deleteSplitItem, submitSplitItem, displayPreviousSubmissionOptions
             displaySubmissionMessage, validateUserId, openFile, handle_input_keypress, addFilesFromInput,
-            dropWithMultipleZips, initMaxNoFiles, setUsePrevious, readPrevious, createArray, initializeDragAndDrop */
+            dropWithMultipleZips, initMaxNoFiles, setUsePrevious, readPrevious, createArray, initializeDragAndDrop setButtonStatus */
 /* global buildCourseUrl, buildUrl, getFileExtension, csrfToken, removeMessagePopup, newOverwriteCourseMaterialForm, displayErrorMessage, displayMessage */
 
 /*
@@ -1035,8 +1035,7 @@ function handleSubmission(gradeable_status, remaining_late_days_for_gradeable, c
     if (!vcs_checkout) {
         // Check if new submission
         if (!isValidSubmission() && empty_inputs) {
-            alert('Not a new submission.');
-            window.location.reload();
+            displayMessage('Duplicate submission detected. No attempts used', 'warning');
             return;
         }
 
