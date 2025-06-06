27c26c7419206fdfc5570a2c1c336b2a19253a9d
Justin Manion <jmanion32@gmail.com>
2025-06-06 11:08:16 -0400

    Revert "Checking Routes"
    This reverts commit bf06b265801783348dff985779106793516aa18f.

--- site/app/controllers/admin/AdminGradeableController.php
+++ site/app/controllers/admin/AdminGradeableController.php
@@ -1980,8 +1980,8 @@ class AdminGradeableController extends AbstractController {
 
     /**
      * Loads config info for a gradeable to allow editing
-     * @Route("/courses/{_semester}/{_course}/gradeable/edit/load", methods={"POST"})
      */
+    #[Route("/courses/{_semester}/{_course}/gradeable/edit/load", methods: ["POST"])]
     public function loadConfigEditor(): void {
         $gradeable = $this->tryGetGradeable($_POST['gradeable_id']);
 
@@ -2009,8 +2009,8 @@ class AdminGradeableController extends AbstractController {
 
     /**
      * Saves config info from a gradeable edit
-     * @Route("/courses/{_semester}/{_course}/gradeable/edit/save", methods={"POST"})
      */
+    #[Route("/courses/{_semester}/{_course}/gradeable/edit/save", methods: ["POST"])]
     public function saveConfigEdit(): void {
         $gradeable = $this->tryGetGradeable($_POST['gradeable_id']);
         if ($gradeable === false) {
