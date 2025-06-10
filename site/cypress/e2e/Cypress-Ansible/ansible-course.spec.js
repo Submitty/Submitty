a57a86dbc2be89457a5113b589d07c9da4070571
IDzyre <reapernsgaming@gmail.com>
2025-06-10 10:23:02 -0800

    testing get api key

--- site/cypress/e2e/Cypress-Ansible/ansible-course.spec.js
+++ site/cypress/e2e/Cypress-Ansible/ansible-course.spec.js
@@ -1,6 +1,6 @@
 describe('Testing website when created by ansible scripts', () => {
     it('Should be able to login and see the course', () => {
         cy.login('instructor');
-        cy.visit('term', 'course');
+        cy.visit('term/course');
     });
 });
