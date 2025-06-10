f4eb42e0719f4e5efe2f5f3aa0e55447039ce1a6
williamschen23 <chenwill2005@gmail.com>
2025-06-10 11:14:21 -0400

    modularize everything

--- site/cypress/e2e/Cypress-Autograding/electronic_gradeable.spec.js
+++ site/cypress/e2e/Cypress-Autograding/electronic_gradeable.spec.js
@@ -1,14 +1,22 @@
 /**
- * Uploads a file and compares the results with expected scores.
- * @param {string} fileUploadName the fixture to upload
- * @param {number[]} fullScores the max scores for the gradeables
- * @param {number[]} expectedScores the expected score for the submission
+ * switches versions if we are not on the version number we want
+ * @param {number} versionNumber
  */
-const submitAndCheckResults = (fileUploadName, expectedScores, fullScores) => {
-    expect(expectedScores.length).to.eq(fullScores.length);
-    const scoreTotal = fullScores.reduce((partial, actual) => partial + actual, 0);
-    const expectedTotal = expectedScores.reduce((partial, actual) => partial + actual, 0);
+const switchVersion = (versionNumber) => {
+    cy.get('[data-testid="submission-version-select"').find('option:selected').then((selectedOption) => {
+        const currentVersion = selectedOption.val();
+        if (currentVersion !== versionNumber) {
+            cy.get('[data-testid="submission-version-select"').select(versionNumber);
+        }
+    });
+};
 
+/**
+ * sets up the gradeable in a state where we can submit a new submission
+ * @param {string} gradeableName the gradeable we want to submit for
+ */
+const newSubmission = (gradeableName) => {
+    cy.visit(['development', 'gradeable', gradeableName]);
     // Wait for client JS to load - reduces flakyness
     cy.get('[data-testid="gradeable-time-remaining-text"]').contains('days');
     // If the clear button exists, we should click it.
@@ -17,13 +25,40 @@ const submitAndCheckResults = (fileUploadName, expectedScores, fullScores) => {
             cy.wrap($btn).click();
         }
     });
+};
 
-    // make sure that we have no files.
-    cy.get('[data-testid="file-upload-table-1"] > tr').should('not.exist');
-    cy.get('[data-testid="select-files"]').attachFile(fileUploadName);
+/**
+ * Uploads a file and compares the results with expected scores.
+ * @param {string} fileUploadName the fixture to upload
+ * @param {number} bucket the bucket to submit to. Default gradeables has buckets starting from 1
+ */
+const submitFiles = (fileUploadName, bucket, firstFile = false) => {
+    if (firstFile) {
+        cy.get(`[data-testid="file-upload-table-${bucket}"] > tr`).should('not.exist');
+    }
+    cy.get(`[data-testid="upload-files-${bucket}"]`).find('[data-testid="select-files"]').attachFile(fileUploadName);
+};
+
+/**
+ * Hits the gradeable submit button
+ * @param {number} versionNumber the version number we are on
+ */
+const submitGradeable = (versionNumber) => {
     cy.get('[data-testid="submit-gradeable"').click();
-    cy.get('[data-testid="popup-message"]').contains('Successfully uploaded version');
+    cy.get('[data-testid="submission-version-select"').find('option:selected').should('contain.text', `Version #${versionNumber}`);
+    cy.get('[data-testid="popup-message"]').should('contain.text', `Successfully uploaded version ${versionNumber}`);
+};
 
+/**
+ * @param {number} versionNumber version number that we should check the grades against
+ * @param {number[]} expectedScores the expected score for the submission
+ * @param {number[]} fullScores the max scores for the gradeables
+ */
+const checkNonHiddenResults = (versionNumber, expectedScores, fullScores) => {
+    expect(expectedScores.length).to.eq(fullScores.length);
+    // switchVersion(versionNumber);
+    const scoreTotal = fullScores.reduce((partial, actual) => partial + actual, 0);
+    const expectedTotal = expectedScores.reduce((partial, actual) => partial + actual, 0);
     // wait for autograding results and compares expected to score pills
     cy.get('[data-testid="autograding-total-no-hidden"]', { timeout: 60000 });
     cy.get('[data-testid="autograding-total-no-hidden"]').find('[data-testid="score-pill-badge"]').contains(`${expectedTotal} / ${scoreTotal}`);
@@ -35,10 +70,18 @@ const submitAndCheckResults = (fileUploadName, expectedScores, fullScores) => {
 describe('Test the development course gradeables', () => {
     it('Should test the cpp cats gradeable with full and buggy submissions', () => {
         const fullScores = [2, 3, 4, 4, 4, 4, 4];
-        cy.login('instructor');
-        cy.visit(['development', 'gradeable', 'cpp_cats']);
+        cy.login('bitdiddle');
 
-        submitAndCheckResults('cpp_cats_submissions/allCorrect.zip', fullScores, fullScores);
-        submitAndCheckResults('cpp_cats_submissions/extraLinesAtEnd.zip', [2, 3, 2, 2, 2, 2, 0], fullScores);
+        // submits and checks the all correct files
+        newSubmission('cpp_cats');
+        submitFiles('cpp_cats_submissions/allCorrect.zip', 1, true);
+        submitGradeable(1);
+        checkNonHiddenResults(1, fullScores, fullScores);
+
+        // submits the half incorrect files
+        newSubmission('cpp_cats');
+        submitFiles('cpp_cats_submissions/extraLinesAtEnd.zip', 1, true);
+        submitGradeable(2);
+        checkNonHiddenResults(2, [2, 3, 2, 2, 2, 2, 0], fullScores);
     });
 });
