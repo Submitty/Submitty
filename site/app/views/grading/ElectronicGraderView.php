a4b41615a6ca1346c46b3dac2726764524368662
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-06-11 09:49:16 -0400

    Revert autograding enabled status logic

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -317,7 +317,7 @@ class ElectronicGraderView extends AbstractView {
             "rotating_sections_error" => (!$gradeable->isGradeByRegistration()) && $no_rotating_sections
                 && $this->core->getUser()->getGroup() == User::GROUP_INSTRUCTOR,
             "ta_grading_enabled" => $gradeable->isTaGrading(),
-            "autograding_enabled" => $gradeable->hasAutogradingConfig(),
+            "autograding_enabled" => $gradeable->hasAutogradingConfig() ? $gradeable->getAutogradingConfig()->anyPoints() : false,
             "autograding_non_extra_credit" => $gradeable->hasAutogradingConfig() ? $gradeable->getAutogradingConfig()->getTotalNonExtraCredit() : 0,
             "peer" => $peer,
             "blind_status" => $gradeable->getPeerBlind(),
