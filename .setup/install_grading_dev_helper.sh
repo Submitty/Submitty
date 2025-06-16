1870acb7c3ec7e414b6b595d7342d022d0334f73
Barb Cutler <Barb Cutler>
2025-06-16 11:39:13 -0400

    update shell script

--- .setup/install_grading_dev_helper.sh
+++ .setup/install_grading_dev_helper.sh
@@ -1,15 +1,24 @@
+#!/usr/bin/env bash
 
+#
+# This helper script is useful during development of the autograding system code.
+# It contains a subset of commands from .setup/INSTALL_SUBMITTY_HELPER.sh related to autograding.
+#
+
+# copy the grading code
 rsync -rtz /usr/local/submitty/GIT_CHECKOUT/Submitty/grading /usr/local/submitty/src
 
+# substitute the global variables
 sed -i -e "s|__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__|/usr/local/submitty|g" /usr/local/submitty/src/grading/CMakeLists.txt
 sed -i -e "s|__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__|/usr/local/submitty|g" /usr/local/submitty/src/grading/Sample_CMakeLists.txt
-
 sed -i -e "s|__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__|/usr/local/submitty|g" /usr/local/submitty/src/grading/system_call_check.cpp
 
-cd /usr/local/submitty/src/grading/lib
+# build the grading library
+cd /usr/local/submitty/src/grading/lib || exit
 cmake ..
 make
 
+# set the permissions
 chown -R  root:root /usr/local/submitty/src
 find /usr/local/submitty/src -type d -exec chmod 555 {} \;
 find /usr/local/submitty/src -type f -exec chmod 444 {} \;
