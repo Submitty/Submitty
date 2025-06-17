e1cda5d6707e934e438caebc7a9ae99d33e8d580
Christopher Poon <christopherpoonc@gmail.com>
2025-06-17 15:44:23 -0400

    remove single use images from cypress and configure

--- .setup/CONFIGURE_SUBMITTY.py
+++ .setup/CONFIGURE_SUBMITTY.py
@@ -575,14 +575,10 @@ if not args.worker:
                           "submitty/gcc:latest",
                           "submitty/rust:latest",
                           "submitty/java:latest",
-                          "submitty/pdflatex:latest",
-                          "submitty/haskell:8.8.4",
-                          "submitty/prolog:8",
-                          "submitty/verilog:latest"
+                          "submitty/pdflatex:latest"
                         ],
             "python":   [
                           "submitty/autograding-default:latest",
-                          "submitty/qiskit:qiskit",
                           "submitty/python:latest"
                         ],
             "cpp":      [
