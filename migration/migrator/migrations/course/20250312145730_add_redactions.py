f3643f6d89d9f11fa1cc96a48506a6084b91a18b
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-06-20 11:58:38 -0400

    fix indentation in gradeable_redaction table creation

--- migration/migrator/migrations/course/20250312145730_add_redactions.py
+++ migration/migrator/migrations/course/20250312145730_add_redactions.py
@@ -16,13 +16,13 @@ def up(config, database, semester, course):
     """
     database.execute("""
                      CREATE TABLE IF NOT EXISTS gradeable_redaction (
-                     redaction_id SERIAL PRIMARY KEY,
-                     g_id character varying(255) NOT NULL REFERENCES gradeable(g_id) ON DELETE CASCADE,
-                     page integer NOT NULL,
-                     x1 float NOT NULL,
-                     x2 float NOT NULL,
-                     y1 float NOT NULL,
-                     y2 float NOT NULL
+                        redaction_id SERIAL PRIMARY KEY,
+                        g_id character varying(255) NOT NULL REFERENCES gradeable(g_id) ON DELETE CASCADE,
+                        page integer NOT NULL,
+                        x1 float NOT NULL,
+                        x2 float NOT NULL,
+                        y1 float NOT NULL,
+                        y2 float NOT NULL
                      )
     """)
 
