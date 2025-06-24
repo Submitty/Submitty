Submitty/Submitty

commit 97dc7f1e2cdc5a2230171ad4786cfb4488a0a529

[Feature:TAGrading] Add column preferences (#11487)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
<!-- List issue if it fixes/closes/implements one using the "Fixes
#<number>" or "Closes #<number>" syntax -->
All columns are always shown for TAGrading, which can be quite
cumbersome to the user.

### What is the new behavior?
The user is able to select which columns they want to see when
TAGrading.

![image](https://github.com/user-attachments/assets/c62b90ac-d1a1-48a0-a458-da6f7fce902c)

![image](https://github.com/user-attachments/assets/62ba1057-3b0d-4146-9f42-4db7863a2d52)

![image](https://github.com/user-attachments/assets/3f12956f-b29b-408c-b54b-91ed2fdb4bd5)


### Other information?
<!-- Is this a breaking change? -->
<!-- How did you test -->

---------

Co-authored-by: William Allen <16820599+williamjallen@users.noreply.github.com>
Co-authored-by: Justin Manion <jmanion32@gmail.com>

site/app/templates/grading/electronic/Details.twig
@@ -77,6 +77,41 @@
     {% endif %}
     <button type="button" class="btn btn-primary" onclick="collapseAllSections()" data-testid="collapse-all-sections">Collapse All Sections</button>
     <button type="button" class="btn btn-primary" onclick="expandAllSections()" data-testid="expand-all-sections">Expand All Sections</button>
+    {% include 'Vue.twig' with {
+        'component': 'toggleColumns',
+        'style': 'display: inline;',
+        'args': {
+            'columns': [
+                "index",
+                "section",
+                "user_id",
+                "user_given",
+                "user_family",
+                "autograding",
+                "graded_questions",
+                "total",
+                "grading",
+                "active_version"
+            ],
+            'labels': [
+                "# / Count / Index",
+                "Section",
+                "User ID",
+                "User Given",
+                "User Family",
+                "Autograding",
+                "Graded Questions",
+                "Total",
+                "Grading",
+                "Active Version"
+            ],
+            'forced': [
+                'grading'
+            ],
+            'cookie': 'grading_details_columns',
+            'format': 'json',
+        },
+    } %}
     {% if message is not empty %}
     <div class="details-warning-box{% if message_warning %} details-warning-highlight{% else %} details-warning-neutral{% endif %}">
         {{ message|nl2br }}
@@ -127,7 +162,7 @@
             <tr>
                 {% for column in columns %}
                     {% if column.sort_type is defined %}
-                        <th style="width:{{ column.width }}; text-align: center" data-col-title="{{ column.title }}">
+                        <th style="text-align: center" data-col-title="{{ column.title }}">
                             <a href="#" onclick= "sortTableByColumn('{{ column.sort_type }}', '{{ sort == column.sort_type ? (direction == 'ASC' ? 'DESC' : 'ASC') : 'ASC' }}')"
                             {% if sort == column.sort_type %}
                                 class="active-sort"
@@ -142,7 +177,7 @@
                             </a>
                         </th>
                     {% else %}
-                        <th style="width:{{ column.width }}; text-align: center" data-col-title="{{ column.title }}">{{ column.title }}</th>
+                        <th style="text-align: center" data-col-title="{{ column.title }}">{{ column.title }}</th>
                     {% endif %}
                 {% endfor %}
             </tr>

site/app/views/grading/ElectronicGraderView.php
@@ -494,9 +494,18 @@ HTML;
      * @param bool $show_import_teams_button
      * @param bool $show_export_teams_button
      * @param bool $show_edit_teams
+     * @param string $past_grade_start_date
+     * @param bool $view_all
+     * @param string $sort
+     * @param string $direction
+     * @param bool $anon_mode
+     * @param array<string, bool> $overrides
+     * @param array<string, string> $anon_ids
+     * @param bool $inquiry_status
+     * @param array<string,bool> $grading_details_columns
      * @return string
      */
-    public function detailsPage(Gradeable $gradeable, $graded_gradeables, $teamless_users, $graders, $empty_teams, $show_all_sections_button, $show_import_teams_button, $show_export_teams_button, $show_edit_teams, $past_grade_start_date, $view_all, $sort, $direction, $anon_mode, $overrides, $anon_ids, $inquiry_status) {
+    public function detailsPage(Gradeable $gradeable, array $graded_gradeables, array $teamless_users, array $graders, array $empty_teams, bool $show_all_sections_button, bool $show_import_teams_button, bool $show_export_teams_button, bool $show_edit_teams, string $past_grade_start_date, bool $view_all, string $sort, string $direction, bool $anon_mode, array $overrides, array $anon_ids, bool $inquiry_status, array $grading_details_columns) {
         $collapsed_sections = isset($_COOKIE['collapsed_sections']) ? json_decode(rawurldecode($_COOKIE['collapsed_sections'])) : [];
 
         $peer = false;
@@ -504,12 +513,11 @@ HTML;
             $peer = true;
         }
         //Each table column is represented as an array with the following entries:
-        // width => how wide the column should be on the page, <td width=X>
         // title => displayed title in the table header
         // function => maps to a macro in Details.twig:render_student
         $columns = [];
-        $columns[]             = ["width" => "2%",  "title" => "",                 "function" => "index"];
-        $columns[]             = ["width" => "8%",  "title" => "Section",          "function" => "section"];
+        $columns[] = ["title" => "#", "function" => "index"];
+        $columns[] = ["title" => "Section", "function" => "section"];
 
         $team_and_anon = ($this->core->getUser()->getGroup() === User::GROUP_LIMITED_ACCESS_GRADER &&
             $gradeable->getLimitedAccessBlind() === 2);
@@ -517,24 +525,24 @@ HTML;
         if ($peer || $anon_mode || $team_and_anon) {
             if ($gradeable->isTeamAssignment()) {
                 if ($gradeable->getPeerBlind() === Gradeable::DOUBLE_BLIND_GRADING || $anon_mode || $team_and_anon) {
-                    $columns[] = ["width" => "10%", "title" => "Team ID",          "function" => "team_id_anon"];
+                    $columns[] = ["title" => "Team ID", "function" => "team_id_anon"];
                 }
                 $peer_and_anon = ($this->core->getUser()->getGroup() === User::GROUP_STUDENT &&
                     $gradeable->getPeerBlind() === Gradeable::DOUBLE_BLIND_GRADING);
                 if ($team_and_anon || $peer_and_anon || $anon_mode) {
-                    $columns[] = ["width" => "30%", "title" => "Team Members",     "function" => "team_members_anon"];
+                    $columns[] = ["title" => "Team Members", "function" => "team_members_anon"];
                 }
                 else {
-                    $columns[] = ["width" => "10%", "title" => "Team ID",          "function" => "team_id", "sort_type" => "id"];
-                    $columns[] = ["width" => "6%",  "title" => "Team Name",        "function" => "team_name"];
-                    $columns[] = ["width" => "32%", "title" => "Team Members",     "function" => "team_members"];
+                    $columns[] = ["title" => "Team ID", "function" => "team_id", "sort_type" => "id"];
+                    $columns[] = ["title" => "Team Name", "function" => "team_name"];
+                    $columns[] = ["title" => "Team Members", "function" => "team_members"];
                 }
             }
             elseif ($gradeable->getPeerBlind() !== Gradeable::DOUBLE_BLIND_GRADING && !$anon_mode) {
-                $columns[]     = ["width" => "30%", "title" => "Student",          "function" => "user_id"];
+                $columns[] = ["title" => "Student", "function" => "user_id"];
             }
             else {
-                $columns[]     = ["width" => "30%", "title" => "Student",          "function" => "user_id_anon"];
+                $columns[] = ["title" => "Student", "function" => "user_id_anon"];
             }
             // NOTE/REDESIGN FIXME: We might have autograding that is
             // penalty only.  The available positive autograding
@@ -546,64 +554,64 @@ HTML;
             // See also note in ElectronicGradeController.php
             if (count($gradeable->getAutogradingConfig()->getAllTestCases()) > 1 && $peer === false) {
                 //if ($gradeable->getAutogradingConfig()->getTotalNonHiddenNonExtraCredit() !== 0) {
-                $columns[]     = ["width" => "15%", "title" => "Autograding",      "function" => "autograding_peer", "sort_type" => "auto"];
+                $columns[] = ["title" => "Autograding", "function" => "autograding_peer", "sort_type" => "auto"];
             }
             if ($gradeable->isTaGrading()) {
-                $columns[]     = ["width" => "8%",  "title" => "Graded Questions", "function" => "graded_questions"];
+                $columns[] = ["title" => "Graded Questions", "function" => "graded_questions"];
             }
             if ($gradeable->isTeamAssignment() || $gradeable->getPeerBlind() !== Gradeable::DOUBLE_BLIND_GRADING) {
-                $columns[]     = ["width" => "10%", "title" => "Grading",          "function" => "grading"];
+                $columns[] = ["title" => "Grading", "function" => "grading"];
             }
             else {
-                $columns[]     = ["width" => "10%", "title" => "Grading",          "function" => "grading_blind"];
+                $columns[] = ["title" => "Grading", "function" => "grading_blind"];
             }
             if ($peer === false) {
-                $columns[]     = ["width" => "15%", "title" => "Total",            "function" => "total"];
+                $columns[] = ["title" => "Total", "function" => "total"];
             }
-            $columns[]         = ["width" => "15%", "title" => "Active Version",   "function" => "active_version"];
+            $columns[] = ["title" => "Active Version", "function" => "active_version"];
         }
         else {
             if ($gradeable->isTeamAssignment()) {
                 if ($show_edit_teams) {
-                    $columns[] = ["width" => "5%",  "title" => "Edit Teams",       "function" => "team_edit"];
-                    $columns[] = ["width" => "10%", "title" => "Team ID",          "function" => "team_id", "sort_type" => "id"];
-                    $columns[] = ["width" => "6%",  "title" => "Team Name",        "function" => "team_name"];
-                    $columns[] = ["width" => "26%", "title" => "Team Members",     "function" => "team_members"];
+                    $columns[] = ["title" => "Edit Teams", "function" => "team_edit"];
+                    $columns[] = ["title" => "Team ID", "function" => "team_id", "sort_type" => "id"];
+                    $columns[] = ["title" => "Team Name", "function" => "team_name"];
+                    $columns[] = ["title" => "Team Members", "function" => "team_members"];
                 }
                 else {
-                    $columns[] = ["width" => "10%", "title" => "Team ID",          "function" => "team_id", "sort_type" => "id"];
-                    $columns[] = ["width" => "10%", "title" => "Team Name",        "function" => "team_name"];
-                    $columns[] = ["width" => "40%", "title" => "Team Members",     "function" => "team_members"];
+                    $columns[] = ["title" => "Team ID", "function" => "team_id", "sort_type" => "id"];
+                    $columns[] = ["title" => "Team Name", "function" => "team_name"];
+                    $columns[] = ["title" => "Team Members", "function" => "team_members"];
                 }
             }
             else {
                 if ($this->core->getUser()->getGroup() === User::GROUP_LIMITED_ACCESS_GRADER && $gradeable->getLimitedAccessBlind() === Gradeable::SINGLE_BLIND_GRADING) {
-                    $columns[] = ["width" => "43%", "title" => "Student",          "function" => "user_id_anon"];
+                    $columns[] = ["title" => "Student", "function" => "user_id_anon"];
                 }
                 else {
-                    $columns[] = ["width" => "13%", "title" => "User ID",          "function" => "user_id", "sort_type" => "id"];
-                    $columns[] = ["width" => "15%", "title" => "First Name",       "function" => "user_given", "sort_type" => "first"];
-                    $columns[] = ["width" => "15%", "title" => "Last Name",        "function" => "user_family", "sort_type" => "last"];
+                    $columns[] = ["title" => "User ID", "function" => "user_id", "sort_type" => "id"];
+                    $columns[] = ["title" => "First Name", "function" => "user_given", "sort_type" => "first"];
+                    $columns[] = ["title" => "Last Name", "function" => "user_family", "sort_type" => "last"];
                 }
             }
             // NOTE/REDESIGN FIXME: Same note as above.
             if (count($gradeable->getAutogradingConfig()->getAllTestCases()) > 1) {
                 //if ($gradeable->getAutogradingConfig()->getTotalNonExtraCredit() !== 0) {
-                $columns[]     = ["width" => "9%",  "title" => "Autograding",      "function" => "autograding", "sort_type" => "auto"];
+                $columns[] = ["title" => "Autograding", "function" => "autograding", "sort_type" => "auto"];
             }
             if ($gradeable->isTaGrading()) {
-                $columns[]     = ["width" => "8%",  "title" => "Graded Questions", "function" => "graded_questions"];
+                $columns[] = ["title" => "Graded Questions", "function" => "graded_questions"];
             }
             if ($this->core->getUser()->getGroup() === User::GROUP_LIMITED_ACCESS_GRADER && $gradeable->getLimitedAccessBlind() === Gradeable::SINGLE_BLIND_GRADING) {
-                $columns[]     = ["width" => "8%",  "title" => "Grading",          "function" => "grading_blind"];
+                $columns[] = ["title" => "Grading", "function" => "grading_blind"];
             }
             else {
-                $columns[]     = ["width" => "8%",  "title" => "Grading",          "function" => "grading"];
+                $columns[] = ["title" => "Grading", "function" => "grading"];
             }
-            $columns[]         = ["width" => "7%",  "title" => "Total",            "function" => "total"];
-            $columns[]         = ["width" => "10%", "title" => "Active Version",   "function" => "active_version"];
+            $columns[] = ["title" => "Total", "function" => "total"];
+            $columns[] = ["title" => "Active Version", "function" => "active_version"];
             if ($gradeable->isTaGradeReleased()) {
-                $columns[]     = ["width" => "8%",  "title" => "Viewed Grade",     "function" => "viewed_grade"];
+                $columns[] = ["title" => "Viewed Grade", "function" => "viewed_grade"];
             }
         }
 
@@ -883,6 +891,11 @@ HTML;
                 $team_gradeable_view_history[$team_id]['hover_string'] = $hover_over_string;
             }
         }
+        if (count($grading_details_columns) > 0) {
+            $columns = array_filter($columns, function ($column) use ($grading_details_columns) {
+                return array_key_exists($column['function'], $grading_details_columns) && $grading_details_columns[$column['function']];
+            });
+        }
         $details_base_url = $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'details']);
         $details_base_path = '\/gradeable\/' . $gradeable->getId() . '/grading/details';
         $this->core->getOutput()->addInternalCss('details.css');
@@ -934,6 +947,7 @@ HTML;
             "overrides" => $overrides,
             "anon_ids" => $anon_ids,
             "max_team_name_length" => Team::MAX_TEAM_NAME_LENGTH,
+            "csrf_token" => $this->core->getCsrfToken(),
         ]);
     }
 

site/vue/src/components/toggleColumns.vue
@@ -2,30 +2,61 @@
 import Cookies from 'js-cookie';
 import { onMounted, ref } from 'vue';
 import Popup from './popup.vue';
+export type ColumnFormats = 'bits' | 'json';
 
-const { columns, labels, cookie } = defineProps<{
+const { columns, labels, cookie, forced, format = 'bits', buttonWrapped } = defineProps<{
     columns: string[];
     labels: string[];
     cookie: string;
+    forced?: string[];
+    format?: ColumnFormats;
+    buttonWrapped?: boolean;
 }>();
 
 const selected = ref<boolean[]>([]);
 const visible = ref(false);
 
 function loadColumns() {
-    const cookieData = Cookies.get(cookie)?.split('-') || [];
-    selected.value = columns.map((_, i) => cookieData[i] === '1');
+    if (format === 'json') {
+        const cookieData: Record<string, boolean> = JSON.parse(decodeURIComponent(Cookies.get(cookie) ?? encodeURIComponent('{}'))) as Record<string, boolean>;
+        for (const cookieCol of Object.keys(cookieData)) {
+            if (columns.includes(cookieCol)) {
+                selected.value[columns.indexOf(cookieCol)] = cookieData[cookieCol];
+            }
+        }
+        for (const col of columns) {
+            if (cookieData[col] === undefined) {
+                selected.value[columns.indexOf(col)] = true;
+            }
+        }
+        return;
+    }
+    else if (format === 'bits') {
+        const cookieData = Cookies.get(cookie)?.split('-') || Array(columns.length).fill('1');
+        selected.value = columns.map((_, i) => cookieData[i] === '1');
+    }
 }
 function saveColumns() {
-    Cookies.set(
-        cookie,
-        selected.value.map((v) => (v ? 1 : 0)).join('-'),
-        { expires: 365, path: '' },
-    );
+    if (format === 'json') {
+        const cookieData: Record<string, boolean> = {};
+        columns.forEach((col, i) => {
+            cookieData[col] = selected.value[i];
+        });
+        Cookies.set(cookie, JSON.stringify(cookieData), { expires: 365, path: '/' });
+        window.location.reload();
+        return;
+    }
+    else if (format === 'bits') {
+        Cookies.set(
+            cookie,
+            selected.value.map((v) => (v ? 1 : 0)).join('-'),
+            { expires: 365, path: '/' },
+        );
+    }
     window.location.reload();
 }
 function fillAll(val: boolean) {
-    selected.value = selected.value.map(() => val);
+    selected.value = selected.value.map((_, idx) => forced?.includes(columns[idx]) || val);
 }
 
 function toggle() {
@@ -47,7 +78,10 @@ onMounted(loadColumns);
     @save="saveColumns"
   >
     <template #trigger>
-      <div class="btn-wrapper">
+      <div
+        v-if="buttonWrapped"
+        class="btn-wrapper"
+      >
         <a
           id="toggle-columns"
           data-testid="toggle-columns"
@@ -55,6 +89,13 @@ onMounted(loadColumns);
           @click="toggle"
         >Toggle Columns</a>
       </div>
+      <a
+        v-else
+        id="toggle-columns"
+        data-testid="toggle-columns"
+        class="btn btn-primary"
+        @click="toggle"
+      >Toggle Columns</a>
     </template>
     <template #default>
       <p class="toggle-columns-instructions">
@@ -67,11 +108,12 @@ onMounted(loadColumns);
           class="toggle-checkbox-area"
         >
           <input
-            :id="id"
+            :id="`toggle-${id}`"
             v-model="selected[idx]"
             type="checkbox"
             class="toggle-columns-box"
-            :data-testid="id"
+            :disabled="forced?.includes(id)"
+            :data-testid="`toggle-${id}`"
           />
           <label :for="id">{{ labels[idx] }}</label>
         </div>

site/app/controllers/grading/ElectronicGraderController.php
@@ -982,6 +982,13 @@ class ElectronicGraderController extends AbstractController {
         $sort = isset($_COOKIE['sort']) ? $_COOKIE['sort'] : 'id';
         $direction = isset($_COOKIE['direction']) ? $_COOKIE['direction'] : 'ASC';
 
+
+        //Get grading_details Columns
+        $grading_details_columns = [];
+        if (isset($_COOKIE['grading_details_columns'])) {
+            $grading_details_columns = json_decode($_COOKIE['grading_details_columns'], true);
+        }
+
         //Checks to see if the Grader has access to all users in the course,
         //Will only show the sections that they are graders for if not TA or Instructor
         $can_show_all = $this->core->getAccess()->canI("grading.electronic.details.show_all");
@@ -1080,7 +1087,7 @@ class ElectronicGraderController extends AbstractController {
             }
         }
 
-        $this->core->getOutput()->renderOutput(['grading', 'ElectronicGrader'], 'detailsPage', $gradeable, $graded_gradeables, $teamless_users, $graders, $empty_teams, $show_all_sections_button, $show_import_teams_button, $show_export_teams_button, $show_edit_teams, $past_grade_start_date, $view_all, $sort, $direction, $anon_mode, $overrides, $anon_ids, $inquiry_status);
+        $this->core->getOutput()->renderOutput(['grading', 'ElectronicGrader'], 'detailsPage', $gradeable, $graded_gradeables, $teamless_users, $graders, $empty_teams, $show_all_sections_button, $show_import_teams_button, $show_export_teams_button, $show_edit_teams, $past_grade_start_date, $view_all, $sort, $direction, $anon_mode, $overrides, $anon_ids, $inquiry_status, $grading_details_columns);
 
         if ($show_edit_teams) {
             $all_reg_sections = $this->core->getQueries()->getRegistrationSections();

site/app/templates/admin/users/StudentList.twig
@@ -7,24 +7,25 @@
                 'component': 'toggleColumns',
                 'style': 'flex: 1', 
                 'args': {
+                    'buttonWrapped': true,
                     'columns': [
-                        'toggle-registration-section',
-                        'toggle-user-id',
-                        'toggle-first-name',
-                        'toggle-last-name',
-                        'toggle-pronouns',
-                        'toggle-rotating-section',
-                        'toggle-time-zone',
-                        'toggle-view-grades',
-                        'toggle-late-days',
-                        'toggle-registration-type',
-                        'toggle-edit-student',
-                        'toggle-delete-student',
-                        'toggle-user-numeric-id',
-                        'toggle-legal-first-name',
-                        'toggle-legal-last-name',
-                        'toggle-email',
-                        'toggle-secondary-email',
+                        'registration-section',
+                        'user-id',
+                        'first-name',
+                        'last-name',
+                        'pronouns',
+                        'rotating-section',
+                        'time-zone',
+                        'view-grades',
+                        'late-days',
+                        'registration-type',
+                        'edit-student',
+                        'delete-student',
+                        'user-numeric-id',
+                        'legal-first-name',
+                        'legal-last-name',
+                        'email',
+                        'secondary-email',
                     ],
                     'labels': [
                         'Registration Section',

commit 74644e600acf824fe639bd23593f3a8935a7a672

[Bugfix:TAGrading] Always show new columns in TAGrading (#11794)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
When switching between different TAGrading views, there is a regression
in #11487 that means non-default columns will be hidden by default.
Sometimes it does not even show toggles to enable the new columns.

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->
The list of columns is dynamic and automatically enabled for any columns
that have not yet been seen by the user.
Fix #11754 

### What steps should a reviewer take to reproduce or test the bug or
new feature?
On main:
1. Open the grading details page
2. Turn off first and last name
3. switch to anon mode

### Automated Testing & Documentation
<!-- Is this feature sufficiently tested by unit tests and end-to-end
tests?
If this PR does not add/update the necessary automated tests, write a
new GitHub issue and link it below.
Is this feature sufficiently documented on submitty.org?
Link related PRs or new GitHub issue to update documentation. -->

### Other information
<!-- Is this a breaking change?  
Does this PR include migrations to update existing installations?  
Are there security concerns with this PR? -->

site/app/templates/grading/electronic/Details.twig
@@ -81,30 +81,8 @@
         'component': 'toggleColumns',
         'style': 'display: inline;',
         'args': {
-            'columns': [
-                "index",
-                "section",
-                "user_id",
-                "user_given",
-                "user_family",
-                "autograding",
-                "graded_questions",
-                "total",
-                "grading",
-                "active_version"
-            ],
-            'labels': [
-                "# / Count / Index",
-                "Section",
-                "User ID",
-                "User Given",
-                "User Family",
-                "Autograding",
-                "Graded Questions",
-                "Total",
-                "Grading",
-                "Active Version"
-            ],
+            'columns': all_columns|map(column => column.function),
+            'labels': all_columns|map(column => column.title),
             'forced': [
                 'grading'
             ],

site/app/views/grading/ElectronicGraderView.php
@@ -891,11 +891,11 @@ HTML;
                 $team_gradeable_view_history[$team_id]['hover_string'] = $hover_over_string;
             }
         }
-        if (count($grading_details_columns) > 0) {
-            $columns = array_filter($columns, function ($column) use ($grading_details_columns) {
-                return array_key_exists($column['function'], $grading_details_columns) && $grading_details_columns[$column['function']];
-            });
-        }
+
+        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns) {
+            return !array_key_exists($column['function'], $grading_details_columns) || $grading_details_columns[$column['function']];
+        });
+
         $details_base_url = $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'details']);
         $details_base_path = '\/gradeable\/' . $gradeable->getId() . '/grading/details';
         $this->core->getOutput()->addInternalCss('details.css');
@@ -923,7 +923,8 @@ HTML;
             "show_import_teams_button" => $show_import_teams_button,
             "show_export_teams_button" => $show_export_teams_button,
             "past_grade_start_date" => $past_grade_start_date,
-            "columns" => $columns,
+            "columns" => $shown_columns,
+            "all_columns" => $columns,
             "export_teams_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'export']),
             "randomize_team_rotating_sections_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'randomize_rotating']),
             "grade_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'grade']),

site/vue/src/components/toggleColumns.vue
@@ -38,7 +38,7 @@ function loadColumns() {
 }
 function saveColumns() {
     if (format === 'json') {
-        const cookieData: Record<string, boolean> = {};
+        const cookieData: Record<string, boolean> = JSON.parse(decodeURIComponent(Cookies.get(cookie) ?? encodeURIComponent('{}'))) as Record<string, boolean>;
         columns.forEach((col, i) => {
             cookieData[col] = selected.value[i];
         });