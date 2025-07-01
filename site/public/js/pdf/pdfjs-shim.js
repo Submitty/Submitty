0e6ea08fc9f27b51ccd0a7b875ebf087abafe428
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-07-01 09:09:19 -0400

    Valid PDF Worker Setup

--- site/public/js/pdf/pdfjs-shim.js
+++ site/public/js/pdf/pdfjs-shim.js
@@ -1,12 +1,15 @@
-import * as pdfjsWorker from "/vendor/pdfjs/pdf.worker.min.mjs";
 import * as pdfjsLib from "/vendor/pdfjs/pdf.min.mjs";
 import * as pdfjsViewer from "/vendor/pdfjs/pdf_viewer.mjs";
 
+// Manually set the pdfjsLib and pdfjsViewer global variables
 window.pdfjsLib = pdfjsLib;
 window.pdfjsViewer = pdfjsViewer;
-window.pdfjsLib.GlobalWorkerOptions.workerSrc = "/vendor/pdfjs/pdf.worker.min.mjs";
 
-console.log(window.pdfjsLib);
-console.log(window.pdfjsViewer);
-console.log(pdfjsWorker);
-console.log(window.pdfjsLib.GlobalWorkerOptions.workerSrc);
+// Register the worker through a blob URL to avoid CORS issues
+const absoluteUrl = new URL('/vendor/pdfjs/pdf.worker.min.mjs', window.location.origin).href;
+const src = `import '${absoluteUrl}';`;
+
+const blob = new Blob([src], { type: 'application/javascript' });
+const blobUrl = URL.createObjectURL(blob);
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = blobUrl;
