019819b11076e282278f63502dc7b5a7ff4a0e7e
martig7 <martig7@rpi.edu>
2025-07-09 13:49:04 -0400

    Chatroom Row Refactor

--- site/public/js/chatroom.js
+++ site/public/js/chatroom.js
@@ -1,4 +1,38 @@
-/* global csrfToken, buildCourseUrl, displayErrorMessage, WebSocketClient */
+/* global csrfToken, buildCourseUrl, displayErrorMessage, WebSocketClient, Twig */
+
+/**
+ * Asynchronously load the chatroom row template
+ * @return {Promise}
+ */
+function loadChatroomTemplate(chatroomId) {
+    return new Promise((resolve, reject) => {
+        Twig.twig({
+            id: `ChatroomRow`,
+            href: '/templates/chat/ChatroomRow.twig',
+            allowInlineIncludes: true,
+            async: true,
+            error: function () {
+                reject();
+            },
+            load: function () {
+                resolve();
+            },
+        });
+    });
+}
+
+function renderChatroomRow(chatroomId, description, title, hostName, isAllowAnon, isAdmin, isActive, base_url) {
+    return Twig.twig({ ref: `ChatroomRow` }).render({
+        id: chatroomId,
+        description: description,
+        title: title,
+        hostName: hostName,
+        isAllowAnon: isAllowAnon,
+        isAdmin: isAdmin,
+        isActive: isActive,
+        baseUrl: base_url
+    });
+}
 
 function fetchMessages(chatroomId) {
     $.ajax({
@@ -111,19 +145,16 @@ function initChatroomSocketClient(chatroomId) {
     window.socketClient.open(`chatroom_${chatroomId}`);
 }
 
-function initChatroomListSocketClient() {
+function initChatroomListSocketClient(user_admin, base_url) {
     window.chatroomListSocketClient = new WebSocketClient();
     window.chatroomListSocketClient.onmessage = (msg) => {
         console.log('Received message from chatroom socket:', msg.type, msg);
         switch (msg.type) {
             case 'chat_open':
-                handleChatOpen(msg);
+                handleChatToggle(msg, user_admin, true, base_url);
                 break;
             case 'chat_close': {
-                const row = document.getElementById(`chatroom-row-${msg.id}`);
-                if (row) {
-                    row.remove();
-                }
+                handleChatToggle(msg, user_admin, false), base_url;
                 break;
             }
             default:
@@ -185,7 +216,7 @@ function deleteChatroomForm(chatroom_id, chatroom_name, base_url) {
     }
 }
 
-function toggle_chatroom(chatroomId, active) {
+function toggleChatroom(chatroomId, active) {
     const form = document.getElementById(`chatroom_toggle_form_${chatroomId}`);
     if (!active || confirm('This will close the chatroom. Are you sure?')) {
         form.submit();
@@ -202,60 +233,18 @@ function showJoinMessage(message) {
     }, 3000);
 }
 
-function handleChatOpen(msg) {
+function handleChatToggle(msg, user_admin, isOpening, base_url) {
     const tableBody = document.querySelector('#chatrooms-table tbody');
     if (!tableBody) {
         return;
     }
-    if (document.getElementById(`chatroom-row-${msg.id}`)) {
-        return;
+    const row = document.getElementById(`chatroom-row-${msg.id}`);
+    if (row) {
+        row.remove();
     }
-    const tr = document.createElement('tr');
-    tr.id = `chatroom-row-${msg.id}`;
 
-    const tdTitle = document.createElement('td');
-    const spanTitle = document.createElement('span');
-    spanTitle.className = 'display-short';
-    spanTitle.title = msg.title;
-    spanTitle.textContent = msg.title.length > 30 ? `${msg.title.slice(0, 30)}...` : msg.title;
-    tdTitle.appendChild(spanTitle);
-    tr.appendChild(tdTitle);
-
-    const tdHostName = document.createElement('td');
-    tdHostName.textContent = msg.host_name;
-    tr.appendChild(tdHostName);
-
-    const tdDescription = document.createElement('td');
-    const spanDescription = document.createElement('span');
-    spanDescription.className = 'display-short';
-    spanDescription.title = msg.description;
-    spanDescription.textContent = msg.description.length > 45 ? `${msg.description.slice(0, 45)}...` : msg.description;
-    tdDescription.appendChild(spanDescription);
-    tr.appendChild(tdDescription);
-
-    const tdLinks = document.createElement('td');
-    const joinLink = document.createElement('a');
-    joinLink.href = `${msg.base_url}/${msg.id}`;
-    joinLink.className = 'btn btn-primary';
-    joinLink.textContent = 'Join';
-    tdLinks.appendChild(joinLink);
-
-    if (msg.allow_anon) {
-        tdLinks.appendChild(document.createTextNode(' '));
-        const iTag = document.createElement('i');
-        iTag.textContent = 'or';
-        tdLinks.appendChild(iTag);
-        tdLinks.appendChild(document.createTextNode(' '));
-
-        const anonJoinLink = document.createElement('a');
-        anonJoinLink.href = `${msg.base_url}/${msg.id}/anonymous`;
-        anonJoinLink.className = 'btn btn-default';
-        anonJoinLink.textContent = 'Join As Anon.';
-        tdLinks.appendChild(anonJoinLink);
-    }
-    tr.appendChild(tdLinks);
-
-    tableBody.appendChild(tr);
+    let rowHtml = renderChatroomRow(msg.id, msg.description, msg.title, msg.host_name, msg.allow_anon, user_admin, isOpening, base_url);
+    tableBody.insertAdjacentHTML('beforeend', rowHtml);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -295,7 +284,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
     const chatroomsTable = document.getElementById('chatrooms-table');
-    if (chatroomsTable) {
-        initChatroomListSocketClient();
+    const allChatroomData = document.getElementById('all-chatroom-data');
+    if (chatroomsTable && allChatroomData) {
+        loadChatroomTemplate();
+        const pageData = JSON.parse(allChatroomData.textContent);
+        const { user_admin, base_url } = pageData;
+        initChatroomListSocketClient(user_admin, base_url);
     }
 });
