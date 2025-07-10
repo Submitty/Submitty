3706c78a0c2da3990c52533c2d5145845395120e
williamschen23 <chenwill2005@gmail.com>
2025-07-10 15:06:47 -0400

    verify tutorial courses

--- site/cypress/e2e/Cypress-Autograding-Tutorial/test.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Test course should exist', () => {
-    it('Check test course exists', () => {
-        cy.login();
-        cy.visit(['testing', 'gradeable', 'open_homework', 'grading']);
-        // Use this file to create a test based around submissions
-    });
-});
---
+++ site/cypress/e2e/Cypress-Autograding-Tutorial/verify_tutorial_courses.spec.js
@@ -0,0 +1,29 @@
+const checkGradeable = (gradeable_id) => {
+    cy.visit(['tutorial']);
+    cy.get(`[data-testid="${gradeable_id}"`).find('[data-testid="submit-btn"]').click();
+    cy.get('[data-testid="new-submission-info"]').contains('New submission for');
+};
+
+describe('Test course should exist', () => {
+    it('Check test gradeables exists', () => {
+        cy.login('instructor');
+        checkGradeable('01_simple_python');
+        checkGradeable('02_simple_cpp');
+        checkGradeable('03_multipart');
+        checkGradeable('04_python_static_analysis');
+        checkGradeable('05_cpp_static_analysis');
+        checkGradeable('06_loop_types');
+        checkGradeable('07_loop_depth');
+        checkGradeable('08_memory_debugging');
+        checkGradeable('09_java_testing');
+        checkGradeable('10_java_coverage');
+        checkGradeable('11_resources');
+        checkGradeable('12_system_calls');
+        checkGradeable('13_cmake_compilation');
+        checkGradeable('14_tkinter');
+        checkGradeable('15_GLFW');
+        checkGradeable('16_docker_network_python');
+        checkGradeable('17_dispatch_actions_and_standard_input');
+        // checkGradeable('18_postgres_database');
+    });
+});
