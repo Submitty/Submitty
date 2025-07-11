b096fbc35e3369b1dc54b073da0447ee4d616dd8
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-07-11 15:37:54 -0400

    Linting

--- site/public/js/pdf/pdfjs-shim.js
+++ site/public/js/pdf/pdfjs-shim.js
@@ -1,5 +1,5 @@
-import * as pdfjsLib from "/vendor/pdfjs/pdf.min.mjs";
-import * as pdfjsViewer from "/vendor/pdfjs/pdf_viewer.mjs";
+import * as pdfjsLib from '/vendor/pdfjs/pdf.min.mjs';
+import * as pdfjsViewer from '/vendor/pdfjs/pdf_viewer.mjs';
 
 // Manually set the pdfjsLib and pdfjsViewer global variables
 window.pdfjsLib = pdfjsLib;
