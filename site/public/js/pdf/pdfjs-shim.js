fb4ac23aba997c01656ef4ad51521271a324629b
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-07-14 09:26:06 -0400

    Clearer pdf-shim.js workaround comment

--- site/public/js/pdf/pdfjs-shim.js
+++ site/public/js/pdf/pdfjs-shim.js
@@ -1,6 +1,6 @@
 import * as pdfjsLib from '/vendor/pdfjs/pdf.min.mjs';
 import * as pdfjsViewer from '/vendor/pdfjs/pdf_viewer.mjs';
 
-// Manually set the pdfjsLib and pdfjsViewer global variables
+// Maintain compatibility with code expecting pdfjs globals due to ES6 modules
 window.pdfjsLib = pdfjsLib;
 window.pdfjsViewer = pdfjsViewer;
