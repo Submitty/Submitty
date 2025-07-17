0aade00ba32269b5906feab3e7fc6696d0217dba
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-07-17 15:08:18 -0400

    Upgrade to v3.0.2

--- .setup/install_submitty/install_site.sh
+++ .setup/install_submitty/install_site.sh
@@ -408,12 +408,14 @@ chmod 540 ${SUBMITTY_INSTALL_DIR}/site/cgi-bin/*
 chmod 550 ${SUBMITTY_INSTALL_DIR}/site/cgi-bin/git-http-backend
 
 mkdir -p "${SUBMITTY_INSTALL_DIR}/site/incremental_build"
+mkdir -p ${NODE_FOLDER}/.vue-global-types
 chgrp "${PHP_USER}" "${SUBMITTY_INSTALL_DIR}/site/incremental_build"
 
 echo "Running esbuild"
 chmod a+x ${NODE_FOLDER}/esbuild/bin/esbuild
 chmod a+x ${NODE_FOLDER}/typescript/bin/tsc
 chmod a+x ${NODE_FOLDER}/vue-tsc/bin/vue-tsc.js
+chown -R ${PHP_USER}:${PHP_USER} ${NODE_FOLDER}/.vue-global-types
 chmod a+x ${NODE_FOLDER}/vite/bin/vite.js
 chmod g+w "${SUBMITTY_INSTALL_DIR}/site/incremental_build"
 chmod -R u+w "${SUBMITTY_INSTALL_DIR}/site/incremental_build"
