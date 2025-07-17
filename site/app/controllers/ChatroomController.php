3c8a6775ba52d95b733117b85731642c247ecf0f
William Allen <16820599+williamjallen@users.noreply.github.com>
2025-07-17 12:56:57 -0400

    [Refactor:System] Refactor access control annotations (#11647)
    ### What is the current behavior?
    The current `@AccessControl` annotation uses `doctrine/annotations` to
    parse the annotations from PHPDoc comments. PHP now supports annotations
    natively, rendering `doctrine/annotations` unnecessary.
    
    ### What is the new behavior?
    This PR converts all of the access control annotations from PHPDoc
    comments to native PHP annotations.
    
    ---------
    
    Co-authored-by: Michael Papadopoulos <micpap25@gmail.com>
    Co-authored-by: Alex Lavallee <alex@lavallee.one>
    Co-authored-by: Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>

--- site/app/controllers/ChatroomController.php
+++ site/app/controllers/ChatroomController.php
@@ -61,9 +61,7 @@ class ChatroomController extends AbstractController {
         );
     }
 
-    /**
-     * @AccessControl(role="INSTRUCTOR")
-     */
+    #[AccessControl(role: "INSTRUCTOR")]
     #[Route("/courses/{_semester}/{_course}/chat/new", methods:["POST"])]
     public function addChatroom(): RedirectResponse {
         $em = $this->core->getCourseEntityManager();
@@ -129,9 +127,7 @@ class ChatroomController extends AbstractController {
         );
     }
 
-    /**
-     * @AccessControl(role="INSTRUCTOR")
-     */
+    #[AccessControl(role: "INSTRUCTOR")]
     #[Route("/courses/{_semester}/{_course}/chat/delete", methods: ["POST"])]
     public function deleteChatroom(): JsonResponse {
         $chatroom_id = intval($_POST['chatroom_id'] ?? -1);
@@ -148,9 +144,7 @@ class ChatroomController extends AbstractController {
         return JsonResponse::getSuccessResponse();
     }
 
-    /**
-     * @AccessControl(role="INSTRUCTOR")
-     */
+    #[AccessControl(role: "INSTRUCTOR")]
     #[Route("/courses/{_semester}/{_course}/chat/{chatroom_id}/edit", methods: ["POST"])]
     public function editChatroom(string $chatroom_id): RedirectResponse {
         $em = $this->core->getCourseEntityManager();
@@ -176,9 +170,7 @@ class ChatroomController extends AbstractController {
         return new RedirectResponse($this->core->buildCourseUrl(['chat']));
     }
 
-    /**
-     * @AccessControl(role="INSTRUCTOR")
-     */
+    #[AccessControl(role: "INSTRUCTOR")]
     #[Route("/courses/{_semester}/{_course}/chat/{chatroom_id}/toggleActiveStatus", methods: ["POST"], requirements: ["chatroom_id" => "\d+"])]
     public function toggleChatroomActiveStatus(string $chatroom_id): RedirectResponse {
         $em = $this->core->getCourseEntityManager();
