6a30f810407003e950fc6bc0663c7865fb1076d2
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-07-22 10:53:00 -0400

    [Refactor:Installation] Simplify grading library build process by creating build_grading.sh script

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -202,66 +202,8 @@ chown -R  root:root "${SUBMITTY_INSTALL_DIR}/vendor"
 find "${SUBMITTY_INSTALL_DIR}/vendor" -type d -exec chmod 555 {} \;
 find "${SUBMITTY_INSTALL_DIR}/vendor" -type f -exec chmod 444 {} \;
 
-
-########################################################################################################################
-########################################################################################################################
-# COPY THE CORE GRADING CODE (C++ files) & BUILD THE SUBMITTY GRADING LIBRARY
-
 echo -e "Copy the grading code"
-
-# copy the files from the repo
-rsync -rtz "${SUBMITTY_REPOSITORY}/grading" "${SUBMITTY_INSTALL_DIR}/src"
-
-# copy the allowed_autograding_commands_default.json to config
-rsync -tz "${SUBMITTY_REPOSITORY}/grading/allowed_autograding_commands_default.json" "${SUBMITTY_INSTALL_DIR}/config"
-
-# replace filling variables
-sed -i -e "s|__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__|$SUBMITTY_INSTALL_DIR|g" "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_default.json"
-
-# # change permissions of allowed_autograding_commands_default.json
-chown "root":"root" "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_default.json"
-chmod 644 "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_default.json"
-
-# create allowed_autograding_commands_custom.json if doesnt exist
-if [[ ! -e "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json" ]]; then
-    rsync -tz "${SUBMITTY_REPOSITORY}/grading/allowed_autograding_commands_custom.json" "${SUBMITTY_INSTALL_DIR}/config"
-fi
-
-# replace filling variables
-sed -i -e "s|__INSTALL__FILLIN__SUBMITTY_INSTALL_DIR__|$SUBMITTY_INSTALL_DIR|g" "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json"
-
-# # change permissions of allowed_autograding_commands_custom.json
-chown "root":"root" "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json"
-chmod 644 "${SUBMITTY_INSTALL_DIR}/config/allowed_autograding_commands_custom.json"
-
-#replace necessary variables
-array=( Sample_CMakeLists.txt CMakeLists.txt system_call_check.cpp seccomp_functions.cpp execute.cpp load_config_json.cpp )
-for i in "${array[@]}"; do
-    replace_fillin_variables "${SUBMITTY_INSTALL_DIR}/src/grading/${i}"
-done
-
-# building the autograding library
-mkdir -p "${SUBMITTY_INSTALL_DIR}/src/grading/lib"
-pushd "${SUBMITTY_INSTALL_DIR}/src/grading/lib"
-cmake ..
-set +e
-make
-if [ "$?" -ne 0 ] ; then
-    echo "ERROR BUILDING AUTOGRADING LIBRARY"
-    exit 1
-fi
-set -e
-popd > /dev/null
-
-# root will be owner & group of these files
-chown -R  root:root "${SUBMITTY_INSTALL_DIR}/src"
-# "other" can cd into & ls all subdirectories
-find "${SUBMITTY_INSTALL_DIR}/src" -type d -exec chmod 555 {} \;
-# "other" can read all files
-find "${SUBMITTY_INSTALL_DIR}/src" -type f -exec chmod 444 {} \;
-
-chgrp submitty_daemon "${SUBMITTY_INSTALL_DIR}/src/grading/python/submitty_router.py"
-chmod g+wrx           "${SUBMITTY_INSTALL_DIR}/src/grading/python/submitty_router.py"
+bash "${SUBMITTY_REPOSITORY}/.setup/install_submitty/build_grading.sh" "config=${SUBMITTY_CONFIG_DIR:?}"
 
 
 #Set up sample files if not in worker mode.
