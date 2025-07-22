89bcd974afaea21e92f03e241cd7fc6a1e9314b6
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-07-22 14:41:29 -0400

    Rename DocToPDF to DocxToPDF and update related paths for handling DOCX files

--- .setup/install_submitty/setup_directories.sh
+++ .setup/install_submitty/setup_directories.sh
@@ -108,7 +108,7 @@ mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/autograding_stack_traces"
 if [ "${IS_WORKER:?}" == 0 ]; then
     mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/access"
     mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/bulk_uploads"
-    mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/doc_to_pdf"
+    mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/docx_to_pdf"
     mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/emails"
     mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/site_errors"
     mkdir -p "${SUBMITTY_DATA_DIR:?}/logs/socket_errors"
@@ -152,7 +152,7 @@ chown  -R "${DAEMON_USER:?}":"${DAEMONPHP_GROUP:?}"   "${SUBMITTY_DATA_DIR:?}/lo
 if [ "${IS_WORKER:?}" == 0 ]; then
     chown  -R "${PHP_USER:?}":"${COURSE_BUILDERS_GROUP:?}"    "${SUBMITTY_DATA_DIR:?}/logs/access"
     chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/bulk_uploads"
-    chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/doc_to_pdf"
+    chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/docx_to_pdf"
     chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/emails"
     chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/course_creation"
     chown  -R "${DAEMON_USER:?}":"${COURSE_BUILDERS_GROUP:?}" "${SUBMITTY_DATA_DIR:?}/logs/rainbow_grades"
