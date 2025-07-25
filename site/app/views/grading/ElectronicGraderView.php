Submitty/Submitty

commit e49ee8f53a05fa985b9bfd1614b707bfd82e45bb

[Feature:TAGrading] Add active graders to grading details (#11817)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
Currently the active graders are only visible when grading an individual
student.

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->
They are now available on the grading details page.

![image](https://github.com/user-attachments/assets/8b21e927-2247-4447-b2e4-b33ca4fd0f93)

![image](https://github.com/user-attachments/assets/3b3b9cb1-ee06-44f6-877b-503a3ea413e7)


### What steps should a reviewer take to reproduce or test the bug or
new feature?
1. Start grading a user and open a component
2. In another tab view the grading details page

### Automated Testing & Documentation
<!-- Is this feature sufficiently tested by unit tests and end-to-end
tests?
If this PR does not add/update the necessary automated tests, write a
new GitHub issue and link it below.
Is this feature sufficiently documented on submitty.org?
Link related PRs or new GitHub issue to update documentation. -->

### Other information
<!-- Is this a breaking change?  
Does this PR include migrations to update existing installations?  
Are there security concerns with this PR? -->

site/app/templates/grading/electronic/Details.twig
@@ -32,6 +32,10 @@
                     <i class="fas fa-circle grader-double"></i>
                     Component double-graded by a full access grader or instructor
                 </li>
+                <li>
+                    <i class="fas fa-circle grader-active"></i>
+                    Component actively being graded
+                </li>
             </ul>
 
             <div class="row-wrapper">
@@ -87,6 +91,7 @@
         'args': {
             'columns': all_columns|map(column => column.function),
             'labels': all_columns|map(column => column.title),
+            'hiddenColumns': default_hidden_columns,
             'forced': [
                 'grading'
             ],
@@ -198,7 +203,7 @@
                 <tbody class="details-content panel-content-active"{% if section.title in collapsed_sections %} style="display: none"{% endif %}>
                     {% if section.rows is defined %}
                         {% for info in section.rows %}
-                            {{ _self.render_student(_context, section, info.graded_gradeable, loop.index, info, columns, team_gradeable_view_history, overrides, anon_ids, max_team_name_length) }}
+                            {{ _self.render_student(_context, section, info.graded_gradeable, loop.index, info, columns, team_gradeable_view_history, overrides, anon_ids, max_team_name_length, active_graders) }}
                         {% endfor %}
                     {% endif %}
                     {% if section.teamless_users is defined %}
@@ -253,7 +258,7 @@
     </table>
 </div>
 
-{% macro render_student(context, section, graded_gradeable, index, info, columns, team_gradeable_view_history, overrides, anon_ids, max_team_name_length) %}
+{% macro render_student(context, section, graded_gradeable, index, info, columns, team_gradeable_view_history, overrides, anon_ids, max_team_name_length, active_graders) %}
     <tr class="grade-table" data-testid="grade-table"
         {% if not graded_gradeable.getSubmitter().isTeam() and graded_gradeable.getSubmitter().getUser().accessGrading() %}
             style='background: #7bd0f7;'
@@ -276,7 +281,7 @@
             {% elseif column.function == "autograding_peer" %}
                 {{ _self.render_column_autograding_peer(context, section, graded_gradeable, index, info, column) }}
             {% elseif column.function == "graded_questions" %}
-                {{ _self.render_column_graded_questions(context, section, graded_gradeable, index, info, column) }}
+                {{ _self.render_column_graded_questions(context, section, graded_gradeable, index, info, column, active_graders) }}
             {% elseif column.function == "grading" %}
                 {{ _self.render_column_grading(context, section, graded_gradeable, index, info, column, overrides, anon_ids) }}
             {% elseif column.function == "grading_blind" %}
@@ -407,13 +412,19 @@
 {% endmacro %}
 
 {# Graded Questions #}
-{% macro render_column_graded_questions(context, section, graded_gradeable, index, info, column) %}
+{% macro render_column_graded_questions(context, section, graded_gradeable, index, info, column, active_graders) %}
     <td class="td-graded-questions" style="text-align: center">
         <span>
             {%- for index in 0..info.graded_groups|length-1 -%}
             {# Non-Peer View for Stoplights #}
             {% if core.getUser().accessGrading() %}
-                    {%  if graded_gradeable.getAutoGradedGradeable().getActiveVersion() == 0%}
+                    {% set user_id = graded_gradeable.getSubmitter().getId() %}
+                    {% set user_graders = active_graders[user_id] is defined ? active_graders[user_id] : {} %}
+                    {% set component_graders = user_graders[graded_gradeable.getGradeable().getComponents()[index].getId()] is defined ? user_graders[graded_gradeable.getGradeable().getComponents()[index].getId()] : {} %}
+
+                    {% if component_graders is not empty %}
+                        <i title="Currently being graded by: {{ component_graders | map(grader => grader.grader_id) | join(', ') }}" class="fas fa-circle grader-active"></i>
+                    {%  elseif graded_gradeable.getAutoGradedGradeable().getActiveVersion() == 0%}
                         <i title="{{graded_gradeable.getGradeable().getComponents()[index].getTitle()}}" class="fas fa-circle grader-NULL"></i>
                     {% else %}
                         {% if info.graded_groups[index] =="peer-null"   %}

site/app/views/grading/ElectronicGraderView.php
@@ -504,10 +504,16 @@ HTML;
      * @param bool $inquiry_status
      * @param bool $filter_withdrawn_student
      * @param array<string,bool> $grading_details_columns
+     * @param array<string,array<number,array{
+     *      gc_id: number,
+     *      gc_title: string,
+     *      grader_id: string,
+     *      ag_user_id: ?string,
+     *      ag_team_id: ?string,
+     * }>> $active_graders
      * @return string
      */
-
-    public function detailsPage(Gradeable $gradeable, array $graded_gradeables, array $teamless_users, array $graders, array $empty_teams, bool $show_all_sections_button, bool $show_import_teams_button, bool $show_export_teams_button, bool $show_edit_teams, string $past_grade_start_date, bool $view_all, string $sort, string $direction, bool $anon_mode, array $overrides, array $anon_ids, bool $inquiry_status, bool $filter_withdrawn_student, array $grading_details_columns) {
+    public function detailsPage(Gradeable $gradeable, array $graded_gradeables, array $teamless_users, array $graders, array $empty_teams, bool $show_all_sections_button, bool $show_import_teams_button, bool $show_export_teams_button, bool $show_edit_teams, string $past_grade_start_date, bool $view_all, string $sort, string $direction, bool $anon_mode, array $overrides, array $anon_ids, bool $inquiry_status, bool $filter_withdrawn_student, array $grading_details_columns, array $active_graders) {
         $collapsed_sections = isset($_COOKIE['collapsed_sections']) ? json_decode(rawurldecode($_COOKIE['collapsed_sections'])) : [];
 
         $peer = false;
@@ -609,6 +615,7 @@ HTML;
             }
             else {
                 $columns[] = ["title" => "Grading", "function" => "grading"];
+                $columns[] = ["title" => "Active Graders", "function" => "active_graders"];
             }
             $columns[] = ["title" => "Total", "function" => "total"];
             $columns[] = ["title" => "Active Version", "function" => "active_version"];
@@ -894,7 +901,13 @@ HTML;
             }
         }
 
-        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns) {
+        $default_hidden_columns = ["active_graders"];
+        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns, $default_hidden_columns) {
+            foreach ($default_hidden_columns as $hidden) {
+                if (strcmp($column['function'], $hidden) === 0) {
+                    return array_key_exists($column['function'], $grading_details_columns) && $grading_details_columns['active_graders'];
+                }
+            }
             return !array_key_exists($column['function'], $grading_details_columns) || $grading_details_columns[$column['function']];
         });
 
@@ -929,6 +942,7 @@ HTML;
             "show_export_teams_button" => $show_export_teams_button,
             "past_grade_start_date" => $past_grade_start_date,
             "columns" => $shown_columns,
+            "default_hidden_columns" => $default_hidden_columns,
             "all_columns" => $columns,
             "export_teams_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'export']),
             "randomize_team_rotating_sections_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'randomize_rotating']),
@@ -953,6 +967,7 @@ HTML;
             "overrides" => $overrides,
             "anon_ids" => $anon_ids,
             "max_team_name_length" => Team::MAX_TEAM_NAME_LENGTH,
+            "active_graders" => $active_graders,
             "csrf_token" => $this->core->getCsrfToken(),
         ]);
     }

site/app/controllers/grading/ElectronicGraderController.php
@@ -1096,7 +1096,19 @@ class ElectronicGraderController extends AbstractController {
             }
         }
 
-        $this->core->getOutput()->renderOutput(['grading', 'ElectronicGrader'], 'detailsPage', $gradeable, $graded_gradeables, $teamless_users, $graders, $empty_teams, $show_all_sections_button, $show_import_teams_button, $show_export_teams_button, $show_edit_teams, $past_grade_start_date, $view_all, $sort, $direction, $anon_mode, $overrides, $anon_ids, $inquiry_status, $filter_withdrawn_student, $grading_details_columns);
+        $activeGradersData = $this->core->getQueries()->getActiveGradersForGradeable($gradeable_id);
+        $activeGraders = [];
+        if ($gradeable->isTeamAssignment()) {
+            $key = "ag_team_id";
+        }
+        else {
+            $key = "ag_user_id";
+        }
+        for ($i = 0; $i < count($activeGradersData); $i++) {
+            $activeGraders[$activeGradersData[$i][$key]][$activeGradersData[$i]['gc_id']][] = $activeGradersData[$i];
+        }
+
+        $this->core->getOutput()->renderOutput(['grading', 'ElectronicGrader'], 'detailsPage', $gradeable, $graded_gradeables, $teamless_users, $graders, $empty_teams, $show_all_sections_button, $show_import_teams_button, $show_export_teams_button, $show_edit_teams, $past_grade_start_date, $view_all, $sort, $direction, $anon_mode, $overrides, $anon_ids, $inquiry_status, $filter_withdrawn_student, $grading_details_columns, $activeGraders);
 
         if ($show_edit_teams) {
             $all_reg_sections = $this->core->getQueries()->getRegistrationSections();
@@ -2389,7 +2401,7 @@ class ElectronicGraderController extends AbstractController {
      * @param GradingAction $action
      * @return JsonResponse
      */
-    public function changeComponentGraders(string $gradeable_id, string $anon_id, string $component_id, GradingAction $action) {
+    public function changeComponentGraders(string $gradeable_id, string $anon_id, ?string $component_id = null, ?GradingAction $action = null) {
         $grader = $this->core->getUser();
 
         // Get the gradeable

site/app/libraries/database/DatabaseQueries.php
@@ -9574,4 +9574,24 @@ ORDER BY
         );
         return $this->submitty_db->getRowCount() > 0;
     }
+
+    /**
+     * Gets the active graders for a given gradeable.
+     * @return array<array{
+     *      gc_id: number,
+     *      gc_title: string,
+     *      grader_id: string,
+     *      ag_user_id: ?string,
+     *      ag_team_id: ?string,
+     * }>
+     */
+    public function getActiveGradersForGradeable(string $gradeable_id): array {
+        $this->course_db->query(
+            "SELECT ag.*, gc.gc_title FROM active_graders AS ag
+             JOIN gradeable_component AS gc ON ag.gc_id = gc.gc_id
+             WHERE gc.g_id = ?",
+            [$gradeable_id]
+        );
+        return $this->course_db->rows();
+    }
 }

site/globals.d.ts
@@ -1,3 +1,4 @@
+import * as luxon from 'luxon';
 import * as Twig from 'twig';
 export { };
 
@@ -6,5 +7,6 @@ declare global {
         csrfToken: string;
         $: JQueryStatic;
         Twig: typeof Twig;
+        luxon: typeof luxon;
     }
 }

site/public/css/server.css
@@ -1437,6 +1437,10 @@ end of styles used in the admin gradeable page
         1px 1px 0 var(--text-black);
 }
 
+.grader-active {
+    color: var(--standard-vibrant-red);
+}
+
 /* styles for the colors for the electronic grader details page */
 
 /* color for no grader (PEER) */

site/vue/src/components/ToggleColumns.vue
@@ -4,10 +4,11 @@ import { onMounted, ref } from 'vue';
 import Popup from './Popup.vue';
 export type ColumnFormats = 'bits' | 'json';
 
-const { columns, labels, cookie, forced = [], format = 'bits', buttonWrapped } = defineProps<{
+const { columns, labels, cookie, hiddenColumns = [], forced = [], format = 'bits', buttonWrapped } = defineProps<{
     columns: string[];
     labels: string[];
     cookie: string;
+    hiddenColumns?: string[];
     forced?: string[];
     format?: ColumnFormats;
     buttonWrapped?: boolean;
@@ -25,7 +26,7 @@ function loadColumns() {
             }
         }
         for (const col of columns) {
-            if (cookieData[col] === undefined) {
+            if (cookieData[col] === undefined && !hiddenColumns.includes(col)) {
                 selected.value[columns.indexOf(col)] = true;
             }
         }

commit fa9f1abc697b3f12c124b8eb5ea56582e72991ea

[Bugfix:TAGrading] Remove unused column (#11907)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
There was a regression in #11817 that added an unused column to the
grading details page.

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->
This PR removes the unused columns.

### What steps should a reviewer take to reproduce or test the bug or
new feature?
1. View the grading details page on main and see the unused "Active
Graders" column
2. View the PR and see that it is gone

### Automated Testing & Documentation
<!-- Is this feature sufficiently tested by unit tests and end-to-end
tests?
If this PR does not add/update the necessary automated tests, write a
new GitHub issue and link it below.
Is this feature sufficiently documented on submitty.org?
Link related PRs or new GitHub issue to update documentation. -->

### Other information
<!-- Is this a breaking change?  
Does this PR include migrations to update existing installations?  
Are there security concerns with this PR? -->

site/app/templates/grading/electronic/Details.twig
@@ -91,7 +91,6 @@
         'args': {
             'columns': all_columns|map(column => column.function),
             'labels': all_columns|map(column => column.title),
-            'hiddenColumns': default_hidden_columns,
             'forced': [
                 'grading'
             ],

site/app/views/grading/ElectronicGraderView.php
@@ -616,7 +616,6 @@ HTML;
             }
             else {
                 $columns[] = ["title" => "Grading", "function" => "grading"];
-                $columns[] = ["title" => "Active Graders", "function" => "active_graders"];
             }
             $columns[] = ["title" => "Total", "function" => "total"];
             $columns[] = ["title" => "Active Version", "function" => "active_version"];
@@ -902,13 +901,7 @@ HTML;
             }
         }
 
-        $default_hidden_columns = ["active_graders"];
-        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns, $default_hidden_columns) {
-            foreach ($default_hidden_columns as $hidden) {
-                if (strcmp($column['function'], $hidden) === 0) {
-                    return array_key_exists($column['function'], $grading_details_columns) && $grading_details_columns['active_graders'];
-                }
-            }
+        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns) {
             return !array_key_exists($column['function'], $grading_details_columns) || $grading_details_columns[$column['function']];
         });
 
@@ -944,7 +937,6 @@ HTML;
             "show_export_teams_button" => $show_export_teams_button,
             "past_grade_start_date" => $past_grade_start_date,
             "columns" => $shown_columns,
-            "default_hidden_columns" => $default_hidden_columns,
             "all_columns" => $columns,
             "export_teams_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'export']),
             "randomize_team_rotating_sections_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'randomize_rotating']),
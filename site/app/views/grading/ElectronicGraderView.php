fa9f1abc697b3f12c124b8eb5ea56582e72991ea
Alex Lavallee <alex@lavallee.one>
2025-07-25 18:18:29 -0400

    [Bugfix:TAGrading] Remove unused column (#11907)
    ### Why is this Change Important & Necessary?
    <!-- Include any GitHub issue that is fixed/closed using "Fixes
    #<number>" or "Closes #<number>" syntax.
    Alternately write "Partially addresses #<number>" or "Related to
    #<number>" as appropriate. -->
    There was a regression in #11817 that added an unused column to the
    grading details page.
    
    ### What is the New Behavior?
    <!-- Include before & after screenshots/videos if the user interface has
    changed. -->
    This PR removes the unused columns.
    
    ### What steps should a reviewer take to reproduce or test the bug or
    new feature?
    1. View the grading details page on main and see the unused "Active
    Graders" column
    2. View the PR and see that it is gone
    
    ### Automated Testing & Documentation
    <!-- Is this feature sufficiently tested by unit tests and end-to-end
    tests?
    If this PR does not add/update the necessary automated tests, write a
    new GitHub issue and link it below.
    Is this feature sufficiently documented on submitty.org?
    Link related PRs or new GitHub issue to update documentation. -->
    
    ### Other information
    <!-- Is this a breaking change?  
    Does this PR include migrations to update existing installations?  
    Are there security concerns with this PR? -->

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -616,7 +616,6 @@ HTML;
             }
             else {
                 $columns[] = ["title" => "Grading", "function" => "grading"];
-                $columns[] = ["title" => "Active Graders", "function" => "active_graders"];
             }
             $columns[] = ["title" => "Total", "function" => "total"];
             $columns[] = ["title" => "Active Version", "function" => "active_version"];
@@ -902,13 +901,7 @@ HTML;
             }
         }
 
-        $default_hidden_columns = ["active_graders"];
-        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns, $default_hidden_columns) {
-            foreach ($default_hidden_columns as $hidden) {
-                if (strcmp($column['function'], $hidden) === 0) {
-                    return array_key_exists($column['function'], $grading_details_columns) && $grading_details_columns['active_graders'];
-                }
-            }
+        $shown_columns = array_filter($columns, function ($column) use ($grading_details_columns) {
             return !array_key_exists($column['function'], $grading_details_columns) || $grading_details_columns[$column['function']];
         });
 
@@ -944,7 +937,6 @@ HTML;
             "show_export_teams_button" => $show_export_teams_button,
             "past_grade_start_date" => $past_grade_start_date,
             "columns" => $shown_columns,
-            "default_hidden_columns" => $default_hidden_columns,
             "all_columns" => $columns,
             "export_teams_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'export']),
             "randomize_team_rotating_sections_url" => $this->core->buildCourseUrl(['gradeable', $gradeable->getId(), 'grading', 'teams', 'randomize_rotating']),
