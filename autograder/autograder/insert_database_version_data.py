Submitty/Submitty

commit 3ab6491c97f0d5438767b739191d1b27498bf09a

[SYSADMIN ACTION][Dependency] Bump sqlalchemy from 1.4.29 to 2.0.41 in /.setup/pip (#11578)
Please see the release notes:
https://submitty.org/sysadmin/installation/version_notes/v25.07.01

Bumps [sqlalchemy](https://github.com/sqlalchemy/sqlalchemy) from 1.4.29
to 2.0.40.
In order to test this PR, you would need to run `submitty_install_bin`
as the dependencies are not updated when you run `submitty_install`
Related to https://github.com/Submitty/submitty.github.io/pull/692
<details>
<summary>Release notes</summary>
<p><em>Sourced from <a
href="https://github.com/sqlalchemy/sqlalchemy/releases">sqlalchemy's
releases</a>.</em></p>
<blockquote>
<h1>2.0.40</h1>
<p>Released: March 27, 2025</p>
<h2>orm</h2>
<ul>
<li>
<p><strong>[orm] [bug]</strong> Fixed regression which occurred as of
2.0.37 where the checked
<code>ArgumentError</code> that's raised when an inappropriate type or
object
is used inside of a <code>Mapped</code> annotation would raise
<code>TypeError</code>
with &quot;boolean value of this clause is not defined&quot; if the
object resolved
into a SQL expression in a boolean context, for programs where future
annotations mode was not enabled. This case is now handled explicitly
and
a new error message has also been tailored for this case. In addition,
as
there are at least half a dozen distinct error scenarios for
intepretation
of the <code>Mapped</code> construct, these scenarios have all been
unified
under a new subclass of <code>ArgumentError</code> called
<code>MappedAnnotationError</code>, to provide some continuity between
these
different scenarios, even though specific messaging remains
distinct.</p>
<p>References: <a
href="https://www.sqlalchemy.org/trac/ticket/12329">#12329</a></p>
</li>
<li>
<p><strong>[orm] [bug]</strong> Fixed regression in ORM Annotated
Declarative class interpretation caused
by <code>typing_extension==4.13.0</code> that introduced a different
implementation

autograder/autograder/insert_database_version_data.py
@@ -8,7 +8,7 @@ import json
 import os
 
 from submitty_utils import dateutils
-from sqlalchemy import create_engine, Table, MetaData, bindparam, select, func, insert, delete
+from sqlalchemy import create_engine, Table, MetaData, bindparam, select, func, insert, delete, update
 from . import grade_item
 
 
@@ -62,15 +62,17 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
 
     engine = create_engine(conn_string)
     db = engine.connect()
-    metadata = MetaData(bind=db)
-    autograding_metrics = Table('autograding_metrics', metadata, autoload=True)
+    metadata = MetaData()
+    autograding_metrics = Table('autograding_metrics', metadata, autoload_with=db)
     db.execute(
         delete(autograding_metrics)
         .where(autograding_metrics.c.user_id == bindparam('u_id'))
         .where(autograding_metrics.c.team_id == bindparam('t_id'))
         .where(autograding_metrics.c.g_id == bindparam('g_id'))
         .where(autograding_metrics.c.g_version == bindparam('g_v')),
-        u_id=user_id,  t_id=team_id, g_id=gradeable_id, g_v=version)
+        {'u_id': user_id, 't_id': team_id, 'g_id': gradeable_id, 'g_v': version}
+    )
+    db.commit()
 
     if len(testcases) != len(results['testcases']):
         print(f"ERROR!  mismatched # of testcases {len(testcases)} != {len(results['testcases'])}")
@@ -106,6 +108,7 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
                     hidden=testcases[i]["hidden"],
                 )
             )
+            db.commit()
 
     submission_time = results['submission_time']
 
@@ -119,7 +122,7 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
         non_hidden_non_ec += nonhidden_diff
         # hidden_non_ec += hidden_diff
 
-    data_table = Table('electronic_gradeable_data', metadata, autoload=True)
+    data_table = Table('electronic_gradeable_data', metadata, autoload_with=db)
 
     """
     The data row should have been inserted by PHP when the student uploads the submission, requiring
@@ -128,86 +131,94 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
     will need to handle the active version afterwards.
     """   # noqa: B018
     if is_team is True:
-        result = db.execute(select([func.count()]).select_from(data_table)
+        result = db.execute(select(func.count()).select_from(data_table)
                             .where(data_table.c.g_id == bindparam('g_id'))
                             .where(data_table.c.team_id == bindparam('team_id'))
                             .where(data_table.c.g_version == bindparam('g_version')),
-                            g_id=gradeable_id,  team_id=team_id, g_version=version)
+                            {'g_id': gradeable_id, 'team_id': team_id, 'g_version': version})
         row = result.fetchone()
         result.close()
-        query_type = data_table.insert()
+        query_type = insert(data_table)
         if row[0] > 0:
-            query_type = data_table\
-                .update(
-                    values={
-                        data_table.c.autograding_non_hidden_non_extra_credit:
-                            bindparam("autograding_non_hidden_non_extra_credit"),
-                        data_table.c.autograding_non_hidden_extra_credit:
-                            bindparam("autograding_non_hidden_extra_credit"),
-                        data_table.c.autograding_hidden_non_extra_credit:
-                            bindparam("autograding_hidden_non_extra_credit"),
-                        data_table.c.autograding_hidden_extra_credit:
-                            bindparam("autograding_hidden_extra_credit"),
-                        data_table.c.autograding_complete:
-                            bindparam("autograding_complete")
-                    })\
-                .where(data_table.c.g_id == bindparam('u_g_id'))\
-                .where(data_table.c.team_id == bindparam('u_team_id'))\
+            query_type = (
+                update(data_table)
+                .values(
+                    autograding_non_hidden_non_extra_credit=bindparam("autograding_non_hidden_non_extra_credit"),
+                    autograding_non_hidden_extra_credit=bindparam("autograding_non_hidden_extra_credit"),
+                    autograding_hidden_non_extra_credit=bindparam("autograding_hidden_non_extra_credit"),
+                    autograding_hidden_extra_credit=bindparam("autograding_hidden_extra_credit"),
+                    autograding_complete=bindparam("autograding_complete"),
+                    submission_time=bindparam("submission_time")
+                )
+                .where(data_table.c.g_id == bindparam('u_g_id'))
+                .where(data_table.c.team_id == bindparam('u_team_id'))
                 .where(data_table.c.g_version == bindparam('u_g_version'))
+            )
             # we bind "u_g_id" (and others) as we cannot use "g_id" in the where clause for an
             # update. Passing this as an argument to db.execute doesn't cause any issue when we
             # use the insert query (that doesn't have u_g_id)
-        db.execute(query_type,
-                   g_id=gradeable_id, u_g_id=gradeable_id,
-                   team_id=team_id, u_team_id=team_id,
-                   g_version=version, u_g_version=version,
-                   autograding_non_hidden_non_extra_credit=non_hidden_non_ec,
-                   autograding_non_hidden_extra_credit=non_hidden_ec,
-                   autograding_hidden_non_extra_credit=hidden_non_ec,
-                   autograding_hidden_extra_credit=hidden_ec,
-                   submission_time=submission_time,
-                   autograding_complete=True)
+        db.execute(
+            query_type,
+            {
+                "g_id": gradeable_id,
+                "u_g_id": gradeable_id,
+                "team_id": team_id,
+                "u_team_id": team_id,
+                "g_version": version,
+                "u_g_version": version,
+                "autograding_non_hidden_non_extra_credit": non_hidden_non_ec,
+                "autograding_non_hidden_extra_credit": non_hidden_ec,
+                "autograding_hidden_non_extra_credit": hidden_non_ec,
+                "autograding_hidden_extra_credit": hidden_ec,
+                "submission_time": submission_time,
+                "autograding_complete": True
+            }
+        )
 
     else:
-        result = db.execute(select([func.count()]).select_from(data_table)
-                            .where(data_table.c.g_id == bindparam('g_id'))
-                            .where(data_table.c.user_id == bindparam('user_id'))
-                            .where(data_table.c.g_version == bindparam('g_version')),
-                            g_id=gradeable_id, user_id=user_id, g_version=version)
-        row = result.fetchone()
+        result = db.execute(
+            select(func.count()).select_from(data_table)
+            .where(data_table.c.g_id == bindparam('g_id'))
+            .where(data_table.c.user_id == bindparam('user_id'))
+            .where(data_table.c.g_version == bindparam('g_version')),
+            {"g_id": gradeable_id, "user_id": user_id, "g_version": version}
+        )
+        count = result.scalar() or 0
         result.close()
-        query_type = data_table.insert()
-        if row[0] > 0:
-            query_type = data_table\
-                .update(
-                    values={
-                        data_table.c.autograding_non_hidden_non_extra_credit:
-                            bindparam("autograding_non_hidden_non_extra_credit"),
-                        data_table.c.autograding_non_hidden_extra_credit:
-                            bindparam("autograding_non_hidden_extra_credit"),
-                        data_table.c.autograding_hidden_non_extra_credit:
-                            bindparam("autograding_hidden_non_extra_credit"),
-                        data_table.c.autograding_hidden_extra_credit:
-                            bindparam("autograding_hidden_extra_credit"),
-                        data_table.c.autograding_complete:
-                            bindparam("autograding_complete")
-                    })\
+        query_type = insert(data_table)
+        if count > 0:
+            query_type = update(data_table)\
+                .values(
+                        autograding_non_hidden_non_extra_credit=bindparam("autograding_non_hidden_non_extra_credit"),
+                        autograding_non_hidden_extra_credit=bindparam("autograding_non_hidden_extra_credit"),
+                        autograding_hidden_non_extra_credit=bindparam("autograding_hidden_non_extra_credit"),
+                        autograding_hidden_extra_credit=bindparam("autograding_hidden_extra_credit"),
+                        autograding_complete=bindparam("autograding_complete")
+                )\
                 .where(data_table.c.g_id == bindparam('u_g_id'))\
                 .where(data_table.c.user_id == bindparam('u_user_id'))\
                 .where(data_table.c.g_version == bindparam('u_g_version'))
             # we bind "u_g_id" (and others) as we cannot use "g_id" in the where clause for an
             # update. Passing this as an argument to db.execute doesn't cause any issue when we
             # use the insert query (that doesn't have u_g_id)
-        db.execute(query_type,
-                   g_id=gradeable_id, u_g_id=gradeable_id,
-                   user_id=user_id, u_user_id=user_id,
-                   g_version=version, u_g_version=version,
-                   autograding_non_hidden_non_extra_credit=non_hidden_non_ec,
-                   autograding_non_hidden_extra_credit=non_hidden_ec,
-                   autograding_hidden_non_extra_credit=hidden_non_ec,
-                   autograding_hidden_extra_credit=hidden_ec,
-                   submission_time=submission_time,
-                   autograding_complete=True)
+        db.execute(
+            query_type,
+            {
+                "g_id": gradeable_id,
+                "u_g_id": gradeable_id,
+                "user_id": user_id,
+                "u_user_id": user_id,
+                "g_version": version,
+                "u_g_version": version,
+                "autograding_non_hidden_non_extra_credit": non_hidden_non_ec,
+                "autograding_non_hidden_extra_credit": non_hidden_ec,
+                "autograding_hidden_non_extra_credit": hidden_non_ec,
+                "autograding_hidden_extra_credit": hidden_ec,
+                "submission_time": submission_time,
+                "autograding_complete": True
+            }
+        )
+        db.commit()
     db.close()
     engine.dispose()
 

.setup/bin/populate_office_hours_queue.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from sqlalchemy import create_engine, Table, MetaData, select, update, insert
+from sqlalchemy import create_engine, Table, MetaData, select, update, insert, text
 import random
 import os
 from datetime import datetime, timedelta
@@ -45,14 +45,14 @@ def main():
     engine = create_engine(
         f"postgresql:///{database}?host={DB_HOST}&port={DB_PORT}&user={DB_USER}&password={DB_PASS}")
     conn = engine.connect()
-    metadata = MetaData(bind=engine)
-    queues_table = Table("queue_settings", metadata, autoload=True)
-    queue_entries_table = Table("queue", metadata, autoload=True)
-    users_table = Table("users", metadata, autoload=True)
+    metadata = MetaData()
+    queues_table = Table("queue_settings", metadata, autoload_with=engine)
+    queue_entries_table = Table("queue", metadata, autoload_with=engine)
+    users_table = Table("users", metadata, autoload_with=engine)
 
     # Find all queues in course
     res = conn.execute(select(queues_table))
-    queues_lookup = {x["code"]: x for x in res}
+    queues_lookup = {x["code"]: x for x in res.mappings()}
     res.close()
 
     # finds all open queues
@@ -64,7 +64,7 @@ def main():
 
     # Find all students in class
     res = conn.execute(select(users_table))
-    all_users = res.fetchall()
+    all_users = res.mappings().all()
     res.close()
 
     # Find all ids of students and graders
@@ -97,7 +97,7 @@ def main():
     # Otherwise, they will get either emptied or removed
     res = conn.execute(
         select(queue_entries_table).where(queue_entries_table.c.current_state != "done"))
-    tmp = res.fetchall()
+    tmp = res.mappings().all()
     res.close()
     for row in tmp:
         update_dict = dict()
@@ -116,6 +116,7 @@ def main():
             (queue_entries_table.c.user_id == row["user_id"]) &
             (queue_entries_table.c.time_in == row["time_in"]))
         conn.execute(update_query)
+    conn.commit()
 
     # Hardcoding options that we could use
     queue_current_states = ["done", "being_helped", "waiting"]
@@ -189,11 +190,11 @@ def main():
         else:
             queue_entry["contact_info"] = None
 
-        res = conn.execute(f"SELECT max(time_in) FROM queue WHERE user_id = \
+        res = conn.execute(text(f"SELECT max(time_in) FROM queue WHERE user_id = \
             '{queue_entry['user_id']}' AND UPPER(TRIM(queue_code)) = \
             UPPER(TRIM('{queue_entry['queue_code']}')) AND \
-            (removal_type IN ('helped', 'self_helped') OR help_started_by IS NOT NULL)")
-        queue_entry["last_time_in_queue"] = res.fetchall()[0][0]
+            (removal_type IN ('helped', 'self_helped') OR help_started_by IS NOT NULL)"))
+        queue_entry["last_time_in_queue"] = res.all()[0][0]
         res.close()
 
         if queue_entry["help_started_by"] is not None:
@@ -221,6 +222,7 @@ def main():
         queue_data.append(queue_entry)
 
     conn.execute(insert(queue_entries_table).values(queue_data))
+    conn.commit()
 
 
 if __name__ == "__main__":

.setup/bin/recreate_submissions.py
@@ -5,7 +5,7 @@ import json
 import os
 from pathlib import Path
 import shutil
-import sqlalchemy as sql
+from sqlalchemy import create_engine, Table, MetaData, select, insert, update
 import sys
 import typing
 
@@ -194,18 +194,16 @@ def parseArgs():
 
 def main():
     parseArgs()
-    dbengine_course = sql.create_engine(
+    dbengine_course = create_engine(
         f'postgresql:///submitty_{ARG_SEMESTER}_{ARG_COURSE}?host={ARG_DB_HOST}'
         f'&port={ARG_DB_PORT}&user={ARG_DB_USER}&password={ARG_DB_PASS}')
     dbconn_course = dbengine_course.connect()
-    metadata = sql.MetaData(dbengine_course)
-    table_users = sql.Table('users', metadata, autoload=True)
-    table_egdata = sql.Table('electronic_gradeable_data',
-                             metadata, autoload=True)
-    table_egver = sql.Table('electronic_gradeable_version',
-                            metadata, autoload=True)
-    ret = dbconn_course.execute(sql.select(table_users))
-    user_list = set(row['user_id'] for row in ret.all())
+    metadata = MetaData()
+    table_users = Table('users', metadata, autoload_with=dbconn_course)
+    table_egdata = Table('electronic_gradeable_data', metadata, autoload_with=dbconn_course)
+    table_egver = Table('electronic_gradeable_version', metadata, autoload_with=dbconn_course)
+    ret = dbconn_course.execute(select(table_users))
+    user_list = set(row['user_id'] for row in ret.mappings().all())
     ret.close()
 
     # function for query to electronic_gradeable_data table
@@ -218,7 +216,7 @@ def main():
             print(f'    g_version            = {g_version}')
             print(f'    submission_time      = {submission_time}')
             print(f'    autograding_complete = {autograding_complete}')
-        query = table_egdata.insert().values(
+        query = insert(table_egdata).values(
             g_id=g_id, user_id=user_id, g_version=g_version,
             submission_time=submission_time,
             autograding_complete=autograding_complete
@@ -226,6 +224,7 @@ def main():
         if ARG_VERBOSE:
             print(f'    SQL: {query}')
         dbconn_course.execute(query)
+        dbconn_course.commit()
 
     # function for query to electronic_gradeable_version table
     def update_egver(g_id: str, user_id: str, active_version: int):
@@ -234,7 +233,7 @@ def main():
             print(f'    g_id           = {g_id}')
             print(f'    user_id        = {user_id}')
             print(f'    active_version = {active_version}')
-        query = table_egver.select().where(
+        query = select(table_egver).where(
             (table_egver.c.g_id == g_id) &
             (table_egver.c.user_id == user_id)
         )
@@ -244,13 +243,13 @@ def main():
             print(f'    SQL: {query}')
             print(f'    egver_data = {rows}')
         if len(rows) == 0:  # INSERT
-            query = table_egver.insert().values(
+            query = insert(table_egver).values(
                 g_id=g_id,
                 user_id=user_id,
                 active_version=active_version
             )
         else:  # UPDATE
-            query = table_egver.update().values(
+            query = update(table_egver).values(
                 active_version=active_version
             ).where(
                 (table_egver.columns.g_id == g_id) &
@@ -259,6 +258,8 @@ def main():
         if ARG_VERBOSE:
             print(f'    SQL: {query}')
         dbconn_course.execute(query)
+        dbconn_course.commit()
+
     for user in os.listdir(ARG_SUBMISSIONS):
         if not Path(ARG_SUBMISSIONS, user).is_dir():
             continue

.setup/bin/reupload_old_assignments.py
@@ -4,7 +4,7 @@ import json
 import shutil
 import argparse
 from submitty_utils import dateutils
-from sqlalchemy import create_engine, Table, MetaData, and_
+from sqlalchemy import create_engine, Table, MetaData, and_, insert, update
 import grp
 
 # This file gets installed to SUBMITTY_INSTALL_DIR/.setup/bin/
@@ -79,10 +79,10 @@ def main():
     engine = create_engine("postgresql://{}:{}@{}/{}".format(DB_USER, DB_PASS, DB_HOST,
                                                            database))
     conn = engine.connect()
-    metadata = MetaData(bind=engine)
+    metadata = MetaData()
+    electronic_gradeable_data = Table("electronic_gradeable_data", metadata, autoload_with=engine)
+    electronic_gradeable_version = Table("electronic_gradeable_version", metadata, autoload_with=engine)
     print("(connection made, metadata bound)...")
-    electronic_gradeable_data = Table("electronic_gradeable_data", metadata, autoload=True)
-    electronic_gradeable_version = Table("electronic_gradeable_version", metadata, autoload=True)
 
     course_group = grp.getgrgid(os.stat(os.path.join(SUBMITTY_DATA_DIR,"courses",args.semester,args.course_name)).st_gid)[0]
 
@@ -138,20 +138,23 @@ def main():
             #add each submission to the database.
             current_time_string = dateutils.write_submitty_date()
 
-            conn.execute(electronic_gradeable_data.insert(), g_id=args.assignment_name, user_id=user_name,
-                         g_version=submission, submission_time=current_time_string)
+            conn.execute(insert(electronic_gradeable_data).values(g_id=args.assignment_name, user_id=user_name,
+                         g_version=submission, submission_time=current_time_string))
+            conn.commit()
             #If this is the first submission, create a new entry in the table, otherwise, update.
             #TODO use a more reliable method of determining if this is the first submission.
             if int(submission) == 1:
                 print("Entered new user " + user_name + " because submission was " + submission)
-                conn.execute(electronic_gradeable_version.insert(), g_id=args.assignment_name, user_id=user_name,
-                         active_version=user_assignment_settings['active_version'])
+                conn.execute(insert(electronic_gradeable_version).values(g_id=args.assignment_name, user_id=user_name,
+                         active_version=user_assignment_settings['active_version']))
+                conn.commit()
             else:
                 print("UPDATED: where g_id is " + args.assignment_name + " and user id is " + user_name + " to value " + str(user_assignment_settings['active_version']))
-                stmt = electronic_gradeable_version.update().\
+                stmt = update(electronic_gradeable_version).\
                         where(and_(electronic_gradeable_version.c.g_id==args.assignment_name, electronic_gradeable_version.c.user_id==user_name)).\
                         values(active_version=user_assignment_settings['active_version'])
                 conn.execute(stmt)
+                conn.commit()
             with open(os.path.join(CURRENT_user_path, "user_assignment_settings.json"), "w") as open_file:
                 json.dump(user_assignment_settings, open_file, indent = 4)
 

.setup/bin/sample_courses/models/__init__.py
@@ -11,6 +11,7 @@ from sample_courses.utils.create_or_generate import (
 )
 from sample_courses.utils.checks import user_exists
 from sample_courses.utils.dependent import add_to_group
+from sqlalchemy import insert
 
 
 class Mark(object):
@@ -22,13 +23,14 @@ class Mark(object):
         self.key = None
 
     def create(self, gc_id, conn, table) -> None:
-        ins = table.insert().values(
+        ins = insert(table).values(
             gc_id=gc_id,
             gcm_points=self.points,
             gcm_note=self.note,
             gcm_order=self.order,
         )
         res = conn.execute(ins)
+        conn.commit()
         self.key = res.inserted_primary_key[0]
 
 

.setup/bin/sample_courses/models/component.py
@@ -7,6 +7,7 @@ Contains function:
 from __future__ import print_function, division
 
 from sample_courses.models import generate_random_marks, Mark
+from sqlalchemy import insert
 
 
 class Component(object):
@@ -52,7 +53,7 @@ class Component(object):
         self.key = None
 
     def create(self, g_id, conn, table, mark_table) -> None:
-        ins = table.insert().values(
+        ins = insert(table).values(
             g_id=g_id,
             gc_title=self.title,
             gc_ta_comment=self.ta_comment,
@@ -67,6 +68,7 @@ class Component(object):
             gc_page=self.page,
         )
         res = conn.execute(ins)
+        conn.commit()
         self.key = res.inserted_primary_key[0]
 
         for mark in self.marks:

commit 4e69e0559d3d7030ebadeec956facde18aa49a6f

[Bugfix:Autograding] Fix team autograding (#11919)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
Regression came from #11578 
Current production is broken right now with team gradeables. This is
because the update portion of the team gradeable does not have a
`connection.commit()`, which the non-team gradeable has. Putting the
commit statement outside of both if and else if is going to fix the
issue.

Whenever you submit a team gradeable, you will see the `Something went
wrong will this submission` banner. When you take a closer look in the
`electronic_gradeable_data` table, the value of `completed` will always
be false. We can manually check the grades that we received in
`results/grades.txt` to see that we actually received a grade for the
assignment. As mentioned before, this is because the table is not being
updated when the job finishes, and thus the user recieves the error.

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->
Putting the commit statement outside of both if and else if is going to
fix the issue. This ensures that insertions and updates will both be
commited.
The autoload_with=engine vs db is a semantic difference as it doesnt
impact the overall issue. SQLAlchemy takes both conection and engine,
but as engine was more widely used and recommended in sqlalchemy, we
should use this instead.

### What steps should a reviewer take to reproduce or test the bug or
new feature?

### Automated Testing & Documentation
<!-- Is this feature sufficiently tested by unit tests and end-to-end
tests?
If this PR does not add/update the necessary automated tests, write a
new GitHub issue and link it below.
Is this feature sufficiently documented on submitty.org?

autograder/autograder/insert_database_version_data.py
@@ -63,7 +63,7 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
     engine = create_engine(conn_string)
     db = engine.connect()
     metadata = MetaData()
-    autograding_metrics = Table('autograding_metrics', metadata, autoload_with=db)
+    autograding_metrics = Table('autograding_metrics', metadata, autoload_with=engine)
     db.execute(
         delete(autograding_metrics)
         .where(autograding_metrics.c.user_id == bindparam('u_id'))
@@ -122,7 +122,7 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
         non_hidden_non_ec += nonhidden_diff
         # hidden_non_ec += hidden_diff
 
-    data_table = Table('electronic_gradeable_data', metadata, autoload_with=db)
+    data_table = Table('electronic_gradeable_data', metadata, autoload_with=engine)
 
     """
     The data row should have been inserted by PHP when the student uploads the submission, requiring
@@ -218,7 +218,8 @@ def insert_into_database(config, semester, course, gradeable_id, user_id, team_i
                 "autograding_complete": True
             }
         )
-        db.commit()
+
+    db.commit()
     db.close()
     engine.dispose()
 