a94fa931f6c5bb87c763a3ebb7f273a4c53547fe
williamschen23 <chenwill2005@gmail.com>
2025-07-29 17:41:38 -0400

    rust autograding

--- site/cypress/e2e/Cypress-Autograding-Development/electronic_gradeable_development.spec.js
+++ site/cypress/e2e/Cypress-Autograding-Development/electronic_gradeable_development.spec.js
@@ -7,25 +7,44 @@ const constructFileName = (gradeable, fileName) => {
 
 describe('Test the development course gradeables', { env: { course: 'development' } }, () => {
     it('Should test the cpp cats gradeable with full and buggy submissions', () => {
-        cy.login('instructor');
+        cy.login('aphacker');
 
         const cpp_cats = 'cpp_cats';
         const cpp_cats_full_score = [2, 3, 4, 4, 4, 4, 4];
 
         // Grab the current version, and then submit and check the gradeable
-        switchOrFindVersion(cpp_cats, null).then((startingVersion) => {
+        switchOrFindVersion(cpp_cats, null).then((cpp_cats_starting_version) => {
             newSubmission(cpp_cats);
             submitFiles(constructFileName(cpp_cats, 'allCorrect.zip'), 1, true);
-            submitGradeable(startingVersion + 1);
+            submitGradeable(cpp_cats_starting_version + 1);
 
             // submits the half incorrect files
             newSubmission(cpp_cats);
             submitFiles(constructFileName(cpp_cats, 'extraLinesAtEnd.zip'), 1, true);
-            submitGradeable(startingVersion + 2);
+            submitGradeable(cpp_cats_starting_version + 2);
 
-            // check both results
-            checkNonHiddenResults(cpp_cats, startingVersion + 1, cpp_cats_full_score, cpp_cats_full_score);
-            checkNonHiddenResults(cpp_cats, startingVersion + 2, [2, 3, 2, 2, 2, 2, 0], cpp_cats_full_score);
+            const rust = 'rust_hello_world';
+            const rust_full_score = [5, 5];
+
+            switchOrFindVersion(rust, null).then((rust_starting_version) => {
+                // submit the full score file
+                newSubmission(rust);
+                submitFiles(constructFileName(rust, 'correct.rs'), 1, true);
+                submitGradeable(rust_starting_version + 1);
+
+                // submit the buggy file
+                newSubmission(rust);
+                submitFiles(constructFileName(rust, 'incorrect.rs'), 1, true);
+                submitGradeable(rust_starting_version + 2);
+
+                // check both cpp cats results
+                checkNonHiddenResults(cpp_cats, cpp_cats_starting_version + 1, cpp_cats_full_score, cpp_cats_full_score);
+                checkNonHiddenResults(cpp_cats, cpp_cats_starting_version + 2, [2, 3, 2, 2, 2, 2, 0], cpp_cats_full_score);
+
+                // check both rust results
+                checkNonHiddenResults(rust, rust_starting_version + 1, rust_full_score, rust_full_score);
+                checkNonHiddenResults(rust, rust_starting_version + 2, [5, 0], rust_full_score);
+            });
         });
     });
 });
