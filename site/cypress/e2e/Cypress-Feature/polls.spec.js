703c2d6eb56c305ddc195447c3505d274d65617e
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-07-31 15:05:15 -0400

    Inverse Cypress logic

--- site/cypress/e2e/Cypress-Feature/polls.spec.js
+++ site/cypress/e2e/Cypress-Feature/polls.spec.js
@@ -9,11 +9,13 @@ import { verifyWebSocketStatus } from '../../support/utils';
 
 const visitPoll = (title, text) => {
     cy.contains(title).siblings(':nth-child(3)').contains(text).click();
-    return cy.window().then((win) => {
-        if (win.histogram) {
-            // WebSocket histogram object is only initialized for active polls when first loading the page
-            return cy.url().should('match', /\/polls\/\d+$/).then(() => verifyWebSocketStatus());
-        }
+    return cy.url().should('match', /\/polls\/\d+$/).then(() => {
+        return cy.window().then((win) => {
+            if (win.histogram) {
+                // WebSocket histogram object is only initialized for active polls when first loading the page
+                return verifyWebSocketStatus();
+            }
+        });
     });
 };
 
