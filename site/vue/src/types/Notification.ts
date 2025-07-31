f534a0b74382a07e370d7c71044315232ecd0955
Justin Manion <jmanion32@gmail.com>
2025-07-31 12:21:58 -0400

    Fix lint

--- site/vue/src/types/Notification.ts
+++ site/vue/src/types/Notification.ts
@@ -1,13 +1,13 @@
 export interface Notification {
-  id: number;
-  component: string;
-  metadata: string;
-  content: string;
-  seen: boolean;
-  elapsed_time: number;
-  created_at: string;
-  notify_time: string;
-  semester: string;
-  course: string;
-  notification_url: string;
+    id: number;
+    component: string;
+    metadata: string;
+    content: string;
+    seen: boolean;
+    elapsed_time: number;
+    created_at: string;
+    notify_time: string;
+    semester: string;
+    course: string;
+    notification_url: string;
 }
