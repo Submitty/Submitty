Submitty/Submitty

commit 85efe39ec6123fc1189b314b77eeefbcea5ca514

[UI/UX:Forum] Reformat forum bar (#11607)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [x] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
<!-- List issue if it fixes/closes/implements one using the "Fixes
#<number>" or "Closes #<number>" syntax -->

![image](https://github.com/user-attachments/assets/a10f7037-99f1-4f4e-81a5-5a6b051bd52e)
The old forum bar layout overlaps buttons when the window size shrinks.

### What is the new behavior?

![image](https://github.com/user-attachments/assets/b0bd33e2-223a-4e98-b813-afa797bc5449)
Rewrote the forum bar to use native flexbox instead of Bootstrap's grid
system. Buttons don't overlap now.
Fixes #11592

### Other information?
<!-- Is this a breaking change? -->
<!-- How did you test -->
Tested by increasing and decreasing window size and adding filters on
the main forums page
Tested on Chrome

---------

Co-authored-by: Justin Manion <jmanion32@gmail.com>
Co-authored-by: williamschen23 <chenwill2005@gmail.com>

site/app/templates/forum/ForumBar.twig
@@ -12,23 +12,17 @@
 	</script>
 {% endif %}
 
-<div class="forum_bar">
-
-	<div class="row">
-
-	<div class="col-8">
-
-		{% if thread_exists %}
-
+<div class="forum-bar">
+	<div class="toolbar">
+		<div class="toolbar-left">
+			{% if thread_exists %}
 			<form id="search_block" method="post" action="{{ search_url }}">
 				<input type='hidden' name="csrf_token" value="{{ csrf_token }}" />
-			<div class="row">
-				<div class="col-6">
-					<input type="text" placeholder="Search here..." name="search_content" id="search_content" onchange="$(this).val($(this).val().trim())" required aria-label="Forum Search Input Box" />
-				</div>
-				<div class="col-5">
+
+				<input type="text" placeholder="Search here..." name="search-content" id="search-content" onchange="$(this).val($(this).val().trim())" required aria-label="Forum Search Input Box" />
+				<div class="search-actions">
 					<button type="submit" name="search" title="Submit search" class="btn btn-primary">
-		  				Search
+						Search
 					</button>
 
 					{% for button in forum_bar_buttons_left %}
@@ -36,92 +30,84 @@
 							{% set link = (button.link[0]) ? 'href=' ~ button.link[1] %}
 							{% set onclick = (button.onclick[0]) ? 'onclick=' ~ button.onclick[1] %}
 							{% if link | length > 0 %}
-								<a class="btn btn-primary {{ button.optional_class }}" style="{{ button.style }}" {{ link }} title="{{ button.title }}" {{ onclick }} role="button" >
+								<a class="btn btn-primary {{ button.optional_class }}" {{ link }} title="{{ button.title }}" {{ onclick }} role="button" >
 									{{ button.display_text|raw }}
 								</a>
 							{% else %}
-								<button type="button" class="btn btn-primary {{ button.optional_class }}" style="{{ button.style }}" title="{{ button.title }}" {{ onclick }} >
+								<button type="button" class="btn btn-primary {{ button.optional_class }}" title="{{ button.title }}" {{ onclick }} >
 									{{ button.display_text|raw }}
 								</button>
 							{% endif %}
-
 						{% endif %}
 					{% endfor %}
-
-					<a id="clear_filter_button" class="inline-block text-decoration-none key_to_click" tabindex="0" style="color: var(--actionable-blue); min-width: 120px; "title="Clear Filters" onclick="clearForumFilter(event);">&times; Clear Filters</a>
-
+					<a id="clear_filter_button" class="text-decoration-none key_to_click clear-filter-button" tabindex="0" title="Clear Filters" onclick="clearForumFilter(event);">&times; Clear Filters</a>
 				</div>
-
-			</div>
-
 			</form>
-
-		{% endif %}
-
+			{% endif %}
 		</div>
-
-	<div class="col-4">
-
-	{% if show_more is defined and show_more %}
-	    <div data-testid="more-dropdown" class="dropdown more-dropdown">
-			<div class="btn-group">
-				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					More
-				</button>
-				<div class="dropdown-menu dropdown-menu-right">
-					{% if activeThread != null %}
-	                	<a class="dropdown-item key_to_click" tabindex="0" onClick="markThreadUnread({{activeThread.id}})" title="Mark Thread Unread" aria-label="Mark Thread Unread">Unread Thread</a>
-						<div class="dropdown-divider"></div>
-					{% endif %}
-					{% for more in more_data|slice(1) %}
-						{% if user_group <= more.required_rank %}
-							{% set onclick = (more.onclick[0]) ? 'onclick=' ~ more.onclick[1] %}
-							{% set optional_class = (more.optional_class[0]) ? 'class=' ~ more.optional_class[1] %}
-								<a id="{{ more.id }}" data-testid="{{ more.id }}" class="dropdown-item" title="{{ more.title }}" {{ optional_class }} href="{{ more.link }}" {{ onclick }}>{{ more.display_text }}</a>
+		<div class="toolbar-right">
+			{% if show_more is defined and show_more %}
+			<div data-testid="more-dropdown" class="dropdown more-dropdown">
+				<div class="btn-group">
+					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						More
+					</button>
+					<div class="dropdown-menu dropdown-menu-right">
+						{% if activeThread != null %}
+							<a class="dropdown-item key_to_click" tabindex="0" onClick="markThreadUnread({{activeThread.id}})" title="Mark Thread Unread" aria-label="Mark Thread Unread">Unread Thread</a>
+							<div class="dropdown-divider"></div>
 						{% endif %}
-					{% endfor %}
-					{% if core.getUser().accessGrading() %}
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="{{ manage_categories_url }}" title="Edit Forum Categories" >Edit Categories</a>
-					{% endif %}
-					{% if thread_exists and (is_full_threads_page is not defined or not is_full_threads_page) %}
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item key_to_click" href="#" id="toggle-attachments-button" tabindex="0" title="Click to toggle all post attachments" onclick="loadAllInlineImages()">
-							<span class="status"></span>
-							<span class="attachment-badge badge">{{ total_attachments }}</span>
-						</a>
-						<div class="dropdown-divider"></div>
-						<a class="key_to_click dropdown-item"  id="tree" title="Sort posts by reply hierarchy" onclick="changeDisplayOptions('tree', {{ current_thread }})" href="#">Hierarchical Post Order</a>
-						<a href="#" id="time" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by ascending chronological order" onclick="changeDisplayOptions('time', {{ current_thread }})">Chronological Post Order  <i class="fas fa-angle-up"></i> </a>
-						<a href="#" id="reverse-time" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by descending chronological order" onclick="changeDisplayOptions('reverse-time', {{ current_thread }})">Chronological Post Order  <i class="fas fa-angle-down"></i> </a>
-						{% if user_group <= core.getUser().accessGrading() %}
-							<a href="#" id="alpha" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's last name" onclick="changeDisplayOptions('alpha')">Alphabetical Post Order</a>
-							<a href="#" id="alpha_by_registration" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's registration section then last name" onclick="changeDisplayOptions('alpha_by_registration')">Alpha by Registration Post Order</a>
-							<a href="#" id="alpha_by_rotating" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's rotating section then last name" onclick="changeDisplayOptions('alpha_by_rotating')">Alpha by Rotating Post Order</a>
+						{% for more in more_data|slice(1) %}
+							{% if user_group <= more.required_rank %}
+								{% set onclick = (more.onclick[0]) ? 'onclick=' ~ more.onclick[1] %}
+								{% set optional_class = (more.optional_class[0]) ? 'class=' ~ more.optional_class[1] %}
+									<a id="{{ more.id }}" data-testid="{{ more.id }}" class="dropdown-item" title="{{ more.title }}" {{ optional_class }} href="{{ more.link }}" {{ onclick }}>{{ more.display_text }}</a>
+							{% endif %}
+						{% endfor %}
+						{% if core.getUser().accessGrading() %}
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="{{ manage_categories_url }}" title="Edit Forum Categories" >Edit Categories</a>
 						{% endif %}
-					{% endif %}
+						{% if thread_exists and (is_full_threads_page is not defined or not is_full_threads_page) %}
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item key_to_click" href="#" id="toggle-attachments-button" tabindex="0" title="Click to toggle all post attachments" onclick="loadAllInlineImages()">
+								<span class="status"></span>
+								<span class="attachment-badge badge">{{ total_attachments }}</span>
+							</a>
+							<div class="dropdown-divider"></div>
+							<a class="key_to_click dropdown-item"  id="tree" title="Sort posts by reply hierarchy" onclick="changeDisplayOptions('tree', {{ current_thread }})" href="#">Hierarchical Post Order</a>
+							<a href="#" id="time" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by ascending chronological order" onclick="changeDisplayOptions('time', {{ current_thread }})">Chronological Post Order  <i class="fas fa-angle-up"></i> </a>
+							<a href="#" id="reverse-time" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by descending chronological order" onclick="changeDisplayOptions('reverse-time', {{ current_thread }})">Chronological Post Order  <i class="fas fa-angle-down"></i> </a>
+							{% if user_group <= core.getUser().accessGrading() %}
+								<a href="#" id="alpha" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's last name" onclick="changeDisplayOptions('alpha')">Alphabetical Post Order</a>
+								<a href="#" id="alpha_by_registration" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's registration section then last name" onclick="changeDisplayOptions('alpha_by_registration')">Alpha by Registration Post Order</a>
+								<a href="#" id="alpha_by_rotating" class="key_to_click dropdown-item" tabindex="0" title="Sort posts by author's rotating section then last name" onclick="changeDisplayOptions('alpha_by_rotating')">Alpha by Rotating Post Order</a>
+							{% endif %}
+						{% endif %}
+					</div>
 				</div>
 			</div>
-	    </div>
-	{% endif %}
-
-	{% for button in forum_bar_buttons_right %}
-			{% if user_group <= button.required_rank %}
-				{% set link = (button.link[0]) ? 'href=' ~ button.link[1] %}
-				{% set onclick = (button.onclick[0]) ? 'onclick=' ~ button.onclick[1] %}
-				{% if link | length > 0 %}
-						<a role="button" class="btn btn-primary {{ button.optional_class }}" style="{{ button.style }}" {{ link }} data-testid="{{ button.title }}" title="{{ button.title }}" {{ onclick }} >
-							{{ button.display_text | raw }}
-						</a>
-				{% else %}
-						<button type="button" class="btn btn-primary {{ button.optional_class }}" style="{{ button.style }}" data-testid="{{ button.title }}" title="{{ button.title }}" {{ onclick }} >
-							{{ button.display_text | raw }}
-						</button>
-				{% endif %}
 			{% endif %}
-		{% endfor %}
-
+		</div>
+		<div>
+			{% for button in forum_bar_buttons_right %}
+				{% if user_group <= button.required_rank %}
+					{% set link = (button.link[0]) ? 'href=' ~ button.link[1] %}
+					{% set onclick = (button.onclick[0]) ? 'onclick=' ~ button.onclick[1] %}
+					{% if link | length > 0 %}
+							<a role="button" class="btn btn-primary {{ button.optional_class }} toolbar-right-btn" {{ link }} data-testid="{{ button.title }}" title="{{ button.title }}" {{ onclick }} >
+								{{ button.display_text | raw }}
+							</a>
+					{% else %}
+							<button type="button" class="btn btn-primary {{ button.optional_class }} toolbar-right-btn" data-testid="{{ button.title }}" title="{{ button.title }}" {{ onclick }} >
+								{{ button.display_text | raw }}
+							</button>
+					{% endif %}
+				{% endif %}
+			{% endfor %}
+		</div>
 	</div>
+
 	{% if thread_exists %}
 		<style>
 			{% for category in categories %}
@@ -161,7 +147,6 @@
 		</div>
 	{% endif %}
 </div>
-</div>
 <hr/>
 {% if thread_exists and show_filter %}
 <script>
@@ -197,7 +182,7 @@
 
 		if(filters_applied.length > 0 || unread_marked ){
 			$('#forum_filter_bar').css('display', 'inline-block');
-			$('#clear_filter_button').css('display', 'inline-block');
+			$('#clear_filter_button').css('visibility', 'visible');
 		}
 
 		{% if display_option is defined and display_option in ['tree', 'time', 'alpha'] %}

site/app/templates/forum/ShowForumThreads.twig
@@ -34,7 +34,7 @@
 } %}
 
 <div class="full-height content forum_content forum_show_threads">
-
+    <div>
     {% if not thread_exists  %}
         {%  include "forum/ForumBar.twig" with {
                 "current_thread" : button_params.current_thread,
@@ -83,6 +83,7 @@
             "show_filter": true,
             "activeThread" : generate_post_content.activeThread,
         } %}
+        </div>
 
         <div id="thread-container" class="row">
 

site/public/css/forum.css
@@ -19,12 +19,73 @@
     margin-bottom: 10px;
 }
 
-/* stylelint-disable-next-line selector-class-pattern */
-.forum_bar {
+.forum-bar {
     margin-right: 10px;
     padding-left: 15px;
     margin-top: 10px;
     min-height: 30px;
+}
+
+.forum-bar .toolbar {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+.forum-bar .toolbar-right {
+    flex: 0 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+    margin-left: auto;
+    gap: 0.5rem;
+}
+
+.forum-bar .toolbar-left {
+    flex: 0 1 28rem;
+    min-width: 12rem;
+    display: flex;
+    align-items: stretch;
+    flex-grow: 0.35;
+}
+
+.forum-bar .toolbar-left > form {
+    width: 100%;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: stretch;
+    gap: 0.5rem;
+}
+
+.forum-bar .toolbar-left #search-content {
+    flex: 1 1 auto;
+    height: 100%;
+    width: 100%;
+    min-width: 30px;
+}
+
+.forum-bar .toolbar-left .search-actions {
+    flex: 0 0 auto;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+.clear-filter-button {
+    flex: 0 1 auto;
+    flex-wrap: wrap;
+    min-width: 100px;
+    visibility: hidden;
+    margin-left: 0.5rem;
+    color: var(--actionable-blue);
+}
+
+/* stylelint-disable-next-line selector-id-pattern */
+#clear_filter_button,
+#unread {
+    visibility: hidden;
 }
 
 /* stylelint-disable-next-line selector-id-pattern */
@@ -433,43 +494,7 @@
 #unread {
     margin-right: 15px;
     display: inline-block;
-}
-
-/* stylelint-disable selector-class-pattern, selector-id-pattern */
-.forum_bar div div.col-6,
-#search_block div div,
-.forum_bar div div.col-5 {
-    padding: 0;
-    top: 0;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    flex-wrap: nowrap;
-    width: auto;
-}
-/* stylelint-enable selector-class-pattern, selector-id-pattern */
-
-/* stylelint-disable-next-line selector-id-pattern */
-#search_block div div.col-6 {
-    margin-right: 5px;
-}
-
-/* stylelint-disable-next-line selector-id-pattern */
-#search_block div div.col-6 input {
-    width: 100%;
-    height: 34px;
-}
-
-/* stylelint-disable-next-line selector-id-pattern, no-descending-specificity */
-#search_block div {
-    position: relative;
-    top: 3px;
-}
-
-.more-dropdown {
-    position: absolute;
-    right: 118px;
-    top: 3px;
+    display: none;
 }
 
 #post-hr {
@@ -691,7 +716,6 @@
 }
 /* stylelint-disable-next-line selector-id-pattern */
 #forum_filter_bar {
-    margin-left: 15px;
     margin-top: 10px;
     font-size: small;
     display: none;
@@ -723,10 +747,6 @@
     color: var(--submitty-logo-blue);
 }
 /* stylelint-disable-next-line selector-id-pattern */
-#clear_filter_button,
-#unread {
-    display: none;
-}
 
 .filter-active,
 .filter-active:hover {

site/public/js/forum.js
@@ -2225,7 +2225,7 @@ function clearForumFilter() {
     window.filters_applied = [];
     $('#thread_category button, #thread_status_select button').data('btn-selected', 'false').removeClass('filter-active').addClass('filter-inactive');
     $('#filter_unread_btn').removeClass('filter-active').addClass('filter-inactive');
-    $('#clear_filter_button').hide();
+    $('#clear_filter_button').css('visibility', 'hidden');
 
     // eslint-disable-next-line no-undef
     updateThreads(true, null);
@@ -2258,7 +2258,7 @@ function loadFilterHandlers() {
             clearForumFilter();
         }
         else {
-            $('#clear_filter_button').css('display', 'inline-block');
+            $('#clear_filter_button').css('visibility', 'visible');
         }
         // eslint-disable-next-line no-undef
         updateThreads(true, null);
@@ -2352,7 +2352,7 @@ function checkUnread() {
         // eslint-disable-next-line no-undef
         unread_marked = true;
         $('#filter_unread_btn').removeClass('filter-inactive').addClass('filter-active');
-        $('#clear_filter_button').css('display', 'inline-block');
+        $('#clear_filter_button').css('visibility', 'visible');
         return true;
     }
     else {

commit 95c7818447e95c8f4b2a74f0e7e57c38980f3cf8

[Bugfix:Forum] Fix and test search (#12057)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
Fixes #12017 
bug introduced in #11607

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->

Forum search is fixed and a basic test is added to detect & prevent
future breakages.

### What steps should a reviewer take to reproduce or test the bug or
new feature?

Attempt to search on Main -> see no results, undefined array key error.
Attempt to search on this branch -> see results.

### Automated Testing & Documentation
<!-- Is this feature sufficiently tested by unit tests and end-to-end
tests?
If this PR does not add/update the necessary automated tests, write a
new GitHub issue and link it below.
Is this feature sufficiently documented on submitty.org?
Link related PRs or new GitHub issue to update documentation. -->

Adds a basic cypress test for forum searching.

### Other information
<!-- Is this a breaking change?  
Does this PR include migrations to update existing installations?  
Are there security concerns with this PR? -->

---------

Co-authored-by: Alex Lavallee <alex@lavallee.one>
Co-authored-by: williamschen23 <chenwill2005@gmail.com>

site/app/controllers/forum/ForumController.php
@@ -494,7 +494,7 @@ class ForumController extends AbstractController {
 
     #[Route("/courses/{_semester}/{_course}/forum/search", methods: ["POST"])]
     public function search() {
-        $results = $this->core->getQueries()->searchThreads($_POST['search_content']);
+        $results = $this->core->getQueries()->searchThreads($_POST['search-content']);
         $this->core->getOutput()->renderOutput('forum\ForumThread', 'searchResult', $results);
     }
 

site/app/templates/forum/ForumBar.twig
@@ -19,9 +19,9 @@
 			<form id="search_block" method="post" action="{{ search_url }}">
 				<input type='hidden' name="csrf_token" value="{{ csrf_token }}" />
 
-				<input type="text" placeholder="Search here..." name="search-content" id="search-content" onchange="$(this).val($(this).val().trim())" required aria-label="Forum Search Input Box" />
+				<input type="text" placeholder="Search here..." name="search-content" id="search-content" data-testid="search-content-input" onchange="$(this).val($(this).val().trim())" required aria-label="Forum Search Input Box" />
 				<div class="search-actions">
-					<button type="submit" name="search" title="Submit search" class="btn btn-primary">
+					<button type="submit" name="search" title="Submit search" class="btn btn-primary" data-testid="search-submit">
 						Search
 					</button>
 

site/app/templates/forum/searchResults.twig
@@ -19,7 +19,7 @@
                 <td style="width:10%">Timestamp</td>
             </tr>
             </thead>
-            <tbody>
+            <tbody data-testid="search-result-table">
                 {% for thread in threads %}
                     <tr class="info hoverable cursor-pointer key_to_click" tabindex="0" title="Go to thread" onclick="window.location = '{{ thread.thread_link }}';">
                         <td colspan="10" class="text-center"><h4>{{ thread.thread_title }}</h4></td>

site/cypress/e2e/Cypress-Feature/forums.spec.js
@@ -580,3 +580,19 @@ describe('Should test WebSocket functionality', () => {
         });
     });
 });
+
+describe('Should test Search functionality', () => {
+    beforeEach(() => {
+        cy.login('instructor');
+        cy.visit(['sample', 'forum']);
+    });
+    it('Should find posts containing \'Homework 1\'', () => {
+        cy.get('[data-testid="search-content-input"]').type('Homework 1');
+        cy.get('[data-testid="search-submit"]').click();
+        cy.get('[data-testid="search-result-table"]').should('exist');
+        cy.get('[data-testid="search-result-table"]').should('not.be.empty');
+        cy.get('[data-testid="search-result-table"]').contains('Homework 1 not running');
+        cy.get('[data-testid="search-result-table"]').contains('Homework 1 print clarification');
+        cy.get('[data-testid="search-result-table"]').contains('Homework 1 has been posted on the course website.');
+    });
+});