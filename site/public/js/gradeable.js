f7fa8c18e1f28c5f139b63822b2e9bb2d6930eab
Rkoester47 <koestr@rpi.edu>
2025-10-27 21:39:18 -0400

    Another Lint Fix

--- site/public/js/gradeable.js
+++ site/public/js/gradeable.js
@@ -24,7 +24,7 @@ function loadTemplates() {
         return Promise.resolve();
     }
     loadedTemplates = true;
-    
+
     const templates = [
         { id: 'GradingGradeable', href: '/templates/grading/GradingGradeable.twig' },
         { id: 'PeerGradeable', href: '/templates/grading/PeerGradeable.twig' },
