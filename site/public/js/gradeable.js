d3f97b448a886c8dfe0eb06ee4e7ae3bf39d3578
Rkoester47 <koestr@rpi.edu>
2025-11-17 15:19:25 -0500

    Reverted hardcoded cache busting

--- site/public/js/gradeable.js
+++ site/public/js/gradeable.js
@@ -9,7 +9,6 @@
  */
 const DECIMAL_PRECISION = 3;
 
-const TEMPLATE_VERSION = '20251024';
 // eslint-disable-next-line no-var
 var itempool_items = {};
 
@@ -30,12 +29,12 @@ function loadTemplates() {
         { id: 'PeerGradeable', href: '/templates/grading/PeerGradeable.twig' },
         { id: 'EditGradeable', href: '/templates/grading/EditGradeable.twig' },
         { id: 'Gradeable', href: '/templates/grading/Gradeable.twig' },
-        { id: 'GradingComponent', href: `/templates/grading/GradingComponent.twig?v=${TEMPLATE_VERSION}` },
-        { id: 'GradingComponentHeader', href: `/templates/grading/GradingComponentHeader.twig?v=${TEMPLATE_VERSION}` },
+        { id: 'GradingComponent', href: '/templates/grading/GradingComponent.twig' },
+        { id: 'GradingComponentHeader', href: '/templates/grading/GradingComponentHeader.twig' },
         { id: 'EditComponent', href: '/templates/grading/EditComponent.twig' },
         { id: 'EditComponentHeader', href: '/templates/grading/EditComponentHeader.twig' },
         { id: 'Component', href: '/templates/grading/Component.twig' },
-        { id: 'Mark', href: `/templates/grading/Mark.twig?v=${TEMPLATE_VERSION}` },
+        { id: 'Mark', href: '/templates/grading/Mark.twig' },
         { id: 'OverallComment', href: '/templates/grading/OverallComment.twig' },
         { id: 'TotalScoreBox', href: '/templates/grading/TotalScoreBox.twig' },
         { id: 'TotalPeerScoreBox', href: '/templates/grading/TotalPeerScoreBox.twig' },
