Submitty/Submitty

commit b5f393446c61acb09c08b8033878011b973554c9

[Feature:InstructorUI] Adjust Archived Courses Home Page View (#12154)
### Why is this Change Important & Necessary?
<!-- Include any GitHub issue that is fixed/closed using "Fixes
#<number>" or "Closes #<number>" syntax.
Alternately write "Partially addresses #<number>" or "Related to
#<number>" as appropriate. -->
Closes #12015.
As the linked issue describes, it is possible that an instructor has
been in several now-archived courses and wishes to collapse the view of
those courses on the homepage to reduce clutter. This pull request adds
the ability to toggle the visibility of archived courses, and also
spaces the archived courses out visually on the homepage in accordance
to the semester for which that course corresponds.

### What is the New Behavior?
<!-- Include before & after screenshots/videos if the user interface has
changed. -->
View of homepage as user `instructor` prior to change:
<img width="898" height="846" alt="image"
src="https://github.com/user-attachments/assets/7fe0bc2a-4a51-483c-8400-c4d6e7e263ac"
/>


View of homepage as user `instructor` after change (visible then
hidden):
<img width="891" height="930" alt="image"
src="https://github.com/user-attachments/assets/a3c22552-d338-4d51-8540-c66559ab6e3e"
/>
<img width="904" height="621" alt="image"
src="https://github.com/user-attachments/assets/7aaf63cc-afb1-44e3-bc2a-2e6ef7635207"
/>


### What steps should a reviewer take to reproduce or test the bug or
new feature?
Create new terms and courses as described in [this
documentation](https://submitty.org/sysadmin/configuration/course_creation).
Set those courses to be archived (also as described in [this
documentation](https://submitty.org/sysadmin/configuration/course_archiving)).
Log in as `instructor` (which is the user that should have been assigned
as the instructor for each of the new archived courses); view the
homepage. Checkout changes on both `main` and
`course-list-view-archived` to view the differences.

site/vue/src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, ref, computed, onMounted } from 'vue';
 import { buildUrl } from '../../../ts/utils/server';
 import AllNotificationsDisplay from '@/components/AllNotificationsDisplay.vue';
 import type { Notification } from '@/types/Notification';
@@ -20,13 +20,35 @@ type Course = {
 interface Props {
     statuses: { [key in Status]: { [key: string]: Rank } };
     notifications: Notification[];
+    userId: string;
 }
 
 type SemesterCourses = {
     [semester: string]: Course[];
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+const archivedCoursesVisible = ref(true);
+const hasArchivedCourses = computed(() => {
+    const archivedRanks = props.statuses.archived_courses;
+
+    return archivedRanks && Object.keys(archivedRanks).length > 0
+        && Object.values(archivedRanks).some((rank: Rank) => rank.courses && rank.courses.length > 0);
+});
+const toggleArchivedCourses = () => {
+    archivedCoursesVisible.value = !archivedCoursesVisible.value;
+    // Toggle archived course visibility indefinitely, scoped to user
+    localStorage.setItem(`archived_courses_visible_${props.userId}`, archivedCoursesVisible.value.toString());
+};
+
+onMounted(() => {
+    const storedValue = localStorage.getItem(`archived_courses_visible_${props.userId}`);
+
+    // Default to visible if no value is stored
+    if (storedValue !== null) {
+        archivedCoursesVisible.value = storedValue === 'true';
+    }
+});
 
 const getCourseTypeHeader = (course_type: Status) => {
     if (course_type === 'self_registration_courses') {
@@ -83,10 +105,19 @@ const buildCourseUrl = (course: Course) => {
             data-testid="courses-header"
           >
             {{ getCourseTypeHeader(course_type) }}
+            <button
+              v-if="course_type === 'archived_courses' && hasArchivedCourses"
+              type="button"
+              class="btn btn-default"
+              @click="toggleArchivedCourses"
+            >
+              {{ archivedCoursesVisible ? 'Hide' : 'Show' }}
+            </button>
           </h1>
 
           <div
             v-for="rank in ranks"
+            v-show="course_type !== 'archived_courses' || archivedCoursesVisible"
             :key="rank.title"
           >
             <h3 v-if="course_type !== 'dropped_courses' && course_type !== 'self_registration_courses'">
@@ -119,6 +150,7 @@ const buildCourseUrl = (course: Course) => {
                   </a>
                 </li>
               </ul>
+              <br v-if="course_type === 'archived_courses'" />
             </div>
           </div>
         </div>
@@ -153,7 +185,11 @@ const buildCourseUrl = (course: Course) => {
 
 .courses-header {
     margin-bottom: 5px !important; /* Override submitty-vue.css */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
+
 .div1 {
   grid-column: 1;
   padding: 20px;

site/app/views/HomePageView.php
@@ -64,6 +64,7 @@ class HomePageView extends AbstractView {
             "args" => [
                 "statuses" => $statuses,
                 "notifications" => $notifications,
+                "userId" => $user->getId(),
             ]
         ]);
     }

commit d7f9077f12279d5e14d7cf7b4af847b9b21c1536

[Bugfix:Developer] Fix Registration Test (#12215)
### Why is this Change Important & Necessary?
CI is failing due to `registration.spec.js`. This was caused in #12154
from nesting a `<button>` inside of an `<h1>` element, so when Cypress
went to assert that the header said "My Courses", it returned "My
Courses &nbsp;" instead, causing the test to fail.

### What is the New Behavior?
Take the `<button>` out of the `<h1>`, and adjust the CSS to remain the
same.

---------

Co-authored-by: Justin Manion <jmanion32@gmail.com>

site/vue/src/pages/HomePage.vue
@@ -100,22 +100,24 @@ const buildCourseUrl = (course: Course) => {
         <div v-if="index === 0 || (ranks && Object.keys(ranks).length > 0)">
           <br v-if="index > 0" />
           <br v-if="index > 0" />
-
-          <h1
-            class="courses-header"
-            data-testid="courses-header"
+          <div
+            class="courses-header-container"
           >
-            {{ getCourseTypeHeader(course_type) }}
+            <h1
+              class="courses-header"
+              data-testid="courses-header"
+            >
+              {{ getCourseTypeHeader(course_type) }}
+            </h1>
             <button
               v-if="course_type === 'archived_courses' && hasArchivedCourses"
               type="button"
-              class="btn btn-default"
+              class="btn btn-default archive-toggle-btn"
               @click="toggleArchivedCourses"
             >
               {{ archivedCoursesVisible ? 'Hide' : 'Show' }}
             </button>
-          </h1>
-
+          </div>
           <div
             v-for="rank in ranks"
             v-show="course_type !== 'archived_courses' || archivedCoursesVisible"
@@ -192,11 +194,19 @@ const buildCourseUrl = (course: Course) => {
     }
 }
 
+.courses-header-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
 .courses-header {
     margin-bottom: 5px !important; /* Override submitty-vue.css */
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-grow: 1;
+}
+
+.archive-toggle-btn {
+    flex-grow: 0;
 }
 
 .div1 {

site/cypress/e2e/Cypress-System/login.spec.js
@@ -63,7 +63,7 @@ describe('Test cases revolving around the logging in functionality of the site',
             cy.visit('authentication/login');
             cy.url().should('eq', `${Cypress.config('baseUrl')}/authentication/login`);
             loginViaUI('pearsr');
-            cy.get('#courses > div > h1').contains('My Courses');
+            cy.get('#courses > div > div > h1').contains('My Courses');
             cy.visit(['sample']);
             cy.get('.content').contains('You don\'t have access to this course.');
         });