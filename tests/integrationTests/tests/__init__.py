3011700358bfe3913774679606415fc165950435
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2025-12-09 14:49:56 -0500

    debug

--- tests/integrationTests/tests/__init__.py
+++ tests/integrationTests/tests/__init__.py
@@ -5,4 +5,5 @@ __all__ = []
 for loader, module_name, is_pkg in pkgutil.iter_modules(__path__):
     __all__.append(module_name)
     module = importlib.import_module(f"{__name__}.{module_name}")
+    print(f"MODNAME 1 is {module_name}")
     globals()[module_name] = module
