91af29538f31e8a65d21bc0efce0b7b61991488a
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2025-12-09 14:05:36 -0500

    fix module res

--- tests/integrationTests/tests/__init__.py
+++ tests/integrationTests/tests/__init__.py
@@ -4,5 +4,5 @@ import importlib
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.iter_modules(__path__):
     __all__.append(module_name)
-    module = importlib.import_module(module_name)
+    module = importlib.import_module(f"{__name__}.{module_name}")
     globals()[module_name] = module
