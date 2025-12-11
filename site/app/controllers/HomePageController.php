Submitty/Submitty

commit 65f9b76a771877cb111f76df56ae2203eccfd630

[Feature:Notifications] Home Page Additional Unseen Count (#12227)
### Why is this Change Important & Necessary?
The home page notifications panel only displays the 10 most recent
notifications from each of the user's courses. If the user has 11 unseen
notifications in a course, they will not be able to see the last one on
the home page. Additionally, the user could have more than 10 unseen
notifications between all of their courses, and they would not be able
to see all of them, and there is no indicator as to how many they have
total.

### What is the New Behavior?
Added a `<p>` beneath the displayed notifications in the `Unseen Only`
mode that appears when the user has more than 10 unseen notifications
and displays the total count of unseen notifications - 10 (the ones
already displayed).

#### Light Mode
<img width="717" height="94" alt="image"
src="https://github.com/user-attachments/assets/3961ad96-c12d-4ab6-89ec-51c4757094c8"
/>

#### Dark Mode
<img width="717" height="94" alt="image"
src="https://github.com/user-attachments/assets/75e362d0-99e3-4d4a-a970-1efea015a89e"
/>

#### Cases
1. When there are additional unseen notifications in the front-end, but
they are not currently displayed.
2. There are additional unseen notifications, but they are not able to
be brought to the front-end and displayed.
**When no unseen notifications are displayed, but there are older
ones:**
<img width="1538" height="250" alt="image"
src="https://github.com/user-attachments/assets/2b761f87-380f-4008-a258-29b0947d8542"
/>

#### Implementation
When we grab the notifications for the home page, we loop through and
query each of the course databases, appending the results to an array.
This array is then serialized (to add extra data) when it is sent to
Vue.

When getting the total unseen count, I wanted to avoid the following
complications:
1. Messing up the serialization of the Notifications array.
2. Looping through the course databases two different times.

site/app/controllers/HomePageController.php
@@ -151,10 +151,12 @@ class HomePageController extends AbstractController {
     }
 
     /**
-     * Returns recent notifications for a user
-     * @return array<int, Notification>
+     * Returns recent all recent notifications for a user,
+     * and the total count of their unseen notifications.
+     * @param int $unseen_count
+     * @return array<Notification>
      */
-    private function getAllRecentNotifications(): array {
+    private function getHomeNotificationData(int &$unseen_count = 0): array {
         $user_id = $this->core->getUser()->getId();
         $courses = $this->courses;
         $results = [];
@@ -167,12 +169,14 @@ class HomePageController extends AbstractController {
             $this->core->loadCourseDatabase();
             $course_db = $this->core->getCourseDB();
             $results = array_merge($results, $this->core->getQueries()->getRecentUserNotifications($user_id, $semester, $course_name, $course_db));
+            $unseen_count += (int) $this->core->getQueries()->getUnreadNotificationsCount($user_id, null);
         }
 
         usort($results, fn($a, $b) => $a->getElapsedTime() <=> $b->getElapsedTime());
 
         $this->core->setConfig($original_config);
         $this->core->loadCourseDatabase();
+        $unseen_count = $unseen_count;
         return $results;
     }
 
@@ -243,7 +247,8 @@ class HomePageController extends AbstractController {
     #[Route("/home")]
     public function showHomepage() {
         $courses = $this->getCourses()->json_response->json;
-        $notifications = $this->getAllRecentNotifications();
+        $unseen_count = 0;
+        $notifications = $this->getHomeNotificationData($unseen_count);
         return new MultiResponse(
             null,
             new WebResponse(
@@ -254,7 +259,8 @@ class HomePageController extends AbstractController {
                 $courses["data"]["dropped_courses"],
                 $courses["data"]["archived_courses"],
                 $courses["data"]["self_registration_courses"],
-                $notifications
+                $notifications,
+                $unseen_count
             )
         );
     }

site/vue/src/components/NotificationsDisplay.vue
@@ -12,12 +12,14 @@ import MarkSeenPopup from './MarkSeenPopup.vue';
 
 const props = defineProps<{
     notifications: Notification[];
+    unseenCount: number;
     course: boolean;
 }>();
 
 const showPopup = ref(false);
 const localNotifications = ref<Notification[]>([...props.notifications]);
 const showUnseenOnly = ref(true);
+const localUnseenCount = ref(props.unseenCount);
 
 // Preference is the same between course and home pages
 onMounted(() => {
@@ -86,12 +88,14 @@ function markIndividualSeen({ id, course }: { id: number; course: string }) {
     );
     if (target) {
         target.seen = true;
+        localUnseenCount.value--;
     }
 }
 
 // mark specified course notifications as seen without reloading
 function markAllSeen(courses: Record<string, unknown>[]) {
-    for (const { term, course } of courses) {
+    for (const { term, course, count } of courses) {
+        localUnseenCount.value = localUnseenCount.value - Number(count);
         for (const n of localNotifications.value) {
             if (n.semester === term && n.course === course) {
                 n.seen = true;
@@ -144,7 +148,7 @@ function markAllSeen(courses: Record<string, unknown>[]) {
       No notifications to view.
     </p>
     <p
-      v-if="filteredNotifications.length === 0 && localNotifications.length > 0"
+      v-if="filteredNotifications.length === 0 && localNotifications.length > 0 && localUnseenCount === 0"
       id="no-recent-notifications"
       class="no-recent"
     >
@@ -161,6 +165,24 @@ function markAllSeen(courses: Record<string, unknown>[]) {
         :course="props.course"
         @mark-individual="({ id, course }) => markIndividualSeen({ id, course })"
       />
+    </div>
+    <div
+      v-if="!props.course && showUnseenOnly || (filteredNotifications.length === 0 && localNotifications.length > 0) && localUnseenCount > 0"
+    >
+      <!-- Additional notifications in the front-end -->
+      <p
+        v-if="filteredNotifications.length >= 10 && localUnseenCount >= 11"
+        class="unseen-count-p"
+      >
+        You have <span class="unseen-count">{{ localUnseenCount - 10 }}</span> additional unseen notification<span v-if="localUnseenCount > 11">s</span>.
+      </p>
+      <!-- Unseen notifications that will not reach the front-end -->
+      <p
+        v-if="filteredNotifications.length < 10 && localUnseenCount > 0 && localNotifications.length > 10"
+        class="unseen-count-p"
+      >
+        You have <span class="unseen-count">{{ localUnseenCount - filteredNotifications.length }}</span> older unseen notification<span v-if="localUnseenCount > 1">s</span> in your course notifications not displayed here.
+      </p>
     </div>
   </div>
 </template>
@@ -196,5 +218,13 @@ function markAllSeen(courses: Record<string, unknown>[]) {
 
 .notification-settings-btn {
   font-family: arial, sans-serif;
+}
+.unseen-count-p {
+  padding-top: 10px;
+  font-weight: 600;
+}
+.unseen-count {
+  color: var(--badge-backgroud-red);
+  font-weight: 900;
 }
 </style>

site/vue/src/pages/CourseNotificationsPage.vue
@@ -13,6 +13,7 @@ defineProps<{
   >
     <NotificationsDisplay
       :notifications="notifications"
+      :unseenCount="0"
       :course="true"
     />
   </div>

site/app/views/HomePageView.php
@@ -12,6 +12,7 @@ class HomePageView extends AbstractView {
      * @param array $archived_courses
      * @param array<mixed> $self_registration_courses
      * @param array<array<string, mixed>> $notifications
+     * @param int $unseen_count
      */
     public function showHomePage(
         User $user,
@@ -20,6 +21,7 @@ class HomePageView extends AbstractView {
         array $archived_courses,
         array $self_registration_courses,
         array $notifications,
+        int $unseen_count
     ) {
         $statuses = [];
         $course_types = [
@@ -64,6 +66,7 @@ class HomePageView extends AbstractView {
             "args" => [
                 "statuses" => $statuses,
                 "notifications" => $notifications,
+                "unseenCount" => $unseen_count,
                 "userId" => $user->getId(),
             ]
         ]);

site/tests/app/controllers/HomePageControllerTester.php
@@ -173,6 +173,6 @@ class HomePageControllerTester extends BaseUnitTest {
         $controller = new HomePageController($core);
         $response = $controller->showHomepage();
         $this->assertEquals('showHomePage', $response->web_response->view_function);
-        $this->assertEqualsCanonicalizing([$core->getUser(), [], [], [], [], []], $response->web_response->parameters);
+        $this->assertEqualsCanonicalizing([$core->getUser(), [], [], [], [], [], 0], $response->web_response->parameters);
     }
 }

site/vue/src/components/MarkSeenPopup.vue
@@ -57,6 +57,7 @@ function markSeen() {
         .map((item) => ({
             term: item.term,
             course: item.title,
+            count: item.count,
         }));
 
     if (selectedCourses.length === 0) {

site/vue/src/pages/HomePage.vue
@@ -20,6 +20,7 @@ type Course = {
 interface Props {
     statuses: { [key in Status]: { [key: string]: Rank } };
     notifications: Notification[];
+    unseenCount: number;
     course: boolean;
     userId: string;
 }
@@ -164,6 +165,7 @@ const buildCourseUrl = (course: Course) => {
     >
       <NotificationsDisplay
         :notifications="notifications"
+        :unseenCount="unseenCount"
         :course="false"
       />
     </div>

commit 3f7774a37be30048226aeef6433e1ac6a117d095

[Bugfix:Notifications] Misc Notification UI Bugfixes (#12233)
### Why is this Change Important & Necessary?
There are several small bugs and code convention errors in the
notification UI, mostly introduced by #12227. If necessary, this PR can
be split into multiple smaller ones, but since all changes are minor and
primarily contained in `NotificationsDisplay.vue`, I figured I would
just address all of them in a single PR.

#### UI  Issues
1. **Unseen notification count displayed when 0**
In the original PR, #12227, I did not have a condition for when older
notifications is equal to 0. This meant if the user had an unseen
notification displayed, the older notifications count would always be
visible, even when it was 0.

2. **"No unseen notifications" not appearing dynamically when course
notifications are cleared**
If the user marks an individual course notification as seen and then
clears all of them, the "No unseen notifications" text would not appear
until after a reload. This is because the course notification's prop for
unseenCount is set to 0, meaning it becomes negative if notifications
are marked seen, and thus preventing the text from displaying.

#### Developer Convention Issues
3. **Using Incorrect Notification Arrays**
`NotificationsDisplay.vue` contains 3 notification lists:
- `localNotifications` - all notifications sent from the backend
- `filteredNotifications` - filtered version of `localNotifications`
that is either unseen only, or all based on local storage variable
- `visibleNotifications` - filtered version of `filteredNotifications` -
all for courses, 10 most recent for home page.
When referring to displayed notifications, you should use
`visibleNotifications`. When manipulating the data in notifications
(i.e., mark seen), you should use `localNotifications`, since the
updates will propogate to the other arrays from there.

The logic introduced in #12227 uses `filteredNotifications`. I don't

site/app/controllers/HomePageController.php
@@ -119,8 +119,8 @@ class HomePageController extends AbstractController {
         $course_title = $_POST['course'];
         foreach ($courses as $course) {
             if ($course->getTitle() === $course_title) {
-                $semester = $course->getTerm();
-                $this->core->loadCourseConfig($semester, $course_title);
+                $term = $course->getTerm();
+                $this->core->loadCourseConfig($term, $course_title);
                 $this->core->loadCourseDatabase();
                 $url = $this->core->buildCourseUrl(['notifications']);
                 $this->core->redirect($url);
@@ -140,8 +140,8 @@ class HomePageController extends AbstractController {
         $notification_id = $_POST['notification_id'];
         foreach ($courses as $course) {
             if ($course->getTitle() === $course_title) {
-                $semester = $course->getTerm();
-                $this->core->loadCourseConfig($semester, $course_title);
+                $term = $course->getTerm();
+                $this->core->loadCourseConfig($term, $course_title);
                 $this->core->loadCourseDatabase();
                 $this->core->getQueries()->markNotificationAsSeen($user_id, $notification_id);
                 break;
@@ -163,12 +163,12 @@ class HomePageController extends AbstractController {
         $original_config = clone $this->core->getConfig();
 
         foreach ($courses as $course) {
-            $semester = $course->getTerm();
+            $term = $course->getTerm();
             $course_name = $course->getTitle();
-            $this->core->loadCourseConfig($semester, $course_name);
+            $this->core->loadCourseConfig($term, $course_name);
             $this->core->loadCourseDatabase();
             $course_db = $this->core->getCourseDB();
-            $results = array_merge($results, $this->core->getQueries()->getRecentUserNotifications($user_id, $semester, $course_name, $course_db));
+            $results = array_merge($results, $this->core->getQueries()->getRecentUserNotifications($user_id, $term, $course_name, $course_db));
             $unseen_count += (int) $this->core->getQueries()->getUnreadNotificationsCount($user_id, null);
         }
 

site/vue/src/components/NotificationsDisplay.vue
@@ -17,7 +17,6 @@ const props = defineProps<{
 }>();
 
 const showPopup = ref(false);
-const localNotifications = ref<Notification[]>([...props.notifications]);
 const showUnseenOnly = ref(true);
 const localUnseenCount = ref(props.unseenCount);
 
@@ -40,18 +39,21 @@ function toggleUnseenOnly() {
     );
 }
 
-// # of displayed notifications: All for course, set amount for home page
-const visibleNotifications = computed(() =>
-    props.course
-        ? filteredNotifications.value
-        : filteredNotifications.value.slice(0, 10),
-);
+// All notifications that are sent from the backend
+const localNotifications = ref<Notification[]>([...props.notifications]);
 
-// Filter between most recent notifications and most recent unseen notifications
+// Filter between most recent notifications and most recent unseen notifications based on local storage
 const filteredNotifications = computed(() =>
     showUnseenOnly.value
         ? localNotifications.value.filter((n) => !n.seen)
         : localNotifications.value,
+);
+
+// Manage maxumum number of displayed notifications based on course or home page
+const visibleNotifications = computed(() =>
+    props.course
+        ? filteredNotifications.value
+        : filteredNotifications.value.slice(0, 10),
 );
 
 function markSeen() {
@@ -83,12 +85,12 @@ function markSeen() {
 
 // mark notification as seen without reloading
 function markIndividualSeen({ id, course }: { id: number; course: string }) {
-    const target = localNotifications.value.find(
-        (n) => n.id === id && n.course === course,
-    );
-    if (target) {
-        target.seen = true;
-        localUnseenCount.value--;
+    for (const n of localNotifications.value) {
+        if (n.id === id && n.course === course) {
+            n.seen = true;
+            localUnseenCount.value--;
+            break;
+        }
     }
 }
 
@@ -97,7 +99,7 @@ function markAllSeen(courses: Record<string, unknown>[]) {
     for (const { term, course, count } of courses) {
         localUnseenCount.value = localUnseenCount.value - Number(count);
         for (const n of localNotifications.value) {
-            if (n.semester === term && n.course === course) {
+            if (n.term === term && n.course === course) {
                 n.seen = true;
             }
         }
@@ -148,7 +150,7 @@ function markAllSeen(courses: Record<string, unknown>[]) {
       No notifications to view.
     </p>
     <p
-      v-if="filteredNotifications.length === 0 && localNotifications.length > 0 && localUnseenCount === 0"
+      v-if="visibleNotifications.length === 0 && localNotifications.length > 0 && localUnseenCount <= 0"
       id="no-recent-notifications"
       class="no-recent"
     >
@@ -167,21 +169,21 @@ function markAllSeen(courses: Record<string, unknown>[]) {
       />
     </div>
     <div
-      v-if="!props.course && showUnseenOnly || (filteredNotifications.length === 0 && localNotifications.length > 0) && localUnseenCount > 0"
+      v-if="!props.course && showUnseenOnly || (visibleNotifications.length === 0 && localNotifications.length > 0) && localUnseenCount > 0"
     >
       <!-- Additional notifications in the front-end -->
       <p
-        v-if="filteredNotifications.length >= 10 && localUnseenCount >= 11"
+        v-if="visibleNotifications.length >= 10 && localUnseenCount >= 11"
         class="unseen-count-p"
       >
         You have <span class="unseen-count">{{ localUnseenCount - 10 }}</span> additional unseen notification<span v-if="localUnseenCount > 11">s</span>.
       </p>
       <!-- Unseen notifications that will not reach the front-end -->
       <p
-        v-if="filteredNotifications.length < 10 && localUnseenCount > 0 && localNotifications.length > 10"
+        v-if="visibleNotifications.length < 10 && localUnseenCount > 0 && localNotifications.length > 10 && (localUnseenCount - visibleNotifications.length) > 0"
         class="unseen-count-p"
       >
-        You have <span class="unseen-count">{{ localUnseenCount - filteredNotifications.length }}</span> older unseen notification<span v-if="localUnseenCount > 1">s</span> in your course notifications not displayed here.
+        You have <span class="unseen-count">{{ localUnseenCount - visibleNotifications.length }}</span> older unseen notification<span v-if="localUnseenCount > 1">s</span> in your course notifications not displayed here.
       </p>
     </div>
   </div>

site/vue/src/pages/CourseNotificationsPage.vue
@@ -13,7 +13,7 @@ defineProps<{
   >
     <NotificationsDisplay
       :notifications="notifications"
-      :unseenCount="0"
+      :unseenCount="-1"
       :course="true"
     />
   </div>

site/vue/src/types/Notification.ts
@@ -7,7 +7,7 @@ export interface Notification {
     elapsed_time: number;
     created_at: string;
     notify_time: string;
-    semester: string;
+    term: string;
     course: string;
     url: string;
 }