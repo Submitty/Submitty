b2badf1d84713c58e9d5ed5908d09fda780c1fa0
Rkoester47 <koestr@rpi.edu>
2026-01-20 15:06:24 -0500

    Added some submission steps

--- site/cypress/e2e/Cypress-Gradeable/gradeable.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/gradeable.spec.js
@@ -66,6 +66,30 @@ describe('Tests cases revolving around gradeable access and submission', () => {
     });
     it('Should test whether or not team autograding is working correctly', () => {
         cy.login('student');
-        cy.visit(['sample', 'gradeable', 'closed_team_homework', 'grading', 'grade?who_id=aYl92mR3NvJYGrK&sort=id&direction=ASC']);
+        const testfileteam = '\more_autograding_examples\cpp_hidden_tests\submissions';
+        cy.visit(['sample', 'gradeable', 'closed_team_homework', 'grading']);
+        cy.get('#upload1').selectFile(testfileteam, { action: 'drag-drop' });
+            cy.get('#startnew').click();
+            cy.get('#submit').should('be.disabled');
+            cy.get('#upload1').selectFile(testfileteam, { action: 'drag-drop' });
+
+            cy.waitPageChange(() => {
+                cy.get('#submit').click();
+            });
+            cy.get('#submitted-files > div').contains('frame_hardcoded.cpp');
+            cy.get('#submitted-files > div').contains('Download all files:').should('not.exist');
+
+            cy.get('[fname = "file1.txt"] > td').first().contains('file1.txt').next('.file-trash').click();
+            cy.get('[fname = "file1.txt"]').should('not.exist');
+            cy.get('#upload1').selectFile(testfileteam, { action: 'drag-drop' });
+
+            cy.waitPageChange(() => {
+                cy.get('.alert-success > a').click(); // Dismiss successful upload message
+                cy.get('#submit').click();
+            });
+
+            // Checks submitted files
+            cy.get('#submitted-files > div').contains('span', 'frame_hardcoded.cpp');
+            cy.get('#submitted-files > div').contains('Download all files:');
     });
 });
