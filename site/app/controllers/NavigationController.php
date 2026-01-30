f4a07e936a83aec863ada04c4d0a8357dfcea550
Eli Schwartz <eli.jonathan.schwartz@gmail.com>
2026-01-30 17:12:26 -0500

    Fixed PHP linting

--- site/app/controllers/NavigationController.php
+++ site/app/controllers/NavigationController.php
@@ -15,14 +15,14 @@ class NavigationController extends AbstractController {
     }
 
     #[Route('verify_time.js')]
-    public function verify_time() {
+    public function verify_time(): void {
         $this->core->getOutput()->useHeader(false);
         $this->core->getOutput()->useFooter(false);
         header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
         header("Pragma: no-cache"); // HTTP 1.0.
         header("Expires: 0"); // Proxies.
         header("Content-Type: text/javascript");
-        return $this->core->getOutput()->renderTwigOutput('TimeHeader.twig', ["server_time" => time()]);
+        $this->core->getOutput()->renderTwigOutput('TimeHeader.twig', ["server_time" => time()]);
     }
 
     #[Route('/courses/{_semester}/{_course}', requirements: ['_semester' => '^(?!api)[^\/]+', '_course' => '[^\/]+'])]
