cfb19e1225009a71be740e8000a96b32d8301812
Avi McGrady <mcgraa4@rpi.edu>
2026-02-18 22:36:09 -0500

    created make_preserve_files.py

---
+++ migration/migrator/migrations/system/20260218220517_make_preserve_files.py
@@ -0,0 +1,37 @@
+"""Migration for the Submitty system."""
+
+import os
+import json
+
+def up(config):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    """
+
+    preserve_path = "/usr/local/submitty/config/preserve_files_list.json"
+
+    paths_to_preserve = [
+        "/usr/local/submitty/site/public/img/submitty_logo.png",
+        "/usr/local/submitty/site/public/img/submitty_banner.png",
+        "/usr/local/submitty/testfile.txt"
+    ]
+
+    os.makedirs(os.path.dirname(preserve_path), exist_ok=True)
+    with open(preserve_path, "w", encoding="utf-8") as file:
+        json.dump(paths_to_preserve, file, indent=2)
+        file.write("\n")
+
+    pass
+
+
+def down(config):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    """
+    pass
