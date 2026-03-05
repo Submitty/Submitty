5c0c90683e894c988fb8ab55f08ae64664b19177
jashanpreet-k <kaurjashanpreet713@gmail.com>
2026-03-06 00:41:12 +0530

    Improve Cypress test for legal name privacy (#8435)

--- site/cypress/e2e/Cypress-UI/name_privacy.spec.js
+++ site/cypress/e2e/Cypress-UI/name_privacy.spec.js
@@ -12,9 +12,14 @@ describe('Legal name privacy test', () => {
     cy.document().then((doc) => {
       const html = doc.documentElement.innerHTML;
 
+      // Ensure legal names never appear in UI
       expect(html).to.not.contain(legalFirst);
       expect(html).to.not.contain(legalLast);
     });
+
+    // Also check visible name fields
+    cy.get('body').should('not.contain', legalFirst);
+    cy.get('body').should('not.contain', legalLast);
 
   });
 
