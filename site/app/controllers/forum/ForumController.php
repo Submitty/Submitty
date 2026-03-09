804d9ff736329563e11e5b8286168dd618748a06
AK <anjalikamal3105@gmail.com>
2026-03-10 03:47:27 +0530

    [Bugfix:Forum] Truncate deleted forum post notification content
    Use previewText() to truncate deleted forum post notifications
    instead of including the entire post content.

--- site/app/controllers/forum/ForumController.php
+++ site/app/controllers/forum/ForumController.php
@@ -855,8 +855,9 @@ class ForumController extends AbstractController {
                 $type = "thread";
             }
             $metadata = json_encode([]);
-            $subject = "Deleted: " . $post->getContent();
-            $content = "In " . $full_course_name . "\n\nThread: " . $thread->getTitle() . "\n\nPost:\n" . $post->getContent() . " was deleted.";
+            $preview = $this->previewText($post->getContent(), 120);
+            $subject = "Deleted: " . $preview;
+            $content = "In " . $full_course_name . "\n\nThread: " . $thread->getTitle() . "\n\nPost:\n" . $preview . " was deleted.";
             $event = [ 'component' => 'forum', 'metadata' => $metadata, 'content' => $content, 'subject' => $subject, 'recipient' => $post->getAuthor()->getId(), 'preference' => 'all_modifications_forum'];
             $this->core->getQueries()->removeNotificationsPost($post_id);
             $this->sendSocketMessage(array_merge(
