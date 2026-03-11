3efbe83c43c7bb0b30f2e70210b4145fa46547e9
Khine12 <khinezarhein1@gmail.com>
2026-03-11 18:01:50 -0400

    [Testing:InstructorUI] Fix eslint errors in name privacy tests

--- site/cypress/e2e/Cypress-UI/name_privacy.spec.js
+++ site/cypress/e2e/Cypress-UI/name_privacy.spec.js
@@ -3,15 +3,17 @@ describe('Legal name privacy tests', () => {
         cy.login('student');
         cy.visit('/user_profile');
         cy.get('[data-testid="edit-preferred-name-form"]').invoke('show').within(() => {
-            cy.get('#user-givenname-change').clear().type('PreferredFirst');
-            cy.get('#user-familyname-change').clear().type('PreferredLast');
+        cy.get('#user-givenname-change').clear();
+        cy.get('#user-givenname-change').type('PreferredFirst');
+        cy.get('#user-familyname-change').clear();
+        cy.get('#user-familyname-change').type('PreferredLast');
         });
         cy.get('[data-testid="edit-preferred-name-submit"]').click();
         cy.logout();
     });
 
     const checkNoLegalNames = () => {
-        cy.document().then(doc => {
+        cy.document().then((doc) => {
             expect(doc.documentElement.innerHTML).to.not.include('Joe');
         });
     };
@@ -19,7 +21,7 @@ describe('Legal name privacy tests', () => {
     it('Legal names should not appear on any page', () => {
         cy.login('instructor');
         const pages = ['users', 'graders', 'student_photos', 'forum', 'navigation'];
-        pages.forEach(page => {
+        pages.forEach((page) => {
             cy.visit(['sample', page]);
             checkNoLegalNames();
         });
