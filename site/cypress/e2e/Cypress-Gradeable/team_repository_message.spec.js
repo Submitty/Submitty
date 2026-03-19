a9b7f21b2e02843933220896193f8bb3d8365dc8
Rishika Bothra <rshkbothra@gmail.com>
2026-03-20 01:54:36 +0530

    Add tests for repository message visibility on manage team page

---
+++ site/cypress/e2e/Cypress-Gradeable/team_repository_message.spec.js
@@ -0,0 +1,24 @@
+describe('Test repository error message on manage team page', () => {
+    it('Should NOT show repository message for non-VCS team gradeable', () => {
+        //Login as a student who is already on a team for open_team_homework
+        cy.login('student');
+        cy.visit(['sample', 'gradeable', 'open_team_homework', 'team']);
+        cy.get('h1').should('contain', 'Manage Team For: Open Team Homework');
+        cy.contains('Your Team:').should('be.visible');
+        //error message should not appear
+        cy.contains('Your repository does not exist').should('not.exist');
+        // The repository access instructions should also NOT appear
+        cy.contains('To access your Team Repository').should('not.exist');
+    });
+
+    it('Should show repository message for VCS team gradeable', () => {
+        // Login as a student
+        cy.login('student');
+        cy.visit(['sample', 'gradeable', 'open_vcs_homework', 'team']);
+        cy.get('body').then(($body) => {
+            const hasRepoInstructions = $body.text().includes('To access your Team Repository');
+            const hasRepoError = $body.text().includes('Your repository does not exist');
+            expect(hasRepoInstructions || hasRepoError).to.be.true;
+        });
+    });
+});
