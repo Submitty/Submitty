3a30af72eb7e0810c96c716e66dec44db258a429
Rishika Bothra <rshkbothra@gmail.com>
2026-03-25 16:06:23 +0530

    Improve comment formatting in team repository message tests

--- site/cypress/e2e/Cypress-Gradeable/team_repository_message.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/team_repository_message.spec.js
@@ -1,26 +1,26 @@
 describe('Test repository error message on manage team page', () => {
     it('Should NOT show repository message for non-VCS team gradeable', () => {
-        //Login as a student who is already on a team for open_team_homework
+        // Login as a student who is already on a team for open_team_homework
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_team_homework', 'team']);
         cy.get('h1').should('contain', 'Manage Team For: Open Team Homework');
         cy.contains('Your Team:').should('be.visible');
-        //error message should not appear
+        // error message should not appear
         cy.contains('Your repository does not exist').should('not.exist');
-        //The repository access instructions should also NOT appear
+        // The repository access instructions should also NOT appear
         cy.contains('To access your Team Repository').should('not.exist');
     });
 
     it('Should NOT show repository message for non-VCS submission page', () => {
-        //Login as a student
+        // Login as a student
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_team_homework']);
-        //error message should not appear
+        // error message should not appear
         cy.contains('Your repository does not exist').should('not.exist');
     });
 
     it('Should show repository message on VCS submission page', () => {
-        //Login as a student
+        // Login as a student
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_vcs_homework']);
         cy.get('body').then(($body) => {
