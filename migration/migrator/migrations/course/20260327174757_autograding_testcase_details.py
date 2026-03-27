15d25b907b32af00d2e119cc32b6c07fb5f1ee1e
dagemcn <d.eric.mcnitt@gmail.com>
2026-03-27 19:08:20 -0400

    changed name of table

---
+++ migration/migrator/migrations/course/20260327174757_autograding_testcase_details.py
@@ -0,0 +1,59 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute(
+        """
+        CREATE TABLE IF NOT EXISTS autograding_testcase_details (
+            g_id          character varying(255) NOT NULL,
+            user_id       character varying(255),
+            team_id       character varying(255),
+            g_version     integer                NOT NULL,
+            testcase_id   character varying(255) NOT NULL,
+            testcase_order integer               NOT NULL,
+            hidden        boolean                DEFAULT false NOT NULL,
+            extra_credit  boolean                DEFAULT false NOT NULL,
+            points_possible numeric(10,2)        NOT NULL,
+            points_earned   numeric(10,2)        NOT NULL,
+            CONSTRAINT user_team_id_check CHECK ((user_id IS NOT NULL) != (team_id IS NOT NULL))
+            );
+        """
+        )
+    database.execute(
+        "CREATE INDEX idx_autograding_testcase_details_user ON autograding_testcase_details (g_id, user_id, g_version);"
+    )
+    database.execute(
+        "CREATE INDEX idx_autograding_testcase_details_team ON autograding_testcase_details (g_id, team_id, g_version);"
+    )
+    database.execute(
+        "CREATE INDEX idx_autograding_testcase_details_gradeable ON autograding_testcase_details (g_id, testcase_id);"
+    )
+    pass
+
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    pass
