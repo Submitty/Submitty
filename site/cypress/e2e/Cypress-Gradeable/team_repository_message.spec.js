892e55a4f7fe3689a980755b65a47af2a96f92bb
Rishika Bothra <rshkbothra@gmail.com>
2026-03-28 01:06:37 +0530

    Add data-testid attributes for VCS repository messages and update tests

--- site/cypress/e2e/Cypress-Gradeable/team_repository_message.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/team_repository_message.spec.js
@@ -4,11 +4,11 @@ describe('Test repository error message on manage team page', () => {
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_team_homework', 'team']);
         cy.get('h1').should('contain', 'Manage Team For: Open Team Homework');
-        cy.contains('Your Team:').should('be.visible');
+        cy.get('[data-testid="your-team-header"]').should('be.visible');
         // error message should not appear
-        cy.contains('Your repository does not exist').should('not.exist');
+        cy.get('[data-testid="vcs-repository-error"]').should('not.exist');
         // The repository access instructions should also NOT appear
-        cy.contains('To access your Team Repository').should('not.exist');
+        cy.get('[data-testid="vcs-repository-info"]').should('not.exist');
     });
 
     it('Should NOT show repository message for non-VCS submission page', () => {
@@ -16,7 +16,7 @@ describe('Test repository error message on manage team page', () => {
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_team_homework']);
         // error message should not appear
-        cy.contains('Your repository does not exist').should('not.exist');
+        cy.get('[data-testid="vcs-repository-error"]').should('not.exist');
     });
 
     it('Should show repository message on VCS submission page', () => {
@@ -24,8 +24,8 @@ describe('Test repository error message on manage team page', () => {
         cy.login('student');
         cy.visit(['sample', 'gradeable', 'open_vcs_homework']);
         cy.get('body').then(($body) => {
-            const hasRepoInstructions = $body.text().includes('To access your Repository');
-            const hasRepoError = $body.text().includes('Your repository does not exist');
+            const hasRepoInstructions = $body.find('[data-testid="vcs-repository-info"]').length > 0;
+            const hasRepoError = $body.find('[data-testid="vcs-repository-error"]').length > 0;
             expect(hasRepoInstructions || hasRepoError).to.be.true;
         });
     });
