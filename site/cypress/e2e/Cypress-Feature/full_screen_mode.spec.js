da67be7d6337e08131d57cf81d2ca9eafc7769de
Akarsh Kushwaha <akarshkushwaha593@gmail.com>
2026-04-05 22:49:56 +0530

    UI/UX: Address full screen mode PR review comments

--- site/cypress/e2e/Cypress-Feature/full_screen_mode.spec.js
+++ site/cypress/e2e/Cypress-Feature/full_screen_mode.spec.js
@@ -45,4 +45,21 @@ describe('Full screen mode toggle', () => {
         cy.get('main#main').should('not.have.class', 'full-screen-mode');
         cy.get('[data-fullscreen-toggle] i').should('have.class', 'fa-expand');
     });
+
+    it('Should toggle full screen mode on Live Chat', () => {
+        cy.visit(['sample', 'chat']);
+
+        // Verify full screen button exists
+        cy.get('[data-fullscreen-toggle]').should('exist');
+
+        // Click to enter full screen
+        cy.get('[data-fullscreen-toggle]').click();
+        cy.get('main#main').should('have.class', 'full-screen-mode');
+        cy.get('[data-fullscreen-toggle] i').should('have.class', 'fa-compress');
+
+        // Click to exit full screen
+        cy.get('[data-fullscreen-toggle]').click();
+        cy.get('main#main').should('not.have.class', 'full-screen-mode');
+        cy.get('[data-fullscreen-toggle] i').should('have.class', 'fa-expand');
+    });
 });
