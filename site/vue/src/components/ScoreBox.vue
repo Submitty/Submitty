f1c385edc2327a45cc13264b7d9a395c11795836
Robu9 <anushayjain7622@gmail.com>
2026-04-21 23:19:58 +0530

    Refactor ScoreBox component to utilize Badge component for score display
    - Removed computed properties for badge styling and display text from ScoreBox.vue.
    - Integrated Badge component to handle score representation, simplifying the template.
    - Updated Badge.twig to include Vue component rendering for better modularity.
    
    This change enhances the maintainability of the code by centralizing badge logic within the Badge component.

--- site/vue/src/components/ScoreBox.vue
+++ site/vue/src/components/ScoreBox.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import Badge from './Badge.vue';
 
 interface Props {
     taScore: number;
@@ -8,27 +8,6 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-
-const badgeClass = computed(() => {
-    if (!props.activeSameAsGraded) {
-        return 'gray-background';
-    }
-    if (props.taScore >= props.taMax) {
-        return 'green-background';
-    }
-    if (props.taScore > props.taMax * 0.5) {
-        return 'yellow-background';
-    }
-    return 'red-background';
-});
-
-const displayText = computed(() => {
-    return `${props.taScore} / ${props.taMax}`;
-});
-
-const shouldShow = computed(() => {
-    return props.taMax > 0 || props.taScore < 0;
-});
 </script>
 
 <template>
@@ -40,17 +19,11 @@ const shouldShow = computed(() => {
       class="box-title-total key_to_click"
       tabindex="0"
     >
-      <span
-        v-if="shouldShow"
-        class="badge"
-        :class="badgeClass"
-        data-testid="score-pill-badge"
-      >
-        {{ displayText }}
-      </span>
-      <div
-        v-else
-        class="no-badge"
+      <Badge
+        :earned="props.taScore"
+        :max="props.taMax"
+        :extra-credit="false"
+        :active-same-as-graded="props.activeSameAsGraded"
       />
       <h4>TA / Instructor Grading Total</h4>
     </div>
