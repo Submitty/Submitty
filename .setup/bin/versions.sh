1dd52455873790db523d00cd39b4a400efc6c563
SubmittyBot <SubmittyBot@users.noreply.github.com>
2026-05-01 03:32:31 +0000

    Update RainbowGrades to v26.04.01

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -8,7 +8,7 @@
 export AnalysisTools_Version=v22.03.00
 export AnalysisToolsTS_Version=v26.04.00
 export Lichen_Version=v23.09.00
-export RainbowGrades_Version=v25.10.00
+export RainbowGrades_Version=v26.04.01
 export Tutorial_Version=v25.06.03
 export SysadminTools_Version=v25.01.00
 export Localization_Version=v23.08.02
