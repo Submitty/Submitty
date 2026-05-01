aecee588d62301039237effef53cac84245b1f97
SubmittyBot <43100045+SubmittyBot@users.noreply.github.com>
2026-04-30 23:36:36 -0400

    [Dependency] Bump Lichen from v23.09.00 to v26.04.01 (#12823)
    Bumps [Lichen](https://github.com/Submitty/Lichen) from v23.09.00 to
    v26.04.01.
    
    ---
    [See release
    notes](https://github.com/Submitty/Lichen/releases/tag/v26.04.01)
    
    Co-authored-by: SubmittyBot <SubmittyBot@users.noreply.github.com>
    Co-authored-by: Barb Cutler <bmcutler@users.noreply.github.com>

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -7,7 +7,7 @@
 # SUBMITTY REPOS
 export AnalysisTools_Version=v22.03.00
 export AnalysisToolsTS_Version=v26.04.00
-export Lichen_Version=v23.09.00
+export Lichen_Version=v26.04.01
 export RainbowGrades_Version=v26.04.01
 export Tutorial_Version=v25.06.03
 export SysadminTools_Version=v25.01.00
