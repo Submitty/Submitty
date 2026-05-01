f6b0b8c4c4bb5f050d4dba0c56e5fc2940ad84eb
SubmittyBot <SubmittyBot@users.noreply.github.com>
2026-05-01 03:21:38 +0000

    Update AnalysisToolsTS to v26.04.00

--- .setup/bin/versions.sh
+++ .setup/bin/versions.sh
@@ -6,7 +6,7 @@
 
 # SUBMITTY REPOS
 export AnalysisTools_Version=v22.03.00
-export AnalysisToolsTS_Version=v23.10.00
+export AnalysisToolsTS_Version=v26.04.00
 export Lichen_Version=v23.09.00
 export RainbowGrades_Version=v25.10.00
 export Tutorial_Version=v25.06.03
