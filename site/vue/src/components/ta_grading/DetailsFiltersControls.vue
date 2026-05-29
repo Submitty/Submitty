e2aeecd6429e363075d7f5b27001afcc1066aaed
Ansh Jindal <jndl.ansh@gmail.com>
2026-05-29 16:07:20 +0530

    FIx JS lint

--- site/vue/src/components/ta_grading/DetailsFiltersControls.vue
+++ site/vue/src/components/ta_grading/DetailsFiltersControls.vue
@@ -5,7 +5,7 @@ declare global {
     interface Window {
         updateElectronicGradingRowNumbersAndColors: () => void;
         updateSimpleGradingRowNumbersAndColors: () => void;
-        Cookies?: { 
+        Cookies?: {
             get: (key: string) => string | undefined;
             set: (key: string, value: string, options?: { path?: string; expires?: number }) => void;
         };
@@ -131,85 +131,85 @@ const onFilterWithdrawn = (event: Event) => {
 </script>
 
 <template>
-    <div class="row-wrapper">
-        <label
-            v-if="showAllSections"
-            for="toggle-view-sections"
-            data-testid="view-sections-label"
-            class="column-wrapper"
-        >
-            Only Assigned Sections
-            <input
-                id="toggle-view-sections"
-                type="checkbox"
-                data-testid="view-sections"
-                :checked="viewSectionsChecked"
-                @change="onChangeSections"
-            >
-        </label>
+  <div class="row-wrapper">
+    <label
+      v-if="showAllSections"
+      for="toggle-view-sections"
+      data-testid="view-sections-label"
+      class="column-wrapper"
+    >
+      Only Assigned Sections
+      <input
+        id="toggle-view-sections"
+        type="checkbox"
+        data-testid="view-sections"
+        :checked="viewSectionsChecked"
+        @change="onChangeSections"
+      >
+    </label>
 
-        <label
-            for="toggle-random-order"
-            data-testid="random-order-label"
-            class="column-wrapper"
-        >
-            Randomize Order
-            <input
-                id="toggle-random-order"
-                type="checkbox"
-                data-testid="random-order-checkbox"
-                :checked="randomOrderChecked"
-                @change="onChangeSortOrder"
-            >
-        </label>
+    <label
+      for="toggle-random-order"
+      data-testid="random-order-label"
+      class="column-wrapper"
+    >
+      Randomize Order
+      <input
+        id="toggle-random-order"
+        type="checkbox"
+        data-testid="random-order-checkbox"
+        :checked="randomOrderChecked"
+        @change="onChangeSortOrder"
+      >
+    </label>
 
-        <label
-            v-if="toggleAnon"
-            for="toggle-anon-students"
-            data-testid="anon-students-label"
-            class="column-wrapper"
-            :data-gradeable-id="gradeableId"
-        >
-            Anonymize Student Names
-            <input
-                id="toggle-anon-students"
-                type="checkbox"
-                data-testid="anon-students-checkbox"
-                :checked="anonMode"
-                @change="onChangeAnon"
-            >
-        </label>
+    <label
+      v-if="toggleAnon"
+      for="toggle-anon-students"
+      data-testid="anon-students-label"
+      class="column-wrapper"
+      :data-gradeable-id="gradeableId"
+    >
+      Anonymize Student Names
+      <input
+        id="toggle-anon-students"
+        type="checkbox"
+        data-testid="anon-students-checkbox"
+        :checked="anonMode"
+        @change="onChangeAnon"
+      >
+    </label>
 
-        <label
-            v-if="gradeInquiryOnly"
-            for="toggle-inquiry-only"
-            data-testid="inquiry-only-label"
-            class="column-wrapper"
-        >
-            Grade Inquiries Only
-            <input
-                id="toggle-inquiry-only"
-                type="checkbox"
-                data-testid="inquiry-only-checkbox"
-                :checked="inquiryOnlyChecked"
-                @change="onChangeInquiry"
-            >
-        </label>
+    <label
+      v-if="gradeInquiryOnly"
+      for="toggle-inquiry-only"
+      data-testid="inquiry-only-label"
+      class="column-wrapper"
+    >
+      Grade Inquiries Only
+      <input
+        id="toggle-inquiry-only"
+        type="checkbox"
+        data-testid="inquiry-only-checkbox"
+        :checked="inquiryOnlyChecked"
+        @change="onChangeInquiry"
+      >
+    </label>
 
-        <label
-            v-if="canFilterWithdrawn"
-            for="toggle-filter-withdrawn"
-            data-testid="filter-withdrawn-label"
-            class="column-wrapper"
-        >
-            Hide Withdrawn Students
-            <input
-                id="toggle-filter-withdrawn"
-                type="checkbox"
-                data-testid="filter-withdrawn-checkbox"
-                :checked="withdrawnHiddenChecked"
-                @change="onFilterWithdrawn"
-            >
-        </label>
-    </div>
+    <label
+      v-if="canFilterWithdrawn"
+      for="toggle-filter-withdrawn"
+      data-testid="filter-withdrawn-label"
+      class="column-wrapper"
+    >
+      Hide Withdrawn Students
+      <input
+        id="toggle-filter-withdrawn"
+        type="checkbox"
+        data-testid="filter-withdrawn-checkbox"
+        :checked="withdrawnHiddenChecked"
+        @change="onFilterWithdrawn"
+      >
+    </label>
+  </div>
 </template>
