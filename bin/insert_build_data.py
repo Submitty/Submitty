9cc10a97f5c3c3bebc86917831e55d6f1cf19e70
dagemcn <d.eric.mcnitt@gmail.com>
2026-06-05 11:39:40 -0400

    fixed minor linting and added new file for insert on hw build

---
+++ bin/insert_build_data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+# This script is run by a cron job as the DAEMON_USER
+#
+# Whenever a gradeable is built, insert testcase details into the database
+
+from sqlalchemy import create_engine, Table, MetaData, bindparam, select, func, insert, delete, update
+
+def insert_into_database()
