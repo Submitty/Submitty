9028fcc3d56bca1383fd772060b4c8c3f70047d4
NicholasCiuica <nick.ciuica@gmail.com>
2026-06-09 17:07:24 -0400

    Fixing cypress test
    addresses the following issues: now clears text fields after failing to sign in (in case page isn't reloaded after sign in fails); randomizes username and email that is used to sign in so the current contents of the database (likely) wont affect the current test run; commented out a number of password requirement tests that currently don't work because we are not yet capable of changing configs within cypress tests

--- site/cypress/e2e/Cypress-System/self_account_creation.spec.js
+++ site/cypress/e2e/Cypress-System/self_account_creation.spec.js
@@ -1,6 +1,6 @@
 const valid_given_name = 'GivenName';
-const valid_user_id = 'good_id';
-const valid_email = 'valid.email@gmail.com';
+let valid_user_id = 'good_id';
+let valid_email = 'valid.email@gmail.com';
 const valid_password = 'Password123!';
 const valid_family_name = 'FamilyName';
 const incorrect_verification_code = '99999999';
@@ -15,8 +15,26 @@ function inputData(email = valid_email, user_id = valid_user_id, password = vali
     cy.get('[data-testid="confirm-password"]').type(confirm_password);
 }
 
+function clearTextFields() {
+    // clears all text fields in the Create Account form;
+    // this function may or may not be needed, depending on if frontend validation
+    // for passwords successfully prevents reload when password is incorrect
+    // (event.preventDefault() doesn't always seem to do the trick)
+    cy.get('[data-testid="email"]').clear();
+    cy.get('[data-testid="user-id"]').clear();
+    cy.get('[data-testid="given-name"]').clear();
+    cy.get('[data-testid="family-name"]').clear();
+    cy.get('[data-testid="password"]').clear();
+    cy.get('[data-testid="confirm-password"]').clear();
+}
+
 describe('Self account creation tests', () => {
     it('Test all paths of account creation', () => {
+        // create new randomized alphanumeric user id and email to limit interference of
+        // current database contents on test (especially if this test is run multiple times)
+        valid_user_id = Math.random().toString(36).substring(2, 8);
+        valid_email = `${Math.random().toString(36).substring(2, 8)}@gmail.com`;
+
         cy.visit();
         cy.get('[data-testid="new-account-button"]').click();
         // Not accepted email extension
@@ -31,26 +49,43 @@ describe('Self account creation tests', () => {
         inputData(undefined, '123456789123456789123456789');
         cy.get('[data-testid="sign-up-button"]').click();
         cy.get('[data-testid="popup-message"]').should('contain.text', 'This user id does not meet the requirements');
+
         // Password too short
         inputData(undefined, undefined, 'pass!123', 'pass!123');
         cy.get('[data-testid="sign-up-button"]').click();
         cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
-        // Password missing uppercase
-        inputData(undefined, undefined, 'nouppercase#123', 'nouppercase#123');
-        cy.get('[data-testid="sign-up-button"]').click();
-        cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
-        // Password missing lowercase
-        inputData(undefined, undefined, 'NOLOWERCASE#123', 'NOLOWERCASE#123');
-        cy.get('[data-testid="sign-up-button"]').click();
-        cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
-        // Password missing numbers
-        inputData(undefined, undefined, 'NoNumbersHere!@', 'NoNumbersHere!@');
-        cy.get('[data-testid="sign-up-button"]').click();
-        cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
-        // Password missing special characters
-        inputData(undefined, undefined, 'NoSpecialChar123', 'NoSpecialChar123');
-        cy.get('[data-testid="sign-up-button"]').click();
-        cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
+        clearTextFields(); // remove leftover inputs after frontend validation prevents sign up
+
+        /*
+            FIX ME! Currently the assertions below aren't accurate tests for the
+            password complexity requirements, because these requirements are false by default in the configs
+            and we don't yet have a method for changing configurations within Cypress tests.
+            For reference, the relevant configs are:
+                require_uppercase, require_lowercase, require_numbers, require_special_chars
+        */
+        /*
+            // Password missing uppercase
+            inputData(undefined, undefined, 'nouppercase#123', 'nouppercase#123');
+            cy.get('[data-testid="sign-up-button"]').click();
+            cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
+            clearTextFields();
+            // Password missing lowercase
+            inputData(undefined, undefined, 'NOLOWERCASE#123', 'NOLOWERCASE#123');
+            cy.get('[data-testid="sign-up-button"]').click();
+            cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
+            clearTextFields();
+            // Password missing numbers
+            inputData(undefined, undefined, 'NoNumbersHere!@', 'NoNumbersHere!@');
+            cy.get('[data-testid="sign-up-button"]').click();
+            cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
+            clearTextFields();
+            // Password missing special characters
+            inputData(undefined, undefined, 'NoSpecialChar123', 'NoSpecialChar123');
+            cy.get('[data-testid="sign-up-button"]').click();
+            cy.get('[data-testid="popup-message"]').should('contain.text', 'Password does not meet the requirements');
+            clearTextFields();
+        */
+
         // Passwords don't match
         inputData(undefined, undefined, 'Password123!', 'NotPassword123!');
         cy.get('[data-testid="confirm-password"]').blur();
