d37422da929e7c88d4e7c51c0583a1db66d4f14b
Ansh Jindal <jndl.ansh@gmail.com>
2026-06-15 10:31:11 +0530

    Vue component declaration and definition

---
+++ site/vue/src/components/ta_grading/EditPeerComponentsForm.vue
@@ -0,0 +1,186 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface PeerComponent {
+    id: string;
+    title: string;
+    max: number;
+    marks: number[];
+}
+
+interface MarkInfo {
+    title: string;
+    points: string;
+}
+
+interface PeerDetails {
+    graders: Record<string, string[]>;
+    marks_assigned: Record<string, Record<string, number[]>>;
+}
+
+const props = defineProps<{
+    peers: string[];
+    submitterId: string;
+    gradeableId: string;
+    csrfToken: string;
+    components: PeerComponent[];
+    componentScores: Record<string, Record<string, number>>;
+    peerDetails: PeerDetails;
+    marks: Record<string, MarkInfo>;
+}>();
+
+const safePeers = ref<string[]>([]);
+const safeComponents = ref<PeerComponent[]>([]);
+const safeComponentScores = ref<Record<string, Record<string, number>>>({});
+const safePeerDetails = ref<PeerDetails>({ graders: {}, marks_assigned: {} });
+const safeMarks = ref<Record<string, MarkInfo>>({});
+const selectedPeer = ref('');
+
+function initProps() {
+    safePeers.value = props.peers ?? [];
+    safeComponents.value = props.components ?? [];
+    safeComponentScores.value = props.componentScores ?? {};
+    safePeerDetails.value = props.peerDetails ?? { graders: {}, marks_assigned: {} };
+    safeMarks.value = props.marks ?? {};
+    selectedPeer.value = safePeers.value[0] ?? '';
+}
+
+initProps();
+
+function clearMarks() {
+    (window as any).clearPeerMarks(
+        props.submitterId ?? '', 
+        props.gradeableId ?? '', 
+        selectedPeer.value, // Pass the selected peer here
+        props.csrfToken ?? ''
+    );
+}
+
+function badgeClass(earned: number, max: number): string {
+    if (earned < 0) {
+        return earned < 0.5 * max ? 'red-background' : 'yellow-background';
+    }
+    if (earned >= max) {
+        return 'green-background';
+    }
+    if (earned > max * 0.5) {
+        return 'yellow-background';
+    }
+    return 'red-background';
+}
+
+function shouldShowBadge(earned: number, max: number): boolean {
+    return max > 0 || earned < 0;
+}
+
+function badgeText(earned: number, max: number): string {
+    if (earned < 0) {
+        return `\u2212${Math.abs(earned)} / ${max}`;
+    }
+    return `${earned} / ${max}`;
+}
+
+function isMarkAssigned(componentId: string, peer: string, markId: number): boolean {
+    // Add ?. after marks_assigned
+    return safePeerDetails.value?.marks_assigned?.[componentId]?.[peer]?.includes(markId) ?? false;
+}
+
+function scoreForComponent(componentId: string, peer: string): number | undefined {
+    // Add ?. after componentScores
+    return safeComponentScores.value?.[componentId]?.[peer];
+}
+
+function hasScore(componentId: string, peer: string): boolean {
+    return scoreForComponent(componentId, peer) !== undefined;
+}
+</script>
+
+<template>
+  <div>
+    <span>
+      Select the student whose marks you want to clear
+      <br>WARNING this will remove all of the peer grading done by this student:
+      <br>
+      ** WIP: Editing and deletion of individual components**
+    </span>
+    <select
+      id="edit-peer-select"
+      v-model="selectedPeer"
+      data-testid="edit-peer-select"
+    >
+      <option
+        v-for="peer in safePeers"
+        :key="peer"
+        :value="peer"
+      >
+        {{ peer }}
+      </option>
+    </select>
+    <div
+      v-for="peer in safePeers"
+      :key="peer"
+      v-show="selectedPeer === peer"
+      class="edit-peer-components-block"
+    >
+      <button
+        type="button"
+        class="btn"
+        data-testid="clear-peer-marks"
+        @click="clearMarks"
+      >
+        Clear All Grading
+      </button>
+      <br>
+      <div
+        v-for="component in safeComponents"
+        :key="component.id"
+      >
+        <div
+          v-if="hasScore(component.id, peer)"
+          class="box-badge"
+        >
+          <span
+            v-if="shouldShowBadge(scoreForComponent(component.id, peer) ?? 0, component.max)"
+            class="badge"
+            :class="badgeClass(scoreForComponent(component.id, peer) ?? 0, component.max)"
+            data-testid="score-pill-badge"
+          >
+            {{ badgeText(scoreForComponent(component.id, peer) ?? 0, component.max) }}
+          </span>
+          <div
+            v-else
+            class="no-badge"
+          />
+        </div>
+        <span class="component-title col-no-gutters">
+          <b>{{ component.title }}</b>
+        </span>
+        <div class="received-marks-list container">
+          <div
+            v-for="markId in component.marks"
+            :key="markId"
+            class="row"
+          >
+            <div class="col-no-gutters indicator">
+              <i
+                v-if="isMarkAssigned(component.id, peer, markId)"
+                class="far fa-check-square fa-1g"
+              />
+              <i
+                v-else
+                class="far fa-square fa-1g"
+              />
+            </div>
+            <div class="col-no-gutters point-value">
+              <span>{{ safeMarks[String(markId)]?.points }}</span>
+            </div>
+            <div class="col">
+              <span style="white-space: pre-wrap;">{{ safeMarks[String(markId)]?.title }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+  </div>
+</template>
