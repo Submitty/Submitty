a554c11ab609f4e0a14356d48c330249cd71d48b
dagemcn <d.eric.mcnitt@gmail.com>
2026-06-19 16:50:37 -0400

    js lint again '

--- site/vue/src/components/FullScreenButton.vue
+++ site/vue/src/components/FullScreenButton.vue
@@ -15,12 +15,13 @@ function toggle() {
 </script>
 
 <template>
-  <button 
-          id="fullscreen-btn" 
-          data-testid="fullscreen-btn" 
-          class="btn btn-default" 
-          title="Toggle full screen mode" 
-          @click="toggle">
+  <button
+    id="fullscreen-btn" 
+    data-testid="fullscreen-btn" 
+    class="btn btn-default" 
+    title="Toggle full screen mode" 
+    @click="toggle"
+    >
     <i :class="`fas ${isFullScreen ? 'fa-compress' : 'fa-expand'}`" />
   </button>
 </template>
