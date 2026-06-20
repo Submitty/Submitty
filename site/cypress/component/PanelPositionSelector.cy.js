04420b60ac476de9f57ff9e65a06b67f041a0e8a
Ansh Jindal <jndl.ansh@gmail.com>
2026-06-20 18:50:03 +0530

    Upadate the tests to have better coverage

--- site/cypress/component/PanelPositionSelector.cy.js
+++ site/cypress/component/PanelPositionSelector.cy.js
@@ -1,149 +1,135 @@
 import PanelPositionSelector from '../../vue/src/components/PanelPositionSelector.vue';
 
+function mount(overrides = {}) {
+    return cy.mount(PanelPositionSelector, {
+        props: {
+            panelId: 'test_panel',
+            numOfPanels: 4,
+            dividedColName: 'LEFT',
+            ...overrides,
+        },
+    });
+}
+
+function triggerChange(value) {
+    cy.get('[data-testid="panel-position-select"]').then(($sel) => {
+        $sel[0].value = value;
+        $sel[0].dispatchEvent(new Event('change', { bubbles: true }));
+    });
+}
+
 describe('PanelPositionSelector', () => {
-    it('renders 4 options in 4-panel mode', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 4,
-                dividedColName: 'LEFT',
-            },
-        });
-
-        cy.get('[data-testid="panel-position-select"]').should('be.visible');
+    it('renders all 4 positions in 4-panel mode', () => {
+        mount({ numOfPanels: 4 });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 4);
-        cy.get('[data-testid="panel-position-leftTop"]').should('contain', 'Open as top left panel');
-        cy.get('[data-testid="panel-position-leftBottom"]').should('contain', 'Open as bottom left panel');
-        cy.get('[data-testid="panel-position-rightTop"]').should('contain', 'Open as top right panel');
-        cy.get('[data-testid="panel-position-rightBottom"]').should('contain', 'Open as bottom right panel');
+        cy.get('[data-testid="panel-position-leftTop"]').should('have.text', 'Open as top left panel');
+        cy.get('[data-testid="panel-position-leftBottom"]').should('have.text', 'Open as bottom left panel');
+        cy.get('[data-testid="panel-position-rightTop"]').should('have.text', 'Open as top right panel');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('have.text', 'Open as bottom right panel');
     });
 
-    it('renders 2 options in 2-panel mode', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 2,
-                dividedColName: 'LEFT',
-            },
-        });
-
+    it('renders 2 positions with simplified labels in 2-panel mode', () => {
+        mount({ numOfPanels: 2 });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 2);
-        cy.get('[data-testid="panel-position-leftTop"]').should('contain', 'Open as left panel');
-        cy.get('[data-testid="panel-position-rightTop"]').should('contain', 'Open as right panel');
+        cy.get('[data-testid="panel-position-leftTop"]').should('have.text', 'Open as left panel');
+        cy.get('[data-testid="panel-position-rightTop"]').should('have.text', 'Open as right panel');
+        cy.get('[data-testid="panel-position-leftBottom"]').should('not.exist');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('not.exist');
     });
 
-    it('renders 3 options in 3-panel LEFT mode', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 3,
-                dividedColName: 'LEFT',
-            },
-        });
-
+    it('renders left-divided 3 positions in 3-panel LEFT mode', () => {
+        mount({ numOfPanels: 3, dividedColName: 'LEFT' });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 3);
-        cy.get('[data-testid="panel-position-leftTop"]').should('contain', 'Open as top left panel');
-        cy.get('[data-testid="panel-position-leftBottom"]').should('contain', 'Open as bottom left panel');
-        cy.get('[data-testid="panel-position-rightTop"]').should('contain', 'Open as right panel');
+        cy.get('[data-testid="panel-position-leftTop"]').should('have.text', 'Open as top left panel');
+        cy.get('[data-testid="panel-position-leftBottom"]').should('exist');
+        cy.get('[data-testid="panel-position-rightTop"]').should('have.text', 'Open as right panel');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('not.exist');
     });
 
-    it('renders 3 options in 3-panel RIGHT mode', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 3,
-                dividedColName: 'RIGHT',
-            },
-        });
-
+    it('renders right-divided 3 positions in 3-panel RIGHT mode', () => {
+        mount({ numOfPanels: 3, dividedColName: 'RIGHT' });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 3);
-        cy.get('[data-testid="panel-position-leftTop"]').should('contain', 'Open as left panel');
-        cy.get('[data-testid="panel-position-rightTop"]').should('contain', 'Open as top right panel');
-        cy.get('[data-testid="panel-position-rightBottom"]').should('contain', 'Open as bottom right panel');
+        cy.get('[data-testid="panel-position-leftTop"]').should('have.text', 'Open as left panel');
+        cy.get('[data-testid="panel-position-leftBottom"]').should('not.exist');
+        cy.get('[data-testid="panel-position-rightTop"]').should('have.text', 'Open as top right panel');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('exist');
     });
 
     it('renders no options in 1-panel mode', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 1,
-            },
-        });
+        mount({ numOfPanels: 1 });
+        cy.get('[data-testid="panel-position-select"] option').should('have.length', 0);
+    });
 
+    it('defaults to 1 panel when numOfPanels not provided', () => {
+        cy.mount(PanelPositionSelector, { props: { panelId: 'test' } });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 0);
     });
 
     it('updates options when panel-layout-changed event fires', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'autograding_results',
-                currentPosition: null,
-                numOfPanels: 2,
-            },
-        });
-
+        mount({ numOfPanels: 2 });
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 2);
-
         cy.window().then((win) => {
             win.dispatchEvent(new CustomEvent('panel-layout-changed', {
                 detail: { numOfPanelsEnabled: 4, dividedColName: 'LEFT' },
             }));
         });
-
         cy.get('[data-testid="panel-position-select"] option').should('have.length', 4);
     });
 
-    it('emits position-change with correct payload on selection', () => {
-        const onChange = cy.stub().as('position-change');
-
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'grading_rubric',
-                currentPosition: null,
-                numOfPanels: 4,
-                dividedColName: 'LEFT',
-                'onPosition-change': onChange,
-            },
+    it('updates divided column when panel-layout-changed event fires', () => {
+        mount({ numOfPanels: 3, dividedColName: 'LEFT' });
+        cy.get('[data-testid="panel-position-leftBottom"]').should('exist');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('not.exist');
+        cy.window().then((win) => {
+            win.dispatchEvent(new CustomEvent('panel-layout-changed', {
+                detail: { numOfPanelsEnabled: 3, dividedColName: 'RIGHT' },
+            }));
         });
-
-        cy.get('[data-testid="panel-position-select"]').then(($select) => {
-            const select = $select[0];
-            select.value = 'leftTop';
-            select.dispatchEvent(new Event('change', { bubbles: true }));
-        });
-        cy.get('@position-change').should('have.been.calledWith', {
-            panelId: 'grading_rubric',
-            position: 'leftTop',
-        });
+        cy.get('[data-testid="panel-position-leftBottom"]').should('not.exist');
+        cy.get('[data-testid="panel-position-rightBottom"]').should('exist');
     });
 
-    it('sets the select id from panelId', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'student_info',
-                currentPosition: null,
-                numOfPanels: 4,
-            },
+    it('does not crash on panel-layout-changed with undefined detail', () => {
+        mount({ numOfPanels: 4 });
+        cy.window().then((win) => {
+            win.dispatchEvent(new CustomEvent('panel-layout-changed', { detail: undefined }));
         });
-
-        cy.get('#student_info_select').should('be.visible');
+        cy.get('[data-testid="panel-position-select"] option').should('have.length', 4);
     });
 
-    it('has accessible data-testid on the select element', () => {
-        cy.mount(PanelPositionSelector, {
-            props: {
-                panelId: 'solution_ta_notes',
-                currentPosition: null,
-                numOfPanels: 4,
-            },
-        });
+    it('emits position-change with correct panelId and position', () => {
+        const spy = cy.spy().as('onChange');
+        mount({ panelId: 'grading_rubric', numOfPanels: 4, 'onPosition-change': spy });
+        triggerChange('leftTop');
+        cy.get('@onChange')
+            .should('have.been.calledOnce')
+            .and('have.been.calledWith', { panelId: 'grading_rubric', position: 'leftTop' });
+    });
 
-        cy.get('[data-testid="panel-position-select"]')
-            .should('have.attr', 'id', 'solution_ta_notes_select');
+    it('does not emit position-change on mount', () => {
+        const spy = cy.spy().as('onChange');
+        mount({ numOfPanels: 4, 'onPosition-change': spy });
+        cy.get('@onChange').should('not.have.been.called');
+    });
+
+    it('derives select id from panelId', () => {
+        mount({ panelId: 'autograding_results' });
+        cy.get('#autograding_results_select').should('exist');
+        mount({ panelId: 'notebook-view' });
+        cy.get('#notebook-view_select').should('exist');
+    });
+
+    it('sets :size to match option count', () => {
+        mount({ numOfPanels: 4 });
+        cy.get('[data-testid="panel-position-select"]').should('have.attr', 'size', '4');
+        mount({ numOfPanels: 2 });
+        cy.get('[data-testid="panel-position-select"]').should('have.attr', 'size', '2');
+        mount({ numOfPanels: 1 });
+        cy.get('[data-testid="panel-position-select"]').should('have.attr', 'size', '0');
+    });
+
+    it('treats numOfPanels=0 as single panel', () => {
+        mount({ numOfPanels: 0 });
+        cy.get('[data-testid="panel-position-select"] option').should('have.length', 0);
     });
 });
