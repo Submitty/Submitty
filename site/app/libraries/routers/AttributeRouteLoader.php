a2d3f31bda13eee315f57edd934e07eafd353f38
roye2 <07eeroy@gmail.com>
2026-06-19 20:40:36 -0400

    remove docblock and move ignore to baseline

--- site/app/libraries/routers/AttributeRouteLoader.php
+++ site/app/libraries/routers/AttributeRouteLoader.php
@@ -6,14 +6,7 @@ use Symfony\Component\Routing\Loader\AttributeClassLoader;
 use Symfony\Component\Routing\Route;
 
 class AttributeRouteLoader extends AttributeClassLoader {
-    /**
-     * @param Route $route
-     * @param \ReflectionClass $class
-     * @param \ReflectionMethod $method
-     * @param mixed $annot
-     * * @phpstan-ignore-next-line
-     */
-    protected function configureRoute(Route $route, \ReflectionClass $class, \ReflectionMethod $method, $annot) {
+    protected function configureRoute(Route $route, \ReflectionClass $class, \ReflectionMethod $method, object $annot) {
         $route->setDefault('_controller', $class->getName());
         $route->setDefault('_method', $method->getName());
     }
