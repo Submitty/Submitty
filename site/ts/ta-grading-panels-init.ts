ad909043c40057af8ee68373a44d8a2f251bae3f
NicholasCiuica <nick.ciuica@gmail.com>
2026-06-23 16:30:53 -0400

    more comments and cleanup

--- site/ts/ta-grading-panels-init.ts
+++ site/ts/ta-grading-panels-init.ts
@@ -333,6 +333,7 @@ $(() => {
     changeMobileView();
     initializeTaLayout();
 
+    // switch between standard and mobile layout on resize
     window.addEventListener('resize', () => {
         const wasMobileView = isMobileView;
         changeMobileView();
@@ -342,6 +343,7 @@ $(() => {
         }
     });
 
+    // resize/hide the student's name in the top-left corner on resize
     window.addEventListener('resize', () => {
         if ($('#silent-edit-id').length === 0) {
             return;
@@ -359,7 +361,7 @@ $(() => {
         const overlap_margin = 15;
         const overlapping = (panel_buttons_bbox.left - name_div_bbox.right) < overlap_margin;
         if (overlapping || taLayoutDet.isFullLeftColumnMode) {
-            $('#grading-panel-student-name').hide();
+            name_div.hide();
         }
     });
 
