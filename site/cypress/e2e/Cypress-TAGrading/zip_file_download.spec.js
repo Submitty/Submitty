6d567ab689c5f03618ecda0950deda9421554eb3
Matthew <133825715+mattLif@users.noreply.github.com>
2026-06-29 11:00:49 -0400

    fix zip_file_download.spec.js

--- site/cypress/e2e/Cypress-TAGrading/zip_file_download.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/zip_file_download.spec.js
@@ -3,12 +3,12 @@ describe('download zip file', () => {
     const downloadsFolder = Cypress.config('downloadsFolder');
     it('downloading and verifying the file', () => {
         cy.login();
-        cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=hG1b13ljpDjKu32&sort=id&direction=ASC']);
+        cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=L1LdxbPPN5GiLb4&sort=id&direction=ASC']);
         cy.get('body').type('{A}');
         cy.get('body').type('{O}'); // Solution/Notes
         cy.get('[data-testid="download-zip-file"]').should('contain', 'Download Zip File');
         cy.get('[data-testid="download-zip-file"]').click();
-        const filename = path.join(downloadsFolder, 'grading_homework_hG1b13ljpDjKu32_v1.zip');
+        const filename = path.join(downloadsFolder, 'grading_homework_L1LdxbPPN5GiLb4_v1.zip');
         cy.readFile(filename);
     });
 });
