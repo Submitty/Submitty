c81245e9ebc343ad019bedacf742c84a22d6867f
roye2 <07eeroy@gmail.com>
2026-07-01 14:26:53 -0400

    fix syntax error in url template

--- site/cypress/e2e/Cypress-Gradeable/limited_access_grader.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/limited_access_grader.spec.js
@@ -21,7 +21,7 @@ describe('Limited Access Grader Submission View Restriction', () => {
             }
         });
         cy.contains('td', 'boehmd').siblings('td').find('a[data-testid="grade-button"]').click();
-        cy.url().should('include', '/courses/${getCurrentSemester()}/sample');
+        cy.url().should('include', `/courses/${getCurrentSemester()}/sample`);
         cy.url().should('not.include', 'grading/grade');
         cy.get('[data-testid="popup-message"]').should('be.visible').and('contain', 'You do not have permission to view submissions until grading opens.');
     });
