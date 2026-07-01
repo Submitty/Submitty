62ef890bc60f078fcfea356b807cfd186588949d
Matthew <133825715+mattLif@users.noreply.github.com>
2026-07-01 16:53:23 -0400

    Revert "fix discussion_post.spec.js"
    This reverts commit 0324f8d1cb57d5be727595a0e76ad43100dd5303.

--- site/cypress/e2e/Cypress-Feature/discussion_post.spec.js
+++ site/cypress/e2e/Cypress-Feature/discussion_post.spec.js
@@ -6,7 +6,6 @@ describe('test for discussion post panel', () => {
         cy.visit(['sample', 'gradeable', 'grading_homework', 'update']);
         cy.get('[data-testid="yes-discussion"]').click();
         cy.get('[data-testid="yes-discussion"]').should('be.checked');
-        cy.get('[data-testid="save-status"]').should('contain', 'All Changes Saved', { timeout: 10000 });
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=L1LdxbPPN5GiLb4&sort=id&direction=ASC']);
         cy.get('[data-testid="discussion-browser-btn"]').type('{D}');
         cy.get('[data-testid="posts-list"]').should('contain', 'Discussion Posts');
@@ -15,7 +14,7 @@ describe('test for discussion post panel', () => {
         cy.visit(['sample', 'gradeable', 'grading_homework', 'update']);
         cy.get('[data-testid="discussion-thread-id"]').type('1,2,3');
         cy.get('[data-testid="discussion-thread-id"]').type('{enter}');
-        cy.get('[data-testid="save-status"]').should('contain', 'All Changes Saved', { timeout: 10000 });
+        cy.get('[data-testid="save-status"]').should('contain', 'All Changes Saved');
         cy.visit(['sample', 'gradeable', 'grading_homework', 'grading', 'grade?who_id=L1LdxbPPN5GiLb4&sort=id&direction=ASC']);
         cy.get('[data-testid="posts-list"]').should('contain', 'No posts for thread id: 1');
         cy.get('[data-testid="create-post-head"]').first().should('contain', '(2) Homework 1 print clarification');
