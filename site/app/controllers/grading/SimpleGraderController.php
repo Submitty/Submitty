9859eb4752a24fdefbefc8b71042241a7dcf462a
D'Andre Collins <dandrecollins07@gmail.com>
2026-07-02 16:55:15 -0400

    Fix bracket order

--- site/app/controllers/grading/SimpleGraderController.php
+++ site/app/controllers/grading/SimpleGraderController.php
@@ -268,15 +268,12 @@ class SimpleGraderController extends AbstractController {
         if ($sort === "id") {
             $sort_key = "u.user_id";
         }
-
         elseif ($sort === "first") {
             $sort_key = "coalesce(u.user_preferred_givenname, u.user_givenname)";
         }
-
         elseif ($sort === "last") {
             $sort_key = "coalesce(u.user_preferred_familyname, u.user_familyname)";
         }
-
         else {
             $sort_key = "u.registration_subsection";
         }
