ad08f617bd4ffe09299d040d9d47af977d643b76
roye2 <07eeroy@gmail.com>
2026-07-03 17:47:21 -0400

    update arg parsing

--- submitty_test.sh
+++ submitty_test.sh
@@ -16,46 +16,76 @@ DOCKER_RUN=(docker run --rm -u "$(id -u):$(id -g)" -e HOME=/tmp \
     --init \
     "$IMAGE_NAME")
 
-FIX=false
-ARGS=()
-for arg in "${@:2}"; do
-    if [ "$arg" == "--fix" ]; then
-        FIX=true
-    else
-        ARGS+=("$arg")
-    fi
-done
+# parse args with --fix options
+parse_args() {
+    FIX=false
+    ARGS=()
+    for arg in "$@"; do
+        if [ "$arg" == "--fix" ]; then
+            FIX=true
+        else
+            ARGS+=("$arg")
+        fi
+    done
+}
 
-# tests / linters
+# functions for tests and linters
 run_php_stan() {
-    "${DOCKER_RUN[@]}" composer run-script static-analysis -- "${ARGS[@]:-}"
+    parse_args "${@:2}"
+    if [ ${#ARGS[@]} -gt 0 ]; then
+        "${DOCKER_RUN[@]}" composer run-script static-analysis -- "${ARGS[@]}"
+    else
+        "${DOCKER_RUN[@]}" composer run-script static-analysis
+    fi
 }
 run_php_cs() {
+    parse_args "${@:2}"
     if $FIX; then
-        "${DOCKER_RUN[@]}" vendor/bin/phpcbf "${ARGS[@]:-}"
+        "${DOCKER_RUN[@]}" vendor/bin/phpcbf "${ARGS[@]}"
+    elif [ ${#ARGS[@]} -gt 0 ]; then
+        "${DOCKER_RUN[@]}" composer run-script lint -- "${ARGS[@]}"
     else
-        "${DOCKER_RUN[@]}" composer run-script lint -- "${ARGS[@]:-}"
+        "${DOCKER_RUN[@]}" composer run-script lint
     fi
 }
 run_js_es() {
+    parse_args "${@:2}"
     if $FIX; then
-        "${DOCKER_RUN[@]}" npm run eslint:fix -- "${ARGS[@]:-}"
+        if [ ${#ARGS[@]} -gt 0 ]; then
+            "${DOCKER_RUN[@]}" npm run eslint:fix -- "${ARGS[@]}"
+        else
+            "${DOCKER_RUN[@]}" npm run eslint:fix
+        fi
     else
-        "${DOCKER_RUN[@]}" npm run eslint -- "${ARGS[@]:-}"
+        if [ ${#ARGS[@]} -gt 0 ]; then
+            "${DOCKER_RUN[@]}" npm run eslint -- "${ARGS[@]}"
+        else
+            "${DOCKER_RUN[@]}" npm run eslint
+        fi
     fi
 }
 run_css_style() {
+    parse_args "${@:2}"
     if $FIX; then
-        "${DOCKER_RUN[@]}" npm run css-stylelint:fix -- "${ARGS[@]:-}"
+        if [ ${#ARGS[@]} -gt 0 ]; then
+            "${DOCKER_RUN[@]}" npm run css-stylelint:fix -- "${ARGS[@]}"
+        else
+            "${DOCKER_RUN[@]}" npm run css-stylelint:fix
+        fi
     else
-        "${DOCKER_RUN[@]}" npm run css-stylelint -- "${ARGS[@]:-}"
+        if [ ${#ARGS[@]} -gt 0 ]; then
+            "${DOCKER_RUN[@]}" npm run css-stylelint -- "${ARGS[@]}"
+        else
+            "${DOCKER_RUN[@]}" npm run css-stylelint
+        fi
     fi
 }
 run_php_unit() {
-    "${DOCKER_RUN[@]}" php vendor/bin/phpunit "${ARGS[@]:-}"
+    parse_args "${@:2}"
+    "${DOCKER_RUN[@]}" php vendor/bin/phpunit "${ARGS[@]}"
 }
 
-# process arguments
+# process input arguments
 if [ -z "${1:-}" ] || [ "$1" == "help" ]; then
     echo "
           phpstan : php static analysis [option: --memory-limit 4G, --generate-baseline ...]
@@ -66,18 +96,18 @@ if [ -z "${1:-}" ] || [ "$1" == "help" ]; then
           css-lint: css-stylelint [option: --fix]
           "
 elif [ "$1" == "phpstan" ]; then
-    run_php_stan
+    run_php_stan "$@"
 elif [ "$1" == "phpcs" ]; then
-    run_php_cs
+    run_php_cs "$@"
 elif [ "$1" == "php-lint" ]; then
-    run_php_cs
-    run_php_stan
+    run_php_cs "$@"
+    run_php_stan "$@"
 elif [ "$1" == "php-unit" ]; then
-    run_php_unit
+    run_php_unit "$@"
 elif [ "$1" == "js-lint" ]; then
-    run_js_es
+    run_js_es "$@"
 elif [ "$1" == "css-lint" ]; then
-    run_css_style
+    run_css_style "$@"
 else
     echo "Unknown test type: $1
         use phpstan, phpcs, php-lint, php-unit, js-lint, css-lint
