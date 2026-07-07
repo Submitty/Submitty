49875ad484e5e75d022862ec0cc0ec95ba9060e2
NicholasCiuica <nick.ciuica@gmail.com>
2026-07-07 16:23:33 -0400

    standardizing my use of singlequotes

--- site/app/views/superuser/GradeablesView.php
+++ site/app/views/superuser/GradeablesView.php
@@ -34,7 +34,7 @@ class GradeablesView extends AbstractView {
         }
 
         $this->core->getOutput()->addInternalCss("navigation.css");
-        $this->core->getOutput()->addInternalModuleJs("sort-table-by-column.js");
+        $this->core->getOutput()->addInternalModuleJs('sort-table-by-column.js');
         $this->core->getOutput()->enableMobileViewport();
         $this->core->getOutput()->addBreadcrumb("Pending Gradeables");
 
