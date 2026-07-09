153ba6aa2f204dbb94bfcaf496b71e6b6e525054
Ansh Jindal <jndl.ansh@gmail.com>
2026-07-09 20:10:36 +0530

    Define and declare the new vue component

---
+++ site/vue/src/components/ta_grading/MarkSelector.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+    markId: number;
+    componentId: number;
+    order: number;
+    isChecked: boolean;
+}>();
+
+// PR #1: routed through events bridge to window.onToggleMarkById.
+// PR #3: RubricComponent catches @toggle-mark directly, no bridge needed.
+const emit = defineEmits<{
+    'toggle-mark': [data: { componentId: number; markId: number }];
+}>();
+
+function handleClick(event: MouseEvent) {
+    event.stopPropagation();
+    emit('toggle-mark', { componentId: props.componentId, markId: props.markId });
+}
+</script>
+
+<template>
+  <span
+    class="mark-selector-container"
+    :data-mark_id="markId"
+    @click="handleClick"
+  >
+    <span
+      :class="['mark-selector', 'col-no-gutters', { 'mark-selected': isChecked }]"
+      :data-mark_id="markId"
+    >
+      {{ order }}
+    </span>
+  </span>
+</template>
