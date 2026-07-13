381329b6432d50d68e1daec9714b3bd256c68c26
NicholasCiuica <nick.ciuica@gmail.com>
2026-07-13 12:18:30 -0400

    hacky css pr
    fixes alternating row colors for gradeable tables, and reuses existing row counter css to add counters to these tables (the hacky part, because of code reuse). A better solution would be to unify these tables styles in one file, but that would require its own pr because table css is distributed and messy.

--- site/ts/ta-grading-cookies.ts
+++ site/ts/ta-grading-cookies.ts
@@ -9,69 +9,11 @@ declare global {
         changeInquiry: () => void;
         changeSortOrder: () => void;
         changeAnon: () => void;
-        updateSimpleGradingRowNumbersAndColors: () => void;
-        updateElectronicGradingRowNumbersAndColors: () => void;
     }
 }
 
 const coursePath = document.body.dataset.coursePath ?? '';
 const cookieArguments = { path: coursePath, expires: 365 };
-
-function updateSimpleGradingRowNumbersAndColors() {
-    $('tbody[id^="section-"]').each(function () {
-        let rowNumber = 1;
-
-        $(this).find('tr[data-student="simple-grade-active"], tr[data-student="simple-grade-withdrawn"]').each(function () {
-            if ($(this).is(':visible')) {
-                $(this).find('td:first').text(rowNumber);
-                const color = rowNumber % 2 === 1 ? 'var(--default-white)' : 'var(--standard-hover-light-gray)';
-
-                $(this).css('background-color', `${color} !important`);
-                $(this).find('td').each(function () {
-                    if ($(this).hasClass('simple-full-credit')) {
-                        $(this).css('background-color', 'var(--simple-full-credit-dark-blue) !important');
-                    }
-                    else if ($(this).hasClass('simple-half-credit')) {
-                        $(this).css('background-color', 'var(--simple-half-credit-light-blue) !important');
-                    }
-                    else if ($(this).hasClass('simple-save-error')) {
-                        $(this).css('background-color', 'var(--simple-save-error-red) !important');
-                    }
-                    else {
-                        $(this).css('background-color', `${color} !important`);
-                    }
-                });
-                rowNumber++;
-            }
-            else {
-                $(this).css('background-color', 'var(--default-white) !important');
-                $(this).find('td').css('background-color', 'var(--default-white) !important');
-            }
-        });
-    });
-}
-
-function updateElectronicGradingRowNumbersAndColors() {
-    $('tbody.details-content').each(function () {
-        let rowNumber = 1;
-
-        $(this).find('tr[data-student="electronic-grade-active"], tr[data-student="electronic-grade-withdrawn"]').each(function () {
-            if ($(this).is(':visible')) {
-                $(this).find('td:first').text(rowNumber);
-                if (rowNumber % 2 === 1) {
-                    $(this).css('background-color', 'var(--default-white) !important');
-                }
-                else {
-                    $(this).css('background-color', 'var(--standard-hover-light-gray) !important');
-                }
-                rowNumber++;
-            }
-            else {
-                $(this).css('background-color', 'var(--default-white) !important');
-            }
-        });
-    });
-}
 
 window.filter_overriden_grades = () => {
     const override_status = window.Cookies.get('include_grade_override') ?? 'omit';
@@ -109,10 +51,6 @@ window.filter_withdrawn_students = () => {
 
     // Remove table-striped to prevent CSS conflicts with JS-set colors
     $('table').removeClass('table-striped');
-
-    // Update row numbers and colors after filtering
-    updateSimpleGradingRowNumbersAndColors();
-    updateElectronicGradingRowNumbersAndColors();
 };
 
 window.changeSections = () => {
@@ -144,6 +82,3 @@ window.changeAnon = () => {
     window.Cookies.set('anon_mode', $('#toggle-anon-students').is(':checked') ? 'on' : 'off', cookieArguments);
     location.reload();
 };
-
-window.updateSimpleGradingRowNumbersAndColors = updateSimpleGradingRowNumbersAndColors;
-window.updateElectronicGradingRowNumbersAndColors = updateElectronicGradingRowNumbersAndColors;
