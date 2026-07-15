2af32b1bc339be9d7319858f47c1b6235e7c835a
NicholasCiuica <nick.ciuica@gmail.com>
2026-07-15 12:44:26 -0400

    remove download student pdf

--- site/app/views/PDFView.php
+++ site/app/views/PDFView.php
@@ -42,70 +42,69 @@ class PDFView extends AbstractView {
             'grader_id' => $this->core->getUser()->getId(),
             'filename' => $file_name,
             'file_path' => $file_path,
-            'annotation_jsons' => json_encode($annotation_jsons),
-            'student_popup' => $is_student,
-            'can_download' => !$is_peer_grader,
+            // 'annotation_jsons' => json_encode($annotation_jsons),
+            // 'student_popup' => $is_student,
             'page_num' => $page_num,
             'pdf_url_base' => $pdf_url,
             'localcss' => $localcss,
             'localjs' => $localjs,
-            'csrfToken' => $this->core->getCsrfToken(),
-            'student_pdf_download_url' => $this->core->buildCourseUrl(['gradeable', $gradeable_id, 'download_pdf']),
-            'anon_path' => $anon_path,
-            'download_path' => $download_path
+            // 'csrfToken' => $this->core->getCsrfToken(),
+            // 'student_pdf_download_url' => $this->core->buildCourseUrl(['gradeable', $gradeable_id, 'download_pdf']),
+            // 'anon_path' => $anon_path,
+            // 'download_path' => $download_path
         ]);
     }
 
     /**
      * adds to our buffer a twig output of either student view or grader view.
      */
-    public function downloadPDFEmbedded(
-        string $gradeable_id,
-        string $user_id,
-        string $file_name,
-        string $file_path,
-        array $annotation_jsons,
-        bool $rerender_annotated_pdf,
-        bool $is_student,
-        int $page_num,
-        bool $jquery
-    ): void {
-        $this->core->getOutput()->useFooter(false);
-        $this->core->getOutput()->useHeader(false);
-        $pdf_url = $this->core->buildCourseUrl(['gradeable', $gradeable_id, 'encode_pdf']);
+    // public function downloadPDFEmbedded(
+    //     string $gradeable_id,
+    //     string $user_id,
+    //     string $file_name,
+    //     string $file_path,
+    //     array $annotation_jsons,
+    //     bool $rerender_annotated_pdf,
+    //     bool $is_student,
+    //     int $page_num,
+    //     bool $jquery
+    // ): void {
+    //     $this->core->getOutput()->useFooter(false);
+    //     $this->core->getOutput()->useHeader(false);
+    //     $pdf_url = $this->core->buildCourseUrl(['gradeable', $gradeable_id, 'encode_pdf']);
 
-        $localcss = [];
-        $localcss[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'pdf_embedded.css'), 'css');
-        $localcss[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf_viewer.css'), 'vendor');
+    //     $localcss = [];
+    //     $localcss[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'pdf_embedded.css'), 'css');
+    //     $localcss[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf_viewer.css'), 'vendor');
 
-        $localjs = [];
+    //     $localjs = [];
 
-        //This jquery file should not need to be added here as jquery should already be in the header on any page
-        if ($jquery) {
-            $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('jquery', 'jquery.min.js'), 'vendor');
-        }
+    //     //This jquery file should not need to be added here as jquery should already be in the header on any page
+    //     if ($jquery) {
+    //         $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('jquery', 'jquery.min.js'), 'vendor');
+    //     }
 
-        $this->core->getOutput()->addModuleJs($this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'pdfjs-shim.js'), 'js'));
-        $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf.min.mjs'), 'vendor');
-        $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf_viewer.mjs'), 'vendor');
-        $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf.worker.min.mjs'), 'vendor');
-        $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'PDFAnnotateEmbedded.js'), 'js');
-        $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('jspdf', 'jspdf.min.js'), 'vendor');
+    //     $this->core->getOutput()->addModuleJs($this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'pdfjs-shim.js'), 'js'));
+    //     $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf.min.mjs'), 'vendor');
+    //     $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf_viewer.mjs'), 'vendor');
+    //     $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdfjs', 'pdf.worker.min.mjs'), 'vendor');
+    //     $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('pdf', 'PDFAnnotateEmbedded.js'), 'js');
+    //     $localjs[] = $this->core->getOutput()->timestampResource(FileUtils::joinPaths('jspdf', 'jspdf.min.js'), 'vendor');
 
-        $this->core->getOutput()->renderTwigOutput('grading/electronic/PDFAnnotationEmbedded.twig', [
-            'gradeable_id' => $gradeable_id,
-            'user_id' => $user_id,
-            'grader_id' => $this->core->getUser()->getId(),
-            'filename' => $file_name,
-            'file_path' => $file_path,
-            'annotation_jsons' => json_encode($annotation_jsons),
-            'rerender_annotated_pdf' => $rerender_annotated_pdf,
-            'student_download' => $is_student,
-            'page_num' => $page_num,
-            'pdf_url_base' => $pdf_url,
-            'localcss' => $localcss,
-            'localjs' => $localjs,
-            'csrfToken' => $this->core->getCsrfToken()
-        ]);
-    }
+    //     $this->core->getOutput()->renderTwigOutput('grading/electronic/PDFAnnotationEmbedded.twig', [
+    //         'gradeable_id' => $gradeable_id,
+    //         'user_id' => $user_id,
+    //         'grader_id' => $this->core->getUser()->getId(),
+    //         'filename' => $file_name,
+    //         'file_path' => $file_path,
+    //         'annotation_jsons' => json_encode($annotation_jsons),
+    //         'rerender_annotated_pdf' => $rerender_annotated_pdf,
+    //         'student_download' => $is_student,
+    //         'page_num' => $page_num,
+    //         'pdf_url_base' => $pdf_url,
+    //         'localcss' => $localcss,
+    //         'localjs' => $localjs,
+    //         'csrfToken' => $this->core->getCsrfToken()
+    //     ]);
+    // }
 }
