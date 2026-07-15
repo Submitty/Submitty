097cabba41875fb5b6062ec28a76ef6f32ac0cdf
D'Andre Collins <dandrecollins07@gmail.com>
2026-07-15 10:01:16 -0400

    Fix PHP file newline

--- site/app/libraries/database/DatabaseQueries.php
+++ site/app/libraries/database/DatabaseQueries.php
@@ -9772,7 +9772,7 @@ WHERE current_state IN
                 return '';
             }
             // Use simplified expression for empty keymap
-            if (empty($key_map)) {
+            if ($key_map === []) {
                 return 'ORDER BY ' . implode(',', $sort_keys);
             }
             return 'ORDER BY ' . implode(
