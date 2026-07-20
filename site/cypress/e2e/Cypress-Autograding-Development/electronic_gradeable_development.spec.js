a10924749a23bd9f6677c27ce5a3f244c0674b7f
Pierce05 <khandelwalpurvanshi@gmail.com>
2026-07-20 19:02:02 +0530

    Remove unused scheme submissions array per review feedback

--- site/cypress/e2e/Cypress-Autograding-Development/electronic_gradeable_development.spec.js
+++ site/cypress/e2e/Cypress-Autograding-Development/electronic_gradeable_development.spec.js
@@ -13,11 +13,6 @@ describe('Test the development course gradeables', { env: { course: 'development
             { submissionFiles: { 1: ['extraLinesAtEnd.zip'] }, expected: [2, 3, 2, 2, 2, 2, 0], full: [2, 3, 4, 4, 4, 4, 4] },
         ];
 
-        const scheme = [
-            { submissionFiles: { 1: ['main.rkt'] }, expected: [5], full: [5] },
-            { submissionFiles: { 1: ['goodbye.rkt'] }, expected: [0], full: [5] },
-        ];
-
         const gradeables = [
             { name: 'cpp_cats', submissions: cppCats },
             { name: 'rust_hello_world', submissions: rust },
