3edb4618dc1642d794981eeb1fadbc8d1234f6fd
Ansh Jindal <jndl.ansh@gmail.com>
2026-07-21 23:58:00 +0530

    Use popup.vue + remove base props in cypress CT + remove extra close button + restore functionality

--- site/vue/src/components/GradeableMessage.vue
+++ site/vue/src/components/GradeableMessage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, nextTick, onMounted } from 'vue';
+import { ref } from 'vue';
+import Popup from './Popup.vue';
 
 const props = defineProps<{
     storageKey: string;
@@ -10,6 +11,7 @@ const props = defineProps<{
 const emit = defineEmits<{
     cancel: [];
 }>();
+
 const isGrader = props.userGroup === 2 || props.userGroup === 3;
 const isPeer = props.userGroup === 4;
 const isInstructor = props.userGroup === 1;
@@ -24,21 +26,9 @@ else {
     visible.value = true;
 }
 
-const popupRef = ref<HTMLElement | null>(null);
-
-onMounted(async () => {
-    if (visible.value) {
-        await nextTick();
-        popupRef.value?.focus();
-    }
-});
-
 function showReview(): void {
     canAgree.value = false;
     visible.value = true;
-    void nextTick(() => {
-        popupRef.value?.focus();
-    });
 }
 
 function agree(): void {
@@ -68,125 +58,105 @@ function close(): void {
     >
       Responsibilities as a Grader
     </button>
-    <div
-      v-if="visible"
-      id="gradeable-message-popup"
-      ref="popupRef"
-      class="popup-form"
-      tabindex="-1"
-      @keydown.escape="close"
+    <Popup
+      :visible="visible"
+      title="Your Responsibility as a Grader"
+      @toggle="close"
     >
-      <div
-        class="popup-box"
-        @click="close"
-      >
+      <div class="gradeable-message-content">
         <div
-          class="popup-window"
-          data-testid="popup-window"
-          @click.stop
+          v-if="isGrader"
+          class="content gradeable_message"
         >
-          <div class="form-title">
-            <h1>Your Responsibility as a Grader</h1>
+          <p>
+            You will be reviewing materials submitted by students and these materials are considered confidential.<br>
+            The author of this material is entitled to copyright protection. As such,
+          </p>
+          <ul>
+            <li>You may not use or share any of these materials or ideas without explicit permission from the author.</li>
+            <li>
+              You may not retain this material.<br>
+              You must delete/destroy any copies of this material after your review and grading of the work is complete.
+            </li>
+            <li v-if="blindStatus === 3">
+              Your instructor has configured the grading for this assignment to be <em>blinded</em>.<br>
+              You should not attempt to discover the identities of the students.<br>
+              However, their identities may be unintentionally or unavoidably revealed.<br>
+              Please respect your students' privacy.
+            </li>
+          </ul>
+          <p>
+            Please communicate with the instructor if you realize you have a conflict of interest with a student that you have been assigned to grade.
+          </p>
+        </div>
+        <div
+          v-else-if="isPeer"
+          class="content gradeable_message"
+        >
+          <p>
+            You will be reviewing materials submitted by your classmates and these materials are considered confidential.<br>
+            Your classmate, the author of this material, is entitled to copyright protection. As such,
+          </p>
+          <ul>
+            <li>You may not use or share any of these materials or ideas without explicit permission from the author.</li>
+            <li>You may not retain this material -- you must delete/destroy any copies of this material after your review and grading of the work is complete.</li>
+            <li v-if="blindStatus === 3">
+              Your instructor has configured the grading for this assignment to be double-blind.<br>
+              You should not attempt to discover the identities of your classmates.<br>
+              However, the identities of the classmates may be unintentionally or unavoidably revealed.<br>
+              Please respect your classmates' privacy.
+            </li>
+            <li v-else-if="blindStatus === 2">
+              Your instructor has configured the grading for this assignment to be single-blind.
+            </li>
+            <li v-else>
+              Your instructor has configured the grading for this assignment to be unblinded.
+            </li>
+          </ul>
+          <p>
+            Please communicate with your instructor if you realize you have a conflict of interest with a peer that you have been assigned to grade.
+          </p>
+        </div>
+        <div class="form-buttons">
+          <div class="form-button-container">
+            <template v-if="canAgree">
+              <button
+                id="cancel-button"
+                class="btn btn-default close-button"
+                type="button"
+                @click="cancel"
+              >
+                Cancel
+              </button>
+              <button
+                id="agree-button"
+                class="btn btn-primary"
+                type="button"
+                data-testid="agree-popup-btn"
+                @click="agree"
+              >
+                Agree
+              </button>
+            </template>
             <button
+              v-else
+              id="close-hidden-button"
               class="btn btn-default close-button"
-              data-testid="close-button"
-              tabindex="-1"
               type="button"
+              data-testid="close-hidden-button"
               @click="close"
             >
               Close
             </button>
           </div>
-          <div class="form-body">
-            <div
-              v-if="isGrader"
-              class="content gradeable_message"
-            >
-              <p>
-                You will be reviewing materials submitted by students and these materials are considered confidential.<br>
-                The author of this material is entitled to copyright protection. As such,
-              </p>
-              <ul>
-                <li>You may not use or share any of these materials or ideas without explicit permission from the author.</li>
-                <li>
-                  You may not retain this material.<br>
-                  You must delete/destroy any copies of this material after your review and grading of the work is complete.
-                </li>
-                <li v-if="blindStatus === 3">
-                  Your instructor has configured the grading for this assignment to be <em>blinded</em>.<br>
-                  You should not attempt to discover the identities of the students.<br>
-                  However, their identities may be unintentionally or unavoidably revealed.<br>
-                  Please respect the students' privacy.
-                </li>
-              </ul>
-              <p>
-                Please communicate with the instructor if you realize you have a conflict of interest with a student that you have been assigned to grade.
-              </p>
-            </div>
-            <div
-              v-else-if="isPeer"
-              class="content gradeable_message"
-            >
-              <p>
-                You will be reviewing materials submitted by your classmates and these materials are considered confidential.<br>
-                Your classmate, the author of this material, is entitled to copyright protection. As such,
-              </p>
-              <ul>
-                <li>You may not use or share any of these materials or ideas without explicit permission from the author.</li>
-                <li>You may not retain this material -- you must delete/destroy any copies of this material after your review and grading of the work is complete.</li>
-                <li v-if="blindStatus === 3">
-                  Your instructor has configured the grading for this assignment to be double-blind.<br>
-                  You should not attempt to discover the identities of your classmates.<br>
-                  However, the identities of the classmates may be unintentionally or unavoidably revealed.<br>
-                  Please respect your classmates' privacy.
-                </li>
-                <li v-else-if="blindStatus === 2">
-                  Your instructor has configured the grading for this assignment to be single-blind.
-                </li>
-                <li v-else>
-                  Your instructor has configured the grading for this assignment to be unblinded.
-                </li>
-              </ul>
-              <p>
-                Please communicate with your instructor if you realize you have a conflict of interest with a peer that you have been assigned to grade.
-              </p>
-            </div>
-            <div
-              class="form-buttons"
-            >
-              <div class="form-button-container">
-                <button
-                  v-if="canAgree"
-                  id="cancel-button"
-                  class="btn btn-default close-button"
-                  type="button"
-                  @click="cancel"
-                >
-                  Cancel
-                </button>
-                <button
-                  v-if="canAgree"
-                  id="agree-button"
-                  class="btn btn-primary"
-                  type="button"
-                  data-testid="agree-popup-btn"
-                  @click="agree"
-                >
-                  Agree
-                </button>
-                <button
-                  class="btn btn-default close-button"
-                  type="button"
-                  data-testid="close-hidden-button"
-                  @click="close"
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-    </div>
+    </Popup>
   </div>
 </template>
+
+<style scoped>
+:deep(.popup-window > .form-buttons) {
+    display: none;
+}
+</style>
