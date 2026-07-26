234c510120d44e688473c6c10a5bfb2723ac56fb
Ansh Jindal <jndl.ansh@gmail.com>
2026-07-27 00:15:05 +0530

    Use defaultProps and add isChecked prop back

--- site/cypress/component/MarkSelector.cy.js
+++ site/cypress/component/MarkSelector.cy.js
@@ -2,49 +2,55 @@ import MarkSelector from '../../vue/src/components/ta_grading/MarkSelector.vue';
 import { mountWithEmitSpy } from '../support/component_test_utils.js';
 
 describe('MarkSelector', () => {
-    const base = { markId: 5, componentId: 1, order: 2, editMarksEnabled: false };
+    const defaultProps = () => ({
+        markId: 5,
+        componentId: 1,
+        order: 2,
+        isChecked: false,
+        editMarksEnabled: false,
+    });
 
     it('renders the mark order number', () => {
-        cy.mount(MarkSelector, { props: { ...base, isChecked: false } });
+        cy.mount(MarkSelector, { props: defaultProps() });
         cy.get('[data-testid="mark-selector"]').should('contain', '2');
     });
 
     it('shows mark-selected class when isChecked is true', () => {
-        cy.mount(MarkSelector, { props: { ...base, isChecked: true } });
+        cy.mount(MarkSelector, { props: { ...defaultProps(), isChecked: true } });
         cy.get('[data-testid="mark-selector"] .mark-selector').should('have.class', 'mark-selected');
     });
 
     it('hides mark-selected class when isChecked is false', () => {
-        cy.mount(MarkSelector, { props: { ...base, isChecked: false } });
+        cy.mount(MarkSelector, { props: defaultProps() });
         cy.get('[data-testid="mark-selector"] .mark-selector').should('not.have.class', 'mark-selected');
     });
 
     it('sets data-mark_id for DOM-based id lookup by legacy code', () => {
-        cy.mount(MarkSelector, { props: { ...base, markId: 42, isChecked: false } });
+        cy.mount(MarkSelector, { props: { ...defaultProps(), markId: 42 } });
         cy.get('[data-testid="mark-selector"]').should('have.attr', 'data-mark_id', '42');
     });
 
     it('emits toggle-mark with componentId and markId on click', () => {
-        mountWithEmitSpy(MarkSelector, 'toggleMark', { ...base, isChecked: false }, 'toggleMark');
+        mountWithEmitSpy(MarkSelector, 'toggleMark', defaultProps(), 'toggleMark');
         cy.get('[data-testid="mark-selector"]').click();
         cy.get('@toggleMark').should('have.been.calledWith', { componentId: 1, markId: 5 });
     });
 
     it('stops propagation so parent onclick does not double-fire', () => {
         const onToggleMark = cy.stub().as('onToggleMark');
-        cy.mount(MarkSelector, { props: { ...base, isChecked: false, onToggleMark } });
+        cy.mount(MarkSelector, { props: { ...defaultProps(), onToggleMark } });
         cy.get('[data-testid="mark-selector"]').click();
         cy.get('@onToggleMark').should('have.callCount', 1);
     });
 
     it('does not emit toggle-mark when editMarksEnabled is true', () => {
-        mountWithEmitSpy(MarkSelector, 'toggleMark', { ...base, editMarksEnabled: true }, 'toggleMark');
+        mountWithEmitSpy(MarkSelector, 'toggleMark', { ...defaultProps(), editMarksEnabled: true }, 'toggleMark');
         cy.get('[data-testid="mark-selector"]').click();
         cy.get('@toggleMark').should('not.have.been.called');
     });
 
     it('handles markId 0 used for custom marks', () => {
-        cy.mount(MarkSelector, { props: { ...base, markId: 0, isChecked: true } });
+        cy.mount(MarkSelector, { props: { ...defaultProps(), markId: 0, isChecked: true } });
         cy.get('[data-testid="mark-selector"]').should('have.attr', 'data-mark_id', '0');
         cy.get('[data-testid="mark-selector"] .mark-selector').should('have.class', 'mark-selected');
     });
