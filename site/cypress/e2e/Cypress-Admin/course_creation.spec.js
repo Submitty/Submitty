61fa893e1c1af57eef51be20801b5aa7db611b60
NicholasCiuica <183861504+NicholasCiuica@users.noreply.github.com>
2026-07-29 16:44:06 -0400

    invalid course code tests

--- site/cypress/e2e/Cypress-Admin/course_creation.spec.js
+++ site/cypress/e2e/Cypress-Admin/course_creation.spec.js
@@ -4,10 +4,41 @@ describe('Test cases revolving around course creation through UI', () => {
     });
 
     it('Should see all default courses on home page', () => {
-        // list of all default courses that should be on home page for instructor 
+        // List of all default courses that should be on home page for instructor
         const default_courses = ['blank', 'development', 'sample', 'tutorial', 'testing', 'archived'];
         for (const default_course of default_courses) {
             cy.get(`[data-testid=${default_course}-button]`).should('exist');
         }
+    });
+
+    it('Should fail to make a course when using an invalid course code', () => {
+        // Requirements for course code: only lowercase letters (a-z), digits (0-9), and the underscore character.
+        const invalid_course_codes = ['UPPER', 'special!@#', 'sp a ces', 'hyph-en', '_punc.,', 'other%`~'];
+        cy.visit('/home/courses/new');
+        for (const invalid_course_code of invalid_course_codes) {
+            cy.get('[data-testid="course-title-input"]').type(invalid_course_code);
+            cy.get('[data-testid="course-group-select"]').select('sample_tas_www');
+            cy.get('[data-testid="create-course-submit"]').click();
+            cy.get('[data-testid="popup-message"]').should('contain', 'The course code must contain only lowercase letters (a-z), digits (0-9), and the underscore character.');
+        }
+        const course_code = `${Math.random().toString(36).substring(2, 8)}_course_creation_test`;
+        cy.get('[data-testid="course-title-input"]').type(course_code);
+        cy.get('[data-testid="course-group-select"]').select('sample_tas_www');
+        cy.get('[data-testid="create-course-submit"]').click();
+        // eslint-disable-next-line no-restricted-syntax
+        cy.waitAndReloadUntil(() => {
+            return cy.get('body').then(($body) => {
+                return $body.find(`[data-testid="${course_code}-button"]`).length > 0;
+            });
+        }, 5000, 100);
+    });
+
+    it('Should see course on home page once it is created', () => {
+    });
+
+    it('Archived course should be moved to archive section on home page', () => {
+    });
+
+    it('Once removed from course, instructor should not see that course', () => {
     });
 });
