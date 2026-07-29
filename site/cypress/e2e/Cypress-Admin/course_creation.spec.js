708da3ffb599dcda95fceabdae724a63e8678f92
NicholasCiuica <183861504+NicholasCiuica@users.noreply.github.com>
2026-07-29 17:04:50 -0400

    archived course test start

--- site/cypress/e2e/Cypress-Admin/course_creation.spec.js
+++ site/cypress/e2e/Cypress-Admin/course_creation.spec.js
@@ -1,6 +1,9 @@
 describe('Test cases revolving around course creation through UI', () => {
+    let valid_course_code;
+
     beforeEach(() => {
         cy.login();
+        valid_course_code = `${Math.random().toString(36).substring(2, 8)}_course_creation_test`;
     });
 
     it('Should see all default courses on home page', () => {
@@ -21,14 +24,14 @@ describe('Test cases revolving around course creation through UI', () => {
             cy.get('[data-testid="create-course-submit"]').click();
             cy.get('[data-testid="popup-message"]').should('contain', 'The course code must contain only lowercase letters (a-z), digits (0-9), and the underscore character.');
         }
-        const course_code = `${Math.random().toString(36).substring(2, 8)}_course_creation_test`;
-        cy.get('[data-testid="course-title-input"]').type(course_code);
+
+        cy.get('[data-testid="course-title-input"]').type(valid_course_code);
         cy.get('[data-testid="course-group-select"]').select('sample_tas_www');
         cy.get('[data-testid="create-course-submit"]').click();
         // eslint-disable-next-line no-restricted-syntax
         cy.waitAndReloadUntil(() => {
             return cy.get('body').then(($body) => {
-                return $body.find(`[data-testid="${course_code}-button"]`).length > 0;
+                return $body.find(`[data-testid="${valid_course_code}-button"]`).length > 0;
             });
         }, 5000, 100);
     });
@@ -37,6 +40,11 @@ describe('Test cases revolving around course creation through UI', () => {
     });
 
     it('Archived course should be moved to archive section on home page', () => {
+        cy.visit(`/courses/f26/${valid_course_code}/config`);
+        cy.get('[data-testid="course-archive"]').click();
+        cy.visit('/home');
+        // new course should be within archived section
+        // cy.get(`[data-testid=${valid_course_code}-button]`).should('exist');
     });
 
     it('Once removed from course, instructor should not see that course', () => {
