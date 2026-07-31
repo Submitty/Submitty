df70067cd7d11477ce01cc1a188db9b87c99dcec
Ansh Jindal <jndl.ansh@gmail.com>
2026-07-31 13:43:29 +0530

    Remove ta-grading-rubric-conflict.js references from PHP contoller and view + delete ta-grading-rubric-conficts.ts

--- site/app/controllers/admin/AdminGradeableController.php
+++ site/app/controllers/admin/AdminGradeableController.php
@@ -541,7 +541,6 @@ class AdminGradeableController extends AbstractController {
         $hasCustomMarks =  $this->core->getQueries()->getHasCustomMarks($gradeable->getId());
         if ($gradeable->getType() === GradeableType::ELECTRONIC_FILE) {
             $this->core->getOutput()->addVendorJs(FileUtils::joinPaths('twigjs', 'twig.min.js'));
-            $this->core->getOutput()->addInternalModuleJs('ta-grading-rubric-conflict.js');
             $this->core->getOutput()->addInternalModuleJs('ta-grading-rubric.js');
             $this->core->getOutput()->addInternalJs('gradeable.js');
             $this->core->getOutput()->addInternalJs('gradeable-config-utils.js');
