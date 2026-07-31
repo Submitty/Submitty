7193d49718b75a1ca1c0cb76a0079df470e9f0f1
Matthew <133825715+mattLif@users.noreply.github.com>
2026-07-31 11:54:12 -0400

    ad test for peer restrictions

---
+++ site/cypress/e2e/Cypress-TAGrading/peer_restrictions.spec.js
@@ -0,0 +1,56 @@
+describe('Test that grading restrictions on peer graders work', () => {
+    it('Should save and apply peer grading panel view options', () => {
+        const panelOptions = [
+            {
+                setting: '[data-testid="peer-autograding-panel-option"]',
+                gradingButton: '[data-testid="autograding-results-btn"]',
+            },
+            {
+                setting: '[data-testid="peer-rubric-panel-option"]',
+                gradingButton: '[data-testid="grading-rubric-btn"]',
+            },
+            {
+                setting: '[data-testid="peer-files-panel-option"]',
+                gradingButton: '[data-testid="submission-browser-btn"]',
+            },
+            {
+                setting: '[data-testid="peer-solutions-panel-option"]',
+                gradingButton: '[data-testid="solution-ta-notes-btn"]',
+            },
+        ];
+        cy.login('instructor');
+        cy.visit(['sample', 'gradeable', 'grading_homework', 'update']);
+        panelOptions.forEach(({ setting }) => {
+            cy.get(setting, { timeout: 10000 }).should('be.checked').uncheck();
+        });
+        cy.get('[data-testid="save-status"]', { timeout: 10000 }).should('contain.text', 'All Changes Saved');
+        cy.reload();
+        panelOptions.forEach(({ setting }) => {
+            cy.get(setting, { timeout: 10000 }).should('not.be.checked');
+        });
+        cy.logout();
+        cy.login('bitdiddle');
+        cy.visit([
+            'sample',
+            'gradeable',
+            'grading_homework',
+            'grading',
+            'details',
+        ]);
+        cy.get('[data-testid="grade-button"]', { timeout: 10000 }).filter(':visible').first().click();
+        panelOptions.forEach(({ gradingButton }) => {
+            cy.get(gradingButton, { timeout: 10000 }).should('not.exist');
+        });
+        cy.logout();
+        cy.login('instructor');
+        cy.visit(['sample', 'gradeable', 'grading_homework', 'update']);
+        panelOptions.forEach(({ setting }) => {
+            cy.get(setting, { timeout: 10000 }).should('not.be.checked').check();
+        });
+        cy.get('[data-testid="save-status"]', { timeout: 10000 }).should('contain.text', 'All Changes Saved');
+        cy.reload();
+        panelOptions.forEach(({ setting }) => {
+            cy.get(setting, { timeout: 10000 }).should('be.checked');
+        });
+    });
+});
