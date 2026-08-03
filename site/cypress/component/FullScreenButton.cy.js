bc7da5797c6632d04aa1a3fa4ccd458c270504d2
dagemcn <d.eric.mcnitt@gmail.com>
2026-08-03 16:31:06 -0400

    updated cypress spec

--- site/cypress/component/FullScreenButton.cy.js
+++ site/cypress/component/FullScreenButton.cy.js
@@ -66,38 +66,31 @@ describe('FullScreenButton', () => {
             cy.get('[data-testid="fullscreen-btn"]').type('{enter}');
             cy.get('@eventHandler').should('have.been.calledOnceWith', true);
         });
-
-        it('does not modify the DOM directly', () => {
-            mountWithEmitSpy(FullScreenButton, 'toggle', {});
-            cy.get('[data-testid="fullscreen-btn"]').click();
-            cy.get('main#main').should('not.exist');
-            cy.get('.full-screen-mode').should('not.exist');
-        });
     });
 
     describe('escape key', () => {
         it('emits false when Escape is pressed while full screen', () => {
             mountWithEmitSpy(FullScreenButton, 'toggle', { initialFullScreen: true });
-            cy.get('body').type('{esc}');
+            cy.get('[data-testid="content-main"]').type('{esc}');
             cy.get('@eventHandler').should('have.been.calledOnceWith', false);
         });
 
         it('does not emit when Escape is pressed while not full screen', () => {
             mountWithEmitSpy(FullScreenButton, 'toggle', {});
-            cy.get('body').type('{esc}');
+            cy.get('[data-testid="content-main"]').type('{esc}');
             cy.get('@eventHandler').should('not.have.been.called');
         });
 
         it('resets the icon when Escape exits full screen', () => {
             cy.mount(FullScreenButton, { props: { initialFullScreen: true } });
-            cy.get('body').type('{esc}');
+            cy.get('[data-testid="content-main"]').type('{esc}');
             cy.get('[data-testid="fullscreen-btn"] i').should('have.class', 'fa-expand');
         });
 
         it('emits only once when Escape is pressed twice', () => {
             mountWithEmitSpy(FullScreenButton, 'toggle', { initialFullScreen: true });
-            cy.get('body').type('{esc}');
-            cy.get('body').type('{esc}');
+            cy.get('[data-testid="content-main"]').type('{esc}');
+            cy.get('[data-testid="content-main"]').type('{esc}');
             cy.get('@eventHandler').should('have.been.calledOnce');
         });
     });
