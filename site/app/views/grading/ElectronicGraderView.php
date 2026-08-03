be76338b24a2eca6bbffbba785bd3f0a817f380a
Ansh Jindal <jndl.ansh@gmail.com>
2026-08-03 10:12:37 +0530

    [Refactor:TAGrading] Migrate PanelSelectorModal in TAGrading (#12901)
    ### Why is this Change Important & Necessary?
    In the ongoing migration of Submitty's UI from legacy Twig + jQuery to
    Vue.js, in this PR we migrate the panel selector modal present on the
    toolbar of the Grading page. The toolbar section has already been moved
    in PR [#11805](https://github.com/Submitty/Submitty/pull/11805) . The
    refactor is super minimal, the tests take up **300 lines of code**
    itself.
    
    ### What is the New Behavior?
    The behaviour is identical to the behavior on main.
    
    
    https://github.com/user-attachments/assets/d1ffdddb-d6f4-41d1-8696-359705b0a663
    
    
    
    ### What steps should a reviewer take to reproduce or test the bug or
    new feature?
    
    - Navigate to this URL :
    `http://localhost:1511/courses/s26/sample/gradeable/open_homework/grading/grade?who_id=IRVZDgfZwWgUsHi&sort=id&direction=ASC`
    or similar.
    - Click on the panel selector present of the toolbar.
    - Observe how the Panel Ui changes when u select from 9 different
    options.
    
    ### Automated Testing & Documentation
    
    - Cypress component tests shall be added.
    - 
    
    
    https://github.com/user-attachments/assets/f271a600-aafa-4c5c-aec8-b7ceb32cc479
    
    
    
    ### Other information
    - Not a Breaking Change.
    
    ---------
    
    Co-authored-by: Garvit Khandelwal <garvitkk2006@gmail.com>
    Co-authored-by: Justin Manion <jmanion32@gmail.com>

--- site/app/views/grading/ElectronicGraderView.php
+++ site/app/views/grading/ElectronicGraderView.php
@@ -1298,8 +1298,6 @@ HTML;
         $this->core->getOutput()->addInternalModuleJs('ta-grading-rubric-conflict.js');
         $this->core->getOutput()->addInternalJs('gradeable.js');
         $this->core->getOutput()->addInternalModuleJs('ta-grading-rubric.js');
-        $this->core->getOutput()->addInternalModuleJs('panel-selector-modal.js');
-        $this->core->getOutput()->addInternalModuleJs('panel-selector-modal-init.js');
         $this->core->getOutput()->addInternalModuleJs('ta-grading-keymap.js');
         $this->core->getOutput()->addInternalModuleJs('ta-grading-keymap-init.js');
         $this->core->getOutput()->addInternalModuleJs('ta-grading.js');
