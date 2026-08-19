1430586242d069c87a0983bedfd11d111535fb6e
Ansh Jindal <jndl.ansh@gmail.com>
2026-08-19 14:19:14 +0530

    address requested changes

--- site/vue/src/components/forum/ForumStatsTable.vue
+++ site/vue/src/components/forum/ForumStatsTable.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import SortableTableHeader from '../table_sorting/SortableTableHeader.vue';
-import { colDataTypes } from '../../../../ts/sort-table-by-column';
+import { colDataTypes, resolveSortToggle, type SortDirection } from '../../../../ts/sort-table-by-column';
 
 interface ForumPost {
     id: number;
@@ -31,7 +31,6 @@ const emit = defineEmits<{
 }>();
 
 type SortKey = 'user' | 'total_posts' | 'total_threads' | 'total_deleted' | 'total_upducks';
-type SortDirection = 'ASC' | 'DESC';
 
 const sortKey = ref<SortKey | null>(null);
 const sortDirection = ref<SortDirection>('ASC');
@@ -67,14 +66,9 @@ const sortedUsers = computed(() => {
 });
 
 function handleSort(payload: { sortKey: string }) {
-    const key = payload.sortKey as SortKey;
-    if (sortKey.value === key) {
-        sortDirection.value = sortDirection.value === 'ASC' ? 'DESC' : 'ASC';
-    }
-    else {
-        sortKey.value = key;
-        sortDirection.value = 'ASC';
-    }
+    const next = resolveSortToggle(sortKey.value, payload.sortKey, sortDirection.value);
+    sortKey.value = next.key as SortKey;
+    sortDirection.value = next.direction;
     // Collapse all expanded rows when a sortable header is clicked
     expandedUserIds.value = new Set();
 }
@@ -100,148 +94,150 @@ function navigateToThread(threadId: number) {
 </script>
 
 <template>
-  <colgroup>
-    <col style="width:15%">
-    <col>
-    <col style="width:15%">
-    <col style="width:15%">
-    <col style="width:15%">
-    <col style="width:25%">
-  </colgroup>
-  <thead>
-    <tr>
-      <th scope="col">
-        <SortableTableHeader
-          table-id="forum-stats-table"
-          title="User"
-          sort-key="user"
-          :col-data-type="colDataTypes.String"
-          :using-row-groups="true"
-          :active="sortKey === 'user'"
-          :sort-direction="sortDirection"
-          @sort-table-column-click="handleSort"
-        />
-      </th>
-      <th scope="col">
-        <SortableTableHeader
-          table-id="forum-stats-table"
-          title="Total Posts (not deleted)"
-          sort-key="total_posts"
-          :col-data-type="colDataTypes.Number"
-          :using-row-groups="true"
-          :active="sortKey === 'total_posts'"
-          :sort-direction="sortDirection"
-          @sort-table-column-click="handleSort"
-        />
-      </th>
-      <th scope="col">
-        <SortableTableHeader
-          table-id="forum-stats-table"
-          title="Total Threads"
-          sort-key="total_threads"
-          :col-data-type="colDataTypes.Number"
-          :using-row-groups="true"
-          :active="sortKey === 'total_threads'"
-          :sort-direction="sortDirection"
-          @sort-table-column-click="handleSort"
-        />
-      </th>
-      <th scope="col">
-        <SortableTableHeader
-          table-id="forum-stats-table"
-          title="Total Deleted Posts"
-          sort-key="total_deleted"
-          :col-data-type="colDataTypes.Number"
-          :using-row-groups="true"
-          :active="sortKey === 'total_deleted'"
-          :sort-direction="sortDirection"
-          @sort-table-column-click="handleSort"
-        />
-      </th>
-      <th scope="col">
-        <SortableTableHeader
-          table-id="forum-stats-table"
-          title="Total Upducks"
-          sort-key="total_upducks"
-          :col-data-type="colDataTypes.Number"
-          :using-row-groups="true"
-          :active="sortKey === 'total_upducks'"
-          :sort-direction="sortDirection"
-          @sort-table-column-click="handleSort"
-        />
-      </th>
-      <th scope="col">
-        Show Posts
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <template
-      v-for="user in sortedUsers"
-      :key="user.userId"
-    >
-      <tr
-        class="user_stat"
-        data-testid="user-stat"
-      >
-        <td>{{ user.familyName }}, {{ user.givenName }}</td>
-        <td>{{ user.postCount }}</td>
-        <td>{{ user.totalThreads }}</td>
-        <td>{{ user.numDeleted }}</td>
-        <td
-          class="upduck_stat"
-          data-testid="upduck-stat"
-        >
-          {{ user.totalUpducks }}
-        </td>
-        <td>
-          <button
-            v-if="user.posts.length > 0"
-            type="button"
-            class="btn btn-default"
-            data-testid="expand-button"
-            :title="isExpanded(user.userId) ? 'Collapse posts' : 'Expand posts'"
-            :aria-expanded="isExpanded(user.userId)"
-            @click="toggleExpand(user.userId)"
-          >
-            {{ isExpanded(user.userId) ? 'Collapse' : 'Expand' }}
-          </button>
-          <span v-else>No Posts</span>
-        </td>
+  <table class="table table-striped table-bordered persist-area">
+    <colgroup>
+      <col style="width:15%">
+      <col>
+      <col style="width:15%">
+      <col style="width:15%">
+      <col style="width:15%">
+      <col style="width:25%">
+    </colgroup>
+    <thead>
+      <tr>
+        <th scope="col">
+          <SortableTableHeader
+            table-id="forum-stats-table"
+            title="User"
+            sort-key="user"
+            :col-data-type="colDataTypes.String"
+            :using-row-groups="true"
+            :active="sortKey === 'user'"
+            :sort-direction="sortDirection"
+            @sort-table-column-click="handleSort"
+          />
+        </th>
+        <th scope="col">
+          <SortableTableHeader
+            table-id="forum-stats-table"
+            title="Total Posts (not deleted)"
+            sort-key="total_posts"
+            :col-data-type="colDataTypes.Number"
+            :using-row-groups="true"
+            :active="sortKey === 'total_posts'"
+            :sort-direction="sortDirection"
+            @sort-table-column-click="handleSort"
+          />
+        </th>
+        <th scope="col">
+          <SortableTableHeader
+            table-id="forum-stats-table"
+            title="Total Threads"
+            sort-key="total_threads"
+            :col-data-type="colDataTypes.Number"
+            :using-row-groups="true"
+            :active="sortKey === 'total_threads'"
+            :sort-direction="sortDirection"
+            @sort-table-column-click="handleSort"
+          />
+        </th>
+        <th scope="col">
+          <SortableTableHeader
+            table-id="forum-stats-table"
+            title="Total Deleted Posts"
+            sort-key="total_deleted"
+            :col-data-type="colDataTypes.Number"
+            :using-row-groups="true"
+            :active="sortKey === 'total_deleted'"
+            :sort-direction="sortDirection"
+            @sort-table-column-click="handleSort"
+          />
+        </th>
+        <th scope="col">
+          <SortableTableHeader
+            table-id="forum-stats-table"
+            title="Total Upducks"
+            sort-key="total_upducks"
+            :col-data-type="colDataTypes.Number"
+            :using-row-groups="true"
+            :active="sortKey === 'total_upducks'"
+            :sort-direction="sortDirection"
+            @sort-table-column-click="handleSort"
+          />
+        </th>
+        <th scope="col">
+          Show Posts
+        </th>
       </tr>
-      <template v-if="isExpanded(user.userId)">
+    </thead>
+    <tbody>
+      <template
+        v-for="user in sortedUsers"
+        :key="user.userId"
+      >
         <tr
-          v-for="post in user.posts"
-          :key="post.id"
-          class="forum-stats-detail-row"
-          data-testid="post-detail-row"
+          class="user_stat"
+          data-testid="user-stat"
         >
-          <td />
-          <td>{{ post.timestamp }}</td>
+          <td>{{ user.familyName }}, {{ user.givenName }}</td>
+          <td>{{ user.postCount }}</td>
+          <td>{{ user.totalThreads }}</td>
+          <td>{{ user.numDeleted }}</td>
           <td
-            style="cursor:pointer;"
-            data-testid="post-thread-cell"
-            @click="navigateToThread(post.threadId)"
+            class="upduck_stat"
+            data-testid="upduck-stat"
           >
-            <pre
-              class="pre-forum"
-              style="white-space: pre-wrap;"
-            >{{ post.threadTitle }}</pre>
+            {{ user.totalUpducks }}
           </td>
-          <td
-            colspan="2"
-            style="cursor:pointer; text-align:left;"
-            data-testid="post-content-cell"
-            @click="navigateToThread(post.threadId)"
-          >
-            <pre
-              class="pre-forum"
-              style="white-space: pre-wrap;"
-            >{{ post.content }}</pre>
+          <td>
+            <button
+              v-if="user.posts.length > 0"
+              type="button"
+              class="btn btn-default"
+              data-testid="expand-button"
+              :title="isExpanded(user.userId) ? 'Collapse posts' : 'Expand posts'"
+              :aria-expanded="isExpanded(user.userId)"
+              @click="toggleExpand(user.userId)"
+            >
+              {{ isExpanded(user.userId) ? 'Collapse' : 'Expand' }}
+            </button>
+            <span v-else>No Posts</span>
           </td>
-          <td />
         </tr>
+        <template v-if="isExpanded(user.userId)">
+          <tr
+            v-for="post in user.posts"
+            :key="post.id"
+            class="forum-stats-detail-row"
+            data-testid="post-detail-row"
+          >
+            <td />
+            <td>{{ post.timestamp }}</td>
+            <td
+              style="cursor:pointer;"
+              data-testid="post-thread-cell"
+              @click="navigateToThread(post.threadId)"
+            >
+              <pre
+                class="pre-forum"
+                style="white-space: pre-wrap;"
+              >{{ post.threadTitle }}</pre>
+            </td>
+            <td
+              colspan="2"
+              style="cursor:pointer; text-align:left;"
+              data-testid="post-content-cell"
+              @click="navigateToThread(post.threadId)"
+            >
+              <pre
+                class="pre-forum"
+                style="white-space: pre-wrap;"
+              >{{ post.content }}</pre>
+            </td>
+            <td />
+          </tr>
+        </template>
       </template>
-    </template>
-  </tbody>
+    </tbody>
+  </table>
 </template>
